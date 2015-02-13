124a300a979e420d10cb178c1cdfd7c3e81df597
Peter Honeder <peterhon@microsoft.com>
2015-02-13 14:36:52 -0800

    fixed prefix to use abs path and switched to libc++ with clang

--- scripts/simplebuild.sh
+++ scripts/simplebuild.sh
@@ -7,7 +7,11 @@ fi
 
 pushd ext-src/cppunit
 ./autogen.sh
-./configure --prefix=../../externals/cppunit
+CWD=$(pwd)
+if [ -f Makefile ]; then
+  make distclean
+fi
+./configure LD=clang LDFLAGS="-stdlib=libc++" CXX=clang++ CC=clang CXXFLAGS="-stdlib=libc++" --prefix=$CWD/../../externals/cppunit
 make
 make install
 
