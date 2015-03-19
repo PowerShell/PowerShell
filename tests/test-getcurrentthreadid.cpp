0c5f78a223210371576a5c5ec7cda2ae035196bc
Pete Baker <v-pebak@microsoft.com>
2015-03-19 13:09:36 -0700

    built and tested - pass

---
+++ tests/test-getcurrentthreadid.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "getcurrentthreadid.h"
+#include <pthread.h>
+
+TEST(GetCurrentThreadId,simple)
+{
+	const HANDLE currentThreadId = GetCurrentThreadId();
+	const pid_t tid = pthread_self();
+
+	// first make sure that on this platform those types are of the same size
+	ASSERT_TRUE(sizeof(HANDLE) >= sizeof(pid_t));
+	
+	// now compare the actual values
+	ASSERT_EQ(currentThreadId,reinterpret_cast<HANDLE>(tid));
+}
+
