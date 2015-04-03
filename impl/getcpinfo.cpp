c84dfa9b63056325345d303d22de631c79a13584
Aaron Katz <v-aakatz@microsoft.com>
2015-04-03 11:06:30 -0700

    Changes from notes

--- impl/getcpinfo.cpp
+++ impl/getcpinfo.cpp
@@ -1,27 +1,28 @@
 #include "getcpinfo.h"
 #include <unistd.h>
+#include <string>
 
-bool GetCPInfo(unsigned int codepage, LPCPINFO cpinfo)
+BOOL GetCPInfo(UINT codepage, CPINFO &cpinfo)
 {
+  std::string test;
   switch(codepage)
   {
     case 65000:
-      	 cpinfo->DefaultChar[0]= '?';
-	 cpinfo->DefaultChar[1]= '0';
-	 cpinfo->LeadByte[0] = cpinfo->LeadByte[1] = '0';
-	 cpinfo->MaxCharSize =  5;
-	 return true;
-      break;
+      	 cpinfo.DefaultChar[0]= '?';
+	 cpinfo.DefaultChar[1]= '0';
+	 cpinfo.LeadByte[0] = '0';
+	 cpinfo.LeadByte[1] = '0';
+	 cpinfo.MaxCharSize =  5;
+      return TRUE;
     case 65001:
-      	 cpinfo->DefaultChar[0]= '?';
-	 cpinfo->DefaultChar[1]= '0';
-	 cpinfo->LeadByte[0] = cpinfo->LeadByte[1] = '0';
-	 cpinfo->MaxCharSize =  4;
-      return true;
-      break;
+      	 cpinfo.DefaultChar[0]= '?';
+	 cpinfo.DefaultChar[1]= '0';
+	 cpinfo.LeadByte[0] = '0';
+	 cpinfo.LeadByte[1] = '0';
+	 cpinfo.MaxCharSize =  4;
+      return TRUE;
     default:
-      return false;
+      return FALSE;
   }
-
 }
 
--- tests/test-getcpinfo.cpp
+++ tests/test-getcpinfo.cpp
@@ -4,37 +4,36 @@
 // This is a very simple test case to show how tests can be written
 TEST(GetCPInfo,utf8)
 {
-  
-	LPCPINFO cpinfo = new CPINFO();
-	bool utf8 = GetCPInfo(65001, cpinfo);
+	CPINFO cpinfo;
+	BOOL utf8 = GetCPInfo(65001, cpinfo);
 	
 
-	// first make sure that on this platform those types are of the same size
-	ASSERT_TRUE(utf8 == 1);
+	// first make sure that the function worked
+	ASSERT_TRUE(utf8 == TRUE);
 	
 	// now compare the actual values
-	ASSERT_EQ(cpinfo->DefaultChar[0],'?');
-	ASSERT_EQ(cpinfo->DefaultChar[1],'0');
-	ASSERT_EQ(cpinfo->LeadByte[0] ,'0');
-	ASSERT_EQ(cpinfo->LeadByte[1] ,'0');
-	ASSERT_EQ(cpinfo->MaxCharSize ,4);
+	ASSERT_EQ(cpinfo.DefaultChar[0],'?');
+	ASSERT_EQ(cpinfo.DefaultChar[1],'0');
+	ASSERT_EQ(cpinfo.LeadByte[0] ,'0');
+	ASSERT_EQ(cpinfo.LeadByte[1] ,'0');
+	ASSERT_EQ(cpinfo.MaxCharSize ,4);
 	
 }
 
 
 TEST(GetCPInfo,utf7)
 {
-	LPCPINFO cpinfo = new CPINFO();
-	bool utf7 = GetCPInfo(65000, cpinfo);
+	CPINFO cpinfo;
+	BOOL utf7 = GetCPInfo(65000, cpinfo);
 	
 
-	// first make sure that on this platform those types are of the same size
-	ASSERT_TRUE(utf7 == 1);
+	// first make sure that the function worked
+	ASSERT_TRUE(utf7 == TRUE);
 	
 	// now compare the actual values
-	ASSERT_EQ(cpinfo->DefaultChar[0],'?');
-	ASSERT_EQ(cpinfo->DefaultChar[1],'0');
-	ASSERT_EQ(cpinfo->LeadByte[0] ,'0');
-	ASSERT_EQ(cpinfo->LeadByte[1] ,'0');
-	ASSERT_EQ(cpinfo->MaxCharSize ,5);
+	ASSERT_EQ(cpinfo.DefaultChar[0],'?');
+	ASSERT_EQ(cpinfo.DefaultChar[1],'0');
+	ASSERT_EQ(cpinfo.LeadByte[0] ,'0');
+	ASSERT_EQ(cpinfo.LeadByte[1] ,'0');
+	ASSERT_EQ(cpinfo.MaxCharSize ,5);
 }
