e4df047464f0ec3fa8391b54434ea68ea5033823
Andrew Schwartzmeyer <anschwa@microsoft.com>
2015-06-05 16:41:43 -0700

    Use Xunit assertions
    * Update monad-ext submodule with Xunit dlls
    * Point Makefile xunit targets to monad-ext/xunit
    * Add xunit.assert target to Makefile
    * Add xunit.assert as dependency of ps_test.exe
    * Add corerun as dependency of ps_test.exe
    * Link ps_test.exe with xunit.assert
    * Temporarily remove libps.so from ps_test.exe dependencies (WIP)
    * Use Xunit in ps_test.cs
    * Remove Assert mock
    * Temporarily remove Platform.UserName (WIP)
    
    With this patch, `make cleanall && make prepare && make run-test`
    executes our (few) tests correctly using Xunit (without needing to `make
    run` first).

--- src/ps_test/ps_test.cs
+++ src/ps_test/ps_test.cs
@@ -1,18 +1,8 @@
-//using Xunit;
+using Xunit;
 using System;
 
 namespace PSTests
 {
-	public static class Assert
-	{
-		public static void Equal<T>(T a, T b) where T : System.IComparable<T>
-		{
-			if (a.CompareTo(b) != 0)
-				throw new Exception("Assert failed: a == b (" + a.ToString() + " == " + b.ToString());
-			Console.WriteLine("Assert.Equal OK");
-		}
-	}
-
 	public class BlahTests
 	{
 
@@ -25,7 +15,8 @@ namespace PSTests
 		public static void testPlatform()
 		{
 			Assert.Equal(System.Management.Automation.Platform.IsLinux(),true);
-			Assert.Equal(System.Management.Automation.Platform.UserName,"peter2");
+			// UserName not yet implemented
+			// Assert.Equal(System.Management.Automation.Platform.UserName,"peter2");
 		}
 
 		static void Main(string[] args)
