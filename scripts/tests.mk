cd0d85049c3d3f5a8c23078968da23fc4aae67a0
Andrew Schwartzmeyer <anschwa@microsoft.com>
2015-06-12 08:29:05 -0700

    Factor run-test dependencies out of Makefile
    - tests.mk includes all test related code except run-test target
    - run-test moved after run target
    - TEST_SRCS uses wildcard to collect files
    - dotnetlibs/xunit* uses wildcard to copy dependencies for monad-ext
      - not %.dll because of the soon to be used xunit.console.netcore.exe

---
+++ scripts/tests.mk
@@ -0,0 +1,11 @@
+TEST_FOLDER=../src/ps_test
+TEST_SRCS=$(addprefix $(TEST_FOLDER)/, test_*.cs)
+
+dotnetlibs/xunit%: $(MONAD_EXT)/xunit/xunit%
+	cp -f $^ $@
+
+dotnetlibs/ps_test.dll: $(TEST_SRCS) dotnetlibs/xunit.core.dll dotnetlibs/xunit.assert.dll dotnetlibs/System.Management.Automation.dll dotnetlibs/Microsoft.PowerShell.Commands.Management.dll dotnetlibs/$(ASSEMBLY_LOAD_CONTEXT_TARGET)
+	$(CSC) -out:$@ -noconfig -nostdlib -target:library -r:dotnetlibs/System.Management.Automation.dll -r:dotnetlibs/$(ASSEMBLY_LOAD_CONTEXT_TARGET) -r:dotnetlibs/xunit.core.dll -r:dotnetlibs/xunit.assert.dll ${COREREF} $(TEST_SRCS)
+
+dotnetlibs/ps_test_runner.exe: $(TEST_FOLDER)/ps_test.cs dotnetlibs/ps_test.dll dotnetlibs/System.Management.Automation.dll dotnetlibs/Microsoft.PowerShell.Commands.Management.dll dotnetlibs/$(ASSEMBLY_LOAD_CONTEXT_TARGET) dotnetlibs/xunit.core.dll dotnetlibs/xunit.assert.dll
+	$(CSC) -out:$@ -noconfig -nostdlib -target:exe -r:dotnetlibs/ps_test.dll -r:dotnetlibs/System.Management.Automation.dll -r:dotnetlibs/$(ASSEMBLY_LOAD_CONTEXT_TARGET) -r:dotnetlibs/xunit.core.dll -r:dotnetlibs/xunit.assert.dll ${COREREF} $<
