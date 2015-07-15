2b079b2cb95ef55e9d95c7e65b784d87a1efd522
Zachary Folwick <v-zafolw@microsoft.com>
2015-07-15 11:40:03 -0700

    removing commit

--- src/pester-tests/Get-HelloWorld.Tests.ps1
+++
@@ -1,9 +0,0 @@
-﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-. "$here\$sut"
-
-Describe "Get-HelloWorld" {
-    It "outputs 'HelloWorld!'" {
-        Get-HelloWorld | Should Be 'HelloWorld!'
-    }
-}
--- src/pester-tests/Get-HelloWorld.ps1
+++
@@ -1,3 +0,0 @@
-﻿function Get-HelloWorld{
-	'HelloWorld!'
-}
