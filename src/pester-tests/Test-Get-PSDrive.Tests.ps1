59015da5a2adaf5bb88b9de6faf8b22de112affe
Aaron <v-aakatz@microsoft.com>
2015-07-21 10:08:09 -0700

    initial commit

---
+++ src/pester-tests/Test-Get-PSDrive.Tests.ps1
@@ -0,0 +1,32 @@
+﻿
+Describe "Test-Get-PSDrive" {
+    It "Should not throw" {
+        Get-PSDrive | Should Not BeNullOrEmpty
+        (Get-PSDrive).Name | Should Not BeNullOrEmpty 
+        (Get-PSDrive).Root.Length | Should Not BeLessThan 1
+
+        gdr | Should Not BeNullOrEmpty
+        (gdr).Name | Should Not BeNullOrEmpty 
+        (gdr).Root.Length | Should Not BeLessThan 1
+    }
+
+    It "Should return drive info"{
+        (Get-PSDrive D).Name | Should Be D
+        (Get-PSDrive D).Root | Should Be D:\
+        (Get-PSDrive D).Provider.Name | Should Be FileSystem
+
+        (gdr D).Name | Should Be D
+        (gdr D).Root | Should Be D:\
+        (gdr D).Provider.Name | Should Be FileSystem
+    }
+
+    It "Should be able to access switches"{
+        (Get-PSDrive -PSProvider FileSystem).Name.Length | Should BeGreaterThan 0
+
+        (gdr -PSProvider FileSystem).Name.Length | Should BeGreaterThan 0
+    }
+
+    It "Should return true for a drive that does not exist"{
+        !(Get-PSDrive fake -ErrorAction SilentlyContinue) | Should Be $True
+    }
+}
