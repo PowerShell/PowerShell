a18a5912ab92c78abb17909baf4003c75d73427a
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-07-22 10:15:45 -0700

    Explain GetUserName signature

--- impl/getusername.h
+++ impl/getusername.h
@@ -4,7 +4,8 @@
 
 PAL_BEGIN_EXTERNC
 
-BOOL GetUserName(WCHAR_T* lpBuffer, LPDWORD lpnSize);
+// WCHAR_T * is a Unicode LPTSTR
+// See https://msdn.microsoft.com/en-us/library/windows/desktop/aa383751(v=vs.85).aspx#LPTSTR
+BOOL GetUserName(WCHAR_T *lpBuffer, LPDWORD lpnSize);
 
 PAL_END_EXTERNC
-
