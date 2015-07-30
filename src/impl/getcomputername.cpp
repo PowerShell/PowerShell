bf92f710e87bd5663fd7375dd4ac0d63ff0fd3c1
Aaron <v-aakatz@microsoft.com>
2015-07-30 12:46:28 -0700

    Added case for failure

--- src/impl/getcomputername.cpp
+++ src/impl/getcomputername.cpp
@@ -1,12 +1,19 @@
 #include "getcomputername.h"
 #include <unistd.h>
+#include <errno.h>
 
 BOOL GetComputerName(LPTSTR name, LPDWORD len)
 {
+    errno = 0;
+    
     int host =  gethostname(name, HOST_NAME_MAX);
     if(host == 0)
     {
         return TRUE;
-    }  
-    return FALSE;
+    }
+    else 
+    {
+        errno = ERROR_BUFFER_OVERFLOW;
+        return FALSE;
+    }
 }
