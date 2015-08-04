f24dfb02dc3b77290e31d8c1b2a9758c9c47b126
Zachary Folwick <v-zafolw@microsoft.com>
2015-08-04 16:07:14 -0700

    added Format-List test

---
+++ src/pester-tests/Test-Format-List.Tests.ps1
@@ -0,0 +1,36 @@
+Describe "Test-Format-List" {
+    It "Should call format list without error" {
+        { Get-Process | Format-List } | Should Not Throw
+
+        { Get-Process | fl } | Should Not Throw
+    }
+
+    It "Should be able to call format list on piped in variable objects" {
+        $a = Get-Process
+
+        { $a | Format-List } | Should Not Throw
+
+        { $a | fl } | Should Not Throw
+    }
+
+    It "Should be able to call a property of the piped input" {
+        #Tested on two input commands to veryify functionality.
+        $testCommand1 = Get-Process
+        $testCommand2 = Get-Date
+
+        { $testCommand1 | Format-List -Property Name }        | Should Not Throw
+        { $testCommand2 | Format-List -Property DisplayName } | Should Not Throw
+
+
+        { $testCommand1 | fl -Property Name }        | Should Not Throw
+        { $testCommand2 | fl -Property DisplayName } | Should Not Throw
+    }
+
+    It "Should be able to display a list of props when separated by a comma" {
+        $testCommand = Get-Process
+
+        { $testCommand | Format-List -Property Name,BasePriority }| Should Not Throw
+
+        { $testCommand | fl -Property Name,BasePriority } | Should Not Throw
+    }
+}
