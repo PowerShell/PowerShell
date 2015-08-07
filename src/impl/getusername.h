062eb5a7b4e8be20bbde962239abfc6843d10076
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-08-06 17:06:04 -0700

    Fix style issues
    - Use Doxygen style comments (with better comments)
    - Use `type* ident` instead of `type *ident`
    - Start { on new lines
    - Use TRUE/FALSE from PAL.H

--- src/impl/getusername.h
+++ src/impl/getusername.h
@@ -4,8 +4,6 @@
 
 PAL_BEGIN_EXTERNC
 
-// WCHAR_T * is a Unicode LPTSTR
-// See https://msdn.microsoft.com/en-us/library/windows/desktop/aa383751(v=vs.85).aspx#LPTSTR
-BOOL GetUserNameW(WCHAR_T *lpBuffer, LPDWORD lpnSize);
+BOOL GetUserNameW(WCHAR_T* lpBuffer, LPDWORD lpnSize);
 
 PAL_END_EXTERNC
