8456bf9d4d1450c413a7a0c66890795688079096
Zachary Folwick <v-zafolw@microsoft.com>
2015-08-26 15:13:12 -0700

    added modifications from code review.

--- src/pester-tests/Test-Remove-Item.Tests.ps1
+++ src/pester-tests/Test-Remove-Item.Tests.ps1
@@ -4,13 +4,10 @@
     $testfilepath = $testpath + $testfile
     Context "File removal Tests" {
         BeforeEach {
-            # redundant code in case the AfterEach fails
-            if (Test-Path $testfilepath)
-            {
-                Remove-Item $testfilepath -Force
-            }
+            New-Item -Name $testfile -Path $testpath -ItemType "file" -Value "lorem ipsum" -Force
 
-            New-Item -Name $testfile -Path $testpath -ItemType "file" -Value "lorem ipsum"
+            Test-Path $testfilepath | Should Be $true
+
         }
 
         It "Should be able to be called on a regular file without error using the Path switch" {
@@ -54,18 +51,15 @@
             Set-ItemProperty -Path $testfilepath -Name IsReadOnly -Value $true
 
             # attempt to remove the file
-            { Remove-Item $testfilepath -ErrorAction SilentlyContinue } | Should Not Throw
+            { Remove-Item $testfilepath  } | Should Not Throw
 
             # validate
             Test-Path $testfilepath | Should Be $true
 
-            # set to not be read only
-            Set-ItemProperty -Path $testfilepath -Name IsReadOnly -Value $false
-
-            # remove
+            # remove using the -force switch on the readonly object
             Remove-Item  $testfilepath -Force
 
-            # Validate 
+            # Validate
             Test-Path $testfilepath | Should Be $false
         }
 
@@ -82,10 +76,12 @@
             Test-path /tmp/file1.txt | Should Be $false
             Test-path /tmp/file2.txt | Should Be $false
             Test-path /tmp/file3.txt | Should Be $false
-            Test-Path $testfilepath   | Should Be $true
+            Test-Path $testfilepath  | Should Be $true
 
             # Delete the non-matching strings
             Remove-Item $testfilepath
+
+            Test-Path $testfilepath  | Should Be $false
         }
 
         It "Should be able to not remove any files matching a regular expression with the exclude switch" {
@@ -95,7 +91,6 @@
 
             # Create a single file that does not match that string
             New-Item -Name file1.txt -Path $testpath -ItemType "file" -Value "lorem ipsum"
-            New-Item -Name file2.txt -Path $testpath -ItemType "file" -Value "lorem ipsum"
 
             # Delete the specific string
             Remove-Item /tmp/file* -Exclude *.wav -Include *.txt
@@ -104,7 +99,6 @@
             Test-Path /tmp/file1.wav | Should Be $true
             Test-Path /tmp/file2.wav | Should Be $true
             Test-Path /tmp/file1.txt | Should Be $false
-            Test-path /tmp/file2.txt | Should Be $false
 
             # Delete the non-matching strings
             Remove-Item /tmp/file1.wav
@@ -118,17 +112,10 @@
     Context "Directory Removal Tests" {
         $testdirectory = "/tmp/testdir"
         $testsubdirectory = $testdirectory + "/subd"
-        BeforeEach{
-            if (Test-Path $testdirectory)
-            {
-                Remove-Item $testdirectory -Force
-            }
+        BeforeEach {
+            New-Item -Name "testdir" -Path "/tmp/" -ItemType "directory" -Force
 
-            test-path $testdirectory | Should Be $false
-
-            New-Item -Name "testdir" -Path "/tmp/" -ItemType "directory"
-
-            test-path $testdirectory | Should Be $true
+            Test-Path $testdirectory | Should Be $true
         }
 
         It "Should be able to remove a directory" {
