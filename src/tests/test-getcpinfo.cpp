a1f94e6c702c0a4bbb4f1dcba7dfac7ed518e9d9
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-08-28 15:57:31 -0700

    Fix GetCpInfo
    - Move cpinfo related code out of pal.h and into getcpinfo.h
    - Use static cpinfo struct (not pointer) in test and pass address
    - Whitespace cleanup

--- src/tests/test-getcpinfo.cpp
+++ src/tests/test-getcpinfo.cpp
@@ -5,33 +5,30 @@
 #include <gtest/gtest.h>
 #include "getcpinfo.h"
 
-// This test is with correct parameters
-TEST(GetCPInfo,Utf8)
+TEST(GetCPInfo, CodePageIsUTF8)
 {
-    CPINFO* cpinfo = new CPINFO();
-    int UTF8CodePageNumber = const_cpinfo::UTF8;
-    BOOL result = GetCPInfoW(UTF8CodePageNumber, cpinfo);
+    CPINFO cpinfo;
+    BOOL result = GetCPInfoW(const_cpinfo::UTF8, &cpinfo);
 
     // first make sure that the function worked
     ASSERT_EQ(TRUE, result);
-    
+
     // now compare the actual values
-    EXPECT_EQ(cpinfo->DefaultChar[0], '?');
-    EXPECT_EQ(cpinfo->DefaultChar[1], '0');
-    EXPECT_EQ(cpinfo->MaxCharSize,4);
+    EXPECT_EQ(cpinfo.DefaultChar[0], '?');
+    EXPECT_EQ(cpinfo.DefaultChar[1], '0');
+    EXPECT_EQ(cpinfo.MaxCharSize, 4);
+
     for (int i = 0; i < const_cpinfo::MAX_LEADBYTES; i++)
     {
-        EXPECT_EQ(cpinfo->LeadByte[i], '0');
+        EXPECT_EQ(cpinfo.LeadByte[i], '0');
     }
 }
 
-// This test is with codepage not being utf8
-TEST(GetCPInfo, CodePageNotUTF8)
+TEST(GetCPInfo, CodePageIsNotUTF8)
 {
-    CPINFO* cpinfo = new CPINFO();;
-    BOOL result = GetCPInfoW(65000, cpinfo);
-    
+    CPINFO cpinfo;
+    BOOL result = GetCPInfoW(const_cpinfo::UTF8+1, &cpinfo);
+
     ASSERT_EQ(FALSE, result);
     ASSERT_EQ(ERROR_INVALID_PARAMETER, errno);
-    
 }
