5a5169ac60e1400a7058ea22ef498b722890c770
Zachary Folwick <v-zafolw@microsoft.com>
2015-10-20 12:17:42 -0700

    Added import-csv test suite

---
+++ src/pester-tests/Test-Import-Csv.Tests.ps1
@@ -0,0 +1,51 @@
+﻿Describe "Test-Import-Csv" {
+    $testCsv = "./TestAssets/TestCsv.csv"
+
+    It "Should be able to call without error" {
+        { Import-Csv $testCsv } | Should Not Throw
+    }
+
+    It "Should be able to assign to a variable" {
+        $actual = Import-Csv $testCsv
+
+        $actual                    | Should Not BeNullOrEmpty
+        $actual.GetType().BaseType | Should Be array
+    }
+
+    It "Should have the data from the csv file" {
+        $actualContent = $(Get-Content $testCsv)[0]
+        $testContent   = $($(Import-Csv $testCsv) | Get-Member) | ? { $_.MemberType -eq "NoteProperty" } | % { $_.Name } | Select-Object -First 1
+
+        $actualContent.IndexOf($testContent) | Should BeGreaterThan -1
+    }
+
+    It "Should be able to prepend a custom header" {
+        $header = "test1","test2","test3"
+
+        $originalContent = $($(Import-Csv $testCsv) | Get-Member) | ? { $_.MemberType -eq "NoteProperty" } | % { $_.Name } | Select-Object -First 1
+
+        $testContent = $($(Import-Csv $testCsv -Header $header) | Get-Member) | ? { $_.MemberType -eq "NoteProperty" } | % { $_.Name } | Select-Object -First 3
+
+        # the original csv file doesn't contain the headers
+        $originalContent.IndexOf($header[0]) | Should Be -1
+
+        # but it does with the -Header switch!
+        $testContent[0] | Should Be $header[0]
+        $testContent[1] | Should Be $header[1]
+        $testContent[2] | Should Be $header[2]
+    }
+
+    It "Should be able to use the alias without error" {
+        { Import-Csv $testCsv } | Should Not Throw
+    }
+
+    It "Should have the same output between the alias and the full cmdlet name" {
+        $alias  = $($(ipcsv $testCsv) | Get-Member) | ? { $_.MemberType -eq "NoteProperty" } | % { $_.Name } | Select-Object -First 1
+        $cmdlet = $($(Import-Csv $testCsv) | Get-Member) | ? { $_.MemberType -eq "NoteProperty" } | % { $_.Name } | Select-Object -First 1
+
+        $alias[0] | Should Be $cmdlet[0]
+        $alias[1] | Should Be $cmdlet[1]
+        $alias[2] | Should Be $cmdlet[2]
+
+    }
+}
