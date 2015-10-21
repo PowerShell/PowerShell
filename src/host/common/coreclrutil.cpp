fb9c2424c3c80b1b5b789f34fc62f97c401bf58e
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-10-21 15:22:57 -0700

    Reindent host code using project standards

--- src/host/common/coreclrutil.cpp
+++ src/host/common/coreclrutil.cpp
@@ -7,103 +7,102 @@
 
 namespace CoreCLRUtil
 {
-
-bool GetAbsolutePath(const char* path, std::string& absolutePath)
-{
-    bool result = false;
-
-    char realPath[PATH_MAX];
-    if (realpath(path, realPath) != nullptr && realPath[0] != '\0')
+    bool GetAbsolutePath(const char* path, std::string& absolutePath)
     {
-        absolutePath.assign(realPath);
-        // realpath should return canonicalized path without the trailing slash
-        assert(absolutePath.back() != '/');
+        bool result = false;
 
-        result = true;
-    }
-
-    return result;
-}
-
-bool GetDirectory(const char* absolutePath, std::string& directory)
-{
-    directory.assign(absolutePath);
-    size_t lastSlash = directory.rfind('/');
-    if (lastSlash != std::string::npos)
-    {
-        directory.erase(lastSlash);
-        return true;
-    }
-
-    return false;
-}
-
-bool GetClrFilesAbsolutePath(const char* currentExePath, const char* clrFilesPath, std::string& clrFilesAbsolutePath)
-{
-    std::string clrFilesRelativePath;
-    const char* clrFilesPathLocal = clrFilesPath;
-    if (clrFilesPathLocal == nullptr)
-    {
-        // There was no CLR files path specified, use the folder of the corerun/coreconsole
-        if (!GetDirectory(currentExePath, clrFilesRelativePath))
+        char realPath[PATH_MAX];
+        if (realpath(path, realPath) != nullptr && realPath[0] != '\0')
         {
-            perror("Failed to get directory from argv[0]");
-            return false;
+            absolutePath.assign(realPath);
+            // realpath should return canonicalized path without the trailing slash
+            assert(absolutePath.back() != '/');
+
+            result = true;
         }
 
-        clrFilesPathLocal = clrFilesRelativePath.c_str();
-
-        // TODO: consider using an env variable (if defined) as a fall-back.
-        // The windows version of the corerun uses core_root env variable
+        return result;
     }
 
-    if (!GetAbsolutePath(clrFilesPathLocal, clrFilesAbsolutePath))
+    bool GetDirectory(const char* absolutePath, std::string& directory)
     {
-        perror("Failed to convert CLR files path to absolute path");
+        directory.assign(absolutePath);
+        size_t lastSlash = directory.rfind('/');
+        if (lastSlash != std::string::npos)
+        {
+            directory.erase(lastSlash);
+            return true;
+        }
+
         return false;
     }
 
-    return true;
-}
-
-void AddFilesFromDirectoryToTpaList(const char* directory, std::string& tpaList)
-{
-    const char * const tpaExtensions[] = {
-                ".ni.dll",      // Probe for .ni.dll first so that it's preferred if ni and il coexist in the same dir
-                ".dll",
-                ".ni.exe",
-                ".exe",
-                };
-                
-    DIR* dir = opendir(directory);
-    if (dir == nullptr)
+    bool GetClrFilesAbsolutePath(const char* currentExePath, const char* clrFilesPath, std::string& clrFilesAbsolutePath)
     {
-        return;
+        std::string clrFilesRelativePath;
+        const char* clrFilesPathLocal = clrFilesPath;
+        if (clrFilesPathLocal == nullptr)
+        {
+            // There was no CLR files path specified, use the folder of the corerun/coreconsole
+            if (!GetDirectory(currentExePath, clrFilesRelativePath))
+            {
+                perror("Failed to get directory from argv[0]");
+                return false;
+            }
+
+            clrFilesPathLocal = clrFilesRelativePath.c_str();
+
+            // TODO: consider using an env variable (if defined) as a fall-back.
+            // The windows version of the corerun uses core_root env variable
+        }
+
+        if (!GetAbsolutePath(clrFilesPathLocal, clrFilesAbsolutePath))
+        {
+            perror("Failed to convert CLR files path to absolute path");
+            return false;
+        }
+
+        return true;
     }
 
-    std::set<std::string> addedAssemblies;
-
-    // Walk the directory for each extension separately so that we first get files with .ni.dll extension,
-    // then files with .dll extension, etc.
-    for (int extIndex = 0; extIndex < sizeof(tpaExtensions) / sizeof(tpaExtensions[0]); extIndex++)
+    void AddFilesFromDirectoryToTpaList(const char* directory, std::string& tpaList)
     {
-        const char* ext = tpaExtensions[extIndex];
-        int extLength = strlen(ext);
-
-        struct dirent* entry;
-
-        // For all entries in the directory
-        while ((entry = readdir(dir)) != nullptr)
+        const char * const tpaExtensions[] = {
+            ".ni.dll",      // Probe for .ni.dll first so that it's preferred if ni and il coexist in the same dir
+            ".dll",
+            ".ni.exe",
+            ".exe",
+        };
+                
+        DIR* dir = opendir(directory);
+        if (dir == nullptr)
         {
-            // We are interested in files only
-            switch (entry->d_type)
-            {
-            case DT_REG:
-                break;
+            return;
+        }
 
-            // Handle symlinks and file systems that do not support d_type
-            case DT_LNK:
-            case DT_UNKNOWN:
+        std::set<std::string> addedAssemblies;
+
+        // Walk the directory for each extension separately so that we first get files with .ni.dll extension,
+        // then files with .dll extension, etc.
+        for (int extIndex = 0; extIndex < sizeof(tpaExtensions) / sizeof(tpaExtensions[0]); extIndex++)
+        {
+            const char* ext = tpaExtensions[extIndex];
+            int extLength = strlen(ext);
+
+            struct dirent* entry;
+
+            // For all entries in the directory
+            while ((entry = readdir(dir)) != nullptr)
+            {
+                // We are interested in files only
+                switch (entry->d_type)
+                {
+                case DT_REG:
+                    break;
+
+                    // Handle symlinks and file systems that do not support d_type
+                case DT_LNK:
+                case DT_UNKNOWN:
                 {
                     std::string fullFilename;
 
@@ -124,44 +123,42 @@ void AddFilesFromDirectoryToTpaList(const char* directory, std::string& tpaList)
                 }
                 break;
 
-            default:
-                continue;
+                default:
+                    continue;
+                }
+
+                std::string filename(entry->d_name);
+
+                // Check if the extension matches the one we are looking for
+                int extPos = filename.length() - extLength;
+                if ((extPos <= 0) || (filename.compare(extPos, extLength, ext) != 0))
+                {
+                    continue;
+                }
+
+                std::string filenameWithoutExt(filename.substr(0, extPos));
+
+                // Make sure if we have an assembly with multiple extensions present,
+                // we insert only one version of it.
+                if (addedAssemblies.find(filenameWithoutExt) == addedAssemblies.end())
+                {
+                    addedAssemblies.insert(filenameWithoutExt);
+
+                    tpaList.append(directory);
+                    tpaList.append("/");
+                    tpaList.append(filename);
+                    tpaList.append(":");
+                }
             }
-
-            std::string filename(entry->d_name);
-
-            // Check if the extension matches the one we are looking for
-            int extPos = filename.length() - extLength;
-            if ((extPos <= 0) || (filename.compare(extPos, extLength, ext) != 0))
-            {
-                continue;
-            }
-
-            std::string filenameWithoutExt(filename.substr(0, extPos));
-
-            // Make sure if we have an assembly with multiple extensions present,
-            // we insert only one version of it.
-            if (addedAssemblies.find(filenameWithoutExt) == addedAssemblies.end())
-            {
-                addedAssemblies.insert(filenameWithoutExt);
-
-                tpaList.append(directory);
-                tpaList.append("/");
-                tpaList.append(filename);
-                tpaList.append(":");
-            }
-        }
         
-        // Rewind the directory stream to be able to iterate over it for the next extension
-        rewinddir(dir);
-    }
+            // Rewind the directory stream to be able to iterate over it for the next extension
+            rewinddir(dir);
+        }
     
-    closedir(dir);
+        closedir(dir);
 
-    // strip any trailing : from the tpaList
-    if (tpaList.size() > 0 && tpaList[tpaList.size()-1] == ':')
-        tpaList.resize(tpaList.size()-1);
+        // strip any trailing : from the tpaList
+        if (tpaList.size() > 0 && tpaList[tpaList.size()-1] == ':')
+            tpaList.resize(tpaList.size()-1);
+    }
 }
-
-} // namespace CoreCLRUtil
-
--- src/host/common/hostutil.h
+++ src/host/common/hostutil.h
@@ -5,44 +5,40 @@
 
 namespace HostUtil
 {
-
-//!\brief       get a list of absolute paths separated by : from a list of relative/absolute paths separated by :
-std::string getAbsolutePathList(const std::string& paths)
-{
-    //std::cerr << "getAbsolutePathList: paths=" << paths << std::endl;
-    std::string result;
-
-    // split by :
-    size_t lastPos = 0;
-    size_t curPos = paths.find(':',lastPos);
-    do
+    //!\brief get a list of absolute paths separated by : from a list of relative/absolute paths separated by :
+    std::string getAbsolutePathList(const std::string& paths)
     {
-        const std::string token = paths.substr(lastPos,curPos-lastPos);
-        //std::cerr << "curPos=" << curPos << " lastPos=" << lastPos << " token=" << token << std::endl;
+        //std::cerr << "getAbsolutePathList: paths=" << paths << std::endl;
+        std::string result;
 
-        // skip empty tokens
-        if (token != "")
+        // split by :
+        size_t lastPos = 0;
+        size_t curPos = paths.find(':',lastPos);
+        do
         {
-            std::string absolutePath;
-            if (CoreCLRUtil::GetAbsolutePath(token.c_str(),absolutePath))
+            const std::string token = paths.substr(lastPos,curPos-lastPos);
+            //std::cerr << "curPos=" << curPos << " lastPos=" << lastPos << " token=" << token << std::endl;
+
+            // skip empty tokens
+            if (token != "")
             {
-                // add colons correctly
-                if (result.size() == 0)
-                    result += absolutePath;
-                else
-                    result += ":" + absolutePath;
+                std::string absolutePath;
+                if (CoreCLRUtil::GetAbsolutePath(token.c_str(),absolutePath))
+                {
+                    // add colons correctly
+                    if (result.size() == 0)
+                        result += absolutePath;
+                    else
+                        result += ":" + absolutePath;
+                }
             }
+
+            // increment lastPos to skip the :
+            lastPos += token.size() + 1;
+            curPos = paths.find(':',lastPos);
         }
+        while (lastPos < paths.size());
 
-        // increment lastPos to skip the :
-        lastPos += token.size() + 1;
-        curPos = paths.find(':',lastPos);
+        return result;
     }
-    while (lastPos < paths.size());
-
-    return result;
 }
-
-}
-
-
