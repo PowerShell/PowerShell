db3f9d9df497a4cfc6c212ac80e97a891a996d5e
Zachary Folwick <v-zafolw@microsoft.com>
2015-10-22 16:32:36 -0700

    remove reference to old naming convention in the Describe block

---
+++ src/pester-tests/Test-Mocks.ps1
@@ -0,0 +1,18 @@
+Function GetFileMock () { 
+    $objs = @( [pscustomobject]@{ Size=4533816; Mode="-a---l"; LastWriteTime="9/1/2015  11:15 PM"; Name="explorer.exe" },
+               [pscustomobject]@{ Size=994816;  Mode="-a---l"; LastWriteTime="9/1/2015  11:13 PM"; Name="HelpPane.exe" },
+               [pscustomobject]@{ Size=316640;  Mode="-a---l"; LastWriteTime="9/1/2015  11:17 PM"; Name="WMSysPr9.prx" },
+               [pscustomobject]@{ Size=215040;  Mode="-a---l"; LastWriteTime="9/1/2015  11:20 PM"; Name="notepad.exe"  },
+               [pscustomobject]@{ Size=207239;  Mode="-a----"; LastWriteTime="10/7/2015  2:37 PM"; Name="setupact.log" },
+               [pscustomobject]@{ Size=181064;  Mode="-a----"; LastWriteTime="9/9/2015  11:54 PM"; Name="PSEXESVC.EXE" })
+    return $objs
+}
+
+filter addOneToSizeProperty() {
+    $_.Size += 1
+    $_
+} 
+
+filter pipelineConsume() {
+    $_
+}
