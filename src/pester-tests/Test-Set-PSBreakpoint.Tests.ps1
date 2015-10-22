0325edc3266a315140e44b469ffb1a7c11531478
Zachary Folwick <v-zafolw@microsoft.com>
2015-10-22 09:15:19 -0700

    added set-psbreakpoint tests

---
+++ src/pester-tests/Test-Set-PSBreakpoint.Tests.ps1
@@ -0,0 +1,13 @@
+﻿Describe "Test-Set-PSBreakpoint" {
+    $script = ./assets/testablescript.ps1
+    # nv -name script -value ./assets/testablescript.ps1 -scope Global
+
+    It "Should be able to called with script and line parameters without error " {
+        { Set-PSBreakpoint -Script $script -Line 1 } | Should Not Throw
+    }
+
+    It "Should be able to be called using the command switch" {
+        # Note- we don't actually require the command to exist in the script- it will simply not get a breakpoint
+        { Set-PSBreakpoint -Script $script -Command aoeuaoeu } | Should Not Throw
+    }
+}
