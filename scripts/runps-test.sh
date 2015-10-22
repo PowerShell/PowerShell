f9217fdd11275f047dfda7ca5294819045db5eac
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-10-22 15:47:44 -0700

    Use CORE_ROOT instead of -c

--- scripts/runps-test.sh
+++ scripts/runps-test.sh
@@ -4,4 +4,4 @@ CWD=$(pwd)
 SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 cd "$SCRIPTDIR"
-PSMODULEPATH="$SCRIPTDIR/Modules" LD_LIBRARY_PATH="$SCRIPTDIR" ./host_cmdline -c ../coreclr -alc Microsoft.PowerShell.CoreCLR.AssemblyLoadContext.dll -tpa xunit.console.netcore.exe xunit.console.netcore.exe "$@"
+CORE_ROOT=../coreclr PSMODULEPATH="$SCRIPTDIR/Modules" LD_LIBRARY_PATH="$SCRIPTDIR" ./host_cmdline -alc Microsoft.PowerShell.CoreCLR.AssemblyLoadContext.dll -tpa xunit.console.netcore.exe xunit.console.netcore.exe "$@"
