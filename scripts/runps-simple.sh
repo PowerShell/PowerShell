ddfa85b3128def0bd40b782d2b8e914e5e0c0f6a
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-10-22 17:02:38 -0700

    Use PWRSH_ROOT instead of -tpa

--- scripts/runps-simple.sh
+++ scripts/runps-simple.sh
@@ -4,4 +4,4 @@ CWD=$(pwd)
 SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 cd "$SCRIPTDIR"
-CORE_ROOT=../coreclr PSMODULEPATH="$SCRIPTDIR/Modules" LD_LIBRARY_PATH="$SCRIPTDIR" ./host_cmdline -alc Microsoft.PowerShell.CoreCLR.AssemblyLoadContext.dll -tpa powershell-simple.exe powershell-simple.exe "$@"
+CORE_ROOT=../coreclr PWRSH_ROOT=. PSMODULEPATH="$SCRIPTDIR/Modules" LD_LIBRARY_PATH="$SCRIPTDIR" ./host_cmdline -alc Microsoft.PowerShell.CoreCLR.AssemblyLoadContext.dll powershell-simple.exe "$@"
