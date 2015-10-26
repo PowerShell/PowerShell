3ca864660b0e901ae334f9ffd7dd065c5687d6ba
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-10-25 20:38:39 -0700

    Update library names
    - libps to libpsnative
    - ps_test to PowerShell.Linux.Test

--- src/assembly-info/System.Management.Automation.assembly-info.cs
+++ src/assembly-info/System.Management.Automation.assembly-info.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly:InternalsVisibleTo("Microsoft.PowerShell.Commands.Utility")]
 [assembly:InternalsVisibleTo("Microsoft.PowerShell.Security")]
 [assembly:InternalsVisibleTo("Microsoft.PowerShell.CoreCLR.AssemblyLoadContext")]
-[assembly:InternalsVisibleTo("ps_test")]
+[assembly:InternalsVisibleTo("PowerShell.Linux.Test")]
 [assembly:AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly:AssemblyVersion("1.0.0.0")]
 
