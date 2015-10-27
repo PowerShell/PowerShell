01b7c282728e5fb5208f45bbd1d415c1ab95e9e2
Zachary Folwick <v-zafolw@microsoft.com>
2015-10-27 11:20:35 -0700

    added pop-location tests

---
+++ src/pester-tests/Pop-Location.Tests.ps1
@@ -0,0 +1,28 @@
+﻿Describe "Pop-Location" {
+    $startDirectory = $(Get-Location).Path
+
+    BeforeEach { Set-Location $startDirectory }
+
+    It "Should be able to be called without error" {
+        { Pop-Location } | Should Not Throw
+    }
+
+    It "Should not take a parameter" {
+        { Pop-Location .. } | Should  Throw
+    }
+
+    It "Should be able pop multiple times" {
+        Push-Location ..
+        Push-Location ..
+        Push-Location ..
+
+        Pop-Location
+        Pop-Location
+        Pop-Location
+
+        $(Get-Location).Path | Should Be $startDirectory
+
+    }
+
+    Set-Location $startDirectory
+}
