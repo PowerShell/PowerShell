8f9042a1514f17b0d6e7bb597b7c74e1a02a4e91
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-11-02 15:54:26 -0800

    Fix host Makefile for file renames

--- src/host/host.mk
+++ src/host/host.mk
@@ -3,12 +3,12 @@
 # Currently there is powershell-run.exe, which is a very generic interactive and
 # non-interactive host
 
-POWERSHELL_RUN_FOLDER=$(MONAD)/src/powershell-run
-POWERSHELL_RUN_SRCS=$(addprefix $(POWERSHELL_RUN_FOLDER)/, main.cs host.cs ui.cs rawui.cs readline.cs powershell-run.assembly-info.cs)
+POWERSHELL_RUN_FOLDER=$(MONAD)/src/host
+POWERSHELL_RUN_SRCS=$(addprefix $(POWERSHELL_RUN_FOLDER)/, main.cs host.cs ui.cs rawui.cs readline.cs powershell.assembly-info.cs)
 
 # direct dependencies to be linked in
 POWERSHELL_RUN_DEPS=$(addprefix $(PSLIB)/, System.Management.Automation.dll Microsoft.PowerShell.Commands.Management.dll $(ASSEMBLY_LOAD_CONTEXT_TARGET))
 POWERSHELL_RUN_REFS=$(addprefix -r:, $(POWERSHELL_RUN_DEPS))
 
-$(PSLIB)/powershell-run.exe: $(POWERSHELL_RUN_SRCS) $(POWERSHELL_RUN_DEPS)
+$(PSLIB)/powershell.exe: $(POWERSHELL_RUN_SRCS) $(POWERSHELL_RUN_DEPS)
 	    $(CSC) -out:$@ -noconfig -nostdlib -target:exe $(POWERSHELL_RUN_REFS) $(COREREF) $(POWERSHELL_RUN_SRCS)
