ccc873b45f4a8446f7df0dcaa19e4ec8901e3e12
Zachary Folwick <v-zafolw@microsoft.com>
2015-11-02 13:45:39 -0800

    made changes according to PR

--- src/pester-tests/ConvertFrom-Csv.Tests.ps1
+++ src/pester-tests/ConvertFrom-Csv.Tests.ps1
@@ -1,6 +1,7 @@
 ﻿Describe "ConvertFrom-Csv" {
     $testObject = "a", "1"
-    $testDelimiter = "a; b"
+    $testcsv = "./assets/TestCsv2.csv"
+    $testName = "Zaphod BeebleBrox"
     $testColumns = @"
     a,b,c
     1,2,3
@@ -13,13 +14,35 @@
         { $testObject | ConvertFrom-Csv } | Should Not Throw
     }
 
+    It "Should have expected results when using piped inputs" {
+        $csvContent   = Get-Content $testcsv
+        $actualresult = $csvContent | ConvertFrom-Csv
+
+        $actualresult.GetType().BaseType.Name | Should Be "Array"
+        $actualresult[0].GetType().Name          | Should Be "PSCustomObject"
+
+        #Should have a name property in the result
+        $actualresult[0].Name | Should Be $testName
+    }
+
     It "Should be able to set a delimiter" {
-        { $testDelimiter | ConvertFrom-Csv -Delimiter ";" } | Should Not Throw
+        { $testcsv | ConvertFrom-Csv -Delimiter ";" } | Should Not Throw
+    }
+
+    It "Should actually delimit the output" {
+        $csvContent   = Get-Content $testcsv
+        $actualresult = $csvContent | ConvertFrom-Csv -Delimiter ";"
+
+        $actualresult.GetType().BaseType.Name    | Should Be "Array"
+        $actualresult[0].GetType().Name          | Should Be "PSCustomObject"
+
+        # ConvertFrom-Csv takes the first line of the input as a header by default
+        $actualresult.Length | Should Be $($csvContent.Length - 1)
     }
 
     It "Should be able to have multiple columns" {
         $actualData   = $testColumns | ConvertFrom-Csv
-        
+
         $actualLength = $($( $actualData | gm) | Where-Object {$_.MemberType -eq "NoteProperty" }).Length
 
         $actualLength | Should Be 3
