e884990d617a9fab38cab0766b77be0db5b0f5db
Zachary Folwick <v-zafolw@microsoft.com>
2015-11-03 11:44:51 -0800

    added additional tests to ConvertTo-Csv tests

--- src/pester-tests/ConvertTo-Csv.Tests.ps1
+++ src/pester-tests/ConvertTo-Csv.Tests.ps1
@@ -1,6 +1,32 @@
 Describe "ConvertTo-Csv" {
+    $Name = "Hello"; $Data = "World";
+    $testObject = New-Object psobject -Property @{ FirstColumn = $Name; SecondColumn = $Data }
+
+
     It "Should Be able to be called without error" {
-        { Get-Process | ConvertTo-Csv } | Should Not Throw
+        { $testObject | ConvertTo-Csv } | Should Not Throw
+    }
+
+    It "Should output an array of objects" {
+        $result = $testObject | ConvertTo-Csv
+
+        $result.GetType().BaseType.Name | Should Be "Array"
+    }
+
+    It "Should return the type of data in the first element of the output array" {
+        $result = $testObject | ConvertTo-Csv
+
+        $result[0] | Should Be "#TYPE System.Management.Automation.PSCustomObject"
+    }
+
+    It "Should return the column info in the second element of the output array" {
+        $result = $testObject | ConvertTo-Csv
+        $result[1] | Should Be "`"FirstColumn`",`"SecondColumn`""
+    }
+
+    It "Should return the data as a comma-separated list in the third element of the output array" {
+        $result = $testObject | ConvertTo-Csv
+        $result[2] | Should Be "`"Hello`",`"World`""
     }
 
 }
