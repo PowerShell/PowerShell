aca21860c3c834df9ad3b409114e99afa95368c3
Zachary Folwick <v-zafolw@microsoft.com>
2015-11-09 12:06:52 -0800

    removed old titled version 1. fixed typo

--- src/pester-tests/Get-Date.Tests.ps1
+++ src/pester-tests/Get-Date.Tests.ps1
@@ -1,4 +1,4 @@
-﻿doDescribe "Get-Date" {
+﻿Describe "Get-Date" {
     It "Should return a DateTime object upon being called" {
         (Get-Date).GetType().Name.Equals('DateTime') | Should Be $true
     }
