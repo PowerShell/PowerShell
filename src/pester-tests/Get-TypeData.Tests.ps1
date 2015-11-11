b823dd7eddbd19eaded6d63b13c141f26b416022
Zachary Folwick <v-zafolw@microsoft.com>
2015-11-10 16:06:24 -0800

    added Get-TypeData tests

---
+++ src/pester-tests/Get-TypeData.Tests.ps1
@@ -0,0 +1,17 @@
+﻿Describe "Get-TypeData" {
+    It "Should be able to call Get-TypeData with no arguments without throwing" {
+        { Get-TypeData } | Should Not Throw
+    }
+
+    It "Should return an array of several elements when no arguments are used" {
+        $output = Get-TypeData
+
+        $output.Length | Should BeGreaterThan 1
+    }
+
+    It "Should be able to take wildcard input" {
+        $output = Get-TypeData *Sys*
+
+        $output.Length | Should BeGreaterThan 1
+    }
+}
