909a993a628646e2d452939dfbd4df38a264bbb1
Alex Jordan <pslsrv@microsoft.com>
2015-11-19 06:23:31 +0000

    Added pester tests for Start-Sleep

---
+++ src/pester-tests/Start-Sleep.Tests.ps1
@@ -0,0 +1,14 @@
+Describe "Start-Sleep" {
+
+    Context "Validate Start-Sleep works properly" {
+        It "Should only sleep for at least 3 seconds"{
+           $result = Measure-Command { Start-Sleep -s 3 }
+	     $result.Seconds | Should BeGreaterThan 3
+	 }
+
+        It "Should sleep for at least 3 seconds using the alias" {
+	    $result = Measure-Command { sleep -s 3 }
+	    $result.Seconds | Should BeGreaterThan 3
+	}
+    }
+}
