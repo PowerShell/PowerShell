621757c8a2f1f226f7911d40b81805f8357f0955
Alex Jordan <pslsrv@microsoft.com>
2015-11-19 06:22:29 +0000

    Added pester tests for Register-EngineEvent

---
+++ src/pester-tests/Register-EngineEvent.Tests.ps1
@@ -0,0 +1,9 @@
+Describe "Register-EngineEvent" {
+
+    Context "Check return type of Register-ObjectEvent" {	
+        It "Should return System.Management.Automation.PSEventJob as return type of Register-EngineEvent" {
+	   ( Register-EngineEvent -SourceIdentifier PesterTestRegister -Action {echo registerengineevent} ).GetType() | Should Be System.Management.Automation.PSEventJob
+           Unregister-Event -sourceidentifier PesterTestRegister
+	}
+    }
+}
