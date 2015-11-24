fc0506f6bc700e4ff4bcf1c48a3b6dd1db32f783
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-11-23 19:29:12 -0800

    Clean up assembly infos

--- src/assembly-info/System.Management.Automation.assembly-info.cs
+++ src/assembly-info/System.Management.Automation.assembly-info.cs
@@ -6,20 +6,18 @@ using System.Reflection;
 [assembly:InternalsVisibleTo("Microsoft.PowerShell.Security")]
 [assembly:InternalsVisibleTo("Microsoft.PowerShell.Linux.Host")]
 [assembly:InternalsVisibleTo("Microsoft.PowerShell.Linux.UnitTests")]
-[assembly:InternalsVisibleTo("powershell")]
 [assembly:AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly:AssemblyVersion("1.0.0.0")]
 
-
 namespace System.Management.Automation
 {
-	internal class NTVerpVars
-	{
-		internal const int PRODUCTMAJORVERSION = 10;
-		internal const int PRODUCTMINORVERSION = 0;
-		internal const int PRODUCTBUILD        = 10032;
-		internal const int PRODUCTBUILD_QFE    = 0;
-		internal const int PACKAGEBUILD_QFE    = 814;
-	}
+    internal class NTVerpVars
+    {
+        internal const int PRODUCTMAJORVERSION = 10;
+        internal const int PRODUCTMINORVERSION = 0;
+        internal const int PRODUCTBUILD        = 10032;
+        internal const int PRODUCTBUILD_QFE    = 0;
+        internal const int PACKAGEBUILD_QFE    = 814;
+    }
 }
 
