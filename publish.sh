6f151ab2c433e94b6204931c49bf660a296c664f
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-12-01 10:47:10 -0800

    Bump DNVM version in scripts

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 
 cd src/Microsoft.PowerShell.Linux.Host
 source ~/.dnx/dnvm/dnvm.sh
-dnvm use 1.0.0-rc2-16177
-dnu publish --out ../.. --runtime dnx-coreclr-linux-x64.1.0.0-rc2-16177 --no-source
+dnvm use 1.0.0-rc2-16237
+dnu publish --out ../.. --runtime dnx-coreclr-linux-x64.1.0.0-rc2-16237 --no-source
 cd ../..
