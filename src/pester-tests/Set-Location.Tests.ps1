0c1be1bf1297c485c45783c923aa2d97f5f89b3d
Zachary Folwick <v-zafolw@microsoft.com>
2015-12-01 10:30:27 -0800

    Modified tests for cross-platform testing 1. removed the context block.

--- src/pester-tests/Set-Location.Tests.ps1
+++ src/pester-tests/Set-Location.Tests.ps1
@@ -1,61 +1,66 @@
 ﻿Describe "Set-Location" {
-    Context "Functionality testing" {
-        $startDirectory = Get-Location
+    $startDirectory = Get-Location
+    if ( $env:TEMP -eq "/tmp")
+    {
+        $target = "/"
+    }
+    else
+    {
+        $target = "C:\"
+    }
 
-	It "Should be able to be called without error" {
-            { Set-Location / }    | Should Not Throw
-	}
+    It "Should be able to be called without error" {
+        { Set-Location $target }    | Should Not Throw
+    }
 
-	It "Should be able to be called on different providers" {
-            { Set-Location alias: } | Should Not Throw
-            { Set-Location env: }   | Should Not Throw
-	}
+    It "Should be able to be called on different providers" {
+        { Set-Location alias: } | Should Not Throw
+        { Set-Location env: }   | Should Not Throw
+    }
 
-	It "Should be able use the cd alias without error" {
-            { cd / } | Should Not Throw
-	}
+    It "Should be able use the cd alias without error" {
+        { cd $target } | Should Not Throw
+    }
 
-	It "Should be able to use the chdir alias without error" {
-            { chdir / } | Should Not Throw
-	}
+    It "Should be able to use the chdir alias without error" {
+        { chdir $target } | Should Not Throw
+    }
 
-	It "Should be able to use the sl alias without error" {
-            { sl / } | Should Not Throw
-	}
+    It "Should be able to use the sl alias without error" {
+        { sl $target } | Should Not Throw
+    }
 
-        It "Should have the correct current location when using the set-location cmdlet" {
-            Set-Location $startDirectory
-
-            $(Get-Location).Path | Should Be $startDirectory.Path
-        }
-
-        It "Should have the correct current location when using the cd alias" {
-            cd /
-
-            $(Get-Location).Path | Should Be /
-        }
-
-        It "Should have the correct current location when using the chdir alias" {
-            chdir /
-
-            $(Get-Location).Path | Should Be /
-        }
-
-        It "Should have the correct current location when using the chdir alias" {
-            sl /
-
-            $(Get-Location).Path | Should Be /
-        }
-
-        It "Should be able to use the Path switch" {
-            { Set-Location -Path / } | Should Not Throw
-        }
-
-        It "Should generate a pathinfo object when using the Passthru switch" {
-            $(Set-Location / -PassThru).GetType().Name | Should Be PathInfo
-        }
-
+    It "Should have the correct current location when using the set-location cmdlet" {
         Set-Location $startDirectory
 
+        $(Get-Location).Path | Should Be $startDirectory.Path
     }
+
+    It "Should have the correct current location when using the cd alias" {
+        cd $target
+
+        $(Get-Location).Path | Should Be $target
+    }
+
+    It "Should have the correct current location when using the chdir alias" {
+        chdir $target
+
+        $(Get-Location).Path | Should Be $target
+    }
+
+    It "Should have the correct current location when using the chdir alias" {
+        sl $target
+
+        $(Get-Location).Path | Should Be $target
+    }
+
+    It "Should be able to use the Path switch" {
+        { Set-Location -Path $target } | Should Not Throw
+    }
+
+    It "Should generate a pathinfo object when using the Passthru switch" {
+        $(Set-Location $target -PassThru).GetType().Name | Should Be PathInfo
+    }
+
+    Set-Location $startDirectory
 }
