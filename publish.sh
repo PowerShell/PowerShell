0b6b1f116e4fd9ca05252b94d3e7663880fbf540
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-12-03 15:14:30 -0800

    Update for use with dotnet-cli
    https://github.com/dotnet/cli

--- publish.sh
+++ publish.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 cd src/Microsoft.PowerShell.Linux.Host
-source ~/.dnx/dnvm/dnvm.sh
-dnvm use 1.0.0-rc2-16249
-dnu publish --out ../.. --runtime dnx-coreclr-linux-x64.1.0.0-rc2-16249 --no-source
+dotnet publish --framework dnxcore50 --runtime ubuntu.14.04-x64 --output ../../bin
 cd ../..
