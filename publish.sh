702416e2d63e7c7f5f3674a6d63e6938178875a4
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-12-04 12:18:03 -0800

    Bump .NET version

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 
 cd src/Microsoft.PowerShell.Linux.Host
 source ~/.dnx/dnvm/dnvm.sh
-dnvm use 1.0.0-rc2-16237
-dnu publish --out ../.. --runtime dnx-coreclr-linux-x64.1.0.0-rc2-16237 --no-source
+dnvm use 1.0.0-rc2-16249
+dnu publish --out ../.. --runtime dnx-coreclr-linux-x64.1.0.0-rc2-16249 --no-source
 cd ../..
--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 cd src/Microsoft.PowerShell.Linux.UnitTests
 source ~/.dnx/dnvm/dnvm.sh
-dnvm use 1.0.0-rc2-16237 -r coreclr
+dnvm use 1.0.0-rc2-16249 -r coreclr
 dnx test
 cd ../..
