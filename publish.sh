9271861b79340f4e66c0b2937aa22dea6d4470c4
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-12-10 12:33:52 -0800

    Patch things in publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+# Publish PowerShell
 cd src/Microsoft.PowerShell.Linux.Host
 dotnet publish --framework dnxcore50 --runtime ubuntu.14.04-x64 --output ../../bin
 cp *.ps1xml ../../bin
 cd ../..
+
+# Copy Pester
+mkdir -p bin/Modules/Pester
+cp -r ext-src/pester bin/Modules/Pester
+
+# Patch
+cp patches/*.dll bin/
+cp patches/runtime/*.so bin/
