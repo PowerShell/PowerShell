656daf6a805a7e8a35d3cabe85b4da59421ee543
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-12-14 15:36:22 -0800

    Update the type catalog

--- src/TypeCatalogGen/build.sh
+++ src/TypeCatalogGen/build.sh
@@ -1,7 +1,7 @@
 . mono-snapshot mono
 export DOTNET_REFERENCE_ASSEMBLIES_PATH=$MONO_PREFIX/lib/mono/xbuild-frameworks/
 
-(cd Runtime && dotnet publish --framework dnxcore50 --runtime ubuntu.14.04-x64 --output bin)
+(cd Runtime && dotnet restore && dotnet publish --framework dnxcore50 --runtime ubuntu.14.04-x64 --output bin)
 ls Runtime/bin/*.dll | sed ':a;N;$!ba;s/\n/;/g' > powershell.inc
 
 dotnet restore
