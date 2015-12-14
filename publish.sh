3f5a6bc893cf0e287e46e5e61333a021917b848f
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-12-14 10:36:40 -0800

    Build native components in publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+# Build native components
+(
+    cd src/monad-native
+    cmake -DCMAKE_BUILD_TYPE=Debug .
+    make -j
+)
+
 # Publish PowerShell
-cd src/Microsoft.PowerShell.Linux.Host
-dotnet publish --framework dnxcore50 --runtime ubuntu.14.04-x64 --output ../../bin
-cp *.ps1xml *_profile.ps1 ../../bin
-cd ../..
+(
+    cd src/Microsoft.PowerShell.Linux.Host
+    dotnet publish --framework dnxcore50 --runtime ubuntu.14.04-x64 --output ../../bin
+    cp *.ps1xml *_profile.ps1 ../../bin
+)
 
 # Copy Pester
 mkdir -p bin/Modules/Pester
