f018ea47a4d3702147c355b14c77514005af1d96
George Fleming <v-geflem@microsoft.com>
2015-12-23 10:41:36 -0800

    Added Get-OmiInstance Pester test

---
+++ src/pester-tests/Get-OmiInstance.Tests.ps1
@@ -0,0 +1,12 @@
+﻿# Note that omicli must be in PATH and omiserver should be started with 
+#    --ignoreAuthentication option
+
+Describe "Get-OmiInstance" {
+    Import-Module Microsoft.PowerShell.Commands.Omi
+
+    It "Should execute basic command correctly" {
+        $obj = Get-OmiInstance -NameSpace root/omi -ClassName OMI_Identify
+
+        $obj.Length      | Should Be 27
+    }
+}
