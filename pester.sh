b8ca768099fef4adc4c735c7d2551d21e7db385c
Andrew Schwartzmeyer <andschwa@microsoft.com>
2015-12-23 13:12:40 -0800

    Output Pester results in XML again
    And bump submodule.

--- pester.sh
+++ pester.sh
@@ -1,4 +1,6 @@
 export CORE_ROOT=$(pwd)/bin
-./powershell -c "Invoke-Pester src/pester-tests/$1"
+./powershell -c "Invoke-Pester src/pester-tests/$1 -OutputFile pester-tests.xml -OutputFormat NUnitXml"
+# XML files are not executable
+chmod -x pester-tests.xml
 # Fix tty due to possible bug in .NET
 stty echo
