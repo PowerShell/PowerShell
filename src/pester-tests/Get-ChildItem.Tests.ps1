05989fe455fcefaf0f6eb1c5405eb971c9627eff
Zachary Folwick <v-zafolw@microsoft.com>
2015-12-28 10:15:19 -0800

    moved alias test to its own test case

--- src/pester-tests/Get-ChildItem.Tests.ps1
+++ src/pester-tests/Get-ChildItem.Tests.ps1
@@ -1,18 +1,15 @@
 ﻿Describe "Get-ChildItem" {
     It "Should list the contents of the current folder" {
         (Get-ChildItem .).Name.Length | Should BeGreaterThan 0
-
-        (ls .).Name.Length | Should BeGreaterThan 0
     }
 
     It "Should list the contents of the home directory" {
         pushd $HOME
         (Get-ChildItem .).Name.Length | Should BeGreaterThan 0
         popd
+    }
 
-        pushd $HOME
-        (ls .).Name.Length | Should BeGreaterThan 0
-        popd
-
+    It "Should be able to use the ls alias" {
+        $(ls .).Name.Length | Should Be $(Get-ChildItem .).Name.Length
     }
 }
