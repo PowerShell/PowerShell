1b4276f56306eed1172fd08bbb41d6c0fa8f330a
Zachary Folwick <v-zafolw@microsoft.com>
2015-12-28 10:39:36 -0800

    Added test to validate that fields are getting populated

--- src/pester-tests/Get-ChildItem.Tests.ps1
+++ src/pester-tests/Get-ChildItem.Tests.ps1
@@ -12,4 +12,14 @@
     It "Should be able to use the ls alias" {
         $(ls .).Name.Length | Should Be $(Get-ChildItem .).Name.Length
     }
+
+    It "Should have a the proper fields and be populated" {
+        $var = Get-Childitem .
+
+        $var.Name.Length   | Should BeGreaterThan 0
+        $var.Mode.Length   | Should BeGreaterThan 0
+        $var.LastWriteTime | Should BeGreaterThan 0
+        $var.Length.Length | Should BeGreaterThan 0
+
+    }
 }
