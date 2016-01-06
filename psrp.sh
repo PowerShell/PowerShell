a9b7928d40b1133317b31aa77aedbaf485e2a61f
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-01-05 17:46:05 -0800

    Add logging to PSRP script

--- psrp.sh
+++ psrp.sh
@@ -1,2 +1,2 @@
 export CORE_ROOT=$(pwd)/bin
-lldb-3.6 -o "plugin load ./bin/libsosplugin.so" -- ./src/omi/Unix/output/bin/omiserver --ignoreAuthentication
+lldb-3.6 -o "plugin load ./bin/libsosplugin.so" -- ./src/omi/Unix/output/bin/omiserver --ignoreAuthentication -l --loglevel 2
