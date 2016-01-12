8ca4fdb7fdb034a2bee1af4e39d3e6c67e74532e
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-01-12 12:25:02 -0800

    Remove stty echo workaround
    Note that the run script is now entirely deprecated, since the host can be run directly.

--- pester.sh
+++ pester.sh
@@ -1,5 +1,3 @@
 ./bin/Microsoft.PowerShell.Linux.Host -c "Invoke-Pester src/pester-tests/$1 -OutputFile pester-tests.xml -OutputFormat NUnitXml"
 # XML files are not executable
 chmod -x pester-tests.xml
-# Fix tty due to possible bug in .NET
-stty echo || true
--- run.sh
+++
@@ -1,3 +0,0 @@
-./bin/Microsoft.PowerShell.Linux.Host $@
-# Fix tty due to possible bug in .NET
-stty echo
