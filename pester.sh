bbebf2f76a7d9a8c24852ba081659fa8df195819
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-01-14 17:00:06 -0800

    Reorganize tests
    - Pester source code moved to `test/Pester`, deleted `ext-src`.
    - Pester tests (.ps1 files) moved to `test/powershell`
    - xUnit tests (.cs files) moved to `test/csharp`
    - Third-party script test moved to `test/shebang`

--- pester.sh
+++ pester.sh
@@ -1,3 +1,3 @@
-./bin/Microsoft.PowerShell.Linux.Host -c "Invoke-Pester src/pester-tests/$1 -OutputFile pester-tests.xml -OutputFormat NUnitXml"
+./bin/Microsoft.PowerShell.Linux.Host -c "Invoke-Pester test/powershell/$1 -OutputFile pester-tests.xml -OutputFormat NUnitXml"
 # XML files are not executable
 chmod -x pester-tests.xml
