f8ef2191863bd4621707976d0c151309af507990
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-01-15 16:40:10 -0800

    Clean up PSL_profile.ps1
    Remove unneeded cruft and use here-string.

--- src/Microsoft.PowerShell.Linux.Host/PSL_profile.ps1
+++ src/Microsoft.PowerShell.Linux.Host/PSL_profile.ps1
@@ -1,25 +1,14 @@
-(get-psprovider 'FileSystem').Home = $env:HOME
-
-function prompt
-{
-    "PSL " + $(get-location) + "> "
+function prompt {
+   "PSL " + $(get-location) + "> "
 }
 
-& {
-    for ($i = 0; $i -lt 26; $i++)
-    {
-        $funcname = ([System.Char]($i+65)) + ':'
-        $str = "function global:$funcname { set-location $funcname } "
-        invoke-expression $str
-    }
-}
+@"
 
-function banner 
-{
-   "`nPowerShell for Linux interactive console"
-   "========================================"
-   "- Type 'get-help' for help"
-   "- Type 'exit' to exit`n"
-}
+Welcome to Project Magrathea!
 
-banner
+This is the Core PowerShell On Linux /OS X console
+==================================================
+- Type 'get-help' for help
+- Type 'exit' to exit
+
+"@
