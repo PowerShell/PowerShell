b451d02c5af05e018ec499fe66e1403b6f4f6c8c
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-01-20 10:26:13 -0800

    Update profile banner

--- src/Microsoft.PowerShell.Linux.Host/PSL_profile.ps1
+++ src/Microsoft.PowerShell.Linux.Host/PSL_profile.ps1
@@ -6,9 +6,10 @@ function prompt {
 
 Welcome to Project Magrathea!
 
-This is the Core PowerShell On Linux /OS X console
-==================================================
-- Type 'get-help' for help
-- Type 'exit' to exit
+This is the Core PowerShell On Linux / OS X / Windows console
+=============================================================
+- Type 'Get-Help' for help
+- Type 'Exit' to exit
+- Type 'Invoke-Pester test/powershell' to run the tests
 
 "@
