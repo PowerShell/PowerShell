cd81479b8724c4074f1441365a243f26eaabaefd
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-01-22 14:29:30 -0800

    Add test failure threshold to pester.sh
    Set to 14 because of non-tty and random process failures.

--- pester.sh
+++ pester.sh
@@ -1,3 +1,10 @@
-./bin/powershell -c "Invoke-Pester test/powershell/$1 -OutputFile pester-tests.xml -OutputFormat NUnitXml"
+#!/usr/bin/env bash
+
+./bin/powershell -c "Invoke-Pester test/powershell/$1 -OutputFile pester-tests.xml -OutputFormat NUnitXml -EnableExit"
+failed_tests=$?
+
 # XML files are not executable
 chmod -x pester-tests.xml
+
+# Exit with failure if number of failed tests exceeds threshold
+[ ! $failed_tests -gt 14 ]
