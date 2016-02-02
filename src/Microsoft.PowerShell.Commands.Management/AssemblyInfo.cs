bd79884b81adea63e5f7d316d30ea658b533a2c7
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-02-02 13:31:25 -0800

    Update AssemblyInfo files for FullCLR build

--- src/Microsoft.PowerShell.Commands.Management/AssemblyInfo.cs
+++ src/Microsoft.PowerShell.Commands.Management/AssemblyInfo.cs
@@ -2,3 +2,8 @@ using System.Reflection;
 
 [assembly:AssemblyFileVersionAttribute("3.0.0.0")]
 [assembly:AssemblyVersion("3.0.0.0")]
+
+#if !CORECLR
+[assembly:AssemblyKeyFileAttribute(@"..\..\src\monad\monad\src\graphicalhost\visualstudiopublic.snk")]
+[assembly:AssemblyDelaySignAttribute(true)]
+#endif
