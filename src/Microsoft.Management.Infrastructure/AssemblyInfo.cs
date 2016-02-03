f42cf5985feb99d8d55a27249eade6a8dfa28563
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-02-03 11:03:13 -0800

    Fix guard in MMI's AssemblyInfo

--- src/Microsoft.Management.Infrastructure/AssemblyInfo.cs
+++ src/Microsoft.Management.Infrastructure/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Runtime.CompilerServices;
 using System.Reflection;
 
-#if CORECLR
+#if _CORECLR
 [assembly:InternalsVisibleTo("System.Management.Automation")]
 [assembly:AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly:AssemblyVersion("1.0.0.0")]
