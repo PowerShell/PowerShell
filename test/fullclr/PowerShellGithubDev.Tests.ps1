2096bca2b85eabaa1cc73883e55a6599045c80f2
Sergei Vorobev <sevoroby@microsoft.com>
2016-02-03 17:29:27 -0800

    Run FullCLR sanitary tests in AppVeyor
    This change brings a very minimal set of tests and make
    build fail, if we cannot load some PS assamblies on FullCLR
    
    We should increase test coverage for FullCLR in future.

---
+++ test/fullclr/PowerShellGithubDev.Tests.ps1
@@ -0,0 +1,32 @@
+Describe 'PowerShellGithubDev.psm1 and powershell.exe' {
+    Context '$env:DEVPATH assemblies loading' {
+        It 'has $env:DEVPATH set' {
+            $env:DEVPATH | Should Not Be $null
+        }
+
+        It 'loads System.Management.Automation.dll' {
+            [psobject].Assembly.Location | Should Be (
+                Join-Path $env:DEVPATH System.Management.Automation.dll)
+        }
+
+        It 'loads Microsoft.PowerShell.Commands.Management.dll' {
+            [Microsoft.PowerShell.Commands.GetChildItemCommand].Assembly.Location | Should Be (
+                Join-Path $env:DEVPATH Microsoft.PowerShell.Commands.Management.dll)
+        }
+
+        It 'loads Microsoft.PowerShell.Commands.Utility.dll' {
+            [Microsoft.PowerShell.Commands.UtilityResources].Assembly.Location | Should Be (
+                Join-Path $env:DEVPATH Microsoft.PowerShell.Commands.Utility.dll)
+        }
+
+        It 'loads Microsoft.PowerShell.ConsoleHost.dll' {
+            [Microsoft.PowerShell.ConsoleShell].Assembly.Location | Should Be (
+                Join-Path $env:DEVPATH Microsoft.PowerShell.ConsoleHost.dll)
+        }
+
+        It 'loads Microsoft.PowerShell.Security.dll' {
+            [Microsoft.PowerShell.Commands.SecurityDescriptorCommandsBase].Assembly.Location | Should Be (
+                Join-Path $env:DEVPATH Microsoft.PowerShell.Security.dll)
+        }
+    }
+}
