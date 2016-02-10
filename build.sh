5916b0026171d0f4bafbd2603d2bad21464693fb
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-02-10 13:05:46 -0800

    Fix handling of bin directory

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
 export BIN=$(pwd)/bin
+mkdir -p $BIN
 
 # Build native components
-(
-    cd src/libpsl-native
-    cmake -DCMAKE_BUILD_TYPE=Debug .
-    make -j
-    ctest -V
-    cp src/libpsl-native.* $BIN
-)
+pushd src/libpsl-native
+cmake -DCMAKE_BUILD_TYPE=Debug .
+make -j
+ctest -V
+cp src/libpsl-native.* $BIN
+popd
 
 # Publish PowerShell
-(
-    cd src/Microsoft.PowerShell.Linux.Host
-    dotnet publish --framework dnxcore50 --output $BIN --configuration Linux
-    # Copy files that dotnet-publish does not currently deploy
-)
+pushd src/Microsoft.PowerShell.Linux.Host
+dotnet publish --framework dnxcore50 --output $BIN --configuration Linux
+popd
