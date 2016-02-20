aeb665feae80f5a997e65d2d9646ef3a014a4b33
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-02-20 14:28:26 -0800

    Remove banner from profile
    When trying to use PowerShell a lot, this default banner gets annoying.
    I don't think we need it any more. Also change the prompt to just "PS".

--- src/Microsoft.PowerShell.Linux.Host/PSL_profile.ps1
+++ src/Microsoft.PowerShell.Linux.Host/PSL_profile.ps1
@@ -1,15 +1,3 @@
 function prompt {
-   "PSL " + $(get-location) + "> "
+   "PS " + $(get-location) + "> "
 }
-
-@"
-
-Welcome to Project Magrathea!
-
-This is the Core PowerShell On Linux / OS X / Windows console
-=============================================================
-- Type 'Get-Help' for help
-- Type 'Exit' to exit
-- Type 'Invoke-Pester test/powershell' to run the tests
-
-"@
