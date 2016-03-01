3abbf62428677f7dfaae0582d3695fd59ddada08
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-03-01 13:22:52 -0800

    Fix TypeCatalogGen script

--- src/TypeCatalogGen/build.sh
+++ src/TypeCatalogGen/build.sh
@@ -1,7 +1,11 @@
 . mono-snapshot mono
 export DOTNET_REFERENCE_ASSEMBLIES_PATH=$MONO_PREFIX/lib/mono/xbuild-frameworks/
 
-(cd Runtime && dotnet restore && dotnet publish --framework dnxcore50 --runtime ubuntu.14.04-x64 --output bin)
+pushd Runtime
+dotnet restore
+dotnet publish --output bin
+popd
+
 ls Runtime/bin/*.dll | sed ':a;N;$!ba;s/\n/;/g' > powershell.inc
 
 dotnet restore
