6be57a0b5ccfed106065068a6b89043d40c1f054
TingLiu6 <v-litin@microsoft.com>
2016-03-18 02:30:26 -0700

    Add Pester Test for Get-PSBreakpoint, Import-Module, Sort-Object

---
+++ test/powershell/Get-PSBreakpoint.Tests.ps1
@@ -0,0 +1,29 @@
+﻿Describe "Get-PSBreakpoint" {
+
+    $scriptName = "Get-PSBreakpoint.Tests.ps1"
+
+    AfterEach {
+        Get-PSBreakpoint -Script "$PSScriptRoot\$scriptName" | Remove-PSBreakpoint 
+    }
+
+    It "should be able to get PSBreakpoint with using Id switch" {
+        Set-PSBreakpoint -Script "$PSScriptRoot\$scriptName" -Line 1
+        
+        { Get-PSBreakpoint -Script "$PSScriptRoot\$scriptName" } | Should Not Throw
+
+        $Id = (Get-PSBreakpoint -Script "$PSScriptRoot\$scriptName").Id
+        $Id | Should be 0
+
+    }
+
+    It "sshould be able to get PSBreakpoint with using Variable switch" {
+        Set-PSBreakpoint -Script "$PSScriptRoot\$scriptName" -Variable "$scriptName"
+        
+        { Get-PSBreakpoint -Variable "$scriptName" -Script "$PSScriptRoot\$scriptName" } | Should Not Throw
+
+        $Id = (Get-PSBreakpoint -Variable "$scriptName" -Script "$PSScriptRoot\$scriptName").Variable
+        $Id | Should be $scriptName
+
+    }
+}
+
---
+++ test/powershell/Sort-Object.Tests.ps1
@@ -0,0 +1,22 @@
+﻿Describe "Sort-Object" {
+
+    It "should be able to sort object in ascending with using Property switch" {
+        { Get-ChildItem -Path $PSScriptRoot -Include *.ps1 -Recurse | Sort-Object -Property Length } | Should Not Throw
+        
+        $firstLen = (Get-ChildItem -Path $PSScriptRoot -Include *.ps1 -Recurse | Sort-Object -Property Length | Select-Object -First 1).Length
+        $lastLen = (Get-ChildItem -Path $PSScriptRoot -Include *.ps1 -Recurse | Sort-Object -Property Length | Select-Object -Last 1).Length
+
+        $firstLen -lt $lastLen | Should be $true
+
+    }
+
+    It "should be able to sort object in descending with using Descending switch" {
+        { Get-ChildItem -Path $PSScriptRoot -Include *.ps1 -Recurse | Sort-Object -Property Length -Descending } | Should Not Throw
+        
+        $firstLen = (Get-ChildItem -Path $PSScriptRoot -Include *.ps1 -Recurse | Sort-Object -Property Length -Descending | Select-Object -First 1).Length
+        $lastLen = (Get-ChildItem -Path $PSScriptRoot -Include *.ps1 -Recurse | Sort-Object -Property Length -Descending | Select-Object -Last 1).Length
+
+        $firstLen -gt $lastLen | Should be $true
+    }
+}
+
