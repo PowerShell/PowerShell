25f8d334d642c0ec83519b2511186cdf631f7096
TingLiu6 <v-litin@microsoft.com>
2016-03-20 22:04:08 -0700

    Modify Pester Test for Get-PSBreakpoint

--- test/powershell/Get-PSBreakpoint.Tests.ps1
+++ test/powershell/Get-PSBreakpoint.Tests.ps1
@@ -1,27 +1,28 @@
 ﻿Describe "Get-PSBreakpoint" {
 
     $scriptName = "Get-PSBreakpoint.Tests.ps1"
+    $fullScriptPath = Join-Path -Path $PSScriptRoot -ChildPath $scriptName
 
     AfterEach {
-        Get-PSBreakpoint -Script "$PSScriptRoot\$scriptName" | Remove-PSBreakpoint 
+        Get-PSBreakpoint -Script $fullScriptPath | Remove-PSBreakpoint 
     }
 
     It "should be able to get PSBreakpoint with using Id switch" {
-        Set-PSBreakpoint -Script "$PSScriptRoot\$scriptName" -Line 1
+        Set-PSBreakpoint -Script $fullScriptPath -Line 1
         
-        { Get-PSBreakpoint -Script "$PSScriptRoot\$scriptName" } | Should Not Throw
+        { Get-PSBreakpoint -Script $fullScriptPath } | Should Not Throw
 
-        $Id = (Get-PSBreakpoint -Script "$PSScriptRoot\$scriptName").Id
+        $Id = (Get-PSBreakpoint -Script $fullScriptPath).Id
         $Id | Should be 0
 
     }
 
     It "sshould be able to get PSBreakpoint with using Variable switch" {
-        Set-PSBreakpoint -Script "$PSScriptRoot\$scriptName" -Variable "$scriptName"
+        Set-PSBreakpoint -Script $fullScriptPath -Variable "$scriptName"
         
-        { Get-PSBreakpoint -Variable "$scriptName" -Script "$PSScriptRoot\$scriptName" } | Should Not Throw
+        { Get-PSBreakpoint -Variable "$scriptName" -Script $fullScriptPath } | Should Not Throw
 
-        $Id = (Get-PSBreakpoint -Variable "$scriptName" -Script "$PSScriptRoot\$scriptName").Variable
+        $Id = (Get-PSBreakpoint -Variable "$scriptName" -Script $fullScriptPath).Variable
         $Id | Should be $scriptName
 
     }
