ea73db0d68ad1faed2e9b547bea5cb7fa38f658c
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-04-20 15:21:55 -0700

    Make Start-PSBootstrap re-install dotnet on Windows
    It now helpfully deletes the previous `dotnet` directory so that older
    versions of the CLI don't muck things up.

--- PowerShellGitHubDev.psm1
+++ PowerShellGitHubDev.psm1
@@ -374,6 +374,7 @@ function Start-PSBootstrap {
         sudo installer -pkg dotnet-dev-osx-x64.latest.pkg -target /
 
     } elseif ($IsWindows -And -Not $IsCore) {
+        Remove-Item -ErrorAction SilentlyContinue -Recurse -Force ~\AppData\Local\Microsoft\dotnet
         Invoke-WebRequest -Uri https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.ps1 -OutFile install.ps1
         ./install.ps1
 
