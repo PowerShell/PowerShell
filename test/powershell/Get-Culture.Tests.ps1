7a9ba07fbce616922fee6695f0f521f8d6c905e0
TingLiu6 <v-litin@microsoft.com>
2016-04-20 01:28:43 -0700

    Add Unit Test for Get-Culture

--- test/powershell/Get-Culture.Tests.ps1
+++ test/powershell/Get-Culture.Tests.ps1
@@ -1,3 +1,12 @@
+Describe "Get-Culture DRT Unit Tests" -Tags DRT{
+    It "Should works proper with get-culture" {
+        $results = get-Culture
+        $results | Should Not BeNullOrEmpty 
+        $results[0].GetType() | Should Be CultureInfo
+        $results[0].Name | Should Be $PSCulture
+    }
+}
+
 Describe "Get-Culture" {
 
     It "Should return a type of CultureInfo for Get-Culture cmdlet" {
