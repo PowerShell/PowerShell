6d6c4db2b80c00e058e51be9c379add5a038f956
Bryce Milton <brycem@live.com>
2016-04-21 13:13:09 -0700

    Revert "attempting to work around Linux\OS1 dynamic file"
    This reverts commit 1d883c705d89d7de053328c036a53e1ded5320a9.

--- test/powershell/Add-Content.Tests.ps1
+++ test/powershell/Add-Content.Tests.ps1
@@ -11,11 +11,11 @@ Describe "Add-Content cmdlet tests" {
             $result = get-content -path testdrive:\$file1
             $result | Should BeExactly "ExpectedContent"
         }
-        It "should Add-Content to testdrive:\dynamicfile.txt with dynamic parameters" {
-            $result=add-content -path (Join-Path testdrive: -ChildPath dynamicfile.txt) -value "ExpectedContent" -passthru
+        It "should Add-Content to testdrive:\dynamicfile.txt with dynamic parameters" -Pending:($IsLinux -Or $IsOSX) {#https://github.com/PowerShell/PowerShell/issues/891
+            $result=add-content -path testdrive:\dynamicfile.txt -value "ExpectedContent" -passthru
             $result| Should BeExactly "ExpectedContent"
         }
-        It "should return expected string from testdrive:\dynamicfile.txt" {
+        It "should return expected string from testdrive:\dynamicfile.txt" -Pending:($IsLinux -Or $IsOSX) {#https://github.com/PowerShell/PowerShell/issues/891
             $result = get-content -path testdrive:\dynamicfile.txt
             $result | Should BeExactly "ExpectedContent"
         }
@@ -35,10 +35,10 @@ Describe "Add-Content cmdlet tests" {
             #[DRT][BugId(BugDatabase.WindowsOutOfBandReleases, 906022)]
             {add-content -path HKLM:\\software\\microsoft -value "ShouldNotWorkBecausePathIsUnsupported" -ea stop} | Should Throw "IContentCmdletProvider interface is not implemented"
         }
-        It "should be able to pass multiple [string]`$objects to Add-Content through the pipeline to output a dynamic Path file" {
+        It "should be able to pass multiple [string]`$objects to Add-Content through the pipeline to output a dynamic Path file" -Pending:($IsLinux -Or $IsOSX) {#https://github.com/PowerShell/PowerShell/issues/891
             #[DRT][BugId(BugDatabase.WindowsOutOfBandReleases, 9058182)]
-            "hello","world"|add-content -path (Join-Path testdrive: -ChildPath dynamicfile2.txt)
-            $result=get-content -path testdrive:\dynamicfile2.txt
+            "hello","world"|add-content testdrive:\dynamicfile2.txt
+            $result=get-content testdrive:\dynamicfile2.txt
             $result.length |Should be 2
             $result[0]     |Should be "hello"
             $result[1]     |Should be "world"
