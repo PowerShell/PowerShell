04c49c376b50b79a2839f6569b01569bd96b1f9f
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-04-27 12:30:35 -0700

    Fix resource manager
    Since the project is `Microsoft.PowerShell.PSReadLine`, the embedded
    resource name had to be fixed.

--- src/Microsoft.PowerShell.PSReadLine/PSReadLineResources.Designer.cs
+++ src/Microsoft.PowerShell.PSReadLine/PSReadLineResources.Designer.cs
@@ -39,7 +39,7 @@ namespace Microsoft.PowerShell {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PSReadLine.PSReadLineResources", typeof(PSReadLineResources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.PowerShell.PSReadLine.PSReadLineResources", typeof(PSReadLineResources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
