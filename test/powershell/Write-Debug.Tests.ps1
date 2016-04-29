5b9ffb7ff58a12bf2975b1d39ca2c8d9f4d023cb
TingLiu6 <v-litin@microsoft.com>
2016-04-29 01:07:57 -0700

    Fix the CR issues for Write command test

--- test/powershell/Write-Debug.Tests.ps1
+++ test/powershell/Write-Debug.Tests.ps1
@@ -1,23 +1,46 @@
-﻿Describe "Write-Debug DRT Unit Tests" -Tags DRT{
+﻿Describe "Write-Debug DRT Unit Tests" -Tags DRT{     
     function unittest-writedebugline
     {
-        Write-Host ""
-        Write-Host "foo"
-        Write-Host "hello" -ForegroundColor Black -BackgroundColor Cyan
-        Write-Debug "this is a test"
+        $DebugPreference = 'continue'
+        Write-Debug "this is a test" 
+    }
+    
+    function unittest-writedebug
+    {
+        [cmdletbinding()]
+        Param (
+            [String]$Value
+        )
+
+        $PSBoundParameters.GetEnumerator() | ForEach {
+        Write-Verbose $_
+        }
+        Write-Verbose "DebugPreference: $DebugPreference"
+
+        If ($PSBoundParameters['Debug']) {
+        $DebugPreference = 'Continue'
+        }
+
+        Write-Debug $Value
     }
 
-    It "Write-Debug Test" {
-        $o = "this is a test"
-        { Write-Debug $o } | Should Not Throw
+    It 'Write-Debug Test1: $DebugPreference is set' {
+        $ps = [PowerShell]::Create()
+        $null = $ps.AddScript("`$o = 'this is a test';`$DebugPreference = 'continue';Write-Debug `$o").Invoke()
+        $ps.Streams.Debug | Should Be 'this is a test'
     }
 
-    It "Write-Debug Test2" {
-        { $debugpreference='continue'; Write-Debug 'hello' } | Should Not Throw
-        $debugpreference ='SilentlyContinue'
+    It "Write-Debug Test2: Calling a cmdleting binding and passing -Debug " {      
+        $ps = [PowerShell]::Create()
+        $null = $ps.AddScript("Write-Debug foo -debug").Invoke()
+        $ps.Streams.Debug | Should Be 'foo'
+    }
+    
+    It "Write-Debug Test3: Calling a regular function" {              
+        unittest-writedebugline 5>&1 | Should Be 'this is a test'
     }
 
-    It "Write-Debug Test3" {
-        { unittest-writedebugline } | Should Not Throw
+    It "Write-Debug Test4: Redirecting the debug stream" {
+        unittest-writedebug -Value "foo" -Debug 5>&1 | Should Be 'foo'
     }
 }
