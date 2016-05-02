2d738fca35301f10f0529e786366b690ae4acbcf
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-05-02 12:50:32 -0700

    Don't build EngineInstaller

--- src/Microsoft.PowerShell.ConsoleHost/singleshell/installer/EngineInstaller.cs
+++ src/Microsoft.PowerShell.ConsoleHost/singleshell/installer/EngineInstaller.cs
@@ -1,3 +1,4 @@
+#if !OPEN
 /********************************************************************++
 Copyright (c) Microsoft Corporation.  All rights reserved.
 --********************************************************************/
@@ -86,3 +87,4 @@ namespace Microsoft.PowerShell
         }
     }
 }
+#endif
