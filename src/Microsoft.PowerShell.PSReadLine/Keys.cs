4142d9ec34f682c53f86d9393c1591ccc0030296
George Fleming <v-geflem@microsoft.com>
2016-05-03 15:48:10 -0700

    Modify KeyChar so that bindings will show up correctly

--- src/Microsoft.PowerShell.PSReadLine/Keys.cs
+++ src/Microsoft.PowerShell.PSReadLine/Keys.cs
@@ -108,9 +108,17 @@ namespace Microsoft.PowerShell
         new
         public static ConsoleKeyInfo Equals      = new ConsoleKeyInfo('=', ConsoleKey.OemPlus, false, false, false);
 
+#if CORECLR
+        public static ConsoleKeyInfo CtrlAt       = new ConsoleKeyInfo('@', ConsoleKey.D2, true, false, true);
+#else
         public static ConsoleKeyInfo CtrlAt       = new ConsoleKeyInfo((char)0, ConsoleKey.D2, true, false, true);
+#endif
         public static ConsoleKeyInfo AltUnderbar = new ConsoleKeyInfo('_', ConsoleKey.OemMinus, true, true, false);
+#if CORECLR
+        public static ConsoleKeyInfo CtrlUnderbar = new ConsoleKeyInfo('_', ConsoleKey.OemMinus, true, false, true);
+#else
         public static ConsoleKeyInfo CtrlUnderbar = new ConsoleKeyInfo((char)31, ConsoleKey.OemMinus, true, false, true);
+#endif
         public static ConsoleKeyInfo AltEquals    = new ConsoleKeyInfo('=', ConsoleKey.OemPlus, false, true, false);
         public static ConsoleKeyInfo Space        = new ConsoleKeyInfo(' ', ConsoleKey.Spacebar, false, false, false);
         // Useless because it's caught by the console to bring up the system menu.
@@ -118,10 +126,19 @@ namespace Microsoft.PowerShell
         public static ConsoleKeyInfo CtrlSpace   = new ConsoleKeyInfo(' ', ConsoleKey.Spacebar, false, false, true);
         public static ConsoleKeyInfo AltLess     = new ConsoleKeyInfo('<', ConsoleKey.OemComma, true, true, false);
         public static ConsoleKeyInfo AltGreater  = new ConsoleKeyInfo('>', ConsoleKey.OemPeriod, true, true, false);
+#if CORECLR
+        public static ConsoleKeyInfo CtrlRBracket = new ConsoleKeyInfo(']', ConsoleKey.Oem6, false, false, true);
+        public static ConsoleKeyInfo AltCtrlRBracket = new ConsoleKeyInfo(']', ConsoleKey.Oem6, false, true, true);
+#else
         public static ConsoleKeyInfo CtrlRBracket = new ConsoleKeyInfo((char)29, ConsoleKey.Oem6, false, false, true);
         public static ConsoleKeyInfo AltCtrlRBracket = new ConsoleKeyInfo((char)0, ConsoleKey.Oem6, false, true, true);
+#endif
         public static ConsoleKeyInfo AltPeriod    = new ConsoleKeyInfo('.', ConsoleKey.OemPeriod, false, true, false);
+#if CORECLR
+        public static ConsoleKeyInfo CtrlAltQuestion  = new ConsoleKeyInfo('?', ConsoleKey.Oem2, true, true, true);
+#else
         public static ConsoleKeyInfo CtrlAltQuestion  = new ConsoleKeyInfo((char)0, ConsoleKey.Oem2, true, true, true);
+#endif
         public static ConsoleKeyInfo AltQuestion  = new ConsoleKeyInfo('?', ConsoleKey.Oem2, true, true, false);
 
         public static ConsoleKeyInfo Alt0 = new ConsoleKeyInfo('0', ConsoleKey.D0, false, true, false);
