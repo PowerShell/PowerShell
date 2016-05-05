5c0d17e3a2973dd7be5cae7b0793af6bdcb4b2ba
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-05-04 18:05:46 -0700

    Update ConsoleHost tests

--- test/powershell/ConsoleHost.Tests.ps1
+++ test/powershell/ConsoleHost.Tests.ps1
@@ -1,11 +1,11 @@
-Describe "CoreConsoleHost unit tests" {
+Describe "ConsoleHost unit tests" {
     $powershell = Join-Path -Path $PsHome -ChildPath "powershell"
 
     Context "Command-line parsing" {
 
         foreach ($x in "--help", "-help", "-h", "-?", "--he", "-hel", "--HELP", "-hEl") {
             It "Accepts '$x' as a parameter for help" {
-                & $powershell -noprofile $x | ?{ $_ -match "usage: powershell" } | Should Not BeNullOrEmpty
+                & $powershell -noprofile $x | ?{ $_ -match "PowerShell[.exe] -Help | -? | /?" } | Should Not BeNullOrEmpty
             }
         }
 
