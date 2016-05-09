555e4c2e26ab9f9a60c26e68ef361ab53834aa5d
Dicey Dai <v-saidai@microsoft.com>
2016-05-08 22:10:56 -0700

    Add Unit Test for Get-UICulture

--- test/powershell/Get-UICulture.Tests.ps1
+++ test/powershell/Get-UICulture.Tests.ps1
@@ -1,3 +1,12 @@
+Describe "Get-UICulture DRT Unit Tests" -Tags DRT{ 
+    It "Should works proper with get-uiculture" { 
+        $results = get-uiculture 
+        $results[0].GetType().Name | Should Be CultureInfo 
+        $results[0].Name | Should Be $PsUICulture 
+     } 
+
+} 
+
 Describe "Get-UICulture" {
     It "Should return a type of CultureInfo as the name of the type" {
 	(Get-UICulture).GetType().Name | Should Match CultureInfo
