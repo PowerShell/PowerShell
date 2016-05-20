c2479d38b4cda378b944eba373b5e257e25b5bb1
TingLiu6 <v-litin@microsoft.com>
2016-05-19 20:00:38 -0700

    Fixed CR Issues

--- test/powershell/ConvertTo-Xml.Tests.ps1
+++ test/powershell/ConvertTo-Xml.Tests.ps1
@@ -5,18 +5,14 @@
         $returnObject = $customPSObject | ConvertTo-Xml -Depth 1 
         $returnObject -is [System.Xml.XmlDocument] | Should Be $true
         #$xml = [System.Xml.XmlDocument]$returnObject
-        $expectedValue = @"
-<?xml version="1.0" encoding="utf-8"?><Objects><Object Type="System.Management.Automation.PSCustomObject"><Property Name="prop1" Type="System.String">val1</Property><Property Name="prop2" Type="System.String">val2</Property></Object></Objects>
-"@
+        $expectedValue = '<?xml version="1.0" encoding="utf-8"?><Objects><Object Type="System.Management.Automation.PSCustomObject">' + '<Property Name="prop1" Type="System.String">val1</Property><Property Name="prop2" Type="System.String">val2</Property></Object></Objects>'
         $returnObject.OuterXml | Should Be $expectedValue
     }
 
     It "Test convertto-xml with notypeinfo parameter" {
         $returnObject = $customPSObject | ConvertTo-Xml -NoTypeInformation
         $returnObject -is [System.Xml.XmlDocument] | Should Be $true
-        $expectedValue = @"
-<?xml version="1.0" encoding="utf-8"?><Objects><Object><Property Name="prop1">val1</Property><Property Name="prop2">val2</Property></Object></Objects>
-"@
+        $expectedValue = '<?xml version="1.0" encoding="utf-8"?><Objects><Object>' + '<Property Name="prop1">val1</Property><Property Name="prop2">val2</Property></Object></Objects>'
         $returnObject.OuterXml | Should Be $expectedValue
     }
 
@@ -33,29 +29,24 @@
     It "Test convertto-xml as Stream" {
         $returnObject = $customPSObject | ConvertTo-Xml -As Stream
         $returnObject -is [System.Array] | Should Be $true
-        $stream1 = @"
-<?xml version="1.0" encoding="utf-8"?>
-"@
-        $stream2 = "<Objects>"
+        $stream1 = '<?xml version="1.0" encoding="utf-8"?>'
+        $stream2 = '<Objects>'
         $stream3 = @"
 <Object Type="System.Management.Automation.PSCustomObject">$newLine  <Property Name="prop1" Type="System.String">val1</Property>$newLine  <Property Name="prop2" Type="System.String">val2</Property>$newLine</Object>
 "@
-        $stream4 = "</Objects>"
+        $stream4 = '</Objects>'
 
         $returnObject.Count | Should Be 4
         $returnObject[0] | Should Be $stream1
         $returnObject[1] | Should Be $stream2
         $returnObject[2] | Should Be $stream3
         $returnObject[3] | Should Be $stream4
-
     }
 
     It "Test convertto-xml as Document" {
         $returnObject = $customPSObject | ConvertTo-Xml -As Document -NoTypeInformation
         $returnObject -is [System.Xml.XmlDocument] | Should Be $true
-        $expectedValue = @"
-<?xml version="1.0" encoding="utf-8"?><Objects><Object><Property Name="prop1">val1</Property><Property Name="prop2">val2</Property></Object></Objects>
-"@
+        $expectedValue = '<?xml version="1.0" encoding="utf-8"?><Objects><Object><Property Name="prop1">val1</Property><Property Name="prop2">val2</Property></Object></Objects>'
         $returnObject.OuterXml | Should Be $expectedValue
     }
 }
