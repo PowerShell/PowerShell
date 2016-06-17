552354e4e2980d5abece2a5d1e07588cd5bdca72
PowerShell Team <PowerShellTeam@hotmail.com>
2016-06-16 19:07:37 -0700

    Copy PSScheduledJob sources from psl-monad [SD:715912]
    commit 77eda78

---
+++ src/Microsoft.PowerShell.ScheduledJob/commands/DisableJobDefinition.cs
@@ -0,0 +1,32 @@
+﻿/********************************************************************++
+Copyright (c) Microsoft Corporation.  All rights reserved.
+--********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace Microsoft.PowerShell.ScheduledJob
+{
+    /// <summary>
+    /// This cmdlet disables the specified ScheduledJobDefinition.
+    /// </summary>
+    [Cmdlet(VerbsLifecycle.Disable, "ScheduledJob", SupportsShouldProcess = true, DefaultParameterSetName = DisableScheduledJobDefinitionBase.DefinitionParameterSet,
+        HelpUri = "http://go.microsoft.com/fwlink/?LinkID=223927")]
+    [OutputType(typeof(ScheduledJobDefinition))]
+    public sealed class DisableScheduledJobCommand : DisableScheduledJobDefinitionBase
+    {
+        #region Properties
+
+        /// <summary>
+        /// Returns true if scheduled job defintion should be enabled,
+        /// false otherwise.
+        /// </summary>
+        protected override bool Enabled
+        {
+            get { return false; }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Microsoft.PowerShell.ScheduledJob/commands/EnableJobTrigger.cs
@@ -0,0 +1,36 @@
+﻿/********************************************************************++
+Copyright (c) Microsoft Corporation.  All rights reserved.
+--********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Management.Automation;
+using System.Management.Automation.Internal;
+using System.Management.Automation.Host;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Microsoft.PowerShell.ScheduledJob
+{
+    /// <summary>
+    /// This cmdlet disables triggers on a ScheduledJobDefinition object.
+    /// </summary>
+    [Cmdlet(VerbsLifecycle.Enable, "JobTrigger", SupportsShouldProcess = true, DefaultParameterSetName = EnableJobTriggerCommand.EnabledParameterSet,
+        HelpUri = "http://go.microsoft.com/fwlink/?LinkID=223917")]
+    public sealed class EnableJobTriggerCommand : EnableDisableScheduledJobCmdletBase
+    {
+       #region Enabled Implementation
+
+        /// <summary>
+        /// Property to determine if trigger should be enabled or disabled.
+        /// </summary>
+        internal override bool Enabled
+        {
+            get { return true; }
+        }
+
+        #endregion
+    }
+}
