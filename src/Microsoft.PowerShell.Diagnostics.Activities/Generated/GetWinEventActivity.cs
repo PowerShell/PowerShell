800abb620347309fd8467cc7d51cf0187830bfeb
PowerShell Team <PowerShellTeam@hotmail.com>
2016-06-21 15:00:20 -0700

    Add source files for Microsoft.PowerShell.Diagnostics.Activities

---
+++ src/Microsoft.PowerShell.Diagnostics.Activities/Generated/GetWinEventActivity.cs
@@ -0,0 +1,212 @@
+//
+//    Copyright (C) Microsoft.  All rights reserved.
+//
+using Microsoft.PowerShell.Activities;
+using System.Management.Automation;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+
+namespace Microsoft.PowerShell.Diagnostics.Activities
+{
+    /// <summary>
+    /// Activity to invoke the Microsoft.PowerShell.Diagnostics\Get-WinEvent command in a Workflow.
+    /// </summary>
+    [System.CodeDom.Compiler.GeneratedCode("Microsoft.PowerShell.Activities.ActivityGenerator.GenerateFromName", "3.0")]
+    public sealed class GetWinEvent : PSRemotingActivity
+    {
+        /// <summary>
+        /// Gets the display name of the command invoked by this activity.
+        /// </summary>
+        public GetWinEvent()
+        {
+            this.DisplayName = "Get-WinEvent";
+        }
+
+        /// <summary>
+        /// Gets the fully qualified name of the command invoked by this activity.
+        /// </summary>
+        public override string PSCommandName { get { return "Microsoft.PowerShell.Diagnostics\\Get-WinEvent"; } }
+        
+        // Arguments
+        
+        /// <summary>
+        /// Provides access to the ListLog parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.String[]> ListLog { get; set; }
+
+        /// <summary>
+        /// Provides access to the LogName parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.String[]> LogName { get; set; }
+
+        /// <summary>
+        /// Provides access to the ListProvider parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.String[]> ListProvider { get; set; }
+
+        /// <summary>
+        /// Provides access to the ProviderName parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.String[]> ProviderName { get; set; }
+
+        /// <summary>
+        /// Provides access to the Path parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.String[]> Path { get; set; }
+
+        /// <summary>
+        /// Provides access to the MaxEvents parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.Int64> MaxEvents { get; set; }
+
+        /// <summary>
+        /// Provides access to the Credential parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.Management.Automation.PSCredential> Credential { get; set; }
+
+        /// <summary>
+        /// Provides access to the FilterXPath parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.String> FilterXPath { get; set; }
+
+        /// <summary>
+        /// Provides access to the FilterXml parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.Xml.XmlDocument> FilterXml { get; set; }
+
+        /// <summary>
+        /// Provides access to the FilterHashtable parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.Collections.Hashtable[]> FilterHashtable { get; set; }
+
+        /// <summary>
+        /// Provides access to the Force parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.Management.Automation.SwitchParameter> Force { get; set; }
+
+        /// <summary>
+        /// Provides access to the Oldest parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.Management.Automation.SwitchParameter> Oldest { get; set; }
+
+        /// <summary>
+        /// Declares that this activity supports its own remoting.
+        /// </summary>        
+        protected override bool SupportsCustomRemoting { get { return true; } }
+
+
+        // Module defining this command
+        
+
+        // Optional custom code for this activity
+        
+
+        /// <summary>
+        /// Returns a configured instance of System.Management.Automation.PowerShell, pre-populated with the command to run.
+        /// </summary>
+        /// <param name="context">The NativeActivityContext for the currently running activity.</param>
+        /// <returns>A populated instance of Sytem.Management.Automation.PowerShell</returns>
+        /// <remarks>The infrastructure takes responsibility for closing and disposing the PowerShell instance returned.</remarks>
+        protected override ActivityImplementationContext GetPowerShell(NativeActivityContext context)
+        {
+            System.Management.Automation.PowerShell invoker = global::System.Management.Automation.PowerShell.Create();
+            System.Management.Automation.PowerShell targetCommand = invoker.AddCommand(PSCommandName);
+
+            // Initialize the arguments
+            
+            if(ListLog.Expression != null)
+            {
+                targetCommand.AddParameter("ListLog", ListLog.Get(context));
+            }
+
+            if(LogName.Expression != null)
+            {
+                targetCommand.AddParameter("LogName", LogName.Get(context));
+            }
+
+            if(ListProvider.Expression != null)
+            {
+                targetCommand.AddParameter("ListProvider", ListProvider.Get(context));
+            }
+
+            if(ProviderName.Expression != null)
+            {
+                targetCommand.AddParameter("ProviderName", ProviderName.Get(context));
+            }
+
+            if(Path.Expression != null)
+            {
+                targetCommand.AddParameter("Path", Path.Get(context));
+            }
+
+            if(MaxEvents.Expression != null)
+            {
+                targetCommand.AddParameter("MaxEvents", MaxEvents.Get(context));
+            }
+
+            if(Credential.Expression != null)
+            {
+                targetCommand.AddParameter("Credential", Credential.Get(context));
+            }
+
+            if(FilterXPath.Expression != null)
+            {
+                targetCommand.AddParameter("FilterXPath", FilterXPath.Get(context));
+            }
+
+            if(FilterXml.Expression != null)
+            {
+                targetCommand.AddParameter("FilterXml", FilterXml.Get(context));
+            }
+
+            if(FilterHashtable.Expression != null)
+            {
+                targetCommand.AddParameter("FilterHashtable", FilterHashtable.Get(context));
+            }
+
+            if(Force.Expression != null)
+            {
+                targetCommand.AddParameter("Force", Force.Get(context));
+            }
+
+            if(Oldest.Expression != null)
+            {
+                targetCommand.AddParameter("Oldest", Oldest.Get(context));
+            }
+
+            if(GetIsComputerNameSpecified(context) && (PSRemotingBehavior.Get(context) == RemotingBehavior.Custom))
+            {
+                targetCommand.AddParameter("ComputerName", PSComputerName.Get(context));
+            }
+
+
+            return new ActivityImplementationContext() { PowerShellInstance = invoker };
+        }
+    }
+}
---
+++ src/Microsoft.PowerShell.Diagnostics.Activities/Generated/NewWinEventActivity.cs
@@ -0,0 +1,106 @@
+//
+//    Copyright (C) Microsoft.  All rights reserved.
+//
+using Microsoft.PowerShell.Activities;
+using System.Management.Automation;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+
+namespace Microsoft.PowerShell.Diagnostics.Activities
+{
+    /// <summary>
+    /// Activity to invoke the Microsoft.PowerShell.Diagnostics\New-WinEvent command in a Workflow.
+    /// </summary>
+    [System.CodeDom.Compiler.GeneratedCode("Microsoft.PowerShell.Activities.ActivityGenerator.GenerateFromName", "3.0")]
+    public sealed class NewWinEvent : PSRemotingActivity
+    {
+        /// <summary>
+        /// Gets the display name of the command invoked by this activity.
+        /// </summary>
+        public NewWinEvent()
+        {
+            this.DisplayName = "New-WinEvent";
+        }
+
+        /// <summary>
+        /// Gets the fully qualified name of the command invoked by this activity.
+        /// </summary>
+        public override string PSCommandName { get { return "Microsoft.PowerShell.Diagnostics\\New-WinEvent"; } }
+        
+        // Arguments
+        
+        /// <summary>
+        /// Provides access to the ProviderName parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.String> ProviderName { get; set; }
+
+        /// <summary>
+        /// Provides access to the WinEventId parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.Int32> WinEventId { get; set; }
+
+        /// <summary>
+        /// Provides access to the Version parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.Byte> Version { get; set; }
+
+        /// <summary>
+        /// Provides access to the Payload parameter.
+        /// </summary>
+        [ParameterSpecificCategory]
+        [DefaultValue(null)]
+        public InArgument<System.Object[]> Payload { get; set; }
+
+
+        // Module defining this command
+        
+
+        // Optional custom code for this activity
+        
+
+        /// <summary>
+        /// Returns a configured instance of System.Management.Automation.PowerShell, pre-populated with the command to run.
+        /// </summary>
+        /// <param name="context">The NativeActivityContext for the currently running activity.</param>
+        /// <returns>A populated instance of Sytem.Management.Automation.PowerShell</returns>
+        /// <remarks>The infrastructure takes responsibility for closing and disposing the PowerShell instance returned.</remarks>
+        protected override ActivityImplementationContext GetPowerShell(NativeActivityContext context)
+        {
+            System.Management.Automation.PowerShell invoker = global::System.Management.Automation.PowerShell.Create();
+            System.Management.Automation.PowerShell targetCommand = invoker.AddCommand(PSCommandName);
+
+            // Initialize the arguments
+            
+            if(ProviderName.Expression != null)
+            {
+                targetCommand.AddParameter("ProviderName", ProviderName.Get(context));
+            }
+
+            if(WinEventId.Expression != null)
+            {
+                targetCommand.AddParameter("Id", WinEventId.Get(context));
+            }
+
+            if(Version.Expression != null)
+            {
+                targetCommand.AddParameter("Version", Version.Get(context));
+            }
+
+            if(Payload.Expression != null)
+            {
+                targetCommand.AddParameter("Payload", Payload.Get(context));
+            }
+
+
+            return new ActivityImplementationContext() { PowerShellInstance = invoker };
+        }
+    }
+}
