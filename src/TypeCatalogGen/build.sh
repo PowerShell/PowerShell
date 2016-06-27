5e3150f429085812acbf89108f08d326aa07630b
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-06-27 15:41:47 -0700

    Use Start-TypeGen to generate type catalog dynamically

--- src/TypeCatalogGen/build.sh
+++
@@ -1,7 +0,0 @@
-pushd ../TypeCatalogParser
-dotnet restore -v Warning
-dotnet run
-popd
-
-dotnet restore -v Warning
-dotnet run ../Microsoft.PowerShell.CoreCLR.AssemblyLoadContext/CorePsTypeCatalog.cs powershell.inc
