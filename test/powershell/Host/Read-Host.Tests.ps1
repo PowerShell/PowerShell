8d23dad0ab62945219317a61adf2e245d9fcf840
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-07-07 14:46:14 -0700

    Add Read-Host regression test

---
+++ test/powershell/Host/Read-Host.Tests.ps1
@@ -0,0 +1,20 @@
+Describe "Read-Host" {
+    Context "[Console]::ReadKey() implementation on non-Windows" {
+        BeforeAll {
+            $powershell = Join-Path -Path $PsHome -ChildPath "powershell"
+            $assetsDir = Join-Path -Path $PSScriptRoot -ChildPath assets
+            if ($IsWindows) {
+                $ItArgs = @{ skip = $true }
+            } elseif (-not (Get-Command expect -ErrorAction Ignore)) {
+                $ItArgs = @{ pending = $true }
+            } else {
+                $ItArgs = @{ }
+            }
+        }
+
+        It @ItArgs "Should output correctly" {
+            & (Join-Path $assetsDir "Read-Host.Output.expect") $powershell | Out-Null
+            $LASTEXITCODE | Should Be 0
+        }
+    }
+}
