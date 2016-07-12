6f938bd8604f66f0f169b339c78aa98f2011edb6
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-07-12 16:14:37 -0700

    Add regression test for Get-Process -IncludeUserName

--- test/powershell/Modules/Microsoft.PowerShell.Management/Get-Process.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Get-Process.Tests.ps1
@@ -1,4 +1,8 @@
 Describe "Get-Process" {
+    It "Should support -IncludeUserName" {
+        (Get-Process powershell -IncludeUserName | Select-Object -First 1).UserName | Should Match $env:USERNAME
+    }
+
     # These tests are no good, please replace!
     It "Should return a type of Object[] for Get-Process cmdlet" -Pending:$IsOSX {
         (Get-Process).GetType().BaseType | Should Be 'array'
