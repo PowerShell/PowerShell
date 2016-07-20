229aa31bb8ba9a760681c9207cea7d3553e62b71
James Truher <jimtru@microsoft.com>
2016-07-20 14:28:19 -0700

    Add migrated files

---
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/NewGuid.Tests.ps1
@@ -0,0 +1,19 @@
+Describe -Tags "Unit","DRT" "New-Guid" {
+
+    It "returns a new guid" {
+        $guid = New-Guid
+        $guid.GetType().FullName | Should Be "System.Guid"
+    }
+
+    It "should not be all zeros" {
+        $guid = New-Guid
+        $guid.ToString() | Should Not Be "00000000-0000-0000-0000-000000000000"
+    }
+
+    It "should return different guids with each call" {
+        $guid1 = New-Guid
+        $guid2 = New-Guid
+        $guid1.ToString() | Should Not Be $guid2.ToString()
+    }
+}
+
---
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/string.tests.ps1
@@ -0,0 +1,61 @@
+Describe "String cmdlets" -Tags 'innerloop', 'P1', 'RI' {
+    Context "Select-String" {
+        BeforeAll {
+            $fileName = New-Item 'TestDrive:\selectStr[ingLi]teralPath.txt'
+            "abc" | Out-File -LiteralPath $fileName.fullname            
+	        "bcd" | Out-File -LiteralPath $fileName.fullname -Append
+	        "cde" | Out-File -LiteralPath $fileName.fullname -Append
+
+            $fileNameWithDots = $fileName.FullName.Replace("\", "\.\")
+            
+            $tempFile = New-TemporaryFile            
+            "abc" | Out-File -LiteralPath $tempFile.fullname            
+	        "bcd" | Out-File -LiteralPath $tempFile.fullname -Append
+	        "cde" | Out-File -LiteralPath $tempFile.fullname -Append
+            $driveLetter = $tempFile.PSDrive.Name
+            $fileNameAsNetworkPath = "\\localhost\$driveLetter`$" + $tempFile.FullName.SubString(2)
+
+	        Push-Location "$fileName\.."
+        }
+
+        AfterAll {
+            Remove-Item $tempFile -Force -ErrorAction SilentlyContinue
+            Pop-Location
+        }
+
+        It "LiteralPath with relative path" {
+            (select-string -LiteralPath (Get-Item -LiteralPath $fileName).Name "b").count | Should Be 2	    
+        } 
+
+        It "LiteralPath with absolute path" {	        
+            (select-string -LiteralPath $fileName "b").count | Should Be 2	    
+        }
+
+        It "LiteralPath with dots in path" {	        
+            (select-string -LiteralPath $fileNameWithDots "b").count | Should Be 2	    
+        }
+
+        It "Network path" {
+            (select-string -LiteralPath $fileNameAsNetworkPath "b").count | Should Be 2
+        }
+
+        It "throws error for non filesystem providers" {
+            select-string -literalPath cert:\currentuser\my "a" -ErrorAction SilentlyContinue -ErrorVariable selectStringError
+            $selectStringError.FullyQualifiedErrorId | Should Be 'ProcessingFile,Microsoft.PowerShell.Commands.SelectStringCommand'
+        }
+
+        It "throws parameter binding exception for invalid context" {
+            { select-string It $PSScriptRoot -Context -1,-1 } | Should Throw Context
+        }
+
+        It "match object supports RelativePath method" {
+            $file = "Modules\Microsoft.PowerShell.Utility\Microsoft.PowerShell.Utility.psd1"
+            
+            $match = Select-String CmdletsToExport $pshome/$file
+            
+            $match.RelativePath($pshome) | Should Be $file
+            $match.RelativePath($pshome.ToLower()) | Should Be $file
+            $match.RelativePath($pshome.ToUpper()) | Should Be $file
+        }
+    }
+}
