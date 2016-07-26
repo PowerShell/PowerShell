3240a2d3696f8fb5c0e78764a50cecf1e176901d
Sergei Vorobev <sevoroby@microsoft.com>
2016-07-26 10:36:53 -0700

    Fix ConvertFrom-Json to handle mutlilines
    Fix #956
    
    Add Newtonsoft.Json.JsonSerializationException to catch block
    that determines which mode to use: single-string or multi-string

---
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertFrom-Json.Tests.ps1
@@ -0,0 +1,16 @@
+Describe 'ConvertFrom-Json' {
+    It 'can convert a single-line object' {
+        ('{"a" : "1"}' | ConvertFrom-Json).a | Should Be 1
+    }
+
+    It 'can convert one string-per-object' {
+        $json = @('{"a" : "1"}', '{"a" : "x"}') | ConvertFrom-Json
+        $json.Count | Should Be 2
+        $json[1].a | Should Be 'x'
+    }
+
+    It 'can convert mutli-line object' {
+        $json = @('{"a" :', '"x"}') | ConvertFrom-Json
+        $json.a | Should Be 'x'
+    }
+}
