ac8238075d5af5be06f0467a61599bb6ba411baa
James Truher <jimtru@microsoft.com>
2016-07-27 12:14:08 -0700

    mark the add-type tests as pending for $IsCore

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Add-Type.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Add-Type.Tests.ps1
@@ -1,11 +1,11 @@
 $guid = [Guid]::NewGuid().ToString().Replace("-","")
 
 Describe "Add-Type" -Tags "CI" {
-    It "Should not throw given a simple class definition" {
+    It "Should not throw given a simple class definition" -pending:($IsCore) {
         { Add-Type -TypeDefinition "public static class foo { }" } | Should Not Throw
     }
 
-    It "Can use System.Management.Automation.CmdletAttribute" {
+    It "Can use System.Management.Automation.CmdletAttribute" -pending:($IsCore) {
         $code = @"
 [System.Management.Automation.Cmdlet("Get", "Thing", ConfirmImpact = System.Management.Automation.ConfirmImpact.High, SupportsPaging = true)]
 public class AttributeTest$guid {}
@@ -13,7 +13,7 @@ public class AttributeTest$guid {}
         Add-Type -TypeDefinition $code -PassThru | Should Not Be $null
     }
 
-    It "Can load TPA assembly System.Runtime.Serialization.Primitives.dll" {
+    It "Can load TPA assembly System.Runtime.Serialization.Primitives.dll" -pending:($IsCore) {
         Add-Type -AssemblyName 'System.Runtime.Serialization.Primitives' -PassThru | Should Not Be $null
     }
 }
