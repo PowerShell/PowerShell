f0233a8d20b654bef97a82d7f415c35ddd73dd53
James Truher <jimtru@microsoft.com>
2016-07-27 11:37:06 -0700

    fix bad tag to check tag checking

--- test/powershell/engine/BasicEngine.Tests.ps1
+++ test/powershell/engine/BasicEngine.Tests.ps1
@@ -1,4 +1,4 @@
-Describe 'Basic engine APIs' -Tags "DRT" {
+Describe 'Basic engine APIs' -Tags "CI" {
     Context 'powershell::Create' {
         It 'can create default instance' {
             [powershell]::Create() | Should Not Be $null
