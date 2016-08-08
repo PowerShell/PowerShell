PowerShell/PowerShell

commit 0dd425da1994bb3c0580ca1bb0d0856d0aa7161b

Integrate changes between [SD:717473] and [SD:725290]

src/Modules/Windows+Unix-Core/Microsoft.PowerShell.Utility/Microsoft.PowerShell.Utility.psd1
@@ -1,17 +1,17 @@
-@{
+﻿@{
 GUID="1DA87E53-152B-403E-98DC-74D7B4D63D59"
 Author="Microsoft Corporation"
 CompanyName="Microsoft Corporation"
-Copyright="� Microsoft Corporation. All rights reserved."
+Copyright="© Microsoft Corporation. All rights reserved."
 ModuleVersion="3.1.0.0"
 PowerShellVersion="3.0"
 CmdletsToExport= "Format-List", "Format-Custom", "Format-Table", "Format-Wide",
     "Out-File", "Out-String", "Get-FormatData", "Export-FormatData", "ConvertFrom-Json", "ConvertTo-Json",
-    "Invoke-RestMethod", "Invoke-WebRequest",
-    "Register-ObjectEvent", "Register-EngineEvent", "Wait-Event", "Get-Event", "Remove-Event",
-    "Get-EventSubscriber", "Unregister-Event", "New-Event", "Add-Member", "Add-Type", "Compare-Object",
-    "ConvertFrom-StringData", "Export-Csv", "Import-Csv", "ConvertTo-Csv", "ConvertFrom-Csv", "Export-Alias",
-    "Invoke-Expression", "Get-Alias", "Get-Culture", "Get-Date", "Get-Host", "Get-Member", "Get-Random",
+    "Invoke-RestMethod", "Invoke-WebRequest", "Register-ObjectEvent", "Register-EngineEvent",
+    "Wait-Event", "Get-Event", "Remove-Event", "Get-EventSubscriber", "Unregister-Event",
+    "New-Event", "Add-Member", "Add-Type", "Compare-Object", "ConvertFrom-StringData", 
+    "Export-Csv", "Import-Csv", "ConvertTo-Csv", "ConvertFrom-Csv", "Export-Alias", "Invoke-Expression",
+    "Get-Alias", "Get-Culture", "Get-Date", "Get-Host", "Get-Member", "Get-Random",
     "Get-UICulture", "Get-Unique", "Import-Alias", "Import-LocalizedData",
     "Select-String", "Measure-Object", "New-Alias", "New-TimeSpan", "Read-Host", "Set-Alias", "Set-Date",
     "Start-Sleep", "Tee-Object", "Measure-Command", "Update-TypeData", "Update-FormatData",
@@ -20,7 +20,6 @@ CmdletsToExport= "Format-List", "Format-Custom", "Format-Table", "Format-Wide",
     "Clear-Variable", "Export-Clixml", "Import-Clixml", "ConvertTo-Xml", "Select-Xml", "Write-Debug",
     "Write-Verbose", "Write-Warning", "Write-Error", "Write-Information", "Write-Output", "Set-PSBreakpoint",
     "Get-PSBreakpoint", "Remove-PSBreakpoint", "Enable-PSBreakpoint", "Disable-PSBreakpoint", "Get-PSCallStack",
-    "Get-TraceSource", "Set-TraceSource", "Trace-Command",
     "Unblock-File", "Get-Runspace", "Debug-Runspace", "Enable-RunspaceDebug", "Disable-RunspaceDebug",
     "Get-RunspaceDebug", "Wait-Debugger" 
 FunctionsToExport= "Get-FileHash", "New-TemporaryFile", "New-Guid", "Format-Hex", "Import-PowerShellDataFile",

src/Microsoft.PackageManagement.CoreProviders/Bootstrap/BootstrapProvider.cs
@@ -32,7 +32,6 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
     using Directory = System.IO.Directory;
     using ErrorCategory = PackageManagement.Internal.ErrorCategory;
     using File = System.IO.File;
-    using System.IO.Compression;
 
     public class BootstrapProvider {
         private static readonly Dictionary<string, string[]> _features = new Dictionary<string, string[]> {
@@ -324,125 +323,10 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
                             return false;
                     }
 
-                case Iso19770_2.MediaType.NuGetPackage:
-                    return InstallNugetPackage(provider, link, fastPath, request);
-
                 default:
                     request.Warning("Provider '{0}' with link '{1}' has unknown media type '{2}'.", provider.Name, link.HRef, link.MediaType);
                     return false;
             }
-        }
-
-        private bool InstallNugetPackage(Package provider, Link link, string fastPath, BootstrapRequest request)
-        {
-            // download the nuget package
-            string downloadedNupkg = request.DownloadAndValidateFile(provider._swidtag);
-
-            if (downloadedNupkg != null)
-            {
-                // extracted folder
-                string extractedFolder = String.Concat(FilesystemExtensions.GenerateTemporaryFileOrDirectoryNameInTempDirectory());
-
-                try
-                {
-                    //unzip the file
-                    ZipFile.ExtractToDirectory(downloadedNupkg, extractedFolder);
-
-                    if (Directory.Exists(extractedFolder))
-                    {
-                        string versionFolder = Path.Combine(request.DestinationPath(request), provider.Name, provider.Version);
-                        // tool folder is where we find things like nuget.exe
-                        string toolFolder = Path.Combine(extractedFolder, "tools");
-                        string libFolder = Path.Combine(extractedFolder, "lib");
-
-                        // create the directory version folder if not exist
-                        if (!Directory.Exists(versionFolder))
-                        {
-                            Directory.CreateDirectory(versionFolder);
-                        }
-
-                        // copy the tools directory
-                        if (Directory.Exists(toolFolder))
-                        {
-                            string destinationToolFolder = Path.Combine(versionFolder, "tools");
-
-                            if (!Directory.Exists(destinationToolFolder))
-                            {
-                                Directory.CreateDirectory(destinationToolFolder);
-                            }
-
-                            foreach (string child in Directory.EnumerateFiles(toolFolder))
-                            {
-                                try
-                                {
-                                    // try copy and overwrite
-                                    File.Copy(child, Path.Combine(destinationToolFolder, Path.GetFileName(child)), true);
-                                }
-                                catch (Exception e)
-                                {
-                                    request.Debug(e.StackTrace);
-                                    if (!(e is UnauthorizedAccessException || e is IOException))
-                                    {
-                                        // something wrong, delete the version folder
-                                        versionFolder.TryHardToDelete();
-                                        return false;
-                                    }
-
-                                    // otherwise this means the file is just being used. so just moves on to copy other files
-                                }
-                            }
-                        }
-
-                        // copy files from lib
-                        if (Directory.Exists(libFolder))
-                        {
-                            // check that the lib folder has at most 1 dll
-                            if (Directory.EnumerateFiles(libFolder).Count(file => String.Equals(Path.GetExtension(file), ".dll", StringComparison.OrdinalIgnoreCase)) > 1)
-                            {
-                                request.Warning(String.Format(CultureInfo.CurrentCulture, Resources.Messages.MoreThanOneDllExists, provider.Name));
-                                return false;
-                            }
-
-                            foreach (string child in Directory.EnumerateFiles(libFolder))
-                            {
-                                try
-                                {
-                                    File.Copy(child, Path.Combine(versionFolder, Path.GetFileName(child)), true);
-                                }
-                                catch (Exception e)
-                                {
-                                    request.Debug(e.StackTrace);
-                                    if (!(e is UnauthorizedAccessException || e is IOException))
-                                    {
-                                        // something wrong, delete the version folder
-                                        versionFolder.TryHardToDelete();
-                                        return false;
-                                    }
-
-                                    // otherwise this means the file is just being used. so just moves on to copy other files
-                                }
-                            }
-                        }
-
-                        // target file name is the assembly provider
-                        string targetFile = Path.Combine(versionFolder, Path.GetFileName(link.Attributes[Iso19770_2.Discovery.TargetFilename]));
-
-                        if (File.Exists(targetFile))
-                        {
-                            request.Verbose(Resources.Messages.InstalledPackage, provider.Name, targetFile);
-                            request.YieldFromSwidtag(provider, fastPath);
-                            return true;
-                        }
-                    }
-                }
-                finally
-                {
-                    downloadedNupkg.TryHardToDelete();
-                    extractedFolder.TryHardToDelete();
-                }
-            }
-
-            return false;
         }
 
         private bool InstallPackageFile(Package provider, string fastPath, BootstrapRequest request) {
@@ -522,7 +406,6 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
                 return false;
             }
 
-                        
             string targetFilename = fastPath;
             string file = fastPath;
 
@@ -532,7 +415,7 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
                 targetFilename = link.Attributes[Iso19770_2.Discovery.TargetFilename];
 
                 // download the file
-                file = request.DownloadAndValidateFile(provider._swidtag);              
+                file = request.DownloadAndValidateFile(provider._swidtag);
 
             }
 
@@ -551,22 +434,23 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
             //... providername\version\.dll
             var versionFolder = Path.Combine(request.DestinationPath(request), provider.Name, provider.Version);
 
-            if (!Directory.Exists(versionFolder)) {
-                //we create it
+            // if version folder exists, remove it
+            if (Directory.Exists(versionFolder))
+            {
+                RemoveDirectory(versionFolder);
+            }
+
+            // create the directory if we successfully deleted it
+            if (!Directory.Exists(versionFolder))
+            {
                 Directory.CreateDirectory(versionFolder);
             }
 
             var targetFile = Path.Combine(versionFolder, targetFilename);
-                           
+
             if (file != null) {
                 try
                 {
-                    // looks good! let's keep it
-                    if (File.Exists(targetFile)) {
-                        request.Debug("Removing old file '{0}'", targetFile);
-                        targetFile.TryHardToDelete();
-                    }
-
                     // is that file still there?
                     if (File.Exists(targetFile)) {
                         request.Error(ErrorCategory.InvalidOperation, fastPath, Constants.Messages.UnableToRemoveFile, targetFile);
@@ -575,7 +459,16 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
 
                     request.Debug("Copying file '{0}' to '{1}'", file, targetFile);
                     try {
-                        File.Copy(file, targetFile);
+                        if (File.Exists(file))
+                        {
+                            // if this is a file
+                            File.Copy(file, targetFile);
+                        }
+                        else if (Directory.Exists(file))
+                        {
+                            // if this is a directory, copy items over
+                            CopyDirectory(file, versionFolder);
+                        }
                     }
                     catch (Exception ex) {
                         request.Debug(ex.StackTrace);
@@ -599,9 +492,59 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
                         file.TryHardToDelete();
                     }
                 }
-            }
+            }            
 
             return false;
+        }
+
+        private void RemoveDirectory(string directoryFolder)
+        {
+            // remove all files
+            foreach (var fileToBeRemoved in Directory.EnumerateFiles(directoryFolder))
+            {
+                fileToBeRemoved.TryHardToDelete();
+            }
+
+            // remove all subdirectories
+            foreach (var folderToBeRemoved in Directory.EnumerateDirectories(directoryFolder))
+            {
+                RemoveDirectory(folderToBeRemoved);
+            }
+
+            try
+            {
+                // now try to remove the directory
+                Directory.Delete(directoryFolder);
+            }
+            catch { }
+        }
+
+        private void CopyDirectory(string sourceFolder, string destinationFolder)
+        {
+            // check that source and destination folders exist
+            if (!sourceFolder.DirectoryExists() || !destinationFolder.DirectoryExists())
+            {
+                return;
+            }
+
+            // copy the files over
+            foreach (var file in Directory.EnumerateFiles(sourceFolder))
+            {
+                File.Copy(file, Path.Combine(destinationFolder, Path.GetFileName(file)), true);
+            }
+
+            // copy the directories over
+            foreach (var directory in Directory.EnumerateDirectories(sourceFolder))
+            {
+                var destinationDirName = Path.Combine(destinationFolder, Path.GetFileName(directory));
+
+                if (!Directory.Exists(destinationDirName))
+                {
+                    Directory.CreateDirectory(destinationDirName);
+                }
+
+                CopyDirectory(directory, destinationDirName);
+            }
         }
 
         private void InstallPackageFromFile(string fastPath, BootstrapRequest request)

src/Microsoft.PackageManagement.CoreProviders/Bootstrap/BootstrapRequest.cs
@@ -29,6 +29,9 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
     using PackageManagement.Internal.Utility.Collections;
     using PackageManagement.Internal.Utility.Extensions;
     using ErrorCategory = PackageManagement.Internal.ErrorCategory;
+    using System.IO.Compression;
+    using File = System.IO.File;
+    using Directory = System.IO.Directory;
 
     public abstract class BootstrapRequest : Request {
         internal Uri[] _urls
@@ -202,6 +205,51 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
         }
 
         /// <summary>
+        /// Extract zipped package and return the unzipped folder
+        /// </summary>
+        /// <param name="zippedPackagePath"></param>
+        /// <returns></returns>
+        private string ExtractZipPackage(string zippedPackagePath)
+        {
+            if (zippedPackagePath != null && zippedPackagePath.FileExists())
+            {
+                // extracted folder
+                string extractedFolder = FilesystemExtensions.GenerateTemporaryFileOrDirectoryNameInTempDirectory();
+
+                try
+                {
+                    //unzip the file
+                    ZipFile.ExtractToDirectory(zippedPackagePath, extractedFolder);
+
+                    // extraction fails
+                    if (!Directory.Exists(extractedFolder))
+                    {
+                        Verbose(string.Format(CultureInfo.CurrentCulture, Resources.Messages.FailToExtract, zippedPackagePath, extractedFolder));
+                        return string.Empty;
+                    }
+
+                    // the zipped folder
+                    var zippedDirectory = Directory.EnumerateDirectories(extractedFolder).FirstOrDefault();
+
+                    if (!string.IsNullOrWhiteSpace(zippedDirectory) && Directory.Exists(zippedDirectory))
+                    {
+                        return zippedDirectory;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Verbose(string.Format(CultureInfo.CurrentCulture, Resources.Messages.FailToInstallZipFolder, zippedPackagePath, ex.Message));
+                    Debug(ex.StackTrace);
+
+                    // remove the extracted folder
+                    extractedFolder.TryHardToDelete();
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
         /// Helper function to retry downloading a file.
         /// downloadFileFunction is the main function that is used to download the file when given a uri
         /// numberOfTry is how many times we can try to download it
@@ -257,7 +305,22 @@ namespace Microsoft.PackageManagement.Providers.Internal.Bootstrap {
                     link.HRef);
 
                 // got a valid file!
-                if (file != null && file.FileExists()) {                    
+                if (file != null && file.FileExists()) {
+                    // if file is zip, unpack it and return the unpacked folder
+                    if (link.MediaType == Iso19770_2.MediaType.ZipPackage)
+                    {
+                        try
+                        {
+                            // let's extract the zipped file
+                            return ExtractZipPackage(file);
+                        }
+                        finally
+                        {
+                            // delete the zipped file
+                            file.TryHardToDelete();
+                        }
+                    }
+
                     return file;
                 }
             }

src/Microsoft.PackageManagement.CoreProviders/resources/Microsoft.PackageManagement.Providers.Resources.Messages.resx
@@ -121,6 +121,16 @@
     <value>Fail to extract and copy to '{0}'.</value>
     <comment>0 - The directory we are copying to</comment>
   </data>
+  <data name="FailToDeleteExistingFolder" xml:space="preserve">
+    <value>Cannot delete existing provider folder '{0}'.</value>
+  </data>
+  <data name="FailToExtract" xml:space="preserve">
+    <value>Cannot extract from '{0}' to '{1}'</value>
+  </data>
+  <data name="FailToInstallZipFolder" xml:space="preserve">
+    <value>Fail to extract zip package '{0}': '{1}'</value>
+    <comment>0 - package path, 1 - exception message</comment>
+  </data>
   <data name="FindingPackage" xml:space="preserve">
     <value>Finding the package '{0}'.</value>
     <comment>0 - package name</comment>

src/Microsoft.PackageManagement.NuGetProvider/Repository/LocalPackageRepository.cs
@@ -240,7 +240,17 @@
                 var partialManifestNameMatches = GetPackageFiles(partialManifestName).Where(
                     path => FileNameMatchesPattern(packageId, version, path));
 
-                return filesMatchingFullName.Concat(partialNameMatches).Concat(partialManifestNameMatches);
+                filesMatchingFullName = filesMatchingFullName.Concat(partialNameMatches).Concat(partialManifestNameMatches);
+            }
+
+            // cannot find matching files, we should try to search for just packageid.nupkg
+            if (filesMatchingFullName.Count() == 0)
+            {
+                // exclude version
+                var packageWithoutVersionName = FileUtility.MakePackageFileName(true, packageId, null, NuGetConstant.PackageExtension);
+                var packageWithoutVersionManifest = Path.ChangeExtension(packageWithoutVersionName, NuGetConstant.ManifestExtension);
+
+                return GetPackageFiles(packageWithoutVersionName).Concat(GetPackageFiles(packageWithoutVersionManifest));
             }
 
             return filesMatchingFullName;

src/Microsoft.PackageManagement.PackageSourceListProvider/PackageList/ExePackageInstaller.cs
@@ -44,10 +44,8 @@ namespace Microsoft.PackageManagement.PackageSourceListProvider
                 request.Verbose("Package: '{0}'", exePackage);
 
                 // validate the file
-                if (!WebDownloader.PerformSecurityScan(exePackage))
-                {
-                    //TODO Security Scan work
-                    request.WriteError(ErrorCategory.InvalidOperation, Constants.ProviderName, "The package download from '{0}' failed in the security scan.", package.Source);
+                if (!WebDownloader.VerifyHash(exePackage,package, request))
+                {                    
                     return false;
                 }
 

src/Microsoft.PackageManagement.PackageSourceListProvider/PackageList/PackageListParser.cs
@@ -180,6 +180,11 @@ namespace Microsoft.PackageManagement.PackageSourceListProvider
                             {
                                 item.Common = common;
                                 item.FilePath = packageSpecPath;
+
+                                if (string.IsNullOrWhiteSpace(item.Version))
+                                {
+                                    throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, Resources.Messages.VersionNotFound, item.Name));
+                                }
                             }
 
                             item.PackageSource = packageSource;
@@ -201,6 +206,11 @@ namespace Microsoft.PackageManagement.PackageSourceListProvider
                         else
                         {
                             convertedPackage.Common = new CommonDefinition();
+
+                            if (String.IsNullOrWhiteSpace(convertedPackage.Version))
+                            {
+                                throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, Resources.Messages.VersionNotFound, convertedPackage.Name, packageSpecPath));
+                            }
                         }
 
                         convertedPackage.FilePath = packageSpecPath;
@@ -249,6 +259,13 @@ namespace Microsoft.PackageManagement.PackageSourceListProvider
         public List<string> installationOption { get; set; }
     }
 
+    public class PackageHash
+    {
+        public string algorithm { get; set; }
+        public string hashCode { get; set; }
+
+    }
+
     public class CommonDefinition
     {
         public string Name { get; set; }
@@ -285,15 +302,19 @@ namespace Microsoft.PackageManagement.PackageSourceListProvider
         private string _destination;
         private List<Dependencies> _dependencies;
         private List<PackageJson> _dependencyObjects;
-        private OSRequirement _osRequirement;
+        private OSRequirement _osRequirement;        
         private string _type;
         private string _isTrustedSource;
         private bool _isPackageProvider;
         private string _installArguments;
         private string _unInstallAdditionalArguments;
+        private string _source;
 
         public CommonDefinition Common { get; set; }
         public string Version { get; set; }
+
+
+        public PackageHash Hash { get; set; }
 
         public SemanticVersion SemVer
         {
@@ -306,7 +327,36 @@ namespace Microsoft.PackageManagement.PackageSourceListProvider
                 return null;
             }
         }
-        public string Source { get; set; }
+        public string Source
+        {
+            get
+            {
+                return _source;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    Uri uri;
+
+                    // check whether source is http instead of https
+                    if (!Uri.TryCreate(value, UriKind.Absolute, out uri))
+                    {
+                        throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, Resources.Messages.UnsuportedUriFormat, Constants.ProviderName, value));
+                    }
+
+                    if (!uri.IsFile)
+                    {
+                        if (uri.Scheme != Uri.UriSchemeHttps)
+                        {
+                            throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, Resources.Messages.UriSchemeNotSupported, uri.Scheme, Uri.UriSchemeHttps));
+                        }
+                    }
+
+                    _source = value;
+                }
+            }
+        }
 
         public string Name
         {

commit 4c830d5e327b34c7bf8b92d3a67c4e2250c2edee

Add Trace-Commands back to Utility module
This fixes a regression introduced by 0dd425da1.

src/Modules/Windows+Unix-Core/Microsoft.PowerShell.Utility/Microsoft.PowerShell.Utility.psd1
@@ -20,6 +20,7 @@ CmdletsToExport= "Format-List", "Format-Custom", "Format-Table", "Format-Wide",
     "Clear-Variable", "Export-Clixml", "Import-Clixml", "ConvertTo-Xml", "Select-Xml", "Write-Debug",
     "Write-Verbose", "Write-Warning", "Write-Error", "Write-Information", "Write-Output", "Set-PSBreakpoint",
     "Get-PSBreakpoint", "Remove-PSBreakpoint", "Enable-PSBreakpoint", "Disable-PSBreakpoint", "Get-PSCallStack",
+    "Get-TraceSource", "Set-TraceSource", "Trace-Command",
     "Unblock-File", "Get-Runspace", "Debug-Runspace", "Enable-RunspaceDebug", "Disable-RunspaceDebug",
     "Get-RunspaceDebug", "Wait-Debugger" 
 FunctionsToExport= "Get-FileHash", "New-TemporaryFile", "New-Guid", "Format-Hex", "Import-PowerShellDataFile",