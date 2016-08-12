401b6194da2427cf4a85d854cd2d5b0e896ca8c1
Dongbo Wang <dongbow@microsoft.com>
2016-08-11 17:59:04 -0700

    Address review comments in test code

--- test/powershell/enginecore/ModulePath.Tests.ps1
+++ test/powershell/enginecore/ModulePath.Tests.ps1
@@ -1,32 +1,45 @@
-Describe "Module Path Basic Tests" -tags "CI" {
+Describe "SxS Module Path Basic Tests" -tags "CI" {
     
-    It "validate module path on windows" -Skip:(!$IsWindows) {
-        
-        $paths = $env:PSMODULEPATH -split [System.IO.Path]::PathSeparator
-        $paths.Count | Should Be 3
+    BeforeAll {
 
-        $expectedUserPath = Join-Path -Path $HOME -ChildPath 'Documents\PowerShell\Modules'
-        $paths[0] | Should Be $expectedUserPath
-
-        $expectedSharedPath = Join-Path -Path $env:ProgramFiles -ChildPath 'PowerShell\Modules'
-        $paths[1] | Should Be $expectedSharedPath
-
+        if ($IsWindows) 
+        {
+            $powershell = "$PSHOME\powershell.exe"
+            $ProductName = "WindowsPowerShell"
+            if ($IsCoreCLR -and ($PSHOME -notlike "*Windows\System32\WindowsPowerShell\v1.0"))
+            {
+                $ProductName =  "PowerShell"
+            }
+            $expectedUserPath = Join-Path -Path $HOME -ChildPath "Documents\$ProductName\Modules"
+            $expectedSharedPath = Join-Path -Path $env:ProgramFiles -ChildPath "$ProductName\Modules"
+        }
+        else
+        {
+            $powershell = "$PSHOME/powershell"
+            $expectedUserPath = [System.Management.Automation.Platform]::SelectProductNameForDirectory("USER_MODULES")
+            $expectedSharedPath = [System.Management.Automation.Platform]::SelectProductNameForDirectory("SHARED_MODULES")
+        }
         $expectedSystemPath = Join-Path -Path $PSHOME -ChildPath 'Modules'
-        $paths[2] | Should Be $expectedSystemPath
     }
 
-    It "validate module path on unix" -Skip:($IsWindows) {
-    
-        $paths = $env:PSMODULEPATH -split [System.IO.Path]::PathSeparator
+    BeforeEach {
+        $originalModulePath = $env:PSMODULEPATH
+    }
+
+    AfterEach {
+        $env:PSMODULEPATH = $originalModulePath
+    }
+
+    It "validate sxs module path" {
+
+        $env:PSMODULEPATH = ""
+        $defaultModulePath = & $powershell -nopro -c '$env:PSMODULEPATH'
+
+        $paths = $defaultModulePath -split [System.IO.Path]::PathSeparator
+
         $paths.Count | Should Be 3
-
-        $expectedUserPath = [System.Management.Automation.Platform]::SelectProductNameForDirectory("USER_MODULES")
         $paths[0] | Should Be $expectedUserPath
-
-        $expectedSharedPath = [System.Management.Automation.Platform]::SelectProductNameForDirectory("SHARED_MODULES")
         $paths[1] | Should Be $expectedSharedPath
-
-        $expectedSystemPath = Join-Path -Path $PSHOME -ChildPath 'Modules'
         $paths[2] | Should Be $expectedSystemPath
     }
 }
