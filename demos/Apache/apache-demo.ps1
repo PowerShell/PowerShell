081fcc8f21ce5d16259df0577bfef3658c4dd3b3
Kris Bash <krbash@microsoft.com>
2016-08-17 10:57:31 -0700

    Fix capitalization in apache-demo.ps1

--- demos/Apache/apache-demo.ps1
+++ demos/Apache/apache-demo.ps1
@@ -2,11 +2,11 @@ Import-Module $PSScriptRoot/Apache/Apache.psm1
 
 #list Apache Modules
 Write-Host -Foreground Blue "Get installed Apache Modules like *proxy* and Sort by name"
-Get-ApacheModule |Where {$_.ModuleName -like "*proxy*"}|Sort-Object ModuleName |out-host
+Get-ApacheModule |Where {$_.ModuleName -like "*proxy*"}|Sort-Object ModuleName | Out-Host
 
 #Graceful restart of Apache
 Write-host -Foreground Blue "Restart Apache Server gracefully"
-restart-ApacheHTTPServer -graceful |out-host
+Restart-ApacheHTTPServer -Graceful | Out-Host
 
 #Enumerate current virtual hosts (web sites)
 Write-Host -Foreground Blue "Enumerate configured Apache Virtual Hosts"
@@ -14,7 +14,7 @@ Get-ApacheVHost |out-host
 
 #Add a new virtual host
 Write-Host -Foreground Yellow "Create a new Apache Virtual Host"
-New-ApacheVHost -ServerName "mytestserver" -DocumentRoot /var/www/html/mystestserver -VirtualHostIPAddress * -VirtualHostPort 8090|out-host
+New-ApacheVHost -ServerName "mytestserver" -DocumentRoot /var/www/html/mystestserver -VirtualHostIPAddress * -VirtualHostPort 8090 | Out-Host
 
 #Enumerate new set of virtual hosts
 Write-Host -Foreground Blue "Enumerate Apache Virtual Hosts Again"
