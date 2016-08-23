bb774e24788116c78a094f8bb3d754fce5204b3a
probonopd <probonopd@users.noreply.github.com>
2016-08-24 00:21:46 +0200

    Copyright and license
    https://github.com/PowerShell/PowerShell/pull/2027#discussion_r75960505

--- tools/appimage.sh
+++ tools/appimage.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-# Generate AppImage, http://appimage.org
+# The MIT License (MIT)
+# Copyright (c) 2016 Simon Peter
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-# Known to run on
+# Generate AppImage, http://appimage.org
+#
+# The resulting PowerShell AppImage is known to run on
 # CentOS-7.0-1406-x86_64-GnomeLive.iso
 # CentOS-7-x86_64-LiveGNOME-1511.iso
 # Chromixium-1.5-amd64.iso
