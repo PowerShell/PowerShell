e7ca878bc4bac8c271aa5561c945d0960199960c
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-08-26 10:49:09 -0700

    Move AppImage generation to travis.sh
    As it works independently of New-UnixPackage,
    I don't think it should be run there.

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,8 +1,10 @@
-set -x
+set -ex
 ulimit -n 4096
 # Only build packages for branches, not pull requests
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     powershell -c "Import-Module ./build.psm1; Start-PSBootstrap -Package; Start-PSBuild -CrossGen; Start-PSPackage; Start-PSPester; Start-PSxUnit"
+    # Generate AppImage
+    ./tools/appimage.sh
 else
     powershell -c "Import-Module ./build.psm1; Start-PSBootstrap; Start-PSBuild -CrossGen; Start-PSPester; Start-PSxUnit"
 fi
