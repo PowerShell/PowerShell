574d9068a7f67cd4c265a9dafa85d02095f9f241
Yanbing Wang <yawang@microsoft.com>
2016-08-26 14:08:18 -0700

    add tests to language part 1

---
+++ test/powershell/Language/Scripting/CheckRestrictedlanguage.Tests.ps1
@@ -0,0 +1,112 @@
+﻿#  <Test>
+#    <summary>Test restricted language check method on scriptblocks</summary>
+#  </Test>
+Describe "Test restricted language check method on scriptblocks" -Tags "CI" {
+        BeforeAll {
+            set-strictmode -v 2
+            function list {
+
+            $l = new-object system.collections.generic.list[string]
+            $args | foreach {$l.Add($_)}
+            , $l
+            }
+        }
+        
+
+        It 'Check basic expressions' {
+
+            {{2+2}.CheckRestrictedLanguage($null, $null, $false) } | Should Not Throw  # Succeed with no variables
+        }
+        
+        It 'Check default variables' {
+
+            {{ $PSCulture, $PSUICulture, $true, $false, $null}.CheckRestrictedLanguage($null, $null, $false) } | Should Not Throw
+        }
+
+        It 'Check default variables' {
+            
+            try
+            {
+                {2+$a}.CheckRestrictedLanguage($null, $null, $false)
+                Throw "Execution OK"
+            }
+            catch
+            {
+                $_.FullyQualifiedErrorId | Should Be 'ParseException'
+            }
+            
+        }
+
+        It 'Check union of default + one allowed variables' {
+
+            { { 2 + $a }.CheckRestrictedLanguage($null, (list a), $false) }| Should Not Throw  # succeed
+        }
+
+        It 'Check union of default + two allowed variables' {
+
+            { { $a + $b }.CheckRestrictedLanguage($null, (list a b), $false) } | Should Not Throw  # succeed
+        }
+
+        It 'Check union of default + allowed variables' {
+
+            { { $PSCulture, $PSUICulture, $true, $false, $null,  $a, $b}.CheckRestrictedLanguage($null, (list a b), $false) }| Should Not Throw
+        }
+
+        It 'Check union of default + one disallowed variables' {
+
+            try
+            {
+                { $a + $b + $c }.CheckRestrictedLanguage($null, (list a b), $false)   # fail
+                Throw "Execution OK"
+            }
+            catch
+            {
+                $_.FullyQualifiedErrorId | Should Be 'ParseException'
+            }
+        }
+
+        It 'Check union of default + one allowed variable and but not allow evironment variable' {
+
+            try
+            {
+                { 2 + $a + $env:foo }.CheckRestrictedLanguage($null, (list a), $false)   # fail
+                Throw "Execution OK"
+            }
+            catch
+            {
+                $_.FullyQualifiedErrorId | Should Be 'ParseException'
+            }
+        }
+
+        It 'Check union of default + one allowed varialbe name and allow environment variable ' {
+                    
+            {{ 2 + $a + $env:foo }.CheckRestrictedLanguage($null, (list a), $true)}   | Should Not Throw # succeed
+        }
+
+        It 'Check that wildcard allows env even if the flag is set to false' {
+
+            { { 2 + $a + $b + $c + $env:foo }.CheckRestrictedLanguage($null, (list *), $false)} | Should Not Throw   # succeed
+        }
+
+        It 'Check for restricted commands' {
+        
+            try
+            {
+                {get-date}.CheckRestrictedLangauge($null, $null, $false)
+                Throw "Execution OK"
+            }
+            catch
+            {
+                $_.FullyQualifiedErrorId | Should Be 'MethodNotFound'
+            }
+        }
+
+        It 'Check for allowed commands and variables' {
+
+            { { get-process | where name -match $pattern | foreach $prop }.CheckRestrictedLanguage(
+                (list get-process where foreach),
+                (list prop pattern)
+                , $false) }| Should Not Throw
+        }
+
+}
