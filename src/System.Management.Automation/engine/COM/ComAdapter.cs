9fb40a2d15262a44405907d905aab5c28b7e03fe
Josh Soref <jsoref@users.noreply.github.com>
2016-08-28 06:13:41 +0000

    spelling: comments in src/System.Management.Automation/engine/COM

--- src/System.Management.Automation/engine/COM/ComAdapter.cs
+++ src/System.Management.Automation/engine/COM/ComAdapter.cs
@@ -170,7 +170,7 @@ namespace System.Management.Automation
         /// </summary>
         /// <param name="property">PSProperty coming from a previous call to DoGetProperty</param>
         /// <param name="setValue">value to set the property with</param>
-        ///  <param name="convertIfPossible">instructs the adapter to convert before setting, if the adapter suports conversion</param>
+        ///  <param name="convertIfPossible">instructs the adapter to convert before setting, if the adapter supports conversion</param>
         protected override void PropertySet(PSProperty property, object setValue, bool convertIfPossible)
         {
             ComProperty prop = (ComProperty)property.adapterData;
