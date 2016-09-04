2b3f79345f83c42a1b45cfd860dc364d33300883
Tim Curwick <MadWithPowerShell@gmail.com>
2016-09-04 10:42:58 -0500

    Fix ConvertTo-Html single matched column header bug
    Fix ConvertTo-Html single matched column header bug
    Add ConvertTo-Html pester tests for single matched column header bug

---
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Html.Tests.ps1
@@ -0,0 +1,17 @@
+﻿Describe "ConvertTo-Html test" -Tags "CI" {
+    $customPSObject = [pscustomobject]@{ "prop1" = "val1"; "prop2" = "val2" } 
+
+    It "Test ConvertTo-Html header with default single property" -Skip:( $IsLinux -or $IsOSX -or $IsCoreCLR ) {
+        $returnObject = $customPSObject | Select prop1 | ConvertTo-Html
+        $columnHeader = $returnObject.Where{ $_.StartsWith( '<tr><th>' ) }[0]
+        $expectedValue = '<tr><th>prop1</th></tr>'
+        $columnHeader | Should Be $expectedValue
+    }
+
+    It "Test ConvertTo-Html header with matched single property" -Skip:( $IsLinux -or $IsOSX -or $IsCoreCLR ) {
+        $returnObject = $customPSObject | Select prop1 | ConvertTo-Html -Property prop*
+        $columnHeader = $returnObject.Where{ $_.StartsWith( '<tr><th>' ) }[0]
+        $expectedValue = '<tr><th>prop1</th></tr>'
+        $columnHeader | Should Be $expectedValue
+    }
+}
