67fcc7809c0da35e907c8add21a1fe27242ad0dc
PaulHigin <paulhi@microsoft.com>
2016-09-12 13:10:16 -0700

    Removed unneeded using statement

--- src/Microsoft.PowerShell.PSReadLine/ReadLine.cs
+++ src/Microsoft.PowerShell.PSReadLine/ReadLine.cs
@@ -4,7 +4,6 @@ Copyright (c) Microsoft Corporation.  All rights reserved.
 
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
