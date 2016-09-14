fdc69f0c3f59d5d5ddf6951d7263075f7edbb784
Sergei Vorobev <sevoroby@microsoft.com>
2016-09-13 17:24:06 -0700

    Add 'RequireAdminOnWindows' tag to some tests suites

--- test/powershell/Modules/Microsoft.PowerShell.Management/Get-Process.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Get-Process.Tests.ps1
@@ -1,8 +1,10 @@
-Describe "Get-Process" -Tags "CI" {
+Describe "Get-Process for admin" -Tags @('CI', 'RequireAdminOnWindows') {
     It "Should support -IncludeUserName" {
         (Get-Process powershell -IncludeUserName | Select-Object -First 1).UserName | Should Match $env:USERNAME
     }
+}
 
+Describe "Get-Process" -Tags "CI" {
     # These tests are no good, please replace!
     It "Should return a type of Object[] for Get-Process cmdlet" -Pending:$IsOSX {
         (Get-Process).GetType().BaseType | Should Be 'array'
