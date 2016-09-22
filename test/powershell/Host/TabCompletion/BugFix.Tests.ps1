a9247f3ec655e02ecf422db62277325f0b2f9300
Dongbo Wang <dongbow@microsoft.com>
2016-09-21 22:51:32 -0700

    Fix binary operator tab completion

--- test/powershell/Host/TabCompletion/BugFix.Tests.ps1
+++ test/powershell/Host/TabCompletion/BugFix.Tests.ps1
@@ -1,9 +1,24 @@
-Describe "Issue#682 - [system.manage<tab>] should work" -Tags "CI" {
+Describe "Tab completion bug fix" -Tags "CI" {
     
-    It "[system.manage<tab>" {
+    It "Issue#682 - '[system.manage<tab>' should work" {
         $result = TabExpansion2 -inputScript "[system.manage" -cursorColumn "[system.manage".Length
         $result | Should Not BeNullOrEmpty
         $result.CompletionMatches.Count | Should Be 1
         $result.CompletionMatches[0].CompletionText | Should Be "System.Management"
+    }
+
+    It "Issue#1350 - '1 -sp<tab>' should work" {
+        $result = TabExpansion2 -inputScript "1 -sp" -cursorColumn "1 -sp".Length
+        $result | Should Not BeNullOrEmpty
+        $result.CompletionMatches.Count | Should Be 1
+        $result.CompletionMatches[0].CompletionText | Should Be "-split"
+    }
+
+    It "Issue#1350 - '1 -a<tab>' should work" {
+        $result = TabExpansion2 -inputScript "1 -a" -cursorColumn "1 -a".Length
+        $result | Should Not BeNullOrEmpty
+        $result.CompletionMatches.Count | Should Be 2
+        $result.CompletionMatches[0].CompletionText | Should Be "-and"
+        $result.CompletionMatches[1].CompletionText | Should Be "-as"
     }
 }
