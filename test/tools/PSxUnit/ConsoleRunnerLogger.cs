942e5e3bc6b909adea6e327c69ba577d8ebd8ae0
chunqingchen <cchen@microsoft.com>
2016-09-27 15:49:25 -0700

    add psXunit runner project

---
+++ test/tools/PSxUnit/ConsoleRunnerLogger.cs
@@ -0,0 +1,76 @@
+﻿#if NETCOREAPP1_0
+
+using System;
+
+namespace Xunit
+{
+    /// <summary>
+    /// An implementation of <see cref="IRunnerLogger"/> which logs messages
+    /// to <see cref="Console"/> and <see cref="Console.Error"/>.
+    /// </summary>
+    public class ConsoleRunnerLogger : IRunnerLogger
+    {
+        readonly bool useColors;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleRunnerLogger"/> class.
+        /// </summary>
+        /// <param name="useColors">A flag to indicate whether colors should be used when
+        /// logging messages.</param>
+        public ConsoleRunnerLogger(bool useColors)
+        {
+            this.useColors = useColors;
+        }
+
+        /// <inheritdoc/>
+        public object LockObject { get; } = new object();
+
+        /// <inheritdoc/>
+        public void LogError(StackFrameInfo stackFrame, string message)
+        {
+            using (SetColor(ConsoleColor.Red))
+                lock (LockObject)
+                    Console.Error.WriteLine(message);
+        }
+
+        /// <inheritdoc/>
+        public void LogImportantMessage(StackFrameInfo stackFrame, string message)
+        {
+            //using (SetColor(ConsoleColor.Gray))
+            //    lock (LockObject)
+            //        Console.WriteLine(message);
+        }
+
+        /// <inheritdoc/>
+        public void LogMessage(StackFrameInfo stackFrame, string message)
+        {
+            //using (SetColor(ConsoleColor.DarkGray))
+            //    lock (LockObject)
+            //        Console.WriteLine(message);
+        }
+
+        /// <inheritdoc/>
+        public void LogWarning(StackFrameInfo stackFrame, string message)
+        {
+            using (SetColor(ConsoleColor.Yellow))
+                lock (LockObject)
+                    Console.WriteLine(message);
+        }
+
+        IDisposable SetColor(ConsoleColor color)
+            => useColors ? new ColorRestorer(color) : null;
+
+        class ColorRestorer : IDisposable
+        {
+            public ColorRestorer(ConsoleColor color)
+            {
+                Console.ForegroundColor = color;
+            }
+
+            public void Dispose()
+                => Console.ResetColor();
+        }
+    }
+}
+
+#endif
---
+++ test/tools/PSxUnit/XunitProjectAssembly2.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace Xunit.Runner.DotNet
+{
+    class XunitProjectAssembly2 : XunitProjectAssembly
+    {
+        public TestAssemblyConfiguration ConfigurationStream
+            => LoadConfiguration(AssemblyFilename);
+
+        static TestAssemblyConfiguration LoadConfiguration(string assemblyName)
+        {
+            var stream = GetConfigurationStreamForAssembly(assemblyName);
+            return stream == null ? new TestAssemblyConfiguration() : ConfigReader.Load(stream);
+        }
+
+        static Stream GetConfigurationStreamForAssembly(string assemblyFilename)
+        {
+            // get parent directory
+            var directory = Path.GetDirectoryName(assemblyFilename);
+            var assemblyName = Path.GetFileNameWithoutExtension(assemblyFilename);
+
+            var pathWithAssemblyName = Path.Combine(directory, assemblyName);
+
+            // See if there's a directory with the assm name. this might be the case for appx
+            if (Directory.Exists(pathWithAssemblyName))
+            {
+                if (File.Exists(Path.Combine(pathWithAssemblyName, $"{assemblyName}.xunit.runner.json")))
+                    return File.OpenRead(Path.Combine(pathWithAssemblyName, $"{assemblyName}.xunit.runner.json"));
+
+                if (File.Exists(Path.Combine(pathWithAssemblyName, "xunit.runner.json")))
+                    return File.OpenRead(Path.Combine(pathWithAssemblyName, "xunit.runner.json"));
+            }
+
+            // Fallback to directory with assembly
+            if (File.Exists(Path.Combine(directory, $"{assemblyName}.xunit.runner.json")))
+                return File.OpenRead(Path.Combine(directory, $"{assemblyName}.xunit.runner.json"));
+
+            if (File.Exists(Path.Combine(directory, "xunit.runner.json")))
+                return File.OpenRead(Path.Combine(directory, "xunit.runner.json"));
+
+            return null;
+        }
+    }
+}
