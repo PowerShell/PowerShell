011d947fd539b05859dbbcf3be90669b8ebf0e49
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-10-05 16:41:59 -0700

    Clean up GetFileOwner

--- src/libpsl-native/src/getfileowner.cpp
+++ src/libpsl-native/src/getfileowner.cpp
@@ -6,6 +6,7 @@
 #include "getpwuid.h"
 #include "getfileowner.h"
 
+#include <assert.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -24,24 +25,15 @@
 //! char* is marshaled as an LPStr, which on Linux is UTF-8.
 //! @endparblock
 //!
-//! @exception errno Passes this error via errno to GetLastError:
-//! - ERROR_INVALID_PARAMETER: parameter is not valid
-//!
 //! @retval file owner, or NULL if unsuccessful
 //!
 char* GetFileOwner(const char* fileName)
 {
-    int32_t ret = 0;
+    assert(fileName);
     errno = 0;
 
-    if (!fileName)
-    {
-        errno = ERROR_INVALID_PARAMETER;
-        return NULL;
-    }
-
     struct stat buf;
-    ret = GetStat(fileName, &buf);
+    int32_t ret = GetStat(fileName, &buf);
     if (ret != 0)
     {
         return NULL;
