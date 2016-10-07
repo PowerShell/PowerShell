64e692299ffba3952f2e3fdc60f513cfbf5fba16
iSazonov <darpa@yandex.ru>
2016-10-07 06:06:48 +0300

    Correct typo

--- test/powershell/Modules/Microsoft.PowerShell.Diagnostics/Get-WinEvent.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Diagnostics/Get-WinEvent.Tests.ps1
@@ -108,7 +108,7 @@ Describe 'Get-WinEvent' -Tags "CI" {
             # this relies on apriori knowledge about the log file
             # the provided log file has been edited to remove MS PII, so we must use -ea silentlycontinue
             $eventLogFile = [io.path]::Combine($PSScriptRoot, "assets", "Saved-Events.evtx")
-            $filter = @{ path = "$eventLogFile"; ; PackageAware="Not package aware"; DriverName = "Remote Desktop Easy Print", "Microsoft enhanced Point and Print compatibility driver" } 
+            $filter = @{ path = "$eventLogFile"; PackageAware="Not package aware"; DriverName = "Remote Desktop Easy Print", "Microsoft enhanced Point and Print compatibility driver" } 
             $results = Get-WinEvent -filterHashtable $filter -ea silentlycontinue
             @($results).Count | Should be 2
             ($results.RecordId -contains 9) | should be $true
