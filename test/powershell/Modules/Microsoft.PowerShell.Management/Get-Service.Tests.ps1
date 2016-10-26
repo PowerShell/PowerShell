d6648a602aabfd5b113a7753133a22adecb48360
Hemant Mahawar <hemantm@microsoft.com>
2016-10-26 14:21:19 -0700

    Updated tests to be more precise

--- test/powershell/Modules/Microsoft.PowerShell.Management/Get-Service.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Get-Service.Tests.ps1
@@ -1,18 +1,35 @@
-# Service cmdlet is currently working on windows builds only
-if ($IsWindows)
-{
-  Describe "Get-Service cmdlet tests" -Tags "CI" {
-    It "should throw if null is passed to -Name parameter" {
-      {Get-Service -Name $null} | Should Throw
+Describe "Get-Service cmdlet tests" -Tags "CI" {
+  # Service cmdlet is currently working on windows only
+  # So skip the tests on non-Windows
+  BeforeAll {
+    $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+    if ( -not $IsWindows ) {
+        $PSDefaultParameterValues["it:skip"] = $true
     }
-    It "should throw if empty string is passed to -Name parameter" {
-      {Get-Service -Name [String]::Empty} | Should Throw
+  }
+  # Restore the defaults
+  AfterAll {
+      $global:PSDefaultParameterValues = $originalDefaultParameterValues
+  }
+  Context 'Check null or empty value to the -Name parameter' {
+    It 'Should throw if null is passed to -Name parameter' {
+      try {Get-Service -Name $null }
+      catch [System.Management.Automation.ParameterBindingException] {$_.Exception.ErrorId | Should Be 'ParameterArgumentValidationError'}
     }
-    It "should throw if null is passed through pipeline to -Name parameter" {
-      {$null | Get-Service} | Should Throw
+    It 'Should throw if empty string is passed to -Name parameter' {
+      try {Get-Service -Name [String]::Empty }
+      catch [System.Management.Automation.ParameterBindingException] {$_.Exception.ErrorId | Should Be 'ParameterArgumentValidationError'}
     }
-    It "should throw if empty string is passed through pipeline to -Name parameter" {
-      {[String]::Empty | Get-Service} | Should Throw
+  }
+  Context 'Check null or empty value to the -Name parameter via pipeline' {
+    It 'Should throw if null is passed through pipeline to -Name parameter' {
+      try {$null | Get-Service -ErrorAction Stop}
+      catch [System.Management.Automation.ParameterBindingException] {$_.Exception.ErrorId | Should Be 'ParameterArgumentValidationError'}
     }
-  } 
+    It 'Should throw if empty string is passed through pipeline to -Name parameter' {
+      try {[String]::Empty | Get-Service -ErrorAction Stop}
+      catch [System.Management.Automation.ParameterBindingException] {$_.Exception.ErrorId | Should Be 'ParameterArgumentValidationError'}
+
+    }
+  }
 }
