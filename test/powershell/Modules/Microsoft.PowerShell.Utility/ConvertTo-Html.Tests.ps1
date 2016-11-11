8aa1772e72730ad4732e7968147d65e1d598e81a
Tim Curwick <MadWithPowerShell@gmail.com>
2016-11-11 11:55:44 -0800

    Replaced aliases in ConvertTo-HTML.tests.ps1

--- test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Html.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Html.Tests.ps1
@@ -2,14 +2,14 @@
     $customPSObject = [pscustomobject]@{ "prop1" = "val1"; "prop2" = "val2" } 
 
     It "Test ConvertTo-Html header with default single property" -Skip:( $IsLinux -or $IsOSX -or $IsCoreCLR ) {
-        $returnObject = $customPSObject | Select prop1 | ConvertTo-Html
+        $returnObject = $customPSObject | Select-Object prop1 | ConvertTo-Html
         $columnHeader = $returnObject.Where{ $_.StartsWith( '<tr><th>' ) }[0]
         $expectedValue = '<tr><th>prop1</th></tr>'
         $columnHeader | Should Be $expectedValue
     }
 
     It "Test ConvertTo-Html header with matched single property" -Skip:( $IsLinux -or $IsOSX -or $IsCoreCLR ) {
-        $returnObject = $customPSObject | Select prop1 | ConvertTo-Html -Property prop*
+        $returnObject = $customPSObject | Select-Object prop1 | ConvertTo-Html -Property prop*
         $columnHeader = $returnObject.Where{ $_.StartsWith( '<tr><th>' ) }[0]
         $expectedValue = '<tr><th>prop1</th></tr>'
         $columnHeader | Should Be $expectedValue
