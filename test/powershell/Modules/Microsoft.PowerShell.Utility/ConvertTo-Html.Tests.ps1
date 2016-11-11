6b1ecd4f9bc579525adb865035d6989443add1fa
Tim Curwick <MadWithPowerShell@gmail.com>
2016-11-11 12:35:37 -0800

    Resolved conflicts

--- test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Html.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Html.Tests.ps1
@@ -1,17 +1,153 @@
-﻿Describe "ConvertTo-Html test" -Tags "CI" {
-    $customPSObject = [pscustomobject]@{ "prop1" = "val1"; "prop2" = "val2" } 
+Describe "ConvertTo-Html Tests" -Tags "CI" {
 
-    It "Test ConvertTo-Html header with default single property" -Skip:( $IsLinux -or $IsOSX -or $IsCoreCLR ) {
-        $returnObject = $customPSObject | Select-Object prop1 | ConvertTo-Html
-        $columnHeader = $returnObject.Where{ $_.StartsWith( '<tr><th>' ) }[0]
-        $expectedValue = '<tr><th>prop1</th></tr>'
-        $columnHeader | Should Be $expectedValue
+    BeforeAll {
+        $customObject = [pscustomobject]@{"Name" = "John Doe"; "Age" = 42; "Friends" = ("Jack", "Jill")}
+        $newLine = "`r`n"
     }
 
-    It "Test ConvertTo-Html header with matched single property" -Skip:( $IsLinux -or $IsOSX -or $IsCoreCLR ) {
-        $returnObject = $customPSObject | Select-Object prop1 | ConvertTo-Html -Property prop*
-        $columnHeader = $returnObject.Where{ $_.StartsWith( '<tr><th>' ) }[0]
-        $expectedValue = '<tr><th>prop1</th></tr>'
-        $columnHeader | Should Be $expectedValue
+    function normalizeLineEnds([string]$text)
+    {
+        $text -replace "`r`n?|`n", "`r`n"
+    }
+
+    It "Test ConvertTo-Html with no parameters" {
+        $returnObject = $customObject | ConvertTo-Html
+        $returnObject -is [System.Array] | Should Be $true
+        $returnString = $returnObject -join $newLine
+        $expectedValue = normalizeLineEnds @"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>HTML TABLE</title>
+</head><body>
+<table>
+<colgroup><col/><col/><col/></colgroup>
+<tr><th>Name</th><th>Age</th><th>Friends</th></tr>
+<tr><td>John Doe</td><td>42</td><td>System.Object[]</td></tr>
+</table>
+</body></html>
+"@
+        $returnString | Should Be $expectedValue
+    }
+
+    It "Test ConvertTo-Html Fragment parameter" {
+        $returnString = ($customObject | ConvertTo-Html -Fragment) -join $newLine
+        $expectedValue = normalizeLineEnds @"
+<table>
+<colgroup><col/><col/><col/></colgroup>
+<tr><th>Name</th><th>Age</th><th>Friends</th></tr>
+<tr><td>John Doe</td><td>42</td><td>System.Object[]</td></tr>
+</table>
+"@
+        $returnString | Should Be $expectedValue
+    }
+
+    It "Test ConvertTo-Html as List" {
+        $returnString = ($customObject | ConvertTo-Html -As List) -join $newLine
+        $expectedValue = normalizeLineEnds @"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>HTML TABLE</title>
+</head><body>
+<table>
+<tr><td>Name:</td><td>John Doe</td></tr>
+<tr><td>Age:</td><td>42</td></tr>
+<tr><td>Friends:</td><td>System.Object[]</td></tr>
+</table>
+</body></html>
+"@
+        $returnString | Should Be $expectedValue
+    }
+
+    It "Test ConvertTo-Html specified properties" {
+        $returnString = ($customObject | ConvertTo-Html -Property Name, Friends -As List) -join $newLine
+        $expectedValue = normalizeLineEnds @"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>HTML TABLE</title>
+</head><body>
+<table>
+<tr><td>Name:</td><td>John Doe</td></tr>
+<tr><td>Friends:</td><td>System.Object[]</td></tr>
+</table>
+</body></html>
+"@
+        $returnString | Should Be $expectedValue
+    }
+
+    It "Test ConvertTo-Html using page parameters" {
+        $returnString = ($customObject | ConvertTo-Html -Title "Custom Object" -Body "Body Text" -CssUri "page.css" -As List) -join $newLine
+        $expectedValue = normalizeLineEnds @"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Custom Object</title>
+<link rel="stylesheet" type="text/css" href="page.css" />
+</head><body>
+Body Text
+<table>
+<tr><td>Name:</td><td>John Doe</td></tr>
+<tr><td>Age:</td><td>42</td></tr>
+<tr><td>Friends:</td><td>System.Object[]</td></tr>
+</table>
+</body></html>
+"@
+        $returnString | Should Be $expectedValue
+    }
+
+    It "Test ConvertTo-Html pre and post" {
+        $returnString = ($customObject | ConvertTo-Html -PreContent "Before the object" -PostContent "After the object") -join $newLine
+        $expectedValue = normalizeLineEnds @"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>HTML TABLE</title>
+</head><body>
+Before the object
+<table>
+<colgroup><col/><col/><col/></colgroup>
+<tr><th>Name</th><th>Age</th><th>Friends</th></tr>
+<tr><td>John Doe</td><td>42</td><td>System.Object[]</td></tr>
+</table>
+After the object
+</body></html>
+"@
+        $returnString | Should Be $expectedValue
+    }
+    It "Test ConvertTo-Html header with default single property" {
+        $returnString = ($customObject | Select-Object Name | ConvertTo-Html) -join $newLine
+        $expectedValue = normalizeLineEnds @"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>HTML TABLE</title>
+</head><body>
+<table>
+<colgroup><col/></colgroup>
+<tr><th>Name</th></tr>
+<tr><td>John Doe</td></tr>
+</table>
+</body></html>
+"@
+        $returnString | Should Be $expectedValue
+    }
+    It "Test ConvertTo-Html header with matched single property" {
+        $returnString = ($customObject | ConvertTo-Html -Property Name*) -join $newLine
+        $expectedValue = normalizeLineEnds @"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>HTML TABLE</title>
+</head><body>
+<table>
+<colgroup><col/></colgroup>
+<tr><th>Name</th></tr>
+<tr><td>John Doe</td></tr>
+</table>
+</body></html>
+"@
+        $returnString | Should Be $expectedValue
     }
 }
