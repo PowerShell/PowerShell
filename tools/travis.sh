ca0be9ad6103c1ef0b48b52ae0a85666f7bf2332
James Truher [MSFT] <jimtru@microsoft.com>
2016-11-17 11:59:55 -0800

    Jameswtruher/dailytravis (#2696)
    * Add daily build code for travis
    
    A cron job needs to be created to start the build, and the variable
    TRAVIS_DAILY_BUILD must be set to true
    
    * Change environment variable to the one suggested by the travis-ci documentation
    
    TRAVIS_EVENT_TYPE == "cron"

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,7 +1,10 @@
 set -x
 ulimit -n 4096
+# do this for our daily build test run
+if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
+    powershell -c "Import-Module ./build.psm1; Start-PSBootstrap; Start-PSBuild -CrossGen; Start-PSPester -Tag @('CI','Feature','Scenario') -ExcludeTag RequireAdminOnWindows; Start-PSxUnit"
 # Only build packages for branches, not pull requests
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+elif [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     powershell -c "Import-Module ./build.psm1; Start-PSBootstrap -Package; Start-PSBuild -CrossGen; Start-PSPackage; Start-PSPester -ThrowOnFailure; Test-PSPesterResults; Start-PSxUnit"
 else
     powershell -c "Import-Module ./build.psm1; Start-PSBootstrap; Start-PSBuild -CrossGen; Start-PSPester -ThrowOnFailure; Start-PSxUnit"
