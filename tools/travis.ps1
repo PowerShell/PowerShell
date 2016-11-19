961a76197b2574c8112aad6aa3a26add3c74dde8
Sergei Vorobev <sevoroby@microsoft.com>
2016-11-18 16:17:59 -0800

    Replace travis.sh by travis.ps1
    Fix problem with running Start-PSPester from the wrong directory

---
+++ tools/travis.ps1
@@ -0,0 +1,28 @@
+Import-Module $PSScriptRoot/../build.psm1 -Force
+
+$isPR = $env:TRAVIS_PULL_REQUEST -eq 'true'
+$isCron = $env:TRAVIS_EVENT_TYPE -eq 'cron'
+
+Start-PSBootstrap -Package:(-not $isPr)
+$output = Split-Path -Parent (Get-PSOutput -Options (New-PSOptions -Publish))
+Start-PSBuild -CrossGen
+
+$pesterParam = @{ 'binDir' = $output }
+
+if ($cron) {
+    # daily builds
+    $pesterParam['Tag'] = @('CI','Feature','Scenario')
+    $pesterParam['ExcludeTag'] = @()
+} else {
+    $pesterParam['Tag'] = @('CI')
+    $pesterParam['ThrowOnFailure'] = $true
+}
+
+Start-PSPester @pesterParam
+if (-not $isPr) {
+    # Only build packages for branches, not pull requests
+    Start-PSPackage
+    Test-PSPesterResults
+}
+
+Start-PSxUnit
