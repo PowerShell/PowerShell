4bbde92bb455c9cb93bffbdf083d28b0af4a79ce
Staffan Gustafsson <info@powercode-consulting.se>
2016-11-19 02:25:21 +0100

    Make Get-Process <id> work as well as Get-Process <name>
    At the end of RetrieveMatchingProcessesByProcessName,
    a check is added to see if the pattern is a positive integer.
    In that case an attempt is made to match it to a process id.

--- src/Microsoft.PowerShell.Commands.Management/commands/management/Process.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/Process.cs
@@ -15,6 +15,7 @@ using System.Management.Automation;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Principal;
@@ -201,17 +202,29 @@ namespace Microsoft.PowerShell.Commands
                     found = true;
                     AddIdempotent(process);
                 }
+
                 if (!found &&
                     !WildcardPattern.ContainsWildcardCharacters(pattern))
                 {
-                    WriteNonTerminatingError(
-                        pattern,
-                        0,
-                        pattern,
-                        null,
-                        ProcessResources.NoProcessFoundForGivenName,
-                        "NoProcessFoundForGivenName",
-                        ErrorCategory.ObjectNotFound);
+                    int processIdCandidate;
+                    if (int.TryParse(pattern, out processIdCandidate) && processIdCandidate > 0)
+                    {
+                        var process = AllProcesses.FirstOrDefault(p => p.Id == processIdCandidate);
+                        if (process != null)
+                        {
+                            AddIdempotent(process);
+                        }
+                    }
+                    else {
+                        WriteNonTerminatingError(
+                            pattern,
+                            0,
+                            pattern,
+                            null,
+                            ProcessResources.NoProcessFoundForGivenName,
+                            "NoProcessFoundForGivenName",
+                            ErrorCategory.ObjectNotFound);
+                    }
                 }
             }
         } // MatchingProcessesByProcessName
