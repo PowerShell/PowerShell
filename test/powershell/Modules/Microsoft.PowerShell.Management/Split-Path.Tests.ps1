731b68f6a6c7ce2baf32d764cc6e99013c9a7409
dchristian3188 <dchritsian3188@gmail.com>
2016-11-30 19:55:30 -0800

    Updated Tests

--- test/powershell/Modules/Microsoft.PowerShell.Management/Split-Path.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Split-Path.Tests.ps1
@@ -69,4 +69,53 @@ Describe "Split-Path" -Tags "CI" {
 	Split-Path -Parent "/usr/local/bin"  | Should Be "${dirSep}usr${dirSep}local"
 	Split-Path -Parent "usr/local/bin"   | Should Be "usr${dirSep}local"
     }
+
+    Context "SplitPath Tests (Windows Only)" {
+
+        BeforeAll {
+            $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+            if ( $IsWindows ) {
+            
+                $multilevelPath = 'C:\Temp\folder1'
+                $singlelevelPath = 'C:\Temp'
+                $multilevelUNC = '\\server1\share1\folder'
+                $UNCRoot = '\\server1\share1'
+
+                $splitPathParentCases = @(
+                    @{TestName='Multilevel'; Path = $multiLevelPath; Result = 'C:\temp'}
+                    @{TestName='Single Level'; Path = $singlelevelPath; Result = 'C:\'}
+                    @{TestName='Multilevel UNC'; Path = $multilevelUNC; Result = '\\server1\share1'}
+                    @{TestName='UNC Root'; Path = $UNCRoot; Result = '\\server1'}
+                )
+
+                $splitPathChildCases = @(
+                    @{TestName='Multilevel'; Path = $multiLevelPath; Result = 'folder1'}
+                    @{TestName='Single Level'; Path = $singlelevelPath; Result = 'temp'}
+                    @{TestName='Multilevel UNC'; Path = $multilevelUNC; Result = 'folder'}
+                    @{TestName='UNC Root'; Path = $UNCRoot; Result = 'share1'}
+                )
+
+            }
+            else{
+                $PSDefaultParameterValues["it:skip"] = $true
+            }
+        }
+        AfterAll {
+            $global:PSDefaultParameterValues = $originalDefaultParameterValues
+        }
+    
+        It 'Splits Path on a <TestName> Path and gets the parent' -TestCases $splitPathParentCases -test {
+            param($TestName,$Path,$Result)
+                Split-Path -Path $Path -Parent | Should be $Result
+        }
+
+        It 'Splits Path on a <TestName> Path and gets the child' -TestCases $splitPathChildCases -test {
+            param($TestName,$Path,$Result)
+                Split-Path -Path $Path -Leaf | Should be $Result
+        }
+    
+        It 'Does not split a drive leter'{
+            Split-Path -Path 'C:\' | Should be ''
+        }
+    } 
 }
