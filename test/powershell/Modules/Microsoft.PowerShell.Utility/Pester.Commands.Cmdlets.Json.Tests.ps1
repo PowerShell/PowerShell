979048798b66a32a83a5516d60c106ab28125e5e
kittholland <kittholland@gmail.com>
2016-11-30 16:46:57 -0800

    Moved tests and removed extraneous file.
    Moved pretty/compressed json tests from standalone file into the
    existing ConvertTo-Json test file.

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Pester.Commands.Cmdlets.Json.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Pester.Commands.Cmdlets.Json.Tests.ps1
@@ -1352,6 +1352,23 @@ Describe "Validate Json serialization" -Tags "CI" {
             $actual | Should Be $expectedNoWhiteSpace
         }
     }
+
+
+    Context "Validate Json output is either Pretty or Compressed" {
+
+        It "Should indent by two spaces" {
+            (@{ a = 1 } | ConvertTo-Json)[6] | Should Be 'a'
+            (1, 2 | ConvertTo-Json)[5] | Should Be '1'
+        }
+
+        It "Should have one space after colons" {
+            (@{ a = 1 } | ConvertTo-Json)[10] | Should Be '1'
+        }
+        
+        It "Should minify Json with Compress switch" {
+            (@{ a = 1 } | ConvertTo-Json -Compress).Length | Should Be 7
+        }
+    }
 }
 
 Describe "Json Bug fixes"  -Tags "Feature" {
