563a3117f257bbbbef0ed11d84f438d99c4ba382
Christopher Nelson <nadiasvertex@gmail.com>
2016-12-01 19:27:20 -0500

    build: Update build.sh to run fully manual build
    Also, add ubuntu-16.10-x64 to the list of architectures to build for.w

--- build.sh
+++ build.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
+set -e
 
 if hash powershell 2>/dev/null; then
     echo 'Continuing with `powershell -noprofile -c Start-PSBuild`'
     powershell -noprofile -c "Import-Module ./build.psm1; Start-PSBuild"
 else
-    echo 'No `powershell`, see docs/building/linux.md or macos.md to build PowerShell!'
+   echo 'Continuing with full manual build'
+   dotnet restore
+   pushd src/ResGen
+   dotnet run
+   popd
+
+   pushd src/TypeCatalogParser
+   dotnet run
+   popd
+
+   pushd src/TypeCatalogGen
+   dotnet run ../Microsoft.PowerShell.CoreCLR.AssemblyLoadContext/CorePsTypeCatalog.cs powershell.inc
+   popd
+
+   pushd src/libpsl-native
+   cmake -DCMAKE_BUILD_TYPE=Debug .
+   make -j
+   make test
+   popd
 fi
