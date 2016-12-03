258932cb2e55aa6989c14777c7c9554c7b090205
Christopher Nelson <nadiasvertex@gmail.com>
2016-12-03 10:28:17 -0500

    build: Move from-scratch build into a separate script, add final part of build

--- build.sh
+++ build.sh
@@ -6,22 +6,5 @@ if hash powershell 2>/dev/null; then
     powershell -noprofile -c "Import-Module ./build.psm1; Start-PSBuild"
 else
    echo 'Continuing with full manual build'
-   dotnet restore
-   pushd src/ResGen
-   dotnet run
-   popd
-
-   pushd src/TypeCatalogParser
-   dotnet run
-   popd
-
-   pushd src/TypeCatalogGen
-   dotnet run ../Microsoft.PowerShell.CoreCLR.AssemblyLoadContext/CorePsTypeCatalog.cs powershell.inc
-   popd
-
-   pushd src/libpsl-native
-   cmake -DCMAKE_BUILD_TYPE=Debug .
-   make -j
-   make test
-   popd
+   ./build-from-scratch.sh
 fi
