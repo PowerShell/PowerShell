939360f5ef63570d6536df40834de4c00ca5083a
Jason Shirk <jason@truewheels.net>
2016-12-09 11:55:03 -0800

    Fix spelling

--- test/powershell/Language/Scripting/Trap.Tests.ps1
+++ test/powershell/Language/Scripting/Trap.Tests.ps1
@@ -8,21 +8,21 @@ Describe "Test trap" -Tags "CI" {
         }
 
         It "Line after exception should NOT be continued and both inner and outter traps should be triggered" {
-            $a = . {trap {"outter trap"; continue;}; . {trap {"inner trap"; break;}; "hello"; throw "exception"; "world"}}
+            $a = . {trap {"outer trap"; continue;}; . {trap {"inner trap"; break;}; "hello"; throw "exception"; "world"}}
             $a.Length | Should Be 3
-            $a -join "," | Should Be "hello,inner trap,outter trap"
+            $a -join "," | Should Be "hello,inner trap,outer trap"
         }
 
         It "Line after exception should be invoked after continue" {
-            $a = . {trap {"outter trap"; continue;} "hello"; throw "exception"; "world"}
+            $a = . {trap {"outer trap"; continue;} "hello"; throw "exception"; "world"}
             $a.Length | Should Be 3
-            $a -join "," | Should Be "hello,outter trap,world"
+            $a -join "," | Should Be "hello,outer trap,world"
         }
 
         It "Line after exception should NOT be invoked and inner trap should not be triggered" {
-            $a = . {trap {"outter trap"; continue;}; . {trap [system.Argumentexception] {"inner trap"; continue;}; "hello"; throw "exception"; "world"}}
+            $a = . {trap {"outer trap"; continue;}; . {trap [system.Argumentexception] {"inner trap"; continue;}; "hello"; throw "exception"; "world"}}
             $a.Length | Should Be 2
-            $a -join "," | Should Be "hello,outter trap"
+            $a -join "," | Should Be "hello,outer trap"
         }
     }
 }
