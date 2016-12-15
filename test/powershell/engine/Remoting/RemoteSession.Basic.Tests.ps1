f443bb80eb688fc08ad915d3130d8efcdb8df727
Robert Holt <rjmholt@gmail.com>
2016-12-14 18:54:45 -0800

    Added a condition to skip New-PSSession test in Start-PSPester on Linux, since currently unsupported

--- test/powershell/engine/Remoting/RemoteSession.Basic.Tests.ps1
+++ test/powershell/engine/Remoting/RemoteSession.Basic.Tests.ps1
@@ -1,11 +1,14 @@
-
 Describe "New-PSSession basic test" -Tag @("CI") {
+    if ( ! $IsWindows ) {
+        $PSDefaultParameterValues['it:skip'] = $true
+    }
     It "New-PSSession should not crash powershell" {
         try {
-            New-PSSession -ComputerName nonexistcomputer -Authentication Basic
-            throw "New-PSSession should throw"
+    	New-PSSession -ComputerName nonexistcomputer -Authentication Basic
+    	throw "New-PSSession should throw"
         } catch {
-            $_.FullyQualifiedErrorId | Should Be "InvalidOperation,Microsoft.PowerShell.Commands.NewPSSessionCommand"
+    	$_.FullyQualifiedErrorId | Should Be "InvalidOperation,Microsoft.PowerShell.Commands.NewPSSessionCommand"
         }
     }
+    $PSDefaultParameterValues.remove('it:skip')
 }
