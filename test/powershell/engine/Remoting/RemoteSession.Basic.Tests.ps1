3f274aad9ce6d30f709cfc79dcebc7c943093316
Chunqing Chen <cchen@microsoft.com>
2017-03-15 22:06:07 -0700

    make transcripts include the configuration name in the transcript header (#2890)
    * PowerShell transcripts should include the configuration name in the transcript header
    * adding test case for PowerShell transcripts should include the configuration name in the transcript header #2890

--- test/powershell/engine/Remoting/RemoteSession.Basic.Tests.ps1
+++ test/powershell/engine/Remoting/RemoteSession.Basic.Tests.ps1
@@ -9,3 +9,26 @@ Describe "New-PSSession basic test" -Tag @("CI") {
         }
     }
 }
+
+Describe "JEA session Transcprit script test" -Tag @("Feature", 'RequireAdminOnWindows') {
+    It "Configuration name should be in the transcript header" {
+        [string] $RoleCapDirectory = (New-Item -Path "$TestDrive\RoleCapability" -ItemType Directory -Force).FullName
+        [string] $PSSessionConfigFile = "$RoleCapDirectory\TestConfig.pssc"
+        [string] $transScriptFile = "$RoleCapDirectory\*.txt"
+        try
+        {
+            New-PSSessionConfigurationFile -Path $PSSessionConfigFile -TranscriptDirectory $RoleCapDirectory -SessionType RestrictedRemoteServer
+            Register-PSSessionConfiguration -Name JEA -Path $PSSessionConfigFile -Force -ErrorAction SilentlyContinue 
+            $scriptBlock = {Enter-PSSession -ComputerName Localhost -ConfigurationName JEA; Exit-PSSession}
+            & $scriptBlock
+            $headerFile = Get-ChildItem $transScriptFile | Sort-Object LastWriteTime | Select-Object -Last 1
+            $header = Get-Content $headerFile | Out-String
+            $header | Should BeLike "Configuration Name: JEA"
+        }
+        finally
+        {
+            Unregister-PSSessionConfiguration -Name JEA -Force -ErrorAction SilentlyContinue
+        }
+    }
+
+}
