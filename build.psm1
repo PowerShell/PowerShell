0424231f1d248de0bd7c1313cc729f6726a9d05e
Travis Plunk <travis.plunk@microsoft.com>
2017-03-24 19:00:00 -0700

    Update start-pspester to run common tests

--- build.psm1
+++ build.psm1
@@ -708,7 +708,7 @@ function Start-PSPester {
         [string]$OutputFile = "pester-tests.xml",
         [string[]]$ExcludeTag = 'Slow',
         [string[]]$Tag = "CI",
-        [string]$Path = "$PSScriptRoot/test/powershell",
+        [string[]]$Path = @("$PSScriptRoot/test/common","$PSScriptRoot/test/powershell"),
         [switch]$ThrowOnFailure,
         [switch]$FullCLR,
         [string]$binDir = (Split-Path (New-PSOptions -FullCLR:$FullCLR).Output),
@@ -790,7 +790,7 @@ function Start-PSPester {
         $Command += "-PassThru "
     }
 
-    $Command += "'" + $Path + "'"
+    $Command += "'" + ($Path -join "','") + "'"
     if ($Unelevate)
     {
         $Command += " *> $outputBufferFilePath; '__UNELEVATED_TESTS_THE_END__' >> $outputBufferFilePath"
