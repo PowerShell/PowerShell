456cb939e38a3f68e88baf4426c2e8ca33cde7a5
iSazonov <darpa@yandex.ru>
2017-03-31 08:28:22 +0300

    Remove comment and use another StringBuilder constructor

--- src/System.Management.Automation/engine/InitialSessionState.cs
+++ src/System.Management.Automation/engine/InitialSessionState.cs
@@ -3937,8 +3937,8 @@ namespace System.Management.Automation.Runspaces
 
             if (errors.Count > 0)
             {
-                // Note: don't use '\n' ('char' convert implicitly to 'ushort' and 'int' and initialize 'StringBuilder' size)
-                var allErrors = new StringBuilder("\n");
+                var allErrors = new StringBuilder();
+                allErrors.Append('\n');
                 foreach (string error in errors)
                 {
                     if (!string.IsNullOrEmpty(error))
