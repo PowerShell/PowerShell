b7cb06f2181e7f87c580ea1115b43b6df8f3a9b2
iSazonov <darpa@yandex.ru>
2017-04-12 10:34:27 +0300

    Add tests and remove unneeded 'using'

--- src/System.Management.Automation/engine/PSVersionInfo.cs
+++ src/System.Management.Automation/engine/PSVersionInfo.cs
@@ -5,8 +5,6 @@ Copyright (c) Microsoft Corporation.  All rights reserved.
 using System.Diagnostics;
 using System.Reflection;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Globalization;
 using System.Management.Automation.Internal;
 using Microsoft.Win32;
