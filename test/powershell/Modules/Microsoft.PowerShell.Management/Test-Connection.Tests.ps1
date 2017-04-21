16d6c934d0b7efe442ba4ee20ac1f8830a30434b
dchristian3188 <dchristian3188@gmail.com>
2017-04-20 21:11:55 -0700

    Fixed Capitilization Removed Slow Tests

--- test/powershell/Modules/Microsoft.PowerShell.Management/Test-Connection.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Test-Connection.Tests.ps1
@@ -23,35 +23,17 @@ Describe "Test-Connection" -Tags "CI" {
     }
     It "Gets the right IP Address" {
         $localHost = Test-Connection -ComputerName localhost -Count 1
-        $localHost.IPV4Address | should be '127.0.0.1'
-        $localhost.IPV6Address | should be '::1'
+        $localHost.IPV4Address | Should Be '127.0.0.1'
+        $localhost.IPV6Address | Should Be '::1'
     }
     
     It "The count parameter counting to <count>" -TestCases $countCases {
         param($count)
-        (Test-Connection -ComputerName localhost -Count $count).Count | should be $count
+        (Test-Connection -ComputerName localhost -Count $count).Count | Should Be $count
     }
         
     It "The quiet parameter on a <message> computer" -TestCases $quietTests {
         param($computername, $result)
-        Test-Connection -ComputerName $computername -Count 1 -Quiet | should be $result
-    }
-}
-
-Describe  "Test-Connection Slow Tests" -Tags  @('CI', 'Slow') {
-    BeforeAll {
-        $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
-        if ( ! $IsWindows )
-        {
-            $PSDefaultParameterValues["it:skip"] = $true
-        }
-    }
-    AfterAll {
-        $global:PSDefaultParameterValues = $originalDefaultParameterValues
-    }
-    It "Uses the timeout parameter" {
-        (Measure-Command {
-                Test-Connection -Computer _bad_computer_name_ -Timeout 10000 -ea silentlycontinue
-            }).TotalSeconds | should BeGreaterThan 10.0
+        Test-Connection -ComputerName $computername -Count 1 -Quiet | Should Be $result
     }
 }
