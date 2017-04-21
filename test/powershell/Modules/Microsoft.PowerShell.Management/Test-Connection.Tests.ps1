cb8984dc29a54fce3302f994f45e56cda8fbf00c
dchristian3188 <dchristian3188@gmail.com>
2017-04-20 18:04:47 -0700

    Updated Pester Tests

--- test/powershell/Modules/Microsoft.PowerShell.Management/Test-Connection.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Test-Connection.Tests.ps1
@@ -1,37 +1,57 @@
 Describe "Test-Connection" -Tags "CI" {
-    if ( -not($IsWindows)) {
-         $PSDefaultParameterValues["it:skip"] = $true
+    BeforeAll {
+        $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+        if ( ! $IsWindows )
+        {
+            $PSDefaultParameterValues["it:skip"] = $true
+        }
+        else
+        {
+            $countCases = @(
+                @{count = 2}
+                @{count = 3}
+            )
+
+            $quietTests = @(
+                @{computerName = 'localhost'; message = 'online' ; result = $true}
+                @{computerName = '_fake_computer_namex'; message = 'offline' ; result = $false}
+            )
+        }
     }
-    Context "Querying localhost" {
-        it "Gets the right IP Address" {
-            $localHost = Test-Connection -ComputerName localhost -Count 1
-            $localHost.IPV4Address | should be '127.0.0.1'
-            $localhost.IPV6Address | should be '::1'
-        }
-
-        $countCases = @(
-            @{count = 2}
-            @{count = 3}
-        )
-        it "Respects the count parameter counting to <count>" -TestCases $countCases {
-            param($count)
-            (Test-Connection -ComputerName localhost -Count $count).Count | should be $count
-        }
+    AfterAll {
+        $global:PSDefaultParameterValues = $originalDefaultParameterValues
+    }
+    It "Gets the right IP Address" {
+        $localHost = Test-Connection -ComputerName localhost -Count 1
+        $localHost.IPV4Address | should be '127.0.0.1'
+        $localhost.IPV6Address | should be '::1'
+    }
+    
+    It "The count parameter counting to <count>" -TestCases $countCases {
+        param($count)
+        (Test-Connection -ComputerName localhost -Count $count).Count | should be $count
+    }
         
-        $quietTests = @(
-            @{computerName = 'localhost'; message = 'good' ; result = $true}
-            @{computerName = '_fake_computer_namex'; message = 'bad' ; result = $false}
-        )
-        it "Respects the quiet parameter on a <message> computer" -TestCases $quietTests {
-            param($computername,$result)
-            Test-Connection -ComputerName $computername -Count 1 -Quiet | should be $result
-        }
+    It "The quiet parameter on a <message> computer" -TestCases $quietTests {
+        param($computername, $result)
+        Test-Connection -ComputerName $computername -Count 1 -Quiet | should be $result
+    }
+}
 
-        it "Respects the timeout parameter" {
-            $start = [datetime]::now
-            Test-Connection -Computer _bad_computer_name_ -Timeout 10000 -ea silentlycontinue
-            $end = [datetime]::now
-            ($end - $start).TotalSeconds | should BeGreaterThan 10.0
+Describe  "Test-Connection Slow Tests" -Tags "Slow" {
+    BeforeAll {
+        $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+        if ( ! $IsWindows )
+        {
+            $PSDefaultParameterValues["it:skip"] = $true
         }
+    }
+    AfterAll {
+        $global:PSDefaultParameterValues = $originalDefaultParameterValues
+    }
+    It "Uses the timeout parameter" {
+        (Measure-Command {
+                Test-Connection -Computer _bad_computer_name_ -Timeout 10000 -ea silentlycontinue
+            }).TotalSeconds | should BeGreaterThan 10.0
     }
 }
