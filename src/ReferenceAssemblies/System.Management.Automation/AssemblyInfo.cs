1c472f5aa4d005c1c47d3098415c18c5a1a09198
Travis Plunk <travis.plunk@microsoft.com>
2017-05-09 14:59:00 -0700

    Add Assembly info for reference assembly

---
+++ src/ReferenceAssemblies/System.Management.Automation/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyFileVersionAttribute("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: ReferenceAssembly()]
+
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
