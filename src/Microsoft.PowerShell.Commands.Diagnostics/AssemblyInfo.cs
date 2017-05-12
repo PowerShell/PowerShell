bbf4d01b95dfcc2b57a0ad178691c2a7e840667e
iSazonov <darpa@yandex.ru>
2017-05-12 19:34:32 +0300

    Update assemblies versions by MSBuild

--- src/Microsoft.PowerShell.Commands.Diagnostics/AssemblyInfo.cs
+++ src/Microsoft.PowerShell.Commands.Diagnostics/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyFileVersionAttribute("3.0.0.0")]
-[assembly: AssemblyVersion("3.0.0.0")]
-
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
--- src/Microsoft.WSMan.Management/AssemblyInfo.cs
+++ src/Microsoft.WSMan.Management/AssemblyInfo.cs
@@ -1,4 +1,1 @@
 using System.Reflection;
-
-[assembly:AssemblyFileVersionAttribute("3.0.0.0")]
-[assembly:AssemblyVersion("3.0.0.0")]
