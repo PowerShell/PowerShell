7d6a94820108ecea17e751060625371813572434
Paul Higinbotham <paulhi@microsoft.com>
2017-05-25 14:00:00 -0700

    Initial changes

--- src/System.Management.Automation/CoreCLR/CorePsPlatform.cs
+++ src/System.Management.Automation/CoreCLR/CorePsPlatform.cs
@@ -602,7 +602,101 @@ namespace System.Management.Automation
             return IsOSX ? Unix.NativeMethods.GetPPid(pid) : Unix.GetProcFSParentPid(pid);
         }
 
+        #region CreateProcess
 
+        internal static unsafe void CreateProcess(
+            string filename, string[] argv, string[] envp, string cwp,
+            bool redirectStdin, bool redirectStdout, bool redirectStderr, int creationFlags,
+            out int lpChildPid, out int stdinFd, out int stdoutFd, out int stderrFd)
+        {
+#if UNIX
+            byte** argvPtr = null, envpPtr = null;
+            try
+            {
+                AllocNullTerminatedArray(argv, ref argvPtr);
+                AllocNullTerminatedArray(envp, ref envpPtr);
+                int result = ForkAndExecProcess(
+                    filename, argvPtr, envpPtr, cwd,
+                    redirectStdin ? 1 : 0, redirectStdout ? 1 : 0, redirectStderr ? 1 : 0, createNewProcessGroup ? 1 : 0,
+                    out lpChildPid, out stdinFd, out stdoutFd, out stderrFd);
+                if (result != 0)
+                {
+                    // Normally we'd simply make this method return the result of the native
+                    // call and allow the caller to use GetLastWin32Error.  However, we need
+                    // to free the native arrays after calling the function, and doing so
+                    // stomps on the runtime's captured last error.  So we need to access the
+                    // error here, and without SetLastWin32Error available, we can't propagate
+                    // the error to the caller via the normal GetLastWin32Error mechanism.  We could
+                    // return 0 on success or the GetLastWin32Error value on failure, but that's
+                    // technically ambiguous, in the case of a failure with a 0 errno.  Simplest
+                    // solution then is just to throw here the same exception the Process caller
+                    // would have.  This can be revisited if we ever have another call site.
+                    throw new Win32Exception();
+                }
+            }
+            finally
+            {
+                FreeArray(envpPtr, envp.Length);
+                FreeArray(argvPtr, argv.Length);
+            }
+#else
+            throw new PSNotSupportedException();
+#endif
+        }
+
+#if UNIX
+        private static unsafe void AllocNullTerminatedArray(string[] arr, ref byte** arrPtr)
+        {
+            int arrLength = arr.Length + 1; // +1 is for null termination
+
+            // Allocate the unmanaged array to hold each string pointer.
+            // It needs to have an extra element to null terminate the array.
+            arrPtr = (byte**)Marshal.AllocHGlobal(sizeof(IntPtr) * arrLength);
+            Debug.Assert(arrPtr != null);
+
+            // Zero the memory so that if any of the individual string allocations fails,
+            // we can loop through the array to free any that succeeded.
+            // The last element will remain null.
+            for (int i = 0; i < arrLength; i++)
+            {
+                arrPtr[i] = null;
+            }
+
+            // Now copy each string to unmanaged memory referenced from the array.
+            // We need the data to be an unmanaged, null-terminated array of UTF8-encoded bytes.
+            for (int i = 0; i < arr.Length; i++)
+            {
+                byte[] byteArr = Encoding.UTF8.GetBytes(arr[i]);
+
+                arrPtr[i] = (byte*)Marshal.AllocHGlobal(byteArr.Length + 1); //+1 for null termination
+                Debug.Assert(arrPtr[i] != null);
+
+                Marshal.Copy(byteArr, 0, (IntPtr)arrPtr[i], byteArr.Length); // copy over the data from the managed byte array
+                arrPtr[i][byteArr.Length] = (byte)'\0'; // null terminate
+            }
+        }
+
+        private static unsafe void FreeArray(byte** arr, int length)
+        {
+            if (arr != null)
+            {
+                // Free each element of the array
+                for (int i = 0; i < length; i++)
+                {
+                    if (arr[i] != null)
+                    {
+                        Marshal.FreeHGlobal((IntPtr)arr[i]);
+                        arr[i] = null;
+                    }
+                }
+
+                // And then the array itself
+                Marshal.FreeHGlobal((IntPtr)arr);
+            }
+        }
+#endif
+
+#endregion
 
         // Unix specific implementations of required functionality
         //
@@ -799,6 +893,12 @@ namespace System.Management.Automation
                 [return: MarshalAs(UnmanagedType.I1)]
                 internal static extern bool IsSameFileSystemItem([MarshalAs(UnmanagedType.LPStr)]string filePathOne,
                                                                  [MarshalAs(UnmanagedType.LPStr)]string filePathTwo);
+
+                [DllImport(psLib, CharSet = CharSet.Ansi, SetLastError = true)]
+                internal static extern unsafe int ForkAndExecProcess(
+                    string filename, byte** argv, byte** envp, string cwd,
+                    int redirectStdin, int redirectStdout, int redirectStderr, int creationFlags,
+                    out int lpChildPid, out int stdinFd, out int stdoutFd, out int stderrFd);
             }
         }
     }
---
+++ src/libpsl-native/src/createprocess.cpp
@@ -0,0 +1,193 @@
+#include "createprocess.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <limits>
+#include <limits.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <syslog.h>
+#include <unistd.h>
+
+enum
+{
+    CREATE_NEW_PROCESS_SESSION = 0x00000001
+};
+
+enum
+{
+    READ_END_OF_PIPE = 0,
+    WRITE_END_OF_PIPE = 1
+};
+
+static void CloseIfOpen(int fd)
+{
+    if (fd >= 0)
+    {
+        close(fd); // Ignoring errors from close is a deliberate choice
+    }
+}
+
+// Checks if the IO operation was interupted and needs to be retried.
+// Returns true if the operation was interupted; otherwise, false.
+template <typename TInt>
+static inline bool CheckInterrupted(TInt result)
+{
+    return result < 0 && errno == EINTR;
+}
+
+static int Dup2WithInterruptedRetry(int oldfd, int newfd)
+{
+    int result;
+    while (CheckInterrupted(result = dup2(oldfd, newfd)));
+    return result;
+}
+
+int32_t ForkAndExecProcess(
+    const char* filename,
+    char* const argv[],
+    char* const envp[],
+    const char* cwd,
+    int32_t redirectStdin,
+    int32_t redirectStdout,
+    int32_t redirectStderr,
+    int32_t creationFlags,
+    int32_t* childPid,
+    int32_t* stdinFd,
+    int32_t* stdoutFd,
+    int32_t* stderrFd)
+{
+    int success = true;
+    int processId = -1;
+    int stdinFds = { -1, -1 };
+    int stdoutFds[2] = { -1, -1 };
+    int stderrFds[2] = { -1, -1 };
+
+    // Validate arguments
+    if (nullptr == filename || nullptr == argv || nullptr == envp || nullptr == stdinFd || nullptr == stdoutFd ||
+        nullptr == stderrFd || nullptr == childPid)
+    {
+        assert(false && "null argument.");
+        errno = EINVAL;
+        success = false;
+        goto done;
+    }
+
+    if ((redirectStdin & ~1) != 0 || (redirectStdout & ~1) != 0 || (redirectStderr & ~1) != 0)
+    {
+        assert(false && "Boolean redirect* inputs must be 0 or 1.");
+        errno = EINVAL;
+        success = false;
+        goto done;
+    }
+
+    // Make sure we can find and access the executable. exec will do this, of course, but at that point it's already
+    // in the child process, at which point it'll translate to the child process' exit code rather than to failing
+    // the Start itself.  There's a race condition here, in that this could change prior to exec's checks, but there's
+    // little we can do about that. There are also more rigorous checks exec does, such as validating the executable
+    // format of the target; such errors will emerge via the child process' exit code.
+    if (access(filename, X_OK) != 0)
+    {
+        success = false;
+        goto done;
+    }
+
+    // Open pipes for any requests to redirect stdin/stdout/stderr
+    if ((redirectStdin && pipe(stdinFds) != 0) || (redirectStdout && pipe(stdoutFds) != 0) ||
+        (redirectStderr && pipe(stderrFds) != 0))
+    {
+        success = false;
+        goto done;
+    }
+
+    // Fork the child process
+    if ((processId = fork()) == -1)
+    {
+        success = false;
+        goto done;
+    }
+
+    if (processId == 0) // processId == 0 if this is child process
+    {
+        // Close the child's copy of the parent end of any open pipes
+        CloseIfOpen(stdinFds[WRITE_END_OF_PIPE]);
+        CloseIfOpen(stdoutFds[READ_END_OF_PIPE]);
+        CloseIfOpen(stderrFds[READ_END_OF_PIPE]);
+
+        // For any redirections that should happen, dup the pipe descriptors onto stdin/out/err.
+        // Then close out the old pipe descriptors, which we no longer need.
+        if ((redirectStdin && Dup2WithInterruptedRetry(stdinFds[READ_END_OF_PIPE], STDIN_FILENO) == -1) ||
+            (redirectStdout && Dup2WithInterruptedRetry(stdoutFds[WRITE_END_OF_PIPE], STDOUT_FILENO) == -1) ||
+            (redirectStderr && Dup2WithInterruptedRetry(stderrFds[WRITE_END_OF_PIPE], STDERR_FILENO) == -1))
+        {
+            _exit(errno != 0 ? errno : EXIT_FAILURE);
+        }
+        CloseIfOpen(stdinFds[READ_END_OF_PIPE]);
+        CloseIfOpen(stdoutFds[WRITE_END_OF_PIPE]);
+        CloseIfOpen(stderrFds[WRITE_END_OF_PIPE]);
+
+        // Change to the designated working directory, if one was specified
+        if (nullptr != cwd)
+        {
+            int result;
+            while (CheckInterrupted(result = chdir(cwd)));
+            if (result == -1)
+            {
+                _exit(errno != 0 ? errno : EXIT_FAILURE);
+            }
+        }
+
+        // If CREATE_NEW_PROCESS_SESSION was chosen then set the child process to be in its own
+        // session.  The processId, processGroupId, and sessionId will all be the same.
+        // Note that this creates an "orphaned" process.
+        if (creationFlags & CREATE_NEW_PROCESS_SESSION)
+        {
+            int result;
+            while (CheckInterrupted(result = setsid()));
+            if (result == -1)
+            {
+                _exit(errno != 0 ? errno : EXIT_FAILURE);
+            }
+        }
+
+        // Finally, execute the new process.  execve will not return if it's successful.
+        execve(filename, argv, envp);
+        _exit(errno != 0 ? errno : EXIT_FAILURE); // execve failed
+    }
+
+    // This is the parent process. processId == pid of the child
+    *childPid = processId;
+    *stdinFd = stdinFds[WRITE_END_OF_PIPE];
+    *stdoutFd = stdoutFds[READ_END_OF_PIPE];
+    *stderrFd = stderrFds[READ_END_OF_PIPE];
+
+done:
+    int priorErrno = errno;
+
+    // Regardless of success or failure, close the parent's copy of the child's end of
+    // any opened pipes.  The parent doesn't need them anymore.
+    CloseIfOpen(stdinFds[READ_END_OF_PIPE]);
+    CloseIfOpen(stdoutFds[WRITE_END_OF_PIPE]);
+    CloseIfOpen(stderrFds[WRITE_END_OF_PIPE]);
+
+    // If we failed, close everything else and give back error values in all out arguments.
+    if (!success)
+    {
+        CloseIfOpen(stdinFds[WRITE_END_OF_PIPE]);
+        CloseIfOpen(stdoutFds[READ_END_OF_PIPE]);
+        CloseIfOpen(stderrFds[READ_END_OF_PIPE]);
+
+        *stdinFd = -1;
+        *stdoutFd = -1;
+        *stderrFd = -1;
+        *childPid = -1;
+
+        errno = priorErrno;
+        return -1;
+    }
+
+    return 0;
+}
