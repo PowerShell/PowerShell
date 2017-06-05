992fe42829fa6bdc445a28ff81bcd23ec9286683
Jeff Bienstadt <v-jebien@microsoft.com>
2017-06-05 15:34:29 -0700

    Changes per code review.

--- test/powershell/Modules/Microsoft.PowerShell.Management/Move-Item.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Move-Item.Tests.ps1
@@ -32,69 +32,70 @@ Describe "Move-Item tests" -Tag "CI" {
             $booContent = "boo content"
         }
         BeforeEach {
-            New-Item -ItemType Directory -Path $filterPath
-            New-Item -ItemType Directory -Path $moveToPath
-            New-Item -ItemType File -Path $fooPath -Value $fooContent
-            New-Item -ItemType File -Path $barPath -Value $barContent
-            New-Item -ItemType File -Path $booPath -Value $booContent
+            New-Item -ItemType Directory -Path $filterPath | Out-Null
+            New-Item -ItemType Directory -Path $moveToPath | Out-Null
+            New-Item -ItemType File -Path $fooPath -Value $fooContent | Out-Null
+            New-Item -ItemType File -Path $barPath -Value $barContent | Out-Null
+            New-Item -ItemType File -Path $booPath -Value $booContent | Out-Null
         }
         AfterEach {
             Remove-Item $filterPath -Recurse -Force -ErrorAction SilentlyContinue
             Remove-Item $moveToPath -Recurse -Force -ErrorAction SilentlyContinue
         }
         It "Can move to different directory, filtered with -Include" {
-            Move-Item -Path $filePath -Destination $moveToPath -Include "bar*"
-            $? | Should Be $true
-            Test-Path -Path $barPath | Should Be $false
-            Test-Path -Path $newBarPath | Should Be $true
-            Test-Path -Path $booPath | Should Be $true
-            Test-Path -Path $fooPath | Should Be $true
+            Move-Item -Path $filePath -Destination $moveToPath -Include "bar*" -ErrorVariable e -ErrorAction SilentlyContinue
+            $e | Should BeNullOrEmpty
+            #Test-Path -Path $barPath | Should Be $false
+            $barPath | Should Not Exist
+            $newBarPath | Should Exist
+            $booPath | Should Exist
+            $fooPath | Should Exist
             $newBarPath | Should ContainExactly $barContent
         }
         It "Can move to different directory, filtered with -Exclude" {
-            Move-Item -Path $filePath -Destination $moveToPath -Exclude "b*"
-            $? | Should Be $true
-            Test-Path -Path $fooPath | Should Be $false
-            Test-Path -Path $newFooPath | Should Be $true
-            Test-Path -Path $booPath | Should Be $true
-            Test-Path -Path $barPath | Should Be $true
+            Move-Item -Path $filePath -Destination $moveToPath -Exclude "b*" -ErrorVariable e -ErrorAction SilentlyContinue
+            $e | Should BeNullOrEmpty
+            $fooPath | Should Not Exist
+            $newFooPath | Should Exist
+            $booPath | Should Exist
+            $barPath | Should Exist
             $newFooPath | Should ContainExactly $fooContent
         }
         It "Can move to different directory, filtered with -Filter" {
-            Move-Item -Path $filePath -Destination $moveToPath -Filter "bo*"
-            $? | Should Be $true
-            Test-Path -Path $booPath | Should Be $false
-            Test-Path -Path $newBooPath | Should Be $true
-            Test-Path -Path $barPath | Should Be $true
-            Test-Path -Path $fooPath | Should Be $true
+            Move-Item -Path $filePath -Destination $moveToPath -Filter "bo*" -ErrorVariable e -ErrorAction SilentlyContinue
+            $e | Should BeNullOrEmpty
+            $booPath | Should Not Exist
+            $newBooPath | Should Exist
+            $barPath | Should Exist
+            $fooPath | Should Exist
             $newBooPath | Should ContainExactly $booContent
         }
 
         It "Can rename via move, filtered with -Include" {
-            Move-Item -Path $filePath -Destination $renameToPath -Include "bar*"
-            $? | Should Be $true
-            Test-Path -Path $renameToPath | Should Be $true
-            Test-Path -Path $barPath | Should Be $false
-            Test-Path -Path $booPath | Should Be $true
-            Test-Path -Path $fooPath | Should Be $true
+            Move-Item -Path $filePath -Destination $renameToPath -Include "bar*" -ErrorVariable e -ErrorAction SilentlyContinue
+            $e | Should BeNullOrEmpty
+            $renameToPath | Should Exist
+            $barPath | Should Not Exist
+            $booPath | Should Exist
+            $fooPath | Should Exist
             $renameToPath | Should ContainExactly $barContent
         }
         It "Can rename via move, filtered with -Exclude" {
-            Move-Item -Path $filePath -Destination $renameToPath -Exclude "b*"
-            $? | Should Be $true
-            Test-Path -Path $renameToPath | Should Be $true
-            Test-Path -Path $fooPath | Should Be $false
-            Test-Path -Path $booPath | Should Be $true
-            Test-Path -Path $barPath | Should Be $true
+            Move-Item -Path $filePath -Destination $renameToPath -Exclude "b*" -ErrorVariable e -ErrorAction SilentlyContinue
+            $e | Should BeNullOrEmpty
+            $renameToPath | Should Exist
+            $fooPath | Should Not Exist
+            $booPath | Should Exist
+            $barPath | Should Exist
             $renameToPath | Should ContainExactly $fooContent
         }
         It "Can rename via move, filtered with -Filter" {
-            Move-Item -Path $filePath -Destination $renameToPath -Filter "bo*"
-            $? | Should Be $true
-            Test-Path -Path $renameToPath | Should Be $true
-            Test-Path -Path $booPath | Should Be $false
-            Test-Path -Path $fooPath | Should Be $true
-            Test-Path -Path $barPath | Should Be $true
+            Move-Item -Path $filePath -Destination $renameToPath -Filter "bo*" -ErrorVariable e -ErrorAction SilentlyContinue
+            $e | Should BeNullOrEmpty
+            $renameToPath | Should Exist
+            $booPath | Should Not Exist
+            $fooPath | Should Exist
+            $barPath | Should Exist
             $renameToPath | Should ContainExactly $booContent
         }
     }
