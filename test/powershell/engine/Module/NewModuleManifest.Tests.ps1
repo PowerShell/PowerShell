85cde4cca69f50dbc025484195c3cca92239e395
Andrew <anmenaga@microsoft.com>
2017-06-06 12:51:01 -0700

    Addressed PR feedback

--- test/powershell/engine/Module/NewModuleManifest.Tests.ps1
+++ test/powershell/engine/Module/NewModuleManifest.Tests.ps1
@@ -20,10 +20,24 @@ Describe "New-ModuleManifest tests" -tags "CI" {
         $module.PrivateData.PSData.ProjectUri | Should BeExactly $absoluteUri
     }
 
-    It "Verify module manifest encoding" {
+    function TestNewModuleManifestEncoding {
+        param ([byte[]]$expected)
         New-ModuleManifest -Path $testModulePath
-        # verify first line of the manifest - 3 bytes - '#' '\r' '\n' - in UTF-8 no BOM this should be @(35,13,10)
-        Get-Content -Encoding Byte -Path $testModulePath -TotalCount 3 | Should Be @(35,13,10)
+        (Get-Content -Encoding Byte -Path $testModulePath -TotalCount $expected.Length) -join ',' | Should Be ($expected -join ',')
+    }
+
+    It "Verify module manifest encoding on Windows " -Skip:(-not $IsWindows) {
+        
+        # verify first line of the manifest - 3 characters - '#' '\r' '\n'
+        # On Windows platforms - in UTF-16 with BOM - this should be @(255,254,35,0,13,0,10,0)
+        TestNewModuleManifestEncoding -expected @(255,254,35,0,13,0,10,0)
+    }
+
+    It "Verify module manifest encoding on non-Windows " -Skip:($IsWindows) {
+        
+        # verify first line of the manifest - 3 characters - '#' '\r' '\n'
+        # On non-Windows platforms - in UTF-8 no BOM - this should be @(35,13,10)
+        TestNewModuleManifestEncoding -expected @(35,13,10)
     }
 
     It "Relative URIs are not allowed" {
