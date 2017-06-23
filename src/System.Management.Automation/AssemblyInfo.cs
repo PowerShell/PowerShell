f1ff798c737e474b37b0c1262c35e23990a20e7a
Mike Richmond <mirichmo@microsoft.com>
2017-06-23 09:46:03 -0700

    Revert "Set assembly versions by MSBuild (#3917)"
    This reverts commit c549925f67db1cec8f1c4d3130f02ab9dd1d684d.

--- src/System.Management.Automation/AssemblyInfo.cs
+++ src/System.Management.Automation/AssemblyInfo.cs
@@ -2,6 +2,9 @@ using System.Runtime.CompilerServices;
 using System.Reflection;
 using System.Resources;
 
+[assembly: AssemblyFileVersionAttribute("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
