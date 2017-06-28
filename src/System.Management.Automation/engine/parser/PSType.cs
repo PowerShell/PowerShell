56c72fb4605f3bb0805c336eae22e90a50807e4f
Tim Curwick <MadWithPowerShell@gmail.com>
2017-06-28 14:26:04 -0500

    Added comma to replaced characters in assemblyname

--- src/System.Management.Automation/engine/parser/PSType.cs
+++ src/System.Management.Automation/engine/parser/PSType.cs
@@ -1113,12 +1113,13 @@ namespace System.Management.Automation.Language
             var definedTypes = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
             // First character is a special mark that allows us to cheaply ignore dynamic generated assemblies in ClrFacede.GetAssemblies()
-            // Two replaces at the end are for not-allowed characters. They are replaced by similar-looking chars.
+            // The replaces at the end are for not-allowed characters. They are replaced by similar-looking chars.
             string assemblyName = ClrFacade.FIRST_CHAR_PSASSEMBLY_MARK + (string.IsNullOrWhiteSpace(rootAst.Extent.File)
                                       ? "powershell"
                                       : rootAst.Extent.File
                                       .Replace('\\', (char)0x29f9)
                                       .Replace('/', (char)0x29f9)
+                                      .Replace(',', (char)0x201a)
                                       .Replace(':', (char)0x0589));
 
             var assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(assemblyName),
