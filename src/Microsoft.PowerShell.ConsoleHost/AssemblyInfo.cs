a8e3d8968fd1b74b133ecd00cb576f57cc2a054b
Ilya <darpa@yandex.ru>
2017-06-29 21:25:49 +0400

    Refactor MSBuild project files by adding the common property file 'PowerShell.Common.props' (#4106)
    `AssemblyVersion` and `FileVersion` are now inferred from the `Version` property, which is inferred by `VersionPrefix`. So now both `AssemblyVersion` and `FileVersion` are 6.0.0.0 for each of PowerShell assemblies, and the `ProductVersion` and `InformationalVersion` are 6.0.0.

--- src/Microsoft.PowerShell.ConsoleHost/AssemblyInfo.cs
+++ src/Microsoft.PowerShell.ConsoleHost/AssemblyInfo.cs
@@ -8,10 +8,7 @@ using System.Resources;
 
 [assembly: InternalsVisibleTo("powershell-tests,PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")]
 
-#if CORECLR
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en-US")]
-#else
+#if !CORECLR
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyInformationalVersionAttribute(@"10.0.10011.16384")]
 [assembly: ReliabilityContractAttribute(Consistency.MayCorruptAppDomain, Cer.MayFail)]
@@ -23,13 +20,3 @@ using System.Resources;
 #endif
 
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
-[assembly: System.Reflection.AssemblyVersion("3.0.0.0")]
-[assembly: System.Reflection.AssemblyProduct("Microsoft (R) Windows (R) Operating System")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright (c) Microsoft Corporation. All rights reserved.")]
-[assembly: System.Reflection.AssemblyCompany("Microsoft Corporation")]
-
-internal static class AssemblyStrings
-{
-    internal const string AssemblyVersion = @"3.0.0.0";
-    internal const string AssemblyCopyright = "Copyright (C) 2006 Microsoft Corporation. All rights reserved.";
-}
