bdbe68d9803e720272e9b38f4f74b2a5467f6396
Dongbo Wang <dongbow@microsoft.com>
2017-07-19 12:19:58 -0700

    Update tests again

--- test/powershell/Language/Scripting/Array.Tests.ps1
+++ test/powershell/Language/Scripting/Array.Tests.ps1
@@ -1,41 +1,54 @@
 Describe "ArrayExpression Tests" -Tags "CI" {
-    It "@([object[]](1,2,3)) should return an array of object[]" {
+    It "@([object[]](1,2,3)) should return a 3-element array of object[]" {
         $result = @([object[]](1,2,3))
         $result.GetType().FullName | Should Be "System.Object[]"
-        $result.Count | Should Be 3
+        $result.Length | Should Be 3
     }
 
-    It "@([int[]](1,2,3)) should return an array of object[]" {
+    It "@([int[]](1,2,3)) should return a 3-element array of object[]" {
         $result = @([int[]](1,2,3))
         $result.GetType().FullName | Should Be "System.Object[]"
-        $result.Count | Should Be 3
+        $result.Length | Should Be 3
     }
 
-    It "@([object[]]`$null) should return an array of object[]" {
+    It "@([object[]]`$null) should return a 1-element(`$null) array of object[]" {
         $result = @([object[]]$null)
         $result.GetType().FullName | Should Be "System.Object[]"
-        $result.Count | Should Be 1
+        $result.Length | Should Be 1
         $result[0] | Should Be $null
     }
 
-    It "@([int[]]`$null) should return an array of object[]" {
+    It "@([int[]]`$null) should return a 1-element(`$null) array of object[]" {
         $result = @([int[]]$null)
         $result.GetType().FullName | Should Be "System.Object[]"
-        $result.Count | Should Be 1
+        $result.Length | Should Be 1
         $result[0] | Should Be $null
     }
 
-    It "@([object[]][System.Management.Automation.Internal.AutomationNull]::Value) should return an array of object[]" {
+    It "@([object[]][System.Management.Automation.Internal.AutomationNull]::Value) should return a 1-element(`$null) array of object[]" {
         $result = @([object[]][System.Management.Automation.Internal.AutomationNull]::Value)
         $result.GetType().FullName | Should Be "System.Object[]"
-        $result.Count | Should Be 1
+        $result.Length | Should Be 1
         $result[0] | Should Be $null
     }
 
-    It "@([int[]][System.Management.Automation.Internal.AutomationNull]::Value) should return an array of object[]" {
+    It "@([int[]][System.Management.Automation.Internal.AutomationNull]::Value) should return a 1-element(`$null) array of object[]" {
         $result = @([int[]][System.Management.Automation.Internal.AutomationNull]::Value)
         $result.GetType().FullName | Should Be "System.Object[]"
-        $result.Count | Should Be 1
+        $result.Length | Should Be 1
         $result[0] | Should Be $null
+    }
+
+    It "@(`$null) should return a 1-element(`$null) array of object[]" {
+        $result = @($null)
+        $result.GetType().FullName | Should Be "System.Object[]"
+        $result.Length | Should Be 1
+        $result[0] | Should Be $null
+    }
+
+    It "@([System.Management.Automation.Internal.AutomationNull]::Value) should return an empty array of object[]" {
+        $result = @([System.Management.Automation.Internal.AutomationNull]::Value)
+        $result.GetType().FullName | Should Be "System.Object[]"
+        $result.Length | Should Be 0
     }
 }
