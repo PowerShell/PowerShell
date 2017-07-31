7da758f99efec41b75fe3cb1ac428cf712d9ddb5
Andrew <anmenaga@microsoft.com>
2017-07-31 16:35:37 -0700

    PR feedback 2

--- test/powershell/Modules/Microsoft.PowerShell.Diagnostics/New-WinEvent.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Diagnostics/New-WinEvent.Tests.ps1
@@ -2,29 +2,40 @@ Describe 'New-WinEvent' -Tags "CI" {
 
     Context "New-WinEvent tests" {
         
-        It 'Simple New-WinEvent' -Skip:(-not $IsWindows) {
+        BeforeAll {
+            $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+            if ( ! $IsWindows ) {
+                $PSDefaultParameterValues["it:skip"] = $true
+            }
+        }
+        
+        AfterAll {
+            $global:PSDefaultParameterValues = $originalDefaultParameterValues
+        }
+
+        It 'Simple New-WinEvent' {
             New-WinEvent -ProviderName Microsoft-Windows-PowerShell -Id 40962 -Version 1 # simple event without any payload
             $filter = @{ ProviderName = 'Microsoft-Windows-PowerShell'; Id = 40962}
             (Get-WinEvent -filterHashtable $filter).Count | Should BeGreaterThan 0
         }
         
-        It 'No provider found error' -Skip:(-not $IsWindows) {
+        It 'No provider found error' {
             { New-WinEvent -ProviderName NonExistingProvider -Id 0 } | ShouldBeErrorId 'System.ArgumentException,Microsoft.PowerShell.Commands.NewWinEventCommand'
         }
         
-        It 'EmptyProviderName error' -Skip:(-not $IsWindows) {
+        It 'EmptyProviderName error' {
             { New-WinEvent -ProviderName $null -Id 0 } | ShouldBeErrorId 'ParameterArgumentValidationErrorNullNotAllowed,Microsoft.PowerShell.Commands.NewWinEventCommand'
         }
 
-        It 'IncorrectEventId error' -Skip:(-not $IsWindows) {
+        It 'IncorrectEventId error' {
             { New-WinEvent Microsoft-Windows-PowerShell -Id 999999 } | ShouldBeErrorId 'Microsoft.PowerShell.Commands.EventWriteException,Microsoft.PowerShell.Commands.NewWinEventCommand'
         }
 
-        It 'IncorrectEventVersion error' -Skip:(-not $IsWindows) {
+        It 'IncorrectEventVersion error' {
             { New-WinEvent -ProviderName Microsoft-Windows-PowerShell -Id 40962 -Version 99 } | ShouldBeErrorId 'Microsoft.PowerShell.Commands.EventWriteException,Microsoft.PowerShell.Commands.NewWinEventCommand'
         }
         
-        It 'PayloadMismatch error' -Skip:(-not $IsWindows) {
+        It 'PayloadMismatch error' {
             $logPath = join-path $TestDrive 'testlog1.txt'
             # this will print the warning with expected event template to the file
             New-WinEvent -ProviderName Microsoft-Windows-PowerShell -Id 32868 *> $logPath
