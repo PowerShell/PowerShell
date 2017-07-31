8a633996c2f479dd16aa5dd463a162789738bf03
iSazonov <darpa@yandex.ru>
2017-07-31 18:08:05 +0300

    Cleanup '#if CORE' in remoterunspace.cs

--- src/System.Management.Automation/engine/remoting/client/remoterunspace.cs
+++ src/System.Management.Automation/engine/remoting/client/remoterunspace.cs
@@ -220,11 +220,7 @@ namespace System.Management.Automation
         /// <summary>
         /// runspaceConfiguration information for this runspace
         /// </summary>
-#if CORECLR
         internal
-#else
-        public
-#endif
         override RunspaceConfiguration RunspaceConfiguration
         {
             get
