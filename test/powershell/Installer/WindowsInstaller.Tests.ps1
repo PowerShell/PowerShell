f5b7dc2a72db08a4e4737e787dbfa9cc64d97c16
Christoph Bergmeister <c.bergmeister@gmail.com>
2017-08-17 23:57:01 +0100

    #4458 Removed language identifiers in URLs as requested by code review in PR #4602

--- test/powershell/Installer/WindowsInstaller.Tests.ps1
+++ test/powershell/Installer/WindowsInstaller.Tests.ps1
@@ -4,7 +4,7 @@ $wixProductFile = Join-Path $thisTestFolder "..\..\..\assets\Product.wxs"
 Describe "Windows Installer" -Tags "Scenario" {
 
     Context "Universal C Runtime Download Link" {
-        $universalCRuntimeDownloadLink = 'https://www.microsoft.com/en-us/download/details.aspx?id=50410'
+        $universalCRuntimeDownloadLink = 'https://www.microsoft.com/download/details.aspx?id=50410'
         It "Wix file should have download link about Universal C runtime" {
            (Get-Content $wixProductFile -Raw).Contains($universalCRuntimeDownloadLink) | Should Be $true
         }
@@ -14,7 +14,7 @@ Describe "Windows Installer" -Tags "Scenario" {
     }
 
     Context "Visual Studio C++ Redistributables Link" {
-        $visualStudioCPlusPlusRedistributablesDownloadLink = 'https://www.microsoft.com/en-gb/download/details.aspx?id=48145'
+        $visualStudioCPlusPlusRedistributablesDownloadLink = 'https://www.microsoft.com/download/details.aspx?id=48145'
         It "WiX file should have documentation about Visual Studio C++ redistributables" {
            (Get-Content $wixProductFile -Raw).Contains($visualStudioCPlusPlusRedistributablesDownloadLink) | Should Be $true
         }
