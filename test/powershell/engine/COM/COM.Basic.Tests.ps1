12002e7f2deac22561fe37707decccf072861f80
Dongbo Wang <dongb.wang@gmail.com>
2017-08-28 13:19:02 -0700

    Use stricter rule to unwrap a PSObject that wraps a COM object (#4614)
    GetMember/SetMember/InvokeMember operations on a COM object will generate code to unwrap the COM object if it's wrapped in PSObject. Due to a loose restriction, if you have a string wrapped to a PSObject with an ETS member of the same name, then the string PSObject will be unwrapped too and the ETS member will be lost. The fix is to use a more restricted rule by checking if the base object is a COM object.

--- test/powershell/engine/COM/COM.Basic.Tests.ps1
+++ test/powershell/engine/COM/COM.Basic.Tests.ps1
@@ -16,7 +16,11 @@ try {
             $items = $folder.Items()
 
             ## $items is a collection of all items belong to the folder, and it should be enumerated.
+            $items.Count | Should Be 3
             $items | Measure-Object | ForEach-Object Count | Should Be $items.Count
+
+            $names = $items | ForEach-Object { $_.Name }
+            $names -join "," | Should Be "file1,file2,file3"
         }
 
         It "Should enumerate IEnumVariant interface object without exception" {
@@ -26,6 +30,7 @@ try {
 
             ## $enumVariant is an IEnumVariant interface of all items belong to the folder, and it should be enumerated.
             $enumVariant = $items._NewEnum()
+            $items.Count | Should Be 3
             $enumVariant | Measure-Object | ForEach-Object Count | Should Be $items.Count
         }
 
@@ -40,6 +45,52 @@ try {
             $element = $drives | Select-Object -First 1
             [System.Object]::ReferenceEquals($element, $drives) | Should Be $false
             $element | Should Be $drives.Item($element.DriveLetter)
+        }
+    }
+
+    Describe 'GetMember/SetMember/InvokeMember binders should have more restricted rule for COM object' -Tags "CI" {
+        BeforeAll {
+            if ([System.Management.Automation.Platform]::IsWindowsDesktop) {
+                $null = New-Item -Path $TESTDRIVE/bar -ItemType Directory -Force
+
+                $shell = New-Object -ComObject "Shell.Application"
+                $folder = $shell.Namespace("$TESTDRIVE")
+                $item = $folder.Items().Item(0)
+                $item = [psobject]::AsPSObject($item)
+
+                ## Create a PSObject that has an instance member 'Name' and a script method 'Windows'
+                $str = Add-Member -InputObject "abc" -MemberType NoteProperty -Name Name -Value "Hello" -PassThru
+                $str = Add-Member -InputObject $str -MemberType ScriptMethod -Name Windows -Value { "Windows" } -PassThru
+            }
+        }
+
+        It "GetMember binder should differentiate PSObject that wraps COM object from other PSObjects" {
+            ## GetMember on the member name 'Name'.
+            $entry1 = ($item, "bar")
+            $entry2 = ($str, "Hello")
+
+            foreach ($pair in ($entry1, $entry2, $entry2, $entry1, $entry1, $entry2)) {
+                $pair[0].Name | Should Be $pair[1]
+            }
+        }
+
+        It "SetMember binder should differentiate PSObject that wraps COM object from other PSObjects" {
+            ## SetMember on the member name 'Name'
+            $entry1 = ($item, "foo")
+            $entry2 = ($str, "World")
+
+            foreach ($pair in ($entry1, $entry2)) {
+                $pair[0].Name = $pair[1]
+                $pair[0].Name | Should Be $pair[1]
+            }
+        }
+
+        It "InvokeMember binder should differentiate PSObject that wraps COM object from other PSObjects" {
+            ## InvokeMember on the member name 'Windows'
+            $shell | ForEach-Object { $_.Windows() } > $null
+
+            ## '$str' is a PSObject that wraps a string, but with ScriptMethod 'Windows'
+            $str.Windows() | Should Be "Windows"
         }
     }
 
