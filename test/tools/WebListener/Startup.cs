a2dd0ca4b5d90c55889e43805a23eb0c7175c00e
Mark Kraus <github@markekraus.com>
2017-09-18 11:26:01 -0500

    Replace httpbin.org/redirect Tests with WebListener (#4852)

--- test/tools/WebListener/Startup.cs
+++ test/tools/WebListener/Startup.cs
@@ -43,6 +43,11 @@ namespace mvc
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "redirect",
+                    template: "Redirect/{count?}",
+                    defaults: new {controller = "Redirect", action = "Index"}
+                );
             });
         }
     }
