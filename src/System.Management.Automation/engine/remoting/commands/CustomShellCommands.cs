8c29037d46b48f72c1692083ac43361cec82da49
iSazonov <darpa@yandex.ru>
2017-09-20 16:41:17 +0300

    Fix Enable-PSRemoting

--- src/System.Management.Automation/engine/remoting/commands/CustomShellCommands.cs
+++ src/System.Management.Automation/engine/remoting/commands/CustomShellCommands.cs
@@ -4874,7 +4874,7 @@ param(
                 # Section 1:
                 # Move pwrshplugin.dll from $PSHOME to the endpoint directory
                 #
-                $pluginInstallPath = Join-Path ""$env:WINDIR\System32\PowerShell"" $psversiontable.GitCommitId
+                $pluginInstallPath = Join-Path ""$env:WINDIR\System32\PowerShell"" (""v"" + $psversiontable.GitCommitId -replace "" Commits: "", ""-""  -replace "" SHA: "", ""-"")
                 if (!(Test-Path $pluginInstallPath))
                 {{
                     $resolvedPluginInstallPath = New-Item -Type Directory -Path $pluginInstallPath
