a1f2fe2e305e884c13c3391640178f57de35bb47
Jim Truher <jimtru@microsoft.com>
2017-09-21 16:32:51 -0700

    Add Tests for *-computer cmdlets
    Remove all code protected by #!CORE
    Add InternalTestHooks to improve testability of the cmdlets

--- test/powershell/Modules/Microsoft.PowerShell.Management/Test-Connection.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Test-Connection.Tests.ps1
@@ -36,4 +36,8 @@ Describe "Test-Connection" -Tags "CI" {
         param($computername, $result)
         Test-Connection -ComputerName $computername -Count 1 -Quiet | Should Be $result
     }
+
+    It "The AsJob parameter throws the appropriate error" {
+        { Test-Connection -ComputerName . -AsJob } | ShouldBeErrorId "NotSupported,Microsoft.PowerShell.Commands.TestConnectionCommand"
+    }
 }
