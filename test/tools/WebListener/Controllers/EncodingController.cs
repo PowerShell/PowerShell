65cf571b5475581c0596de81c2fe4027c731359d
Mark Kraus <github@markekraus.com>
2017-09-22 12:09:49 -0500

    Replace httpbin.org/encoding Tests with WebListener (#4869)

---
+++ test/tools/WebListener/Controllers/EncodingController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Net.Http.Headers;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class EncodingController : Controller
+    {
+        public ActionResult Index()
+        {
+            string url = "/Encoding/Utf8";
+            ViewData["Url"] = url;
+            Response.Redirect(url, false);
+            return View("~/Views/Redirect/Index.cshtml");
+        }
+
+        public ActionResult Utf8()
+        {
+            MediaTypeHeaderValue mediaType = new MediaTypeHeaderValue("text/html");
+            mediaType.Encoding = Encoding.UTF8;
+            return View();
+        }
+        
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
