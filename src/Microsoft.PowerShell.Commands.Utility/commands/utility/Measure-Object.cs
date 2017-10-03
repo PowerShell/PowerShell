ca4b539ea1dea2dac966da19eac5e29ae1fc3286
dfinke <finked@hotmail.com>
2017-10-03 13:53:11 -0400

    Set Average true if StdDeviation is set

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/Measure-Object.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/Measure-Object.cs
@@ -295,6 +295,8 @@ namespace Microsoft.PowerShell.Commands
             set
             {
                 _measureStdDeviation = value;
+                if(value == true)
+                    _measureAverage = true;
             }
         }
 
