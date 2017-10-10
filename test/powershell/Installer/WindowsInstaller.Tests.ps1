f6e13db90aeb70bd057c80a572388ab095b99643
Dongbo Wang <dongbow@microsoft.com>
2017-10-10 14:46:37 -0700

    Address more comments

--- test/powershell/Installer/WindowsInstaller.Tests.ps1
+++ test/powershell/Installer/WindowsInstaller.Tests.ps1
@@ -1,22 +1,9 @@
-$thisTestFolder = Split-Path $MyInvocation.MyCommand.Path -Parent
-$wixProductFile = Join-Path $thisTestFolder "..\..\..\assets\Product.wxs"
-
 Describe "Windows Installer" -Tags "Scenario" {
 
-    BeforeAll {
-        $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
-        if ( ! $IsWindows ) {
-            $PSDefaultParameterValues["it:skip"] = $true
-        }
-    }
-
-    AfterAll {
-        $global:PSDefaultParameterValues = $originalDefaultParameterValues
-    }
-
-    $preRequisitesLink =  'https://github.com/PowerShell/PowerShell/blob/master/docs/installation/windows.md#prerequisites'
+    $preRequisitesLink =  'https://aka.ms/pscore6-prereq'
         
     It "WiX (Windows Installer XML) file contains pre-requisites link $preRequisitesLink" {
+        $wixProductFile = Join-Path -Path $PSScriptRoot -ChildPath "..\..\..\assets\Product.wxs"
         (Get-Content $wixProductFile -Raw).Contains($preRequisitesLink) | Should Be $true
     }
 
@@ -24,5 +11,4 @@ Describe "Windows Installer" -Tags "Scenario" {
         # Because an outdated link 'https://www.microsoft.com/download/details.aspx?id=504100000' would still return a 200 reponse (due to a redirection to an error page), it only checks that it returns something
         (Invoke-WebRequest $preRequisitesLink -UseBasicParsing) | Should Not Be $null
     }
-	
 }
