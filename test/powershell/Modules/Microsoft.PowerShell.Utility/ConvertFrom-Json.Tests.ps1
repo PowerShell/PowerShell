69df716520194470fb692b6ddfe3060f51e108c9
Christoph Bergmeister <c.bergmeister@gmail.com>
2017-10-18 18:52:51 +0100

    Address PR comments about tests (mostly style) by @iSazonov

--- test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertFrom-Json.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertFrom-Json.Tests.ps1
@@ -1,16 +1,19 @@
 Describe 'ConvertFrom-Json' -tags "CI" {
     
-    $testCasesWithAndWithoutAsHashtableSwitch = @(
-        @{ AsHashtable = $true  }
-        @{ AsHashtable = $false }
-    )
+    BeforeAll {
+        $testCasesWithAndWithoutAsHashtableSwitch = @(
+            @{ AsHashtable = $true  }
+            @{ AsHashtable = $false }
+        )
+    }
 
-    It 'can convert a single-line object with AsHashtable switch set to <AsHashtable>' -TestCase $testCasesWithAndWithoutAsHashtableSwitch {
+
+    It 'Can convert a single-line object with AsHashtable switch set to <AsHashtable>' -TestCase $testCasesWithAndWithoutAsHashtableSwitch {
         Param($AsHashtable)
         ('{"a" : "1"}' | ConvertFrom-Json -AsHashtable:$AsHashtable).a | Should Be 1
     }
 
-    It 'can convert one string-per-object with AsHashtable switch set to <AsHashtable>' -TestCase $testCasesWithAndWithoutAsHashtableSwitch {
+    It 'Can convert one string-per-object with AsHashtable switch set to <AsHashtable>' -TestCase $testCasesWithAndWithoutAsHashtableSwitch {
         Param($AsHashtable)
         $json = @('{"a" : "1"}', '{"a" : "x"}') | ConvertFrom-Json -AsHashtable:$AsHashtable
         $json.Count | Should Be 2
@@ -21,7 +24,7 @@ Describe 'ConvertFrom-Json' -tags "CI" {
         }
     }
 
-    It 'can convert multi-line object with AsHashtable switch set to <AsHashtable>' -TestCase $testCasesWithAndWithoutAsHashtableSwitch {
+    It 'Can convert multi-line object with AsHashtable switch set to <AsHashtable>' -TestCase $testCasesWithAndWithoutAsHashtableSwitch {
         Param($AsHashtable)
         $json = @('{"a" :', '"x"}') | ConvertFrom-Json -AsHashtable:$AsHashtable
         $json.a | Should Be 'x'
--- test/powershell/Modules/Microsoft.PowerShell.Utility/JsonObject.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/JsonObject.Tests.ps1
@@ -1,6 +1,11 @@
 ﻿Describe 'Unit tests for JsonObject' -tags "CI" {
 
-    $validStrings = @(
+    BeforeAll {
+        $jsonWithEmptyKey = '{"": "Value"}'
+    }
+
+
+    It 'No error for valid string ''<name>'' with -ReturnHashTable:$<ReturnHashTable>' -TestCase @(
         @{ name = "null";   str = $null;   ReturnHashTable = $true  }
         @{ name = "empty";  str = "";      ReturnHashTable = $true  }
         @{ name = "spaces"; str = "  ";    ReturnHashTable = $true  }
@@ -9,37 +14,31 @@
         @{ name = "empty";  str = "";      ReturnHashTable = $false }
         @{ name = "spaces"; str = "  ";    ReturnHashTable = $false }
         @{ name = "object"; str = "{a:1}"; ReturnHashTable = $false }
-    )
-
-    It 'no error for valid string ''<name>'' with -ReturnHashTable:$<ReturnHashTable>' -TestCase $validStrings {
+    ) {
         param ($str, $ReturnHashTable)
         $errRecord = $null
         [Microsoft.PowerShell.Commands.JsonObject]::ConvertFromJson($str, $ReturnHashTable, [ref]$errRecord)
         $errRecord | Should BeNullOrEmpty
     }
 
-    $invalidStrings = @(
+    It 'Throw ArgumentException for invalid string ''<name>'' with -ReturnHashTable:$<ReturnHashTable>' -TestCase @(
         @{ name = "plain text"; str = "plaintext"; ReturnHashTable = $true  }
         @{ name = "part";       str = '{"a" :';    ReturnHashTable = $true  }
         @{ name = "plain text"; str = "plaintext"; ReturnHashTable = $false }
         @{ name = "part";       str = '{"a" :';    ReturnHashTable = $false }
-    )
-
-    It 'throw ArgumentException for invalid string ''<name>'' with -ReturnHashTable:$<ReturnHashTable>' -TestCase $invalidStrings  {
+    )  {
         param ($str, $ReturnHashTable)
         $errRecord = $null
         { [Microsoft.PowerShell.Commands.JsonObject]::ConvertFromJson($str, $ReturnHashTable, [ref]$errRecord) } | ShouldBeErrorId "ArgumentException"
     }
-
-    $jsonWithEmptyKey = '{"": "Value"}'
-    It 'throw InvalidOperationException when json contains empty key name' {
+    
+    It 'Throw InvalidOperationException when json contains empty key name' {
         $errorRecord = $null
         [Microsoft.PowerShell.Commands.JsonObject]::ConvertFromJson($jsonWithEmptyKey, [ref]$errorRecord)
-        $errorRecord.Exception.GetType() | Should Be System.InvalidOperationException
-        $errorRecord.FullyQualifiedErrorId | Should Be EmptyKeyInJsonString
+        $errorRecord.FullyQualifiedErrorId | Should Be 'EmptyKeyInJsonString'
     }
 
-    It 'not throw when json contains empty key na' {
+    It 'Not throw when json contains empty key na' {
         $errorRecord = $null
         $result = [Microsoft.PowerShell.Commands.JsonObject]::ConvertFromJson($jsonWithEmptyKey, $true, [ref]$errorRecord)
         $result | Should Not Be $null
