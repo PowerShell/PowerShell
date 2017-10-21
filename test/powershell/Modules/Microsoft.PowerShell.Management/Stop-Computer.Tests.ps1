35b36a718365360ae4240d91eba7d484e1243fac
Jim Truher <jimtru@microsoft.com>
2017-10-20 17:07:11 -0700

    Refactor testhook setting code to module
    remove unneeded DCOM resources

--- test/powershell/Modules/Microsoft.PowerShell.Management/Stop-Computer.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Stop-Computer.Tests.ps1
@@ -1,51 +1,36 @@
 # note these will manipulate private data in the PowerShell engine which will
 # enable us to not actually stop the system, but return right before we do
-$TesthooksType = [system.management.automation.internal.internaltesthooks]
-$TesthookName = "TestStopComputer"
-$TesthookResultName = "TestStopComputerResults"
+$stopTesthook = "TestStopComputer"
+$stopTesthookResultName = "TestStopComputerResults"
 $DefaultResultValue = 0
-function Set-HookResult([int]$result) {
-    ${TesthooksType}::SetTestHook($TesthookResultName, $result)
-}
-
-# protect the user from from stop their computer 
-function Test-TesthookIsSet() {
-    try {
-        return ${TesthooksType}.GetField($TesthookName, "NonPublic,Static").GetValue($null)
-    }
-    catch {
-        # fall through
-    }
-    return $false
-}
 
 try 
 {
     # set up for testing
     $PSDefaultParameterValues["it:skip"] = ! $IsWindows
-    ${TesthooksType}::SetTestHook($TesthookName,$true)
+    Enable-Testhook -testhookName $stopTesthook
 
     Describe "Stop-Computer" -Tag Feature {
         # if we throw in BeforeEach, the test will fail and the stop will not be called
         BeforeEach {
-            if ( ! (Test-TesthookIsSet) ) {
-                throw "Testhook '${TesthookName}' is not set"
+            if ( ! (Test-TesthookIsSet -testhookName $stopTesthook) ) {
+                throw "Testhook '${stopTesthook}' is not set"
             }
         }
 
         AfterEach {
-            Set-HookResult -result $defaultResultValue
+            Set-TesthookResult -testhookName $stopTesthookResultName -Value $defaultResultValue
         }
 
         It "Should stop the local computer" {
-            Set-HookResult -result $defaultResultValue
-            Stop-Computer -ea Stop| Should BeNullOrEmpty
+            Set-TesthookResult -testhookName $stopTesthookResultName -Value $defaultResultValue
+            Stop-Computer -ErrorAction Stop| Should BeNullOrEmpty
         }
 
-        It "Should support -computer parameter" {
-            Set-HookResult -result $defaultResultValue
+        It "Should support -Computer parameter" {
+            Set-TesthookResult -testhookName $stopTesthookResultName -Value $defaultResultValue
             $computerNames = "localhost",(hostname)
-            Stop-Computer -computer $computerNames -ea Stop| Should BeNullOrEmpty
+            Stop-Computer -Computer $computerNames -ErrorAction Stop| Should BeNullOrEmpty
         }
     
         It "Should support WsmanAuthentication types" {
@@ -57,8 +42,8 @@ try
 
         Context "Stop-Computer Error Conditions" {
             It "Should return the proper error when it occurs" {
-                Set-HookResult -result 0x300000
-                Stop-Computer -ev StopError 2>$null
+                Set-TesthookResult -testhookName $stopTesthookResultName -Value 0x300000
+                Stop-Computer -ErrorVariable StopError 2>$null
                 $StopError.Exception.Message | Should match 0x300000
             }
 
@@ -69,7 +54,6 @@ try
 finally
 {
     $PSDefaultParameterValues.Remove("it:skip")
-    ${TesthooksType}::SetTestHook($TesthookName, $false)
-    Set-HookResult -result 0
-    ${TesthooksType}::SetTestHook($TesthookResultName, $DefaultResultValue)
+    Disable-Testhook -testhookName $stopTesthook
+    Set-TesthookResult -testhookName $stopTesthookResultName -Value $DefaultResultValue
 }
