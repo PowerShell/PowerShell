79f396d93059ea5f05e49a1351bfdab89ee529b6
Raphael <raphael.faeh@corporatesoftware.ch>
2017-10-25 18:51:29 +0200

    Add ErrorAction Stop to all Get-Alias and Set-Alias Cmdlets in all tests for consistency. Add Should BeNullOrEmpty to "should throw if alias does not exist" test to verify that foo alias really is not there before removing it.

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Remove-Alias.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Remove-Alias.Tests.ps1
@@ -1,45 +1,46 @@
 Describe "Remove-Alias" -Tags "CI" {
     It "Remove-Alias should remove a non-readonly alias"{
         {
-            Set-Alias -Name "foo" -Value "bar"
-            Remove-Alias -Name "foo"
+            Set-Alias -Name "foo" -Value "bar" -ErrorAction Stop
+            Remove-Alias -Name "foo" -ErrorAction Stop
             Get-Alias -Name "foo" -ErrorAction Stop
         } | ShouldBeErrorId 'ItemNotFoundException,Microsoft.PowerShell.Commands.GetAliasCommand'
     }
 
     It "Remove-Alias should throw on a readonly alias"{
         {
-            Set-Alias -Name "foo" -Value "bar" -Option ReadOnly
+            Set-Alias -Name "foo" -Value "bar" -Option ReadOnly -ErrorAction Stop
             Remove-Alias -Name "foo" -ErrorAction Stop
         } | ShouldBeErrorId 'AliasNotRemovable,Microsoft.PowerShell.Commands.RemoveAliasCommand'
     }
 
     It "Remove-Alias should remove a non-readonly alias with force"{
         {
-            Set-Alias -Name "foo" -Value "bar"
-            Remove-Alias -Name "foo" -Force
+            Set-Alias -Name "foo" -Value "bar" -ErrorAction Stop
+            Remove-Alias -Name "foo" -Force -ErrorAction Stop
             Get-Alias -Name "foo" -ErrorAction Stop
         } | ShouldBeErrorId 'ItemNotFoundException,Microsoft.PowerShell.Commands.GetAliasCommand'
     }
 
     It "Remove-Alias should remove a readonly alias with force"{
         {
-            Set-Alias -Name "foo" -Value "bar" -Option ReadOnly
-            Remove-Alias -Name "foo" -Force
+            Set-Alias -Name "foo" -Value "bar" -Option ReadOnly -ErrorAction Stop
+            Remove-Alias -Name "foo" -Force -ErrorAction Stop
             Get-Alias -Name "foo" -ErrorAction Stop
         } | ShouldBeErrorId 'ItemNotFoundException,Microsoft.PowerShell.Commands.GetAliasCommand'
     }
 
     It "Remove-Alias should throw if alias does not exist"{
-        {            
-            Remove-Alias -Name "foo" -ErrorAction Stop            
+        {
+            Get-Alias -Name "foo" -ErrorAction SilentlyContinue | Should BeNullorEmpty
+            Remove-Alias -Name "foo" -ErrorAction Stop
         } | ShouldBeErrorId 'ItemNotFoundException,Microsoft.PowerShell.Commands.RemoveAliasCommand'
     }
 
     It "Remove-Alias should throw on out-of-range scope"{
         {
-            Set-Alias -Name "foo" -Value "bar"
-            Remove-Alias -Name "foo" -Scope 99999 -ErrorAction Stop            
+            Set-Alias -Name "foo" -Value "bar" -ErrorAction Stop
+            Remove-Alias -Name "foo" -Scope 99999 -ErrorAction Stop
         } | ShouldBeErrorId "ArgumentOutOfRange,Microsoft.PowerShell.Commands.RemoveAliasCommand"
     }
 }
