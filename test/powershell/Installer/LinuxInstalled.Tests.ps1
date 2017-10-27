632a96042eb78dafaf2b4f8677f2834e7d6590c8
Dan Travison <dantra@ntdev.microsoft.com>
2017-10-27 13:19:29 -0700

    Fix test skip filter

--- test/powershell/Installer/LinuxInstalled.Tests.ps1
+++ test/powershell/Installer/LinuxInstalled.Tests.ps1
@@ -1,17 +1,17 @@
 Describe 'Ubuntu/Debian Post-Install Tests' -Tag 'CI' {
     BeforeAll {
-        $skip = $IsWindows
+        $skip_libgssapi = $IsWindows
 
-        if (-not $skip)
+        if (-not $skip_libgssapi)
         {
             $LinuxInfo = Get-Content /etc/os-release -Raw | ConvertFrom-StringData
             $IsUbuntu = $LinuxInfo.ID -match 'ubuntu'
             $IsDebian = $LinuxInfo.ID -match 'debian'
-            $skiplibgssapi = -not ($IsUbuntu -or $IsDebian)
+            $skip_libgssapi = -not ($IsUbuntu -or $IsDebian)
         }
     }
 
-    It "libgssapi_krb5.so symbolic link exists in the $PSHOME directory" -skip:$skiplibgssapi {
+    It "libgssapi_krb5.so symbolic link exists in the $PSHOME directory" -skip:$skip_libgssapi {
         $expectedPath = Join-Path -Path $PSHOME -ChildPath 'libgssapi_krb5.so'
 
         Test-Path -path $expectedPath | Should Be $true
