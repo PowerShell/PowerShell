b58df7d60082598cf1218bb0688c28e21c06d7d1
Jason Shirk <jasonsh@microsoft.com>
2017-10-27 15:47:45 -0700

    Rework passing array literal to native commands

--- test/powershell/Language/Scripting/NativeExecution/NativeCommandArguments.Tests.ps1
+++ test/powershell/Language/Scripting/NativeExecution/NativeCommandArguments.Tests.ps1
@@ -43,4 +43,21 @@ Describe "Native Command Arguments" -tags "CI" {
             $lines[$i] | Should Be "Arg $i is <$($expected[$i])>"
         }
     }
+
+    It "Should handle PowerShell arrays with or without spaces correctly: <arguments>" -TestCases @(
+        @{arguments = "a 1,2"; expected = "a", "1,2"}
+        @{arguments = "1, 2"; expected = "1,", "2"}
+        @{arguments = "1 ,2"; expected = "1", ",2"}
+        @{arguments = "1 , 2"; expected = "1", ",", "2"}
+        @{arguments = "1, 2,3"; expected = "1,", "2,3"}
+        @{arguments = "1 ,2,3"; expected = "1", ",2,3"}
+        @{arguments = "1 , 2,3"; expected = "1", ",", "2,3"}
+    ) {
+        param($arguments, $expected)
+        $lines = Invoke-Expression "testexe -echoargs $arguments"
+        $lines.Count | Should Be $expected.Count
+        for ($i = 0; $i -lt $lines.Count; $i++) {
+            $lines[$i] | Should Be "Arg $i is <$($expected[$i])>"
+        }
+    }
 }
