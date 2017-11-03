c9f83fecfcec38fe678356ade01177ed1621b416
Raphael <PowershellNinja@users.noreply.github.com>
2017-11-03 09:08:37 +0100

    Add Remove-Alias Command (#5143)
    * Add Remove-Alias Command. Add Remove-Alias Test. Add Remove-Alias to .psd1.
    
    * Fix code-formatting. Fix ErrorAction on Remove-Alias instead of preference variable. Remove unnecessary Get-Alias calls. Switch Force parameter to auto property. Add ValueFromPipeline to Remove-Alias Name parameter.
    
    * Remove empty lines. Add Remove-Alias to Unix Module .psd1. Add Remove-Alias to approved Commands test. Remove Try/Catch for SessionstateException. Add WriteError for aliasnotfound exception.
    
    * Add ErrorAction Stop to all Get-Alias and Set-Alias Cmdlets in all tests for consistency. Add Should BeNullOrEmpty to "should throw if alias does not exist" test to verify that foo alias really is not there before removing it.
    
    * Changed Remove-Alias parameter Name to String-Array. Changed Cmdlet Process Logic to work with String Array. Added Alias named "ral". Added test case for Alias "ral". Added "ral" Alias to list of approved Aliases. Replaced foo and bar values in all tests with better names.
    
    * Remove "ral"-alias test as this is covered in DefaultCommands.Tests.ps1. Add test for removing multiple alias at once. Fix wrong curly braces positioning.
    
    * Remove $FullCLR in DefaultCommands.Tests.ps1 for Remove-Alias and "ral" alias.
    
    * Add Alias "ral" to Remove-Alias Cmdlet.
    
    * Remove "ral" alias as this is handled by using the [Alias()] attribute on the Remove-Alias Cmdlet.
    
    * Replace alias names used in Remove-Alias Cmdlet tests with GUIDs to avoid collisions

---
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/RemoveAliasCommand.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Internal;
+
+namespace Microsoft.PowerShell.Commands
+{
+    /// <summary>
+    /// The implementation of the "Remove-Alias" cmdlet.
+    /// </summary>
+    ///
+    [Cmdlet(VerbsCommon.Remove, "Alias", DefaultParameterSetName = "Default", HelpUri = "")]
+    [Alias("ral")]
+    public class RemoveAliasCommand : PSCmdlet
+    {
+         #region Parameters
+
+        /// <summary>
+        /// The alias name to remove.
+        /// </summary>        
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        public string[] Name { get; set; }
+        
+        /// <summary>
+        /// The scope parameter for the command determines
+        /// which scope the alias is removed from.
+        /// </summary>        
+        [Parameter]
+        public string Scope { get; set; }
+
+        /// <summary>
+        /// If set to true and an existing alias of the same name exists
+        /// and is ReadOnly, it will still be deleted.
+        /// </summary>        
+        [Parameter]
+        public SwitchParameter Force { get; set; }        
+
+        #endregion Parameters
+
+        #region Command code
+
+        /// <summary>
+        /// The main processing loop of the command.
+        /// </summary>        
+        protected override void ProcessRecord()
+        {
+            foreach(string aliasName in Name)
+            {            
+                AliasInfo existingAlias = null;           
+                if (String.IsNullOrEmpty(Scope))
+                {
+                    existingAlias = SessionState.Internal.GetAlias(aliasName);
+                }
+                else
+                {
+                    existingAlias = SessionState.Internal.GetAliasAtScope(aliasName, Scope);
+                }
+
+                if (existingAlias != null)
+                {
+                    SessionState.Internal.RemoveAlias(aliasName, Force);
+                }
+                else
+                {
+                    ItemNotFoundException notAliasFound = new ItemNotFoundException(StringUtil.Format(AliasCommandStrings.NoAliasFound, "name", aliasName));
+                    ErrorRecord error = new ErrorRecord(notAliasFound, "ItemNotFoundException",ErrorCategory.ObjectNotFound, aliasName);
+                    WriteError(error);
+                }
+            }
+        }
+        #endregion Command code
+    }
+}
