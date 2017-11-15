aa2d69b68867adcf1f20184184b0bbc5b6837d4c
Travis Plunk <travis.plunk@microsoft.com>
2017-11-14 18:13:17 -0800

    address PR Feedback

--- build.psm1
+++ build.psm1
@@ -597,14 +597,10 @@ function Restore-PSModuleToBuild
 
     $modulesDir = Join-Path -Path $publishPath -ChildPath "Modules"
 
-    # Restore modules from myget feed
+    # Restore modules from powershellgallery feed
     Restore-PSModule -Destination $modulesDir -Name @(
         # PowerShellGet depends on PackageManagement module, so PackageManagement module will be installed with the PowerShellGet module.
         'PowerShellGet'
-    ) -SourceLocation "https://www.powershellgallery.com/api/v2/"
-
-    # Restore modules from powershellgallery feed
-    Restore-PSModule -Destination $modulesDir -Name @(
         'Microsoft.PowerShell.Archive'
     ) -SourceLocation "https://www.powershellgallery.com/api/v2/"
 }
