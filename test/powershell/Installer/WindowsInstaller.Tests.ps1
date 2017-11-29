38e22bb177ec2322127e88af7ac5a2b1e79092c7
Steve Lee <slee@microsoft.com>
2017-11-28 16:34:04 -0800

    output url so when test fails you can more easily troubleshoot (#5570)

--- test/powershell/Installer/WindowsInstaller.Tests.ps1
+++ test/powershell/Installer/WindowsInstaller.Tests.ps1
@@ -9,13 +9,13 @@ Describe "Windows Installer" -Tags "Scenario" {
             @{ Name = "WMF 5.1"; Url = "https://www.microsoft.com/download/details.aspx?id=54616" }
         )
     }
-        
+
     It "WiX (Windows Installer XML) file contains pre-requisites link $preRequisitesLink" {
         $wixProductFile = Join-Path -Path $PSScriptRoot -ChildPath "..\..\..\assets\Product.wxs"
         (Get-Content $wixProductFile -Raw).Contains($preRequisitesLink) | Should Be $true
     }
 
-    It "Pre-Requisistes link for '<Name>' is reachable" -TestCases $linkCheckTestCases -Test {
+    It "Pre-Requisistes link for '<Name>' is reachable: <url>" -TestCases $linkCheckTestCases -Test {
         param ($Url)
 
         # Because an outdated link 'https://www.microsoft.com/download/details.aspx?id=504100000' would still return a 200 reponse (due to a redirection to an error page), it only checks that it returns something
