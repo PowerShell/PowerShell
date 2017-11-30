52df947080105f8c68ec3eb4fac8fbc0c459862c
Chunqing Chen <cchen@microsoft.com>
2017-11-30 14:44:41 -0800

    Fix xunnit test for PS (#4780)
    * Initial work to enable xunit
    
    * Moved AssemblyOriginatorKeyFile to csharp.tests.csproj
    
    * Native binary has dylib extension on macOS

--- tools/travis.ps1
+++ tools/travis.ps1
@@ -240,7 +240,10 @@ elseif($Stage -eq 'Build')
     }
 
     try {
-        Start-PSxUnit
+        $testResultsXUnitFile = "$pwd/TestResultsXUnit.xml"
+        Start-PSxUnit -TestResultsFile $testResultsXUnitFile
+        # If there are failures, Test-XUnitTestResults throws
+        $testPassResult = Test-XUnitTestResults -TestResultsFile $testResultsXUnitFile
     }
     catch {
         $result = "FAIL"
