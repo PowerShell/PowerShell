a9c6292903d86a7bf3b3bfb59110d03d1234c525
Staffan Gustafsson <staffangu@outlook.com>
2017-12-02 03:40:35 +0100

    Enable conversions from PSMethod to Delegate (#5287)
    Underpinnings to make calling of Extension methods /Linq easier from PowerShell.
    Enables the following that previously had to be done via reflection.
    
    class M {
      [int] Twice([int] $value) { return 2 * $value }
    
      [int] DoubleSum([int[]] $values) {
         return [Linq.Enumerable]::Sum($values, [M]::Twice)
      }
    }
    
    Each PSMethod is created as with a unique type for the combinations of method signatures in the MethodInfos it represents.
    
    PSMethod<T> where T is a MethodGroup<>, potentially recursive in the last template argument.
    
    This way, we can determine by just looking at the type of a PSMethod if there exists a conversion from the PSMethod to a delegate.

--- src/System.Management.Automation/engine/runtime/Binding/Binders.cs
+++ src/System.Management.Automation/engine/runtime/Binding/Binders.cs
@@ -5609,7 +5609,7 @@ namespace System.Management.Automation.Language
                     else
                     {
                         DotNetAdapter.MethodCacheEntry method = new DotNetAdapter.MethodCacheEntry(candidateMethods.ToArray());
-                        memberInfo = new PSMethod(this.Name, PSObject.dotNetInstanceAdapter, null, method);
+                        memberInfo = PSMethod.Create(this.Name, PSObject.dotNetInstanceAdapter, null, method);
                     }
                 }
             }
