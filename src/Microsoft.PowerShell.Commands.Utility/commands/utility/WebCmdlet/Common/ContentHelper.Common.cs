0d91d66251660cb039725a768fa9a94f09cce6e1
Mark Kraus <github@markekraus.com>
2017-12-19 17:23:37 -0600

    [Feature] Merge and sort ContentHelper

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/Common/ContentHelper.Common.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/Common/ContentHelper.Common.cs
@@ -3,45 +3,49 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 --********************************************************************/
 
 using System;
-using System.Management.Automation;
-using System.Text;
-using Microsoft.Win32;
 using System.Linq;
+using System.Management.Automation;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.Win32;
 
 namespace Microsoft.PowerShell.Commands
 {
-    internal static partial class ContentHelper
+    internal static class ContentHelper
     {
         #region Constants
-
-        // used to split contentType arguments
-        private static readonly char[] s_contentTypeParamSeparator = { ';' };
 
         // default codepage encoding for web content.  See RFC 2616.
         private const string _defaultCodePage = "ISO-8859-1";
 
         #endregion Constants
 
+        #region Fields
+
+        // used to split contentType arguments
+        private static readonly char[] s_contentTypeParamSeparator = { ';' };
+
+        #endregion Fields
+
         #region Internal Methods
 
-        internal static bool IsText(string contentType)
+        internal static string GetContentType(HttpResponseMessage response)
         {
-            contentType = GetContentTypeSignature(contentType);
-            return CheckIsText(contentType);
+            // ContentType may not exist in response header.  Return null if not.
+            return response.Content.Headers.ContentType?.MediaType;
         }
 
-        internal static bool IsXml(string contentType)
+        internal static Encoding GetDefaultEncoding()
         {
-            contentType = GetContentTypeSignature(contentType);
-            return CheckIsXml(contentType);
+            return GetEncodingOrDefault((string)null);
         }
 
-        internal static bool IsJson(string contentType)
+        internal static Encoding GetEncoding(HttpResponseMessage response)
         {
-            contentType = GetContentTypeSignature(contentType);
-            return CheckIsJson(contentType);
+            // ContentType may not exist in response header.
+            string charSet = response.Content.Headers.ContentType?.CharSet;
+            return GetEncodingOrDefault(charSet);
         }
 
         internal static Encoding GetEncodingOrDefault(string characterSet)
@@ -63,22 +67,87 @@ namespace Microsoft.PowerShell.Commands
             return encoding;
         }
 
-        internal static Encoding GetDefaultEncoding()
+        internal static StringBuilder GetRawContentHeader(HttpResponseMessage response)
         {
-            return GetEncodingOrDefault((string)null);
+            StringBuilder raw = new StringBuilder();
+
+            string protocol = WebResponseHelper.GetProtocol(response);
+            if (!string.IsNullOrEmpty(protocol))
+            {
+                int statusCode = WebResponseHelper.GetStatusCode(response);
+                string statusDescription = WebResponseHelper.GetStatusDescription(response);
+                raw.AppendFormat("{0} {1} {2}", protocol, statusCode, statusDescription);
+                raw.AppendLine();
+            }
+
+            HttpHeaders[] headerCollections =
+            {
+                response.Headers,
+                response.Content == null ? null : response.Content.Headers
+            };
+
+            foreach (var headerCollection in headerCollections)
+            {
+                if (headerCollection == null)
+                {
+                    continue;
+                }
+                foreach (var header in headerCollection)
+                {
+                    // Headers may have multiple entries with different values
+                    foreach (var headerValue in header.Value)
+                    {
+                        raw.Append(header.Key);
+                        raw.Append(": ");
+                        raw.Append(headerValue);
+                        raw.AppendLine();
+                    }
+                }
+            }
+
+            raw.AppendLine();
+            return raw;
+        }
+
+        internal static bool IsJson(string contentType)
+        {
+            contentType = GetContentTypeSignature(contentType);
+            return CheckIsJson(contentType);
+        }
+
+        internal static bool IsText(string contentType)
+        {
+            contentType = GetContentTypeSignature(contentType);
+            return CheckIsText(contentType);
+        }
+
+        internal static bool IsXml(string contentType)
+        {
+            contentType = GetContentTypeSignature(contentType);
+            return CheckIsXml(contentType);
         }
 
         #endregion Internal Methods
 
         #region Private Helper Methods
 
-        private static string GetContentTypeSignature(string contentType)
+        private static bool CheckIsJson(string contentType)
         {
             if (String.IsNullOrEmpty(contentType))
-                return null;
+                return false;
 
-            string sig = contentType.Split(s_contentTypeParamSeparator, 2)[0].ToUpperInvariant();
-            return (sig);
+            // the correct type for JSON content, as specified in RFC 4627
+            bool isJson = contentType.Equals("application/json", StringComparison.OrdinalIgnoreCase);
+
+            // add in these other "javascript" related types that
+            // sometimes get sent down as the mime type for JSON content
+            isJson |= contentType.Equals("text/json", StringComparison.OrdinalIgnoreCase)
+            || contentType.Equals("application/x-javascript", StringComparison.OrdinalIgnoreCase)
+            || contentType.Equals("text/x-javascript", StringComparison.OrdinalIgnoreCase)
+            || contentType.Equals("application/javascript", StringComparison.OrdinalIgnoreCase)
+            || contentType.Equals("text/javascript", StringComparison.OrdinalIgnoreCase);
+
+            return (isJson);
         }
 
         private static bool CheckIsText(string contentType)
@@ -132,85 +201,15 @@ namespace Microsoft.PowerShell.Commands
             return (isXml);
         }
 
-        private static bool CheckIsJson(string contentType)
+        private static string GetContentTypeSignature(string contentType)
         {
             if (String.IsNullOrEmpty(contentType))
-                return false;
+                return null;
 
-            // the correct type for JSON content, as specified in RFC 4627
-            bool isJson = contentType.Equals("application/json", StringComparison.OrdinalIgnoreCase);
-
-            // add in these other "javascript" related types that
-            // sometimes get sent down as the mime type for JSON content
-            isJson |= contentType.Equals("text/json", StringComparison.OrdinalIgnoreCase)
-            || contentType.Equals("application/x-javascript", StringComparison.OrdinalIgnoreCase)
-            || contentType.Equals("text/x-javascript", StringComparison.OrdinalIgnoreCase)
-            || contentType.Equals("application/javascript", StringComparison.OrdinalIgnoreCase)
-            || contentType.Equals("text/javascript", StringComparison.OrdinalIgnoreCase);
-
-            return (isJson);
+            string sig = contentType.Split(s_contentTypeParamSeparator, 2)[0].ToUpperInvariant();
+            return (sig);
         }
 
-        #endregion Internal Helper Methods
-    }
-
-    // TODO: merge Partials
-
-    internal static partial class ContentHelper
-    {
-        internal static Encoding GetEncoding(HttpResponseMessage response)
-        {
-            // ContentType may not exist in response header.
-            string charSet = response.Content.Headers.ContentType?.CharSet;
-            return GetEncodingOrDefault(charSet);
-        }
-
-        internal static string GetContentType(HttpResponseMessage response)
-        {
-            // ContentType may not exist in response header.  Return null if not.
-            return response.Content.Headers.ContentType?.MediaType;
-        }
-
-        internal static StringBuilder GetRawContentHeader(HttpResponseMessage response)
-        {
-            StringBuilder raw = new StringBuilder();
-
-            string protocol = WebResponseHelper.GetProtocol(response);
-            if (!string.IsNullOrEmpty(protocol))
-            {
-                int statusCode = WebResponseHelper.GetStatusCode(response);
-                string statusDescription = WebResponseHelper.GetStatusDescription(response);
-                raw.AppendFormat("{0} {1} {2}", protocol, statusCode, statusDescription);
-                raw.AppendLine();
-            }
-
-            HttpHeaders[] headerCollections =
-            {
-                response.Headers,
-                response.Content == null ? null : response.Content.Headers
-            };
-
-            foreach (var headerCollection in headerCollections)
-            {
-                if (headerCollection == null)
-                {
-                    continue;
-                }
-                foreach (var header in headerCollection)
-                {
-                    // Headers may have multiple entries with different values
-                    foreach (var headerValue in header.Value)
-                    {
-                        raw.Append(header.Key);
-                        raw.Append(": ");
-                        raw.Append(headerValue);
-                        raw.AppendLine();
-                    }
-                }
-            }
-
-            raw.AppendLine();
-            return raw;
-        }
+        #endregion Private Helper Methods
     }
 }
