73a25e249b7072b97edc69fe91a2cbdef61e407b
Dan Travison <dantra@ntdev.microsoft.com>
2018-01-02 16:51:04 -0800

    [FEATURE] Use the latest commit

--- build.psm1
+++ build.psm1
@@ -673,7 +673,7 @@ function Restore-PSPester
         $Destination = ([IO.Path]::Combine((Split-Path (Get-PSOptions -DefaultToNew).Output), "Modules"))
     )
 
-    Restore-GitModule -Destination $Destination -Uri 'https://github.com/PowerShell/psl-pester' -Name Pester -CommitSha '87f531db89a91db86736ac11b4e565ad10c3e334'
+    Restore-GitModule -Destination $Destination -Uri 'https://github.com/PowerShell/psl-pester' -Name Pester -CommitSha '1f546b6aaa0893e215e940a14f57c96f56f7eff1'
 }
 function Compress-TestContent {
     [CmdletBinding()]
