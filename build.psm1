dea2e36d390bc0171413ae376a425c9772530e3b
Chris <c.bergmeister@gmail.com>
2018-01-17 07:29:18 +0000

    Update recommended WiX toolset link to be generic to WiX 3.x but mention that the latest version of 3.11 has to be taken.

--- build.psm1
+++ build.psm1
@@ -2109,7 +2109,7 @@ function New-MSIPackage
     Write-Verbose "Ensure Wix Toolset is present on the machine @ $wixToolsetBinPath"
     if (-not (Test-Path $wixToolsetBinPath))
     {
-        throw "Wix Toolset is required to create MSI package. Please install it from https://github.com/wixtoolset/wix3/releases/download/wix311rtm/wix311.exe"
+        throw "Wix Toolset 3.11 is required to create MSI package. Please install it from https://github.com/wixtoolset/wix3/releases"
     }
 
     ## Get the latest if multiple versions exist.
