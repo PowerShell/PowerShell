fdd7e2b52c92fd70011f1cc7bc31736d811152a1
stknohg <stknohg@gmail.com>
2018-01-24 16:49:56 +0900

    Change Microsoft.PowerShell.Commands.SetDateCommand.SystemTime to struct.

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
@@ -10,6 +10,12 @@ Describe "Set-Date" -Tag "CI" {
         { get-date | set-date } | Should not throw
     }
 
+    It "Set-Date should be able to set the date with -Date parameter" -Skip:(! $IsElevated) {
+        $target = Get-Date
+        $expected = $target
+        Set-Date -Date $target | Should be $expected
+    }
+
     It "Set-Date should produce an error in a non-elevated context" -Skip:($IsElevated) {
         { get-date | set-date} | should throw
         $Error[0].FullyQualifiedErrorId | should be "System.ComponentModel.Win32Exception,Microsoft.PowerShell.Commands.SetDateCommand"
