7ea25512b9b14227ecf6b226c16a866b21bfd405
stknohg <stknohg@gmail.com>
2018-01-24 19:08:36 +0900

    fix casing of cmdlet, pester operators.

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
@@ -7,17 +7,17 @@ Describe "Set-Date" -Tag "CI" {
     }
 
     It "Set-Date should be able to set the date in an elevated context" -Skip:(! $IsElevated) {
-        { get-date | set-date } | Should not throw
+        { Get-Date | Set-Date } | Should Not Throw
     }
 
     It "Set-Date should be able to set the date with -Date parameter" -Skip:(! $IsElevated) {
         $target = Get-Date
         $expected = $target
-        Set-Date -Date $target | Should be $expected
+        Set-Date -Date $target | Should Be $expected
     }
 
     It "Set-Date should produce an error in a non-elevated context" -Skip:($IsElevated) {
-        { get-date | set-date} | should throw
-        $Error[0].FullyQualifiedErrorId | should be "System.ComponentModel.Win32Exception,Microsoft.PowerShell.Commands.SetDateCommand"
+        { Get-Date | Set-Date } | Should Throw
+        $Error[0].FullyQualifiedErrorId | Should Be "System.ComponentModel.Win32Exception,Microsoft.PowerShell.Commands.SetDateCommand"
     }
 }
