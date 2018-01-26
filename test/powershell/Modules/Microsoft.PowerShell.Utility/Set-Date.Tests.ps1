ff9a0dde9d655efbe1460e0fab8e3f5dcf7ddee2
stknohg <stknohg@gmail.com>
2018-01-27 08:30:13 +0900

    Fix incorrect condition of user privilege in Linux/macOS CI

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
@@ -1,9 +1,10 @@
 Describe "Set-Date for admin" -Tag @('CI', 'RequireAdminOnWindows') {
-    It "Set-Date should be able to set the date in an elevated context" {
+    # Currently, CI tests on Linux/macOS are always run as normal user. So we need to skip these tests on non-Windows platform.
+    It "Set-Date should be able to set the date in an elevated context" -Skip:(!$IsWindows) {
         { Get-Date | Set-Date } | Should Not Throw
     }
 
-    It "Set-Date should be able to set the date with -Date parameter" {
+    It "Set-Date should be able to set the date with -Date parameter" -Skip:(!$IsWindows) {
         $target = Get-Date
         $expected = $target
         Set-Date -Date $target | Should Be $expected
@@ -11,8 +12,7 @@ Describe "Set-Date for admin" -Tag @('CI', 'RequireAdminOnWindows') {
 }
 
 Describe "Set-Date" -Tag 'CI' {
-    # Currently CI tests on Linux/macOS are always run as sudo, so we need to skip this test on non-Windows platform.
-    It "Set-Date should produce an error in a non-elevated context" -Skip:(!$IsWindows) {
+    It "Set-Date should produce an error in a non-elevated context" {
         { Get-Date | Set-Date } | ShouldBeErrorId "System.ComponentModel.Win32Exception,Microsoft.PowerShell.Commands.SetDateCommand"
     }
 }
