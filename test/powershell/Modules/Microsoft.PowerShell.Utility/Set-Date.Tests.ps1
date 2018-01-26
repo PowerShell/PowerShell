df3371990529ae64f481760f1edb7ccdbb89d9d7
stknohg <stknohg@gmail.com>
2018-01-26 11:10:22 +0900

    Use 'RequireAdminOnWindows' tag instead of 'Test-IsElevated' function.

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
@@ -1,22 +1,18 @@
-# first check to see which platform we're on. If we're on windows we should be able
-# to be sure whether we're running elevated. If we're on Linux, we can use whoami to
-# determine whether we're elevated
-Describe "Set-Date" -Tag "CI" {
-    BeforeAll {
-        $IsElevated = Test-IsElevated
-    }
-
-    It "Set-Date should be able to set the date in an elevated context" -Skip:(! $IsElevated) {
+Describe "Set-Date for admin" -Tag @('CI', 'RequireAdminOnWindows') {
+    It "Set-Date should be able to set the date in an elevated context" {
         { Get-Date | Set-Date } | Should Not Throw
     }
 
-    It "Set-Date should be able to set the date with -Date parameter" -Skip:(! $IsElevated) {
+    It "Set-Date should be able to set the date with -Date parameter" {
         $target = Get-Date
         $expected = $target
         Set-Date -Date $target | Should Be $expected
     }
+}
 
-    It "Set-Date should produce an error in a non-elevated context" -Skip:($IsElevated) {
+Describe "Set-Date" -Tag 'CI' {
+    # Currently CI tests on Linux/macOS are always run as sudo, so we need to skip this test on non-Windows platform.
+    It "Set-Date should produce an error in a non-elevated context" -Skip:(!$IsWindows) {
         { Get-Date | Set-Date } | ShouldBeErrorId "System.ComponentModel.Win32Exception,Microsoft.PowerShell.Commands.SetDateCommand"
     }
 }
