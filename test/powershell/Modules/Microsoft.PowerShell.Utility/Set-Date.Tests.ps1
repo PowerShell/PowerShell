9d328a296aea505f4515d04b77c349a861ad6423
stknohg <stknohg@gmail.com>
2018-01-29 15:35:54 +0900

    Add link to #5645

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
@@ -1,5 +1,7 @@
 Describe "Set-Date for admin" -Tag @('CI', 'RequireAdminOnWindows') {
     # Currently, CI tests on Linux/macOS are always run as normal user. So we need to skip these tests on non-Windows platform.
+    # CI tests in root privilege on Linux/macOS is not supported.
+    # See : https://github.com/PowerShell/PowerShell/issues/5645
     It "Set-Date should be able to set the date in an elevated context" -Skip:(!$IsWindows) {
         { Get-Date | Set-Date } | Should Not Throw
     }
