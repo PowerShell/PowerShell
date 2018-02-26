22b1f11de1f3a12a37818262ba63e73eb3ceaf07
Travis Plunk <travis.plunk@microsoft.com>
2018-02-26 15:45:15 -0800

    fix MSI creation errors, and capture wixpdb for later patch creation (#6221)
    - add `wixpdb` output when creating `MSI` package
    - capture `wixpdb` in official build
    - clean up anything left behind from previous MSI builds before starting MSI build to prevent using dirty files.
    - make sure MSI creation fails if there is an error
    - ignore `.wixpdb` files in git
    - Add functionality to `Start-NativeExecution` to
        - only display output if there is an error
        - log caller information
    - WXS validation error fixes
        - Remove unused `ExitDialog` to fix ICE82
        - Add KeyPath to `SetPath` to fix ICE18
        - Use `HKMU` which translates to `HKLM` to runtime to fix various validation errors about creating the shortcut
    - Suppress Validation errors
        - suppress ICE61, which is about same version upgrades being allowed
        - suppress ICE57, caused by the shortcut not being installed per user
    # Conflicts:
    #	assets/Product.wxs

--- build.psm1
+++ build.psm1
@@ -2009,15 +2009,50 @@ function script:precheck([string]$command, [string]$missedMessage) {
 
 # this function wraps native command Execution
 # for more information, read https://mnaoumov.wordpress.com/2015/01/11/execution-of-external-commands-in-powershell-done-right/
-function script:Start-NativeExecution([scriptblock]$sb, [switch]$IgnoreExitcode)
+function script:Start-NativeExecution
 {
+    param(
+        [scriptblock]$sb,
+        [switch]$IgnoreExitcode,
+        [switch]$VerboseOutputOnError
+    )
     $backupEAP = $script:ErrorActionPreference
     $script:ErrorActionPreference = "Continue"
     try {
-        & $sb
+        if($VerboseOutputOnError.IsPresent)
+        {
+            $output = & $sb
+        }
+        else
+        {
+            & $sb
+        }
+
         # note, if $sb doesn't have a native invocation, $LASTEXITCODE will
         # point to the obsolete value
         if ($LASTEXITCODE -ne 0 -and -not $IgnoreExitcode) {
+            if($VerboseOutputOnError.IsPresent -and $output)
+            {
+                $output | Out-String | Write-Verbose -Verbose
+            }
+
+            # Get caller location for easier debugging
+            $caller = Get-PSCallStack -ErrorAction SilentlyContinue
+            if($caller)
+            {
+                $callerLocationParts = $caller[1].Location -split ":\s*line\s*"
+                $callerFile = $callerLocationParts[0]
+                $callerLine = $callerLocationParts[1]
+
+                $errorMessage = "Execution of {$sb} by ${callerFile}: line $callerLine failed with exit code $LASTEXITCODE"
+
+                if ($null -ne $env:CI)
+                {
+                   Add-AppveyorCompilationMessage $errorMessage -Category Error -FileName $callerFile -Line $callerLine
+                }
+
+                throw $errorMessage
+            }
             throw "Execution of {$sb} failed with exit code $LASTEXITCODE"
         }
     } finally {
--- tools/packaging/packaging.psm1
+++ tools/packaging/packaging.psm1
@@ -1540,36 +1540,49 @@ function New-MSIPackage
     $wixObjProductPath = Join-Path $env:Temp "Product.wixobj"
     $wixObjFragmentPath = Join-Path $env:Temp "Fragment.wixobj"
 
+    # cleanup any garbage on the system
+    Remove-Item -ErrorAction SilentlyContinue $wixFragmentPath -Force
+    Remove-Item -ErrorAction SilentlyContinue $wixObjProductPath -Force
+    Remove-Item -ErrorAction SilentlyContinue $wixObjFragmentPath -Force
+
     $packageName = $productSemanticVersionWithName
     if ($ProductNameSuffix) {
         $packageName += "-$ProductNameSuffix"
     }
     $msiLocationPath = Join-Path $pwd "$packageName.msi"
+    $msiPdbLocationPath = Join-Path $pwd "$packageName.wixpdb"
 
     if(!$Force.IsPresent -and (Test-Path -Path $msiLocationPath))
     {
         Write-Error -Message "Package already exists, use -Force to overwrite, path:  $msiLocationPath" -ErrorAction Stop
     }
 
-    $WiXHeatLog = & $wixHeatExePath dir  $ProductSourcePath -dr  $productVersionWithName -cg $productVersionWithName -gg -sfrag -srd -scom -sreg -out $wixFragmentPath -var env.ProductSourcePath -v
-    $WiXCandleLog = & $wixCandleExePath  "$ProductWxsPath"  "$wixFragmentPath" -out (Join-Path "$env:Temp" "\\") -ext WixUIExtension -ext WixUtilExtension -arch $ProductTargetArchitecture -v
-    $WiXLightLog = & $wixLightExePath -out $msiLocationPath $wixObjProductPath $wixObjFragmentPath -ext WixUIExtension -ext WixUtilExtension -dWixUILicenseRtf="$LicenseFilePath" -v
+    log "running heat..."
+    Start-NativeExecution -VerboseOutputOnError { & $wixHeatExePath dir  $ProductSourcePath -dr  $productVersionWithName -cg $productVersionWithName -gg -sfrag -srd -scom -sreg -out $wixFragmentPath -var env.ProductSourcePath -v}
 
-    Remove-Item -ErrorAction SilentlyContinue *.wixpdb -Force
+    log "running candle..."
+    Start-NativeExecution -VerboseOutputOnError { & $wixCandleExePath  "$ProductWxsPath"  "$wixFragmentPath" -out (Join-Path "$env:Temp" "\\") -ext WixUIExtension -ext WixUtilExtension -arch $ProductTargetArchitecture -v}
+
+    log "running light..."
+    # suppress ICE61, because we allow same version upgrades
+    # suppress ICE57, this suppresses an error caused by our shortcut not being installed per user
+    Start-NativeExecution -VerboseOutputOnError {& $wixLightExePath -sice:ICE61 -sice:ICE57 -out $msiLocationPath -pdbout $msiPdbLocationPath $wixObjProductPath $wixObjFragmentPath -ext WixUIExtension -ext WixUtilExtension -dWixUILicenseRtf="$LicenseFilePath"}
+
     Remove-Item -ErrorAction SilentlyContinue $wixFragmentPath -Force
     Remove-Item -ErrorAction SilentlyContinue $wixObjProductPath -Force
     Remove-Item -ErrorAction SilentlyContinue $wixObjFragmentPath -Force
 
-    if (Test-Path $msiLocationPath)
+    if ((Test-Path $msiLocationPath) -and (Test-Path $msiPdbLocationPath))
     {
+        Write-Verbose "You can find the WixPdb @ $msiPdbLocationPath" -Verbose
         Write-Verbose "You can find the MSI @ $msiLocationPath" -Verbose
-        $msiLocationPath
+        [pscustomobject]@{
+            msi=$msiLocationPath
+            wixpdb=$msiPdbLocationPath
+        }
     }
     else
     {
-        $WiXHeatLog   | Out-String | Write-Verbose -Verbose
-        $WiXCandleLog | Out-String | Write-Verbose -Verbose
-        $WiXLightLog  | Out-String | Write-Verbose -Verbose
         $errorMessage = "Failed to create $msiLocationPath"
         if ($null -ne $env:CI)
         {
