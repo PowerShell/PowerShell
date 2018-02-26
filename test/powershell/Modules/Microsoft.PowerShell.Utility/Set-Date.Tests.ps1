858dff1d7562175df5344ece994b6ab7b1278334
Steve Lee <slee@microsoft.com>
2018-02-25 19:47:10 -0800

    [feature]
    mark Set-Date tests as failing due to #6227

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
@@ -1,11 +1,11 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 Describe "Set-Date for admin" -Tag @('CI', 'RequireAdminOnWindows', 'RequireSudoOnUnix') {
-    It "Set-Date should be able to set the date in an elevated context" {
+    It "Set-Date should be able to set the date in an elevated context" -Pending {
         { Get-Date | Set-Date } | Should Not Throw
     }
 
-    It "Set-Date should be able to set the date with -Date parameter" {
+    It "Set-Date should be able to set the date with -Date parameter" -Pending {
         $target = Get-Date
         $expected = $target
         Set-Date -Date $target | Should Be $expected
