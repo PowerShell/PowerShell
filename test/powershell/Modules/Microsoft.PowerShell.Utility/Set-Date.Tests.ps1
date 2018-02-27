00c746ba57a41429c2d461d3c6ed03942cbbd210
Steve Lee <slee@microsoft.com>
2018-02-26 16:07:07 -0800

    revert changes in set-date.tests.ps1 and travis.ps1

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
@@ -1,6 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-
 Describe "Set-Date for admin" -Tag @('CI', 'RequireAdminOnWindows', 'RequireSudoOnUnix') {
     It "Set-Date should be able to set the date in an elevated context" -Pending {
         { Get-Date | Set-Date } | Should Not Throw
@@ -13,7 +12,7 @@ Describe "Set-Date for admin" -Tag @('CI', 'RequireAdminOnWindows', 'RequireSudo
     }
 }
 
-Describe "Set-Date" -Tag @('CI') {
+Describe "Set-Date" -Tag 'CI' {
     It "Set-Date should produce an error in a non-elevated context" {
         { Get-Date | Set-Date } | ShouldBeErrorId "System.ComponentModel.Win32Exception,Microsoft.PowerShell.Commands.SetDateCommand"
     }
