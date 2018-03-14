27451163b93dd69fba9fee265a7a6f6e69c9e9a5
Klaudia Algiz <klalgiz@microsoft.com>
2018-03-13 17:04:54 -0700

    [Feature]

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Remove-Variable.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Remove-Variable.Tests.ps1
@@ -194,14 +194,30 @@ Describe "Remove-Variable basic functionality" -Tags "CI" {
 
 	It "Remove-Variable Constant variable should throw SessionStateUnauthorizedAccessException"{
 		New-Variable foo bar -Option Constant
-		$e = { Remove-Variable foo -EA Stop } | Should -Throw -ErrorId "VariableNotRemovable,Microsoft.PowerShell.Commands.RemoveVariableCommand"
-		$e.CategoryInfo | Should -Match "SessionStateUnauthorizedAccessException"
+		try
+		{
+			Remove-Variable foo -EA Stop
+			Throw "Execution OK"
+		}
+		catch
+		{
+			$_.CategoryInfo | Should -Match "SessionStateUnauthorizedAccessException"
+			$_.FullyQualifiedErrorId | Should -Be "VariableNotRemovable,Microsoft.PowerShell.Commands.RemoveVariableCommand"
+		}
 	}
 
 	It "Remove-Variable ReadOnly variable should throw SessionStateUnauthorizedAccessException and force remove should work"{
 		New-Variable foo bar -Option ReadOnly
-		{ Remove-Variable foo -EA Stop } | Should -Throw -ErrorId "VariableNotRemovable,Microsoft.PowerShell.Commands.RemoveVariableCommand"
-		$e.CategoryInfo | Should -Match "SessionStateUnauthorizedAccessException"
+		try
+		{
+			Remove-Variable foo -EA Stop
+			Throw "Execution OK"
+		}
+		catch
+		{
+			$_.CategoryInfo| Should -Match "SessionStateUnauthorizedAccessException"
+			$_.FullyQualifiedErrorId | Should -Be "VariableNotRemovable,Microsoft.PowerShell.Commands.RemoveVariableCommand"
+		}
 		Remove-Variable foo -Force
 		$var1 = Get-Variable -Name foo -EA SilentlyContinue
 		$var1 | Should -BeNullOrEmpty
@@ -209,12 +225,27 @@ Describe "Remove-Variable basic functionality" -Tags "CI" {
 
 	It "Remove-Variable Constant variable should throw SessionStateUnauthorizedAccessException and force remove should also throw exception"{
 		New-Variable foo bar -Option Constant
-		$e = { Remove-Variable foo -EA Stop } | Should -Throw -ErrorId "VariableNotRemovable,Microsoft.PowerShell.Commands.RemoveVariableCommand"
-		$e.CategoryInfo | Should -Match "SessionStateUnauthorizedAccessException"
+		try
+		{
+			Remove-Variable foo -EA Stop
+			Throw "Execution OK"
+		}
+		catch
+		{
+			$_.CategoryInfo | Should -Match "SessionStateUnauthorizedAccessException"
+			$_.FullyQualifiedErrorId | Should -Be "VariableNotRemovable,Microsoft.PowerShell.Commands.RemoveVariableCommand"
+		}
 
-		$e = { Remove-Variable foo -Force -EA Stop } | Should -Throw -ErrorId "VariableNotRemovable,Microsoft.PowerShell.Commands.RemoveVariableCommand"
-		$e.CategoryInfo | Should -Match "SessionStateUnauthorizedAccessException"
-
+		try
+		{
+			Remove-Variable foo -Force -EA Stop
+			Throw "Execution OK"
+		}
+		catch
+		{
+			$_.CategoryInfo | Should -Match "SessionStateUnauthorizedAccessException"
+			$_.FullyQualifiedErrorId | Should -Be "VariableNotRemovable,Microsoft.PowerShell.Commands.RemoveVariableCommand"
+		}
 	}
 
 	It "Remove-Variable variable in new scope should works and Get-Variable with different scope should have different result"{
@@ -222,15 +253,22 @@ Describe "Remove-Variable basic functionality" -Tags "CI" {
 		&{
 			Clear-Variable foo
 			Remove-Variable foo
-			$e = { Get-Variable -Name foo -Scope local -EA Stop } | Should -Throw -ErrorId "VariableNotFound,Microsoft.PowerShell.Commands.GetVariableCommand"
-			$e.CategoryInfo | Should -Match "ItemNotFoundException"
+			try{
+				Get-Variable -Name foo -Scope local -EA Stop
+				Throw "Execution OK"
+			}
+			catch
+			{
+				$_.CategoryInfo | Should -Match "ItemNotFoundException"
+				$_.FullyQualifiedErrorId | Should -Be "VariableNotFound,Microsoft.PowerShell.Commands.GetVariableCommand"
+			}
 		}
 
 		$var1 = Get-Variable -Name foo
 		$var1.Name | Should -Be "foo"
 		$var1.Value | Should -Be "bar"
 		$var1.Options | Should -Be "None"
-		$var1.Description | Should -BeNullOrEmpty
+		$var1.Description | Should -Be ""
 
 	}
 }
