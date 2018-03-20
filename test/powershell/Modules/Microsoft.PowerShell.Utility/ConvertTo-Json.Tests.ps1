6c8a6ae4ba7c460048bb18b814abf118f40bb060
kalgiz <klalgiz@microsoft.com>
2018-03-20 09:29:22 -0700

    Add -AsArray parameter to ConvertoTo-Json command to always pack the output string in array symbols.

--- test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Json.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Json.Tests.ps1
@@ -35,4 +35,17 @@ Describe 'ConvertTo-Json' -tags "CI" {
         $ps.InvocationStateInfo.State | Should -BeExactly "Stopped"
         $ps.Dispose()
     }
+
+    It "The result string is packed in an array symbols when AsArray parameter is used." {
+        $output = ConvertTo-Json -InputObject 1 -AsArray
+        $output | Should -BeLike "``[*1*]"
+
+        $output = ConvertTo-Json -InputObject 1,2 -AsArray
+        $output | Should -BeLike "``[*1*2*]"
+    }
+
+    It "The result string is not packed in the array symbols when there is only one input object and AsArray parameter is not used." {
+        $output = ConvertTo-Json -InputObject 1
+        $output | Should -Be '1'
+    }
 }
