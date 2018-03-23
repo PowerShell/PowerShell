e86fea6acd508297646e307ffeae2340d0bafaa8
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-03-23 17:48:26 +0000

    Upgrade tests in test\powershell\Host folder to PesterV4 syntax (#6250)

--- test/powershell/Host/PSVersionTable.Tests.ps1
+++ test/powershell/Host/PSVersionTable.Tests.ps1
@@ -25,51 +25,51 @@ Describe "PSVersionTable" -Tags "CI" {
     }
 
     It "Should have version table entries" {
-       $PSVersionTable.Count | Should Be 9
+       $PSVersionTable.Count | Should -Be 9
     }
 
     It "Should have the right version table entries" {
-       $PSVersionTable.ContainsKey("PSVersion")                 | Should Be True
-       $PSVersionTable.ContainsKey("PSEdition")                 | Should Be True
-       $PSVersionTable.ContainsKey("WSManStackVersion")         | Should Be True
-       $PSVersionTable.ContainsKey("SerializationVersion")      | Should Be True
-       $PSVersionTable.ContainsKey("PSCompatibleVersions")      | Should Be True
-       $PSVersionTable.ContainsKey("PSRemotingProtocolVersion") | Should Be True
-       $PSVersionTable.ContainsKey("GitCommitId")               | Should Be True
-       $PSVersionTable.ContainsKey("Platform")                  | Should Be True
-       $PSVersionTable.ContainsKey("OS")                        | Should Be True
+       $PSVersionTable.ContainsKey("PSVersion")                 | Should -BeTrue
+       $PSVersionTable.ContainsKey("PSEdition")                 | Should -BeTrue
+       $PSVersionTable.ContainsKey("WSManStackVersion")         | Should -BeTrue
+       $PSVersionTable.ContainsKey("SerializationVersion")      | Should -BeTrue
+       $PSVersionTable.ContainsKey("PSCompatibleVersions")      | Should -BeTrue
+       $PSVersionTable.ContainsKey("PSRemotingProtocolVersion") | Should -BeTrue
+       $PSVersionTable.ContainsKey("GitCommitId")               | Should -BeTrue
+       $PSVersionTable.ContainsKey("Platform")                  | Should -BeTrue
+       $PSVersionTable.ContainsKey("OS")                        | Should -BeTrue
 
     }
 
     It "PSVersion property" {
-       $PSVersionTable.PSVersion | Should BeOfType "System.Management.Automation.SemanticVersion"
-       $PSVersionTable.PSVersion | Should BeExactly $expectedPSVersion
-       $PSVersionTable.PSVersion | Should Match $expectedVersionPattern
-       $PSVersionTable.PSVersion.Major | Should Be 6
+       $PSVersionTable.PSVersion | Should -BeOfType "System.Management.Automation.SemanticVersion"
+       $PSVersionTable.PSVersion | Should -BeExactly $expectedPSVersion
+       $PSVersionTable.PSVersion | Should -Match $expectedVersionPattern
+       $PSVersionTable.PSVersion.Major | Should -Be 6
     }
 
     It "GitCommitId property" {
-       $PSVersionTable.GitCommitId | Should BeOfType "System.String"
-       $PSVersionTable.GitCommitId | Should Match $expectedGitCommitIdPattern
-       $PSVersionTable.GitCommitId | Should Not Match $unexpectectGitCommitIdPattern
-       $PSVersionTable.GitCommitId | Should BeExactly $rawGitCommitId
+       $PSVersionTable.GitCommitId | Should -BeOfType "System.String"
+       $PSVersionTable.GitCommitId | Should -Match $expectedGitCommitIdPattern
+       $PSVersionTable.GitCommitId | Should -Not -Match $unexpectectGitCommitIdPattern
+       $PSVersionTable.GitCommitId | Should -BeExactly $rawGitCommitId
     }
 
     It "Should have the correct platform info" {
        $platform = [String][System.Environment]::OSVersion.Platform
-       [String]$PSVersionTable["Platform"] | Should Be $platform
+       [String]$PSVersionTable["Platform"] | Should -Be $platform
     }
 
     It "Should have the correct OS info" {
        if ($IsCoreCLR)
        {
            $OSDescription = [String][System.Runtime.InteropServices.RuntimeInformation]::OSDescription
-           [String]$PSVersionTable["OS"] | Should Be $OSDescription
+           [String]$PSVersionTable["OS"] | Should -Be $OSDescription
        }
        else
        {
            $OSDescription = [String][System.Environment]::OSVersion
-           [String]$PSVersionTable["OS"] | Should Be $OSDescription
+           [String]$PSVersionTable["OS"] | Should -Be $OSDescription
        }
     }
 
@@ -81,27 +81,27 @@ Describe "PSVersionTable" -Tags "CI" {
         {
             $edition = "Desktop"
         }
-        $PSVersionTable["PSEdition"] | Should Be $edition
+        $PSVersionTable["PSEdition"] | Should -Be $edition
     }
 
     It "Verify `$PSVersionTable is ordered and 'PSVersion' is on first place" {
         $PSVersionName = "PSVersion"
         $keys1 = ($PSVersionTable | Format-Table -HideTableHeaders -Property Name | Out-String) -split [System.Environment]::NewLine | Where-Object {$_} | ForEach-Object {$_.Trim()}
 
-        $keys1[0] | Should Be "PSVersion"
-        $keys1[1] | Should Be "PSEdition"
+        $keys1[0] | Should -Be "PSVersion"
+        $keys1[1] | Should -Be "PSEdition"
 
         $keys1last = $keys1[2..($keys1.length-1)]
         $keys1sortedlast = $keys1last | Sort-Object
 
-        Compare-Object -ReferenceObject $keys1last -DifferenceObject $keys1sortedlast -SyncWindow 0 | Should Be $null
+        Compare-Object -ReferenceObject $keys1last -DifferenceObject $keys1sortedlast -SyncWindow 0 | Should -Be $null
     }
 
     It "Verify `$PSVersionTable can be formatted correctly when it has non-string key" {
         try {
             $key = Get-Item $PSScriptRoot
             $PSVersionTable.Add($key, "TEST")
-            { $PSVersionTable | Format-Table } | Should Not Throw
+            { $PSVersionTable | Format-Table } | Should -Not -Throw
         } finally {
             $PSVersionTable.Remove($key)
         }
@@ -113,12 +113,12 @@ Describe "PSVersionTable" -Tags "CI" {
             $PSVersionTable.Remove("PSVersion")
 
             $keys1 = ($PSVersionTable | Format-Table -HideTableHeaders -Property Name | Out-String) -split [System.Environment]::NewLine | Where-Object {$_} | ForEach-Object {$_.Trim()}
-            $keys1[0] | Should Be "PSEdition"
-            $keys1.Length | Should Be $PSVersionTable.Count
+            $keys1[0] | Should -Be "PSEdition"
+            $keys1.Length | Should -Be $PSVersionTable.Count
 
             $keys1last = $keys1[1..($keys1.length-1)]
             $keys1sortedlast = $keys1last | Sort-Object
-            Compare-Object -ReferenceObject $keys1last -DifferenceObject $keys1sortedlast -SyncWindow 0 | Should Be $null
+            Compare-Object -ReferenceObject $keys1last -DifferenceObject $keys1sortedlast -SyncWindow 0 | Should -Be $null
         } finally {
             $PSVersionTable.Add("PSVersion", $VersionValue)
         }
@@ -130,12 +130,12 @@ Describe "PSVersionTable" -Tags "CI" {
             $PSVersionTable.Remove("PSEdition")
 
             $keys1 = ($PSVersionTable | Format-Table -HideTableHeaders -Property Name | Out-String) -split [System.Environment]::NewLine | Where-Object {$_} | ForEach-Object {$_.Trim()}
-            $keys1[0] | Should Be "PSVersion"
-            $keys1.Length | Should Be $PSVersionTable.Count
+            $keys1[0] | Should -Be "PSVersion"
+            $keys1.Length | Should -Be $PSVersionTable.Count
 
             $keys1last = $keys1[1..($keys1.length-1)]
             $keys1sortedlast = $keys1last | Sort-Object
-            Compare-Object -ReferenceObject $keys1last -DifferenceObject $keys1sortedlast -SyncWindow 0 | Should Be $null
+            Compare-Object -ReferenceObject $keys1last -DifferenceObject $keys1sortedlast -SyncWindow 0 | Should -Be $null
         } finally {
             $PSVersionTable.Add("PSEdition", $EditionValue)
         }
@@ -149,10 +149,10 @@ Describe "PSVersionTable" -Tags "CI" {
             $PSVersionTable.Remove("PSEdition")
 
             $keys1 = ($PSVersionTable | Format-Table -HideTableHeaders -Property Name | Out-String) -split [System.Environment]::NewLine | Where-Object {$_} | ForEach-Object {$_.Trim()}
-            $keys1.Length | Should Be $PSVersionTable.Count
+            $keys1.Length | Should -Be $PSVersionTable.Count
 
             $keys1sortedlast = $keys1 | Sort-Object
-            Compare-Object -ReferenceObject $keys1 -DifferenceObject $keys1sortedlast -SyncWindow 0 | Should Be $null
+            Compare-Object -ReferenceObject $keys1 -DifferenceObject $keys1sortedlast -SyncWindow 0 | Should -Be $null
         } finally {
             $PSVersionTable.Add("PSVersion", $VersionValue)
             $PSVersionTable.Add("PSEdition", $EditionValue)
