f0b6ef2eb470c0866f21768547f34af4670467fd
Klaudia Algiz <algizklaudia@gmail.com>
2018-04-03 10:58:06 -0700

    Terminate the loop in PsUtils.GetMainModule() which was infinite in case there was no main module. (#6358)
    * Terminate the loop in GetMainModule if main module is null.
    
    * Run tests with FileVersionInfo also on non-windows platforms. Add test for process which main module can be null.

--- src/System.Management.Automation/utils/PsUtils.cs
+++ src/System.Management.Automation/utils/PsUtils.cs
@@ -59,13 +59,12 @@ namespace System.Management.Automation
         internal static ProcessModule GetMainModule(Process targetProcess)
         {
             int caughtCount = 0;
-            ProcessModule mainModule = null;
 
-            while (mainModule == null)
+            while (true)
             {
                 try
                 {
-                    mainModule = targetProcess.MainModule;
+                    return targetProcess.MainModule;
                 }
                 catch (System.ComponentModel.Win32Exception e)
                 {
@@ -81,8 +80,6 @@ namespace System.Management.Automation
                         throw;
                 }
             }
-
-            return mainModule;
         }
 
         // Cache of the current process' parentId
