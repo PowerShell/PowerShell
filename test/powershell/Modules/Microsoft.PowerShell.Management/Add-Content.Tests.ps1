52964aa05cc37a7231acacb4478e5bf98849b321
sethvs <sethvs@users.noreply.github.com>
2018-04-08 12:17:27 +0300

    Fix formatting.

--- test/powershell/Modules/Microsoft.PowerShell.Management/Add-Content.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Add-Content.Tests.ps1
@@ -6,25 +6,25 @@ Describe "Add-Content cmdlet tests" -Tags "CI" {
 
   Context "Add-Content should actually add content" {
     It "should Add-Content to testdrive:\$file1" {
-      $result=add-content -path testdrive:\$file1 -value "ExpectedContent" -passthru
-      $result| Should -BeExactly "ExpectedContent"
+      $result = Add-Content -Path testdrive:\$file1 -Value "ExpectedContent" -PassThru
+      $result | Should -BeExactly "ExpectedContent"
     }
     It "should return expected string from testdrive:\$file1" {
-      $result = get-content -path testdrive:\$file1
+      $result = Get-Content -Path testdrive:\$file1
       $result | Should -BeExactly "ExpectedContent"
     }
     It "should Add-Content to testdrive:\dynamicfile.txt with dynamic parameters" -Pending:($IsLinux -Or $IsMacOS) {#https://github.com/PowerShell/PowerShell/issues/891
-      $result=add-content -path testdrive:\dynamicfile.txt -value "ExpectedContent" -passthru
-      $result| Should -BeExactly "ExpectedContent"
+      $result = Add-Content -Path testdrive:\dynamicfile.txt -Value "ExpectedContent" -PassThru
+      $result | Should -BeExactly "ExpectedContent"
     }
     It "should return expected string from testdrive:\dynamicfile.txt" -Pending:($IsLinux -Or $IsMacOS) {#https://github.com/PowerShell/PowerShell/issues/891
-      $result = get-content -path testdrive:\dynamicfile.txt
+      $result = Get-Content -Path testdrive:\dynamicfile.txt
       $result | Should -BeExactly "ExpectedContent"
     }
     It "should Add-Content to testdrive:\$file1 even when -Value is `$null" {
-      $AsItWas=get-content testdrive:\$file1
-      {add-content -path testdrive:\$file1 -value $null -ErrorAction Stop} | Should -Not -Throw
-      get-content testdrive:\$file1 | Should -BeExactly $AsItWas
+      $AsItWas = Get-Content -Path testdrive:\$file1
+      { Add-Content -Path testdrive:\$file1 -Value $null -ErrorAction Stop } | Should -Not -Throw
+      Get-Content -Path testdrive:\$file1 | Should -BeExactly $AsItWas
     }
     It "should throw 'ParameterArgumentValidationErrorNullNotAllowed' when -Path is `$null" {
       { Add-Content -Path $null -Value "ShouldNotWorkBecausePathIsNull" -ErrorAction Stop } | Should -Throw -ErrorId "ParameterArgumentValidationErrorNullNotAllowed,Microsoft.PowerShell.Commands.AddContentCommand"
@@ -39,8 +39,8 @@ Describe "Add-Content cmdlet tests" -Tags "CI" {
     }
     #[BugId(BugDatabase.WindowsOutOfBandReleases, 9058182)]
     It "should be able to pass multiple [string]`$objects to Add-Content through the pipeline to output a dynamic Path file" -Pending:($IsLinux -Or $IsMacOS) {#https://github.com/PowerShell/PowerShell/issues/891
-      "hello","world"|add-content testdrive:\dynamicfile2.txt
-      $result=get-content testdrive:\dynamicfile2.txt
+      "hello","world" | Add-Content -Path testdrive:\dynamicfile2.txt
+      $result = Get-Content -Path testdrive:\dynamicfile2.txt
       $result.length | Should -Be 2
       $result[0]     | Should -BeExactly "hello"
       $result[1]     | Should -BeExactly "world"
