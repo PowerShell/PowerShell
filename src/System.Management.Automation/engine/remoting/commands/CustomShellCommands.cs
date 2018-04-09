f7b012753ae055454209fdb4cfd549f5b299e390
Klaudia Algiz <klalgiz@microsoft.com>
2018-04-09 12:02:52 -0700

    Correcting erorr appearing because of the Strict Mode

--- src/System.Management.Automation/engine/remoting/commands/CustomShellCommands.cs
+++ src/System.Management.Automation/engine/remoting/commands/CustomShellCommands.cs
@@ -4796,8 +4796,6 @@ $_ | Disable-PSSessionConfiguration -force $args[0] -whatif:$args[1] -confirm:$a
 
         //TODO: CLR4: Remove the logic for setting the MaxMemoryPerShellMB to 200 MB once IPMO->Get-Command->Get-Help memory usage issue is fixed.
         private const string enableRemotingSbFormat = @"
-Set-StrictMode -Version Latest
-
 function New-PluginConfigFile
 {{
 [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact=""Medium"")]
