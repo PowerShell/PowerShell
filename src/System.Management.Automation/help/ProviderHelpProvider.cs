f0f97c2569817c3bf0ffbd79c7af154f4b4187d0
Dongbo Wang <dongbow@microsoft.com>
2018-04-12 00:08:20 -0700

    [Feature] Clean up 'GetTypeInfo()' calls in 'help', 'cimSupport' and 'DscSupport' folders

--- src/System.Management.Automation/help/ProviderHelpProvider.cs
+++ src/System.Management.Automation/help/ProviderHelpProvider.cs
@@ -82,7 +82,7 @@ namespace System.Management.Automation
                 if (this.HelpSystem.LastHelpCategory == HelpCategory.Provider)
                 {
                     ErrorRecord errorRecord = new ErrorRecord(e, "ProviderLoadError", ErrorCategory.ResourceUnavailable, null);
-                    errorRecord.ErrorDetails = new ErrorDetails(typeof(ProviderHelpProvider).GetTypeInfo().Assembly, "HelpErrors", "ProviderLoadError", helpRequest.Target, e.Message);
+                    errorRecord.ErrorDetails = new ErrorDetails(typeof(ProviderHelpProvider).Assembly, "HelpErrors", "ProviderLoadError", helpRequest.Target, e.Message);
                     this.HelpSystem.LastErrors.Add(errorRecord);
                 }
             }
@@ -126,7 +126,7 @@ namespace System.Management.Automation
             if (providerInfo.ImplementingType == null)
                 return null;
 
-            return Path.GetDirectoryName(providerInfo.ImplementingType.GetTypeInfo().Assembly.Location);
+            return Path.GetDirectoryName(providerInfo.ImplementingType.Assembly.Location);
         }
 
         /// <summary>
