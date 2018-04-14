1476740d50fdf56630a09326ee564a1f11023804
Ilya <darpa@yandex.ru>
2018-04-14 19:11:21 +0500

    Remove alias tests and extra lines

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Format-Table.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Format-Table.Tests.ps1
@@ -3,17 +3,13 @@
 Describe "Format-Table" -Tags "CI" {
         It "Should call format table on piped input without error" {
                 { Get-Date | Format-Table } | Should -Not -Throw
-
-                { Get-Date | ft } | Should -Not -Throw
         }
 
         It "Should return a format object data type" {
-                $val = (Get-Date | Format-Table | gm )
-
-                $val2 = (Get-Date | Format-Table | gm )
+                $val = (Get-Date | Format-Table | Get-Member )
+                $val2 = (Get-Date | Format-Table | Get-Member )
 
                 $val.TypeName | Should -Match "Microsoft.Powershell.Commands.Internal.Format"
-
                 $val2.TypeName | Should -Match "Microsoft.Powershell.Commands.Internal.Format"
         }
 
@@ -21,11 +17,6 @@ Describe "Format-Table" -Tags "CI" {
                 $v1 = (Get-Date | Format-Table *)
                 $v2 = (Get-Date | Format-Table -Property Hour)
                 $v3 = (Get-Date | Format-Table -GroupBy Hour)
-
-                $v12 = (Get-Date | ft *)
-                $v22 = (Get-Date | ft -Property Hour)
-                $v32 = (Get-Date | ft -GroupBy Hour)
-
         }
 }
 
