5198a3535a41eb20510da0ca73e3a500d834cef1
Ilya <darpa@yandex.ru>
2018-04-21 22:58:52 +0500

    Use runspace and move all Export-FormatData tests in one file

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Export-FormatData.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Export-FormatData.Tests.ps1
@@ -10,6 +10,56 @@ Describe "Export-FormatData" -Tags "CI" {
         Remove-Item $testOutput -Force -ErrorAction SilentlyContinue
     }
 
+    It "Can export all types" {
+        try
+        {
+            $fd | Export-FormatData -path $TESTDRIVE\allformat.ps1xml -IncludeScriptBlock
+
+            $sessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
+            $sessionState.Formats.Clear()
+            $sessionState.Types.Clear()
+
+            $runspace = [System.Management.Automation.Runspaces.RunspaceFactory]::CreateRunspace($sessionState)
+            $runspace.Open()
+
+            $runspace.CreatePipeline("Update-FormatData -AppendPath $TESTDRIVE\allformat.ps1xml").Invoke()
+            $actualAllFormat = $runspace.CreatePipeline("Get-FormatData -TypeName *").Invoke()
+
+            $fd.Count | Should -Be $actualAllFormat.Count
+            Compare-Object $fd $actualAllFormat | Should -Be $null
+        }
+        finally
+        {
+            $runspace.Close()
+            Remove-Item -Path $TESTDRIVE\allformat.ps1xml -Force -ErrorAction SilentlyContinue
+        }
+    }
+
+    It "Works with literal path" {
+        $filename = 'TestDrive:\[formats.ps1xml'
+        $fd | Export-FormatData -LiteralPath $filename
+        (Test-Path -LiteralPath $filename) | Should -BeTrue
+    }
+
+    It "Should overwrite the destination file" {
+        $filename = 'TestDrive:\ExportFormatDataWithForce.ps1xml'
+        $unexpected = "SHOULD BE OVERWRITTEN"
+        $unexpected | Out-File -FilePath $filename -Force
+        $file = Get-Item  $filename
+        $file.IsReadOnly = $true
+        $fd | Export-FormatData -Path $filename -Force
+
+        $actual = @(Get-Content $filename)[0]
+        $actual | Should -Not -Be $unexpected
+    }
+
+    It "should not overwrite the destination file with NoClobber" {
+        $filename = "TestDrive:\ExportFormatDataWithNoClobber.ps1xml"
+        $fd | Export-FormatData -LiteralPath $filename
+
+        { $fd | Export-FormatData -LiteralPath $filename -NoClobber } | Should -Throw -ErrorId 'NoClobber,Microsoft.PowerShell.Commands.ExportFormatDataCommand'
+    }
+
     It "Test basic functionality" {
         Export-FormatData -InputObject $fd[0] -Path $testOutput
         $content = Get-Content $testOutput -Raw
@@ -28,7 +78,7 @@ Describe "Export-FormatData" -Tags "CI" {
         $piped[0] | Should -BeExactly "<"
     }
 
-    It "Should well format output xml" {
+    It "Should pretty print xml output" {
         $xmlContent=@"
             <Configuration>
             <ViewDefinitions>
@@ -78,13 +128,29 @@ Describe "Export-FormatData" -Tags "CI" {
   </ViewDefinitions>
 </Configuration>
 "@
-        $testfilename = [guid]::NewGuid().ToString('N')
-        $testfile = Join-Path -Path $TestDrive -ChildPath "$testfilename.ps1xml"
-        Set-Content -Path $testfile -Value $xmlContent
-        Update-FormatData -Append $testfile
-        Get-FormatData -TypeName "ExportFormatDataTypeName" | Export-FormatData -Path $testOutput
-        $content = Get-Content $testOutput -Raw
+        try
+        {
+            $testfilename = [guid]::NewGuid().ToString('N')
+            $testfile = Join-Path -Path $TestDrive -ChildPath "$testfilename.ps1xml"
+            Set-Content -Path $testfile -Value $xmlContent
 
-        $content | Should -BeExactly $expected
+            $sessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
+            $sessionState.Formats.Clear()
+            $sessionState.Types.Clear()
+
+            $runspace = [System.Management.Automation.Runspaces.RunspaceFactory]::CreateRunspace($sessionState)
+            $runspace.Open()
+
+            $runspace.CreatePipeline("Update-FormatData -prependPath $testfile").Invoke()
+            $runspace.CreatePipeline("Get-FormatData -TypeName 'ExportFormatDataTypeName' | Export-FormatData -Path $testOutput").Invoke()
+
+            $content = Get-Content $testOutput -Raw
+
+            $content | Should -BeExactly $expected
+        }
+        finally
+        {
+            $runspace.Close()
+        }
     }
 }
