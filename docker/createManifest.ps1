fc831922a54de28ebff6461a61200cdd24974995
Travis Plunk <travis.plunk@microsoft.com>
2018-04-25 16:05:10 -0700

    Add script to create a container manifest

---
+++ docker/createManifest.ps1
@@ -0,0 +1,38 @@
+# Used to create a container manifest.
+# default scenarios is the latest image which will point to ubuntu-16.04 for linux and windowsservercore for windows
+param(
+    [parameter(Mandatory)]
+    [string]
+    $ContainerRegistry,
+
+    [ValidateNotNullOrEmpty()]
+    [ValidatePattern('^[abcdefghijklmnopqrstuvwxyz-]+$')]
+    [string]
+    $ManifestTag = 'lastest',
+
+    [ValidateNotNullOrEmpty()]
+    [ValidatePattern('^[abcdefghijklmnopqrstuvwxyz-]+$')]
+    [string]
+    $Image = 'powershell',
+
+    [ValidateNotNullOrEmpty()]
+    [ValidatePattern('^[abcdefghijklmnopqrstuvwxyz-]+$')]
+    [string[]]
+    $TagList = ('ubuntu-16.04', 'windowsservercore')
+
+)
+
+$manifestList = @()
+foreach($tag in $TagList)
+{
+    $manifestList += "$ContainerRegistry/${Image}:$tag"
+}
+
+# Create the manifest
+docker manifest create $ContainerRegistry/${Image}:$ManifestTag $manifestList
+
+# Inspect (print) the manifest
+docker manifest inspect $ContainerRegistry/${Image}:$ManifestTag
+
+# push the manifest
+docker manifest push $ContainerRegistry/${Image}:$ManifestTag
