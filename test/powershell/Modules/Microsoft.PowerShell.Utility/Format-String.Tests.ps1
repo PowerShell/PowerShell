2854d272d26997ac246439452f713a04e3666b0d
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-04-27 11:22:14 +0200

    remove culture specific test component

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Format-String.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Format-String.Tests.ps1
@@ -9,8 +9,8 @@ Describe "Format-String Unit Tests" -Tag "CI" {
 	It "Should properly format strings" {
 		1..4 | Format-String "Foo {0}" | Should -Be "Foo 1", "Foo 2", "Foo 3", "Foo 4"
 		"abc" | Format-String "{0} Foo {0}" | Should -Be "abc Foo abc"
-		1 | Format-String "{0:D2} {0:N2}" | Should -Be "01 1,00"
-		1..4 | Format-String "{0:D2} {1:N2}" -Count 2 | Should -Be "01 2,00", "03 4,00"
-		1..3 | Format-String "{0:D2} {1:N2}" 2 | Should -Be "01 2,00", "03 "
+		1 | Format-String "{0:D2} {0}" | Should -Be "01 1"
+		1..4 | Format-String "{0:D2} {1}" -Count 2 | Should -Be "01 2", "03 4"
+		1..3 | Format-String "{0:D2} {1}" 2 | Should -Be "01 2", "03 "
 	}
 }
