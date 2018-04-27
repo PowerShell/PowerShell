f1e911183f0e59f2e745ef7f3a17ce17cd74243f
Robert Holt <rjmholt_msft@outlook.com>
2018-04-26 17:26:54 -0700

    [feature]Add property override test

--- test/powershell/Language/Classes/scripting.Classes.inheritance.tests.ps1
+++ test/powershell/Language/Classes/scripting.Classes.inheritance.tests.ps1
@@ -330,6 +330,38 @@ Describe 'Classes methods with inheritance' -Tags "CI" {
             $intList.Count | Should -Be 1
             $intList[0] | Should -Be 200402
         }
+
+        It 'overrides abstract base class properties' {
+            class TestHost : System.Management.Automation.Host.PSHost
+            {
+                [String]$myName = "MyHost"
+                [Version]$myVersion = [Version]"1.0.0.0"
+                [Guid]$myInstanceId = [guid]::NewGuid()
+                [System.Globalization.CultureInfo]$myCurrentCulture = "en-us"
+                [System.Globalization.CultureInfo]$myCurrentUICulture = "en-us"
+                [System.Management.Automation.Host.PSHostUserInterface]$myUI = $null
+                [bool]$IsInteractive
+                [void]SetShouldExit([int]$exitCode) { }
+                [void]EnterNestedPrompt(){ throw "Unsupported" }
+                [void]ExitNestedPrompt(){ throw "Unsupported" }
+                [void]NotifyBeginApplication() { }
+                [void]NotifyEndApplication() { }
+                [string]get_Name() { return $this.myName; write-host "MyName" }
+                [version]get_Version() { return $this.myVersion }
+                [System.Globalization.CultureInfo]get_CurrentCulture() { return $this.myCurrentCulture }
+                [System.Globalization.CultureInfo]get_CurrentUICulture() { return $this.myCurrentUICulture }
+                [System.Management.Automation.Host.PSHostUserInterface]get_UI() { return $this.myUI }
+                [guid]get_InstanceId() { return $this.myInstanceId }
+                TestHost() {
+                }
+                TestHost([bool]$isInteractive) {
+                    $this.IsInteractive = $isInteractive
+                }
+            }
+
+            $th = [TestHost]::new()
+            $th.Name | Should -BeExactly "MyHost"
+        }
     }
 
     Context 'base static method call' {
@@ -500,5 +532,36 @@ class Derived : Base
 '@)
         $sb.Invoke() | Should -Be 200
         $sb.Invoke() | Should -Be 200
+    }
+
+    It 'successfully inherits from abstract classes' {
+        class TestHost : System.Management.Automation.Host.PSHost
+        {
+            [String]$myName = "MyHost"
+            [Version]$myVersion = [Version]"1.0.0.0"
+            [Guid]$myInstanceId = [guid]::NewGuid()
+            [System.Globalization.CultureInfo]$myCurrentCulture = "en-us"
+            [System.Globalization.CultureInfo]$myCurrentUICulture = "en-us"
+            [System.Management.Automation.Host.PSHostUserInterface]$myUI = $null
+            [bool]$IsInteractive
+            [void]SetShouldExit([int]$exitCode) { }
+            [void]EnterNestedPrompt(){ throw "Unsupported" }
+            [void]ExitNestedPrompt(){ throw "Unsupported" }
+            [void]NotifyBeginApplication() { }
+            [void]NotifyEndApplication() { }
+            [string]get_Name() { return $this.myName; write-host "MyName" }
+            [version]get_Version() { return $this.myVersion }
+            [System.Globalization.CultureInfo]get_CurrentCulture() { return $this.myCurrentCulture }
+            [System.Globalization.CultureInfo]get_CurrentUICulture() { return $this.myCurrentUICulture }
+            [System.Management.Automation.Host.PSHostUserInterface]get_UI() { return $this.myUI }
+            [guid]get_InstanceId() { return $this.myInstanceId }
+            TestHost() {
+            }
+            TestHost([bool]$isInteractive) {
+                $this.IsInteractive = $isInteractive
+            }
+        }
+
+        [TestHost]::new().myName | Should -BeExactly "MyHost"
     }
 }
