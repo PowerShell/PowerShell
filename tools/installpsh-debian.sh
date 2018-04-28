62c3db5f34164923fd632c9122dd91365114156f
bjh7242 <bjh7242@rit.edu>
2018-04-28 16:56:15 -0400

    updating error when running powershell installer as root

--- tools/installpsh-debian.sh
+++ tools/installpsh-debian.sh
@@ -94,7 +94,7 @@ if (( $EUID != 0 )); then
 fi
 
 #Check that sudo is available
-if [[ "$SUDO" -eq "sudo" && ! ("'$*'" =~ skip-sudo-check) ]]; then
+if [[ $EUID != 0 && "$SUDO" -eq "sudo" && ! ("'$*'" =~ skip-sudo-check) ]]; then
 
     $SUDO -v
     if [ $? -ne 0 ]; then
