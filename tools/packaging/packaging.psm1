e5a7c6dfc100b720d9a5333d759aaa25c3908f3c
Robert Holt <rjmholt_msft@outlook.com>
2018-05-04 10:17:19 -0700

    [Package] Remove libssl1.1 requirement

--- tools/packaging/packaging.psm1
+++ tools/packaging/packaging.psm1
@@ -995,7 +995,7 @@ function Get-PackageDependencies
                 "ubuntu.14.04" { $Dependencies += @("libssl1.0.0", "libicu52") }
                 "ubuntu.16.04" { $Dependencies += @("libssl1.0.0", "libicu55") }
                 "ubuntu.17.10" { $Dependencies += @("libssl1.0.0", "libicu57") }
-                "ubuntu.18.04" { $Dependencies += @("libssl1.0.0", "libssl1.1", "libicu60") }
+                "ubuntu.18.04" { $Dependencies += @("libssl1.0.0", "libicu60") }
                 "debian.8" { $Dependencies += @("libssl1.0.0", "libicu52") }
                 "debian.9" { $Dependencies += @("libssl1.0.2", "libicu57") }
                 default { throw "Debian distro '$Distribution' is not supported." }
