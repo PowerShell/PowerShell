3f26fd121cb3879e1e49e01443618b91dac02c4e
Travis Plunk <travis.plunk@microsoft.com>
2018-05-07 12:16:09 -0700

    Add empty line after copyright

--- test/powershell/engine/Basic/Assembly.LoadWithPartialName.Tests.ps1
+++ test/powershell/engine/Basic/Assembly.LoadWithPartialName.Tests.ps1
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
 Describe "Assembly::LoadWithPartialName Validation Test" -Tags "CI" {
 
     $defaultErrorId = 'FileLoadException'
