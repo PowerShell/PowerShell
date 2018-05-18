c7f7a2e36d5507042e117cf98477860a53916424
Travis Plunk <travis.plunk@microsoft.com>
2018-05-18 14:12:39 -0700

    Add a function to clean the cache before caching

--- tools/appveyor.psm1
+++ tools/appveyor.psm1
@@ -551,3 +551,9 @@ function Invoke-AppveyorFinish
         throw $_
     }
 }
+
+# Clean the Cache of files which should not be cached
+function Invoke-AppVeyorCleanCache
+{
+    Remove-Item $env:LocalAppData\Microsoft\dotnet\optimizationdata -Recurse -force
+}
