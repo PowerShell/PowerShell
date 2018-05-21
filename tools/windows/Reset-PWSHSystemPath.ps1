57e02d20ee99e14d2408da260437be4cebbe0092
Darwin <darwinjsgithub.services@sanoys.com>
2018-05-21 18:39:03 -0400

    commenting and cleanup

--- tools/windows/Reset-PWSHSystemPath.ps1
+++ tools/windows/Reset-PWSHSystemPath.ps1
@@ -14,19 +14,19 @@
   Default: machine
 
 .PARAMETER RemoveAllOccurences
-  By default the cleanup leaves the highest sorted PowerShell Core path alone.  
+  By default the cleanup leaves the highest sorted PowerShell Core path alone.
   This switch causes it to be cleaned up as well.
   Default: false
 
 .EXAMPLE
   .\Reset-PWSHSystemPath.ps1
-  
+
   Removes all PowerShell core paths but the very last one when sorted in ascending order from the Machine level path.
   Good for running on systems that already has at least one valid PowerShell install.
 
 .EXAMPLE
   .\Reset-PWSHSystemPath.ps1 -RemoveAllOccurences
-  
+
   Removes ALL PowerShell core paths from the Machine level path.
   Good for running right before upgrading PowerShell core.
 .EXAMPLE
@@ -44,24 +44,29 @@ param (
   [string[]]$PathScope="machine",
   [switch]$RemoveAllOccurences
 )
+
+#Do the cleanup for all specified path scopes
 ForEach ($PathScopeItem in $PathScope)
 {
   $AssembledNewPath = $NewPath = ''
+  #From the current path scope. retrieve the array of paths that match the pathspec of PowerShell Core (to use as a filter)
   $pathstoremove = @([Environment]::GetEnvironmentVariable("PATH","$PathScopeItem").split(';') | Where { $_ -ilike "*\Program Files\Powershell\6*"})
-  If (!$RemoveAllOccurences) 
+  If (!$RemoveAllOccurences)
   {
+    #If we are not removing all occurances of powershell core paths, then remove the highest sorted path from the filter
     $pathstoremove = @($pathstoremove | sort-object | Select-Object -skiplast 1)
   }
-  Write-Host "Reset-PWSHSystemPath: Found $($pathstoremove.count) paths to remove from $PathScopeItem path scope: $($Pathstoremove -join ', ' | out-string)"
+  Write-Verbose "Reset-PWSHSystemPath: Found $($pathstoremove.count) paths to remove from $PathScopeItem path scope: $($Pathstoremove -join ', ' | out-string)"
   If ($pathstoremove.count -gt 0)
   {
-    foreach ($path in [Environment]::GetEnvironmentVariable("PATH","$PathScopeItem").split(';'))
+    foreach ($Path in [Environment]::GetEnvironmentVariable("PATH","$PathScopeItem").split(';'))
     {
+      #rebuild the path in the same order, but eliminate the paths in the filter array and blanks
       If ($Path)
       {
         If ($pathstoremove -inotcontains "$Path")
         {
-          [string[]]$Newpath += "$path"
+          [string[]]$Newpath += "$Path"
         }
       }
     }
