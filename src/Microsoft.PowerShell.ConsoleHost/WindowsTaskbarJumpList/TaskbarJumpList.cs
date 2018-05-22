2651c841017cfc2a47720824768943094f3edd67
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-05-22 21:33:58 +0100

    improve error handling: abort jumplist creation and use debug.assert instead of throwing an exception. Since this now not a user facing string any more, I do not think it is worth having the StringUtil.Format wrapper for it

--- src/Microsoft.PowerShell.ConsoleHost/WindowsTaskbarJumpList/TaskbarJumpList.cs
+++ src/Microsoft.PowerShell.ConsoleHost/WindowsTaskbarJumpList/TaskbarJumpList.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Diagnostics;
 using System.Reflection;
 using static Microsoft.PowerShell.ComInterfaces;
 
@@ -22,9 +23,19 @@ namespace Microsoft.PowerShell
                 var CLSID_DestinationList = new Guid(@"77f10cf0-3db5-4966-b520-b7c54fd35ed6");
                 const uint CLSCTX_INPROC_SERVER = 1;
                 var IID_IUnknown = new Guid("00000000-0000-0000-C000-000000000046");
-                CheckAndReturn(CoCreateInstance(ref CLSID_DestinationList, null, CLSCTX_INPROC_SERVER, ref IID_IUnknown, out object pCustDestListobj));
+                var hResult = CoCreateInstance(ref CLSID_DestinationList, null, CLSCTX_INPROC_SERVER, ref IID_IUnknown, out object pCustDestListobj);
+                if (hResult < 0)
+                {
+                    Debug.Fail($"Creating ICustomDestinationList failed with HResult '{hResult}'.");
+                    return;
+                }
                 var pCustDestList = (ICustomDestinationList)pCustDestListobj;
-                CheckAndReturn(pCustDestList.BeginList(out uint uMaxSlots, new Guid(@"92CA9DCD-5622-4BBA-A805-5E9F541BD8C9"), out object pRemovedItems));
+                hResult = pCustDestList.BeginList(out uint uMaxSlots, new Guid(@"92CA9DCD-5622-4BBA-A805-5E9F541BD8C9"), out object pRemovedItems);
+                if (hResult < 0)
+                {
+                    Debug.Fail($"BeginList on ICustomDestinationList failed with HResult '{hResult}'.");
+                    return;
+                }
 
                 if (uMaxSlots >= 1)
                 {
@@ -39,30 +50,46 @@ namespace Microsoft.PowerShell
                     flags |= 0x00002000; // SLDF_RUNAS_USER
                     shellLinkDataList.SetFlags(flags);
                     var PKEY_TITLE = new PropertyKey(new Guid("{F29F85E0-4FF9-1068-AB91-08002B27B3D9}"), 2);
-                    CheckAndReturn(nativePropertyStore.SetValue(ref PKEY_TITLE, new PropVariant(title)));
-                    CheckAndReturn(nativePropertyStore.Commit());
+                    hResult = nativePropertyStore.SetValue(ref PKEY_TITLE, new PropVariant(title));
+                    if (hResult < 0)
+                    {
+                        pCustDestList.AbortList();
+                        Debug.Fail($"SetValue on IPropertyStore with title '{title}' failed with HResult '{hResult}'.");
+                        return;
+                    }
+                    hResult = nativePropertyStore.Commit();
+                    if (hResult < 0)
+                    {
+                        pCustDestList.AbortList();
+                        Debug.Fail($"Commit on IPropertyStore failed with HResult '{hResult}'.");
+                        return;
+                    }
 
                     // Create collection and add JumpListLink
                     var CLSID_EnumerableObjectCollection = new Guid(@"2d3468c1-36a7-43b6-ac24-d3f02fd9607a");
                     const uint CLSCTX_INPROC_HANDLER = 2;
                     const uint CLSCTX_INPROC = CLSCTX_INPROC_SERVER | CLSCTX_INPROC_HANDLER;
                     var ComSvrInterface_GUID = new Guid(@"555E2D2B-EE00-47AA-AB2B-39F953F6B339");
-                    CheckAndReturn(CoCreateInstance(ref CLSID_EnumerableObjectCollection, null, CLSCTX_INPROC, ref IID_IUnknown, out object instance));
+                    hResult = CoCreateInstance(ref CLSID_EnumerableObjectCollection, null, CLSCTX_INPROC, ref IID_IUnknown, out object instance);
+                    if (hResult < 0)
+                    {
+                        pCustDestList.AbortList();
+                        Debug.Fail($"Creating IObjectCollection failed with HResult '{hResult}'.");
+                        return;
+                    }
                     var pShortCutCollection = (IObjectCollection)instance;
                     pShortCutCollection.AddObject((IShellLinkW)nativePropertyStore);
 
                     // Add collection to custom destination list and commit the result
-                    CheckAndReturn(pCustDestList.AddUserTasks((IObjectArray)pShortCutCollection));
+                    hResult = pCustDestList.AddUserTasks((IObjectArray)pShortCutCollection);
+                    if (hResult < 0)
+                    {
+                        pCustDestList.AbortList();
+                        Debug.Fail($"AddUserTasks on ICustomDestinationList failed with HResult '{hResult}'.");
+                        return;
+                    }
                     pCustDestList.CommitList();
                 }
-            }
-        }
-
-        private static void CheckAndReturn(HResult hResult)
-        {
-            if (hResult < 0)
-            {
-                throw new Exception($"HResult from COM call was negative, which indicates a failure. Was: '{hResult}'");
             }
         }
     }
