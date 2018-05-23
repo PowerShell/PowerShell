058e36dc794284af54c3a17287a6045fff9aea52
sethvs <sethvs@users.noreply.github.com>
2018-05-23 17:12:28 +0300

    Fix CodeFactor issues.

--- src/System.Management.Automation/engine/parser/tokenizer.cs
+++ src/System.Management.Automation/engine/parser/tokenizer.cs
@@ -720,7 +720,7 @@ namespace System.Management.Automation.Language
 
         private char GetChar()
         {
-            Diagnostics.Assert(0 <= _currentIndex, "GetChar reading before start of input.");
+            Diagnostics.Assert(_currentIndex >= 0, "GetChar reading before start of input.");
             Diagnostics.Assert(_currentIndex <= _script.Length + 1, "GetChar reading after end of input.");
 
             // Increment _currentIndex, even if it goes over the Length so callers can call UngetChar to unget EOF.
@@ -741,7 +741,7 @@ namespace System.Management.Automation.Language
 
         private char PeekChar()
         {
-            Diagnostics.Assert(0 <= _currentIndex && _currentIndex <= _script.Length, "PeekChar out of range.");
+            Diagnostics.Assert(_currentIndex >= 0 && _currentIndex <= _script.Length, "PeekChar out of range.");
 
             if (_currentIndex == _script.Length)
             {
@@ -904,7 +904,7 @@ namespace System.Management.Automation.Language
             {
                 _currentIndex = _script.Length + 1;
             }
-            else if (0 > _currentIndex)
+            else if (_currentIndex < 0)
             {
                 _currentIndex = 0;
             }
