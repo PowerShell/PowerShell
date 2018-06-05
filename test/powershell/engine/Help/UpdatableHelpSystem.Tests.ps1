8c92938003a8e47847e830effd60f006cb752b2f
Steve Lee <slee@microsoft.com>
2018-06-04 23:13:56 -0700

    [feature] address Jason's feedback

--- test/powershell/engine/Help/UpdatableHelpSystem.Tests.ps1
+++ test/powershell/engine/Help/UpdatableHelpSystem.Tests.ps1
@@ -145,7 +145,7 @@ $testCases = @{
     }
 
     "PSReadline" = @{
-        HelpFiles            = "Microsoft.PowerShell.PSReadline.dll-help.xml"
+        HelpFiles            = "Microsoft.PowerShell.PSReadLine.dll-help.xml"
         HelpInfoFiles        = "PSReadline_5714753b-2afd-4492-a5fd-01d9e2cff8b5_HelpInfo.xml"
         CompressedFiles      = "PSReadline_5714753b-2afd-4492-a5fd-01d9e2cff8b5_en-US_helpcontent$extension"
         HelpInstallationPath = "$pshome\Modules\PSReadLine\2.0.0\en-US"
