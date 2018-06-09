24d0bac25001b2dbe40598cae9c2d3a0cac11f61
iSazonov <darpa@yandex.ru>
2018-06-09 15:02:41 +0500

    [Feature] Use File.Exists in AnalysisCache.cs

--- src/System.Management.Automation/engine/Modules/AnalysisCache.cs
+++ src/System.Management.Automation/engine/Modules/AnalysisCache.cs
@@ -650,10 +650,9 @@ namespace System.Management.Automation
             var keys = Entries.Keys;
             foreach (var key in keys)
             {
-                if (!Utils.FileExists(key))
+                if (!File.Exists(key))
                 {
-                    ModuleCacheEntry unused;
-                    removedSomething |= Entries.TryRemove(key, out unused);
+                    removedSomething |= Entries.TryRemove(key, out ModuleCacheEntry _);
                 }
             }
 
@@ -690,7 +689,7 @@ namespace System.Management.Automation
 
             try
             {
-                if (Utils.FileExists(filename))
+                if (File.Exists(filename))
                 {
                     var fileLastWriteTime = new FileInfo(filename).LastWriteTime;
                     if (fileLastWriteTime > this.LastReadTime)
@@ -987,7 +986,7 @@ namespace System.Management.Automation
             {
                 try
                 {
-                    if (Utils.FileExists(s_cacheStoreLocation))
+                    if (File.Exists(s_cacheStoreLocation))
                     {
                         return Deserialize(s_cacheStoreLocation);
                     }
