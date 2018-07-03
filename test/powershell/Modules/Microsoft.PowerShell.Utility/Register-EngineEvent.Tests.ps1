55bbc3fcd184df96e4f729392ebe5c8d2c6d312b
Alex Jordan <alexjordan6@gmail.com>
2018-07-03 12:41:15 -0500

    Fix the name of a 'Register-EngineEvent' test (#7222)

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Register-EngineEvent.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Register-EngineEvent.Tests.ps1
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 Describe "Register-EngineEvent" -Tags "CI" {
 
-    Context "Check return type of Register-ObjectEvent" {
+    Context "Check return type of Register-EngineEvent" {
 	It "Should return System.Management.Automation.PSEventJob as return type of Register-EngineEvent" {
 	    Register-EngineEvent -SourceIdentifier PesterTestRegister -Action {echo registerengineevent} | Should -BeOfType System.Management.Automation.PSEventJob
 	    Unregister-Event -sourceidentifier PesterTestRegister
