0b4bae4ceceb7e0bef2eacf51f7efeefc2855ffe
Dongbo Wang <dongbow@microsoft.com>
2018-07-03 22:22:30 -0700

    [Feature] Fix error position for switch-statement condition and for-statement initializer

--- src/System.Management.Automation/engine/parser/Compiler.cs
+++ src/System.Management.Automation/engine/parser/Compiler.cs
@@ -743,14 +743,16 @@ namespace System.Management.Automation.Language
             s_builtinAttributeGenerator.Add(typeof(ValidateNotNullOrEmptyAttribute), NewValidateNotNullOrEmptyAttribute);
         }
 
-        private Compiler(List<IScriptExtent> sequencePoints)
+        private Compiler(List<IScriptExtent> sequencePoints, Dictionary<IScriptExtent, int> sequencePointIndexMap)
         {
             _sequencePoints = sequencePoints;
+            _sequencePointIndexMap = sequencePointIndexMap;
         }
 
         internal Compiler()
         {
             _sequencePoints = new List<IScriptExtent>();
+            _sequencePointIndexMap = new Dictionary<IScriptExtent, int>();
         }
 
         internal bool CompilingConstantExpression { get; set; }
@@ -766,6 +768,7 @@ namespace System.Management.Automation.Language
         private int _switchTupleIndex = VariableAnalysis.Unanalyzed;
         private int _foreachTupleIndex = VariableAnalysis.Unanalyzed;
         private readonly List<IScriptExtent> _sequencePoints;
+        private readonly Dictionary<IScriptExtent, int> _sequencePointIndexMap;
         private int _stmtCount;
 
         internal bool CompilingMemberFunction { get; set; }
@@ -903,17 +906,31 @@ namespace System.Management.Automation.Language
                                    ExpressionCache.Constant(version));
         }
 
-        internal Expression UpdatePosition(Ast ast)
+        private int AddSequencePoint(IScriptExtent extent)
         {
-            _sequencePoints.Add(ast.Extent);
+            // Make sure we don't add the same extent to the sequence point list twice.
+            if (!_sequencePointIndexMap.TryGetValue(extent, out int index))
+            {
+                _sequencePoints.Add(extent);
+                index = _sequencePoints.Count - 1;
+                _sequencePointIndexMap.Add(extent, index);
+            }
+
+            return index;
+        }
+
+        private Expression UpdatePosition(Ast ast)
+        {
+            IScriptExtent extent = ast.Extent;
+            int index = AddSequencePoint(extent);
 
             // If we just added the first sequence point, then we don't want to check for breakpoints - we'll do that
             // in EnterScriptFunction.
             // Except for while/do loops, in this case we want to check breakpoints on the first sequence point since it
             // will be executed multiple times.
-            return ((_sequencePoints.Count == 1) && !_generatingWhileOrDoLoop)
+            return (index == 0 && !_generatingWhileOrDoLoop)
                        ? ExpressionCache.Empty
-                       : new UpdatePositionExpr(ast.Extent, _sequencePoints.Count - 1, _debugSymbolDocument, !_compilingSingleExpression);
+                       : new UpdatePositionExpr(extent, index, _debugSymbolDocument, !_compilingSingleExpression);
         }
 
         private int _tempCounter;
@@ -1683,7 +1700,7 @@ namespace System.Management.Automation.Language
                 // on this sequence point, but it makes it safe to access the CurrentPosition
                 // property in FunctionContext (which can happen if there are exceptions
                 // defining the functions.)
-                _sequencePoints.Add(ast.Extent);
+                AddSequencePoint(ast.Extent);
             }
 
             var compileInterpretChoice = (_stmtCount > 300) ? CompileInterpretChoice.NeverCompile : CompileInterpretChoice.CompileOnDemand;
@@ -1853,8 +1870,8 @@ namespace System.Management.Automation.Language
             var exprs = new List<Expression>();
             var temps = new List<ParameterExpression> { _executionContextParameter, LocalVariablesParameter };
             GenerateFunctionProlog(exprs, temps, null);
-            _sequencePoints.Add(expressionAst.Extent);
-            exprs.Add(new UpdatePositionExpr(expressionAst.Extent, _sequencePoints.Count - 1, _debugSymbolDocument, checkBreakpoints: true));
+            int index = AddSequencePoint(expressionAst.Extent);
+            exprs.Add(new UpdatePositionExpr(expressionAst.Extent, index, _debugSymbolDocument, checkBreakpoints: true));
             var result = Compile(expressionAst).Cast(typeof(object));
             exprs.Add(Expression.Label(_returnTarget, result));
             var body = Expression.Block(new[] { _executionContextParameter, LocalVariablesParameter }, exprs);
@@ -2143,7 +2160,7 @@ namespace System.Management.Automation.Language
 
         private Tuple<Action<FunctionContext>, Type> CompileTrap(TrapStatementAst trap)
         {
-            var compiler = new Compiler(_sequencePoints) { _compilingTrap = true };
+            var compiler = new Compiler(_sequencePoints, _sequencePointIndexMap) { _compilingTrap = true };
             string funcName = _currentFunctionName + "<trap>";
             if (trap.TrapType != null)
             {
@@ -2517,8 +2534,8 @@ namespace System.Management.Automation.Language
 
                 if (entryExtent != null)
                 {
-                    _sequencePoints.Add(entryExtent);
-                    exprs.Add(new UpdatePositionExpr(entryExtent, _sequencePoints.Count - 1, _debugSymbolDocument, checkBreakpoints: false));
+                    int index = AddSequencePoint(entryExtent);
+                    exprs.Add(new UpdatePositionExpr(entryExtent, index, _debugSymbolDocument, checkBreakpoints: false));
                 }
 
                 exprs.Add(
@@ -2568,8 +2585,8 @@ namespace System.Management.Automation.Language
                 }
 
                 var extent = propertyMember.InitialValue.Extent;
-                _sequencePoints.Add(extent);
-                exprs.Add(new UpdatePositionExpr(extent, _sequencePoints.Count - 1, _debugSymbolDocument, checkBreakpoints: false));
+                int index = AddSequencePoint(extent);
+                exprs.Add(new UpdatePositionExpr(extent, index, _debugSymbolDocument, checkBreakpoints: false));
                 var property = _memberFunctionType.Type.GetProperty(propertyMember.Name, bindingFlags);
                 exprs.Add(
                     Expression.Assign(
@@ -3022,7 +3039,7 @@ namespace System.Management.Automation.Language
             }
 
             var exprs = new List<Expression>
-                            {
+                        {
                             // Set current position in case of errors.
                             UpdatePosition(assignmentStatementAst),
                             ReduceAssignment((ISupportsAssignment)assignmentStatementAst.Left,
@@ -4240,10 +4257,26 @@ namespace System.Management.Automation.Language
             // $foreach/$switch = GetEnumerator $enumerable
             var enumerable = NewTemp(typeof(object), "enumerable");
             temps.Add(enumerable);
+
+            // Update position to make it safe to access 'CurrentPosition' property in FunctionContext in case
+            // that the evaluation of 'stmt.Condition' throws exception.
             if (generatingForeach)
             {
+                // For foreach statement, we want the debugger to stop at 'stmt.Condition' before evaluating it.
+                // The debugger will stop at 'stmt.Condition' only once. The following enumeration will stop at
+                // the foreach variable.
                 exprs.Add(UpdatePosition(stmt.Condition));
             }
+            else
+            {
+                // For switch statement, we don't want the debugger to stop at 'stmt.Condition' before evaluating it.
+                // The following enumeration will stop at 'stmt.Condition' again, and we don't want the debugger to
+                // stop at 'stmt.Condition' twice before getting into one of its case clauses.
+                var extent = stmt.Condition.Extent;
+                int index = AddSequencePoint(extent);
+                exprs.Add(new UpdatePositionExpr(extent, index, _debugSymbolDocument, checkBreakpoints: false));
+            }
+
             exprs.Add(
                 Expression.Assign(enumerable,
                                   GetRangeEnumerator(stmt.Condition.GetPureExpression())
@@ -4367,13 +4400,25 @@ namespace System.Management.Automation.Language
         public object VisitForStatement(ForStatementAst forStatementAst)
         {
             // We should not preserve the partial output if exception is thrown when evaluating the initializer.
-            var init = (forStatementAst.Initializer != null)
-                           ? CaptureStatementResults(forStatementAst.Initializer, CaptureAstContext.AssignmentWithoutResultPreservation)
-                           : null;
+            Expression init = null;
+            PipelineBaseAst initializer = forStatementAst.Initializer;
+            if (initializer != null)
+            {
+                init = CaptureStatementResults(initializer, CaptureAstContext.AssignmentWithoutResultPreservation);
+                if (initializer is PipelineAst pipelineAst && !pipelineAst.Background && pipelineAst.GetPureExpression() != null)
+                {
+                    // If the initializer is a pure expression (CommandExpressionAst without redirection),
+                    // then we need to add a sequence point. If it's an AssignmentStatementAst, we don't
+                    // need to add sequence point here because a sequence point will be added when processing
+                    // the AssignmentStatementAst.
+                    init = Expression.Block(UpdatePosition(initializer), init);
+                }
+            }
 
-            var generateCondition = forStatementAst.Condition != null
-                ? () => Expression.Block(UpdatePosition(forStatementAst.Condition),
-                                         CaptureStatementResults(forStatementAst.Condition, CaptureAstContext.Condition))
+            PipelineBaseAst condition = forStatementAst.Condition;
+            var generateCondition = condition != null
+                ? () => Expression.Block(UpdatePosition(condition),
+                                         CaptureStatementResults(condition, CaptureAstContext.Condition))
                 : (Func<Expression>)null;
 
             var loop = GenerateWhileLoop(forStatementAst.Label, generateCondition,
--- test/powershell/Language/Scripting/Debugging/Debugging.Tests.ps1
+++ test/powershell/Language/Scripting/Debugging/Debugging.Tests.ps1
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
 $script1 = @'
 'aaa'.ToString() > $null
 'aa' > $null
@@ -8,32 +9,83 @@ $script1 = @'
 'bb'.ToSTring() > $null
 'bbb'
 '@
+
 $script2 = @'
+$test = 1..2
+switch ($test)
+{
+    default {}
+}
+'@
+
+$script3 = @'
+$test = 2
+for ("string".Length;
+     $test -gt 0; $test--) { }
+'@
+
+$script4 = @'
 "line 1"
 "line 2"
 "line 3"
 '@
 
 Describe "Breakpoints when set should be hit" -tag "CI" {
-    BeforeAll {
-        $path = setup -pass -f TestScript_1.ps1 -content $script1
-        $bps = 1..6 | ForEach-Object { set-psbreakpoint -script $path -line $_ -Action { continue } }
+    Context "Basic tests" {
+        BeforeAll {
+            $path = Setup -PassThru -File TestScript_1.ps1 -Content $script1
+            $bps = 1..6 | ForEach-Object { set-psbreakpoint -script $path -line $_ -Action { continue } }
+        }
+
+        AfterAll {
+            $bps | Remove-PSBreakPoint
+        }
+
+        It "A redirected breakpoint is hit" {
+            & $path
+            foreach ( $bp in $bps ) {
+                $bp.HitCount | Should -Be 1
+            }
+        }
     }
-    AfterAll {
-        $bps | Remove-PSBreakPoint
+
+    Context "Break point on switch condition should be hit only when enumerating it" {
+        BeforeAll {
+            $path = Setup -PassThru -File TestScript_2.ps1 -Content $script2
+            $breakpoint = Set-PSBreakpoint -Script $path -Line 2 -Action { continue }
+        }
+
+        AfterAll {
+            Remove-PSBreakpoint -Breakpoint $breakpoint
+        }
+
+        It "switch condition should be hit 3 times" {
+            ## MoveNext() will be called on the condition for 3 times
+            & $path
+            $breakpoint.HitCount | Should -Be 3
+        }
     }
-    It "A redirected breakpoint is hit" {
-        & $path
-        foreach ( $bp in $bps ) {
-            $bp.HitCount | Should -Be 1
+
+    Context "Break point on for-statement initializer should be hit" {
+        BeforeAll {
+            $path = Setup -PassThru -File TestScript_3.ps1 -Content $script3
+            $breakpoint = Set-PSBreakpoint -Script $path -Line 2 -Action { continue }
+        }
+
+        AfterAll {
+            Remove-PSBreakpoint -Breakpoint $breakpoint
+        }
+
+        It "for-statement initializer should be hit once" {
+            & $path
+            $breakpoint.HitCount | Should -Be 1
         }
     }
 }
 
 Describe "It should be possible to reset runspace debugging" -tag "Feature" {
     BeforeAll {
-        $path = setup -pass -f TestScript_2.ps1 -content $script2
-        $scriptPath = "$testdrive/TestScript_2.ps1"
+        $scriptPath = Setup -PassThru -File TestScript_2.ps1 -Content $script4
         $iss = [initialsessionstate]::CreateDefault2();
         $rs = [runspacefactory]::CreateRunspace($iss)
         $rs.Name = "TestRunspaceDebuggerReset"
@@ -87,6 +139,6 @@ Describe "It should be possible to reset runspace debugging" -tag "Feature" {
     }
     It "The script should run without a break" {
         $ps.Commands.Clear()
-        $ps.addscript("$testdrive/TestScript_2.ps1").Invoke().Count | Should -Be 3
+        $ps.addscript($scriptPath).Invoke().Count | Should -Be 3
     }
 }
