ba96ba3a32a3f7eb74831e0dc07f4b77379ab804
kwkam <kwkam@users.noreply.github.com>
2018-08-01 21:50:56 +0800

    Revert "[Feature] Remove unnecessary finally"
    This reverts commit be59b3d05f189ec569ff55b99bce752e10105f5c.

--- src/Microsoft.PowerShell.Commands.Management/commands/management/Navigation.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/Navigation.cs
@@ -3672,6 +3672,8 @@ namespace Microsoft.PowerShell.Commands
                         break;
                 }
             }
+            finally {
+            }
         }
 
         private void RenameItem(string path, bool literalPath = false)
