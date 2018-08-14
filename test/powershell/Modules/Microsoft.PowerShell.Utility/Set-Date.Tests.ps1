bc6eb0f8e8d26edc832a94d9ef52041ad2e14f4c
Travis Plunk <travis.plunk@microsoft.com>
2018-08-14 11:12:38 -0700

    skip some tests on vsts linux

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Set-Date.Tests.ps1
@@ -1,11 +1,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
+Import-Module HelpersCommon
+
 Describe "Set-Date for admin" -Tag @('CI', 'RequireAdminOnWindows', 'RequireSudoOnUnix') {
-    It "Set-Date should be able to set the date in an elevated context" {
+    # Fails in VSTS Linux with Operation not permitted
+    It "Set-Date should be able to set the date in an elevated context" -Skip:(Test-IsVstsLinux) {
         { Get-Date | Set-Date } | Should -Not -Throw
     }
 
-    It "Set-Date should be able to set the date with -Date parameter" {
+    # Fails in VSTS Linux with Operation not permitted
+    It "Set-Date should be able to set the date with -Date parameter" -Skip:(Test-IsVstsLinux) {
         $target = Get-Date
         $expected = $target
         Set-Date -Date $target | Should -Be $expected
