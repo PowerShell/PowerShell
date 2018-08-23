71ca7e5f19c8b37e5e9a63264e863a3ac7704772
Travis Plunk <travis.plunk@microsoft.com>
2018-08-23 15:55:42 -0700

    [feature] fix test failure preview versions are any version with dash in the name

--- test/powershell/Modules/Microsoft.PowerShell.Core/PSSessionConfiguration.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/PSSessionConfiguration.Tests.ps1
@@ -847,7 +847,7 @@ namespace PowershellTestConfigNamespace
         It "Enable-PSSession Cmdlet creates a default PSSession configuration untied to a specific PowerShell version." {
             $dotPos = $PSVersionTable.PSVersion.ToString().IndexOf(".")
             $endpointName = "PowerShell." + $PSVersionTable.PSVersion.ToString().Substring(0, $dotPos)
-            if ($PSVersionTable.GitCommitId.Contains("preview"))
+            if ($PSVersionTable.GitCommitId.Contains("-"))
             {
                 $endpointName += "-preview"
             }
