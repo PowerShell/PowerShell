2273dc5e7b746cb356611f5550f6b550b26dd06d
iSazonov <darpa@yandex.ru>
2018-09-04 19:41:33 +0500

    Add -Name and -ListAvailable parameters to Get-Culture cmdlet

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/GetCultureCommand.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/GetCultureCommand.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Globalization;
 using System.Management.Automation;
 
 namespace Microsoft.PowerShell.Commands
@@ -8,16 +9,53 @@ namespace Microsoft.PowerShell.Commands
     /// <summary>
     /// Returns the thread's current culture.
     /// </summary>
-    [Cmdlet(VerbsCommon.Get, "Culture", HelpUri = "https://go.microsoft.com/fwlink/?LinkID=113312")]
+    [Cmdlet(VerbsCommon.Get, "Culture", DefaultParameterSetName = NameParameterSet, HelpUri = "https://go.microsoft.com/fwlink/?LinkID=113312")]
     [OutputType(typeof(System.Globalization.CultureInfo))]
     public sealed class GetCultureCommand : PSCmdlet
     {
+        private const string NameParameterSet = "Name";
+        private const string ListAvailableParameterSet = "ListAvailable";
+
+        /// <summary>
+        /// Gets the specified culture.
+        /// </summary>
+        [Parameter(ParameterSetName = NameParameterSet)]
+        [ValidateNotNull]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// List all available cultures.
+        /// </summary>
+        [Parameter(ParameterSetName = ListAvailableParameterSet)]
+        public SwitchParameter ListAvailable { get; set; }
+
         /// <summary>
         /// Output the current Culture info object.
         /// </summary>
-        protected override void BeginProcessing()
+        protected override void EndProcessing()
         {
-            WriteObject(Host.CurrentCulture);
+            switch (ParameterSetName)
+            {
+                case NameParameterSet:
+                    if (Name == null)
+                    {
+                        WriteObject(Host.CurrentCulture);
+                    }
+                    else
+                    {
+                        CultureInfo ci = CultureInfo.GetCultureInfo(Name);
+                        WriteObject(ci);
+                    }
+
+                    break;
+                case ListAvailableParameterSet:
+                    foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+                    {
+                        WriteObject(ci);
+                    }
+
+                    break;
+            }
         }
     }
 }
--- test/powershell/Modules/Microsoft.PowerShell.Utility/Get-Culture.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Get-Culture.Tests.ps1
@@ -1,25 +1,32 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-Describe "Get-Culture DRT Unit Tests" -Tags "CI" {
-    It "Should works proper with get-culture" {
-        $results = get-Culture
-        $results -is "System.Globalization.CultureInfo" | Should -BeTrue
-        $results[0].Name | Should -Be $PSCulture
-    }
-}
 
 Describe "Get-Culture" -Tags "CI" {
 
     It "Should return a type of CultureInfo for Get-Culture cmdlet" {
 
-	Get-Culture | Should -BeOfType CultureInfo
+        Get-Culture | Should -BeOfType [CultureInfo]
 
     }
 
     It "Should have $ culture variable be equivalent to (Get-Culture).Name" {
 
-	(Get-Culture).Name | Should -Be $PsCulture
+        (Get-Culture).Name | Should -Be $PsCulture
 
     }
 
+    It "Should return the specified culture with '-Name' parameter" {
+
+        $ci = Get-Culture -Name ru-RU
+        $ci | Should -BeOfType [CultureInfo]
+        $ci.Name | Should -BeExactly "ru-RU"
+    }
+
+    It "Should return the culture array with '-ListAvailable' parameter" {
+
+        $ciArray = Get-Culture -ListAvailable
+        ,$ciArray | Should -BeOfType [System.Array]
+        $ciArray.Count | Should -BeGreaterThan 0
+        $ciArray[0] | Should -BeOfType [CultureInfo]
+    }
 }
