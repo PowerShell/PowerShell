34b404acd9c97cad5d84965c26d45d6bd1fd895c
iSazonov <darpa@yandex.ru>
2018-09-14 18:36:35 +0500

    Fix test

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Get-Culture.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Get-Culture.Tests.ps1
@@ -64,7 +64,7 @@ Describe "Get-Culture" -Tags "CI" {
         # that .Net Core behavior depend on the underlying OS
         # and can differ on different platforms.
         # See https://github.com/dotnet/corefx/issues/6374#issuecomment-418827420
-        $exc = { Get-Culture -Name "abcdefghijkl" -ErrorAction Stop } | Should -Throw -ErrorId "ItemNotFoundException,Microsoft.PowerShell.Commands.GetCultureCommand"
+        $exc = { Get-Culture -Name "abcdefghijkl" -ErrorAction Stop } | Should -PassThru -Throw -ErrorId "ItemNotFoundException,Microsoft.PowerShell.Commands.GetCultureCommand"
         $exc.Exception | Should -BeOfType [System.Globalization.CultureNotFoundException]
     }
 }
