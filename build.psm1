faea4315a85bbbe2a0dc93a859f064b268db0579
Robert Holt <rjmholt_msft@outlook.com>
2018-09-26 16:36:30 -0700

    Install SelfSignedCertificate rather than save it

--- build.psm1
+++ build.psm1
@@ -1037,7 +1037,7 @@ function Publish-PSTestTools {
     }
 
     # Get the SelfSignedCertificate module so the web listener can use it
-    Save-Module -Name SelfSignedCertificate -Path "$PSScriptRoot/test/tools/Modules/" -Repository "PSGallery" -MinimumVersion '0.0.2'
+    Install-Module -Name SelfSignedCertificate -Scope CurrentUser -Repository "PSGallery" -MinimumVersion '0.0.2'
 }
 
 function Get-ExperimentalFeatureTests {
