db62e111436ee709e83aa3de368ea2e22cbf5d0c
Robert Holt <rjmholt_msft@outlook.com>
2018-09-26 18:31:49 -0700

    Add -AllowClobber to Install-Module

--- build.psm1
+++ build.psm1
@@ -1037,7 +1037,7 @@ function Publish-PSTestTools {
     }
 
     # Get the SelfSignedCertificate module so the web listener can use it
-    Install-Module -Name SelfSignedCertificate -Scope CurrentUser -Repository "PSGallery" -MinimumVersion '0.0.2' -Force -Confirm:$false
+    Install-Module -Name SelfSignedCertificate -Scope CurrentUser -Repository "PSGallery" -MinimumVersion '0.0.2' -Force -Confirm:$false -AllowClobber
 }
 
 function Get-ExperimentalFeatureTests {
