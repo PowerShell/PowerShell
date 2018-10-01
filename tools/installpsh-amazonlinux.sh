651cb93f04384a1d2c89500272b0f3ba938955ab
Darwin <darwinjsgithub.services@sanoys.com>
2018-10-01 04:25:30 -0400

    fix syntax error for issue 7903

--- tools/installpsh-amazonlinux.sh
+++ tools/installpsh-amazonlinux.sh
@@ -168,7 +168,7 @@ if [[ "'$*'" =~ preview ]] ; then
 else
     echo "Finding the latest production release"
     release=$(curl https://api.github.com/repos/PowerShell/PowerShell/releases | grep -Po '"tag_name":(\d*?,|.*?[^\\]",)' | grep -Po '\d+.\d+.\d+[\da-z.-]*' | grep -v '[a-z]' | sort | tail -n1)
-if
+fi
 
 #DIRECT DOWNLOAD
 package=powershell-${release}-linux-x64.tar.gz
