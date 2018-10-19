2f31fef63258727237f1c830f80eecf3046228ac
Kirk Munro <KirkMunro@users.noreply.github.com>
2018-10-19 10:29:44 -0300

    CodeFactor changes

--- src/System.Management.Automation/engine/hostifaces/Connection.cs
+++ src/System.Management.Automation/engine/hostifaces/Connection.cs
@@ -1,15 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.Serialization;
-using System.Threading;
-using System.Management.Automation;
-using System.Management.Automation.Host;
-using System.Management.Automation.Internal;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Management.Automation;
 using Dbg = System.Management.Automation.Diagnostics;
+using System.Management.Automation.Host;
+using System.Management.Automation.Internal;
+using System.Runtime.Serialization;
+using System.Threading;
 
 namespace System.Management.Automation.Runspaces
 {
