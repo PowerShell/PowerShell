05e9f78bcb004c02235ff2ed9e6a8cca4d7c2176
Aditya Patwardhan <adityap@microsoft.com>
2018-10-23 16:31:04 -0700

    Publish test package at AGENT_WORKFOLDER if TEMP is not available (#8108)

--- build.psm1
+++ build.psm1
@@ -2997,7 +2997,13 @@ function New-TestPackage
         Write-Verbose -Message "Creating destination folder: $Destination"
     }
 
-    $packageRoot = Join-Path $env:TEMP ('TestPackage-' + (new-guid))
+    $rootFolder = $env:TEMP
+
+    if (-not $rootFolder -and $env:TF_BUILD) {
+        $rootFolder = $env:AGENT_WORKFOLDER
+    }
+
+    $packageRoot = Join-Path $rootFolder ('TestPackage-' + (new-guid))
     $null = New-Item -ItemType Directory -Path $packageRoot -Force
     $packagePath = Join-Path $Destination "TestPackage.zip"
 
