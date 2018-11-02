b643fe72bc6e6a35fa534a0d4e732887a9a37ff5
Ilya <darpa@yandex.ru>
2018-11-02 10:40:37 -0700

    Use BeExactly instead of Be
    Co-Authored-By: rjmholt <rjmholt@gmail.com>

--- test/powershell/Modules/Microsoft.PowerShell.Core/Get-Module.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Get-Module.Tests.ps1
@@ -164,7 +164,7 @@ Describe "Get-Module" -Tags "CI" {
         $modules.Count | Should -Be 5
         $modules = $modules | Sort-Object -Property Name, Version
         $modules.Name -join "," | Should -BeExactly "Az,Bar,Foo,Foo,Zoo"
-        $modules[2].Version | Should -Be "1.1"
+        $modules[2].Version | Should -BeExactly "1.1"
         $modules[3].Version | Should -Be '2.0'
     }
 
