f7e1f628be7917718e2f6128de0a768b1449fdb6
Kirk Munro <KirkMunro@users.noreply.github.com>
2018-11-04 17:14:59 -0400

    first crack at some C# unit tests

---
+++ test/csharp/test_PowerShell.cs
@@ -0,0 +1,56 @@
+using System.Diagnostics;
+using System.Management.Automation;
+using Xunit;
+
+namespace PSTests.Parallel
+{
+    public class PowerShellTests
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task TestPowerShellInvokeAsync()
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.AddCommand("Get-Process")
+                  .AddParameter("Id", Process.GetCurrentProcess().Id);
+
+                PSDataCollection<PSObject> results = await ps.InvokeAsync();
+
+                Assert.Single(results);
+                Assert.IsType<Process>(results[0]?.BaseObject);
+                Assert.Equal(Process.GetCurrentProcess().Id, ((Process)results[0].BaseObject).Id);
+            }
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task TestPowerShellInvokeAsyncWithInput()
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.AddCommand("Get-Command");
+
+                PSDataCollection<PSObject> results = await ps.InvokeAsync(new PSDataCollection<string>(new string[] { "Get-Command" }));
+
+                Assert.Single(results);
+                Assert.IsType<CmdletInfo>(results[0]?.BaseObject);
+                Assert.Equal("Get-Command", ((CmdletInfo)results[0].BaseObject).Name);
+            }
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task TestPowerShellInvokeAsyncWithInputAndOutput()
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.AddCommand("Get-Command");
+
+                PSDataCollection<CmdletInfo> results = new PSDataCollection<CmdletInfo>();
+                await ps.InvokeAsync(new PSDataCollection<string>(new string[] { "Get-Command" }), results);
+
+                Assert.Single(results);
+                Assert.IsType<CmdletInfo>(results[0]);
+                Assert.Equal("Get-Command", results[0].Name);
+            }
+        }
+    }
+}
