e42db00b753edc9f8d1c2c272d2c18f9eaf0229e
Kirk Munro <KirkMunro@users.noreply.github.com>
2018-11-04 20:21:54 -0400

    updated PowerShell member count

--- test/powershell/Host/TabCompletion/TabCompletion.Tests.ps1
+++ test/powershell/Host/TabCompletion/TabCompletion.Tests.ps1
@@ -696,7 +696,7 @@ dir -Recurse `
         It "Test member completion of a static method invocation" {
             $inputStr = '[powershell]::Create().'
             $res = TabExpansion2 -inputScript $inputStr -cursorColumn $inputStr.Length
-            $res.CompletionMatches | Should -HaveCount 31
+            $res.CompletionMatches | Should -HaveCount 32
             $res.CompletionMatches[0].CompletionText | Should -BeExactly "Commands"
         }
     }
