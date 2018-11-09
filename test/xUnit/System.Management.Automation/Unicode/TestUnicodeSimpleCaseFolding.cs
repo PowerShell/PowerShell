c5298ae5d9f66c95815e10f5427fb8c6a263430d
iSazonov <darpa@yandex.ru>
2018-11-09 15:53:25 +0500

    Add IndexOfFolded tests

--- test/xUnit/System.Management.Automation/Unicode/TestUnicodeSimpleCaseFolding.cs
+++ test/xUnit/System.Management.Automation/Unicode/TestUnicodeSimpleCaseFolding.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.Management.Automation.Unicode;
 using Xunit;
 
@@ -9,10 +11,99 @@ namespace System.Management.Automation.Unicode.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("", '\u007f', -1)]
+        [InlineData("Hello", 'o', 4)]
+        [InlineData("Hello", 'O', 4)]
+        [InlineData("Hello", 'h', 0)]
+        [InlineData("Hello", 'H', 0)]
+        [InlineData("Hello", 'g', -1)]
+        [InlineData("Hello", 'G', -1)]
+        [InlineData("HelLo", '\0', -1)]
+        [InlineData("!@#$%", '%', 4)]
+        [InlineData("!@#$", '!', 0)]
+        [InlineData("!@#$", '@', 1)]
+        [InlineData("_____________\u807f", '\u007f', -1)]
+        [InlineData("_____________\u807f__", '\u007f', -1)]
+        [InlineData("_____________\u807f\u007f_", '\u007f', 14)]
+        [InlineData("__\u807f_______________", '\u007f', -1)]
+        [InlineData("__\u807f___\u007f___________", '\u007f', 6)]
+        public void Test_IndexOfFolded(string s, char target, int expected)
         {
-            Assert.Equal(0, "".CompareFolded(""));
+            Assert.Equal(expected, s.IndexOfFolded(target));
+        }
+
+        // Follow tests comes from src\System.Runtime\tests\System\StringTests.netcoreapp.cs
+        [Fact]
+        public static void IndexOf_TurkishI_TurkishCulture_Char()
+        {
+            var savedCulture = CultureInfo.CurrentCulture;
+            try
+            {
+                CultureInfo.CurrentCulture = new CultureInfo("tr-TR");
+
+                string s = "Turkish I \u0131s TROUBL\u0130NG!";
+                char value = '\u0130';
+                Assert.Equal(19, s.IndexOf(value));
+                Assert.Equal(19, s.IndexOf(value, StringComparison.CurrentCulture));
+                Assert.Equal(4, s.IndexOf(value, StringComparison.CurrentCultureIgnoreCase));
+                Assert.Equal(19, s.IndexOf(value, StringComparison.Ordinal));
+                Assert.Equal(19, s.IndexOf(value, StringComparison.OrdinalIgnoreCase));
+
+                ReadOnlySpan<char> span = s.AsSpan();
+                Assert.Equal(19, span.IndexOf(new char[] { value }, StringComparison.CurrentCulture));
+                Assert.Equal(4, span.IndexOf(new char[] { value }, StringComparison.CurrentCultureIgnoreCase));
+                Assert.Equal(19, span.IndexOf(new char[] { value }, StringComparison.Ordinal));
+                Assert.Equal(19, span.IndexOf(new char[] { value }, StringComparison.OrdinalIgnoreCase));
+
+                value = '\u0131';
+                Assert.Equal(10, s.IndexOf(value, StringComparison.CurrentCulture));
+                Assert.Equal(8, s.IndexOf(value, StringComparison.CurrentCultureIgnoreCase));
+                Assert.Equal(10, s.IndexOf(value, StringComparison.Ordinal));
+                Assert.Equal(10, s.IndexOf(value, StringComparison.OrdinalIgnoreCase));
+
+                Assert.Equal(10, span.IndexOf(new char[] { value }, StringComparison.CurrentCulture));
+                Assert.Equal(8, span.IndexOf(new char[] { value }, StringComparison.CurrentCultureIgnoreCase));
+                Assert.Equal(10, span.IndexOf(new char[] { value }, StringComparison.Ordinal));
+                Assert.Equal(10, span.IndexOf(new char[] { value }, StringComparison.OrdinalIgnoreCase));
+            }
+            finally
+            {
+                CultureInfo.CurrentCulture = savedCulture;
+            }
+        }
+
+        [Fact]
+        public static void IndexOf_TurkishI_Char()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            string s = "Turkish I \u0131s TROUBL\u0130NG!";
+            char value = '\u0130';
+            Assert.Equal(19, s.IndexOfFolded(value));
+
+            value = '\u0131';
+            Assert.Equal(10, s.IndexOfFolded(value));
+        }
+
+        [Fact]
+        public static void IndexOf_EquivalentDiacritics_Char()
+        {
+            string s = "Exhibit a\u0300\u00C0";
+            char value = '\u00C0';
+            Assert.Equal(10, s.IndexOfFolded(value));
+
+            value = '\u0300';
+            Assert.Equal(9, s.IndexOfFolded(value));
+        }
+
+        [Fact]
+        public static void IndexOf_CyrillicE_Char()
+        {
+            string s = "Foo\u0400Bar";
+            char value = '\u0400';
+
+            Assert.Equal(3, s.IndexOfFolded(value));
         }
     }
 }
