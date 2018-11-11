be9c5a4dc143310b32888e3178e289451d904e27
Robert Holt <rjmholt_msft@outlook.com>
2018-11-10 17:29:21 -0800

    [Feature] Add requires -Module tests

--- test/powershell/Language/Scripting/Requires.Tests.ps1
+++ test/powershell/Language/Scripting/Requires.Tests.ps1
@@ -4,13 +4,13 @@ Describe "Requires tests" -Tags "CI" {
     Context "Parser error" {
 
         $testcases = @(
-                        @{command = "#requiresappID`r`n$foo = 1; $foo" ; testname = "appId with newline"}
-                        @{command = "#requires -version A `r`n$foo = 1; $foo" ; testname = "version as character"}
-                        @{command = "#requires -version 2b `r`n$foo = 1; $foo" ; testname = "alphanumeric version"}
-                        @{command = "#requires -version 1. `r`n$foo = 1; $foo" ; testname = "version with dot"}
-                        @{command = "#requires -version '' `r`n$foo = 1; $foo" ; testname = "empty version"}
-                        @{command = "#requires -version 1.0. `r`n$foo = 1; $foo" ; testname = "version with two dots"}
-                        @{command = "#requires -version 1.A `r`n$foo = 1; $foo" ; testname = "alphanumeric version with dots"}
+                        @{command = "#requiresappID`r`n`$foo = 1; `$foo" ; testname = "appId with newline"}
+                        @{command = "#requires -version A `r`n`$foo = 1; `$foo" ; testname = "version as character"}
+                        @{command = "#requires -version 2b `r`n`$foo = 1; `$foo" ; testname = "alphanumeric version"}
+                        @{command = "#requires -version 1. `r`n`$foo = 1; `$foo" ; testname = "version with dot"}
+                        @{command = "#requires -version '' `r`n`$foo = 1; `$foo" ; testname = "empty version"}
+                        @{command = "#requires -version 1.0. `r`n`$foo = 1; `$foo" ; testname = "version with two dots"}
+                        @{command = "#requires -version 1.A `r`n`$foo = 1; `$foo" ; testname = "alphanumeric version with dots"}
                     )
 
         It "throws ParserException - <testname>" -TestCases $testcases {
@@ -34,6 +34,112 @@ Describe "Requires tests" -Tags "CI" {
             $settings.AddToHistory = $true
 
             { $ps.AddScript("#requires").Invoke(@(), $settings) } | Should -Not -Throw
+        }
+    }
+}
+
+Describe "#requires -Modules" {
+    BeforeAll {
+        $success = 'SUCCESS'
+
+        $moduleName = 'Banana'
+        $moduleVersion = '0.12.1'
+        $moduleDirPath = Join-Path $TestDrive 'modules'
+        $modulePath = Join-Path $moduleDirPath $moduleName
+        $manifestPath = Join-Path $modulePath "$moduleName.psd1"
+        $psm1Path = Join-Path $modulePath "$moduleName.psm1"
+        New-Item -Path $psm1Path -Value "function Test-RequiredModule { '$success' }"
+        New-ModuleManifest -Path $manifestPath -ModuleVersion $moduleVersion
+    }
+
+    Context "Requiring non-existent modules" {
+        BeforeAll {
+            $badName = 'ModuleThatDoesNotExist'
+            $badPath = Join-Path $TestDrive 'ModuleThatDoesNotExist'
+            $version = '1.0'
+            $testCases = @(
+                @{ ModuleRequirement = "'$badName'"; Scenario = 'name' }
+                @{ ModuleRequirement = "'$badPath'"; Scenario = 'path' }
+                @{ ModuleRequirement = "@{ ModuleName = '$badName'; ModuleVersion = '$version' }"; Scenario = 'fully qualified name with name' }
+                @{ ModuleRequirement = "@{ ModuleName = '$badPath'; ModuleVersion = '$version' }"; Scenario = 'fully qualified name with path' }
+            )
+        }
+
+        It "Fails parsing a script that requires module by <Scenario>" -TestCases $testCases {
+            param([string]$ModuleRequirement, [string]$Scenario)
+
+            $script = "#requires -Module $ModuleRequirement`n`nWrite-Output 'failed'"
+            { & $script } | Should -Throw -ErrorId 'ParseException'
+        }
+    }
+
+    Context "Already loaded module" {
+        BeforeAll {
+            Import-Module $moduleName
+            $testCases = @(
+                @{ ModuleRequirement = "'$moduleName'"; Scenario = 'name' }
+                @{ ModuleRequirement = "'$modulePath'"; Scenario = 'path' }
+                @{ ModuleRequirement = "'$manifestPath'"; Scenario = 'manifest path' }
+                @{ ModuleRequirement = "@{ ModuleName='$moduleName'; ModuleVersion='$moduleVersion' }"; Scenario = 'fully qualified name with name' }
+                @{ ModuleRequirement = "@{ ModuleName='$modulePath'; ModuleVersion='$moduleVersion' }"; Scenario = 'fully qualified name with path' }
+                @{ ModuleRequirement = "@{ ModuleName='$manifestPath'; ModuleVersion='$moduleVersion' }"; Scenario = 'fully qualified name with manifest path' }
+            )
+        }
+
+        AfterAll {
+            Remove-Module $moduleName -ErrorAction SilentlyContinue
+        }
+
+        It "Successfully runs a script requiring a loaded module by <Scenario>" -TestCases $testCases {
+            param([string]$ModuleRequirement, [string]$Scenario)
+
+            $script = "#requires -Modules $ModuleRequirement`n`nTest-RequiredModule"
+            & $script | Should -BeExactly $success
+        }
+    }
+
+    Context "Loading by name" {
+        BeforeAll {
+            $oldModulePath = $env:PSModulePath
+            $env:PSModulePath = $moduleDirPath + [System.IO.Path]::PathSeparator + $env:PSModulePath
+
+            $testCases = @(
+                @{ ModuleRequirement = "'$moduleName'"; Scenario = 'name' }
+                @{ ModuleRequirement = "'$modulePath'"; Scenario = 'path' }
+                @{ ModuleRequirement = "'$manifestPath'"; Scenario = 'manifest path' }
+                @{ ModuleRequirement = "@{ ModuleName='$moduleName'; ModuleVersion='$moduleVersion' }"; Scenario = 'fully qualified name with name' }
+                @{ ModuleRequirement = "@{ ModuleName='$modulePath'; ModuleVersion='$moduleVersion' }"; Scenario = 'fully qualified name with path' }
+                @{ ModuleRequirement = "@{ ModuleName='$manifestPath'; ModuleVersion='$moduleVersion' }"; Scenario = 'fully qualified name with manifest path' }
+            )
+        }
+
+        AfterAll {
+            $env:PSModulePath = $oldModulePath
+        }
+
+        It "Successfully runs a script requiring a module on the module path by <Scenario>" -TestCases $testCases {
+            param([string]$ModuleRequirement, [string]$Scenario)
+
+            $script = "#requires -Modules $ModuleRequirement`n`nTest-RequiredModule"
+            & $script | Should -BeExactly $success
+        }
+    }
+
+    Context "Loading by absolute path" {
+        BeforeAll {
+            $testCases = @(
+                @{ ModuleRequirement = "'$modulePath'"; Scenario = 'path' }
+                @{ ModuleRequirement = "'$manifestPath'"; Scenario = 'manifest path' }
+                @{ ModuleRequirement = "@{ ModuleName='$modulePath'; ModuleVersion='$moduleVersion' }"; Scenario = 'fully qualified name with path' }
+                @{ ModuleRequirement = "@{ ModuleName='$manifestPath'; ModuleVersion='$moduleVersion' }"; Scenario = 'fully qualified name with manifest path' }
+            )
+        }
+
+        It "Successfully runs a script requiring a module by absolute path by <Scenario>" -TestCases $testCases {
+            param([string]$ModuleRequirement, [string]$Scenario)
+
+            $script = "#requires -Modules $ModuleRequirement`n`nTest-RequiredModule"
+            & $script | Should -BeExactly $success
         }
     }
 }
