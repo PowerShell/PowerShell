28ed3054c85fe64d2f0baa9b53829963a2f74ca0
Steve Lee <slee@microsoft.com>
2018-11-12 14:26:38 -0800

    [feature] added tests

---
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Enter-PSHostProcess.Tests.ps1
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+Describe "Enter-PSHostProcess tests" -Tag Feature {
+    BeforeAll {
+        $si = [System.Diagnostics.ProcessStartInfo]::new()
+        $si.FileName = "pwsh"
+        $si.Arguments = "-noexit"
+        $si.RedirectStandardInput = $true
+        $si.RedirectStandardOutput = $true
+        $si.RedirectStandardError = $true
+        $pwsh = [System.Diagnostics.Process]::Start($si)
+    }
+
+    AfterAll {
+        $pwsh | Stop-Process
+    }
+
+    It "Can enter and exit another PSHost" {
+        "enter-pshostprocess -id $($pwsh.Id)`n`$pid`nexit-pshostprocess" | pwsh -c - | Should -Be $pwsh.Id
+    }
+}
---
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Get-PSHostProcessInfo.Tests.ps1
@@ -0,0 +1,32 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+Describe "Get-PSHostProcessInfo tests" -Tag CI {
+    BeforeAll {
+        $si = [System.Diagnostics.ProcessStartInfo]::new()
+        $si.FileName = "pwsh"
+        $si.Arguments = "-noexit"
+        $si.RedirectStandardInput = $true
+        $si.RedirectStandardOutput = $true
+        $si.RedirectStandardError = $true
+        $pwsh = [System.Diagnostics.Process]::Start($si)
+    }
+
+    AfterAll {
+        $pwsh | Stop-Process
+    }
+
+    It "Should not return own self" {
+        Get-PSHostProcessInfo | Select-Object -ExpandProperty ProcessId | Should -Not -Contain $pid
+    }
+
+    It "Should list info for other PowerShell hosted processes" {
+        # Creation of the named pipe is async
+        Wait-UntilTrue {
+            Get-PSHostProcessInfo | Where-Object { $_.ProcessId -eq $pwsh.Id }
+        }
+        $pshosts = Get-PSHostProcessInfo
+        $pshosts.Count | Should -BeGreaterOrEqual 1
+        $pshosts | Select-Object -ExpandProperty ProcessId | Should -Contain $pwsh.Id
+    }
+}
