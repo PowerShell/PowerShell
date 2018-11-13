a70f6b68d95a3e92a94941caa5a07a81a73e102a
iSazonov <darpa@yandex.ru>
2018-11-13 18:10:51 +0500

    Fix race condition to access powershell.config.json

--- src/System.Management.Automation/engine/PSConfiguration.cs
+++ src/System.Management.Automation/engine/PSConfiguration.cs
@@ -355,7 +355,8 @@ namespace System.Management.Automation.Configuration
             fileLock.EnterReadLock();
             try
             {
-                using (var streamReader = new StreamReader(fileName))
+                using (FileStream readerStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var streamReader = new StreamReader(readerStream))
                 using (var jsonReader = new JsonTextReader(streamReader))
                 {
                     var settings = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.None, MaxDepth = 10 };
