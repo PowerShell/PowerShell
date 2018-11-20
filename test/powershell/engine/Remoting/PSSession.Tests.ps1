3cb156224a30da40eb0dbcbc3ad24a6ade20963b
PaulHigin <paulhi@microsoft.com>
2018-11-20 15:15:20 -0800

    Changes to conform to test pattern requirement

--- test/powershell/engine/Remoting/PSSession.Tests.ps1
+++ test/powershell/engine/Remoting/PSSession.Tests.ps1
@@ -4,74 +4,81 @@
 #
 # PSSession tests for non-Windows platforms
 #
-try
-{
-    if ( $IsWindows ) {
+
+Describe "New-PSSessionOption parameters for non-Windows platforms" -Tag "CI" {
+
+    BeforeAll {
         $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
-        $PSDefaultParameterValues['it:skip'] = $true
-    }
 
-    Describe "New-PSSessionOption parameters for non-Windows platforms" -Tag "CI" {
-
-        It "Verifies New-PSSessionOption parameters" {
-
-            $cmdInfo = Get-Command New-PSSessionOption
-
-            $commonParameterCount = [System.Management.Automation.Internal.CommonParameters].GetProperties().Length
-            $cmdInfo.Parameters.Count | Should -Be ($commonParameterCount + 2) -Because "Only -SkipCACheck and -SkipCNCheck switch parameters are available"
-
-            { $null = $cmdInfo.ResolveParameter("SkipCACheck") } | Should -Not -Throw -Because "SkipCACheck parameter should be available"
-            { $null = $cmdInfo.ResolveParameter("SkipCNCheck") } | Should -Not -Throw -Because "SkipCNCheck parameter should be available"
+        if ($IsWindows)  {
+            $PSDefaultParameterValues['it:skip'] = $true
         }
     }
 
-    Describe "SkipCACheck and SkipCNCheck PSSession options are required for New-PSSession on non-Windows platforms" -Tag "CI" {
+    AfterAll {
+        $global:PSDefaultParameterValues = $originalDefaultParameterValues
+    }
 
-        BeforeAll {
-            if (! $IsWindows) {
-                $userName = "User_$(Get-Random -Maximum 99999)"
-                $userPassword = "Password_$(Get-Random -Maximum 99999)"
-                $cred = [pscredential]::new($userName, (ConvertTo-SecureString -String $userPassword -AsPlainText -Force))
-                $soSkipCA = New-PSSessionOption -SkipCACheck
-                $soSkipCN = New-PSSessionOption -SkipCNCheck
-            }
-        }
+    It "Verifies New-PSSessionOption parameters" {
 
-        $testCases = @(
-            @{
-                Name = 'Verifies expected error when session options is missing'
-                ScriptBlock = { New-PSSession -cn localhost -Credential $cred -Authentication Basic -UseSSL }
-                ExpectedErrorCode = 825
-            },
-            @{
-                Name = 'Verifies expected error when SkipCACheck option is missing'
-                ScriptBlock = { New-PSSession -cn localhost -Credential $cred -Authentication Basic -UseSSl -SessionOption $soSkipCN }
-                ExpectedErrorCode = 825
-            },
-            @{
-                Name = 'Verifies expected error when SkipCNCheck option is missing'
-                ScriptBlock = { New-PSSession -cn localhost -Credential $cred -Authentication Basic -UseSSl -SessionOption $soSkipCA }
-                ExpectedErrorCode = 825
-            }
-        )
+        $cmdInfo = Get-Command New-PSSessionOption
 
-        It "<Name>" -TestCases $testCases {
-            param ($scriptBlock, $expectedErrorCode)
+        $commonParameterCount = [System.Management.Automation.Internal.CommonParameters].GetProperties().Length
+        $cmdInfo.Parameters.Count | Should -Be ($commonParameterCount + 2) -Because "Only -SkipCACheck and -SkipCNCheck switch parameters are available"
 
-            try {
-                & $scriptBlock
-                throw "No Exception!"
-            }
-            catch {
-                $_.Exception.ErrorCode | Should -Be $expectedErrorCode
-            }
-        }
+        { $null = $cmdInfo.ResolveParameter("SkipCACheck") } | Should -Not -Throw -Because "SkipCACheck parameter should be available"
+        { $null = $cmdInfo.ResolveParameter("SkipCNCheck") } | Should -Not -Throw -Because "SkipCNCheck parameter should be available"
     }
 }
-finally
-{
-    if ($originalDefaultParameterValues)
-    {
-        $PSDefaultParameterValues = $originalDefaultParameterValues
+
+Describe "SkipCACheck and SkipCNCheck PSSession options are required for New-PSSession on non-Windows platforms" -Tag "CI" {
+
+    BeforeAll {
+        $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+
+        if ($IsWindows)  {
+            $PSDefaultParameterValues['it:skip'] = $true
+        }
+        else {
+            $userName = "User_$(Get-Random -Maximum 99999)"
+            $userPassword = "Password_$(Get-Random -Maximum 99999)"
+            $cred = [pscredential]::new($userName, (ConvertTo-SecureString -String $userPassword -AsPlainText -Force))
+            $soSkipCA = New-PSSessionOption -SkipCACheck
+            $soSkipCN = New-PSSessionOption -SkipCNCheck
+        }
+    }
+
+    AfterAll {
+        $global:PSDefaultParameterValues = $originalDefaultParameterValues
+    }
+
+    $testCases = @(
+        @{
+            Name = 'Verifies expected error when session options is missing'
+            ScriptBlock = { New-PSSession -cn localhost -Credential $cred -Authentication Basic -UseSSL }
+            ExpectedErrorCode = 825
+        },
+        @{
+            Name = 'Verifies expected error when SkipCACheck option is missing'
+            ScriptBlock = { New-PSSession -cn localhost -Credential $cred -Authentication Basic -UseSSl -SessionOption $soSkipCN }
+            ExpectedErrorCode = 825
+        },
+        @{
+            Name = 'Verifies expected error when SkipCNCheck option is missing'
+            ScriptBlock = { New-PSSession -cn localhost -Credential $cred -Authentication Basic -UseSSl -SessionOption $soSkipCA }
+            ExpectedErrorCode = 825
+        }
+    )
+
+    It "<Name>" -TestCases $testCases {
+        param ($scriptBlock, $expectedErrorCode)
+
+        try {
+            & $scriptBlock
+            throw "No Exception!"
+        }
+        catch {
+            $_.Exception.ErrorCode | Should -Be $expectedErrorCode
+        }
     }
 }
