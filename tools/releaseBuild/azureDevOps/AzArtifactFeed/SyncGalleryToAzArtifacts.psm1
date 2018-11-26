7139efb8daf53846d4e58e75a17f88f428f31f7f
Aditya Patwardhan <adityap@microsoft.com>
2018-11-26 15:49:19 -0800

    Address CR feedback

--- tools/releaseBuild/azureDevOps/AzArtifactFeed/SyncGalleryToAzArtifacts.psm1
+++ tools/releaseBuild/azureDevOps/AzArtifactFeed/SyncGalleryToAzArtifacts.psm1
@@ -1,6 +1,17 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+<#
+.SYNOPSIS
+Downloads to packages from PowerShell Gallery which are missing from the Azure DevOps Artifacts feed.
+
+.PARAMETER AzureDevOpsPAT
+PAT for the username used for authenticating to the Azure DevOps Artifacts feed.
+
+.PARAMETER Destination
+Path to the folder where the packages should be stored for uploading to Azure DevOps Artifacts feed.
+
+#>
 function SyncGalleryToAzArtifacts {
     param(
         [Parameter(Mandatory = $true)] [string] $AzDevOpsPAT,
@@ -19,16 +30,16 @@ function SyncGalleryToAzArtifacts {
 
     $azDevOpsCreds = [pscredential]::new($env:AzDevOpsUserName, (ConvertTo-SecureString -String $AzDevOpsPAT -AsPlainText -Force))
 
-    foreach ($p in $packages) {
+    foreach ($package in $packages) {
         try {
             # Get module from gallery
-            $foundPackageOnGallery = Find-Package -ProviderName NuGet -Source $galleryUrl -AllVersions -Name $p.Name -Force -AllowPreReleaseVersion | Sort-Object -Property Version -Descending | Select-Object -First 1
-            Write-Verbose -Verbose "Found module $($p.Name) - $($foundPackageOnGallery.Version) in gallery"
+            $foundPackageOnGallery = Find-Package -ProviderName NuGet -Source $galleryUrl -AllVersions -Name $package.Name -Force -AllowPreReleaseVersion | Sort-Object -Property Version -Descending | Select-Object -First 1
+            Write-Verbose -Verbose "Found module $($package.Name) - $($foundPackageOnGallery.Version) in gallery"
             $galleryPackages += $foundPackageOnGallery
         } catch {
             if ($_.FullyQualifiedErrorId -eq 'NoMatchFoundForCriteria,Microsoft.PowerShell.PackageManagement.Cmdlets.FindPackage') {
                 # Log and ignore failure is required version is not found on gallery.
-                Write-Warning "Module not found on gallery $($p.Name) - $($p.Version)"
+                Write-Warning "Module not found on gallery $($package.Name) - $($package.Version)"
             }
             else {
                 Write-Error $_
@@ -39,14 +50,14 @@ function SyncGalleryToAzArtifacts {
             # Get module from Az Artifacts
             # There seems to be a bug in the feed with RequiredVersion matching. Adding workaround with post filtering.
             # Issue: https://github.com/OneGet/oneget/issues/397
-            $foundPackageOnAz = Find-Package -ProviderName NuGet -Source $azArtifactsUrl -AllVersions -Name $p.Name -Force -Credential $azDevOpsCreds -AllowPreReleaseVersion | Sort-Object -Property Version -Descending | Select-Object -First 1
-            Write-Verbose -Verbose "Found module $($p.Name) - $($foundPackageOnAz.Version) in azArtifacts"
+            $foundPackageOnAz = Find-Package -ProviderName NuGet -Source $azArtifactsUrl -AllVersions -Name $package.Name -Force -Credential $azDevOpsCreds -AllowPreReleaseVersion | Sort-Object -Property Version -Descending | Select-Object -First 1
+            Write-Verbose -Verbose "Found module $($package.Name) - $($foundPackageOnAz.Version) in azArtifacts"
             $azArtifactsPackages += $foundPackageOnAz
         } catch {
             if ($_.FullyQualifiedErrorId -eq 'NoMatchFoundForCriteria,Microsoft.PowerShell.PackageManagement.Cmdlets.FindPackage') {
                 # Log and add the module to update list.
-                Write-Verbose -Verbose "Az Artifacts Module needs update to - $($p.Name) - $($p.Version)"
-                $modulesToUpdate += $p
+                Write-Verbose -Verbose "Az Artifacts Module needs update to - $($package.Name) - $($package.Version)"
+                $modulesToUpdate += $package
             }
             else {
                 Write-Error $_
@@ -55,12 +66,12 @@ function SyncGalleryToAzArtifacts {
 
         # Check if Az package version is less that gallery version
         if ($foundPackageOnAz.Version -lt $foundPackageOnGallery.Version) {
-            Write-Verbose -Verbose "Module needs to be updated $($p.Name) - $($foundPackageOnGallery.Version)"
+            Write-Verbose -Verbose "Module needs to be updated $($package.Name) - $($foundPackageOnGallery.Version)"
             $modulesToUpdate += $foundPackageOnGallery
         } elseif ($foundPackageOnGallery.Version -lt $foundPackageOnAz.Version) {
             Write-Warning "Newer version found on Az Artifacts - $($foundPackageOnAz.Name) - $($foundPackageOnAz.Version)"
         } else {
-            Write-Verbose -Verbose "Module is in sync - $($p.Name)"
+            Write-Verbose -Verbose "Module is in sync - $($package.Name)"
         }
     }
 
@@ -73,8 +84,8 @@ function SyncGalleryToAzArtifacts {
     "Modules to update:`n"
     $modulesToUpdate
 
-    foreach ($p in $modulesToUpdate) {
-        Save-Package -Provider NuGet -Source $galleryUrl -Name $p.Name -RequiredVersion $p.Version -Path $Destination
+    foreach ($package in $modulesToUpdate) {
+        Save-Package -Provider NuGet -Source $galleryUrl -Name $package.Name -RequiredVersion $package.Version -Path $Destination
     }
 
     # Remove dependent packages downloaded by Save-Module if there are already present in AzArtifacts feed.
@@ -83,12 +94,12 @@ function SyncGalleryToAzArtifacts {
         $packageNamesToKeep = @()
         $savedPackages = Find-Package -Source local -AllVersions -AllowPreReleaseVersion
 
-        foreach($p in $savedPackages) {
-            $foundMatch = $azArtifactsPackages | Where-Object { $_.Name -eq $p.Name -and $_.Version -eq $p.Version }
+        foreach($package in $savedPackages) {
+            $foundMatch = $azArtifactsPackages | Where-Object { $_.Name -eq $package.Name -and $_.Version -eq $package.Version }
 
             if(-not $foundMatch) {
-                Write-Verbose "Keeping package $($p.PackageFileName)"
-                $packageNamesToKeep += $p.PackageFilename
+                Write-Verbose "Keeping package $($package.PackageFileName)"
+                $packageNamesToKeep += $package.PackageFilename
             }
         }
 
