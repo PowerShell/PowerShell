ecf82cf7ec042d4fc569c219035f2e993b4bca16
Ilya <darpa@yandex.ru>
2018-11-30 09:25:32 -0800

    Update test/powershell/engine/ExperimentalFeature/Get-ExperimentalFeature.Tests.ps1
    Co-Authored-By: SteveL-MSFT <slee@microsoft.com>

--- test/powershell/engine/ExperimentalFeature/Get-ExperimentalFeature.Tests.ps1
+++ test/powershell/engine/ExperimentalFeature/Get-ExperimentalFeature.Tests.ps1
@@ -65,7 +65,7 @@ Describe "Get-ExperimentalFeature Tests" -tags "Feature","RequireAdminOnWindows"
             $features = pwsh -noprofile -output xml -command { "ExpTest.FeatureOne", "ExpTest.FeatureTwo" | Get-ExperimentalFeature }
             $features | Should -Not -BeNullOrEmpty
             $features[0].Name | Should -BeExactly "ExpTest.FeatureOne"
-            $features[0].Enabled | Should -Be $false
+            $features[0].Enabled | Should -BeFalse
             $features[0].Source | Should -BeExactly $testModuleManifestPath
 
             $features[1].Name | Should -BeExactly "ExpTest.FeatureTwo"
