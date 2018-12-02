2bab38e2cf8189b6aa29b581a53dd1231df9229e
Mathias R. Jessen <mathias@mrjessen.com>
2018-12-02 19:16:35 +0100

    Update ShouldImplementProperty() to flatten interface hierarchy
    TypeBuilder.GetInterfaces() returns only the interfaces that was explicitly passed to its constructor, so the current implementation doesn't work for inherited interfaces:
    
      Add-Type 'public interface ITestBase { string Base {get;} }
                public interface ITest : ITestBase { string Test {get;} }'
      class : ITest
      {
        [string]$Test
        [string]$Base
      }
    
    In this example, ShouldImplementProperty() won't resolve ITestBase.Base and type definition fails.
    During compilation the interface hierarchy is flattened, so we only need to resolve one level of ancestral interfaces.

--- src/System.Management.Automation/engine/parser/PSType.cs
+++ src/System.Management.Automation/engine/parser/PSType.cs
@@ -266,6 +266,7 @@ namespace System.Management.Automation.Language
             internal readonly TypeBuilder _staticHelpersTypeBuilder;
             private readonly Dictionary<string, PropertyMemberAst> _definedProperties;
             private readonly Dictionary<string, List<Tuple<FunctionMemberAst, Type[]>>> _definedMethods;
+            private HashSet<Tuple<string, Type>> _interfaceProperties;
             internal readonly List<(string fieldName, IParameterMetadataProvider bodyAst, bool isStatic)> _fieldsToInitForMemberFunctions;
             private bool _baseClassHasDefaultCtor;
 
@@ -436,15 +437,29 @@ namespace System.Management.Automation.Language
 
             private bool ShouldImplementProperty(string name, Type type)
             {
-                foreach (var interfaceType in _typeBuilder.GetInterfaces())
+                if (_interfaceProperties == null)
                 {
-                    if (interfaceType.GetProperty(name, type) != null)
+                    _interfaceProperties = new HashSet<Tuple<string, Type>>();
+                    foreach (var interfaceType in _typeBuilder.GetInterfaces())
                     {
-                        return true;
+                        // TypeBuilder.GetInterfaces() returns only the interfaces that was explicitly passed to its constructor.
+                        // During compilation the interface hierarchy is flattened, so we only need to resolve one level of ancestral interfaces.
+                        foreach (var parentInterface in interfaceType.GetInterfaces())
+                        {
+                            foreach (var property in parentInterface.GetProperties())
+                            {
+                                _interfaceProperties.Add(new Tuple<string, Type>(property.Name, property.PropertyType));
+                            }
+                        }
+
+                        foreach (var property in interfaceType.GetProperties())
+                        {
+                            _interfaceProperties.Add(new Tuple<string, Type>(property.Name, property.PropertyType));
+                        }
                     }
                 }
 
-                return false;
+                return _interfaceProperties.Contains(new Tuple<string, Type>(name, type));
             }
 
             public void DefineMembers()
