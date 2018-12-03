f555a2fb78ff9e40fc1e039eb7ca51c18f602f4e
Joel <32407840+vexx32@users.noreply.github.com>
2018-12-03 13:21:17 -0500

    Re-add missing using statement and I guess this is why we keep them alphabetical

--- src/System.Management.Automation/engine/Utils.cs
+++ src/System.Management.Automation/engine/Utils.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
