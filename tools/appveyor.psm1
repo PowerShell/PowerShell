4b926937362f2bd1e536f9c7f898f37f929844f8
Travis Plunk <github@ez13.net>
2018-12-05 15:11:40 -0800

    [Feature] remove appveyor code

--- tools/appveyor.psm1
+++ tools/appveyor.psm1
@@ -531,11 +531,7 @@ function Push-Artifact
         $artifactName = $Name
     }
 
-    if($env:Appveyor)
-    {
-        Push-AppveyorArtifact $Path
-    }
-    elseif ($env:TF_BUILD -and $env:BUILD_REASON -ne 'PullRequest') {
+    if ($env:TF_BUILD -and $env:BUILD_REASON -ne 'PullRequest') {
         # In VSTS
         Write-Host "##vso[artifact.upload containerfolder=$artifactName;artifactname=$artifactName;]$Path"
     }
