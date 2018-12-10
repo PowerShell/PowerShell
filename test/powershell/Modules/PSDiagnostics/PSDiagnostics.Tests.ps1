331112db2e268440a908180909b27b3d1be6b1e8
kvprasoon <prasoonkarunan5@hotmail.com>
2018-12-10 23:51:17 +0530

    Changes as per review comments

--- test/powershell/Modules/PSDiagnostics/PSDiagnostics.Tests.ps1
+++ test/powershell/Modules/PSDiagnostics/PSDiagnostics.Tests.ps1
@@ -3,52 +3,59 @@
 
 Describe "PSDiagnostics cmdlets tests" -Tag "CI", "RequireAdminOnWindows" {
     BeforeAll {
+        $LogType = 'Analytic'
+        $LogSettingBak = Get-LogProperties -Name Microsoft-Windows-PowerShell/$LogType
         $OriginalDefaultParameterValues = $PSDefaultParameterValues.Clone()
         if ( -not $IsWindows ) {
             $PSDefaultParameterValues["it:skip"] = $true
         }
     }
     AfterAll {
+        Set-LogProperties -LogDetails $LogSettingBak -Force
         $Global:PSDefaultParameterValues = $OriginalDefaultParameterValues
     }
 
-    Context "Test for Enable-PSTrace cmdlet" {
-        it "Should enable Analytic logs for Microsoft-Windows-PowerShell." {
+    Context "Test for Enable-PSTrace and Disable-PSTrace cmdlet" {
+        it "Should enable $LogType logs for Microsoft-Windows-PowerShell." {
+            [XML]$CurrentSetting = & wevtutil gl Microsoft-Windows-PowerShell/$LogType /f:xml
+            if($CurrentSetting.Channel.Enabled -eq 'true'){
+                & wevtutil sl Microsoft-Windows-PowerShell/$LogType /e:false /q
+            }
+
             Enable-PSTrace -Force
 
-            [XML]$WevtUtilOutput = & wevtutil gl Microsoft-Windows-PowerShell/Analytic /f:xml
+            [XML]$ExpectedOutput = & wevtutil gl Microsoft-Windows-PowerShell/$LogType /f:xml
 
-            $WevtUtilOutput.Channel.enabled | Should -Be 'true'
+            $ExpectedOutput.Channel.enabled | Should -BeExactly 'true'
         }
-    }
 
-    Context "Test for Disable-PSTrace cmdlet" {
-        it "Should disable Analytic logs for Microsoft-Windows-PowerShell." {
+        it "Should disable $LogType logs for Microsoft-Windows-PowerShell." {
             Disable-PSTrace
 
-            [XML]$WevtUtilOutput = & wevtutil gl Microsoft-Windows-PowerShell/Analytic /f:xml
+            [XML]$ExpectedOutput = & wevtutil gl Microsoft-Windows-PowerShell/$LogType /f:xml
 
-            $WevtUtilOutput.Channel.enabled | Should -Be 'false'
+            $ExpectedOutput.Channel.enabled | Should -Be 'false'
         }
     }
 
     Context "Test for Get-LogProperties cmdlet" {
-        it "Should show properties of Admin logs for 'Microsoft-Windows-PowerShell'." {
-            [XML]$WevtUtilOutput = wevtutil gl Microsoft-Windows-PowerShell/Admin /f:xml
+        it "Should return properties of $LogType logs for 'Microsoft-Windows-PowerShell'." {
+            [XML]$ExpectedOutput = wevtutil gl Microsoft-Windows-PowerShell/$LogType /f:xml
 
-            $LogProperty = Get-LogProperties -Name Microsoft-Windows-PowerShell/Admin
+            $LogProperty = Get-LogProperties -Name Microsoft-Windows-PowerShell/$LogType
 
-            $LogProperty.Name       | Should -Be $WevtUtilOutput.channel.Name
-            $LogProperty.Enabled    | Should -Be $WevtUtilOutput.channel.Enabled
-            $LogProperty.Retention  | Should -Be $WevtUtilOutput.channel.Logging.Retention
-            $LogProperty.AutoBackup | Should -Be $WevtUtilOutput.channel.Logging.AutoBackup
-            $LogProperty.MaxLogSize | Should -Be $WevtUtilOutput.channel.Logging.MaxSize
+            $LogProperty.Name       | Should -Be $ExpectedOutput.channel.Name
+            $LogProperty.Enabled    | Should -Be $ExpectedOutput.channel.Enabled
+            $LogProperty.Retention  | Should -Be $ExpectedOutput.channel.Logging.Retention
+            $LogProperty.AutoBackup | Should -Be $ExpectedOutput.channel.Logging.AutoBackup
+            $LogProperty.MaxLogSize | Should -Be $ExpectedOutput.channel.Logging.MaxSize
+            ($LogProperty | Get-Member -MemberType Property | Measure-Object).Count |
+            Should -Be (($ExpectedOutput.Channel.Logging | Get-Member -MemberType Property | Measure-Object).Count + 2)
         }
     }
 
     Context "Test for Set-LogProperties cmdlet" {
         BeforeAll {
-            $LogType = 'Analytic'
             if ($IsWindows) {
                 [XML]$WevtUtilBefore = wevtutil gl Microsoft-Windows-PowerShell/$LogType /f:xml
                 $LogPropertyToSet = [Microsoft.PowerShell.Diagnostics.LogDetails]::new($WevtUtilBefore.channel.Name,
@@ -65,11 +72,11 @@ Describe "PSDiagnostics cmdlets tests" -Tag "CI", "RequireAdminOnWindows" {
             $LogPropertyToSet.AutoBackup = -not $LogPropertyToSet.AutoBackup
             Set-LogProperties -LogDetails $LogPropertyToSet -Force
 
-            [XML]$WevtUtilOutput = & wevtutil gl Microsoft-Windows-PowerShell/$LogType /f:xml
-            (Get-LogProperties -Name Microsoft-Windows-PowerShell/$LogType).AutoBackup | Should -Be ([bool]::Parse($WevtUtilOutput.Channel.Logging.AutoBackup))
+            [XML]$ExpectedOutput = & wevtutil gl Microsoft-Windows-PowerShell/$LogType /f:xml
+            (Get-LogProperties -Name Microsoft-Windows-PowerShell/$LogType).AutoBackup | Should -Be ([bool]::Parse($ExpectedOutput.Channel.Logging.AutoBackup))
         }
 
-        it "Should throw excpetion for invalid LogName." {
+        it "Should throw exception for invalid LogName." {
             {Set-LogProperties -LogDetails 'Foo' -Force } | Should -Throw -ErrorId 'ParameterArgumentTransformationError'
         }
     }
