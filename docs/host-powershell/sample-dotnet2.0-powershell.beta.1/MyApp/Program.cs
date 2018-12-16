d5f743a3f3e545543ee729c5ec88388f05208c63
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-12-16 14:53:26 +0000

    Update examples with newer version of .Net Core and NuGet APIs. Delete .Net Core 2.2 example and update APIs being called

--- docs/host-powershell/sample-dotnet2.0-powershell.beta.1/MyApp/Program.cs
+++ docs/host-powershell/sample-dotnet2.0-powershell.beta.1/MyApp/Program.cs
@@ -11,7 +11,7 @@ namespace Application.Test
         /// <summary>
         /// Managed entry point shim, which starts the actual program
         /// </summary>
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
             // Application needs to use PowerShell AssemblyLoadContext if it needs to create powershell runspace
             // PowerShell engine depends on PS ALC to provide the necessary assembly loading/searching support that is missing from .NET Core
@@ -19,13 +19,7 @@ namespace Application.Test
             System.Console.WriteLine("\nappBase: {0}", appBase);
 
             // Initialize the PS ALC and let it load 'Logic.dll' and start the execution
-            return (int)PowerShellAssemblyLoadContextInitializer.
-                           InitializeAndCallEntryMethod(
-                               appBase,
-                               new AssemblyName("Logic, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"),
-                               "Application.Test.Logic",
-                               "Start",
-                               new object[] { args });
+            System.Management.Automation.PowerShellAssemblyLoadContextInitializer.SetPowerShellAssemblyLoadContext(appBase);
         }
     }
 }
