2eb7ca8309bb002d1cfaecb6da32b702fb69346c
Steve Lee <slee@microsoft.com>
2018-12-21 23:57:44 -0800

    Fix formatting of header of table when center aligned (#8497)

--- src/System.Management.Automation/FormatAndOutput/common/TableWriter.cs
+++ src/System.Management.Automation/FormatAndOutput/common/TableWriter.cs
@@ -386,7 +386,7 @@ namespace Microsoft.PowerShell.Commands.Internal.Format
         {
             StringCollection sc = StringManipulationHelper.GenerateLines(dc, val,
                                         _si.columnInfo[k].width, _si.columnInfo[k].width);
-            if (addPadding || alignment == TextAlignment.Right)
+            if (addPadding || alignment == TextAlignment.Right || alignment == TextAlignment.Center)
             {
                 // if length is shorter, do some padding
                 for (int col = 0; col < sc.Count; col++)
