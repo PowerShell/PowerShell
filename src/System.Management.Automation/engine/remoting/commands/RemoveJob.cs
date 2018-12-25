4b6ebc2bab93108aa921d33a0c8485477864a21f
iSazonov <darpa@yandex.ru>
2018-12-25 08:39:54 +0500

    Fix RemoveJob.cs

--- src/System.Management.Automation/engine/remoting/commands/RemoveJob.cs
+++ src/System.Management.Automation/engine/remoting/commands/RemoveJob.cs
@@ -46,7 +46,7 @@ namespace Microsoft.PowerShell.Commands
         /// Find the jobs in repository which match matching the specified names
         /// </summary>
         /// <param name="writeobject">If true, method writes the object instead of returning it
-        /// in list (an empty list is returned). .</param>
+        /// in list (an empty list is returned).</param>
         /// <param name="writeErrorOnNoMatch">Write error if no match is found.</param>
         /// <param name="checkIfJobCanBeRemoved">Check if this job can be removed.</param>
         /// <param name="recurse">Recurse and check in child jobs.</param>
@@ -179,7 +179,7 @@ namespace Microsoft.PowerShell.Commands
         /// Find the jobs in repository which match the specified instanceid
         /// </summary>
         /// <param name="writeobject">If true, method writes the object instead of returning it
-        /// in list (an empty list is returned). .</param>
+        /// in list (an empty list is returned).</param>
         /// <param name="writeErrorOnNoMatch">Write error if no match is found.</param>
         /// <param name="checkIfJobCanBeRemoved">Check if this job can be removed.</param>
         /// <param name="recurse">Look in all child jobs.</param>
@@ -296,7 +296,7 @@ namespace Microsoft.PowerShell.Commands
         /// Find the jobs in repository which match the specified session ids
         /// </summary>
         /// <param name="writeobject">If true, method writes the object instead of returning it
-        /// in list (an empty list is returned). .</param>
+        /// in list (an empty list is returned).</param>
         /// <param name="writeErrorOnNoMatch">Write error if no match is found.</param>
         /// <param name="checkIfJobCanBeRemoved">Check if this job can be removed.</param>
         /// <param name="recurse">Look in child jobs as well.</param>
@@ -400,7 +400,7 @@ namespace Microsoft.PowerShell.Commands
         /// Find the jobs in repository which match the specified command
         /// </summary>
         /// <param name="writeobject">If true, method writes the object instead of returning it
-        /// in list (an empty list is returned). .</param>
+        /// in list (an empty list is returned).</param>
         /// <returns>list of matching jobs</returns>
         internal List<Job> FindJobsMatchingByCommand(
             bool writeobject)
@@ -453,7 +453,7 @@ namespace Microsoft.PowerShell.Commands
         /// Find the jobs in repository which match the specified state
         /// </summary>
         /// <param name="writeobject">If true, method writes the object instead of returning it
-        /// in list (an empty list is returned). .</param>
+        /// in list (an empty list is returned).</param>
         /// <returns>list of matching jobs</returns>
         internal List<Job> FindJobsMatchingByState(
             bool writeobject)
@@ -551,7 +551,7 @@ namespace Microsoft.PowerShell.Commands
         /// </summary>
         /// <param name="jobs"></param>
         /// <param name="writeobject">If true, method writes the object instead of returning it
-        /// in list (an empty list is returned). .</param>
+        /// in list (an empty list is returned).</param>
         /// <param name="checkIfJobCanBeRemoved">If true, only jobs which can be removed will be checked.</param>
         /// <returns></returns>
         internal List<Job> CopyJobsToList(Job[] jobs, bool writeobject, bool checkIfJobCanBeRemoved)
