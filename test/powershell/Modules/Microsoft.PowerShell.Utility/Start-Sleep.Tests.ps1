a21c44ff3ec95688140c193aa3901afae2853bac
Prototyyppi <34347399+Prototyyppi@users.noreply.github.com>
2018-12-25 12:31:42 +0200

    Modified existing tests to test fractional seconds

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Start-Sleep.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Start-Sleep.Tests.ps1
@@ -3,16 +3,15 @@
 Describe "Start-Sleep DRT Unit Tests" -Tags "CI" {
 
     # WaitHandle.WaitOne(milliseconds, exitContext) is not accurate.
-    # The wait time varies from 980ms to 1150ms from observation, so
-    # the tests here are changed to be greater than 950ms.
-    $minTime = 950
-    $maxTime = 1200
-    $minTimeFractional = 1450
-    $maxTimeFractional = 1700
+    # The wait time varies from 980ms to 1150ms from observation
+    # when waiting for one second. To test fractional seconds
+    # the expected values start from 1450ms and end to 1700ms.
+    $minTime = 1450
+    $maxTime = 1700
 
     It "Should work properly when sleeping with Second" {
         $watch = [System.Diagnostics.Stopwatch]::StartNew()
-        Start-Sleep -Seconds 1
+        Start-Sleep -Seconds 1.5
         $watch.Stop()
         $watch.ElapsedMilliseconds | Should -BeGreaterThan $minTime
         $watch.ElapsedMilliseconds | Should -BeLessThan $maxTime
@@ -20,7 +19,7 @@ Describe "Start-Sleep DRT Unit Tests" -Tags "CI" {
 
     It "Should work properly when sleeping with Milliseconds" {
         $watch = [System.Diagnostics.Stopwatch]::StartNew()
-        Start-Sleep -Milliseconds 1000
+        Start-Sleep -Milliseconds 1500
         $watch.Stop()
         $watch.ElapsedMilliseconds | Should -BeGreaterThan $minTime
         $watch.ElapsedMilliseconds | Should -BeLessThan $maxTime
@@ -28,35 +27,26 @@ Describe "Start-Sleep DRT Unit Tests" -Tags "CI" {
 
     It "Should work properly when sleeping with ms alias" {
         $watch = [System.Diagnostics.Stopwatch]::StartNew()
-        Start-Sleep -ms 1000
+        Start-Sleep -ms 1500
         $watch.Stop()
         $watch.ElapsedMilliseconds | Should -BeGreaterThan $minTime
         $watch.ElapsedMilliseconds | Should -BeLessThan $maxTime
     }
 
-    It "Should work properly when sleeping 1.5 seconds without parameters" {
+    It "Should work properly when sleeping without parameters" {
         $watch = [System.Diagnostics.Stopwatch]::StartNew()
         Start-Sleep 1.5
         $watch.Stop()
-        $watch.ElapsedMilliseconds | Should -BeGreaterThan $minTimeFractional
-        $watch.ElapsedMilliseconds | Should -BeLessThan $maxTimeFractional
-    }
-
-    It "Should work properly when sleeping 1.5 seconds using Second parameter" {
-        $watch = [System.Diagnostics.Stopwatch]::StartNew()
-        Start-Sleep -Seconds 1.5
-        $watch.Stop()
-        $watch.ElapsedMilliseconds | Should -BeGreaterThan $minTimeFractional
-        $watch.ElapsedMilliseconds | Should -BeLessThan $maxTimeFractional
+        $watch.ElapsedMilliseconds | Should -BeGreaterThan $minTime
+        $watch.ElapsedMilliseconds | Should -BeLessThan $maxTime
     }
 }
 
 Describe "Start-Sleep" -Tags "CI" {
-
     Context "Validate Start-Sleep works properly" {
-	It "Should only sleep for at least 1 second" {
-	    $result = Measure-Command { Start-Sleep -s 1 }
-	    $result.TotalSeconds | Should -BeGreaterThan 0.25
-	}
+        It "Should only sleep for at least 1 second" {
+            $result = Measure-Command { Start-Sleep -s 1 }
+            $result.TotalSeconds | Should -BeGreaterThan 0.25
+        }
     }
 }
