d21868a82bd5f1404fdc43ce6375a7082f399ff7
Rene Hernandez Remedios <rhernandezremedios@kinaxis.com>
2019-01-02 17:00:48 -0500

    Copy PathSeparator property in copy ctor

--- src/System.Management.Automation/engine/DataStoreAdapterProvider.cs
+++ src/System.Management.Automation/engine/DataStoreAdapterProvider.cs
@@ -300,6 +300,7 @@ namespace System.Management.Automation
             PSSnapIn = providerInfo.PSSnapIn;
             _sessionState = providerInfo._sessionState;
             VolumeSeparatedByColon = providerInfo.VolumeSeparatedByColon;
+            PathSeparator = providerInfo.PathSeparator;
         }
 
         /// <summary>
