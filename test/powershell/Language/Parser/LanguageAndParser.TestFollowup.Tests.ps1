6e4faa05385c7e452d0ee1d5f8300a9dbe797082
Christoph Bergmeister [MVP] <c.bergmeister@gmail.com>
2019-01-04 18:14:21 +0900

    Update test/powershell/Language/Parser/LanguageAndParser.TestFollowup.Tests.ps1
    Co-Authored-By: SteveL-MSFT <slee@microsoft.com>

--- test/powershell/Language/Parser/LanguageAndParser.TestFollowup.Tests.ps1
+++ test/powershell/Language/Parser/LanguageAndParser.TestFollowup.Tests.ps1
@@ -306,6 +306,6 @@ Describe "Hash expression with if statement as value" -Tags "CI" {
 
 Describe "Hashtable is case insensitive" -Tag CI {
     It "When LANG is C.UTF-8" -Skip:($IsWindows) {
-        sh -c 'LANG=C.UTF-8 pwsh -c ''$h=@{h=1};$h.H''' | Should -Be 1
+        sh -c 'LANG=C.UTF-8 pwsh -NoProfile -Command ''$h=@{p=1};$h.P''' | Should -Be 1
     }
 }
