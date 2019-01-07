ffa00a25f54823b3b52fa274427439a90254d571
Steve Lee <slee@microsoft.com>
2019-01-07 12:30:38 -0800

    address Aditya's feedback

--- src/System.Management.Automation/engine/InitialSessionState.cs
+++ src/System.Management.Automation/engine/InitialSessionState.cs
@@ -4169,7 +4169,7 @@ param(
 
     $help = Get-Help @PSBoundParameters
 
-    # If a list of help is returned, don't pipe to more
+    # If a list of help is returned or AliasHelpInfo (because it is small), don't pipe to more
     $psTypeNames = ($help | Select-Object -First 1).PSTypeNames
     if ($psTypeNames -Contains 'HelpInfoShort' -Or $psTypeNames -Contains 'AliasHelpInfo')
     {
