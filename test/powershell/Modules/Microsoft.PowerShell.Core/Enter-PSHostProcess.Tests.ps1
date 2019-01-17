fcfb629aa6fbae3013c2f6a7e89c6527cf9db921
Dongbo Wang <dongbow@microsoft.com>
2019-01-17 10:28:28 -0800

    Remove the comment about appveyor

--- test/powershell/Modules/Microsoft.PowerShell.Core/Enter-PSHostProcess.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Enter-PSHostProcess.Tests.ps1
@@ -26,7 +26,6 @@ Describe "Enter-PSHostProcess tests" -Tag Feature {
         }
     }
 
-    # Skip on Appveyor due to PSReadline issue.
     It "Can enter and exit another PSHost" {
         "enter-pshostprocess -id $($pwsh.Id)`n`$pid`nexit-pshostprocess" | pwsh -c - | Should -Be $pwsh.Id
     }
