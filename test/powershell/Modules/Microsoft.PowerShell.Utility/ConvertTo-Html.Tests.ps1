529fb98c14f7b8907646e8f16aeeeedcc48ab9ef
Michael Klement <mklement0@gmail.com>
2019-01-17 18:16:16 -0500

    ???

--- test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Html.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Html.Tests.ps1
@@ -169,16 +169,6 @@ After the object
         $returnString | Should -Be '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
     }
 
-    It "Test ConvertTo-HTML supports scriptblock-based calculated properties: by hashtable"{
-        $returnString = ($customObject | ConvertTo-HTML @{ l='NewAge'; e={ $_.Age + 1 } }) -join $newLine
-        $returnString | Should -Match '\b43\b'
-    }
-
-    It "Test ConvertTo-HTML supports scriptblock-based calculated properties: directly"{
-        $returnString = ($customObject | ConvertTo-HTML { $_.Age + 1 }) -join $newLine
-        $returnString | Should -Match '\b43\b'
-    }
-
     It "Test ConvertTo-HTML calculated property supports 'name' key as alias of 'label'" {
         $returnString = ($customObject | ConvertTo-Html @{ name = 'AgeRenamed'; e = 'Age'}) -join $newLine
         $returnString | Should -Match 'AgeRenamed'
