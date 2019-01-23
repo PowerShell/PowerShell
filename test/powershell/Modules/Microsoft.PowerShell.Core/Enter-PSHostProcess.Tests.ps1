5cacb59441cd0f90e6b7ad517a83ed01368fe044
Steve Lee (POWERSHELL) <slee@ntdev.microsoft.com>
2019-01-23 13:58:47 -0800

    [feature] address Aditya's feedback

--- test/powershell/Modules/Microsoft.PowerShell.Core/Enter-PSHostProcess.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Enter-PSHostProcess.Tests.ps1
@@ -32,17 +32,13 @@ Describe "Enter-PSHostProcess tests" -Tag Feature {
     }
 
     It "Can enter and exit another PSHost" {
-        Wait-UntilTrue {
-            (Test-Path $pwsh_started) -eq $true
-        }
+        Wait-UntilTrue { Test-Path $pwsh_started }
 
         "enter-pshostprocess -id $($pwsh.Id)`n`$pid`nexit-pshostprocess" | pwsh -c - | Should -Be $pwsh.Id
     }
 
     It "Can enter and exit another Windows PowerShell PSHost" -Skip:(!$IsWindows) {
-        Wait-UntilTrue {
-            (Test-Path $powershell_started) -eq $true
-        }
+        Wait-UntilTrue { Test-Path $powershell_started }
 
         "enter-pshostprocess -id $($powershell.Id)`n`$pid`nexit-pshostprocess" | pwsh -c - | Should -Be $powershell.Id
     }
