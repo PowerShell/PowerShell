b7d5d2ae3cc4be9ceafc49f84603cdd09f4930a8
Reece Dunham <me@rdil.rocks>
2019-02-05 19:30:02 -0500

    Update Reset-PWSHSystemPath.ps1

--- tools/windows/Reset-PWSHSystemPath.ps1
+++ tools/windows/Reset-PWSHSystemPath.ps1
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
 <#
 .SYNOPSIS
   Idempotently removes extra PowerShell Core paths from the machine, user and/or process environment scope with no reordering.
