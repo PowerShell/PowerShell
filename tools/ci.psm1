46458b5d5c0dd1c472648ea3475af21ca5e4b8e0
Travis Plunk <travis.plunk@microsoft.com>
2019-02-07 14:07:15 -0500

    Update tools/ci.psm1
    Co-Authored-By: RDIL <jumbocakefdx@gmail.com>

--- tools/ci.psm1
+++ tools/ci.psm1
@@ -767,6 +767,11 @@ function Invoke-LinuxTests
                 Write-Log "pushing $package to $env:NUGET_URL"
                 Start-NativeExecution -sb {dotnet nuget push $package --api-key $NugetKey --source "$env:NUGET_URL/api/v2/package"} -IgnoreExitcode
             }
+            
+            if ($isDailyBuild)
+            {
+                New-TestPackage -Destination "${env:SYSTEM_ARTIFACTSDIRECTORY}"
+            }
 
             if($isDailyBuild)
 	    {
