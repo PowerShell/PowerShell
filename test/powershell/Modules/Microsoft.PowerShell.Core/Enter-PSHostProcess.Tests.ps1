72d76ab53521c2aecc3629fe8552441a7c4d8d83
Tyler Leonhardt <tylerl0706@gmail.com>
2019-02-13 13:29:31 -0800

    [feature] add more tests and pass command down to ServerMode

--- test/powershell/Modules/Microsoft.PowerShell.Core/Enter-PSHostProcess.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Enter-PSHostProcess.Tests.ps1
@@ -2,71 +2,105 @@
 # Licensed under the MIT License.
 
 Describe "Enter-PSHostProcess tests" -Tag Feature {
-    BeforeAll {
-        $pwsh_started = New-TemporaryFile
-        $si = [System.Diagnostics.ProcessStartInfo]::new()
-        $si.FileName = "pwsh"
-        $si.Arguments = "-noexit -command 'pwsh' > '$pwsh_started'"
-        $si.RedirectStandardInput = $true
-        $si.RedirectStandardOutput = $true
-        $si.RedirectStandardError = $true
-        $pwsh = [System.Diagnostics.Process]::Start($si)
+    Context "By Process Id" {
+        BeforeAll {
+            $pwsh_started = New-TemporaryFile
+            $si = [System.Diagnostics.ProcessStartInfo]::new()
+            $si.FileName = "pwsh"
+            $si.Arguments = "-noexit -command 'pwsh' > '$pwsh_started'"
+            $si.RedirectStandardInput = $true
+            $si.RedirectStandardOutput = $true
+            $si.RedirectStandardError = $true
+            $pwsh = [System.Diagnostics.Process]::Start($si)
 
-        if ($IsWindows) {
-            $powershell_started = New-TemporaryFile
-            $si.FileName = "powershell"
-            $si.Arguments = "-noexit -command 'powershell' >'$powershell_started'"
-            $powershell = [System.Diagnostics.Process]::Start($si)
+            if ($IsWindows) {
+                $powershell_started = New-TemporaryFile
+                $si.FileName = "powershell"
+                $si.Arguments = "-noexit -command 'powershell' >'$powershell_started'"
+                $powershell = [System.Diagnostics.Process]::Start($si)
+            }
+
         }
 
-    }
+        AfterAll {
+            $pwsh | Stop-Process
+            Remove-Item $pwsh_started -Force -ErrorAction SilentlyContinue
 
-    AfterAll {
-        $pwsh | Stop-Process
-        Remove-Item $pwsh_started -Force -ErrorAction SilentlyContinue
+            if ($IsWindows) {
+                $powershell | Stop-Process
+                Remove-Item $powershell_started -Force -ErrorAction SilentlyContinue
+            }
+        }
 
-        if ($IsWindows) {
-            $powershell | Stop-Process
-            Remove-Item $powershell_started -Force -ErrorAction SilentlyContinue
+        It "Can enter and exit another PSHost" {
+            Wait-UntilTrue { Test-Path $pwsh_started }
+
+            "enter-pshostprocess -id $($pwsh.Id)
+            `$pid
+            exit-pshostprocess" | pwsh -c - | Should -Be $pwsh.Id
+        }
+
+        It "Can enter and exit another Windows PowerShell PSHost" -Skip:(!$IsWindows) {
+            Wait-UntilTrue { Test-Path $powershell_started }
+
+            "enter-pshostprocess -id $($powershell.Id)
+            `$pid
+            exit-pshostprocess" | pwsh -c - | Should -Be $powershell.Id
+        }
+
+        It "Can enter using NamedPipeConnectionInfo" {
+            $npInfo = [System.Management.Automation.Runspaces.NamedPipeConnectionInfo]::new($pwsh.Id)
+            $rs = [runspacefactory]::CreateRunspace($npInfo)
+            $rs.Open()
+            $ps = [powershell]::Create()
+            $ps.Runspace = $rs
+            $ps.AddScript('$pid').Invoke() | Should -Be $pwsh.Id
+            $rs.Dispose()
         }
     }
 
-    It "Can enter and exit another PSHost" {
-        Wait-UntilTrue { Test-Path $pwsh_started }
+    Context "By DebugPipeName" {
+        BeforeAll {
+            $pipeName = [System.IO.Path]::GetRandomFileName()
+            $pwsh_started = New-TemporaryFile
+            $si = [System.Diagnostics.ProcessStartInfo]::new()
+            $si.FileName = "pwsh"
+            $si.Arguments = "-debugpipename $pipeName -noexit -command 'pwsh -debugpipename $pipeName' > '$pwsh_started'"
+            $si.RedirectStandardInput = $true
+            $si.RedirectStandardOutput = $true
+            $si.RedirectStandardError = $true
+            $pwsh = [System.Diagnostics.Process]::Start($si)
+        }
 
-        "enter-pshostprocess -id $($pwsh.Id)`n`$pid`nexit-pshostprocess" | pwsh -c - | Should -Be $pwsh.Id
-    }
+        AfterAll {
+            $pwsh | Stop-Process
+            Remove-Item $pwsh_started -Force -ErrorAction SilentlyContinue
+        }
 
-    It "Can enter and exit another Windows PowerShell PSHost" -Skip:(!$IsWindows) {
-        Wait-UntilTrue { Test-Path $powershell_started }
+        It "Can enter using DebugPipeName" {
+            Wait-UntilTrue { Test-Path $pwsh_started }
 
-        "enter-pshostprocess -id $($powershell.Id)`n`$pid`nexit-pshostprocess" | pwsh -c - | Should -Be $powershell.Id
-    }
+            "enter-pshostprocess -debugpipename $pipeName
+            `$pid
+            exit-pshostprocess" | pwsh -c - | Should -Be $pwsh.Id
+        }
 
-    It "Can enter using NamedPipeConnectionInfo" {
-        $npInfo = [System.Management.Automation.Runspaces.NamedPipeConnectionInfo]::new($pwsh.Id)
-        $rs = [runspacefactory]::CreateRunspace($npInfo)
-        $rs.Open()
-        $ps = [powershell]::Create()
-        $ps.Runspace = $rs
-        $ps.AddScript('$pid').Invoke() | Should -Be $pwsh.Id
-        $rs.Dispose()
-    }
+        It "Can enter, exit, and re-enter using DebugPipeName" {
+            Wait-UntilTrue { Test-Path $pwsh_started }
 
-    It "Can enter using DebugPipeName" {
+            "enter-pshostprocess -debugpipename $pipeName
+            `$pid
+            exit-pshostprocess" | pwsh -c - | Should -Be $pwsh.Id
 
-        $pipeName = [System.IO.Path]::GetRandomFileName()
-        $pwsh_started = New-TemporaryFile
-        $si = [System.Diagnostics.ProcessStartInfo]::new()
-        $si.FileName = "pwsh"
-        $si.Arguments = "-debugpipename $pipeName -noexit -command 'pwsh -debugpipename $pipeName' > '$pwsh_started'"
-        $si.RedirectStandardInput = $true
-        $si.RedirectStandardOutput = $true
-        $si.RedirectStandardError = $true
-        $pwsh = [System.Diagnostics.Process]::Start($si)
+            "enter-pshostprocess -debugpipename $pipeName
+            `$pid
+            exit-pshostprocess" | pwsh -c - | Should -Be $pwsh.Id
+        }
 
-        Wait-UntilTrue { Test-Path $pwsh_started }
+        It "Should throw if DebugPipeName does not exist" {
+            Wait-UntilTrue { Test-Path $pwsh_started }
 
-        "enter-pshostprocess -debugpipename $pipeName`n`$pid`nexit-pshostprocess" | pwsh -c - | Should -Be $pwsh.Id
+            { Enter-PSHostProcess -DebugPipeName badpipename } | Should -Throw -ExpectedMessage "No named pipe was found with DebugPipeName: badpipename."
+        }
     }
 }
