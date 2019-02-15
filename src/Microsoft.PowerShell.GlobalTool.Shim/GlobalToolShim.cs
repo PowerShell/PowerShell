df36bc4b977ee1d398aeb665a753d7f554d013ce
Aditya Patwardhan <adityap@microsoft.com>
2019-02-15 13:14:57 -0800

    Add shim dll to redirect to appropriate runtime

---
+++ src/Microsoft.PowerShell.GlobalTool.Shim/GlobalToolShim.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Microsoft.PowerShell.GlobalTool.Shim
+{
+    /// <summary>
+    /// Shim layer to chose the appropriate runtime for PowerShell Core DotNet Global tool.
+    /// </summary>
+    public class EntryPoint
+    {
+        private const string PwshDllName = "pwsh.dll";
+
+        private const string WinFolderName = "win";
+
+        private const string UnixFolderName = "unix";
+
+        /// <summary>
+        /// Entry point for the global tool
+        /// </summary>
+        /// <param name="args">Arguments passed to the global tool.</param>
+        public static void Main(string[] args)
+        {
+            var currentPath = new FileInfo(System.Reflection.Assembly.GetEntryAssembly().Location).Directory.FullName;
+            var isWindows = Environment.OSVersion.Platform == System.PlatformID.Win32NT;
+
+            string platformFolder = isWindows ? WinFolderName : UnixFolderName;
+
+            string argsString = args.Length > 0 ? string.Join(" ", args) : null;
+            var pwshPath = Path.Combine(currentPath, platformFolder, PwshDllName);
+            string processArgs = string.IsNullOrEmpty(argsString) ? $"{pwshPath}" : $"{pwshPath} -c {argsString}";
+
+            if (File.Exists(pwshPath))
+            {
+                System.Diagnostics.Process.Start("dotnet", processArgs).WaitForExit();
+            }
+            else
+            {
+                throw new FileNotFoundException(pwshPath);
+            }
+        }
+    }
+}
