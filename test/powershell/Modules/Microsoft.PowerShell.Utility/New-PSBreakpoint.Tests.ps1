c4fa3569d3564050cf0656facf630398a4350434
Tyler Leonhardt <tylerl0706@gmail.com>
2019-02-21 15:02:41 -0800

    handle feature enabled disabled

--- test/powershell/Modules/Microsoft.PowerShell.Utility/New-PSBreakpoint.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/New-PSBreakpoint.Tests.ps1
@@ -1,12 +1,22 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 $ps = Join-Path -Path $PsHome -ChildPath "pwsh"
+$FeatureEnabled = $EnabledExperimentalFeatures.Contains('Microsoft.PowerShell.Utility.PSDebugRunspaceWithBreakpoints')
 
-Describe "New-PSBreakpoint DRT Unit Tests" -Tags "CI" {
-    #Set up
-    $scriptFileName = Join-Path $TestDrive -ChildPath breakpointTestScript.ps1
+Describe "New-PSBreakpoint Unit Tests - Feature-Enabled" -Tags "CI" {
 
-    $contents = @"
+    BeforeAll {
+        if (!$FeatureEnabled) {
+            Write-Verbose "Test Suite Skipped. The test suite requires the experimental feature 'Microsoft.PowerShell.Utility.PSDebugRunspaceWithBreakpoints' to be enabled." -Verbose
+            $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+            $PSDefaultParameterValues["it:skip"] = $true
+            return
+        }
+
+        #Set up script file 1
+        $scriptFileName1 = Join-Path $TestDrive -ChildPath breakpointTestScript.ps1
+
+        $contents = @"
 function Hello
 {
     `$greeting = 'Hello, world!'
@@ -29,51 +39,68 @@ Goodbye
 return
 "@
 
-    $contents > $scriptFileName
+        $contents > $scriptFileName1
+
+        # Set up script file 2
+        $scriptFileName2 = Join-Path -Path $PSScriptRoot -ChildPath psbreakpointtestscript.ps1
+
+        "`$var = 1 " > $scriptFileName2
+    }
+
+    AfterAll {
+        if (!$FeatureEnabled) {
+            $global:PSDefaultParameterValues = $originalDefaultParameterValues
+            return
+        }
+
+        # clean up
+        Remove-Item -Path $scriptFileName1 -Force -ErrorAction SilentlyContinue
+        Remove-Item -Path $scriptFileName2 -Force -ErrorAction SilentlyContinue
+    }
 
     It "Should be able to set psbreakpoints for -Line" {
-        $brk = New-PSBreakpoint -Line 13 -Script $scriptFileName
+        $brk = New-PSBreakpoint -Line 13 -Script $scriptFileName1
         $brk.Line | Should -Be 13
     }
 
     It "Should be able to set psbreakpoints for -Line and -column" {
-        $brk = New-PSBreakpoint -line 13 -column 1 -script $scriptFileName
+        $brk = New-PSBreakpoint -line 13 -column 1 -script $scriptFileName1
         $brk.Line | Should -Be 13
         $brk.Column | Should -Be 1
     }
 
     It "Should be able to set psbreakpoints for -Line and -action" {
-        $brk = New-PSBreakpoint -line 13 -action {{ break; }} -script $scriptFileName
+        $brk = New-PSBreakpoint -line 13 -action {{ break; }} -script $scriptFileName1
         $brk.Line | Should -Be 13
         $brk.Action | Should -Match "break"
     }
 
     It "Should be able to set psbreakpoints for -Line, -column and -action" {
-        $brk = New-PSBreakpoint -line 13 -column 1 -action {{ break; }} -script $scriptFileName
+        $brk = New-PSBreakpoint -line 13 -column 1 -action {{ break; }} -script $scriptFileName1
         $brk.Line | Should -Be 13
         $brk.Column | Should -Be 1
         $brk.Action | Should -Match "break"
     }
 
     It "-script and -line can take multiple items" {
-        $brk = New-PSBreakpoint -line 11,12,13 -column 1 -script $scriptFileName,$scriptFileName
+        $brk = New-PSBreakpoint -line 11,12,13 -column 1 -script $scriptFileName1,$scriptFileName1
         $brk.Line | Should -BeIn 11,12,13
         $brk.Column | Should -BeIn 1
     }
 
     It "-script and -line are positional" {
-        $brk = New-PSBreakpoint $scriptFileName 13
+        $brk = New-PSBreakpoint $scriptFileName1 13
         $brk.Line | Should -Be 13
     }
 
     It "-script, -line and -column are positional" {
-        $brk = New-PSBreakpoint $scriptFileName 13 1
+        $brk = New-PSBreakpoint $scriptFileName1 13 1
         $brk.Line | Should -Be 13
         $brk.Column | Should -Be 1
     }
 
     It "Should throw Exception when missing mandatory parameter -line" -Pending {
-         $output = & $ps -noninteractive -command "nbp -column 1 -script $scriptFileName"
+         $output = & $ps -noninteractive -command "nbp -column 1 -script $scriptFileName1"
          [system.string]::Join(" ", $output) | Should -Match "MissingMandatoryParameter,Microsoft.PowerShell.Commands.NewPSBreakpointCommand"
     }
 
@@ -88,12 +115,12 @@ return
     }
 
     It "Should be able to set psbreakpoints for -command, -script" {
-        $brk = New-PSBreakpoint -command "write-host" -script $scriptFileName
+        $brk = New-PSBreakpoint -command "write-host" -script $scriptFileName1
         $brk.Command | Should -BeExactly "write-host"
     }
 
     It "Should be able to set psbreakpoints for -command, -action and -script" {
-        $brk = New-PSBreakpoint -command "write-host" -action {{ break; }} -script $scriptFileName
+        $brk = New-PSBreakpoint -command "write-host" -action {{ break; }} -script $scriptFileName1
         $brk.Action | Should -Match "break"
     }
 
@@ -103,25 +130,25 @@ return
     }
 
     It "-Script is positional" {
-        $brk = New-PSBreakpoint -command "Hello" $scriptFileName
+        $brk = New-PSBreakpoint -command "Hello" $scriptFileName1
         $brk.Command | Should -BeExactly "Hello"
 
-        $brk = New-PSBreakpoint $scriptFileName -command "Hello"
+        $brk = New-PSBreakpoint $scriptFileName1 -command "Hello"
         $brk.Command | Should -BeExactly "Hello"
     }
 
     It "Should be able to set breakpoints on functions" {
-        $brk = New-PSBreakpoint -command Hello,Goodbye -script $scriptFileName
+        $brk = New-PSBreakpoint -command Hello,Goodbye -script $scriptFileName1
         $brk.Command | Should -Be Hello,Goodbye
     }
 
     It "Should be throw Exception when Column number less than 1" {
-        { New-PSBreakpoint -line 1 -column -1 -script $scriptFileName } | Should -Throw -ErrorId "ParameterArgumentValidationError,Microsoft.PowerShell.Commands.NewPSBreakpointCommand"
+        { New-PSBreakpoint -line 1 -column -1 -script $scriptFileName1 } | Should -Throw -ErrorId "ParameterArgumentValidationError,Microsoft.PowerShell.Commands.NewPSBreakpointCommand"
     }
 
     It "Should be throw Exception when Line number less than 1" {
         $ErrorActionPreference = "Stop"
-        { New-PSBreakpoint -line -1 -script $scriptFileName } | Should -Throw -ErrorId "NewPSBreakpoint:LineLessThanOne,Microsoft.PowerShell.Commands.NewPSBreakpointCommand"
+        { New-PSBreakpoint -line -1 -script $scriptFileName1 } | Should -Throw -ErrorId "NewPSBreakpoint:LineLessThanOne,Microsoft.PowerShell.Commands.NewPSBreakpointCommand"
         $ErrorActionPreference = "SilentlyContinue"
     }
 
@@ -144,42 +171,52 @@ return
         $ErrorActionPreference = "SilentlyContinue"
     }
 
-    # clean up
-    Remove-Item -Path $scriptFileName -Force
-}
-
-Describe "New-PSBreakpoint" -Tags "CI" {
-    # Set up test script
-    $testScript = Join-Path -Path $PSScriptRoot -ChildPath psbreakpointtestscript.ps1
-
-    "`$var = 1 " > $testScript
-
     It "Should be able to set a psbreakpoint on a line" {
         $lineNumber = 1
-        $brk = New-PSBreakpoint -Line $lineNumber -Script $testScript
+        $brk = New-PSBreakpoint -Line $lineNumber -Script $scriptFileName2
         $brk.Line | Should -Be $lineNumber
     }
 
     It "Should throw when a string is entered for a line number" {
         {
             $lineNumber = "one"
-            New-PSBreakpoint -Line $lineNumber -Script $testScript
+            New-PSBreakpoint -Line $lineNumber -Script $scriptFileName2
 
         } | Should -Throw
     }
 
     It "Should be able to set a psbreakpoint on a Command" {
         $command = "theCommand"
-        $brk = New-PSBreakpoint -Command $command -Script $testScript
+        $brk = New-PSBreakpoint -Command $command -Script $scriptFileName2
         $brk.Command | Should -Be $command
     }
 
     It "Should be able to set a psbreakpoint on a variable" {
         $var = "theVariable"
-        $brk = New-PSBreakpoint -Command $var -Script $testScript
+        $brk = New-PSBreakpoint -Command $var -Script $scriptFileName2
         $brk.Command | Should -Be $var
     }
+}
 
-    # clean up after ourselves
-    Remove-Item -Path $testScript
+Describe "New-PSBreakpoint Unit Tests - Feature-Disabled" -Tags "CI" {
+
+    BeforeAll {
+        if ($FeatureEnabled) {
+            Write-Verbose "Test Suite Skipped. The test suite requires the experimental feature 'Microsoft.PowerShell.Utility.PSDebugRunspaceWithBreakpoints' to be disabled." -Verbose
+            $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+            $PSDefaultParameterValues["it:skip"] = $true
+            return
+        }
+    }
+
+    AfterAll {
+        if ($FeatureEnabled) {
+            $global:PSDefaultParameterValues = $originalDefaultParameterValues
+            return
+        }
+    }
+
+    It "Should not have New-PSBreakpoint available" {
+        { New-PSBreakpoint } | Should -Throw -ErrorId CommandNotFoundException
+    }
 }
