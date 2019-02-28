def3d07a75015207f57a162ec1fe6b10400049bb
Kirk Munro <KirkMunro@users.noreply.github.com>
2019-02-27 21:42:40 -0400

    added missing copyright notice

--- test/powershell/engine/Api/MultithreadedEngine.Tests.ps1
+++ test/powershell/engine/Api/MultithreadedEngine.Tests.ps1
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 Describe 'Multithreaded engine APIs' -Tags 'CI' {
     BeforeAll {
         $sbStub = @'
