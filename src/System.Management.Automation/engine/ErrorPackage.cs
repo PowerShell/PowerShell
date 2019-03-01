5aa3311b53431800156376a66821ce481c2fe6a0
Joel <32407840+vexx32@users.noreply.github.com>
2019-03-01 14:26:13 -0500

    :recycle: Cleanup & update ErrorRecord class code style

--- src/System.Management.Automation/engine/ErrorPackage.cs
+++ src/System.Management.Automation/engine/ErrorPackage.cs
@@ -991,11 +991,14 @@ namespace System.Management.Automation
             object targetObject)
         {
             if (exception == null)
+            {
                 throw PSTraceSource.NewArgumentNullException("exception");
+            }
 
             if (errorId == null)
+            {
                 errorId = string.Empty;
-
+            }
             // targetObject may be null
 
             _error = exception;
@@ -1071,13 +1074,7 @@ namespace System.Management.Automation
         /// Is this instance serialized.
         /// </summary>
         /// <value></value>
-        internal bool IsSerialized
-        {
-            get
-            {
-                return _isSerialized;
-            }
-        }
+        internal bool IsSerialized { get => _isSerialized; }
 
         /// <summary>
         /// Value for FullyQualifiedErrorId in case of serialized error record.
@@ -1104,8 +1101,7 @@ namespace System.Management.Automation
         /// <param name="errorCategory_Message"></param>
         /// <param name="errorDetails_Message"></param>
         /// <param name="errorDetails_RecommendedAction"></param>
-        internal ErrorRecord
-        (
+        internal ErrorRecord(
             Exception exception,
             object targetObject,
             string fullyQualifiedErrorId,
@@ -1116,15 +1112,16 @@ namespace System.Management.Automation
             string errorCategory_TargetType,
             string errorCategory_Message,
             string errorDetails_Message,
-            string errorDetails_RecommendedAction
-        )
+            string errorDetails_RecommendedAction)
         {
-            PopulateProperties(exception, targetObject, fullyQualifiedErrorId, errorCategory, errorCategory_Activity,
-                               errorCategory_Reason, errorCategory_TargetName, errorCategory_TargetType,
-                               errorCategory_Message, errorDetails_Message, errorDetails_RecommendedAction, null);
+            PopulateProperties(
+                exception, targetObject, fullyQualifiedErrorId, errorCategory, errorCategory_Activity,
+                errorCategory_Reason, errorCategory_TargetName, errorCategory_TargetType,
+                errorCategory_Message, errorDetails_Message, errorDetails_RecommendedAction, null);
         }
 
-        private void PopulateProperties(Exception exception,
+        private void PopulateProperties(
+            Exception exception,
             object targetObject,
             string fullyQualifiedErrorId,
             ErrorCategory errorCategory,
@@ -1160,10 +1157,10 @@ namespace System.Management.Automation
             _serializedErrorCategoryMessageOverride = errorCategory_Message;
             if (errorDetails_Message != null)
             {
-                _errorDetails = new ErrorDetails(errorDetails_Message);
+                ErrorDetails = new ErrorDetails(errorDetails_Message);
                 if (errorDetails_RecommendedAction != null)
                 {
-                    _errorDetails.RecommendedAction = errorDetails_RecommendedAction;
+                    ErrorDetails.RecommendedAction = errorDetails_RecommendedAction;
                 }
             }
 
@@ -1227,16 +1224,11 @@ namespace System.Management.Automation
         /// <returns>
         /// value of note
         /// </returns>
-        private static object GetNoteValue
-        (
-            PSObject mshObject,
-            string note
-        )
+        private static object GetNoteValue(PSObject mshObject, string note)
         {
-            PSNoteProperty property = mshObject.Properties[note] as PSNoteProperty;
-            if (property != null)
+            if (mshObject.Properties[note] is PSNoteProperty p)
             {
-                return property.Value;
+                return p.Value;
             }
             else
             {
@@ -1258,10 +1250,7 @@ namespace System.Management.Automation
         /// <exception cref="ArgumentNullException">
         /// Thrown if mshObject parameter is null.
         /// </exception>
-        internal static ErrorRecord FromPSObjectForRemoting
-        (
-            PSObject serializedErrorRecord
-        )
+        internal static ErrorRecord FromPSObjectForRemoting(PSObject serializedErrorRecord)
         {
             ErrorRecord er = new ErrorRecord();
             er.ConstructFromPSObjectForRemoting(serializedErrorRecord);
@@ -1400,7 +1389,7 @@ namespace System.Management.Automation
             _targetNameOverride = errorRecord._targetNameOverride;
             _targetTypeOverride = errorRecord._targetTypeOverride;
             if (errorRecord.ErrorDetails != null)
-                _errorDetails = new ErrorDetails(errorRecord.ErrorDetails);
+                ErrorDetails = new ErrorDetails(errorRecord.ErrorDetails);
             SetInvocationInfo(errorRecord._invocationInfo);
             _scriptStackTrace = errorRecord._scriptStackTrace;
             _serializedFullyQualifiedErrorId = errorRecord._serializedFullyQualifiedErrorId;
@@ -1447,10 +1436,7 @@ namespace System.Management.Automation
         /// The object against which the error occurred.
         /// </summary>
         /// <value>may be null</value>
-        public object TargetObject
-        {
-            get { return _target; }
-        }
+        public object TargetObject { get => _target; }
 
         private object _target /* = null */;
         internal void SetTargetObject(object target)
@@ -1464,10 +1450,7 @@ namespace System.Management.Automation
         /// for that ErrorCategory.
         /// </summary>
         /// <value>never null</value>
-        public ErrorCategoryInfo CategoryInfo
-        {
-            get { return _categoryInfo ?? (_categoryInfo = new ErrorCategoryInfo(this)); }
-        }
+        public ErrorCategoryInfo CategoryInfo { get => _categoryInfo ?? (_categoryInfo = new ErrorCategoryInfo(this)); }
 
         private ErrorCategoryInfo _categoryInfo;
 
@@ -1487,13 +1470,15 @@ namespace System.Management.Automation
             get
             {
                 if (_serializedFullyQualifiedErrorId != null)
+                {
                     return _serializedFullyQualifiedErrorId;
+                }
 
                 string typeName = GetInvocationTypeName();
                 string delimiter =
-                    (string.IsNullOrEmpty(typeName)
-                     || string.IsNullOrEmpty(_errorId))
-                        ? string.Empty : ",";
+                    (string.IsNullOrEmpty(typeName) || string.IsNullOrEmpty(_errorId))
+                        ? string.Empty
+                        : ",";
                 return NotNull(_errorId) + delimiter + NotNull(typeName);
             }
         }
@@ -1507,24 +1492,14 @@ namespace System.Management.Automation
         /// contains a replacement message which should be displayed instead of
         /// Exception.Message.
         /// </remarks>
-        public ErrorDetails ErrorDetails
-        {
-            get { return _errorDetails; }
-
-            set { _errorDetails = value; }
-        }
-
-        private ErrorDetails _errorDetails;
+        public ErrorDetails ErrorDetails { get; set; }
 
         /// <summary>
         /// Identifies the cmdlet, script, or other command which caused
         /// the error.
         /// </summary>
         /// <value>may be null</value>
-        public InvocationInfo InvocationInfo
-        {
-            get { return _invocationInfo; }
-        }
+        public InvocationInfo InvocationInfo { get => _invocationInfo; }
 
         private InvocationInfo _invocationInfo /* = null */;
 
@@ -1570,22 +1545,12 @@ namespace System.Management.Automation
         }
 
         // 2005/07/14-913791 "write-error output is confusing and misleading"
-        internal bool PreserveInvocationInfoOnce
-        {
-            get { return _preserveInvocationInfoOnce; }
-
-            set { _preserveInvocationInfoOnce = value; }
-        }
-
-        private bool _preserveInvocationInfoOnce /* = false */;
+        internal bool PreserveInvocationInfoOnce { get; set; }
 
         /// <summary>
         /// The script stack trace for the error.
         /// </summary>
-        public string ScriptStackTrace
-        {
-            get { return _scriptStackTrace; }
-        }
+        public string ScriptStackTrace { get => _scriptStackTrace; }
 
         private string _scriptStackTrace;
 
@@ -1620,13 +1585,7 @@ namespace System.Management.Automation
         /// <summary>
         /// The status of the pipeline when this record was created.
         /// </summary>
-        public ReadOnlyCollection<int> PipelineIterationInfo
-        {
-            get
-            {
-                return _pipelineIterationInfo;
-            }
-        }
+        public ReadOnlyCollection<int> PipelineIterationInfo { get => _pipelineIterationInfo; }
 
         private ReadOnlyCollection<int> _pipelineIterationInfo = Utils.EmptyReadOnlyCollection<int>();
 
@@ -1635,15 +1594,9 @@ namespace System.Management.Automation
         /// </summary>
         internal bool SerializeExtendedInfo
         {
-            get
-            {
-                return _serializeExtendedInfo;
-            }
+            get => _serializeExtendedInfo;
 
-            set
-            {
-                _serializeExtendedInfo = value;
-            }
+            set => _serializeExtendedInfo = value;
         }
 
         private bool _serializeExtendedInfo = false;
@@ -1661,25 +1614,34 @@ namespace System.Management.Automation
         internal string _targetTypeOverride;
         #endregion Exposed by ErrorCategoryInfo
 
-        internal static string NotNull(string s)
-        {
-            return s ?? string.Empty;
-        }
+        internal static string NotNull(string s) => s ?? string.Empty;
 
         private string GetInvocationTypeName()
         {
             InvocationInfo invocationInfo = this.InvocationInfo;
             if (invocationInfo == null)
+            {
                 return string.Empty;
+            }
+
             CommandInfo commandInfo = invocationInfo.MyCommand;
             if (commandInfo == null)
+            {
                 return string.Empty;
+            }
+
             IScriptCommandInfo scriptInfo = commandInfo as IScriptCommandInfo;
             if (scriptInfo != null)
+            {
                 return commandInfo.Name;
+            }
+
             CmdletInfo cmdletInfo = commandInfo as CmdletInfo;
             if (cmdletInfo == null)
+            {
                 return string.Empty;
+            }
+
             return cmdletInfo.ImplementingType.FullName;
         }
 
@@ -1692,8 +1654,7 @@ namespace System.Management.Automation
         /// <returns>Developer-readable identifier.</returns>
         public override string ToString()
         {
-            if (ErrorDetails != null
-                && !string.IsNullOrEmpty(ErrorDetails.Message))
+            if (ErrorDetails != null && !string.IsNullOrEmpty(ErrorDetails.Message))
             {
                 return ErrorDetails.Message;
             }
