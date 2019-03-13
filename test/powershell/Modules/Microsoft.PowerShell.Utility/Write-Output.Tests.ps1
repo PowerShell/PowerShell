PowerShell/PowerShell

commit 94a71b05d474c8e58aff9aebe7b183f68462a6b5

Fix ValueFromRemainingArguments to have consistent behavior between script and C#  (#2038)

src/Microsoft.PowerShell.Commands.Utility/commands/utility/Write-Object.cs
@@ -55,10 +55,8 @@ namespace Microsoft.PowerShell.Commands
             {
                 enumerate = false;
             }
-            foreach (PSObject inputObject in _inputObjects) // compensate for ValueFromRemainingArguments
-            {
-                WriteObject(inputObject, enumerate);
-            }
+
+            WriteObject(_inputObjects, enumerate);
         }//processrecord
     }//WriteOutputCommand
     #endregion

src/System.Management.Automation/engine/CmdletParameterBinderController.cs
@@ -1684,33 +1684,31 @@ namespace System.Management.Automation
                         var cpi = CommandParameterInternal.CreateParameterWithArgument(
                             PositionUtilities.EmptyExtent, varargsParameter.Parameter.Name, "-" + varargsParameter.Parameter.Name + ":",
                             argumentExtent, valueFromRemainingArguments, false);
+
+                        // To make all of the following work similarly (the first is handled elsewhere, but second and third are
+                        // handled here):
+                        //     Set-ClusterOwnerNode -Owners foo,bar
+                        //     Set-ClusterOwnerNode foo bar
+                        //     Set-ClusterOwnerNode foo,bar
+                        // we unwrap our List, but only if there is a single argument of type object[].
+                        if (valueFromRemainingArguments.Count == 1 && valueFromRemainingArguments[0] is object[])
+                        {
+                            cpi.SetArgumentValue(UnboundArguments[0].ArgumentExtent, valueFromRemainingArguments[0]);
+                        }
+
                         try
                         {
                             BindParameter(cpi, varargsParameter, ParameterBindingFlags.ShouldCoerceType);
                         }
                         catch (ParameterBindingException pbex)
                         {
-                            // To make all of the following work similarly (the first is handled elsewhere, but second and third are
-                            // handled here):
-                            //     Set-ClusterOwnerNode -Owners foo,bar
-                            //     Set-ClusterOwnerNode foo bar
-                            //     Set-ClusterOwnerNode foo,bar
-                            // we make one additional attempt at converting, but only if there is a single argument of type object[].
-                            if (valueFromRemainingArguments.Count == 1 && valueFromRemainingArguments[0] is object[])
+                            if (!DefaultParameterBindingInUse)
                             {
-                                cpi.SetArgumentValue(UnboundArguments[0].ArgumentExtent, valueFromRemainingArguments[0]);
-                                BindParameter(cpi, varargsParameter, ParameterBindingFlags.ShouldCoerceType);
+                                throw;
                             }
                             else
                             {
-                                if (!DefaultParameterBindingInUse)
-                                {
-                                    throw;
-                                }
-                                else
-                                {
-                                    ThrowElaboratedBindingException(pbex);
-                                }
+                                ThrowElaboratedBindingException(pbex);
                             }
                         }
                         UnboundArguments.Clear();

test/powershell/engine/ParameterBinding/ParameterBinding.Tests.ps1
@@ -228,7 +228,7 @@
             [CmdletBinding()]
             param (
             [array]$Parameter1,
-            [int[]]$Parameter2 
+            [int[]]$Parameter2
             )
 
             Process {
@@ -327,6 +327,100 @@
 
             $result = & $psPath -NoProfile -File $test2File
             $result | Should Be $expected
+        }
+    }
+
+    Context "ValueFromRemainingArguments" {
+        BeforeAll {
+            function Test-BindingFunction {
+                param (
+                    [Parameter(ValueFromRemainingArguments)]
+                    [object[]] $Parameter
+                )
+
+                return [pscustomobject] @{
+                    ArgumentCount = $Parameter.Count
+                    Value = $Parameter
+                }
+            }
+
+            # Deliberately not using TestDrive:\ here because Pester will fail to clean it up due to the
+            # assembly being loaded in our process.
+
+            if ($IsWindows)
+            {
+                $tempDir = $env:temp
+            }
+            else
+            {
+                $tempDir = '/tmp'
+            }
+
+            $dllPath = Join-Path $tempDir TestBindingCmdlet.dll
+
+            Add-Type -OutputAssembly $dllPath -TypeDefinition '
+                using System;
+                using System.Management.Automation;
+
+                [Cmdlet("Test", "BindingCmdlet")]
+                public class TestBindingCommand : PSCmdlet
+                {
+                    [Parameter(Position = 0, ValueFromRemainingArguments = true)]
+                    public string[] Parameter { get; set; }
+
+                    protected override void ProcessRecord()
+                    {
+                        PSObject obj = new PSObject();
+
+                        obj.Properties.Add(new PSNoteProperty("ArgumentCount", Parameter.Length));
+                        obj.Properties.Add(new PSNoteProperty("Value", Parameter));
+
+                        WriteObject(obj);
+                    }
+                }
+            '
+
+            Import-Module $dllPath
+        }
+
+        AfterAll {
+            Get-Module TestBindingCmdlet | Remove-Module -Force
+        }
+
+        It "Binds properly when passing an explicit array to an advanced function" {
+            $result = Test-BindingFunction 1,2,3
+
+            $result.ArgumentCount | Should Be 3
+            $result.Value[0] | Should Be 1
+            $result.Value[1] | Should Be 2
+            $result.Value[2] | Should Be 3
+        }
+
+        It "Binds properly when passing multiple arguments to an advanced function" {
+            $result = Test-BindingFunction 1 2 3
+
+            $result.ArgumentCount | Should Be 3
+            $result.Value[0] | Should Be 1
+            $result.Value[1] | Should Be 2
+            $result.Value[2] | Should Be 3
+        }
+
+        It "Binds properly when passing an explicit array to a cmdlet" {
+            $result = Test-BindingCmdlet 1,2,3
+
+            $result.ArgumentCount | Should Be 3
+            $result.Value[0] | Should Be 1
+            $result.Value[1] | Should Be 2
+            $result.Value[2] | Should Be 3
+        }
+
+        It "Binds properly when passing multiple arguments to a cmdlet" {
+            $result = Test-BindingCmdlet 1 2 3
+
+            $result.ArgumentCount | Should Be 3
+            $result.Value[0] | Should Be 1
+            $result.Value[1] | Should Be 2
+            $result.Value[2] | Should Be 3
         }
     }
 }

commit 18d5037ad2a97db7b306171a4ba4be0dcce91e5e

Fix -NoEnumerate behaviour in Write-Output (#9069)
Fix is to preserve input collection type in output.
The regression was caused by #2038

src/Microsoft.PowerShell.Commands.Utility/commands/utility/Write-Object.cs
@@ -12,49 +12,32 @@ namespace Microsoft.PowerShell.Commands
     [Cmdlet(VerbsCommunications.Write, "Output", HelpUri = "https://go.microsoft.com/fwlink/?LinkID=113427", RemotingCapability = RemotingCapability.None)]
     public sealed class WriteOutputCommand : PSCmdlet
     {
-        private PSObject[] _inputObjects = null;
-
         /// <summary>
         /// Holds the list of objects to be written.
         /// </summary>
         [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromRemainingArguments = true)]
         [AllowNull]
         [AllowEmptyCollection]
-        public PSObject[] InputObject
-        {
-            get { return _inputObjects; }
-
-            set { _inputObjects = value; }
-        }
+        public PSObject InputObject { get; set; }
 
         /// <summary>
         /// Prevents Write-Output from unravelling collections passed to the InputObject parameter.
         /// </summary>
-        [Parameter()]
-        public SwitchParameter NoEnumerate
-        {
-            get;
-            set;
-        }
+        [Parameter]
+        public SwitchParameter NoEnumerate { get; set; }
 
         /// <summary>
         /// This method implements the ProcessRecord method for Write-output command.
         /// </summary>
         protected override void ProcessRecord()
         {
-            if (_inputObjects == null)
+            if (InputObject == null)
             {
-                WriteObject(_inputObjects);
+                WriteObject(InputObject);
                 return;
             }
 
-            bool enumerate = true;
-            if (NoEnumerate.IsPresent)
-            {
-                enumerate = false;
-            }
-
-            WriteObject(_inputObjects, enumerate);
+            WriteObject(InputObject, !NoEnumerate.IsPresent);
         }
     }
     #endregion

test/powershell/Modules/Microsoft.PowerShell.Utility/Write-Output.Tests.ps1
@@ -18,60 +18,70 @@ Describe "Write-Output DRT Unit Tests" -Tags "CI" {
 
     It "Works with NoEnumerate switch" {
         $objectWritten = 1, 2.2, @("John", "Smith", 10), "abc"
-        [string]$s = Write-Output $objectWritten -NoEnumerate 6>&1 
+        [string]$s = Write-Output $objectWritten -NoEnumerate 6>&1
         $s | Should -Be  '1 2.2 System.Object[] abc'
+    }
+
+    It "Preserves the input collection type and contents with -NoEnumerate" {
+        $List = [System.Collections.Generic.List[string]]::new( [string[]]@("test", "one", "two") )
+        $ObjectWritten = Write-Output $List -NoEnumerate
+
+        $ObjectWritten.GetType() | Should -Be ([System.Collections.Generic.List[string]])
+        $ObjectWritten[0] | Should -BeExactly "test"
+        $ObjectWritten[1] | Should -BeExactly "one"
+        $ObjectWritten[2] | Should -BeExactly "two"
     }
 }
 
 Describe "Write-Output" -Tags "CI" {
     $testString = $testString
     Context "Input Tests" {
-	It "Should allow piped input" {
-	    { $testString | Write-Output } | Should -Not -Throw
-	}
+        It "Should allow piped input" {
+            { $testString | Write-Output } | Should -Not -Throw
+        }
 
-	It "Should write output to the output stream when using piped input" {
-	    $testString | Write-Output | Should -Be $testString
-	}
+        It "Should write output to the output stream when using piped input" {
+            $testString | Write-Output | Should -Be $testString
+        }
 
-	It "Should use inputobject switch" {
-	    { Write-Output -InputObject $testString } | Should -Not -Throw
-	}
+        It "Should use inputobject switch" {
+            { Write-Output -InputObject $testString } | Should -Not -Throw
+        }
 
-	It "Should write output to the output stream when using inputobject switch" {
-	    Write-Output -InputObject $testString | Should -Be $testString
-	}
+        It "Should write output to the output stream when using inputobject switch" {
+            Write-Output -InputObject $testString | Should -Be $testString
+        }
 
-	It "Should be able to write to a variable" {
-	    Write-Output -InputObject $testString -OutVariable var
-	    $var | Should -Be $testString
-	}
+        It "Should be able to write to a variable" {
+            Write-Output -InputObject $testString -OutVariable var
+            $var | Should -Be $testString
+        }
     }
 
     Context "Pipeline Command Tests" {
-	It "Should send object to the next command in the pipeline" {
-	    Write-Output -InputObject (1+1) | Should -Be 2
-	}
+        It "Should send object to the next command in the pipeline" {
+            Write-Output -InputObject (1 + 1) | Should -Be 2
+        }
 
-	It "Should have the same result between inputobject switch and piped input" {
-	    Write-Output -InputObject (1+1) | Should -Be 2
+        It "Should have the same result between inputobject switch and piped input" {
+            Write-Output -InputObject (1 + 1) | Should -Be 2
 
-	    1+1 | Write-Output | Should -Be 2
-	}
+            1 + 1 | Write-Output | Should -Be 2
+        }
     }
 
     Context "Enumerate Objects" {
-	$enumerationObject = @(1,2,3)
-	It "Should see individual objects when not using the NoEnumerate switch" {
-	    $singleCollection = $(Write-Output $enumerationObject| Measure-Object).Count
+        $enumerationObject = @(1, 2, 3)
+        It "Should see individual objects when not using the NoEnumerate switch" {
+            $singleCollection = $(Write-Output $enumerationObject| Measure-Object).Count
 
-	    $singleCollection | Should -Be $enumerationObject.length
-	}
+            $singleCollection | Should -Be $enumerationObject.length
+        }
 
-	It "Should be able to treat a collection as a single object using the NoEnumerate switch" {
-	    $singleCollection = $(Write-Output $enumerationObject -NoEnumerate | Measure-Object).Count
+        It "Should be able to treat a collection as a single object using the NoEnumerate switch" {
+            $singleCollection = $(Write-Output $enumerationObject -NoEnumerate | Measure-Object).Count
 
-	    $singleCollection | Should -Be 1
-	}
+            $singleCollection | Should -Be 1
+        }
     }
 }