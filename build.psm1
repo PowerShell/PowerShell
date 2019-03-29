3a4e6f3c0c60bc1d0b4468f9d94fe60d6a646e78
Travis Plunk <travis.plunk@microsoft.com>
2019-03-29 16:34:18 -0700

    Apply suggestions from code review

--- build.psm1
+++ build.psm1
@@ -908,6 +908,7 @@ function Start-PSPester {
         [string]$ExperimentalFeatureName,
         [Parameter(HelpMessage='Title to publish the results as.')]
         [string]$Title = 'PowerShell Core Tests',
+		[Parameter(ParameterSetName='Wait',Mandatory=$true,HelpMessage='Wait for the debugger to attach to powershell before pester starts.  Debug builds only!')]
         [switch]$Wait
     )
 
@@ -1101,6 +1102,10 @@ function Start-PSPester {
         Set-Content -Path $configFile -Value $content -Encoding Ascii -Force
         $PSFlags = @("-settings", $configFile, "-noprofile")
     }
+
+	# -Wait is only available on Debug builds
+	# It is used to allow the debugger to attach before PowerShell 
+	# runs pester in this case
     if($Wait.IsPresent){
         $PSFlags += '-wait'
     }
