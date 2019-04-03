4561f238e117bee2624028fdfa10647b2a29a6cf
pougetat <thpouget@microsoft.com>
2019-04-03 12:28:35 +0200

    Rearranging tests

--- test/powershell/Modules/Microsoft.PowerShell.Core/Remove-Module.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Remove-Module.Tests.ps1
@@ -1,89 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-Describe "Remove-Module -Name" -Tags "CI" {
-    BeforeAll {
-        Remove-Module -Name "Foo", "Bar", "Baz" -ErrorAction SilentlyContinue
-
-        New-Item -ItemType Directory -Path "$testdrive\Modules\Foo\" -Force > $null
-        New-Item -ItemType Directory -Path "$testdrive\Modules\Bar\" -Force > $null
-        New-Item -ItemType Directory -Path "$testdrive\Modules\Baz\" -Force > $null
-
-        New-ModuleManifest -Path "$testdrive\Modules\Foo\Foo.psd1"
-        New-ModuleManifest -Path "$testdrive\Modules\Bar\Bar.psd1"
-        New-ModuleManifest -Path "$testdrive\Modules\Baz\Baz.psd1"
-
-        New-Item -ItemType File -Path "$testdrive\Modules\Foo\Foo.psm1" > $null
-        New-Item -ItemType File -Path "$testdrive\Modules\Bar\Bar.psm1" > $null
-        New-Item -ItemType File -Path "$testdrive\Modules\Baz\Baz.psm1" > $null
-
-        $removeModuleByNameTestCases = @(
-            # Simple patterns
-            @{ PatternsToRemove = "Foo"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
-            @{ PatternsToRemove = "Bar", "Foo"; ShouldBeRemoved = "Bar", "Foo"; ShouldBePresent = "Baz"}
-            @{ PatternsToRemove = "Bar", "Baz", "Foo"; ShouldBeRemoved = "Bar", "Baz", "Foo"; ShouldBePresent = ""}
-            @{ PatternsToRemove = "Foo", "Foo"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
-            @{ PatternsToRemove = "Foo", "Foo", "Bar"; ShouldBeRemoved = "Bar", "Foo"; ShouldBePresent = "Baz"}
-            @{ PatternsToRemove = "Fo", "Foo"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
-
-            # Regex patterns
-            #@{ PatternsToRemove = "*"; ShouldBeRemoved = "Bar", "Baz", "Foo"; ShouldBePresent = ""} -> this breaks pester for some reason
-            @{ PatternsToRemove = "B*"; ShouldBeRemoved = "Bar", "Baz"; ShouldBePresent = "Foo"}
-            @{ PatternsToRemove = "F*"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
-            @{ PatternsToRemove = "Foo*"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
-            @{ PatternsToRemove = "F*", "Bar"; ShouldBeRemoved = "Foo", "Bar"; ShouldBePresent = "Baz"}
-            @{ PatternsToRemove = "F*", "F*"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
-            @{ PatternsToRemove = "FF*"; ShouldBeRemoved = ""; ShouldBePresent = "Bar", "Baz", "Foo"}
-        )
-
-        $removeModuleByNameErrorTestCases = @(
-            # Invalid patterns
-            @{ PatternsToRemove = "Fo"; ShouldBeRemoved = ""; ShouldBePresent = "Bar", "Baz", "Foo"}
-            @{ PatternsToRemove = "Fo", "Ba"; ShouldBeRemoved = ""; ShouldBePresent = "Bar", "Baz", "Foo"}
-        )
-    }
-
-    BeforeEach {
-        Import-Module -Name "$testdrive\Modules\Foo\Foo.psd1" -Force
-        Import-Module -Name "$testdrive\Modules\Bar\Bar.psd1" -Force
-        Import-Module -Name "$testdrive\Modules\Baz\Baz.psd1" -Force
-
-        (Get-Module -Name "Bar", "Baz", "Foo").Name | Should -BeExactly "Bar", "Baz", "Foo"
-    }
-
-    AfterAll {
-        Remove-Module -Name "Foo", "Bar", "Baz" -ErrorAction SilentlyContinue
-    }
-
-    It "Remove-Module -Name <PatternsToRemove>" -TestCases $removeModuleByNameTestCases {
-        param([string[]]$PatternsToRemove, [string[]]$ShouldBeRemoved, [string[]]$ShouldBePresent)
-
-        { Remove-Module -Name $PatternsToRemove} | Should -Not -Throw
-
-        if ($ShouldBeRemoved) {
-            (Get-Module -Name $ShouldBeRemoved).Name | Should -BeNullOrEmpty
-        }
-
-        if ($ShouldBePresent) {
-            (Get-Module -Name $ShouldBePresent).Name | Should -BeExactly $ShouldBePresent
-        }
-    }
-
-    It "Remove-Module -Name <PatternsToRemove> (Error cases)" -TestCases $removeModuleByNameErrorTestCases {
-        param([string[]]$PatternsToRemove, [string[]]$ShouldBeRemoved, [string[]]$ShouldBePresent)
-
-        { Remove-Module -Name $PatternsToRemove -ErrorAction Stop } | Should -Throw -ErrorId "Modules_NoModulesRemoved,Microsoft.PowerShell.Commands.RemoveModuleCommand"
-
-        if ($ShouldBeRemoved) {
-            (Get-Module -Name $ShouldBeRemoved).Name | Should -BeNullOrEmpty
-        }
-
-        if ($ShouldBePresent) {
-            (Get-Module -Name $ShouldBePresent).Name | Should -BeExactly $ShouldBePresent
-        }
-    }
-}
-
-Describe "Remove-Module -FullyQualifiedName | -ModuleInfo" -Tags "CI" {
+Describe "Remove-Module -Name | -FullyQualifiedName | -ModuleInfo" -Tags "CI" {
     BeforeAll {
         Remove-Module -Name "Foo", "Bar", "Baz" -ErrorAction SilentlyContinue
 
@@ -100,6 +17,32 @@ Describe "Remove-Module -FullyQualifiedName | -ModuleInfo" -Tags "CI" {
         New-Item -ItemType File -Path "$testdrive\Modules\Foo\1.0\Foo.psm1" > $null
         New-Item -ItemType File -Path "$testdrive\Modules\Foo\2.0\Foo.psm1" > $null
         New-Item -ItemType File -Path "$testdrive\Modules\Bar\Bar.psm1" > $null
+
+        $removeModuleByNameTestCases = @(
+            # Simple patterns
+            @{ PatternsToRemove = "Bar"; ShouldBeRemoved = "Bar"; ShouldBePresent = "Baz", "Foo", "Foo"}
+            @{ PatternsToRemove = "Foo"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
+            @{ PatternsToRemove = "Bar", "Foo"; ShouldBeRemoved = "Bar", "Foo"; ShouldBePresent = "Baz"}
+            @{ PatternsToRemove = "Bar", "Baz", "Foo"; ShouldBeRemoved = "Bar", "Baz", "Foo"; ShouldBePresent = ""}
+            @{ PatternsToRemove = "Foo", "Foo"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
+            @{ PatternsToRemove = "Foo", "Foo", "Bar"; ShouldBeRemoved = "Bar", "Foo"; ShouldBePresent = "Baz"}
+            @{ PatternsToRemove = "Fo", "Foo"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
+
+            # Regex patterns
+            #@{ PatternsToRemove = "*"; ShouldBeRemoved = "Bar", "Baz", "Foo"; ShouldBePresent = ""} -> this breaks pester for some reason
+            @{ PatternsToRemove = "B*"; ShouldBeRemoved = "Bar", "Baz"; ShouldBePresent = "Foo", "Foo"}
+            @{ PatternsToRemove = "F*"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
+            @{ PatternsToRemove = "Foo*"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
+            @{ PatternsToRemove = "F*", "Bar"; ShouldBeRemoved = "Foo", "Bar"; ShouldBePresent = "Baz"}
+            @{ PatternsToRemove = "F*", "F*"; ShouldBeRemoved = "Foo"; ShouldBePresent = "Bar", "Baz"}
+            @{ PatternsToRemove = "FF*"; ShouldBeRemoved = ""; ShouldBePresent = "Bar", "Baz", "Foo", "Foo"}
+        )
+
+        $removeModuleByNameErrorTestCases = @(
+            # Invalid patterns
+            @{ PatternsToRemove = "Fo"; ShouldBeRemoved = ""; ShouldBePresent = "Bar", "Baz", "Foo", "Foo"}
+            @{ PatternsToRemove = "Fo", "Ba"; ShouldBeRemoved = ""; ShouldBePresent = "Bar", "Baz", "Foo", "Foo"}
+        )
 
         $removeModuleByFQNTestCases = @(
             @{
@@ -169,6 +112,34 @@ Describe "Remove-Module -FullyQualifiedName | -ModuleInfo" -Tags "CI" {
 
     AfterAll {
         Remove-Module -Name "Foo", "Bar", "Baz" -ErrorAction SilentlyContinue
+    }
+
+    It "Remove-Module -Name <PatternsToRemove>" -TestCases $removeModuleByNameTestCases {
+        param([string[]]$PatternsToRemove, [string[]]$ShouldBeRemoved, [string[]]$ShouldBePresent)
+
+        { Remove-Module -Name $PatternsToRemove} | Should -Not -Throw
+
+        if ($ShouldBeRemoved) {
+            (Get-Module -Name $ShouldBeRemoved).Name | Should -BeNullOrEmpty
+        }
+
+        if ($ShouldBePresent) {
+            (Get-Module -Name $ShouldBePresent).Name | Should -BeExactly $ShouldBePresent
+        }
+    }
+
+    It "Remove-Module -Name <PatternsToRemove> (Error cases)" -TestCases $removeModuleByNameErrorTestCases {
+        param([string[]]$PatternsToRemove, [string[]]$ShouldBeRemoved, [string[]]$ShouldBePresent)
+
+        { Remove-Module -Name $PatternsToRemove -ErrorAction Stop } | Should -Throw -ErrorId "Modules_NoModulesRemoved,Microsoft.PowerShell.Commands.RemoveModuleCommand"
+
+        if ($ShouldBeRemoved) {
+            (Get-Module -Name $ShouldBeRemoved).Name | Should -BeNullOrEmpty
+        }
+
+        if ($ShouldBePresent) {
+            (Get-Module -Name $ShouldBePresent).Name | Should -BeExactly $ShouldBePresent
+        }
     }
 
     It "Remove-Module -FullyQualifiedName <FullyQualifiedName>" -TestCases $removeModuleByFQNTestCases {
