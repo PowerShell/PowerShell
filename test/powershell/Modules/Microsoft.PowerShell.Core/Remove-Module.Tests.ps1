6b31ef2e37bd72e0abc0c0818d65c6a48d95bc01
pougetat <thpouget@microsoft.com>
2019-04-05 19:31:59 +0200

    test currently failing

--- test/powershell/Modules/Microsoft.PowerShell.Core/Remove-Module.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Remove-Module.Tests.ps1
@@ -333,16 +333,24 @@ Describe "Remove-Module : module contains nested modules" -Tags "CI" {
 
         New-Item -ItemType Directory -Path "$testdrive\Modules\Foo\" -Force > $null
         New-Item -ItemType Directory -Path "$testdrive\Modules\Bar\" -Force > $null
+        New-Item -ItemType Directory -Path "$testdrive\Modules\Baz\" -Force > $null
 
-        New-ModuleManifest -Path "$testdrive\Modules\Foo\Foo.psd1" -NestedModules "..\Bar\Bar.psd1" -FunctionsToExport "BarFunc"
-        New-ModuleManifest -Path "$testdrive\Modules\Bar\Bar.psd1" -RootModule "Bar.psm1" -FunctionsToExport "BarFunc"
+        New-ModuleManifest -Path "$testdrive\Modules\Foo\Foo.psd1"
+        New-ModuleManifest -Path "$testdrive\Modules\Bar\Bar.psd1"
+        New-ModuleManifest -Path "$testdrive\Modules\Baz\Baz.psd1"
 
         New-Item -ItemType File -Path "$testdrive\Modules\Foo\Foo.psm1" > $null
         New-Item -ItemType File -Path "$testdrive\Modules\Bar\Bar.psm1" > $null
+        New-Item -ItemType File -Path "$testdrive\Modules\Baz\Baz.psm1" > $null
+        Set-Content -Path "$testdrive\Modules\Foo\Foo.psm1" -Value "function FooFunc {}"
         Set-Content -Path "$testdrive\Modules\Bar\Bar.psm1" -Value "function BarFunc {}"
+        Set-Content -Path "$testdrive\Modules\Baz\Baz.psm1" -Value "function BazFunc {}"
     }
 
     It "Remove-Module : module contains nested modules" {
+        Update-ModuleManifest -Path "$testdrive\Modules\Bar\Bar.psd1" -RootModule "Bar.psm1" -FunctionsToExport "BarFunc"
+        Update-ModuleManifest -Path "$testdrive\Modules\Foo\Foo.psd1" -NestedModules "..\Bar\Bar.psd1" -FunctionsToExport "BarFunc"
+
         Import-Module "$testdrive\Modules\Foo\Foo.psd1" -Force
         (Get-Module -Name "Foo").Name | Should -BeExactly "Foo"
 
