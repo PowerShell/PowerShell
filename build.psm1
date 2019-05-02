cb20154de88e17aad07249b2b2a4847ee2374e9a
Steve Lee <slee@microsoft.com>
2019-05-01 21:56:16 -0700

    Update to Pester 4.8.0 (#9510)

--- build.psm1
+++ build.psm1
@@ -594,7 +594,7 @@ function Restore-PSPester
         [ValidateNotNullOrEmpty()]
         [string] $Destination = ([IO.Path]::Combine((Split-Path (Get-PSOptions -DefaultToNew).Output), "Modules"))
     )
-    Save-Module -Name Pester -Path $Destination -Repository PSGallery -RequiredVersion "4.4.4"
+    Save-Module -Name Pester -Path $Destination -Repository PSGallery -RequiredVersion "4.8.0"
 }
 
 function Compress-TestContent {
