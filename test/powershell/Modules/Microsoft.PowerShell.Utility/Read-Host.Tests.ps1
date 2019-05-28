0cf1ed81b56762428e043325a4d2fd47372a5bdc
Steve Lee <slee@microsoft.com>
2019-05-27 21:44:21 -0700

    Have console host not enter command prompt mode when using `Read-Host -Prompt`

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Read-Host.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Read-Host.Tests.ps1
@@ -9,28 +9,37 @@ Describe "Read-Host Test" -tag "CI" {
         $ps.Runspace = $rs
         $ps.Commands.Clear()
     }
+
     AfterEach {
         $ps.Commands.Clear()
     }
+
     AfterAll {
         $rs.Close()
         $rs.Dispose()
         $ps.Dispose()
     }
+
     It "Read-Host returns expected string" {
         $result = $ps.AddCommand("Read-Host").Invoke()
         $result | Should -Be $th.UI.ReadLineData
     }
+
     It "Read-Host sets the prompt correctly" {
         $result = $ps.AddScript("Read-Host -prompt myprompt").Invoke()
         $prompt = $th.ui.streams.prompt[0]
         $prompt | Should -Not -BeNullOrEmpty
         $prompt.split(":")[-1] | Should -Be myprompt
     }
+
     It "Read-Host returns a secure string when using -AsSecureString parameter" {
         $result = $ps.AddScript("Read-Host -AsSecureString").Invoke() | select-object -first 1
         $result | Should -BeOfType SecureString
-        [pscredential]::New("foo",$result).GetNEtworkCredential().Password | Should -BeExactly TEST
+        [pscredential]::New("foo",$result).GetNetworkCredential().Password | Should -BeExactly TEST
+    }
 
+    It "Read-Host doesn't enter command prompt mode" {
+        $result = "!1" | pwsh -nop -c "Read-host -Prompt 'foo'"
+        $result | should -BeExactly @('foo: !1','!1')
     }
 }
