83708a4242befaae0e26d7f33e7ebe2f0091db08
Ilya <darpa@yandex.ru>
2019-06-01 23:51:25 +0500

    Use new string.ConCat() in Process.cs (#9720)

--- src/Microsoft.PowerShell.Commands.Management/commands/management/Process.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/Process.cs
@@ -795,9 +795,7 @@ namespace Microsoft.PowerShell.Commands
                         break;
                     }
 
-                    // TODO: Use Concat() from .Net Core 3.0
-                    // public static string Concat(ReadOnlySpan<char> str0, ReadOnlySpan<char> str1, ReadOnlySpan<char> str2)
-                    userName = string.Concat(domainNameStr.Slice(0, domainNameLength).ToString(), "\\", userNameStr.Slice(0, userNameLength).ToString());
+                    userName = string.Concat(domainNameStr.Slice(0, domainNameLength), "\\", userNameStr.Slice(0, userNameLength));
                 } while (false);
             }
             catch (NotSupportedException)
