1c1992d1efb9a5fb60f7460b20ae83ea745349b3
Asaf Rosentswaig <asrosent@microsoft.com>
2019-06-03 19:15:38 +0300

    revision 3

--- src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleShell.cs
+++ src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleShell.cs
@@ -3,6 +3,7 @@
 
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
+using System.Runtime.CompilerServices;
 
 namespace Microsoft.PowerShell
 {
@@ -14,6 +15,16 @@ namespace Microsoft.PowerShell
     public static
     class ConsoleShell
     {
+        /// <summary>Entry point in to ConsoleShell. This method is called by main of minishell.</summary>
+        /// <param name="bannerText">Banner text to be displayed by ConsoleHost.</param>
+        /// <param name="helpText">Help text for minishell. This is displayed on 'minishell -?'.</param>
+        /// <param name="args">Commandline parameters specified by user.</param>
+        /// <returns>An integer value which should be used as exit code for the process.</returns>
+        public static int Start(string bannerText, string helpText, string[] args)
+        {
+            return Start(InitialSessionState.CreateDefault2(), bannerText, helpText, args);
+        }
+
         /// <summary>Entry point in to ConsoleShell. Used to create a custom Powershell console application</summary>
         /// <param name="initialSessionState">InitialSessionState to be used by the ConsoleHost.</param>
         /// <param name="bannerText">Banner text to be displayed by ConsoleHost.</param>
@@ -27,22 +38,12 @@ namespace Microsoft.PowerShell
                 throw PSTraceSource.NewArgumentNullException(nameof(initialSessionState));
             }
 
-            ConsoleHost.DefaultInitialSessionState = initialSessionState;
-
-            return Start(bannerText, helpText, args);
-        }
-
-        /// <summary>Entry point in to ConsoleShell. This method is called by main of minishell.</summary>
-        /// <param name="bannerText">Banner text to be displayed by ConsoleHost.</param>
-        /// <param name="helpText">Help text for minishell. This is displayed on 'minishell -?'.</param>
-        /// <param name="args">Commandline parameters specified by user.</param>
-        /// <returns>An integer value which should be used as exit code for the process.</returns>
-        public static int Start(string bannerText, string helpText, string[] args)
-        {
             if (args == null)
             {
                 throw PSTraceSource.NewArgumentNullException(nameof(args));
             }
+
+            ConsoleHost.DefaultInitialSessionState = initialSessionState;
 
             return ConsoleHost.Start(bannerText, helpText, args);
         }
