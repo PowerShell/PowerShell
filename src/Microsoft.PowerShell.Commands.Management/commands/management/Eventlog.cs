7e423e7451a4530060422aff1c821c0dc0712372
Ilya <darpa@yandex.ru>
2019-06-07 08:35:33 +0500

    Fix minor style issues come from last commits (#9640)

--- src/Microsoft.PowerShell.Commands.Management/commands/management/Eventlog.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/Eventlog.cs
@@ -5,8 +5,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel; // Win32Exception
-using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics; // Eventlog class
+using System.Diagnostics.CodeAnalysis;
 using System.Management.Automation;
 using System.Management.Automation.Internal;
 
