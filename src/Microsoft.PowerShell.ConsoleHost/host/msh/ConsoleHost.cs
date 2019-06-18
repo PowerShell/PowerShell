93d67e7171edc830297a3e144e0db27e8740d996
Christoph Bergmeister <c.bergmeister@gmail.com>
2019-06-18 21:30:11 +0100

    Put try catch into task expression

--- src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleHost.cs
+++ src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleHost.cs
@@ -199,14 +199,19 @@ namespace Microsoft.PowerShell
                 // The JumpList does persist as long as the filepath of the executable does not change but there
                 // could be disruptions to it like e.g. the bi-annual Windows update, we decided to
                 // not over-optimize this and always create the JumpList as a non-blocking background task instead.
-                try
+                Task.Run(() =>
                 {
-                    Task.Run(() => TaskbarJumpList.CreateElevatedEntry(ConsoleHostStrings.RunAsAdministrator));
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine($"Exception '{exception}' with stack trace {exception.StackTrace} successfully caught.");
-                }
+                    try
+                    {
+                        TaskbarJumpList.CreateElevatedEntry(ConsoleHostStrings.RunAsAdministrator);
+                    }
+                    catch (Exception exception)
+                    {
+                        // Sporadic failures have been observed in some environments. Since the JumpList persists once it
+                        // has been registered, there is no harm suppressing the occasional failure.
+                        Console.WriteLine($"Exception '{exception}' with stack trace {exception.StackTrace} successfully caught.");
+                    }
+                });
 #endif
 
                 // First check for and handle PowerShell running in a server mode.
