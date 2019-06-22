8b8c2ed6048cfe565e5ca70925e8d6c562858f53
kwkam <kwkam@users.noreply.github.com>
2019-06-22 14:14:28 +0800

    Add copyright header

--- test/powershell/engine/Api/WildcardPattern.Tests.ps1
+++ test/powershell/engine/Api/WildcardPattern.Tests.ps1
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 Describe "WildcardPattern Escape - Experimental-Feature-Disabled" -Tags "CI" {
 
     BeforeAll {
