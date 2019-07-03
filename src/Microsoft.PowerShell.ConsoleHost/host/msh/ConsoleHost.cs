dc8be8f425d93a15d395a9beef143667459e6b87
Christoph Bergmeister <c.bergmeister@gmail.com>
2019-07-03 20:34:19 +0100

    remove task.start and add more catches

--- src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleHost.cs
+++ src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleHost.cs
@@ -200,22 +200,25 @@ namespace Microsoft.PowerShell
                 // The JumpList does persist as long as the filepath of the executable does not change but there
                 // could be disruptions to it like e.g. the bi-annual Windows update, we decided to
                 // not over-optimize this and always create the JumpList as a non-blocking background task instead.
-                Task.Run(() =>
+                try
                 {
-                    try
+                    // APIs are STA only and until issue 7216 is not resolved, PowerShell operates still in MTA mode.
+                    var thread = new Thread(() =>
                     {
-                        // APIs are STA only and until issue 7216 is not resolved, PowerShell operates still in MTA mode.
-                        var thread = new Thread(() => TaskbarJumpList.CreateElevatedEntry(ConsoleHostStrings.RunAsAdministrator));
-                        thread.SetApartmentState(ApartmentState.STA);
-                        thread.Start();
-                    }
-                    catch
-                    {
-                        // Sporadic failures have been observed in some environments. Since the JumpList persists once it
-                        // has been registered, there is no harm suppressing the occasional failure.
-                    }
+                        try
+                        {
+                            TaskbarJumpList.CreateElevatedEntry(ConsoleHostStrings.RunAsAdministrator);
+                        }
+                        catch { }
+                    });
+                    thread.SetApartmentState(ApartmentState.STA);
+                    thread.Start();
                 }
-                );
+                catch
+                {
+                    // Sporadic failures have been observed in some environments. Since the JumpList persists once it
+                    // has been registered, there is no harm suppressing the occasional failure.
+                }
 #endif
 
                 // First check for and handle PowerShell running in a server mode.
