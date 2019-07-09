28ecf303ae0d6bb04c00bad450052e584c27cccc
Ilya <darpa@yandex.ru>
2019-07-09 16:09:21 +0500

    Exclude string allocation

--- src/System.Management.Automation/engine/regex.cs
+++ src/System.Management.Automation/engine/regex.cs
@@ -184,8 +184,8 @@ namespace System.Management.Automation
             if (index == Pattern.Length - 1 && Pattern[index] == '*')
             {
                 // No special characters present in the pattern before last position and last character is asterisk.
-                var patternWithoutAsterisk = Pattern.Substring(0, index);
-                _isMatch = str => str.StartsWith(patternWithoutAsterisk, GetStringComparison());
+                var patternWithoutAsterisk = Pattern.AsMemory().Slice(0, index);
+                _isMatch = str => str.AsSpan().StartsWith(patternWithoutAsterisk.Span, GetStringComparison());
                 return;
             }
 
