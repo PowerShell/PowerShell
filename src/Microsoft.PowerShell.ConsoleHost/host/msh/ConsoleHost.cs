f55bf2a02486d6fdada813cc175622ba440eabc1
Travis Plunk <tplunk@microsoft.com>
2019-07-09 21:45:01 +0000

    Create JumpList in STA thread as some COM APIs are strictly STA only to avoid sporadic CLR crashes (#10057)
    Create JumpList in STA thread as some COM APIs are strictly STA only to avoid sporadic CLR crashes (#10057)
    
    # Conflicts:
    #	src/Microsoft.PowerShell.ConsoleHost/WindowsTaskbarJumpList/TaskbarJumpList.cs

--- src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleHost.cs
+++ src/Microsoft.PowerShell.ConsoleHost/host/msh/ConsoleHost.cs
@@ -193,25 +193,7 @@ namespace Microsoft.PowerShell
                 }
 
 #if !UNIX
-                // Creating a JumpList entry takes around 55ms when the PowerShell process is interactive and
-                // owns the current window (otherwise it does a fast exit anyway). Since there is no 'GET' like API,
-                // we always have to execute this call because we do not know if it has been created yet.
-                // The JumpList does persist as long as the filepath of the executable does not change but there
-                // could be disruptions to it like e.g. the bi-annual Windows update, we decided to
-                // not over-optimize this and always create the JumpList as a non-blocking background task instead.
-                Task.Run(() =>
-                {
-                    try
-                    {
-                        TaskbarJumpList.CreateElevatedEntry(ConsoleHostStrings.RunAsAdministrator);
-                    }
-                    catch
-                    {
-                        // Sporadic failures have been observed in some environments. Since the JumpList persists once it
-                        // has been registered, there is no harm suppressing the occasional failure.
-                    }
-                }
-                );
+                TaskbarJumpList.CreateRunAsAdministratorJumpList();
 #endif
 
                 // First check for and handle PowerShell running in a server mode.
