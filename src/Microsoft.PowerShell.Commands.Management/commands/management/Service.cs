df672de69665fe4ec0a4e4b69128b03238fa6c19
NB <NeoBeum@users.noreply.github.com>
2019-07-13 05:18:20 +0930

    Add alias for Service 'StartType' (#9940)

--- src/Microsoft.PowerShell.Commands.Management/commands/management/Service.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/Service.cs
@@ -1544,13 +1544,14 @@ namespace Microsoft.PowerShell.Commands
 
         /// <summary>
         /// The following is the definition of the input parameter "StartupType".
+        /// "Set-Service -StartType" sets ServiceController.InputObject.StartType.
         /// Changes the starting mode of the service. Valid values for StartupType are:
         /// -- Automatic: Start when the system starts.
         /// -- Manual   : Starts only when started by a user or program.
         /// -- Disabled : Can.
         /// </summary>
         [Parameter]
-        [Alias("StartMode", "SM", "ST")]
+        [Alias("StartMode", "SM", "ST", "StartType")]
         [ValidateNotNullOrEmpty]
         public ServiceStartupType StartupType
         {
