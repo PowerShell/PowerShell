599c53c6e18b292527d259787fe5755b8eed9e83
Dongbo Wang <dongbow@microsoft.com>
2019-07-16 12:35:28 -0700

    Add the license header to `nanoserver.tests.ps1`

--- test/nanoserver/nanoserver.tests.ps1
+++ test/nanoserver/nanoserver.tests.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 Describe "Verify PowerShell Runs" {
     BeforeAll{
         $options = (Get-PSOptions)
