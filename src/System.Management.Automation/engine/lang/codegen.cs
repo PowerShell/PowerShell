9f9302391cf4a100b778ce19bf675106085d1182
msftrncs <msftrncs@midlands.net>
2019-07-25 02:21:29 -0500

    Add argument processing methods
    Add methods QuoteArgument and EscapeDoubleQuotedStringContent to assist
    generating argument completions that are correctly quoted and escaped.
    
    Includes private method to determine if argument content cannot be used
    bareword.

--- src/System.Management.Automation/engine/lang/codegen.cs
+++ src/System.Management.Automation/engine/lang/codegen.cs
@@ -137,5 +137,123 @@ namespace System.Management.Automation.Language
                 "'" + EscapeSingleQuotedStringContent(value) + "'" :
                 value;
         }
+
+        /// <summary>
+        /// Quote an argument, if needed, or if specifically requested, escaping characters accordingly,
+        /// handling escaping of wildcard patterns when argument is not already taken literally.
+        /// For example: QuoteArgument(userContent, quoteInUse, isLiteralArgument)
+        /// </summary>
+        /// <param name="value">The content to be used as an argument value taken literally.</param>
+        /// <param name="quoteInUse">The character to be quoted with.</param>
+        /// <param name="isLiteralArgument">Treat the argument as taken literally, wildcard escaping not required.</param>
+        /// <returns>Content quoted and escaped if required for use as an argument value.</returns>
+        public static string QuoteArgument(string value, char quoteInUse, bool isLiteralArgument)
+        {
+            return string.IsNullOrEmpty(value) ?
+                string.Empty :
+                QuoteArgument(isLiteralArgument ? value : WildcardPattern.Escape(value), quoteInUse);
+        }
+
+        /// <summary>
+        /// Quote an argument, if needed, or if specifically requested, escaping characters accordingly
+        /// For example: QuoteArgument(userContent, quoteInUse)
+        /// </summary>
+        /// <param name="value">The content to be used as an argument value taken literally.</param>
+        /// <param name="quoteInUse">The character to be quoted with.</param>
+        /// <returns>Content quoted and escaped if required for use as an argument value.</returns>
+        public static string QuoteArgument(string value, char quoteInUse)
+        {
+            if (string.IsNullOrEmpty(value))
+                return string.Empty;
+
+            char quoteToUse = quoteInUse;
+            if (quoteToUse == 0)
+                if (ShouldArgumentNotBeBareword(value))
+                    // argument value not compatible with bareword
+                    quoteToUse = '\'';
+                else
+                    // return unmodified argument value
+                    return value;
+
+            // quote and escape argument as needed
+            return quoteToUse + (quoteToUse.IsDoubleQuote() ? EscapeDoubleQuotedStringContent(value) :
+                EscapeSingleQuotedStringContent(value)) + quoteToUse;
+        }
+
+        // brute force method to determine when an argument's value cannot be used bareword
+        private static bool ShouldArgumentNotBeBareword(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            // Rules for bareword arguments:
+            //   characters that cannot be in the first position: '@','#','<','>'
+            //   Pattern that cannot be in the first position: /[1-6]>/
+            bool requiresQuote = "@#<>".Contains(value[0]) ||
+                value.Length > 1 && '1' <= value[0] && value[0] <= '6' && value[1] == '>';
+
+            if (!requiresQuote)
+            {
+                bool lastCharWasDollar = false;
+                foreach (char c in value)
+                {
+                    //   Characters that cannot appear anywhere 
+                    //     ForceStartNewToken
+                    //     IsSingleQuote
+                    //     IsDoubleQuote
+                    if (c.ForceStartNewToken() || c.IsSingleQuote() || c.IsDoubleQuote() || c == '`')
+                    {
+                        requiresQuote = true;
+                        break;
+                    }
+                    if (lastCharWasDollar)
+                        //   IsVariableStart characters cannot appear after a `$`
+                        // note `{` and `(` is handled by ForceStartNewToken()
+                        if (c.IsVariableStart())
+                        {
+                            requiresQuote = true;
+                            break;
+                        }
+                    lastCharWasDollar = c == '$';
+                }
+            }
+            return requiresQuote;
+        }
+
+        /// <summary>
+        /// Escapes content so that it is safe for inclusion in a double-quoted string.
+        /// For example: "\"" + EscapeDoubleQuotedStringContent(userContent) + "\""
+        /// </summary>
+        /// <param name="value">The content to be included in a double-quoted string.</param>
+        /// <returns>Content with all backticks and double-quotes, and only valid variable `$` escaped.</returns>
+        public static string EscapeDoubleQuotedStringContent(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return string.Empty;
+
+            StringBuilder sb = new StringBuilder(value.Length);
+            bool lastCharWasDollar = false;
+            foreach (char c in value)
+            {
+                if (lastCharWasDollar)
+                {
+                    if (c.IsVariableStart() || c == '{' || c == '(')
+                        sb.Append('`');
+                    sb.Append('$');
+                }
+                lastCharWasDollar = c == '$';
+                if (!lastCharWasDollar)
+                {
+                    sb.Append(c);
+                    if (c.IsDoubleQuote() || c == '`')
+                        // double-up quotes & backticks to escape them
+                        sb.Append(c);
+                }
+            }
+            if (lastCharWasDollar)
+                sb.Append('$');
+
+            return sb.ToString();
+        }
     }
 }
