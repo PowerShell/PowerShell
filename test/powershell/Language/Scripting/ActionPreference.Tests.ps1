0efbd38a6332219fea80b1b2c030d332a34a7552
Kirk Munro <KirkMunro@users.noreply.github.com>
2019-08-05 16:59:16 -0300

    add automated tests for local debugger

--- test/powershell/Language/Scripting/ActionPreference.Tests.ps1
+++ test/powershell/Language/Scripting/ActionPreference.Tests.ps1
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
 Describe "Tests for (error, warning, etc) action preference" -Tags "CI" {
         BeforeAll {
             $orgin = $GLOBAL:errorActionPreference
@@ -114,4 +115,223 @@ Describe "Tests for (error, warning, etc) action preference" -Tags "CI" {
             { New-Item @params } | Should -Throw -ErrorId "NewItemIOError,Microsoft.PowerShell.Commands.NewItemCommand"
             Remove-Item "$testdrive\test.txt" -Force
         }
+}
+
+Describe 'ActionPreference.Break tests' -tag 'CI' {
+
+    BeforeAll {
+        Register-DebuggerHandler
+    }
+
+    AfterAll {
+        Unregister-DebuggerHandler
+    }
+
+    Context '-ErrorAction Break should break on a non-terminating error' {
+        BeforeAll {
+            $testScript = {
+                function Test-Break {
+                    [CmdletBinding()]
+                    param()
+                    try {
+                        # Generate a non-terminating error
+                        Get-Process -Id ([int]::MaxValue)
+                        # Do something afterwards
+                        'This should still run'
+                    } catch {
+                        'Do nothing'
+                    } finally {
+                        'This finally runs'
+                    }
+                }
+                Test-Break -ErrorAction Break
+            }
+
+            $results = @(Test-Debugger -ScriptBlock $testScript -CommandQueue 's', 's')
+        }
+
+        It 'Should show 3 debugger commands were invoked' {
+            # There is always an implicit 'c' command that keeps the debugger automation moving
+            $results.Count | Should -Be 3
+        }
+
+        It 'The breakpoint should be the statement that generated the non-terminating error' {
+            $results[0] | ShouldHaveExtent -Line 7 -FromColumn 25 -ToColumn 58
+        }
+
+        It 'The second statement should be the statement after that which generated the non-terminating error' {
+            $results[1] | ShouldHaveExtent -Line 9 -FromColumn 25 -ToColumn 48
+        }
+
+        It 'The third statement should be the statement in the finally block' {
+            $results[2] | ShouldHaveExtent -Line 13 -FromColumn 25 -ToColumn 44
+        }
+    }
+
+    Context '-ErrorAction Break should break on a terminating error' {
+        BeforeAll {
+            $testScript = {
+                function Test-Break {
+                    [CmdletBinding()]
+                    param()
+                    try {
+                        # Generate a terminating error
+                        Get-Process -TheAnswer 42
+                        # Do something afterwards
+                        'This should not run'
+                    } catch {
+                        'Do nothing'
+                    } finally {
+                        'This finally runs'
+                    }
+                }
+                Test-Break -ErrorAction Break
+            }
+
+            $results = @(Test-Debugger -ScriptBlock $testScript -CommandQueue 's', 's')
+        }
+
+        It 'Should show 3 debugger commands were invoked' {
+            # There is always an implicit 'c' command that keeps the debugger automation moving
+            $results.Count | Should -Be 3
+        }
+
+        It 'The breakpoint should be the statement that generated the terminating error' {
+            $results[0] | ShouldHaveExtent -Line 7 -FromColumn 25 -ToColumn 50
+        }
+
+        It 'The second statement should be the statement in the catch block where the terminating error is caught' {
+            $results[1] | ShouldHaveExtent -Line 11 -FromColumn 25 -ToColumn 37
+        }
+
+        It 'The third statement should be the statement in the finally block' {
+            $results[2] | ShouldHaveExtent -Line 13 -FromColumn 25 -ToColumn 44
+        }
+    }
+
+    Context '-ErrorAction Break should not break on a naked rethrow' {
+        BeforeAll {
+            $testScript = {
+                function Test-Break {
+                    [CmdletBinding()]
+                    param()
+                    try {
+                        try {
+                            # Generate a terminating error
+                            Get-Process -TheAnswer 42
+                        } catch {
+                            throw
+                        }
+                    } catch {
+                        # Swallow the exception here
+                    }
+                }
+                Test-Break -ErrorAction Break
+            }
+
+            $results = @(Test-Debugger -ScriptBlock $testScript)
+        }
+
+        It 'Should show 1 debugger command was invoked' {
+            # ErrorAction break should only trigger on the initial terminating error
+            $results.Count | Should -Be 1
+        }
+
+        It 'The breakpoint should be the statement that generated the terminating error' {
+            $results[0] | ShouldHaveExtent -Line 8 -FromColumn 29 -ToColumn 54
+        }
+    }
+
+    Context '-ErrorAction Break should break when throwing a specific error or object' {
+        BeforeAll {
+            $testScript = {
+                function Test-Break {
+                    [CmdletBinding()]
+                    param()
+                    try {
+                        try {
+                            # Generate a terminating error
+                            Get-Process -TheAnswer 42
+                        } catch {
+                            throw $_
+                        }
+                    } catch {
+                        # Swallow the exception here
+                    }
+                }
+                Test-Break -ErrorAction Break
+            }
+
+            $results = @(Test-Debugger -ScriptBlock $testScript)
+        }
+
+        It 'Should show 2 debugger commands were invoked' {
+            # ErrorAction break should trigger on the initial terminating error and the throw
+            # since it throws a "new" error (throwing anything is considered a new terminating
+            # error)
+            $results.Count | Should -Be 2
+        }
+
+        It 'The first breakpoint should be the statement that generated the terminating error' {
+            $results[0] | ShouldHaveExtent -Line 8 -FromColumn 29 -ToColumn 54
+        }
+
+        It 'The second breakpoint should be the statement that threw $_' {
+            $results[1] | ShouldHaveExtent -Line 10 -FromColumn 29 -ToColumn 37
+        }
+    }
+
+    Context 'Other message types should break on their corresponding messages when requested' {
+        BeforeAll {
+            $testScript = {
+                function Test-Break {
+                    [CmdletBinding()]
+                    param()
+                    Write-Warning -Message 'This is a warning message'
+                    Write-Verbose -Message 'This is a verbose message'
+                    Write-Debug -Message 'This is a debug message'
+                    Write-Information -MessageData 'This is an information message'
+                    Write-Progress -Activity 'This shows progress'
+                }
+                Test-Break -WarningAction Break -InformationAction Break *>$null
+                $WarningPreference = $VerbosePreference = $DebugPreference = $InformationPreference = $ProgressPreference = [actionpreference]::Break
+                Test-Break *>$null
+            }
+
+            $results = @(Test-Debugger -ScriptBlock $testScript)
+        }
+
+        It 'Should show 7 debugger commands were invoked' {
+            # When no debugger commands are provided, 'c' is invoked every time a breakpoint is hit
+            $results.Count | Should -Be 7
+        }
+
+        It 'Write-Warning should trigger a breakpoint from -WarningAction Break' {
+            $results[0] | ShouldHaveExtent -Line 5 -FromColumn 21 -ToColumn 71
+        }
+
+        It 'Write-Information should trigger a breakpoint from -InformationAction Break' {
+            $results[1] | ShouldHaveExtent -Line 8 -FromColumn 21 -ToColumn 84
+        }
+
+        It 'Write-Warning should trigger a breakpoint from $WarningPreference = [actionpreference]::Break' {
+            $results[2] | ShouldHaveExtent -Line 5 -FromColumn 21 -ToColumn 71
+        }
+
+        It 'Write-Verbose should trigger a breakpoint from $VerbosePreference = [actionpreference]::Break' {
+            $results[3] | ShouldHaveExtent -Line 6 -FromColumn 21 -ToColumn 71
+        }
+
+        It 'Write-Debug should trigger a breakpoint from $DebugPreference = [actionpreference]::Break' {
+            $results[4] | ShouldHaveExtent -Line 7 -FromColumn 21 -ToColumn 67
+        }
+
+        It 'Write-Information should trigger a breakpoint from $InformationPreference = [actionpreference]::Break' {
+            $results[5] | ShouldHaveExtent -Line 8 -FromColumn 21 -ToColumn 84
+        }
+
+        It 'Write-Progress should trigger a breakpoint from $ProgressPreference = [actionpreference]::Break' {
+            $results[6] | ShouldHaveExtent -Line 9 -FromColumn 21 -ToColumn 67
+        }
+    }
 }
