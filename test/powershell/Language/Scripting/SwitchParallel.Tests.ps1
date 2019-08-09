75774f1e9166598c1903d880c8cd9cb09e3bb743
Kirk Munro <KirkMunro@users.noreply.github.com>
2019-08-09 10:45:39 -0300

    clean up formatting of Pester tests

--- test/powershell/Language/Scripting/SwitchParallel.Tests.ps1
+++ test/powershell/Language/Scripting/SwitchParallel.Tests.ps1
@@ -1,27 +1,48 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-Describe "Parallel switch syntax" -Tags "CI" {
+
+Describe 'Parallel switch syntax' -Tags 'CI' {
 
     Context 'Should be able to retrieve AST of parallel switch' {
-        $ast = [System.Management.Automation.Language.Parser]::ParseInput(
-            'switch -parallel ($foo) {1 {break}}', [ref] $null, [ref] $null)
-        It '$ast.EndBlock.Statements[0].Flags' { $ast.EndBlock.Statements[0].Flags | Should -BeExactly 'Parallel' }
+        BeforeAll {
+            $ast = [System.Management.Automation.Language.Parser]::ParseInput(
+                'switch -parallel ($foo) {1 {break}}', [ref] $null, [ref] $null)
+        }
+
+        It '$ast.EndBlock.Statements[0].Flags' {
+            $ast.EndBlock.Statements[0].Flags | Should -BeExactly 'Parallel'
+        }
     }
 
     Context 'Generates an error on invalid parameter' {
-        $errors = @()
-        $ast = [System.Management.Automation.Language.Parser]::ParseInput(
-            'switch -bogus ($foo) {1 {break}}', [ref]$null, [ref]$errors)
-        It '$errors.Count' { $errors.Count | Should -Be 1 }
-        It '$errors[0].ErrorId' { $errors[0].ErrorId | Should -BeExactly 'InvalidSwitchFlag' }
+        BeforeAll {
+            $errors = @()
+            $ast = [System.Management.Automation.Language.Parser]::ParseInput(
+                'switch -bogus ($foo) {1 {break}}', [ref]$null, [ref]$errors)
+        }
+
+        It '$errors.Count' {
+            $errors.Count | Should -Be 1
+        }
+
+        It '$errors[0].ErrorId' {
+            $errors[0].ErrorId | Should -BeExactly 'InvalidSwitchFlag'
+        }
     }
 
     Context 'Generate an error on -parallel' {
-        $errors = @()
-        $ast = [System.Management.Automation.Language.Parser]::ParseInput(
-            'switch -parallel ($foo) {1 {break}}', [ref]$null, [ref]$errors)
-        It '$errors.Count' { $errors.Count | Should -Be 1 }
-        It '$errors[0].ErrorId' { $errors[0].ErrorId | Should -Be 'KeywordParameterReservedForFutureUse' }
-    }
+        BeforeAll {
+            $errors = @()
+            $ast = [System.Management.Automation.Language.Parser]::ParseInput(
+                'switch -parallel ($foo) {1 {break}}', [ref]$null, [ref]$errors)
+        }
 
+        It '$errors.Count' {
+            $errors.Count | Should -Be 1
+        }
+
+        It '$errors[0].ErrorId' {
+            $errors[0].ErrorId | Should -Be 'KeywordParameterReservedForFutureUse'
+        }
+    }
 }
