6500eb49564f16d7ed319a46cfd62c15781831c2
Travis Plunk <github@ez13.net>
2019-08-13 11:25:48 -0700

    add comments

--- src/System.Management.Automation/DscSupport/CimDSCParser.cs
+++ src/System.Management.Automation/DscSupport/CimDSCParser.cs
@@ -397,8 +397,11 @@ namespace Microsoft.PowerShell.DesiredStateConfiguration
             }
             uint offset = 0;
             #if UNIX
+            // OMI only supports UT8 without BOM
             var encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
             #else
+            // This is what we traditionally use with Windows
+            // DSC asked to keep it UTF-32 for Windows
             var encoding = new UnicodeEncoding();
             #endif
 
