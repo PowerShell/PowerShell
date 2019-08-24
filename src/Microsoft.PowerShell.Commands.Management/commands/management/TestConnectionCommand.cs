febe0da19d4588818903a9fef8f4a6bc3f6e34e9
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-08-24 13:36:10 -0400

    :recycle: Rename method a little more simply

--- src/Microsoft.PowerShell.Commands.Management/commands/management/TestConnectionCommand.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/TestConnectionCommand.cs
@@ -313,7 +313,7 @@ namespace Microsoft.PowerShell.Commands
                 return;
             }
 
-            WriteConsoleTraceRouteHeader(resolvedTargetName, targetAddress.ToString());
+            WriteTraceRouteHeader(resolvedTargetName, targetAddress.ToString());
 
             TraceRouteResult traceRouteResult = new TraceRouteResult(Source, targetAddress, resolvedTargetName);
 
@@ -390,7 +390,7 @@ namespace Microsoft.PowerShell.Commands
             }
         }
 
-        private void WriteConsoleTraceRouteHeader(string resolvedTargetName, string targetAddress)
+        private void WriteTraceRouteHeader(string resolvedTargetName, string targetAddress)
         {
             _testConnectionProgressBarActivity = StringUtil.Format(
                 TestConnectionResources.TraceRouteStart,
