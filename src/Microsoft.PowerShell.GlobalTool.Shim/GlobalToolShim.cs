f137d15d4d3ba0789e881bef0a74eac69d8a1109
Aditya Patwardhan <adityap@microsoft.com>
2019-08-29 11:51:41 -0700

    Fix codefactor issue

--- src/Microsoft.PowerShell.GlobalTool.Shim/GlobalToolShim.cs
+++ src/Microsoft.PowerShell.GlobalTool.Shim/GlobalToolShim.cs
@@ -21,7 +21,8 @@ namespace Microsoft.PowerShell.GlobalTool.Shim
         /// <summary>
         /// Entry point for the global tool.
         /// </summary>
-        /// <param name="args">Arguments passed to the global tool.</param>
+        /// <param name="args">Arguments passed to the global tool.</param>'
+        /// <returns>Exit code returned by pwsh.</returns>
         public static int Main(string[] args)
         {
             var currentPath = new FileInfo(System.Reflection.Assembly.GetEntryAssembly().Location).Directory.FullName;
