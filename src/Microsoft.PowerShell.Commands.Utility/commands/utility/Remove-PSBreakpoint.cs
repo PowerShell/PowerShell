491a687d4338998215f8807b3bbe9e0da62e5444
Kirk Munro <KirkMunro@users.noreply.github.com>
2019-09-06 00:22:31 -0300

    add -runspace parameter to *-PSBreakpoint

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/Remove-PSBreakpoint.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/Remove-PSBreakpoint.cs
@@ -10,14 +10,18 @@ namespace Microsoft.PowerShell.Commands
     /// </summary>
     [Cmdlet(VerbsCommon.Remove, "PSBreakpoint", SupportsShouldProcess = true, DefaultParameterSetName = "Breakpoint",
         HelpUri = "https://go.microsoft.com/fwlink/?LinkID=113375")]
-    public class RemovePSBreakpointCommand : PSBreakpointCommandBase
+    public class RemovePSBreakpointCommand : PSBreakpointUpdaterCommandBase
     {
+        #region overrides
+
         /// <summary>
         /// Removes the given breakpoint.
         /// </summary>
         protected override void ProcessBreakpoint(Breakpoint breakpoint)
         {
-            this.Context.Debugger.RemoveBreakpoint(breakpoint);
+            Runspace.Debugger.RemoveBreakpoint(breakpoint);
         }
+
+        #endregion overrides
     }
 }
