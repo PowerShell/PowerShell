f1a4eb425125b795ce75a6b03f4398e739120238
Dongbo Wang <dongbow@microsoft.com>
2019-09-24 10:27:51 -0700

    Address CodeFactor issue

--- test/hosting/test_HostingBasic.cs
+++ test/hosting/test_HostingBasic.cs
@@ -141,7 +141,7 @@ namespace PowerShell.Hosting.SDK.Tests
         }
 
         /// <summary>
-        /// Reference assemblies should be handled correctly so that Add-Type works in the hosting scenario
+        /// Reference assemblies should be handled correctly so that Add-Type works in the hosting scenario.
         /// </summary>
         [Fact]
         public static void TestAddTypeCmdletInHostScenario()
