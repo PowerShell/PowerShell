8c70dce6cad4701ec59c1317e4486e17dc95069d
Travis Plunk <github@ez13.net>
2019-09-25 15:16:09 -0700

    update logging

--- src/System.Management.Automation/engine/runtime/CompiledScriptBlock.cs
+++ src/System.Management.Automation/engine/runtime/CompiledScriptBlock.cs
@@ -1520,19 +1520,7 @@ namespace System.Management.Automation
 
             if (!wasEncoded)
             {
-                // The logging mechanism(s) cannot handle null and rendering may not be able to handle
-                // null as we have the string defined as a null terminated string in the manifest.
-                // So, replace null characters with the Unicode `SYMBOL FOR NULL`
-                // We don't just remove the characters to preserve the fact that a null character was there.
-                textToLog = textToLog.Replace('\u0000', '\u2400');
-#if UNIX
-                if (Platform.IsLinux)
-                {
-                    // Syslog encodes CR and NL to their octal values.
-                    // We will replace them with a unicode charater for easier viewing
-                    textToLog = textToLog.Replace('\u000A', '\u23CE').Replace('\u000D', '\u23CE');
-                }
-#endif
+                textToLog = FormatLogString(textToLog);
             }
 
             if (scriptBlock._scriptBlockData.HasSuspiciousContent)
@@ -1563,6 +1551,52 @@ namespace System.Management.Automation
             }
 
             return true;
+        }
+
+        private static string FormatLogString(string textToLog)
+        {
+            // characters we ar translating from
+            const char nullControlChar = '\u0000';
+            // new line / line feed control character
+            const char lfControlChar = '\u000A';
+            // carriage return control character
+            const char crControlChar = '\u000D';
+
+            // The null symbol - `␀`
+            const char nullSymbolChar = '\u2400';
+
+            // We chose the return symbol because we believe it is more associated with these concepts
+            // than the carriage return (␍), line feed (␊), or new line (␤) symbols.
+            // The return symbol - `⏎`
+            const char returnSymbolChar = '\u23CE';
+
+            // No logging mechanism(s) cannot handle null and rendering may not be able to handle
+            // null as we have the string defined as a null terminated string in the manifest.
+            // So, replace null characters with the Unicode `SYMBOL FOR NULL`
+            // We don't just remove the characters to preserve the fact that a null character was there.
+#if UNIX
+            if (Platform.IsLinux)
+            {
+                // Because the creation of the string builder is expensive
+                // We only do this on Linux where we are doing multiple replace operations
+                StringBuilder logBuilder = new StringBuilder(textToLog);
+
+                logBuilder.Replace(nullControlChar, nullSymbolChar);
+
+                // Syslog (only used on Linux) encodes CR and NL to their octal values.
+                // We will replace them with a unicode charater for easier viewing
+                logBuilder.Replace(lfControlChar, returnSymbolChar);
+                logBuilder.Replace(crControlChar, returnSymbolChar);
+
+                return logBuilder.ToString();
+            }
+            else
+            {
+                return textToLog.Replace(nullControlChar, nullSymbolChar);
+            }
+#else
+                return textToLog.Replace(nullControlChar, nullSymbolChar);
+#endif
         }
 
         private static bool GetAndValidateEncryptionRecipients(
