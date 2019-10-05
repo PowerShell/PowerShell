05e08d097669c32d57cf09623a6c86ebc4c423e2
Steve Lee <slee@microsoft.com>
2019-10-05 15:49:51 -0700

    add Resolve-ErrorRecord cmdlet

---
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/Resolve-ErrorRecord.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Management.Automation;
+
+namespace Microsoft.PowerShell.Commands
+{
+    /// <summary>
+    /// Class for Resolve-ErrorRecord implementation.
+    /// </summary>
+    [Cmdlet(VerbsDiagnostic.Resolve, "ErrorRecord", HelpUri = "https://go.microsoft.com/fwlink/?LinkID=")]
+    public sealed class ResolveErrorRecordCommand : PSCmdlet
+    {
+
+    }
+}
