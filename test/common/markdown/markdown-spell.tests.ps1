2ecc6ce0cd6443562dde9fbeb757abf3eae82f48
Reece Dunham <me@rdil.rocks>
2019-10-07 18:35:32 -0400

    Update test/common/markdown/markdown-spell.tests.ps1
    Co-Authored-By: Travis Plunk <travis.plunk@microsoft.com>

--- test/common/markdown/markdown-spell.tests.ps1
+++ test/common/markdown/markdown-spell.tests.ps1
@@ -49,7 +49,7 @@ Describe "Verify Markdown Spelling" {
             $file = $jobResult.file
             $result = $jobResult.results
             Context "Verify spelling in $file" {
-                $failures = @($result) -like '*spelling errors found in*'
+                $failures = $result -like '*spelling errors found in*'
                 $passes = @($result) -like '*free of spelling*'
  
                 $trueFailures = foreach ($Failure in $Failures) {
