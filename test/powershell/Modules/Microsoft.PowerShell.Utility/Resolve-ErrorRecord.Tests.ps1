2926591078a83a43bd28ef70c7be63038cb7f09f
Steve Lee <slee@microsoft.com>
2019-10-07 19:42:33 -0700

    add check that Exp Feature is enabled for tests

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Resolve-ErrorRecord.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Resolve-ErrorRecord.Tests.ps1
@@ -2,6 +2,20 @@
 # Licensed under the MIT License.
 
 Describe 'Resolve-ErrorRecord tests' -Tag CI {
+    BeforeAll {
+        $skipTest = -not $EnabledExperimentalFeatures.Contains('Microsoft.PowerShell.Utility.PSResolveErrorRecord')
+        if ($skipTest) {
+            Write-Verbose "Test Suite Skipped. The test suite requires the experimental feature 'Microsoft.PowerShell.Utility.PSResolveErrorRecord' to be enabled." -Verbose
+            $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+            $PSDefaultParameterValues["it:skip"] = $true
+        }
+    }
+
+    AfterAll {
+        if ($skipTest) {
+            $global:PSDefaultParameterValues = $originalDefaultParameterValues
+        }
+    }
 
     It 'Resolve-ErrorRecord resolves $Error[0] and includes InnerException' {
         try {
