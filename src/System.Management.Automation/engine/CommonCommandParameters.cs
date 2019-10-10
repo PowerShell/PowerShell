a5f541eedb8d0b71276e5f11b52e21564b56eced
Kirk Munro <KirkMunro@users.noreply.github.com>
2019-10-09 21:23:21 -0300

    update comment positioning

--- src/System.Management.Automation/engine/CommonCommandParameters.cs
+++ src/System.Management.Automation/engine/CommonCommandParameters.cs
@@ -108,12 +108,14 @@ namespace System.Management.Automation.Internal
         /// <remarks>
         /// This parameter tells the command what to do when an informational record occurs.
         /// </remarks>
+        /// <!--
+        /// NOTE: The "infa" alias name does not follow the same alias naming convention used
+        /// with other common parameter aliases that control stream functionality; however,
+        /// "ia" was already taken as a parameter alias in other commands when this parameter
+        /// was added to PowerShell, so "infa" was chosen instead.
+        /// -->
         [Parameter]
         [Alias("infa")]
-        // NOTE: The "infa" alias name does not follow the same alias naming convention used
-        // with other common parameter aliases that control stream functionality; however,
-        // "ia" was already taken as a parameter alias in other commands when this parameter
-        // was added to PowerShell, so "infa" was chosen instead.
         public ActionPreference InformationAction
         {
             get { return _commandRuntime.InformationPreference; }
