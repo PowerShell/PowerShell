25f73520e78a904e6b5a96b97b615d12595df5da
Kirk Munro <KirkMunro@users.noreply.github.com>
2019-10-10 15:48:37 -0300

    Add comment describing why `-ia` is not the alias for `-InformationAction` common parameter (#10703)

--- src/System.Management.Automation/engine/CommonCommandParameters.cs
+++ src/System.Management.Automation/engine/CommonCommandParameters.cs
@@ -108,6 +108,12 @@ namespace System.Management.Automation.Internal
         /// <remarks>
         /// This parameter tells the command what to do when an informational record occurs.
         /// </remarks>
+        /// <!--
+        /// NOTE: The "infa" alias name does not follow the same alias naming convention used
+        /// with other common parameter aliases that control stream functionality; however,
+        /// "ia" was already taken as a parameter alias in other commands when this parameter
+        /// was added to PowerShell, so "infa" was chosen instead.
+        /// -->
         [Parameter]
         [Alias("infa")]
         public ActionPreference InformationAction
