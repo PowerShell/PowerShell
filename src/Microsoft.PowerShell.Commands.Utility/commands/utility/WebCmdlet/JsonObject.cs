44db3656751518fa8dccdf949b967e9c7289fa3c
Mateus Dalepiane <mdalepiane@gmail.com>
2019-10-13 22:17:55 -0300

    Keep public constructors unchanged and add new ones

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/JsonObject.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/JsonObject.cs
@@ -70,10 +70,41 @@ namespace Microsoft.PowerShell.Commands
             /// </summary>
             /// <param name="maxDepth">The maximum depth to visit the object.</param>
             /// <param name="enumsAsStrings">Indicates whether to use enum names for the JSON conversion.</param>
+            /// <param name="compressOutput">Indicates whether to get the compressed output.</param>
+            public ConvertToJsonContext(int maxDepth, bool enumsAsStrings, bool compressOutput)
+                : this(maxDepth, enumsAsStrings, excludeNull: false, compressOutput, CancellationToken.None, StringEscapeHandling.Default, targetCmdlet: null)
+            {
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="ConvertToJsonContext"/> struct.
+            /// </summary>
+            /// <param name="maxDepth">The maximum depth to visit the object.</param>
+            /// <param name="enumsAsStrings">Indicates whether to use enum names for the JSON conversion.</param>
             /// <param name="excludeNull">Indicates whether to include null values for the JSON conversion.</param>
             /// <param name="compressOutput">Indicates whether to get the compressed output.</param>
             public ConvertToJsonContext(int maxDepth, bool enumsAsStrings, bool excludeNull, bool compressOutput)
                 : this(maxDepth, enumsAsStrings, excludeNull, compressOutput, CancellationToken.None, StringEscapeHandling.Default, targetCmdlet: null)
+            {
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="ConvertToJsonContext"/> struct.
+            /// </summary>
+            /// <param name="maxDepth">The maximum depth to visit the object.</param>
+            /// <param name="enumsAsStrings">Indicates whether to use enum names for the JSON conversion.</param>
+            /// <param name="compressOutput">Indicates whether to get the compressed output.</param>
+            /// <param name="cancellationToken">Specifies the cancellation token for cancelling the operation.</param>
+            /// <param name="stringEscapeHandling">Specifies how strings are escaped when writing JSON text.</param>
+            /// <param name="targetCmdlet">Specifies the cmdlet that is calling this method.</param>
+            public ConvertToJsonContext(
+                int maxDepth,
+                bool enumsAsStrings,
+                bool compressOutput,
+                CancellationToken cancellationToken,
+                StringEscapeHandling stringEscapeHandling,
+                PSCmdlet targetCmdlet)
+                : this(maxDepth, enumsAsStrings, excludeNull: false, compressOutput, cancellationToken, stringEscapeHandling, targetCmdlet)
             {
             }
 
