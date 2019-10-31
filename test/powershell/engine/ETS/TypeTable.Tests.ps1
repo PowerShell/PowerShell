62745d4e33aee4045c223fdb2b2b9ac55ac51954
Dongbo Wang <dongbow@microsoft.com>
2019-10-31 10:26:33 -0700

    Address Aditya's comments

--- test/powershell/engine/ETS/TypeTable.Tests.ps1
+++ test/powershell/engine/ETS/TypeTable.Tests.ps1
@@ -28,53 +28,53 @@ Describe "Built-in type information tests" -Tag "CI" {
         $typeData.Members.Count | Should -BeExactly 6
         $typeData.Members['Size'] | Should -BeOfType "System.Management.Automation.Runspaces.ScriptPropertyData"
         $typeData.Members['Size'].GetScriptBlock.ToString() | Should -BeExactly '$this.ModuleMemorySize / 1024'
-        $typeData.Members['Size'].SetScriptBlock | Should -Be $null
-        $typeData.Members['Size'].IsHidden | Should -Be $false
+        $typeData.Members['Size'].SetScriptBlock | Should -BeNullOrEmpty
+        $typeData.Members['Size'].IsHidden | Should -BeFalse
         $typeData.Members['Size'].Name | Should -Be "Size"
 
         $typeData.Members['Company'] | Should -BeOfType "System.Management.Automation.Runspaces.ScriptPropertyData"
         $typeData.Members['Company'].GetScriptBlock.ToString() | Should -BeExactly '$this.FileVersionInfo.CompanyName'
-        $typeData.Members['Company'].SetScriptBlock | Should -Be $null
-        $typeData.Members['Company'].IsHidden | Should -Be $false
+        $typeData.Members['Company'].SetScriptBlock | Should -BeNullOrEmpty
+        $typeData.Members['Company'].IsHidden | Should -BeFalse
         $typeData.Members['Company'].Name | Should -Be "Company"
 
         $typeData.Members['FileVersion'] | Should -BeOfType "System.Management.Automation.Runspaces.ScriptPropertyData"
         $typeData.Members['FileVersion'].GetScriptBlock.ToString() | Should -BeExactly '$this.FileVersionInfo.FileVersion'
-        $typeData.Members['FileVersion'].SetScriptBlock | Should -Be $null
-        $typeData.Members['FileVersion'].IsHidden | Should -Be $false
+        $typeData.Members['FileVersion'].SetScriptBlock | Should -BeNullOrEmpty
+        $typeData.Members['FileVersion'].IsHidden | Should -BeFalse
         $typeData.Members['FileVersion'].Name | Should -Be "FileVersion"
 
         $typeData.Members['ProductVersion'] | Should -BeOfType "System.Management.Automation.Runspaces.ScriptPropertyData"
         $typeData.Members['ProductVersion'].GetScriptBlock.ToString() | Should -BeExactly '$this.FileVersionInfo.ProductVersion'
-        $typeData.Members['ProductVersion'].SetScriptBlock | Should -Be $null
-        $typeData.Members['ProductVersion'].IsHidden | Should -Be $false
+        $typeData.Members['ProductVersion'].SetScriptBlock | Should -BeNullOrEmpty
+        $typeData.Members['ProductVersion'].IsHidden | Should -BeFalse
         $typeData.Members['ProductVersion'].Name | Should -Be "ProductVersion"
 
         $typeData.Members['Description'] | Should -BeOfType "System.Management.Automation.Runspaces.ScriptPropertyData"
         $typeData.Members['Description'].GetScriptBlock.ToString() | Should -BeExactly '$this.FileVersionInfo.FileDescription'
-        $typeData.Members['Description'].SetScriptBlock | Should -Be $null
-        $typeData.Members['Description'].IsHidden | Should -Be $false
+        $typeData.Members['Description'].SetScriptBlock | Should -BeNullOrEmpty
+        $typeData.Members['Description'].IsHidden | Should -BeFalse
         $typeData.Members['Description'].Name | Should -Be "Description"
 
         $typeData.Members['Product'] | Should -BeOfType "System.Management.Automation.Runspaces.ScriptPropertyData"
         $typeData.Members['Product'].GetScriptBlock.ToString() | Should -BeExactly '$this.FileVersionInfo.ProductName'
-        $typeData.Members['Product'].SetScriptBlock | Should -Be $null
-        $typeData.Members['Product'].IsHidden | Should -Be $false
+        $typeData.Members['Product'].SetScriptBlock | Should -BeNullOrEmpty
+        $typeData.Members['Product'].IsHidden | Should -BeFalse
         $typeData.Members['Product'].Name | Should -Be "Product"
 
-        $typeData.TypeConverter | Should -Be $null
-        $typeData.TypeAdapter | Should -Be $null
-        $typeData.IsOverride | Should -Be $false
-        $typeData.SerializationMethod | Should -Be $null
-        $typeData.TargetTypeForDeserialization | Should -Be $null
+        $typeData.TypeConverter | Should -BeNullOrEmpty
+        $typeData.TypeAdapter | Should -BeNullOrEmpty
+        $typeData.IsOverride | Should -BeFalse
+        $typeData.SerializationMethod | Should -BeNullOrEmpty
+        $typeData.TargetTypeForDeserialization | Should -BeNullOrEmpty
         $typeData.SerializationDepth | Should -Be 0
-        $typeData.DefaultDisplayProperty | Should -Be $null
-        $typeData.InheritPropertySerializationSet | Should -Be $false
-        $typeData.StringSerializationSource | Should -Be $null
-        $typeData.StringSerializationSourceProperty | Should -Be $null
-        $typeData.DefaultDisplayPropertySet | Should -Be $null
-        $typeData.DefaultKeyPropertySet | Should -Be $null
-        $typeData.PropertySerializationSet | Should -Be $null
+        $typeData.DefaultDisplayProperty | Should -BeNullOrEmpty
+        $typeData.InheritPropertySerializationSet | Should -BeFalse
+        $typeData.StringSerializationSource | Should -BeNullOrEmpty
+        $typeData.StringSerializationSourceProperty | Should -BeNullOrEmpty
+        $typeData.DefaultDisplayPropertySet | Should -BeNullOrEmpty
+        $typeData.DefaultKeyPropertySet | Should -BeNullOrEmpty
+        $typeData.PropertySerializationSet | Should -BeNullOrEmpty
     }
 
     It "Should have expected member info for 'System.Management.Automation.ParameterSetMetadata'" {
@@ -83,20 +83,20 @@ Describe "Built-in type information tests" -Tag "CI" {
 
         $typeData.Members.Count | Should -BeExactly 1
         $typeData.Members['Flags'] | Should -BeOfType "System.Management.Automation.Runspaces.CodePropertyData"
-        $typeData.Members['Flags'].IsHidden | Should -Be $false
+        $typeData.Members['Flags'].IsHidden | Should -BeFalse
         $typeData.Members['Flags'].Name | Should -Be "Flags"
 
-        $typeData.TypeConverter | Should -Be $null
-        $typeData.TypeAdapter | Should -Be $null
-        $typeData.IsOverride | Should -Be $false
-        $typeData.TargetTypeForDeserialization | Should -Be $null
+        $typeData.TypeConverter | Should -BeNullOrEmpty
+        $typeData.TypeAdapter | Should -BeNullOrEmpty
+        $typeData.IsOverride | Should -BeFalse
+        $typeData.TargetTypeForDeserialization | Should -BeNullOrEmpty
         $typeData.SerializationDepth | Should -Be 0
-        $typeData.DefaultDisplayProperty | Should -Be $null
-        $typeData.InheritPropertySerializationSet | Should -Be $false
-        $typeData.StringSerializationSource | Should -Be $null
-        $typeData.StringSerializationSourceProperty | Should -Be $null
-        $typeData.DefaultDisplayPropertySet | Should -Be $null
-        $typeData.DefaultKeyPropertySet | Should -Be $null
+        $typeData.DefaultDisplayProperty | Should -BeNullOrEmpty
+        $typeData.InheritPropertySerializationSet | Should -BeFalse
+        $typeData.StringSerializationSource | Should -BeNullOrEmpty
+        $typeData.StringSerializationSourceProperty | Should -BeNullOrEmpty
+        $typeData.DefaultDisplayPropertySet | Should -BeNullOrEmpty
+        $typeData.DefaultKeyPropertySet | Should -BeNullOrEmpty
 
         $typeData.SerializationMethod | Should -BeExactly "SpecificProperties"
         $typeData.PropertySerializationSet | Should -BeOfType "System.Management.Automation.Runspaces.PropertySetData"
@@ -109,17 +109,17 @@ Describe "Built-in type information tests" -Tag "CI" {
 
         $typeData.Members.Count | Should -BeExactly 0
 
-        $typeData.TypeConverter | Should -Be $null
-        $typeData.TypeAdapter | Should -Be $null
-        $typeData.IsOverride | Should -Be $false
-        $typeData.SerializationMethod | Should -Be $null
-        $typeData.TargetTypeForDeserialization | Should -Be $null
-        $typeData.DefaultDisplayProperty | Should -Be $null
-        $typeData.InheritPropertySerializationSet | Should -Be $false
-        $typeData.StringSerializationSource | Should -Be $null
-        $typeData.StringSerializationSourceProperty | Should -Be $null
-        $typeData.DefaultDisplayPropertySet | Should -Be $null
-        $typeData.DefaultKeyPropertySet | Should -Be $null
+        $typeData.TypeConverter | Should -BeNullOrEmpty
+        $typeData.TypeAdapter | Should -BeNullOrEmpty
+        $typeData.IsOverride | Should -BeFalse
+        $typeData.SerializationMethod | Should -BeNullOrEmpty
+        $typeData.TargetTypeForDeserialization | Should -BeNullOrEmpty
+        $typeData.DefaultDisplayProperty | Should -BeNullOrEmpty
+        $typeData.InheritPropertySerializationSet | Should -BeFalse
+        $typeData.StringSerializationSource | Should -BeNullOrEmpty
+        $typeData.StringSerializationSourceProperty | Should -BeNullOrEmpty
+        $typeData.DefaultDisplayPropertySet | Should -BeNullOrEmpty
+        $typeData.DefaultKeyPropertySet | Should -BeNullOrEmpty
 
         $typeData.SerializationDepth | Should -Be 2
     }
