ffebee636b8e3e4efe1c9b27c43c11fb1298d0a9
Michael Poutre <m1kep.my.mail@gmail.com>
2019-10-31 00:44:09 -0700

    Address feedback around logic flow

--- src/System.Management.Automation/engine/CommandCompletion/CompletionCompleters.cs
+++ src/System.Management.Automation/engine/CommandCompletion/CompletionCompleters.cs
@@ -1998,14 +1998,16 @@ namespace System.Management.Automation
             // Fall back to the commandAst command name if a command name is not found. This can be caused by a script block or AST with the matching function definition being passed to CompleteInput
             // This allows for editors and other tools using CompleteInput with Script/AST definations to get values from RegisteredArgumentCompleters to better match the console experience.
             // See issue https://github.com/PowerShell/PowerShell/issues/10567
-            if (string.IsNullOrEmpty(commandName) && string.IsNullOrEmpty(commandAst.GetCommandName()))
+            string actualCommandName = string.IsNullOrEmpty(commandName)
+                        ? commandAst.GetCommandName()
+                        : commandName;
+
+            if (string.IsNullOrEmpty(actualCommandName))
             {
                 return;
             }
 
-            string parameterFullName = string.IsNullOrEmpty(commandName)
-                        ? commandAst.GetCommandName() + ":" + parameterName
-                        : commandName + ":" + parameterName;
+            string parameterFullName = actualCommandName + ":" + parameterName;
 
             ScriptBlock customCompleter = GetCustomArgumentCompleter(
                 "CustomArgumentCompleters",
