6bcdbc32fff35cb59bb6874f2158dff46cba8f6b
Ilya <darpa@yandex.ru>
2019-11-06 10:52:53 +0500

    Fix ast.cs

--- src/System.Management.Automation/engine/parser/ast.cs
+++ src/System.Management.Automation/engine/parser/ast.cs
@@ -5348,7 +5348,7 @@ namespace System.Management.Automation.Language
     public abstract class ChainableAst : PipelineBaseAst
     {
         /// <summary>
-        /// Construct a new chainable AST with the given extent.
+        ///  Initializes a new instance of the new chainable AST with the given extent.
         /// </summary>
         /// <param name="extent">The script extent of the AST.</param>
         protected ChainableAst(IScriptExtent extent) : base(extent)
@@ -5363,7 +5363,7 @@ namespace System.Management.Automation.Language
     public class PipelineChainAst : ChainableAst
     {
         /// <summary>
-        /// Create a new statement chain AST from two statements and an operator.
+        ///  Initializes a new instance of the new statement chain AST from two statements and an operator.
         /// </summary>
         /// <param name="extent">The extent of the chained statement.</param>
         /// <param name="lhsChain">The pipeline or pipeline chain to the left of the operator.</param>
@@ -5403,28 +5403,31 @@ namespace System.Management.Automation.Language
         }
 
         /// <summary>
-        /// The left hand pipeline in the chain.
+        /// Gets the left hand pipeline in the chain.
         /// </summary>
         public ChainableAst LhsPipelineChain { get; }
 
         /// <summary>
-        /// The right hand pipeline in the chain.
+        /// Gets the right hand pipeline in the chain.
         /// </summary>
         public PipelineAst RhsPipeline { get; }
 
         /// <summary>
-        /// The chaining operator used.
+        /// Gets the chaining operator used.
         /// </summary>
         public TokenKind Operator { get; }
 
         /// <summary>
-        /// Indicates whether this chain has been invoked with the background operator.
+        /// Gets a flag that indicates whether this chain has been invoked with the background operator.
         /// </summary>
         public bool Background { get; }
 
         /// <summary>
         /// Create a copy of this Ast.
         /// </summary>
+        /// <returns>
+        /// A fresh copy of this PipelineChainAst instance.
+        /// </returns>
         public override Ast Copy()
         {
             return new PipelineChainAst(Extent, CopyElement(LhsPipelineChain), CopyElement(RhsPipeline), Operator, Background);
