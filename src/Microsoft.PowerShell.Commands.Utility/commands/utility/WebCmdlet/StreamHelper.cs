52a63b1a87d30cf58bb546f1057be101bfe07273
Aditya Patwardhan <adityap@microsoft.com>
2019-11-06 17:08:14 -0800

    Fix CodeFactor issues

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/StreamHelper.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/StreamHelper.cs
@@ -386,8 +386,10 @@ namespace Microsoft.PowerShell.Commands
                     // The change was made in https://github.com/dotnet/coreclr/pull/27229
                     // The recommendation from .NET team is to not check for 'completed' if 'flush' is false.
                     // Break out of the loop if all bytes have been read.
-                    if(!flush && bytesRead == byteIndex)
+                    if (!flush && bytesRead == byteIndex)
+                    {
                         break;
+                    }
                 }
             } while (bytesRead != 0);
 
