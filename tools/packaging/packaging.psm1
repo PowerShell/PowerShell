174c67e640c01aac004e620ea2381736e73102d6
Travis Plunk <github@ez13.net>
2019-11-09 18:14:44 -0800

    fix debian

--- tools/packaging/packaging.psm1
+++ tools/packaging/packaging.psm1
@@ -1165,11 +1165,10 @@ function Get-PackageDependencies
                 "zlib1g"
             )
 
-            switch ($Distribution) {
-                "ubuntu.16.04" { $Dependencies += @("libssl1.0.0", "libicu55") }
-                "ubuntu.17.10" { $Dependencies += @("libssl1.0.0", "libicu57") }
-                "ubuntu.18.04" { $Dependencies += @("libssl1.0.0", "libicu60") }
-                "debian.9" { $Dependencies += @("libssl1.0.2", "libicu57") }
+            switch -regex ($Distribution) {
+                "ubuntu\.16\.04" { $Dependencies += @("libssl1.0.0", "libicu55") }
+                "ubuntu\.18\.04" { $Dependencies += @("libssl1.0.0", "libicu60") }
+                "debian\.(9|10|11)" { $Dependencies += @("libssl1.0.2", "libicu57") }
                 default { throw "Debian distro '$Distribution' is not supported." }
             }
         } elseif ($Environment.IsRedHatFamily) {
