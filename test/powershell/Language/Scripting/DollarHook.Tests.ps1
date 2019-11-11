982d3f034f276565b4d444c1366c185bb63057ce
Robert Holt <rjmholt_msft@outlook.com>
2019-11-11 12:57:54 -0800

    Add tests

---
+++ test/powershell/Language/Scripting/DollarHook.Tests.ps1
@@ -0,0 +1,39 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+Describe 'Tests for setting $? for execution success' -Tag 'CI' {
+    BeforeAll {
+        $script:hookValues = [System.Collections.Generic.List[bool]]::new()
+
+        function Hook
+        {
+            param([Parameter(Position = 0)][bool]$Value)
+
+            $script:hookValues.Add($Value)
+        }
+
+        $testCases = @(
+            @{ Script = '"Hi"; Hook $?'; Results = @($true) }
+            @{ Script = 'Write-Output "Hi"; Hook $?'; Results = @($true) }
+            @{ Script = 'Write-Error "Bad"; Hook $?'; Results = @($false) }
+            @{ Script = 'Write-Error "Bad"; Hook $?; Hook $?'; Results = @($false, $true) }
+            @{ Script = '(Write-Output "Hi"); Hook $?'; Results = @($true) }
+            @{ Script = '("Hi"); Hook $?'; Results = @($true) }
+            @{ Script = '$("Hi"); Hook $?'; Results = @($true) }
+            @{ Script = '(Write-Error "Bad"); Hook $?; Hook $?'; Results = @($false, $true) }
+            @{ Script = '$(Write-Error "Bad"); Hook $?; Hook $?'; Results = @($false, $true) }
+        )
+    }
+
+    AfterEach {
+        $script:hookValues.Clear()
+    }
+
+    It 'Sets $? for case ''<Script>''' -TestCases $testCases {
+        param([string]$Script, [object[]]$Results)
+
+        Invoke-Expression $Script 2>&1 > $null
+
+        $script:hookValues | Should -Be $Results
+    }
+}
