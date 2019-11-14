89d92adde78bb508d3c01c58dc5a90342a6663da
Kirk Munro <KirkMunro@users.noreply.github.com>
2019-11-13 23:16:42 -0400

    put WorkflowInfo public class back as obsolete

---
+++ src/System.Management.Automation/engine/WorkflowInfo.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace System.Management.Automation
+{
+    /// <summary>
+    /// Deprecated. Do not use.
+    /// </summary>
+    /// <remarks>
+    /// This class must not be deleted. Existing modules depend on this public
+    /// interface being present in PowerShell 7 to function properly, even if
+    /// they do not use workflows.
+    /// </remarks>
+    [Obsolete]
+    public class WorkflowInfo
+    {
+    }
+}
