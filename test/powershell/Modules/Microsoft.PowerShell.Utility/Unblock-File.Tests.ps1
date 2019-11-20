8adccdcafe268b4ebe86067f186c3528778971a5
Travis Plunk <github@ez13.net>
2019-11-20 15:24:09 -0800

    Combine macOS and Windows tests

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Unblock-File.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Unblock-File.Tests.ps1
@@ -3,13 +3,33 @@
 
 Describe "Unblock-File" -Tags "CI" {
 
-    Context "Windows" {
+    Context "Windows and macOS" {
         BeforeAll {
-            function Test-UnblockFile {
-                { Get-Content -Path $testfilepath -Stream Zone.Identifier -ErrorAction Stop | Out-Null } |
+
+            if ($IsWindows) {
+                function Test-UnblockFile {
+                    { Get-Content -Path $testfilepath -Stream Zone.Identifier -ErrorAction Stop | Out-Null } |
                     Should -Throw -ErrorId "GetContentReaderFileNotFoundError,Microsoft.PowerShell.Commands.GetContentCommand"
+                }
+                function Block-File {
+                    param($path)
+                    Set-Content -Value "[ZoneTransfer]`r`nZoneId=4" -Path $path -Stream Zone.Identifier
+                }
             }
-                        if ( ! $IsWindows )
+            else {
+                function Test-UnblockFile {
+                    $result = (xattr $testfilepath | Select-String 'com.apple.com.quarantine')
+                    $result | Should -BeNullOrEmpty
+                }
+                function Block-File {
+                    param($path)
+                    Set-Content -Path $path -value 'test'
+                    xattr -w com.apple.quarantine '0081;5dd5c373;Microsoft Edge;1A9A933D-619A-4036-BAF3-17A7966A1BA8' $path
+
+                }
+            }
+
+            if ( $IsLinux )
             {
                 $origDefaults = $PSDefaultParameterValues.Clone()
                 $PSDefaultParameterValues['it:skip'] = $true
@@ -20,15 +40,13 @@ Describe "Unblock-File" -Tags "CI" {
         }
 
         AfterAll {
-            if ( ! $IsWindows ){
+            if ( $IsLinux ){
                 $global:PSDefaultParameterValues = $origDefaults
             }
         }
 
         BeforeEach {
-            if ( $IsWindows ){
-                Set-Content -Value "[ZoneTransfer]`r`nZoneId=4" -Path $testfilepath -Stream Zone.Identifier
-            }
+            Block-File -Path $testfilepath
         }
 
         It "With '-Path': no file exist" {
@@ -54,86 +72,22 @@ Describe "Unblock-File" -Tags "CI" {
 
         It "Write an error if a file is read only" {
             $TestFile = Join-Path $TestDrive "testfileunlock.ps1"
-            Set-Content -Path $TestFile -value 'test'
-            $ZoneIdentifier = {
-                [ZoneTransfer]
-                ZoneId=3
-            }
-            Set-Content -Path $TestFile -Value $ZoneIdentifier -Stream 'Zone.Identifier'
+            Block-File -Path $TestFile
             Set-ItemProperty -Path $TestFile -Name IsReadOnly -Value $True
 
             $TestFileCreated = Get-ChildItem $TestFile
             $TestFileCreated.IsReadOnly | Should -BeTrue
-
-            { Unblock-File -LiteralPath $TestFile -ErrorAction Stop } | Should -Throw -ErrorId "RemoveItemUnableToAccessFile,Microsoft.PowerShell.Commands.UnblockFileCommand"
-        }
-    }
-
-    Context "macOS" {
-        BeforeAll {
-            function Test-UnblockFile {
-                $result = (xattr $testfilepath | Select-String 'com.apple.com.quarantine')
-                $result | Should -BeNullOrEmpty
-            }
-
-            if ( ! $IsMacOS )
+            if($IsWindows)
             {
-                $origDefaults = $PSDefaultParameterValues.Clone()
-                $PSDefaultParameterValues['it:skip'] = $true
-
+                $expectedError = "RemoveItemUnableToAccessFile,Microsoft.PowerShell.Commands.UnblockFileCommand"
             } else {
-                $testfilepath = Join-Path -Path $TestDrive -ChildPath testunblockfile.ttt
-                New-Item -Path $testfilepath -ItemType File
+                $expectedError = "UnblockError,Microsoft.PowerShell.Commands.UnblockFileCommand"
             }
-        }
 
-        AfterAll {
-            if ( ! $IsMacOS ){
-                $global:PSDefaultParameterValues = $origDefaults
-            }
-        }
-
-        BeforeEach {
-            if ( $IsMacOS ){
-                xattr -w com.apple.quarantine '0081;5dd5c373;Microsoft Edge;1A9A933D-619A-4036-BAF3-17A7966A1BA8' $testfilepath
-            }
-        }
-
-        It "With '-Path': no file exist" {
-            { Unblock-File -Path nofileexist.ttt -ErrorAction Stop } | Should -Throw -ErrorId "FileNotFound,Microsoft.PowerShell.Commands.UnblockFileCommand"
-        }
-
-        It "With '-LiteralPath': no file exist" {
-            { Unblock-File -LiteralPath nofileexist.ttt -ErrorAction Stop } | Should -Throw -ErrorId "FileNotFound,Microsoft.PowerShell.Commands.UnblockFileCommand"
-        }
-
-        # the error code in not unique
-        # cannot suppress the error if it's already unblocked.
-        It "With '-Path': file exist" {
-            Unblock-File -Path $testfilepath
-            Test-UnblockFile
-
-            # If a file is not blocked we silently return without an error.
-            { Unblock-File -Path $testfilepath -ErrorAction Stop } | Should -Not -Throw
-        }
-
-        It "With '-LiteralPath': file exist" {
-            Unblock-File -LiteralPath $testfilepath
-            Test-UnblockFile
-        }
-
-        It "Write an error if a file is read only" {
-            $TestFile = Join-Path $TestDrive "testfileunlock.ps1"
-            Set-Content -Path $TestFile -value 'test'
-            xattr -w com.apple.quarantine '0081;5dd5c373;Microsoft Edge;1A9A933D-619A-4036-BAF3-17A7966A1BA8' $TestFile
-            Set-ItemProperty -Path $TestFile -Name IsReadOnly -Value $True
-
-            $TestFileCreated = Get-ChildItem $TestFile
-            $TestFileCreated.IsReadOnly | Should -BeTrue
-
-            { Unblock-File -LiteralPath $TestFile -ErrorAction Stop } | Should -Throw -ErrorId "UnblockError,Microsoft.PowerShell.Commands.UnblockFileCommand"
+            { Unblock-File -LiteralPath $TestFile -ErrorAction Stop } | Should -Throw -ErrorId $expectedError
         }
     }
+
     Context "Linux" {
         BeforeAll {
             if ( ! $IsLinux )
