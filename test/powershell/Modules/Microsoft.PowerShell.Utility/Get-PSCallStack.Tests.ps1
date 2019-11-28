6e5daaa42d00091e316915785480c1c05308fbed
Ilya <darpa@yandex.ru>
2019-11-28 12:48:43 +0500

    Fix regression in Get-PSCallStack

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Get-PSCallStack.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Get-PSCallStack.Tests.ps1
@@ -49,17 +49,20 @@ Describe "Get-PSCallStack DRT Unit Tests" -Tags "CI" {
         $results[0].ScriptName | Should -Be $scriptFilePath
         $results[0].ScriptLineNumber | Should -Be 27
         $results[0].InvocationInfo.ScriptLineNumber | Should -Be 9
+        $results[0].Location | Should -Match $scriptFileName
 
         $results[1].Command | Should -BeExactly "foo"
         $results[1].ScriptName | Should -Be $scriptFilePath
         $results[1].ScriptLineNumber | Should -Be 9
         $results[1].InvocationInfo.ScriptLineNumber | Should -Be 32
+        $results[1].Location | Should -Match $scriptFileName
 
         #InvocationInfo.ScriptLineNumber: Gets the line number of the script that contains the command
         $results[2].Command | Should -Be $scriptFileName
         $results[2].ScriptName | Should -Be $scriptFilePath
         $results[2].ScriptLineNumber | Should -Be 32
         $results[2].InvocationInfo.ScriptLineNumber | Should -Be 46
+        $results[2].Location | Should -Match $scriptFileName
     }
 
     It "Verify that the script block of a trap statement shows up on the call stack" {
@@ -85,5 +88,9 @@ Describe "Get-PSCallStack DRT Unit Tests" -Tags "CI" {
         $results[1].ScriptName | Should -Be $scriptFilePath
         $results[1].ScriptLineNumber | Should -Be 7
         $results[1].InvocationInfo.ScriptLineNumber | Should -Be 77
+    }
+
+    It "Get-PSCallStack returns Arguments" {
+        & { (Get-PSCallStack)[0].Arguments } 'foo'  | Should -Match 'foo'
     }
 }
