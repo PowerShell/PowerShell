fd9f55612a271500498e6288a05be22cad774193
Steve Lee <slee@microsoft.com>
2019-12-03 13:20:47 -0800

    Update calculation of char width to respect CJK chars correctly

--- src/System.Management.Automation/FormatAndOutput/common/ILineOutput.cs
+++ src/System.Management.Automation/FormatAndOutput/common/ILineOutput.cs
@@ -29,7 +29,14 @@ namespace Microsoft.PowerShell.Commands.Internal.Format
 
         internal virtual int Length(string str, int offset)
         {
-            return str.Length - offset;
+            int length = 0;
+
+            foreach (char c in str)
+            {
+                length += LengthInBufferCells(c);
+            }
+
+            return length - offset;
         }
 
         internal virtual int Length(char character) { return 1; }
@@ -57,6 +64,29 @@ namespace Microsoft.PowerShell.Commands.Internal.Format
         }
 
         #region Helpers
+
+        protected static int LengthInBufferCells(char c)
+        {
+            // The following is based on http://www.cl.cam.ac.uk/~mgk25/c/wcwidth.c
+            // which is derived from https://www.unicode.org/Public/UCD/latest/ucd/EastAsianWidth.txt
+
+            bool isWide = c >= 0x1100 &&
+                (c <= 0x115f || /* Hangul Jamo init. consonants */
+                 c == 0x2329 || c == 0x232a ||
+                 (c >= 0x2e80 && c <= 0xa4cf &&
+                  c != 0x303f) || /* CJK ... Yi */
+                 (c >= 0xac00 && c <= 0xd7a3) || /* Hangul Syllables */
+                 (c >= 0xf900 && c <= 0xfaff) || /* CJK Compatibility Ideographs */
+                 (c >= 0xfe10 && c <= 0xfe19) || /* Vertical forms */
+                 (c >= 0xfe30 && c <= 0xfe6f) || /* CJK Compatibility Forms */
+                 (c >= 0xff00 && c <= 0xff60) || /* Fullwidth Forms */
+                 (c >= 0xffe0 && c <= 0xffe6));
+            // We can ignore these ranges because .Net strings use surrogate pairs
+            // for this range and we do not handle surrogage pairs.
+            // (c >= 0x20000 && c <= 0x2fffd) ||
+            // (c >= 0x30000 && c <= 0x3fffd)
+            return 1 + (isWide ? 1 : 0);
+        }
 
         /// <summary>
         /// Given a string and a number of display cells, it computes how many
