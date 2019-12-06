9e74d8ec23a269b891630b437b3f6751f2cf6817
Michael Klement <mklement0@gmail.com>
2019-12-06 08:42:33 -0500

    Addressed @isazanov's feedback

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Get-FormatData.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Get-FormatData.Tests.ps1
@@ -10,25 +10,25 @@ Describe "Get-FormatData" -Tags "CI" {
     }
 
     Context "Local use: Can get format data requiring v5.1 by default" {
-        $cmds =
-            @{ cmd = { Get-FormatData System.IO.FileInfo } },
-            @{ cmd = { (Get-FormatData System.IO.FileInfo &) | Receive-Job -Wait -AutoRemoveJob } }
+        BeforeAll {
+            $cmds =
+                @{ cmd = { Get-FormatData System.IO.FileInfo } },
+                @{ cmd = { (Get-FormatData System.IO.FileInfo &) | Receive-Job -Wait -AutoRemoveJob } }
+        }
         It "Can get format data requiring v5.1 with <cmd>" -TestCases $cmds {
             param([scriptblock] $cmd)
-            $format = & $cmd
-            $format.TypeNames | Should -HaveCount 2
-            $format.TypeNames[0] | Should -BeExactly "System.IO.DirectoryInfo"
-            $format.TypeNames[1] | Should -BeExactly "System.IO.FileInfo"
-            $format.FormatViewDefinition | Should -HaveCount ($IsWindows ? 4 : 5)
+            (& $cmd).TypeNames | Should -Contain 'System.IO.FileInfo'
         }
     }
 
     Context "Remote use: Don't get format data requiring v5.1 by default" {
-        # Simulate a remoting session.
-        $PSSenderInfo = [pscustomobject] @{ PSShowComputerName = $true }
-        $cmds =
-            @{ cmd = { Get-FormatData System.IO.FileInfo }; shouldBeNull = $true },
-            @{ cmd = { Get-FormatData System.IO.FileInfo -PowerShellVersion 5.1 }; shouldBeNull = $false }
+        BeforeAll {
+            # Simulate a remoting session.
+            $PSSenderInfo = [pscustomobject] @{ PSShowComputerName = $true }
+            $cmds =
+                @{ cmd = { Get-FormatData System.IO.FileInfo }; shouldBeNull = $true },
+                @{ cmd = { Get-FormatData System.IO.FileInfo -PowerShellVersion 5.1 }; shouldBeNull = $false }
+        }
         It "When remoting, <cmd> should return <shouldBeNull> for a null-output test" -TestCases $cmds {
             param([scriptblock] $cmd, [bool] $shouldBeNull)
             $null -eq $(& $cmd) | Should -Be $shouldBeNull
