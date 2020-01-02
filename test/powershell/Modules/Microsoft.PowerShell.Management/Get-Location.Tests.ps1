7308897d9ffa48d19c0230431d8c1ebc4a605880
Lucas Galleguillos <h93xV2@users.noreply.github.com>
2020-01-02 00:33:21 -0800

    Update Get-Location tests per review
    Correct the number of spaces used for indentation from 4 to the standard 2.
    Remove alias test because aliases are already tested elsewhere.
    Improve test cleanup.
    Utilize Pester's $TempDrive for automatic directory cleanup.

--- test/powershell/Modules/Microsoft.PowerShell.Management/Get-Location.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Get-Location.Tests.ps1
@@ -1,81 +1,77 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 Describe "Get-Location" -Tags "CI" {
-    $currentDirectory=[System.IO.Directory]::GetCurrentDirectory()
+  $currentDirectory=[System.IO.Directory]::GetCurrentDirectory()
 
-    BeforeEach {
-        Push-Location $currentDirectory
+  BeforeEach {
+    Push-Location $currentDirectory
+  }
+
+  AfterEach {
+	Pop-location
+  }
+
+  It "Should list the output of the current working directory" {
+	(Get-Location).Path | Should -BeExactly $currentDirectory
+  }
+
+  It "Should throw an exception when missing an argument for parameter 'PSProvider'" {
+    { Get-Location -PSProvider } | Should -Throw -ErrorId "MissingArgument"
+  }
+
+  It "Should throw an exception when missing an argument for parameter 'PSDrive'" {
+    { Get-Location -PSDrive } | Should -Throw -ErrorId "MissingArgument"
+  }
+
+  It "Should throw an exception when missing an argument for parameter 'StackName'" {
+    { Get-Location -StackName } | Should -Throw -ErrorId "MissingArgument"
+  }
+
+  It "Should return a PathInfo object when no parameters are given" {
+    Get-Location | Should -BeOfType System.Management.Automation.PathInfo
+  }
+
+  It "Should return a PathInfo object given a valid argument for parameter 'PSDrive'" {
+    $tempPSDriveName = "GetLocationTempPSDrive"
+
+    $tempPSDrive = New-PSDrive -Name $tempPSDriveName -PSProvider "FileSystem" -Root $currentDirectory
+
+    try {
+      Get-Location -PSDrive $tempPSDriveName | Should -BeOfType System.Management.Automation.PathInfo
+    } finally {
+      $tempPSDrive | Remove-PSDrive
     }
+  }
 
-    AfterEach {
-	    Pop-location
-    }
+  It "Should return a PathInfo object given a valid argument for parameter 'PSProvider'" {
+    Get-Location -PSProvider alias | Should -BeOfType System.Management.Automation.PathInfo
+  }
 
-    It "Should list the output of the current working directory" {
-	    (Get-Location).Path | Should -BeExactly $currentDirectory
-    }
+  It "Should return a PathInfoStack object for parameter 'Stack'" {
+    Get-Location -Stack | Should -BeOfType System.Management.Automation.PathInfoStack
+  }
 
-    It "Should do exactly the same thing as its alias" {
-	    (pwd).Path | Should -BeExactly (Get-Location).Path
-    }
+  It "Should return a PathInfoStack with the correct values for parameter 'Stack'" {
+    $stackAsArray = (Get-Location -Stack).ToArray()
 
-    It "Should throw an exception when missing an argument for parameter 'PSProvider'" {
-         { Get-Location -PSProvider } | Should -Throw -ErrorId "MissingArgument"
-    }
+    $stackAsArray.Length | Should -BeGreaterThan 0
 
-    It "Should throw an exception when missing an argument for parameter 'PSDrive'" {
-        { Get-Location -PSDrive } | Should -Throw -ErrorId "MissingArgument"
-    }
+    $stackAsArray[0] | Should -BeExactly $currentDirectory
+  }
 
-    It "Should throw an exception when missing an argument for parameter 'StackName'" {
-        { Get-Location -StackName } | Should -Throw -ErrorId "MissingArgument"
-    }
+  It "Should return a PathInfoStack with the correct values for the argument for parameter 'StackName'" {
+    $tempDirectory = Join-Path -Path $TestDrive -ChildPath "getLocationTempDir"
 
-    It "Should return a PathInfo object when no parameters are given" {
-        Get-Location | Should -BeOfType System.Management.Automation.PathInfo
-    }
+    New-Item -Path ($tempDirectory) -ItemType "directory" > $null
 
-    It "Should return a PathInfo object given a valid argument for parameter 'PSDrive'" {
-        $tempPSDriveName = "GeLocationTempPSDrive"
+    Set-Location -Path $tempDirectory
 
-        $tempPSDrive = New-PSDrive -Name $tempPSDriveName -PSProvider "FileSystem" -Root $currentDirectory
+    Push-Location $currentDirectory -StackName "Stack1"
 
-        Get-Location -PSDrive $tempPSDriveName | Should -BeOfType System.Management.Automation.PathInfo
+    $stackAsArray = (Get-Location -StackName "Stack1").ToArray()
 
-        $tempPSDrive | Remove-PSDrive
-    }
+    $stackAsArray.Length | Should -BeExactly 1
 
-    It "Should return a PathInfo object given a valid argument for parameter 'PSProvider'" {
-        Get-Location -PSProvider alias | Should -BeOfType System.Management.Automation.PathInfo
-    }
-
-    It "Should return a PathInfoStack object for parameter 'Stack'" {
-        Get-Location -Stack | Should -BeOfType System.Management.Automation.PathInfoStack
-    }
-
-    It "Should return a PathInfoStack with the correct values for parameter 'Stack'" {
-        $stackAsArray = (Get-Location -Stack).ToArray()
-
-        $stackAsArray.Length | Should -BeGreaterThan 0
-
-        $stackAsArray[0] | Should -BeExactly $currentDirectory
-    }
-
-    It "Should return a PathInfoStack with the correct values for the argument for parameter 'StackName'" {
-        $tempDirectory = Join-Path -Path $currentDirectory -ChildPath "getLocationTempDir"
-
-        New-Item -Path ($tempDirectory) -ItemType "directory" > $null
-
-        Set-Location -Path $tempDirectory
-
-        Push-Location $currentDirectory -StackName "Stack1"
-
-        $stackAsArray = (Get-Location -StackName "Stack1").ToArray()
-
-        $stackAsArray.Length | Should -BeExactly 1
-
-        $stackAsArray[0].Path | Should -BeExactly $tempDirectory
-
-        Remove-Item -Path $tempDirectory
-    }
+    $stackAsArray[0].Path | Should -BeExactly $tempDirectory
+  }
 }
