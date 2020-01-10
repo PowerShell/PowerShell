66a817e9dd0d08b18eae122198ce0c23fe282f90
Steve Lee <slee@microsoft.com>
2020-01-10 09:53:49 -0800

    Support expanding ~ in $env:PATH when doing command discovery

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Environment-Variables.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Environment-Variables.Tests.ps1
@@ -42,5 +42,44 @@ Describe "Environment-Variables" -Tags "CI" {
 	$ENV:TESTENVIRONMENTVARIABLE | Should -Not -BeNullOrEmpty
 	$ENV:TESTENVIRONMENTVARIABLE | Should -Be $expected
 
-    }
+	}
+
+	Context "~ in PATH" {
+        AfterEach {
+            $env:PATH = $oldPath
+        }
+
+		BeforeAll {
+            $oldPath = $env:PATH
+			$pwsh = (Get-Command pwsh | Select-Object -First 1).Source
+			if ($IsWindows) {
+				$pwsh2 = "pwsh2.exe"
+			}
+			else {
+				$pwsh2 = "pwsh2"
+			}
+
+            Copy-Item -Path $pwsh -Destination "~/$pwsh2"
+            $testPath = Join-Path -Path "~" -ChildPath (New-Guid)
+            New-Item -Path $testPath -ItemType Directory
+            Copy-Item -Path $pwsh -Destination "$testPath/$pwsh2"
+		}
+
+		AfterAll {
+            Remove-Item -Path "~/pwsh2" -Force
+            Remove-Item -Path $testPath -Recurse -Force
+		}
+
+		It "Should be able to resolve ~ in PATH" {
+            $env:PATH = "~" + [System.IO.Path]::PathSeparator + $env:PATH
+            $out = Get-Command pwsh2
+            $out.Source | Should -BeExactly (Join-Path -Path ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)) -ChildPath $pwsh2)
+		}
+
+		It "Should be able to resolve ~/folder in PATH" {
+            $env:PATH = $testPath + [System.IO.Path]::PathSeparator + $env:PATH
+            $out = Get-Command pwsh2
+			$out.Source | Should -BeExactly (Join-Path -Path (Resolve-Path $testPath) -ChildPath $pwsh2)
+		}
+	}
 }
