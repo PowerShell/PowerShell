4f5bb462683dea730535d89da3cdde174300d987
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2020-01-13 18:36:47 +0000

    Remove BOM, insert newline at EOF

--- test/powershell/engine/Remoting/ImplicitRemotingBatching.Tests.ps1
+++ test/powershell/engine/Remoting/ImplicitRemotingBatching.Tests.ps1
@@ -1,4 +1,4 @@
-﻿# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
 Describe "TestImplicitRemotingBatching hook should correctly batch simple remote command pipelines" -Tag 'Feature','RequireAdminOnWindows' {
