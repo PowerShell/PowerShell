823a2805242f78a4d6b424230df227dcb350abe5
James Truher <jimtru@microsoft.com>
2020-01-28 12:30:39 -0800

    Fix typo in test

--- test/powershell/engine/Basic/StandardLibraryTypes.Tests.ps1
+++ test/powershell/engine/Basic/StandardLibraryTypes.Tests.ps1
@@ -5,18 +5,58 @@
 # It does not check member presence and info, just the properties of the type
 Describe "Types referenced by PowerShell Standard should not be missing"  -Tags "CI" {
     BeforeAll {
-        $assets = [IO.Path]::Combine("$PSScriptRoot", "assets", "standardtypes.csv")
+        $assets = [System.IO.Path]::Combine("$PSScriptRoot", "assets", "standardtypes.csv")
         # The properties of a type which should match PowerShell Standard
         # These are not members of the type
-        $typeProperties = "IsCollectible", "IsSZArray", "IsByRefLike", "IsConstructedGenericType", "IsGenericType", "IsGenericTypeDefinition",
-            "IsGenericParameter", "IsTypeDefinition", "IsSecurityCritical", "IsSecuritySafeCritical", "IsSecurityTransparent", "IsInterface",
-            "IsNested", "IsArray", "IsByRef", "IsPointer", "IsGenericTypeParameter", "IsGenericMethodParameter", "IsVariableBoundArray",
-            "IsAbstract", "IsImport", "IsSealed", "IsSpecialName", "IsClass", "IsNestedAssembly", "IsNestedFamANDAssem", "IsNestedFamily",
-            "IsNestedFamORAssem", "IsNestedPrivate", "IsNestedPublic", "IsNotPublic", "IsPublic", "IsAutoLayout", "IsExplicitLayout",
-            "IsLayoutSequential", "IsAnsiClass", "IsAutoClass", "IsUnicodeClass", "IsCOMObject", "IsContextful", "IsEnum", "IsMarshalByRef",
-            "IsPrimitive", "IsValueType", "IsSignatureType", "IsSerializable", "IsVisible"
+        $typeProperties = "IsCollectible",
+            "IsSZArray",
+            "IsByRefLike",
+            "IsConstructedGenericType",
+            "IsGenericType",
+            "IsGenericTypeDefinition",
+            "IsGenericParameter",
+            "IsTypeDefinition",
+            "IsSecurityCritical",
+            "IsSecuritySafeCritical",
+            "IsSecurityTransparent",
+            "IsInterface",
+            "IsNested",
+            "IsArray",
+            "IsByRef",
+            "IsPointer",
+            "IsGenericTypeParameter",
+            "IsGenericMethodParameter",
+            "IsVariableBoundArray",
+            "IsAbstract",
+            "IsImport",
+            "IsSealed",
+            "IsSpecialName",
+            "IsClass",
+            "IsNestedAssembly",
+            "IsNestedFamANDAssem",
+            "IsNestedFamily",
+            "IsNestedFamORAssem",
+            "IsNestedPrivate",
+            "IsNestedPublic",
+            "IsNotPublic",
+            "IsPublic",
+            "IsAutoLayout",
+            "IsExplicitLayout",
+            "IsLayoutSequential",
+            "IsAnsiClass",
+            "IsAutoClass",
+            "IsUnicodeClass",
+            "IsCOMObject",
+            "IsContextful",
+            "IsEnum",
+            "IsMarshalByRef",
+            "IsPrimitive",
+            "IsValueType",
+            "IsSignatureType",
+            "IsSerializable",
+            "IsVisible"
 
-        $tests = Import-Csv $assets | ForEach-Object { 
+        $tests = Import-Csv $assets | ForEach-Object {
             @{ FullName = $_.FullName; TypeMetaData = $_ }
         }
     }
@@ -24,9 +64,9 @@ Describe "Types referenced by PowerShell Standard should not be missing"  -Tags 
     It "Type '<FullName>' should be present with correct attributes" -TestCases $tests {
         param ( $FullName, $TypeMetaData )
         $type = [psobject].Assembly.GetType($FullName)
-        $t | Should -Not -BeNullOrEmpty
+        $type | Should -Not -BeNullOrEmpty
         foreach ( $property in $typeProperties ) {
-            if ( $typeMetaData.$property -ne $t.$property ) {
+            if ( $typeMetaData.$property -ne $type.$property ) {
                 throw "$property value is not correct"
             }
         }
