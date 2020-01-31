c4e144b13e1209a6a723160d38be9f11ab45b8da
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2020-01-31 05:13:12 +0000

    Replace use of throw GetExceptionForHR with ThrowExceptionForHR (#11640)
    `System.Runtime.InteropServices.Marshall.GetExceptionForHR` may not return an exception. Instead of `throw GetExceptionForHR` we can use `ThrowExceptionForHR`.
    
    Fix warnings `CS8597:Thrown value may be null.`

--- src/System.Management.Automation/engine/parser/FusionAssemblyIdentity.cs
+++ src/System.Management.Automation/engine/parser/FusionAssemblyIdentity.cs
@@ -133,17 +133,14 @@ namespace Microsoft.CodeAnalysis
 
             if (hr != ERROR_INSUFFICIENT_BUFFER)
             {
-                throw Marshal.GetExceptionForHR(hr);
+                Marshal.ThrowExceptionForHR(hr);
             }
 
             byte[] data = new byte[(int)characterCountIncludingTerminator * 2];
             fixed (byte* p = data)
             {
                 hr = nameObject.GetDisplayName(p, ref characterCountIncludingTerminator, displayFlags);
-                if (hr != 0)
-                {
-                    throw Marshal.GetExceptionForHR(hr);
-                }
+                Marshal.ThrowExceptionForHR(hr);
 
                 return Marshal.PtrToStringUni((IntPtr)p, (int)characterCountIncludingTerminator - 1);
             }
@@ -162,17 +159,14 @@ namespace Microsoft.CodeAnalysis
 
             if (hr != ERROR_INSUFFICIENT_BUFFER)
             {
-                throw Marshal.GetExceptionForHR(hr);
+                Marshal.ThrowExceptionForHR(hr);
             }
 
             byte[] data = new byte[(int)size];
             fixed (byte* p = data)
             {
                 hr = nameObject.GetProperty(propertyId, p, ref size);
-                if (hr != 0)
-                {
-                    throw Marshal.GetExceptionForHR(hr);
-                }
+                Marshal.ThrowExceptionForHR(hr);
             }
 
             return data;
@@ -210,10 +204,7 @@ namespace Microsoft.CodeAnalysis
             uint result;
             uint size = sizeof(uint);
             int hr = nameObject.GetProperty(propertyId, &result, ref size);
-            if (hr != 0)
-            {
-                throw Marshal.GetExceptionForHR(hr);
-            }
+            Marshal.ThrowExceptionForHR(hr);
 
             if (size == 0)
             {
