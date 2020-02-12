1d2eb201454fca5166050c270cf5ef946d7c708f
Paul Higinbotham <paulhi@microsoft.com>
2020-02-12 09:31:06 -0800

    Update src/System.Management.Automation/engine/runtime/ScriptBlockToPowerShell.cs
    Co-Authored-By: Ilya <darpa@yandex.ru>

--- src/System.Management.Automation/engine/runtime/ScriptBlockToPowerShell.cs
+++ src/System.Management.Automation/engine/runtime/ScriptBlockToPowerShell.cs
@@ -320,7 +320,7 @@ namespace System.Management.Automation
         /// to only the provided scriptblock and no nested scriptblocks.
         /// </summary>
         /// <param name = "scriptBlock">Scriptblock to search.</param>
-        /// <param name = "isTrustedInput">True when input is trusted</param>
+        /// <param name = "isTrustedInput">True when input is trusted.</param>
         /// <param name = "context">Execution context</param>
         /// <returns>Dictionary of using variable map</returns>
         internal static Dictionary<string, object> GetUsingValuesForEachParallel(
