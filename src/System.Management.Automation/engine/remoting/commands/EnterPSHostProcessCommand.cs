0e4e594bd62d1984036fccda2dd456c4edb31260
Ilya <darpa@yandex.ru>
2020-02-19 18:15:46 +0500

    Bring back MainWindowTitle in PSHostProcessInfo

--- src/System.Management.Automation/engine/remoting/commands/EnterPSHostProcessCommand.cs
+++ src/System.Management.Automation/engine/remoting/commands/EnterPSHostProcessCommand.cs
@@ -765,7 +765,6 @@ namespace Microsoft.PowerShell.Commands
             private set;
         }
 
-#if !CORECLR
         /// <summary>
         /// Main window title of the process.
         /// </summary>
@@ -774,7 +773,6 @@ namespace Microsoft.PowerShell.Commands
             get;
             private set;
         }
-#endif
 
         #endregion
 
@@ -794,7 +792,6 @@ namespace Microsoft.PowerShell.Commands
 
             if (string.IsNullOrEmpty(appDomainName)) { throw new PSArgumentNullException("appDomainName"); }
 
-#if !CORECLR
             MainWindowTitle = string.Empty;
             try
             {
@@ -803,7 +800,6 @@ namespace Microsoft.PowerShell.Commands
             }
             catch (ArgumentException) { }
             catch (InvalidOperationException) { }
-#endif
 
             this.ProcessName = processName;
             this.ProcessId = processId;
