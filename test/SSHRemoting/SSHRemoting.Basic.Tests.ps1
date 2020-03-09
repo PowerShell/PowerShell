b6faa0ec82a72fb226a4a34bee3778e65544cbe8
Paul Higinbotham <paulhi@microsoft.com>
2020-03-09 15:23:44 -0700

    Add more New-PSSession tests

--- test/SSHRemoting/SSHRemoting.Basic.Tests.ps1
+++ test/SSHRemoting/SSHRemoting.Basic.Tests.ps1
@@ -3,16 +3,99 @@
 
 Describe "SSHRemoting Basic Tests" -tags CI {
 
+    # SSH remoting is set up to automatically authenticate current user via SSH keys
+    # All tests connect back to same localhost machine
+
+    function VerifySession {
+        param (
+            $session
+        )
+
+        $session.State | Should -BeExactly 'Opened'
+        $session.ComputerName | Should -BeExactly 'localhost'
+        $session.Transport | Should -BeExactly 'SSH'
+        Invoke-Command -Session $session -ScriptBlock { $env:USER } | Should -BeExactly $env:USER
+    }
+
     Context "New-PSSession Tests" {
 
-        It "Verifies New-PSSession can create a localhost connection" {
-            # Create loopback connection to localhost with current user context.
-            # Authentication is performed via SSH keys for current user.
-            $session = New-PSSession -HostName localhost -ErrorVariable err
+        AfterEach {
+            if ($script:session -ne $null) { Remove-PSSession -session $script:session }
+            if ($script:sessions -ne $null) { Remove-PSSession -session $script:sessions }
+        }
+
+        It "Verifies new connection with implicit current User" {
+            $script:session = New-PSSession -HostName localhost -ErrorVariable err
             $err | Should -HaveCount 0
-            $session.State | Should -BeExactly 'Opened'
-            $session.ComputerName | Should -BeExactly 'localhost'
-            Remove-PSSession -Session $session
+            VerifySession $script:session
+        }
+
+        It "Verifies new connection with explicit User parameter" {
+            $script:session = New-PSSession -HostName localhost -UserName ($env:USER) -ErrorVariable err
+            $err | Should -HaveCount 0
+            VerifySession $script:session
+        }
+
+        It "Verifies explicit Name parameter" {
+            $sessionName = 'TestSessionNameA'
+            $script:session = New-PSSession -HostName localhost -Name $sessionName -ErrorVariable err
+            $err | Should -HaveCount 0
+            VerifySession $script:session
+            $script:session.Name | Should -BeExactly $sessionName
+        }
+
+        It "Verifies explicit Port parameter" {
+            $portNum = 22
+            $script:session = New-PSSession -HostName localhost -Port $portNum -ErrorVariable err
+            $err | Should -HaveCount 0
+            VerifySession $script:session
+        }
+
+        It "Verifies explicit Subsystem parameter" {
+            $portNum = 22
+            $subSystem = 'powershell'
+            $script:session = New-PSSession -HostName localhost -Port $portNum -SubSystem $subSystem -ErrorVariable err
+            $err | Should -HaveCount 0
+            VerifySession $script:session
+        }
+
+        It "Verifies explicit KeyFilePath parameter" {
+            $keyFilePath = "$HOME/.ssh/id_rsa"
+            $portNum = 22
+            $subSystem = 'powershell'
+            $script:session = New-PSSession -HostName localhost -Port $portNum -SubSystem $subSystem -KeyFilePath $keyFilePath -ErrorVariable err
+            $err | Should -HaveCount 0
+            VerifySession $script:session
+        }
+
+        It "Verifies SSHConnection hash table parameters" {
+            $sshConnection = @(
+            @{
+                HostName = 'localhost'
+                UserName = $env:USER
+                Port = 22
+                KeyFilePath = "$HOME/.ssh/id_rsa"
+                Subsystem = 'powershell'
+            },
+            @{
+                HostName = 'localhost'
+                KeyFilePath = "$HOME/.ssh/id_rsa"
+                Subsystem = 'powershell'
+            })
+            $script:sessions = New-PSSession -SSHConnection $sshConnection -Name 'Connection1','Connection2' -ErrorVariable err
+            $err | Should -HaveCount 0
+            $script:sessions | Should -HaveCount 2
+            $script:sessions[0].Name | Should -BeLike 'Connection*'
+            $script:sessions[1].Name | Should -BeLike 'Connection*'
+            VerifySession $script:sessions[0]
+            VerifySession $script:sessions[1]
         }
     }
+
+    <#
+    Context "SSH Remoting API Tests" {
+
+
+    }
+    #>
 }
