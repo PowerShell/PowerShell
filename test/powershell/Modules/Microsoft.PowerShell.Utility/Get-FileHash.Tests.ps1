e459953d1731156fbd3a18b0febe117cf310f5a4
Ilya <darpa@yandex.ru>
2020-03-13 14:35:03 +0500

    Fix test 3

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Get-FileHash.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Get-FileHash.Tests.ps1
@@ -30,7 +30,8 @@ Describe "Get-FileHash" -Tags "CI" {
         }
 
         It "Should write non-terminating error if a file is locked" -Skip:(-not $IsWindows) {
-            $result = "$env:SystemDrive\\swapfile.sys", "${pshome}\pwsh.dll" | Get-FileHash -ErrorVariable errorVariable
+            $pagefilePath = (Get-CimInstance -ClassName Win32_PageFileusage).Name
+            $result = $pagefilePath, "${pshome}\pwsh.dll" | Get-FileHash -ErrorVariable errorVariable
             $result.Count | Should -Be 1
             $errorVariable.FullyQualifiedErrorId | Should -BeExactly "FileReadError,Microsoft.PowerShell.Commands.GetFileHashCommand"
         }
