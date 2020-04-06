ae615d05b7465f481a7f19e3babd9b01e0a2751c
Christoph Bergmeister [MVP] <c.bergmeister@gmail.com>
2020-04-06 09:24:16 +0100

    Update build.psm1

--- build.psm1
+++ build.psm1
@@ -689,7 +689,7 @@ function Restore-PSPester
         [ValidateNotNullOrEmpty()]
         [string] $Destination = ([IO.Path]::Combine((Split-Path (Get-PSOptions -DefaultToNew).Output), "Modules"))
     )
-    Save-Module -Name Pester -Path $Destination -Repository PSGallery -RequiredVersion '4.10.1'
+    Save-Module -Name Pester -Path $Destination -Repository PSGallery -MaximumVersion 4.99
 }
 
 function Compress-TestContent {
