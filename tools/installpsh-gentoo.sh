b7c66ab3ef584956a2872856ee8b351784f522d9
Rafael Kitover <rkitover@gmail.com>
2020-04-15 19:42:10 +0000

    Linux: Initial support for Gentoo installations. (#11429)

--- tools/installpsh-gentoo.sh
+++ tools/installpsh-gentoo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#Companion code for the blog https://cloudywindows.com
 #call this code direction from the web with:
-#bash <(wget -O - https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/installpsh-debian.sh) ARGUMENTS
-#bash <(curl -s https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/installpsh-debian.sh) <ARGUMENTS>
+#bash <(wget -O - https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/installpsh-gentoo.sh) ARGUMENTS
+#bash <(curl -s https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/installpsh-gentoo.sh) <ARGUMENTS>
 
 #Usage - if you do not have the ability to run scripts directly from the web,
 #        pull all files in this repo folder and execute, this script
@@ -12,8 +11,6 @@
 #Completely automated install requires a root account or sudo with a password requirement
 
 #Switches
-# -includeide         - installs VSCode and VSCode PowerShell extension (only relevant to machines with desktop environment)
-# -interactivetesting - do a quick launch test of VSCode (only relevant when used with -includeide)
 # -skip-sudo-check    - use sudo without verifying its availability (hard to accurately do on some distros)
 # -preview            - installs the latest preview release of PowerShell side-by-side with any existing production releases
 
@@ -22,9 +19,8 @@
 VERSION="1.2.0"
 gitreposubpath="PowerShell/PowerShell/master"
 gitreposcriptroot="https://raw.githubusercontent.com/$gitreposubpath/tools"
-thisinstallerdistro=debian
-repobased=true
-gitscriptname="installpsh-debian.psh"
+thisinstallerdistro=gentoo
+gitscriptname="installpsh-gentoo.psh"
 powershellpackageid=powershell
 
 echo ;
@@ -75,6 +71,8 @@ else
             DistroBasedOn='mandrake'
         elif [ -f /etc/debian_version ] ; then
             DistroBasedOn='debian'
+        elif [ "$(lsb_release --id 2>/dev/null | sed -E 's/^.*:[[:space:]]*//')" = Gentoo ] ; then
+            DistroBasedOn='gentoo'
         fi
         if [ -f /etc/UnitedLinux-release ] ; then
             DIST="${DIST}[$( (tr "\n" ' ' | sed s/VERSION.*//) < /etc/UnitedLinux-release )]"
@@ -111,13 +109,6 @@ fi
 
 #Collect any variation details if required for this distro
 # shellcheck disable=SC1091
-if [[ -f /etc/lsb-release ]]; then
-    . /etc/lsb-release
-    DISTRIB_ID=$(lowercase "$DISTRIB_ID")
-elif [[ -f /etc/debian_version ]]; then
-    DISTRIB_ID="debian"
-    DISTRIB_RELEASE=$(cat /etc/debian_version)
-fi
 #END Collect any variation details if required for this distro
 
 #If there are known incompatible versions of this distro, put the test, message and script exit here:
@@ -130,7 +121,12 @@ echo
 echo "*** Installing PowerShell for $DistroBasedOn..."
 if ! hash curl 2>/dev/null; then
     echo "curl not found, installing..."
-    $SUDO apt-get install -y curl
+    $SUDO emerge -nv1 net-misc/curl
+fi
+
+if ! hash dpkg 2>/dev/null; then
+    echo "dpkg not found, installing..."
+    $SUDO emerge -nv1 app-arch/dpkg
 fi
 
 # The executable to test.
@@ -143,75 +139,34 @@ if [[ "'$*'" =~ preview ]] ; then
     PWSH=pwsh-preview
 fi
 
-currentversion=$(curl https://api.github.com/repos/powershell/powershell/releases/latest | sed '/tag_name/!d' | sed s/\"tag_name\"://g | sed s/\"//g | sed s/v// | sed s/,//g | sed s/\ //g)
+currentversion=$(curl -s https://api.github.com/repos/powershell/powershell/releases/latest | sed '/tag_name/!d' | sed s/\"tag_name\"://g | sed s/\"//g | sed s/v// | sed s/,//g | sed s/\ //g)
 
-echo "*** Current version on git is: $currentversion, repo version may differ slightly..."
-echo "*** Setting up PowerShell repo..."
-# Import the public repository GPG keys
-curl https://packages.microsoft.com/keys/microsoft.asc | $SUDO apt-key add -
-#Add the Repo
-if [[ "${DISTRIB_ID}" = "linuxmint" ]]; then
-    echo "Attempting to remap linuxmint to an appropriate ubuntu version" >&2
-    LINUXMINT_VERSION=${DISTRIB_RELEASE}
-    #https://en.wikipedia.org/wiki/Linux_Mint_version_history
-    case ${LINUXMINT_VERSION} in
-        19*)
-            DISTRIB_RELEASE=18.04
-        ;;
-        18*)
-            DISTRIB_RELEASE=16.04
-        ;;
-        17*)
-            DISTRIB_RELEASE=14.04
-        ;;
-        *)
-            echo "ERROR: unsupported linuxmint version (${LINUXMINT_VERSION})." >&2
-            echo "Supported versions: 19" >&2
-            echo "For additional versions open an issue or pull request at: https://github.com/powershell/powershell" >&2
-            exit 1
-        ;;
-    esac
-    echo "Remapping linuxmint version ${LINUXMINT_VERSION} to ubuntu version ${DISTRIB_RELEASE}" >&2
+printf "\n*** Current version on git is: $currentversion, repo version may differ slightly...\n\n"
+
+ubuntu_dist=18.04
+
+# Find latest ubuntu packages.
+
+for ubuntu_dist in $(curl -sL 'https://packages.microsoft.com/ubuntu/' | sed -En 's,.*href="([[:digit:]][[:digit:].]+).*,\1,p' | sort -rV); do
+    if ! curl -sL "https://packages.microsoft.com/ubuntu/${ubuntu_dist}/prod/pool/main/p/${powershellpackageid}/" | grep -q '404 Not Found'; then
+        break
+    fi
+done
+
+printf "*** Found packages for Ubuntu $ubuntu_dist...\n\n"
+
+latest_pkg=$(curl -sL "https://packages.microsoft.com/ubuntu/${ubuntu_dist}/prod/pool/main/p/${powershellpackageid}/" | sed -En 's/^.*href="([^"]+\.deb).*/\1/p' | sort -V | tail -1)
+
+if [ ! -f "$latest_pkg" ]; then
+    curl -sL "https://packages.microsoft.com/ubuntu/${ubuntu_dist}/prod/pool/main/p/${powershellpackageid}/${latest_pkg}" -o "$latest_pkg"
 fi
-case $DISTRIB_ID in
-    ubuntu|linuxmint)
-        case $DISTRIB_RELEASE in
-            18.04|16.10|16.04|15.10|14.04)
-                curl https://packages.microsoft.com/config/ubuntu/$DISTRIB_RELEASE/prod.list | $SUDO tee /etc/apt/sources.list.d/microsoft.list
-            ;;
-            *)
-                echo "ERROR: unsupported Ubuntu version ($DISTRIB_RELEASE)." >&2
-                echo "Supported versions: 14.04, 15.10, 16.04, 16.10, 18.04." >&2
-                echo "For additional versions open an issue or pull request at: https://github.com/powershell/powershell" >&2
-                exit 1
-            ;;
-        esac
-    ;;
-    debian)
-        DISTRIB_RELEASE=${DISTRIB_RELEASE%%.*}
-        case $DISTRIB_RELEASE in
-            8|9|10|11)
-                curl https://packages.microsoft.com/config/debian/$DISTRIB_RELEASE/prod.list | $SUDO tee /etc/apt/sources.list.d/microsoft.list
-            ;;
-            *)
-                echo "ERROR: unsupported Debian version ($DISTRIB_RELEASE)." >&2
-                echo "Supported versions: 8, 9." >&2
-                echo "For additional versions open an issue or pull request at: https://github.com/powershell/powershell" >&2
-                exit 1
-            ;;
-        esac
-    ;;
-    *)
-        echo "ERROR: unsupported Debian-based distribution ($DISTRIB_ID)." >&2
-        echo "Supported distributions: Debian, Ubuntu." >&2
-        exit 1
-    ;;
-esac
 
-# Update apt-get
-$SUDO apt-get update
-# Install PowerShell
-$SUDO apt-get install -y ${powershellpackageid}
+$SUDO dpkg -i --force-depends "$latest_pkg" 2>/dev/null
+
+# the postrm breaks removal
+$SUDO rm -f /var/lib/dpkg/info/${powershellpackageid}.postrm
+
+printf "\n\n"
 
 # shellcheck disable=SC2016
 $PWSH -noprofile -c '"Congratulations! PowerShell is installed at $PSHOME.
@@ -227,25 +182,39 @@ fi
 if [[ "'$*'" =~ includeide ]] ; then
     echo
     echo "*** Installing VS Code PowerShell IDE..."
-    echo "*** Setting up VS Code repo..."
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    $SUDO mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-    $SUDO sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    $SUDO apt-get update
-    $SUDO apt-get install -y code
+
+    # install overlay for flatpak and flatpak if needed
+    if ! command -v flatpak >/dev/null; then
+      echo "*** Setting up Flatpak for VS Code..."
+
+      if ! ( ( command -v layman >/dev/null && layman -l | grep -q flatpak-overlay ) || [ -f /etc/portage/repos.conf/flatpak-overlay.conf ] ); then
+        $SUDO sh -c 'cat >/etc/portage/repos.conf/flatpak-overlay.conf' <<EOF
+[flatpak-overlay]
+priority = 50
+location = /usr/local/portage/flatpak-overlay
+sync-type = git
+sync-uri = https://github.com/fosero/flatpak-overlay.git
+auto-sync = Yes
+EOF
+        $SUDO emerge --sync
+      fi
+
+      $SUDO emerge -nv1 sys-apps/flatpak
+    fi
+
+    $SUDO flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+    $SUDO flatpak install -y flathub com.visualstudio.code
 
     echo
     echo "*** Installing VS Code PowerShell Extension"
-    code --install-extension ms-vscode.PowerShell
+    flatpak run com.visualstudio.code --install-extension ms-vscode.PowerShell
     if [[ "'$*'" =~ -interactivetesting ]] ; then
         echo "*** Loading test code in VS Code"
         curl -O ./testpowershell.ps1 https://raw.githubusercontent.com/DarwinJS/CloudyWindowsAutomationCode/master/pshcoredevenv/testpowershell.ps1
-        code ./testpowershell.ps1
+        flatpak run com.visualstudio.code ./testpowershell.ps1
     fi
 fi
 
-if [[ "$repobased" == true ]] ; then
-  echo
-  echo "*** NOTE: Run your regular package manager update cycle to update PowerShell"
-fi
+printf "\n*** NOTE: Run the newest version of this script again to update PowerShell\n\n"
 echo "*** Install Complete"
