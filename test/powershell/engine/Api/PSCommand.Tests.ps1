92f35ae30b414ed944345b829fd2aff2cf6c9d3e
Dongbo Wang <dongbow@microsoft.com>
2020-04-20 11:49:28 -0700

    Add a new line after the header

--- test/powershell/engine/Api/PSCommand.Tests.ps1
+++ test/powershell/engine/Api/PSCommand.Tests.ps1
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
 Describe "PSCommand API tests" -Tag "CI" {
     BeforeAll {
         $shell = [PowerShell]::Create()
