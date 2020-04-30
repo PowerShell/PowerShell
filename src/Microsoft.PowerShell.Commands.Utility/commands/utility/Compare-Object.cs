6d1d62f2f83b526997d23e0606be9d5708246e9f
David Seibel <933503+davidseibel@users.noreply.github.com>
2020-04-30 16:23:55 -0400

    Compare-Object Apply -IncludeEqual when -ExcludeDifferent is specified (#12317)

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/Compare-Object.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/Compare-Object.cs
@@ -372,18 +372,13 @@ namespace Microsoft.PowerShell.Commands
         #region Overrides
 
         /// <summary>
-        /// If the parameter 'ExcludeDifferent' is present, then we need to turn on the
-        /// 'IncludeEqual' switch unless it's turned off by the user specifically.
+        /// If the parameter 'ExcludeDifferent' is present, then the 'IncludeEqual'
+        /// switch is turned on unless it's turned off by the user specifically.
         /// </summary>
         protected override void BeginProcessing()
         {
             if (ExcludeDifferent)
             {
-                if (_isIncludeEqualSpecified == false)
-                {
-                    return;
-                }
-
                 if (_isIncludeEqualSpecified && !_includeEqual)
                 {
                     return;
