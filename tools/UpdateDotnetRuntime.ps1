a543b304bc2deca850f032f9834e1649fb3d650f
Aditya Patwardhan <adityap@microsoft.com>
2020-04-30 11:41:33 -0700

    Add the .NET SDK installation path to the current process path (#12525)

--- tools/UpdateDotnetRuntime.ps1
+++ tools/UpdateDotnetRuntime.ps1
@@ -13,6 +13,11 @@ param (
 function Update-GlobalJson([string] $Version) {
     $psGlobalJsonPath = Resolve-Path "$PSScriptRoot/../global.json"
     $psGlobalJson = Get-Content -Path $psGlobalJsonPath -Raw | ConvertFrom-Json
+
+    if ($psGlobalJson.sdk.version -eq $Version) {
+        throw '.NET SDK version is not updated'
+    }
+
     $psGlobalJson.sdk.version = $Version
     $psGlobalJson | ConvertTo-Json | Out-File -FilePath $psGlobalJsonPath -Force
 }
@@ -118,7 +123,7 @@ function Update-CsprojFile([string] $path, $values) {
     }
 
     if ($updated) {
-        $fileContent | Out-File -FilePath $path -Force
+        ($fileContent).TrimEnd() | Out-File -FilePath $path -Force
     }
 }
 
@@ -146,6 +151,16 @@ $sdkVersion = if ($SDKVersionOverride) { $SDKVersionOverride } else { "latest" }
 Install-Dotnet -Channel "$Channel" -Version $sdkVersion
 
 Write-Verbose -Message "Installing .NET SDK completed." -Verbose
+
+$isWindowsEnv = [System.Environment]::OSVersion.Platform -eq [System.PlatformID]::Win32NT
+
+$dotnetPath = if ($IsWindowsEnv) { "$env:LocalAppData\Microsoft\dotnet" } else { "$env:HOME/.dotnet" }
+
+$pathSep = [System.IO.Path]::PathSeparator
+
+if (-not (($ENV:PATH -split $pathSep) -contains "$dotnetPath")) {
+    $env:PATH = "$dotnetPath" + $pathSep + "$ENV:PATH"
+}
 
 $latestSdkVersion = (dotnet --list-sdks | Select-Object -Last 1 ).Split() | Select-Object -First 1
 
