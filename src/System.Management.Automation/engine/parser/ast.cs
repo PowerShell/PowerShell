b9703a4b3d85239e53105a1e4f3a919040725030
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2020-05-16 00:03:36 -0400

    :art: Linter fixes

--- src/System.Management.Automation/engine/parser/ast.cs
+++ src/System.Management.Automation/engine/parser/ast.cs
@@ -2038,7 +2038,6 @@ namespace System.Management.Automation.Language
                     || blockName == TokenKind.Dynamicparam));
         }
 
-
         // Used by the debugger for command breakpoints
         internal IScriptExtent OpenCurlyExtent { get; }
 
