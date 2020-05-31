8fbb20bdbfb5142c5078e7c24247d9116e3876ef
xtqqczze <xtqqczze@gmail.com>
2020-05-31 19:23:17 +0100

    Genericise `hasUtf8Bom` implementation

--- test/common/FileEncoding/FileEncoding.Tests.ps1
+++ test/common/FileEncoding/FileEncoding.Tests.ps1
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+using namespace System
+
 Describe 'Verify file encoding' {
     BeforeAll {
         Push-Location $PSScriptRoot\..\..\..
@@ -14,11 +16,12 @@ Describe 'Verify file encoding' {
     It 'No UTF-8 BOM' {
         filter hasUtf8Bom {
             $_.Where{
-                $bytes = [byte[]]::new(3)
-                $stream = [System.IO.File]::OpenRead($_)
-                $stream.Read($bytes, 0, 3) > $null
+                $bom = [Text.UnicodeEncoding]::UTF8.GetPreamble()
+                $bytes = [byte[]]::new($bom.Length)
+                $stream = [IO.File]::OpenRead($_)
+                $stream.Read($bytes, 0, $bytes.Length) > $null
                 $stream.Close()
-                $bytes[0] -eq 0xef -and $bytes[1] -eq 0xbb -and $bytes[2] -eq 0xbf
+                [Linq.Enumerable]::SequenceEqual($bom, $bytes)
             }
         }
 
