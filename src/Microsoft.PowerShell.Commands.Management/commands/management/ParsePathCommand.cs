9ab19079a4b9c294bb373df9e2e04c4f69b114f0
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-06-14 20:56:50 +0200

    QUALIFIER for SPLIT-PATH should not be positional
    The syntax ``SPLIT-PATH C: $NULL`` is confusing and should not be supported.

--- src/Microsoft.PowerShell.Commands.Management/commands/management/ParsePathCommand.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/ParsePathCommand.cs
@@ -107,7 +107,7 @@ namespace Microsoft.PowerShell.Commands
         /// The qualifier is the drive or provider that is qualifying
         /// the MSH path.
         /// </value>
-        [Parameter(Position = 1, ValueFromPipelineByPropertyName = true, ParameterSetName = qualifierSet, Mandatory = false)]
+        [Parameter(ValueFromPipelineByPropertyName = true, ParameterSetName = qualifierSet, Mandatory = false)]
         public SwitchParameter Qualifier { get; set; }
 
         /// <summary>
