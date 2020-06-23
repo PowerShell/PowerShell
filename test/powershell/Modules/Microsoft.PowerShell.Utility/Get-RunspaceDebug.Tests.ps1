7b4e80f07d0f643fed92de7033fb9b17ca49b4ff
Paul Higinbotham <paulhi@microsoft.com>
2020-06-23 14:07:52 -0700

    Fix tests to not write errors to console (#13010)

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Get-RunspaceDebug.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Get-RunspaceDebug.Tests.ps1
@@ -4,10 +4,10 @@ Describe "Get-RunspaceDebug" -Tags "CI" {
 
     Context "Check return types of RunspaceDebug" {
 
-    It "Should return Microsoft.Powershell.Commands.PSRunspaceDebug as the return type" {
-        $rs = Get-RunspaceDebug
-        $rs | Should -Not -BeNullOrEmpty
-        $rs[0] | Should -BeOfType Microsoft.PowerShell.Commands.PSRunspaceDebug
-    }
+        It "Should return Microsoft.Powershell.Commands.PSRunspaceDebug as the return type" {
+            $rs = Get-RunspaceDebug -ErrorAction SilentlyContinue
+            $rs | Should -Not -BeNullOrEmpty
+            $rs[0] | Should -BeOfType Microsoft.PowerShell.Commands.PSRunspaceDebug
+        }
     }
 }
