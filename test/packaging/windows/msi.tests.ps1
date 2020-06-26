4d39e6b8c47ccad3210b368707cdb3328b2a1f29
Travis Plunk <travis.plunk@microsoft.com>
2020-06-25 17:09:23 -0700

    Update msi.tests.ps1

--- test/packaging/windows/msi.tests.ps1
+++ test/packaging/windows/msi.tests.ps1
@@ -85,8 +85,8 @@ Describe -Name "Windows MSI" -Fixture {
             $previewUpgladeCode = '39243d76-adaf-42b1-94fb-16ecf83237c8'
         }
         It "Preview MSI should not be installed before test" -Skip:(!(Test-Elevated)) {
-            $result = Get-CimInstance -Query "SELECT Value FROM Win32_Property WHERE Property='UpgradeCode' and Value = '{$previewUpgladeCode}'"
-            $result.Value | Should -BeNullOrEmpty -Because 'Query should return nothing if preview x64 is not installed'
+            $result = @(Get-CimInstance -Query "SELECT Value FROM Win32_Property WHERE Property='UpgradeCode' and Value = '{$previewUpgladeCode}'")
+            $result.Count | Should -Be 0 -Because 'Query should return nothing if preview x64 is not installed'
         }
 
         It "MSI should install without error" -Skip:(!(Test-Elevated)) {
