ce2f447f5d17aabccbb455151d949fe8dd62efcd
Tyler James Leonhardt <tylerl0706@gmail.com>
2020-06-30 19:36:33 -0700

    forgot -Verbose

--- tools/install-powershell.ps1
+++ tools/install-powershell.ps1
@@ -434,7 +434,7 @@ try {
         Write-Verbose "Change icon to disambiguate it from a released installation" -Verbose
         try {
             $rceditArgs = @("$Destination\pwsh.exe", "--set-icon", "$Destination\assets\Powershell_avatar.ico")
-            Write-Verbose "rcedit args: $rceditArgs"
+            Write-Verbose "rcedit args: $rceditArgs" -Verbose
             & $rceditPath $rceditArgs
         } catch {
             Write-Warning "rcedit-x64.exe exited with non-zero exit code. The icon may not have been updated."
