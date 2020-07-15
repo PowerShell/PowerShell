4f0ec3717b19f1592158362ca712253bbe206ffb
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-07-15 11:45:18 +0200

    Remove hyperlink for DefaultPromptFunctionText
    The hyperlink was invalid.  Fix #7795.

--- src/System.Management.Automation/engine/InitialSessionState.cs
+++ src/System.Management.Automation/engine/InitialSessionState.cs
@@ -4765,8 +4765,6 @@ end {
 
         internal const string DefaultPromptFunctionText = @"
 ""PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "";
-# .Link
-# https://go.microsoft.com/fwlink/?LinkID=225750
 # .ExternalHelp System.Management.Automation.dll-help.xml
 ";
 
