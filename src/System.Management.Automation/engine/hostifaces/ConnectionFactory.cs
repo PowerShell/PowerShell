bd1f4c44136cf75626da5902e5f02589c83132d1
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2020-07-31 10:47:56 +0100

    Apply suggestions from code review
    Co-authored-by: Joel Sallow (/u/ta11ow) <32407840+vexx32@users.noreply.github.com>

--- src/System.Management.Automation/engine/hostifaces/ConnectionFactory.cs
+++ src/System.Management.Automation/engine/hostifaces/ConnectionFactory.cs
@@ -454,11 +454,11 @@ namespace System.Management.Automation.Runspaces
         public static RunspacePool CreateRunspacePool(int minRunspaces,
             int maxRunspaces, RunspaceConnectionInfo connectionInfo, PSHost host, TypeTable typeTable, PSPrimitiveDictionary applicationArguments)
         {
-            if ((connectionInfo is not WSManConnectionInfo) &&
-                (connectionInfo is not NewProcessConnectionInfo) &&
-                (connectionInfo is not NamedPipeConnectionInfo) &&
-                (connectionInfo is not VMConnectionInfo) &&
-                (connectionInfo is not ContainerConnectionInfo))
+            if (connectionInfo is not WSManConnectionInfo
+                && connectionInfo is not NewProcessConnectionInfo
+                && connectionInfo is not NamedPipeConnectionInfo
+                && connectionInfo is not VMConnectionInfo
+                && connectionInfo is not ContainerConnectionInfo)
             {
                 throw new NotSupportedException();
             }
@@ -539,12 +539,12 @@ namespace System.Management.Automation.Runspaces
         /// <returns></returns>
         public static Runspace CreateRunspace(RunspaceConnectionInfo connectionInfo, PSHost host, TypeTable typeTable, PSPrimitiveDictionary applicationArguments, string name)
         {
-            if ((connectionInfo is not WSManConnectionInfo) &&
-                (connectionInfo is not NewProcessConnectionInfo) &&
-                (connectionInfo is not NamedPipeConnectionInfo) &&
-                (connectionInfo is not SSHConnectionInfo) &&
-                (connectionInfo is not VMConnectionInfo) &&
-                (connectionInfo is not ContainerConnectionInfo))
+            if (connectionInfo is not WSManConnectionInfo
+                && connectionInfo is not NewProcessConnectionInfo
+                && connectionInfo is not NamedPipeConnectionInfo
+                && connectionInfo is not SSHConnectionInfo
+                && connectionInfo is not VMConnectionInfo
+                && connectionInfo is not ContainerConnectionInfo)
             {
                 throw new NotSupportedException();
             }
@@ -606,4 +606,3 @@ namespace System.Management.Automation.Runspaces
         #endregion V3 Extensions
     }
 }
-
