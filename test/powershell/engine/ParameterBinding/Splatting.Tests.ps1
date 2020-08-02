8adc77ec179c15a7e3bf7f90a5e73025dee6a70e
Dongbo Wang <dongbow@microsoft.com>
2020-08-02 12:05:23 -0700

    Add more tests for steppable pipeline

--- test/powershell/engine/ParameterBinding/Splatting.Tests.ps1
+++ test/powershell/engine/ParameterBinding/Splatting.Tests.ps1
@@ -50,16 +50,36 @@ Describe "Hashtable Splatting Parameter Binding Tests" -Tags "CI" {
                 $ps.Dispose()
             }
         }
+
+        It "works on steppable pipeline" {
+            $hash = @{ Verb = "Get"; OutVariable = "zoo" }
+            $sp = { Get-Verb @hash }.GetSteppablePipeline()
+            try {
+                $sp.Begin($false)
+
+                $result = $sp.Process()
+                $result | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $result.Verb | Should -BeExactly 'Get'
+                $zoo | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $zoo.Verb | Should -BeExactly 'Get'
+
+                $sp.End()
+            } finally {
+                $sp.Dispose()
+            }
+        }
     }
 
     Context "Explicitly specified named parameter supersedes the same one in Hashtable splatting" {
 
         It "works with the same parameter name" {
+            ## Regular use with cmdlet
             $hash = @{ Verb = "Get"; OutVariable = "zoo" }
             Get-Verb @hash -Verb "Send" > $null
             $zoo | Should -BeOfType "System.Management.Automation.VerbInfo"
             $zoo.Verb | Should -BeExactly "Send"
 
+            ## GetPowerShell
             $ps = { param($hash) Get-Verb @hash -Verb "Send"; Get-Variable zoo }.GetPowerShell($hash)
             try {
                 $result = $ps.Invoke()
@@ -69,6 +89,24 @@ Describe "Hashtable Splatting Parameter Binding Tests" -Tags "CI" {
                 $ps.Dispose()
             }
 
+            ## Steppable pipeline
+            $zoo = $null
+            $sp = { Get-Verb @hash -Verb "Send" }.GetSteppablePipeline()
+            try {
+                $sp.Begin($false)
+
+                $result = $sp.Process()
+                $result | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $result.Verb | Should -BeExactly 'Send'
+                $zoo | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $zoo.Verb | Should -BeExactly 'Send'
+
+                $sp.End()
+            } finally {
+                $sp.Dispose()
+            }
+
+            ## Regular use with simple function
             $hash = @{ Name = "Hello"; Path = "World" }
             SimpleTest @hash -Path "Yeah" | Should -BeExactly 'Key: Hello; Path: Yeah; Args: '
 
@@ -77,12 +115,14 @@ Describe "Hashtable Splatting Parameter Binding Tests" -Tags "CI" {
         }
 
         It "works with the same alias name" {
+            ## Regular use with cmdlet
             $hash = @{ Verb = "Get"; ov = "zoo" }
             Get-Verb @hash -Verb "Send" -ov "bar" > $null
             $zoo | Should -BeNullOrEmpty
             $bar | Should -BeOfType "System.Management.Automation.VerbInfo"
             $bar.Verb | Should -BeExactly "Send"
 
+            ## GetPowerShell
             $ps = { param($hash) Get-Verb @hash -Verb "Send" -ov "bar"; Get-Variable bar }.GetPowerShell($hash)
             try {
                 $result = $ps.Invoke()
@@ -92,17 +132,38 @@ Describe "Hashtable Splatting Parameter Binding Tests" -Tags "CI" {
                 $ps.Dispose()
             }
 
+            ## Steppable pipeline
+            $bar = $null
+            $sp = { Get-Verb @hash -Verb "Send" -ov "bar" }.GetSteppablePipeline()
+            try {
+                $sp.Begin($false)
+
+                $result = $sp.Process()
+                $result | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $result.Verb | Should -BeExactly 'Send'
+                $zoo | Should -BeNullOrEmpty
+                $bar | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $bar.Verb | Should -BeExactly 'Send'
+
+                $sp.End()
+            } finally {
+                $sp.Dispose()
+            }
+
+            ## Regular use with simple function
             $hash = @{ key = "Hello"; Path = "World" }
             SimpleTest @hash -Key "Yeah" | Should -BeExactly 'Key: Yeah; Path: World; Args: '
         }
 
         It "works with parameter name and alias name mixed" {
+            ## Regular use with cmdlet
             $hash = @{ Verb = "Get"; OutVariable = "zoo" }
             Get-Verb @hash -Verb "Send" -ov "bar" > $null
             $zoo | Should -BeNullOrEmpty
             $bar | Should -BeOfType "System.Management.Automation.VerbInfo"
             $bar.Verb | Should -BeExactly "Send"
 
+            ## GetPowerShell
             $ps = { param($hash) Get-Verb @hash -Verb "Send" -ov "bar"; Get-Variable bar }.GetPowerShell($hash)
             try {
                 $result = $ps.Invoke()
@@ -112,17 +173,38 @@ Describe "Hashtable Splatting Parameter Binding Tests" -Tags "CI" {
                 $ps.Dispose()
             }
 
+            ## Steppable pipeline
+            $bar = $null
+            $sp = { Get-Verb @hash -Verb "Send" -ov "bar" }.GetSteppablePipeline()
+            try {
+                $sp.Begin($false)
+
+                $result = $sp.Process()
+                $result | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $result.Verb | Should -BeExactly 'Send'
+                $zoo | Should -BeNullOrEmpty
+                $bar | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $bar.Verb | Should -BeExactly 'Send'
+
+                $sp.End()
+            } finally {
+                $sp.Dispose()
+            }
+
+            ## Regular use with simple function
             $hash = @{ Name = "Hello"; Path = "World" }
             SimpleTest @hash -Key "Yeah" | Should -BeExactly 'Key: Yeah; Path: World; Args: '
         }
 
         It "works with unambiguous prefix and parameter name mixed - prefix explicitly specified" {
+            ## Regular use with cmdlet
             $hash = @{ Verb = "Get"; OutVariable = "zoo" }
             Get-Verb @hash -Verb "Send" -outv "bar" > $null
             $zoo | Should -BeNullOrEmpty
             $bar | Should -BeOfType "System.Management.Automation.VerbInfo"
             $bar.Verb | Should -BeExactly "Send"
 
+            ## GetPowerShell
             $ps = { param($hash) Get-Verb @hash -Verb "Send" -outv "bar"; Get-Variable bar }.GetPowerShell($hash)
             try {
                 $result = $ps.Invoke()
@@ -132,17 +214,38 @@ Describe "Hashtable Splatting Parameter Binding Tests" -Tags "CI" {
                 $ps.Dispose()
             }
 
+            ## Steppable pipeline
+            $bar = $null
+            $sp = { Get-Verb @hash -Verb "Send" -outv "bar" }.GetSteppablePipeline()
+            try {
+                $sp.Begin($false)
+
+                $result = $sp.Process()
+                $result | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $result.Verb | Should -BeExactly 'Send'
+                $zoo | Should -BeNullOrEmpty
+                $bar | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $bar.Verb | Should -BeExactly 'Send'
+
+                $sp.End()
+            } finally {
+                $sp.Dispose()
+            }
+
+            ## Regular use with simple function
             $hash = @{ Name = "Hello"; Path = "World" }
             SimpleTest @hash -n "Yeah" | Should -BeExactly 'Key: Yeah; Path: World; Args: '
         }
 
         It "works with unambiguous prefix and parameter name mixed - prefix in splatting hashtable" {
+            ## Regular use with cmdlet
             $hash = @{ Verb = "Get"; outv = "zoo" }
             Get-Verb @hash -Verb "Send" -OutVariable "bar" > $null
             $zoo | Should -BeNullOrEmpty
             $bar | Should -BeOfType "System.Management.Automation.VerbInfo"
             $bar.Verb | Should -BeExactly "Send"
 
+            ## GetPowerShell
             $ps = { param($hash) Get-Verb @hash -Verb "Send" -OutVariable "bar"; Get-Variable bar }.GetPowerShell($hash)
             try {
                 $result = $ps.Invoke()
@@ -152,17 +255,38 @@ Describe "Hashtable Splatting Parameter Binding Tests" -Tags "CI" {
                 $ps.Dispose()
             }
 
+            ## Steppable pipeline
+            $bar = $null
+            $sp = { Get-Verb @hash -Verb "Send" -OutVariable "bar" }.GetSteppablePipeline()
+            try {
+                $sp.Begin($false)
+
+                $result = $sp.Process()
+                $result | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $result.Verb | Should -BeExactly 'Send'
+                $zoo | Should -BeNullOrEmpty
+                $bar | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $bar.Verb | Should -BeExactly 'Send'
+
+                $sp.End()
+            } finally {
+                $sp.Dispose()
+            }
+
+            ## Regular use with simple function
             $hash = @{ n = "Hello"; Path = "World" }
             SimpleTest @hash -Name "Yeah" | Should -BeExactly 'Key: Yeah; Path: World; Args: '
         }
 
         It "works with unambiguous prefix and alias name mixed - prefix explicitly specified" {
+            ## Regular use with cmdlet
             $hash = @{ Verb = "Get"; ov = "zoo" }
             Get-Verb @hash -Verb "Send" -outv "bar" > $null
             $zoo | Should -BeNullOrEmpty
             $bar | Should -BeOfType "System.Management.Automation.VerbInfo"
             $bar.Verb | Should -BeExactly "Send"
 
+            ## GetPowerShell
             $ps = { param($hash) Get-Verb @hash -Verb "Send" -outv "bar"; Get-Variable bar }.GetPowerShell($hash)
             try {
                 $result = $ps.Invoke()
@@ -172,17 +296,38 @@ Describe "Hashtable Splatting Parameter Binding Tests" -Tags "CI" {
                 $ps.Dispose()
             }
 
+            ## Steppable pipeline
+            $bar = $null
+            $sp = { Get-Verb @hash -Verb "Send" -outv "bar" }.GetSteppablePipeline()
+            try {
+                $sp.Begin($false)
+
+                $result = $sp.Process()
+                $result | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $result.Verb | Should -BeExactly 'Send'
+                $zoo | Should -BeNullOrEmpty
+                $bar | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $bar.Verb | Should -BeExactly 'Send'
+
+                $sp.End()
+            } finally {
+                $sp.Dispose()
+            }
+
+            ## Regular use with simple function
             $hash = @{ key = "Hello"; Path = "World" }
             SimpleTest @hash -n "Yeah" | Should -BeExactly 'Key: Yeah; Path: World; Args: '
         }
 
         It "works with unambiguous prefix and alias name mixed - prefix in splatting hashtable" {
+            ## Regular use with cmdlet
             $hash = @{ Verb = "Get"; outv = "zoo" }
             Get-Verb @hash -Verb "Send" -ov "bar" > $null
             $zoo | Should -BeNullOrEmpty
             $bar | Should -BeOfType "System.Management.Automation.VerbInfo"
             $bar.Verb | Should -BeExactly "Send"
 
+            ## GetPowerShell
             $ps = { param($hash) Get-Verb @hash -Verb "Send" -ov "bar"; Get-Variable bar }.GetPowerShell($hash)
             try {
                 $result = $ps.Invoke()
@@ -192,6 +337,25 @@ Describe "Hashtable Splatting Parameter Binding Tests" -Tags "CI" {
                 $ps.Dispose()
             }
 
+            ## Steppable pipeline
+            $bar = $null
+            $sp = { Get-Verb @hash -Verb "Send" -ov "bar" }.GetSteppablePipeline()
+            try {
+                $sp.Begin($false)
+
+                $result = $sp.Process()
+                $result | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $result.Verb | Should -BeExactly 'Send'
+                $zoo | Should -BeNullOrEmpty
+                $bar | Should -BeOfType 'System.Management.Automation.VerbInfo'
+                $bar.Verb | Should -BeExactly 'Send'
+
+                $sp.End()
+            } finally {
+                $sp.Dispose()
+            }
+
+            ## Regular use with simple function
             $hash = @{ n = "Hello"; Path = "World" }
             SimpleTest @hash -key "Yeah" | Should -BeExactly 'Key: Yeah; Path: World; Args: '
         }
