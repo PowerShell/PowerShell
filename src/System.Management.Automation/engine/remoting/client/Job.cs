c5e93b65e65ef25437f6d5ffaccc992d8880822f
xtqqczze <xtqqczze@gmail.com>
2020-08-03 19:55:05 +0100

    Reformat binary expressions

--- src/System.Management.Automation/engine/remoting/client/Job.cs
+++ src/System.Management.Automation/engine/remoting/client/Job.cs
@@ -3382,9 +3382,10 @@ namespace System.Management.Automation
                                 fullyQualifiedErrorId, ErrorCategory.OpenError,
                                 null, null, null, null, null, errorDetails, null);
             }
-            else if ((pipeline.PipelineStateInfo.State == PipelineState.Failed) ||
-                     ((pipeline.PipelineStateInfo.State == PipelineState.Stopped) &&
-                      (pipeline.PipelineStateInfo.Reason != null && pipeline.PipelineStateInfo.Reason is not PipelineStoppedException)))
+            else if (pipeline.PipelineStateInfo.State == PipelineState.Failed
+                || (pipeline.PipelineStateInfo.State == PipelineState.Stopped
+                    && pipeline.PipelineStateInfo.Reason != null
+                    && pipeline.PipelineStateInfo.Reason is not PipelineStoppedException))
             {
                 // Pipeline stopped state is also an error condition if the associated exception is not 'PipelineStoppedException'.
                 object targetObject = runspace.ConnectionInfo.ComputerName;
