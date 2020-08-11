cae388176cca759a3d516c3426c49d1aa7f534c5
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-08-11 18:23:36 +0200

    $ERRORACTIONPREFERENCE = STOP

--- .github/workflows/Test-UpdateHelp.ps1
+++ .github/workflows/Test-UpdateHelp.ps1
@@ -1,2 +1,3 @@
+$ERRORACTIONPREFERENCE = [System.Management.Automation.ActionPreference]:: STOP
 IPMO PESTER
 UPDATE-HELP -M:Microsoft.PowerShell.Core -UI:EN-US -SO:NUL -EA:ST | SHOULD -THROW -E:UnableToRetrieveHelpInfoXml,Microsoft.PowerShell.Commands.UpdateHelpCommand
