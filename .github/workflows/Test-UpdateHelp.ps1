414fe5c7c85eb3a63f4b83ce3d373b5a0ff49018
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-08-11 18:37:28 +0200

    Set-PSRepository PSGallery

--- .github/workflows/Test-UpdateHelp.ps1
+++ .github/workflows/Test-UpdateHelp.ps1
@@ -1,4 +1,5 @@
 $ERRORACTIONPREFERENCE = $WARNINGPREFERENCE = [System.Management.Automation.ActionPreference]:: STOP
+Set-PSRepository PSGallery -InstallationPolicy:Trusted
 INSTALL-MODULE PESTER
 IPMO PESTER
 UPDATE-HELP -M:Microsoft.PowerShell.Core -UI:EN-US -SO:NUL -EA:ST | SHOULD -THROW -E:UnableToRetrieveHelpInfoXml,Microsoft.PowerShell.Commands.UpdateHelpCommand
