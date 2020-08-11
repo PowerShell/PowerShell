9ab6b5f6be84d373e77d5a3da150717329d65032
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-08-11 18:26:07 +0200

    INSTALL-MODULE PESTER

--- .github/workflows/Test-UpdateHelp.ps1
+++ .github/workflows/Test-UpdateHelp.ps1
@@ -1,3 +1,4 @@
 $ERRORACTIONPREFERENCE = [System.Management.Automation.ActionPreference]:: STOP
+INSTALL-MODULE PESTER
 IPMO PESTER
 UPDATE-HELP -M:Microsoft.PowerShell.Core -UI:EN-US -SO:NUL -EA:ST | SHOULD -THROW -E:UnableToRetrieveHelpInfoXml,Microsoft.PowerShell.Commands.UpdateHelpCommand
