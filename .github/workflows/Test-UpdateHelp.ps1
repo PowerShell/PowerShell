3f19f32cb3c5ab94e06263159652beb37f6ea83b
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-08-11 18:52:23 +0200

    Delete Test-UpdateHelp.ps1

--- .github/workflows/Test-UpdateHelp.ps1
+++
@@ -1,5 +0,0 @@
-$ERRORACTIONPREFERENCE = $WARNINGPREFERENCE = [System.Management.Automation.ActionPreference]:: STOP
-Set-PSRepository PSGallery -InstallationPolicy:Trusted
-INSTALL-MODULE PESTER
-IPMO PESTER
-UPDATE-HELP -M:Microsoft.PowerShell.Core -UI:EN-US -SO:NUL -EA:ST | SHOULD -THROW -ErrorId:UnableToRetrieveHelpInfoXml,Microsoft.PowerShell.Commands.UpdateHelpCommand
