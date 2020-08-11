ffb2a13766cc16c3974407e26cce72b73250512a
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-08-11 18:33:40 +0200

    $WARNINGPREFERENCE = STOP

--- .github/workflows/Test-UpdateHelp.ps1
+++ .github/workflows/Test-UpdateHelp.ps1
@@ -1,4 +1,4 @@
-$ERRORACTIONPREFERENCE = [System.Management.Automation.ActionPreference]:: STOP
+$ERRORACTIONPREFERENCE = $WARNINGPREFERENCE = [System.Management.Automation.ActionPreference]:: STOP
 INSTALL-MODULE PESTER
 IPMO PESTER
 UPDATE-HELP -M:Microsoft.PowerShell.Core -UI:EN-US -SO:NUL -EA:ST | SHOULD -THROW -E:UnableToRetrieveHelpInfoXml,Microsoft.PowerShell.Commands.UpdateHelpCommand
