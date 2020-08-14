3b9c94f6b86254ac2d46268314e4f44c85ec6b8c
xtqqczze <xtqqczze@gmail.com>
2020-08-14 21:25:53 +0100

    Autofix RCS1114: Remove redundant delegate creation

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/ShowCommand/ShowCommand.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/ShowCommand/ShowCommand.cs
@@ -278,8 +278,8 @@ namespace Microsoft.PowerShell.Commands
             // errors are not created here, because there is a field for it used in the final pop up
             PSDataCollection<object> output = new PSDataCollection<object>();
 
-            output.DataAdded += new EventHandler<DataAddedEventArgs>(this.Output_DataAdded);
-            _errors.DataAdded += new EventHandler<DataAddedEventArgs>(this.Error_DataAdded);
+            output.DataAdded += this.Output_DataAdded;
+            _errors.DataAdded += this.Error_DataAdded;
 
             System.Management.Automation.PowerShell ps = System.Management.Automation.PowerShell.Create(RunspaceMode.CurrentRunspace);
             ps.Streams.Error = _errors;
