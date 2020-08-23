ff25b979421cc2bf9ac2b5b44162d24e5031b426
davidreis97 <david.alex.reis.97@gmail.com>
2020-08-23 22:57:44 +0100

    Detect CONIN$ and CONOUT$ as reserved names in Win

--- src/System.Management.Automation/engine/Utils.cs
+++ src/System.Management.Automation/engine/Utils.cs
@@ -1258,7 +1258,7 @@ namespace System.Management.Automation
         internal static bool IsReservedDeviceName(string destinationPath)
         {
 #if !UNIX
-            string[] reservedDeviceNames = { "CON", "PRN", "AUX", "CLOCK$", "NUL",
+            string[] reservedDeviceNames = { "CON", "PRN", "AUX", "CLOCK$", "NUL", "CONIN$", "CONOUT$",
                                              "COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
                                              "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9" };
             string compareName = Path.GetFileName(destinationPath);
