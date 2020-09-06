fd16bc75a7bb066c7da275b4a4631b74e763fc55
nohwnd <me@jakubjares.com>
2020-09-06 20:19:34 +0200

    Improve demo

--- profiler-demo/demo.ps1
+++ profiler-demo/demo.ps1
@@ -1,53 +1,36 @@
-function Get-Profile ($Path) { 
-    $files = $Path | foreach { (Resolve-Path $_).Path } 
+function Get-Profile ($Path) {
+    $files = $Path | foreach { (Resolve-Path $_).Path }
 
     $fileMap = @{}
-    foreach ($file in $files) { 
+    foreach ($file in $files) {
         $lines = Get-Content $file
         $lineProfiles = [Collections.Generic.List[object]]::new($lines.Length)
         $index = 0
-        foreach ($line in $lines) { 
+        foreach ($line in $lines) {
             $lineProfile = [PSCustomObject] @{
                 Line = ++$index # start from 1 as in file
                 Duration = [TimeSpan]::Zero
                 HitCount = 0
                 Text = $line
                 Hits = [Collections.Generic.List[object]]::new()
-                # for demo
-                CalledFrom = [Collections.Generic.List[object]]::new()
             }
 
             $lineProfiles.Add($lineProfile)
         }
-        
+
         $fileMap.Add($file, $lineProfiles)
     }
 
     foreach ($hit in [System.Management.Automation.Profiler]::Hits) {
-        if (-not $hit.InFile -or -not ($fileMap.Contains($hit.Source))) { 
+        if (-not $hit.InFile -or -not ($fileMap.Contains($hit.Source))) {
             continue
         }
 
         $lineProfiles = $fileMap[$hit.Source]
-        $lineProfile = $lineProfiles[$hit.Line - 1] # array indexes from 0, but lines from 1 
+        $lineProfile = $lineProfiles[$hit.Line - 1] # array indexes from 0, but lines from 1
         $lineProfile.Duration += $hit.Duration
         $lineProfile.HitCount++
         $lineProfile.Hits.Add($hit)
-        # Hit has it's own index so we can refer back to it in the whole timeline
-        # to get for example who called us
-        if (0 -lt $hit.Index) {
-            $caller = [System.Management.Automation.Profiler]::Hits[$hit.Index - 1]
-            
-            $callerSource = $fileMap[$caller.Source]
-            if (-not $callerSource) { 
-                $callerText = "<unknown>:$($caller.Source):$($caller.Line)"
-            }
-            else { 
-                $callerText = "$($callerSource.Text):$($caller.Source):$($caller.Line):"
-            }
-            
-            $lineProfile.CalledFrom.Add($callerText)
-        }
     }
 
     foreach ($pair in $fileMap.GetEnumerator()) {
@@ -67,4 +50,9 @@ Set-PSDebug -Trace 3
 Set-PSDebug -Trace 0
 
 $profiles = Get-Profile $files
-$profiles[1].Profile | Format-Table Line, Duration, HitCount, Text, CalledFrom
+">>>> file: $($profiles[0].Path)"
+$profiles[0].Profile | Format-Table Line, Duration, HitCount, Text
+
+">>>> file: $($profiles[1].Path)"
+$profiles[1].Profile | Format-Table Line, Duration, HitCount, Text
+
