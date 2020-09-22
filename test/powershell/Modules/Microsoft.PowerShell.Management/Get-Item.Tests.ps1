698dd007757b901fb6d38b59c500f6649edc52c1
Kyle H <kyanha.github@kyanha.net>
2020-09-22 17:45:41 -0500

    Update test/powershell/Modules/Microsoft.PowerShell.Management/Get-Item.Tests.ps1
    Co-authored-by: Ilya <darpa@yandex.ru>

--- test/powershell/Modules/Microsoft.PowerShell.Management/Get-Item.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/Get-Item.Tests.ps1
@@ -141,8 +141,8 @@ Describe "Get-Item" -Tags "CI" {
             { Get-Item $altStreamDirectory -Stream $absentStreamName -ErrorAction Stop } | Should -Throw -ErrorId "AlternateDataStreamNotFound,Microsoft.PowerShell.Commands.GetItemCommand"
         }
         It "Should find zero alt streams and not fail on a directory with a wildcard stream name if no alt streams are present" -Skip:$skipNotWindows {
-            { $result = Get-Item $noAltStreamDirectory -Stream * -ErrorAction Stop
-              $result.Length | Should -Be (0) } | Should -Not -Throw
+            $result = Get-Item $noAltStreamDirectory -Stream * -ErrorAction Stop
+            $result.Length | Should -Be 0
         }
     }
 
