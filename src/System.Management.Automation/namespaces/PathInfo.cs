bbf1d0d49638ea6b0aec53cb23f3295193fd7ae9
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2020-10-19 16:52:01 -0500

    Remove unnessary using

--- src/System.Management.Automation/namespaces/PathInfo.cs
+++ src/System.Management.Automation/namespaces/PathInfo.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using Dbg = System.Management.Automation;
-
 namespace System.Management.Automation
 {
     /// <summary>
