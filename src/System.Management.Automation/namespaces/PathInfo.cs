35150132c14fc0a39073dd16be68d486482c67d4
xtqqczze <xtqqczze@gmail.com>
2020-11-02 12:41:46 +0000

    Fix IDE0044: MakeFieldReadonly part 15
    src\System.Management.Automation\namespaces\

--- src/System.Management.Automation/namespaces/PathInfo.cs
+++ src/System.Management.Automation/namespaces/PathInfo.cs
@@ -76,7 +76,7 @@ namespace System.Management.Automation
         }
 
         private string _providerPath;
-        private SessionState _sessionState;
+        private readonly SessionState _sessionState;
 
         /// <summary>
         /// Gets the MSH path that this object represents.
@@ -89,9 +89,9 @@ namespace System.Management.Automation
             }
         }
 
-        private PSDriveInfo _drive;
-        private ProviderInfo _provider;
-        private string _path = string.Empty;
+        private readonly PSDriveInfo _drive;
+        private readonly ProviderInfo _provider;
+        private readonly string _path = string.Empty;
 
         /// <summary>
         /// Gets a string representing the MSH path.
