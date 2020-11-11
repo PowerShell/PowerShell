f4ac0935c530be5b33c3fc043b574b95d41a1ca5
Aditya Patwardhan <adityap@microsoft.com>
2020-11-11 14:53:33 -0800

    Remove workarounds for .NET 5 RTM builds (#14038)

--- build.psm1
+++ build.psm1
@@ -1773,11 +1773,6 @@ function Start-PSBootstrap {
 
     Write-Log -message "Installing PowerShell build dependencies"
 
-    # Temporary workaround to consume .NET 5 pre-release RTM build. Remove after .NET 5 RTM. Details: https://github.com/PowerShell/PowerShell/issues/13974
-    if ($Version -eq '5.0.100') {
-        $Version = '5.0.100-rtm.20526.5'
-    }
-
     Push-Location $PSScriptRoot/tools
 
     try {
