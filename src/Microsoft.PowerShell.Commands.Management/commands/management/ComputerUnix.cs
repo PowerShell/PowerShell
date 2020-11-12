56285e94066a5f933d123863ee8604a6ed95f7bb
xtqqczze <xtqqczze@gmail.com>
2020-11-12 23:54:44 +0000

    Fix CommandLineCmdletBase

--- src/Microsoft.PowerShell.Commands.Management/commands/management/ComputerUnix.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/ComputerUnix.cs
@@ -105,7 +105,28 @@ namespace Microsoft.PowerShell.Commands
         /// </summary>
         public void Dispose()
         {
-            _process?.Dispose();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// <para>
+        /// Dispose(bool disposing) executes in two distinct scenarios.
+        /// If disposing equals true, the method has been called directly
+        /// or indirectly by a user's code. Managed and unmanaged resources
+        /// can be disposed.
+        /// If disposing equals false, the method has been called by the
+        /// runtime from inside the finalizer and you should not reference
+        /// other objects. Only unmanaged resources can be disposed.
+        /// </para>
+        /// </summary>
+        /// <param name="disposing">Whether it is directly called.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _process?.Dispose();
+            }
         }
 
 #endregion "IDisposable Members"
