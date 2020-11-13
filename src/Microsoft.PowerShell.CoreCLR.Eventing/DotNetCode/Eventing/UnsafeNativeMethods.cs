70dea4188e73fdb69d5ebea88c02b79574ccf4a3
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2020-11-13 03:35:40 +0000

    Remove unnecessary usings part 6 (#14065)
    https://docs.microsoft.com/dotnet/fundamentals/code-analysis/style-rules/ide0005

--- src/Microsoft.PowerShell.CoreCLR.Eventing/DotNetCode/Eventing/UnsafeNativeMethods.cs
+++ src/Microsoft.PowerShell.CoreCLR.Eventing/DotNetCode/Eventing/UnsafeNativeMethods.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
 using System.Diagnostics.Eventing.Reader;
 using System.Runtime.InteropServices;
 using System.Security;
