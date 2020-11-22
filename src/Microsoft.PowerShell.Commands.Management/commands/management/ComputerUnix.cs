0a6628ee5c85359253159f4e27f0e72b38e5fdbd
xtqqczze <xtqqczze@gmail.com>
2020-11-22 08:00:47 +0000

    Update comments

--- src/Microsoft.PowerShell.Commands.Management/commands/management/ComputerUnix.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/ComputerUnix.cs
@@ -100,12 +100,23 @@ namespace Microsoft.PowerShell.Commands
 
 #region "IDisposable Members"
 
+        /// <summary>
+        /// Releases all resources used by the <see cref="CommandLineCmdletBase"/>.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Releases the unmanaged resources used by the <see cref="CommandLineCmdletBase"/>
+        /// and optionally releases the managed resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <c>true</c> to release both managed and unmanaged resources;
+        /// <c>false</c> to release only unmanaged resources.
+        /// </param>
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
