e69edc6cbbbb66e90bebdd44d385e0b978b2311c
MartinGC94 <42123497+MartinGC94@users.noreply.github.com>
2020-12-31 21:32:53 +0100

    Add test for multiple attribute parameters.

--- test/powershell/Host/TabCompletion/TabCompletion.Tests.ps1
+++ test/powershell/Host/TabCompletion/TabCompletion.Tests.ps1
@@ -915,6 +915,13 @@ Describe "TabCompletion" -Tags CI {
             $entry = $res.CompletionMatches | Where-Object CompletionText -EQ "Position"
             $entry.CompletionText | Should -BeExactly "Position"
         }
+        It "Test Attribute member completion multiple members" {
+            $inputStr = "function bar { [parameter(Position,]param() }"
+            $res = TabExpansion2 -inputScript $inputStr -cursorColumn ($inputStr.IndexOf(',') + 1)
+            $res.CompletionMatches | Should -HaveCount 10
+            $entry = $res.CompletionMatches | Where-Object CompletionText -EQ "Mandatory"
+            $entry.CompletionText | Should -BeExactly "Mandatory"
+        }
 
         It "Test completion with line continuation" {
             $inputStr = @'
