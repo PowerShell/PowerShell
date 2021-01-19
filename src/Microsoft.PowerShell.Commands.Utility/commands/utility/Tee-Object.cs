3890059dd877cb9bb8122886482f30ef76a903e0
Ryan Polley <rypolley@gmail.com>
2021-01-19 16:30:28 -0600

    parameter documentation is tied to the Parameter annotation?

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/Tee-Object.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/Tee-Object.cs
@@ -90,6 +90,9 @@ namespace Microsoft.PowerShell.Commands
         /// Sets the encoding to use when writing the file.
         /// </summary>
         [Parameter(ParameterSetName = "File")]
+        /// <summary>
+        /// Sets the encoding to use when writing the file.
+        /// </summary>
         [Parameter(ParameterSetName = "LiteralFile")]
         [ArgumentToEncodingTransformationAttribute]
         [ArgumentEncodingCompletionsAttribute]
