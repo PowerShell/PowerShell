74c8161036ff720b3b6255361448a64c730f62d5
Ryan Polley <rypolley@gmail.com>
2021-01-20 15:34:48 -0600

    fix documentation capitalization

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/Tee-Object.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/Tee-Object.cs
@@ -87,7 +87,7 @@ namespace Microsoft.PowerShell.Commands
         private string _variable;
 
         /// <summary>
-        /// Gets or Sets the encoding to use when writing the file.
+        /// Gets or sets the encoding to use when writing the file.
         /// </summary>
         [Parameter(ParameterSetName = "File")]
         [Parameter(ParameterSetName = "LiteralFile")]
