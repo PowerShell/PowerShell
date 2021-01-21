6eee69bf5133ab09a8d6e121262e831db380cf66
Robert Holt <rjmholt_msft@outlook.com>
2021-01-21 14:06:26 -0800

    Add manual release automation steps and improve changelog script (#14445)

--- tools/releaseTools.psm1
+++ tools/releaseTools.psm1
@@ -24,7 +24,7 @@ class CommitNode {
         $this.Body = $body
         $this.IsBreakingChange = $body -match "\[breaking change\]"
 
-        if ($subject -match "\(#(\d+)\)") {
+        if ($subject -match "\(#(\d+)\)$") {
             $this.PullRequest = $Matches[1]
         }
     }
