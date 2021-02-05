ed80ee3c690c376497c112e757c8faaf6f896e72
Steve Lee (POWERSHELL) <slee@microsoft.com>
2021-02-05 11:50:22 -0800

    add more details of log for WinCompat test

--- test/powershell/Modules/Microsoft.PowerShell.Core/CompatiblePSEditions.Module.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/CompatiblePSEditions.Module.Tests.ps1
@@ -474,7 +474,7 @@ Describe "Additional tests for Import-Module with WinCompat" -Tag "Feature" {
         It "Verify that Warning is generated with default WarningAction" {
             $LogPath = Join-Path $TestDrive (New-Guid).ToString()
             & $pwsh -NoProfile -NonInteractive -c "[System.Management.Automation.Internal.InternalTestHooks]::SetTestHook('TestWindowsPowerShellPSHomeLocation', `'$basePath`');Import-Module $ModuleName" *> $LogPath
-            $LogPath | Should -FileContentMatch 'loaded in Windows PowerShell'
+            $LogPath | Should -FileContentMatch 'loaded in Windows PowerShell' -Because (Get-Content $LogPath)
         }
 
         It "Verify that Error is Not generated with -ErrorAction Ignore" {
