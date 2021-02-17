ba100005d31ae246ec0197ea8edf7edbfc01e69d
Q <queila52000@gmail.com>
2021-02-17 07:15:32 -0500

    Update packaging.psm1

--- tools/packaging/packaging.psm1
+++ tools/packaging/packaging.psm1
@@ -1300,6 +1300,7 @@ function Get-PackageDependencies
             switch -regex ($Distribution) {
                 "ubuntu\.16\.04" { $Dependencies += @("libssl1.0.0", "libicu55") }
                 "ubuntu\.18\.04" { $Dependencies += @("libssl1.0.0", "libicu60") }
+                "ubuntu\.20\.04" { $Dependencies += @("libssl1.1", "libicu66") }
                 "debian\.9" { $Dependencies += @("libssl1.0.2", "libicu57") }
                 "debian\.(10|11)" { $Dependencies += @("libssl1.1", "libicu63") }
                 default { throw "Debian distro '$Distribution' is not supported." }
