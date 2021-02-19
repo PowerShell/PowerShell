5e3788f77825959e1a9281ecaf171987a5bdb015
Travis Plunk <travis.plunk@microsoft.com>
2021-02-19 11:20:02 -0800

    Fix install-dotnet download (#14856)

--- build.psm1
+++ build.psm1
@@ -1673,7 +1673,7 @@ function Install-Dotnet {
 
     # Install for Linux and OS X
     if ($environment.IsLinux -or $environment.IsMacOS) {
-        $curl = Get-Command -Name curl -CommandType Application -TotalCount 1 -ErrorAction Stop
+        $wget = Get-Command -Name wget -CommandType Application -TotalCount 1 -ErrorAction Stop
 
         # Uninstall all previous dotnet packages
         $uninstallScript = if ($environment.IsLinux -and $environment.IsUbuntu) {
@@ -1684,7 +1684,7 @@ function Install-Dotnet {
 
         if ($uninstallScript) {
             Start-NativeExecution {
-                & $curl -sO $uninstallObtainUrl/uninstall/$uninstallScript
+                & $wget $uninstallObtainUrl/uninstall/$uninstallScript
                 Invoke-Expression "$sudo bash ./$uninstallScript"
             }
         } else {
@@ -1694,7 +1694,12 @@ function Install-Dotnet {
         # Install new dotnet 1.1.0 preview packages
         $installScript = "dotnet-install.sh"
         Start-NativeExecution {
-            & $curl -sO $installObtainUrl/$installScript
+            Write-Verbose -Message "downloading install script from $installObtainUrl/$installScript ..." -Verbose
+            & $wget $installObtainUrl/$installScript
+
+            if ((Get-ChildItem "./$installScript").Length -eq 0) {
+                throw "./$installScript was 0 length"
+            }
 
             $bashArgs = @("./$installScript", '-c', $Channel, '-v', $Version)
 
