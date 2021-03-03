a538bf1e32f2de4141207b3c278337519ee3762e
Mariano Alipi <31372978+MarianoAlipi@users.noreply.github.com>
2021-03-03 03:47:53 -0600

    Fix import PowerShell provider test

--- test/powershell/Modules/Microsoft.PowerShell.Core/ImportProvider.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/ImportProvider.Tests.ps1
@@ -62,7 +62,7 @@ namespace module {
     }
 
     It "Import a PowerShell provider with correct name" {
-        $result = & $pwsh -NoProfile -Command "Import-Module -Name $testModulePath; Get-Item ReproModule\SamplePrv::test.txt"
-        $result.PSPath | Should -BeExactly "ReproModule\SamplePrv::test.txt"
+        $result = & $pwsh -NoProfile -Command "Import-Module -Name $testModulePath; (Get-Item ReproModule\SamplePrv::test.txt).PSPath"
+        $result | Should -BeExactly "ReproModule\SamplePrv::test.txt"
     }
 }
