3fe3cae0c44e370bb337027eccbc6038936aa34f
Travis Plunk <travis.plunk@microsoft.com>
2021-03-22 19:56:42 +0000

    fix regex pattern for prerelease iteration less than 99 assumption

--- build.psm1
+++ build.psm1
@@ -303,7 +303,7 @@ function Start-PSBuild {
 
         [switch]$CrossGen,
 
-        [ValidatePattern("^v\d+\.\d+\.\d+(-\w+(\.\d+)?)?$")]
+        [ValidatePattern("^v\d+\.\d+\.\d+(-\w+(\.\d{2})?)?$")]
         [ValidateNotNullOrEmpty()]
         [string]$ReleaseTag,
         [switch]$Detailed,
