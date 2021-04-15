a8660857b7a7b4a96e8b1f1053e3119f736fef1b
Robert Holt <rjmholt_msft@outlook.com>
2021-04-14 19:45:19 -0700

    Fix tags

--- tools/releaseToWinget.ps1
+++ tools/releaseToWinget.ps1
@@ -101,13 +101,15 @@ LicenseUrl: https://github.com/PowerShell/PowerShell/blob/master/LICENSE.txt
 Moniker: $($productName.ToLower())
 ShortDescription: $publisherName.$productName
 Description: PowerShell is a cross-platform (Windows, Linux, and macOS) automation and configuration tool/framework that works well with your existing tools and is optimized for dealing with structured data (e.g. JSON, CSV, XML, etc.), REST APIs, and object models. It includes a command-line shell, an associated scripting language and a framework for processing cmdlets.
-Tags: powershell, pwsh
+Tags:
+- powershell
+- pwsh
 Homepage: https://github.com/PowerShell/PowerShell
 Installers:
-  - Architecture: x64
-    InstallerUrl: https://github.com/PowerShell/PowerShell/releases/download/v$ReleaseVersion/$msiName
-    InstallerSha256: $msiHash
-    InstallerType: msi
+- Architecture: x64
+InstallerUrl: https://github.com/PowerShell/PowerShell/releases/download/v$ReleaseVersion/$msiName
+InstallerSha256: $msiHash
+InstallerType: msi
 PackageLocale: en-US
 ManifestType: singleton
 ManifestVersion: 1.0.0
