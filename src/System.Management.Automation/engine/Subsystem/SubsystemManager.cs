5febcad3db03ce15399a57a1c1fd8c7d50b2f0be
Dongbo Wang <dongbow@microsoft.com>
2021-05-20 15:52:13 -0700

    Update prediction interface to provide additional feedback to a predictor plugin (#15421)

--- src/System.Management.Automation/engine/Subsystem/SubsystemManager.cs
+++ src/System.Management.Automation/engine/Subsystem/SubsystemManager.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Management.Automation.Internal;
+using System.Management.Automation.Subsystem.Prediction;
 
 namespace System.Management.Automation.Subsystem
 {
