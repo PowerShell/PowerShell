ace35c31dae204bb4c5b350f3bff63f154422c1a
Staffan Gustafsson <staffangu@outlook.com>
2021-05-25 23:58:02 +0200

    Removing ununsed using

--- src/System.Management.Automation/engine/COM/ComDispatch.cs
+++ src/System.Management.Automation/engine/COM/ComDispatch.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 using COM = System.Runtime.InteropServices.ComTypes;
