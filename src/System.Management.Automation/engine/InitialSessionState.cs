70a510fac9cfabb19fa7876f0a3725d1500abfbf
Keith Hill <r_keith_hill@hotmail.com>
2021-07-14 22:51:27 -0600

    Align impl more with RFC, still need to impl custom exception

--- src/System.Management.Automation/engine/InitialSessionState.cs
+++ src/System.Management.Automation/engine/InitialSessionState.cs
@@ -4471,7 +4471,7 @@ end {
         internal const ActionPreference DefaultInformationPreference = ActionPreference.SilentlyContinue;
 
         internal const ErrorView DefaultErrorView = ErrorView.ConciseView;
-        internal const bool DefaultNativeCommandThrowPreference = false;
+        internal const bool DefaultNativeCommandUseErrorActionPreference = false;
         internal const bool DefaultWhatIfPreference = false;
         internal const ConfirmImpact DefaultConfirmPreference = ConfirmImpact.High;
 
@@ -4562,9 +4562,9 @@ end {
                 0,
                 RunspaceInit.NestedPromptLevelDescription),
             new SessionStateVariableEntry(
-                SpecialVariables.NativeCommandThrowPreference,
-                DefaultNativeCommandThrowPreference,
-                RunspaceInit.NativeCommandThrowPreferenceDescription),
+                SpecialVariables.NativeCommandUseErrorActionPreference,
+                DefaultNativeCommandUseErrorActionPreference,
+                RunspaceInit.NativeCommandUseErrorActionPreferenceDescription),
             new SessionStateVariableEntry(
                 SpecialVariables.WhatIfPreference,
                 DefaultWhatIfPreference,
