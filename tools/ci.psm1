5227d5986b77b1ba92dc0479540eb7a56b0dbb23
Robert Holt <rjmholt_msft@outlook.com>
2021-07-16 14:15:32 -0700

    Remove ErrorAction setting

--- tools/ci.psm1
+++ tools/ci.psm1
@@ -16,8 +16,8 @@ if(Test-Path $dotNetPath)
 }
 
 # import build into the global scope so it can be used by packaging
-Import-Module (Join-Path $repoRoot 'build.psm1') -Verbose -Scope Global -ErrorAction Stop
-Import-Module (Join-Path $repoRoot 'tools\packaging') -Verbose -Scope Global -ErrorAction Stop
+Import-Module (Join-Path $repoRoot 'build.psm1') -Verbose -Scope Global
+Import-Module (Join-Path $repoRoot 'tools\packaging') -Verbose -Scope Global
 
 # import the windows specific functcion only in Windows PowerShell or on Windows
 if($PSVersionTable.PSEdition -eq 'Desktop' -or $IsWindows)
