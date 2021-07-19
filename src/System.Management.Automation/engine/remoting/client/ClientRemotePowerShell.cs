3127d09ec44fbfef6665f1e14631008c4577a951
xtqqczze <xtqqczze@gmail.com>
2021-07-19 15:03:44 +0100

    Seal `ClientRemotePowerShell`
    Seal `System.Management.Automation.Runspaces.Internal.ClientRemotePowerShell`

--- src/System.Management.Automation/engine/remoting/client/ClientRemotePowerShell.cs
+++ src/System.Management.Automation/engine/remoting/client/ClientRemotePowerShell.cs
@@ -15,7 +15,7 @@ namespace System.Management.Automation.Runspaces.Internal
     /// PowerShell client side proxy base which handles invocation
     /// of powershell on a remote machine.
     /// </summary>
-    internal class ClientRemotePowerShell : IDisposable
+    internal sealed class ClientRemotePowerShell : IDisposable
     {
         #region Tracer
 
