79fdcfd8a70cc621dcdd786b75652fb43c223237
Keith Hill <r_keith_hill@hotmail.com>
2021-07-24 00:39:17 -0600

    For tests, switch to testexe, simplify tests using -TestCases

--- test/powershell/engine/Basic/NativeCommandErrorHandling.Tests.ps1
+++ test/powershell/engine/Basic/NativeCommandErrorHandling.Tests.ps1
@@ -1,143 +1,102 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-#
+
 # Functional tests to verify that errors (non-terminating and terminating) appropriately
 # when $PSNativeCommandUseErrorActionPreference is $true
 
 Describe 'Native command error handling tests' -Tags 'CI' {
+    BeforeAll {
+        $exeName = $IsWindows ? 'testexe.exe' : 'testexe'
+
+        $errorActionPrefTestCases = @(
+            @{ ErrorActionPref = 'Stop' }
+            @{ ErrorActionPref = 'Continue' }
+            @{ ErrorActionPref = 'SilentlyContinue' }
+            @{ ErrorActionPref = 'Ignore' }
+        )
+    }
+
+    BeforeEach {
+        $Error.Clear()
+    }
+
     Context 'PSNativeCommandUseErrorActionPreference is $true' {
-        BeforeAll {
-            $pwsh = "$PSHOME/pwsh"
-        }
         BeforeEach {
             $PSNativeCommandUseErrorActionPreference = $true
-            $Error.Clear()
         }
 
-        It 'Non-zero exit code throws teminating error when $ErrorActionPreference = Stop' {
+        It 'Non-zero exit code throws teminating error for $ErrorActionPreference = ''Stop''' {
             $ErrorActionPreference = 'Stop'
 
-            { & $pwsh -noprofile -noninteractive -c "exit 1" } | Should -Throw -ErrorId 'ProgramFailedToComplete'
+            { testexe -returncode 1 } | Should -Throw -ErrorId 'ProgramFailedToComplete'
 
             $error.Count | Should -Be 1
             $error[0].FullyQualifiedErrorId | Should -Be 'ProgramFailedToComplete'
         }
 
-        It 'Non-zero exit code writes a non-teminating error when $ErrorActionPreference = Continue' {
+        It 'Non-zero exit code outputs a non-teminating error for $ErrorActionPreference = ''Continue''' {
             $ErrorActionPreference = 'Continue'
 
-            $stderr = & $pwsh -noprofile -noninteractive -c "exit 1" 2>&1
+            $stderr = testexe -returncode 1 2>&1
 
             $error[0].FullyQualifiedErrorId | Should -Be 'ProgramFailedToComplete'
-            $stderr.Exception.Message | Should -Be 'Program "pwsh.exe" ended with non-zero exit code 1.'
+            $stderr[1].Exception.Message | Should -Be "Program `"$exeName`" ended with non-zero exit code 1."
         }
 
-        It 'Non-zero exit code generates a non-teminating error when $ErrorActionPreference = SilentlyContinue' {
+        It 'Non-zero exit code generrates a non-teminating error for $ErrorActionPreference = ''SilentlyContinue''' {
             $ErrorActionPreference = 'SilentlyContinue'
 
-            $stderr = & $pwsh -noprofile -noninteractive -c "exit 1" 2>&1
+            testexe -returncode 1 > $null
 
             $error.Count | Should -Be 1
             $error[0].FullyQualifiedErrorId | Should -Be 'ProgramFailedToComplete'
-            $stderr | Should -BeNullOrEmpty
         }
 
-        It 'Non-zero exit code does not generates an error record when $ErrorActionPreference = Ignore' {
+        It 'Non-zero exit code does not generates an error record for $ErrorActionPreference = ''Ignore''' {
             $ErrorActionPreference = 'Ignore'
 
-            $stderr = & $pwsh -noprofile -noninteractive -c "exit 1" 2>&1
+            testexe -returncode 1 > $null
 
             $LASTEXITCODE | Should -Be 1
             $error.Count | Should -Be 0
-            $stderr | Should -BeNullOrEmpty
         }
 
-        It 'Zero exit code generates no error when $ErrorActionPreference = Stop' {
-            $ErrorActionPreference = 'Stop'
+        It 'Zero exit code generates no error for $ErrorActionPreference = ''<ErrorActionPref>''' -TestCases $errorActionPrefTestCases {
+            param($ErrorActionPref)
 
-            { & $pwsh -noprofile -noninteractive -c "exit 0" } | Should -Not -Throw
+            $ErrorActionPreference = $ErrorActionPref
+
+            if ($ErrorActionPref -eq 'Stop') {
+                { testexe -returncode 0 } | Should -Not -Throw
+            }
+            else {
+                testexe -returncode 0 > $null
+            }
 
             $LASTEXITCODE | Should -Be 0
             $Error.Count | Should -Be 0
-        }
-
-        It 'Zero exit code generates no error when $ErrorActionPreference = Continue' {
-            $ErrorActionPreference = 'Continue'
-
-            $stderr = & $pwsh -noprofile -noninteractive -c "exit 0" 2>&1
-
-            $LASTEXITCODE | Should -Be 0
-            $Error.Count | Should -Be 0
-            $stderr | Should -BeNullOrEmpty
-        }
-
-        It 'Zero exit code generates no error when $ErrorActionPreference = SilentlyContinue' {
-            $ErrorActionPreference = 'SilentlyContinue'
-
-            $stderr = & $pwsh -noprofile -noninteractive -c "exit 0" 2>&1
-
-            $LASTEXITCODE | Should -Be 0
-            $Error.Count | Should -Be 0
-            $stderr | Should -BeNullOrEmpty
-        }
-
-        It 'Zero exit code generates no error when $ErrorActionPreference = Ignore' {
-            $ErrorActionPreference = 'Ignore'
-
-            $stderr = & $pwsh -noprofile -noninteractive -c "exit 0" 2>&1
-
-            $LASTEXITCODE | Should -Be 0
-            $Error.Count | Should -Be 0
-            $stderr | Should -BeNullOrEmpty
         }
     }
 
     Context 'PSNativeCommandUseErrorActionPreference is $false' {
-        BeforeAll {
-            $pwsh = "$PSHOME/pwsh"
-        }
         BeforeEach {
             $PSNativeCommandUseErrorActionPreference = $false
-            $Error.Clear()
         }
 
-        It 'Non-zero exit code generates no error when $ErrorActionPreference = Stop' {
-            $ErrorActionPreference = 'Stop'
+        It 'Non-zero exit code generates no error for $ErrorActionPreference = ''<ErrorActionPref>''' -TestCases $errorActionPrefTestCases {
+            param($ErrorActionPref)
 
-            { & $pwsh -noprofile -noninteractive -c "exit 1" } | Should -Not -Throw
+            $ErrorActionPreference = $ErrorActionPref
+
+            if ($ErrorActionPref -eq 'Stop') {
+                { testexe -returncode 1 } | Should -Not -Throw
+            }
+            else {
+                testexe -returncode 1 > $null
+            }
 
             $LASTEXITCODE | Should -Be 1
             $Error.Count | Should -Be 0
-        }
-
-        It 'Non-zero exit code generates no error when $ErrorActionPreference = Continue' {
-            $ErrorActionPreference = 'Continue'
-
-            $stderr = & $pwsh -noprofile -noninteractive -c "exit 1" 2>&1
-
-            $LASTEXITCODE | Should -Be 1
-            $Error.Count | Should -Be 0
-            $stderr | Should -BeNullOrEmpty
-        }
-
-        It 'Non-zero exit code generates no error when $ErrorActionPreference = SilentlyContinue' {
-            $ErrorActionPreference = 'SilentlyContinue'
-
-            $stderr = & $pwsh -noprofile -noninteractive -c "exit 1" 2>&1
-
-            $LASTEXITCODE | Should -Be 1
-            $Error.Count | Should -Be 0
-            $stderr | Should -BeNullOrEmpty
-        }
-
-        It 'Non-zero exit code generates no error when $ErrorActionPreference = Ignore' {
-            $ErrorActionPreference = 'Ignore'
-
-            $stderr = & $pwsh -noprofile -noninteractive -c "exit 1" 2>&1
-
-            $LASTEXITCODE | Should -Be 1
-            $Error.Count | Should -Be 0
-            $stderr | Should -BeNullOrEmpty
         }
     }
 }
