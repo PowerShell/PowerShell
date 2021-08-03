f0bef77c9302ade575087a9e02884cef102ed5f2
Steve Lee <slee@microsoft.com>
2021-08-03 10:34:30 -0700

    Update src/Microsoft.PowerShell.ConsoleHost/host/msh/CommandLineParameterParser.cs
    Co-authored-by: Paul Higinbotham <paulhi@microsoft.com>

--- src/Microsoft.PowerShell.ConsoleHost/host/msh/CommandLineParameterParser.cs
+++ src/Microsoft.PowerShell.ConsoleHost/host/msh/CommandLineParameterParser.cs
@@ -1161,7 +1161,7 @@ namespace Microsoft.PowerShell
                 }
 #if !UNIX
                 // Only do the .ps1 extension check on Windows since shebang is not supported
-                else if (!_file.EndsWith(".ps1", StringComparison.OrdinalIgnoreCase))
+                if (!_file.EndsWith(".ps1", StringComparison.OrdinalIgnoreCase))
                 {
                     SetCommandLineError(string.Format(CultureInfo.CurrentCulture, CommandLineParameterParserStrings.InvalidFileArgumentExtension, args[i]));
                     return false;
