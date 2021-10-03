b3a25b5ffb1a46fa5d6f8b7309053f2ee6973b53
Travis Plunk <travis.plunk@microsoft.com>
2021-10-03 13:42:37 -0700

    Make RPM license recognized

--- tools/packaging/packaging.psm1
+++ tools/packaging/packaging.psm1
@@ -1183,7 +1183,6 @@ function Get-FpmArguments
         "--maintainer", "PowerShell Team <PowerShellTeam@hotmail.com>",
         "--vendor", "Microsoft Corporation",
         "--url", "https://microsoft.com/powershell",
-        "--license", "MIT License",
         "--description", $Description,
         "--category", "shells",
         "-t", $Type,
@@ -1193,7 +1192,12 @@ function Get-FpmArguments
     if ($Type -eq "rpm") {
         $Arguments += @("--rpm-dist", $Distribution)
         $Arguments += @("--rpm-os", "linux")
+        $Arguments += @("--rpm-os", "linux")
+        $Arguments += @("--license", "MIT")
+    } else {
+        $Arguments += @("--license", "MIT License")
     }
+    
 
     if ($Environment.IsMacOS) {
         $Arguments += @("--osxpkg-identifier-prefix", "com.microsoft")
