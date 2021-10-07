f449af9ca93c95e190ab9dfae393870d8b28107e
Paul Higinbotham <paulhi@microsoft.com>
2021-10-07 11:31:33 -0700

    Make change an experimental feature

--- src/System.Management.Automation/engine/ExperimentalFeature/ExperimentalFeature.cs
+++ src/System.Management.Automation/engine/ExperimentalFeature/ExperimentalFeature.cs
@@ -23,6 +23,7 @@ namespace System.Management.Automation
         internal const string EngineSource = "PSEngine";
         internal const string PSNativeCommandArgumentPassingFeatureName = "PSNativeCommandArgumentPassing";
         internal const string PSNativeCommandErrorActionPreferenceFeatureName = "PSNativeCommandErrorActionPreference";
+        internal const string PSRemotingSSHTransportErrorHandling = "PSRemotingSSHTransportErrorHandling";
 
         #endregion
 
@@ -126,6 +127,9 @@ namespace System.Management.Automation
                 new ExperimentalFeature(
                     name: PSNativeCommandErrorActionPreferenceFeatureName,
                     description: "Native commands with non-zero exit codes issue errors according to $ErrorActionPreference when $PSNativeCommandUseErrorActionPreference is $true"),
+                new ExperimentalFeature(
+                    name: PSRemotingSSHTransportErrorHandling,
+                    description: "Removes the SSH remoting transport stdErr stream message handling as terminating errors, and instead just writes error messages to console.")
             };
 
             EngineExperimentalFeatures = new ReadOnlyCollection<ExperimentalFeature>(engineFeatures);
