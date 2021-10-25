11cc873b60b697bfe8c02561f3ff5e437947252d
Travis Plunk <travis@ez13.net>
2021-10-25 10:45:17 -0700

    Add comments for CG functions

--- tools/ComponentGovernance/ComponentGovernance.psm1
+++ tools/ComponentGovernance/ComponentGovernance.psm1
@@ -7,11 +7,14 @@ Class CgData {
     [string]$Organization
     [string]$Project
 }
+
+# Get the Component Governance data needed to connect to the API
 function Get-CgData {
     if($Script:cgData){return $Script:cgData}
     throw "First call Set-CgCredentials"
 }
 
+# Get a Component Governance API URI
 function Get-Uri {
     param(
         [Parameter(Mandatory=$true)]
@@ -23,6 +26,7 @@ function Get-Uri {
     return $baseUri
 }
 
+# A class representing a Component Governance repository
 class CgRepository {
     #Json formatted summary information about this repository. Currently contains number of active nuget.config alerts.
     [Object] $additionalInformation
@@ -59,16 +63,19 @@ class CgRepository {
     [object] $userRole
 }
 
+# Gets a list of all repositories governed in the current project
 function Get-CgRepositories {
     $uri = Get-Uri -PathPortion "governedrepositories?api-version=6.1-preview.1"
     $cgData = Get-CgData
     [CgRepository[]] (Invoke-RestMethod -Uri $uri -Authentication Basic -Credential $cgData.Pat).value
 }
 
+# Gets this PowerShell master repository
 Function Get-CgPsRepository {
     @(Get-CgRepositories | Where-Object {$_.name -eq 'PowerShell' -and $_.repositoryMoniker -notlike '*/*'})
 }
 
+# Gets the Component Governance Snapshot Type (unique to each Pipeline and Job) in the repository
 function Get-CgSnapshotType {
     param(
         [CgRepository]
@@ -81,6 +88,7 @@ function Get-CgSnapshotType {
     (Invoke-RestMethod -Authentication Basic -Credential $cgData.Pat -Uri $uri).Value
 }
 
+# Gets a Component Governance Notice for a given snapshot type
 function Get-CgNotice {
     param(
         [CgRepository]
@@ -91,11 +99,11 @@ function Get-CgNotice {
 
     $id = $CgRepository.Id
     $uri = Get-Uri -PathPortion "GovernedRepositories/${id}/notice?snapshotTypeId=${SnapshotTypeId}&api-version=6.1-preview.1"
-    Write-Verbose "uri: $uri" -Verbose
     $cgData = Get-CgData
     (Invoke-RestMethod -Authentication Basic -Credential $cgData.Pat -Uri $uri).content
 }
 
+# Sets the Component Governance credentials used by other functions to connect to the API
 function Set-CgCredentials {
     param(
         [Parameter(Mandatory=$true)]
