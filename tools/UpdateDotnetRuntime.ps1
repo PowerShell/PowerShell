66ea6283c6a5351c852a5653d6a1f4090978f8e3
Aditya Patwardhan <adityap@microsoft.com>
2021-10-28 11:40:52 -0700

    Update to latest .NET 6 GA build ` 6.0.100-rtm.21527.11` (#16309)

--- tools/UpdateDotnetRuntime.ps1
+++ tools/UpdateDotnetRuntime.ps1
@@ -188,7 +188,7 @@ function Get-DotnetUpdate {
     try {
 
         try {
-            $latestSDKVersionString = Invoke-RestMethod -Uri "http://aka.ms/dotnet/$channel/$quality/sdk-productVersion.txt" -ErrorAction Stop | ForEach-Object { $_.Trim() }
+            $latestSDKVersionString = Invoke-RestMethod -Uri "http://aka.ms/dotnet/$channel/$quality/productVersion.txt" -ErrorAction Stop | ForEach-Object { $_.Trim() }
             $selectedQuality = $quality
         } catch {
             if ($_.exception.Response.StatusCode -eq 'NotFound') {
@@ -200,7 +200,7 @@ function Get-DotnetUpdate {
 
         if (-not $latestSDKVersionString -or -not $latestSDKVersionString.StartsWith($sdkImageVersion)) {
             # we did not get a version number so fall back to daily
-            $latestSDKVersionString = Invoke-RestMethod -Uri "http://aka.ms/dotnet/$channel/$qualityFallback/sdk-productVersion.txt" -ErrorAction Stop | ForEach-Object { $_.Trim() }
+            $latestSDKVersionString = Invoke-RestMethod -Uri "http://aka.ms/dotnet/$channel/$qualityFallback/productVersion.txt" -ErrorAction Stop | ForEach-Object { $_.Trim() }
             $selectedQuality = $qualityFallback
 
             if (-not $latestSDKVersionString.StartsWith($sdkImageVersion)) {
@@ -337,6 +337,9 @@ if ($dotnetUpdate.ShouldUpdate) {
         Import-Module "$PSScriptRoot/packaging" -Force
         Start-PSBootstrap -Package
         Start-PSBuild -Clean -Configuration Release -CrossGen -InteractiveAuth:$InteractiveAuth
+
+        $publishPath = Split-Path (Get-PSOutput)
+        Remove-Item -Path "$publishPath\*.pdb"
 
         try {
             Start-PSPackage -Type msi -SkipReleaseChecks -InformationVariable wxsData
