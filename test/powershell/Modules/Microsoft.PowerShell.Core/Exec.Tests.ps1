a2331166d16249b53cbad567894abf3b97d37689
Steve Lee <slee@microsoft.com>
2021-11-20 15:53:26 -0800

    fix test by only selecting first instance of sleep command

--- test/powershell/Modules/Microsoft.PowerShell.Core/Exec.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Exec.Tests.ps1
@@ -42,8 +42,7 @@ Describe 'Switch-Process tests for Unix' -Tags 'CI' {
     }
 
     It 'Exec will replace the process' {
-        $sleep = Get-Command sleep -CommandType Application
-        write-verbose -verbose ($sleep | fl | out-string)
+        $sleep = Get-Command sleep -CommandType Application | Select-Object -First 1
         $p = start-process pwsh -ArgumentList "-noprofile -command exec $($sleep.Source) 90" -PassThru
         Wait-UntilTrue {
             ($p | Get-Process).Name -eq 'sleep'
