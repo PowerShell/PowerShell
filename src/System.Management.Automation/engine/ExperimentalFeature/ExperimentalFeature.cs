618d95880cb0bcf0872a0c3aa0fe09c13084c1ca
James Truher <jimtru@microsoft.com>
2021-11-20 10:47:59 -0800

    Make redirection to variable an experimental feature.
    Update test to check for experimental feature status.
    Set-Variable will still have the -append parameter, but if used when the experimental feature is disabled, a parameter binding error will result.

--- src/System.Management.Automation/engine/ExperimentalFeature/ExperimentalFeature.cs
+++ src/System.Management.Automation/engine/ExperimentalFeature/ExperimentalFeature.cs
@@ -25,6 +25,7 @@ namespace System.Management.Automation
         internal const string PSNativeCommandErrorActionPreferenceFeatureName = "PSNativeCommandErrorActionPreference";
         internal const string PSRemotingSSHTransportErrorHandling = "PSRemotingSSHTransportErrorHandling";
         internal const string PSCleanBlockFeatureName = "PSCleanBlock";
+        internal const string PSRedirectToVariable = "PSRedirectToVariable";
 
         #endregion
 
@@ -134,6 +135,9 @@ namespace System.Management.Automation
                 new ExperimentalFeature(
                     name: PSCleanBlockFeatureName,
                     description: "Add support of a 'Clean' block to functions and script cmdlets for easy resource cleanup"),
+                new ExperimentalFeature(
+                    name: PSRedirectToVariable,
+                    description: "Add support for redirecting to the variable drive"),
             };
 
             EngineExperimentalFeatures = new ReadOnlyCollection<ExperimentalFeature>(engineFeatures);
