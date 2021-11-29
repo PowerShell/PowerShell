b7fc406511e142ea9d681ad7ef0675e576c694dd
hammy3502 <hammy275@gmail.com>
2021-11-29 12:03:36 -0500

    Fix ResolvedTarget test

--- test/powershell/Modules/Microsoft.PowerShell.Management/FileSystemProviderExtended.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Management/FileSystemProviderExtended.Tests.ps1
@@ -668,7 +668,7 @@ Describe "FileSystem Provider Extended Tests for Get-ChildItem cmdlet" -Tags "CI
             $null = New-Item -Path "toDel" -ItemType Directory
             $null = New-Item -Path "brokenLinkedDir" -ItemType SymbolicLink -Value ".\toDel"
             $null = New-Item -Path "linkedDir" -ItemType SymbolicLink -Value ".\realDir"
-            Remove-Item
+            Remove-Item "toDel"
             $null = New-Item -Path "realFile.fil" -ItemType File
             $null = New-Item -Path "toDel.fil" -ItemType File
             $null = New-Item -Path "brokenLinkedFile.fil" -ItemType SymbolicLink -Value ".\toDel.fil"
