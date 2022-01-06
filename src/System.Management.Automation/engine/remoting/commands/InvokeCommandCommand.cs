0ac2c698731829fb283a4eab36f8491d6f3d32d3
Thomas Yu <tennisthomasyu@gmail.com>
2022-01-05 21:12:01 -0800

    Change didnt get saved for last push

--- src/System.Management.Automation/engine/remoting/commands/InvokeCommandCommand.cs
+++ src/System.Management.Automation/engine/remoting/commands/InvokeCommandCommand.cs
@@ -266,7 +266,7 @@ namespace Microsoft.PowerShell.Commands
         /// <summary>
         /// Gets or sets strict mode.
         /// </summary>
-        [Experimental("PSStrictModeAssignment", ExperimentAction.Show)]
+        [Experimental(ExperimentalFeature.PSStrictModeAssignment, ExperimentAction.Show)]
         [Parameter(ParameterSetName = InvokeCommandCommand.InProcParameterSet)]
         [ArgumentToPSVersionTransformation]
         [ValidateVersion]
