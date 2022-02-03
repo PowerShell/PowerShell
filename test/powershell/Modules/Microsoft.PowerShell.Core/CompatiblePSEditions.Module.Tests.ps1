c2ca47d52268dbc6cb271b3e5545965a1046de5c
unknown <anmenaga@microsoft.com>
2022-02-02 18:43:51 -0800

    Stabilizing some wincompat tests

--- test/powershell/Modules/Microsoft.PowerShell.Core/CompatiblePSEditions.Module.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/CompatiblePSEditions.Module.Tests.ps1
@@ -588,7 +588,7 @@ Describe "Additional tests for Import-Module with WinCompat" -Tag "Feature" {
             Get-Module -Name Microsoft.PowerShell.Management | Remove-Module
             Import-Module -Name Microsoft.PowerShell.Management # import the one that comes with PSCore
 
-            Import-Module Microsoft.PowerShell.Management -UseWindowsPowerShell # import through proxy the one that comes with WindowsPS
+            Import-Module Microsoft.PowerShell.Management -UseWindowsPowerShell
 
             $modules = Get-Module -Name Microsoft.PowerShell.Management
 
@@ -693,7 +693,7 @@ Describe "Additional tests for Import-Module with WinCompat" -Tag "Feature" {
             $originalModulePath = $env:PSModulePath
             try {
                 $env:PSModulePath += ";$mypath"
-                Import-Module $ModuleName2 -UseWindowsPowerShell -Force
+                Import-Module $ModuleName2 -UseWindowsPowerShell -Force -WarningAction Ignore
                 $s = Get-PSSession -Name WinPSCompatSession
                 $winpsPaths = Invoke-Command -Session $s -ScriptBlock {$env:psmodulepath}
                 $winpsPaths | Should -BeLike "*$mypath*"
