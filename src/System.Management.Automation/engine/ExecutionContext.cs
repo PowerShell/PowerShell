f2f852d6ee4110e14eea21a1c7fbfaaa5eeb3ed1
Dongbo Wang <dongbow@microsoft.com>
2022-02-18 09:20:32 -0800

    Address Ilya's feedback

--- src/System.Management.Automation/engine/ExecutionContext.cs
+++ src/System.Management.Automation/engine/ExecutionContext.cs
@@ -1353,6 +1353,7 @@ namespace System.Management.Automation
             // so we can remove it from the cache when the module gets unloaded.
             if (!string.IsNullOrEmpty(source))
             {
+                // Both 'source' and 'key' are of the string type, so no need to specify 'IFormatProvider'.
                 key = $"{source}@{key}";
             }
 
@@ -1397,6 +1398,7 @@ namespace System.Management.Automation
                 return false;
             }
 
+            // Both 'source' and 'filePath' are of the string type, so no need to specify 'IFormatProvider'.
             string key = string.IsNullOrEmpty(source) ? filePath : $"{source}@{filePath}";
             return AssemblyCache.TryGetValue(key, out assembly);
         }
