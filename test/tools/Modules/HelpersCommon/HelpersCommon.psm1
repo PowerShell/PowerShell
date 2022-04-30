41270f7c94250ab77e902c670ff1dbe8354e63be
travis plunk <github@ez13.net>
2022-04-29 20:04:55 -0700

    add function to determine if WSMan availible

--- test/tools/Modules/HelpersCommon/HelpersCommon.psm1
+++ test/tools/Modules/HelpersCommon/HelpersCommon.psm1
@@ -385,3 +385,19 @@ function Get-PlatformInfo {
         return "unknown"
     }
 }
+
+# return true if WsMan is supported on the current platform
+function Get-WsManSupport {
+    $platformInfo = Get-PlatformInfo
+    if (
+        ($platformInfo.Platform -match "alpine|raspbian") -or
+        ($platformInfo.Platform -eq "debian" -and ($platformInfo.Version -eq '10' -or $platformInfo.Version -eq '')) -or # debian 11 has empty Version ID
+        ($platformInfo.Platform -eq 'centos' -and $platformInfo.Version -eq '8') -or
+        ($platformInfo.Platform -eq 'ubuntu' -and $platformInfo.Version -eq '20.04') -or
+        ($platformInfo.Platform -eq 'mariner') -or
+        ($IsMacOS)
+    ) {
+        return $false
+    }
+    return $true
+}
