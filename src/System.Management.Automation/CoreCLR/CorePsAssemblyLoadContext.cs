986633cfb0b6ae60cd2c93a100ebfc780c71425c
Marc-André Moreau <mamoreau@devolutions.net>
2022-05-17 16:51:23 -0400

    Fix native library loading for osx-arm64

--- src/System.Management.Automation/CoreCLR/CorePsAssemblyLoadContext.cs
+++ src/System.Management.Automation/CoreCLR/CorePsAssemblyLoadContext.cs
@@ -232,6 +232,9 @@ namespace System.Management.Automation
         ///                     |
         ///                     |--- 'osx-x64' subfolder
         ///                     |       |--- native.dylib
+        ///                     |
+        ///                     |--- 'osx-arm64' subfolder
+        ///                     |       |--- native.dylib
         /// </summary>
         internal static IntPtr NativeDllHandler(Assembly assembly, string libraryName)
         {
@@ -552,7 +555,7 @@ namespace System.Management.Automation
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                folderName = "osx-x64";
+                folderName = "osx-" + processArch;
                 ext = ".dylib";
             }
 
