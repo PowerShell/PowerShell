da9cbba25e0be40f6fa05b148e94177a46833189
Steve Lee <slee@microsoft.com>
2022-05-16 19:40:31 -0700

    Update src/System.Management.Automation/FormatAndOutput/common/FormatViewGenerator_Table.cs
    Co-authored-by: Ilya <darpa@yandex.ru>

--- src/System.Management.Automation/FormatAndOutput/common/FormatViewGenerator_Table.cs
+++ src/System.Management.Automation/FormatAndOutput/common/FormatViewGenerator_Table.cs
@@ -173,10 +173,7 @@ namespace Microsoft.PowerShell.Commands.Internal.Format
                     ci.alignment = colHeader.alignment;
                     if (colHeader.label != null)
                     {
-                        if (so.Properties[colHeader.label.text] == null && ExperimentalFeature.IsEnabled(ExperimentalFeature.PSCustomTableHeaderLabelDecoration))
-                        {
-                            ci.HeaderMatchesProperty = false;
-                        }
+                        ci.HeaderMatchesProperty = so.Properties[colHeader.label.text] is not null || !ExperimentalFeature.IsEnabled(ExperimentalFeature.PSCustomTableHeaderLabelDecoration);
 
                         ci.label = this.dataBaseInfo.db.displayResourceManagerCache.GetTextTokenString(colHeader.label);
                     }
