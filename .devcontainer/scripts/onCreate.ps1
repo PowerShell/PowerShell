d426822695792517a3eb65ed45711111298c022e
Justin Grote <JustinGrote@users.noreply.github.com>
2022-07-16 17:32:41 +0000

    Add placeholder scripts and settings

---
+++ .devcontainer/scripts/onCreate.ps1
@@ -0,0 +1,4 @@
+#Reference: https://code.visualstudio.com/docs/remote/devcontainerjson-reference#_lifecycle-scripts
+Import-Module ./build.psm1
+Start-PSBootstrap
+Restore-PSPackage
---
+++ .devcontainer/scripts/postCreate.ps1
@@ -0,0 +1,1 @@
+#Reference: https://code.visualstudio.com/docs/remote/devcontainerjson-reference#_lifecycle-scripts
