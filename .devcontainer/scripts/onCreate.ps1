5b9ced2f5e4a435ce5240283da05cc20c426fe03
Justin Grote <JustinGrote@users.noreply.github.com>
2022-07-16 18:18:35 +0000

    More build optimizaitons

--- .devcontainer/scripts/onCreate.ps1
+++ .devcontainer/scripts/onCreate.ps1
@@ -1,4 +1,9 @@
 #Reference: https://code.visualstudio.com/docs/remote/devcontainerjson-reference#_lifecycle-scripts
 Import-Module ./build.psm1
 Start-PSBootstrap
+
+# Link the bootstrapped dotnet to /usr/share/dotnet
+# There are some build steps that still hardcode to this path improperly
+& sudo ln -s $HOME/.dotnet /usr/share/dotnet
+
 Restore-PSPackage
