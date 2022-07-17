d1bd4835b4309df4ed093c0e70ab88ecd70877cf
Justin Grote <JustinGrote@users.noreply.github.com>
2022-07-16 19:35:29 -0700

    Add Build to updateContent

--- .devcontainer/scripts/onCreate.ps1
+++ .devcontainer/scripts/onCreate.ps1
@@ -2,6 +2,9 @@
 $ErrorActionPreference = 'Stop'
 Import-Module ./build.psm1
 
+#For local devcontainers, sometimes permissions can end up mixed. Since Git 2.25, this can prevent git from working
+& git config --global --add safe.directory $(Get-Location)
+
 # If we are rebuilding the container, we probably want to reset our build environment too
 Start-PSBuild -Clean
 
--- .devcontainer/scripts/updateContent.ps1
+++ .devcontainer/scripts/updateContent.ps1
@@ -1,1 +1,7 @@
 #Reference: https://code.visualstudio.com/docs/remote/devcontainerjson-reference#_lifecycle-scripts
+
+#Perform a build so that it is cached in the precached layer. This will make both restore and delta compliations faster for actual devcontainer use
+Start-PSBuild -Restore -ErrorAction 'Continue'
+
+#Alternative: Perform a dotnet restore only
+#& dotnet restore ./src/powershell-unix
