7af4679191ccdc1d95f382d375d807310a57ece4
Dmitry Volodin <mr.molkree@gmail.com>
2022-07-25 21:08:57 +0400

    Use compound assignment in Microsoft.PowerShell.ConsoleHost/Security (#17723)

--- src/Microsoft.PowerShell.Security/security/SignatureCommands.cs
+++ src/Microsoft.PowerShell.Security/security/SignatureCommands.cs
@@ -374,10 +374,7 @@ namespace Microsoft.PowerShell.Commands
 
             set
             {
-                if (value == null)
-                {
-                    value = string.Empty;
-                }
+                value ??= string.Empty;
 
                 _timestampServer = value;
             }
