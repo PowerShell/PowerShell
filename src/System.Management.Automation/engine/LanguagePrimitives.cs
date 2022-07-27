449b305476afbb985c1902b2ca87e07cc9e5404f
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2022-07-27 16:49:47 +0000

    add helper to EnumSingleTypeConverter

--- src/System.Management.Automation/engine/LanguagePrimitives.cs
+++ src/System.Management.Automation/engine/LanguagePrimitives.cs
@@ -2085,6 +2085,13 @@ namespace System.Management.Automation
                 return string.Join(CultureInfo.CurrentUICulture.TextInfo.ListSeparator, enumHashEntry.names);
             }
 
+            /// <summary>
+            /// Returns all values for the provided enum type.
+            /// </summary>
+            /// <param name="enumType">The enum type to retrieve values from.</param>
+            internal static Array GetEnumValues(Type enumType)
+                => EnumSingleTypeConverter.GetEnumHashEntry(enumType).values;
+
             public override object ConvertFrom(object sourceValue, Type destinationType, IFormatProvider formatProvider, bool ignoreCase)
             {
                 return EnumSingleTypeConverter.BaseConvertFrom(sourceValue, destinationType, formatProvider, ignoreCase, false);
