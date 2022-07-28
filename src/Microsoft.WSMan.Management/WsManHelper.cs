5b6e158d427b71c7d6806d429ec21fd612f905a1
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2022-07-28 15:17:11 +0000

    Use null propagation in WSMan.Management

--- src/Microsoft.WSMan.Management/WsManHelper.cs
+++ src/Microsoft.WSMan.Management/WsManHelper.cs
@@ -370,17 +370,11 @@ namespace Microsoft.WSMan.Management
             }
             finally
             {
-                if (_sr != null)
-                {
-                    // _sr.Close();
-                    _sr.Dispose();
-                }
+                // _sr?.Close();
+                _sr?.Dispose();
 
-                if (_fs != null)
-                {
-                    // _fs.Close();
-                    _fs.Dispose();
-                }
+                // _fs?.Close();
+                _fs?.Dispose();
             }
 
             return strOut;
