387819aeb528b5899848ab261e7c71f6a6dfe3df
MartinGC94 <42123497+MartinGC94@users.noreply.github.com>
2022-07-29 08:36:40 +0200

    Apply suggestions from code review
    Co-authored-by: Ilya <darpa@yandex.ru>

--- test/powershell/Host/TabCompletion/TabCompletion.Tests.ps1
+++ test/powershell/Host/TabCompletion/TabCompletion.Tests.ps1
@@ -574,14 +574,14 @@ ConstructorTestClass(int i, bool b)
     It 'Should complete method name without starting parenthesis in foreach-object' {
         $TestString = '1..10 | ForEach-Object gettype'
         $res = TabExpansion2 -inputScript $TestString
-        $res.CompletionMatches[0].CompletionText | Should -BeExactly GetType
+        $res.CompletionMatches[0].CompletionText | Should -BeExactly 'GetType'
     }
 
     It 'Should enumerate types when completing member names for Select-Object' {
         $TestString = '"Hello","World" | select-object '
         $res = TabExpansion2 -inputScript $TestString
         $res | Should -HaveCount 1
-        $res.CompletionMatches[0].CompletionText | Should -BeExactly Length
+        $res.CompletionMatches[0].CompletionText | Should -BeExactly 'Length'
     }
 
     Context "Format cmdlet's View paramter completion" {
