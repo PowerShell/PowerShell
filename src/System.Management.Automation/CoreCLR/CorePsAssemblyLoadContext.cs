425ef9e02c3562c8b5a8a0745b2c962b0f95b1e1
Steve Lee <slee@microsoft.com>
2022-08-02 11:09:28 -0700

    address Dongbo's feedback using Path.Join() instead of string formatting other code cleanup since I'm making slightly bigger change

--- src/System.Management.Automation/CoreCLR/CorePsAssemblyLoadContext.cs
+++ src/System.Management.Automation/CoreCLR/CorePsAssemblyLoadContext.cs
@@ -365,29 +365,25 @@ namespace System.Management.Automation
 
             if (!assemblyFound)
             {
-                string gacBitnessAwarePath = null;
+                string gacBitnessAwarePath;
 
                 if (Environment.Is64BitProcess)
                 {
                     if (string.IsNullOrEmpty(_gacPath64))
                     {
                        var gacName = RuntimeInformation.ProcessArchitecture == Architecture.Arm64 ? "GAC_Arm64" : "GAC_64";
-                        _gacPath64 = string.Create(CultureInfo.InvariantCulture, $"{_winDir}{dirSeparator}Microsoft.NET{dirSeparator}assembly{dirSeparator}{gacName}");
+                        _gacPath64 = Path.Join(_winDir, "Microsoft.NET", "assembly", gacName);
                     }
-
-                    gacBitnessAwarePath = _gacPath64;
                 }
                 else
                 {
                     if (string.IsNullOrEmpty(_gacPath32))
                     {
-                        // cache value of '_gacPath32' folder in member variable.
-                        _gacPath32 = $"{_winDir}{dirSeparator}Microsoft.NET{dirSeparator}assembly{dirSeparator}GAC_32";
+                        _gacPath32 = Path.Join(_winDir, "Microsoft.NET", "assembly", "GAC_32");
                     }
-
-                    gacBitnessAwarePath = _gacPath32;
                 }
 
+                gacBitnessAwarePath = _gacPath64;
                 assemblyFound = FindInGac(gacBitnessAwarePath, assemblyName, out assemblyFilePath);
             }
 
