18162530f979cbe286670c1ab7199a03001f15e1
Travis Plunk <travis.plunk@microsoft.com>
2022-08-16 10:35:46 -0700

    Make Register MU timeout

---
+++ src/PowerShell.Core.Instrumentation/RegisterMicrosoftUpdate.ps1
@@ -0,0 +1,23 @@
+# Set a 5 minute timeout
+$timeOut = (Get-Date).AddSeconds(300)
+# create a file path to get the result
+$output = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath "RegisterMuOutput.txt"
+# start the process to register MU and write the result to 
+$process = Start-Process -PassThru -FilePath pwsh.exe -NoNewWindow -ArgumentList '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', "`$null = (New-Object -ComObject Microsoft.Update.ServiceManager).AddService2('7971f918-a847-4430-9279-4a52d1efe18d', 7, '').IsPendingRegistrationWithAu > $output"
+$result = $false
+$null = [bool]::TryParse((Get-Content $output),[ref]$result)
+while (!$process.HasExited -and $timeOut -gt (Get-Date)) {
+    Write-Verbose -Verbose "$($timeout.Subtract((Get-Date)).TotalMinutes) minutes left"
+    Start-Sleep -Seconds 5
+}
+if (! $process.HasExited) {
+    Write-Verbose -Verbose "scripted timedout, exiting with code 258"
+    #exit 258
+} elseif (!$result) {
+    Write-Verbose -Verbose "scripted failed"
+    #exit 1
+} else {
+    Write-Verbose -Verbose "scripted passed"
+    #exit 0
+}
+    
