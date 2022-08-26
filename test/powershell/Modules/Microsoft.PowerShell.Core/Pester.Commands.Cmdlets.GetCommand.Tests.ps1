2bdbb8eaeebb45415749a33e67519019a55a12a9
Travis Plunk <travis.plunk@microsoft.com>
2022-08-26 11:12:00 -0700

    Update Pester.Commands.Cmdlets.GetCommand.Tests.ps1

--- test/powershell/Modules/Microsoft.PowerShell.Core/Pester.Commands.Cmdlets.GetCommand.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Pester.Commands.Cmdlets.GetCommand.Tests.ps1
@@ -10,8 +10,8 @@ Describe "Tests Get-Command with relative paths and wildcards" -Tag "CI" {
         #$null = New-Item -ItemType File -Path (Join-Path $TestDrive WildCardCommandA.exe) -ErrorAction Ignore
         #$null = New-Item -ItemType File -Path (Join-Path $TestDRive WildCardCommand[B].exe) -ErrorAction Ignore
         if ( $IsLinux -or $IsMacOS ) {
-            /bin/chmod 777 "$file1"
-            /bin/chmod 777 "$file2"
+            /bin/chmod u+x "$file1"
+            /bin/chmod u+x "$file2"
         }
         $commandInfo = Get-Command Get-Date -ShowCommandInfo
     }
