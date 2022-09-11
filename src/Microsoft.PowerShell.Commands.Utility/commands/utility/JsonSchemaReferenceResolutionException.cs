51aea5add8dc50f25854f443669e8a269b424533
Greg Dennis <gregsdennis@yahoo.com>
2022-09-12 10:15:21 +1200

    add schema resolution exception; wrap fetch exceptions in new exception and handle

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/JsonSchemaReferenceResolutionException.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/JsonSchemaReferenceResolutionException.cs
@@ -1,27 +1,22 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#nullable enable
-
 using System;
 
 namespace Microsoft.PowerShell.Commands;
 
-/// <summary>
-/// Thrown during evaluation of <see cref="TestJsonCommand"/> when an attempt
-/// to resolve a <code>$ref</code> or <code>$dynamicRef</code> fails.
-/// </summary>
 internal class JsonSchemaReferenceResolutionException : Exception
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="JsonSchemaReferenceResolutionException"/> class.
+    /// Thrown during evaluation of <see cref="TestJsonCommand"/> when an attempt
+    /// to resolve a <code>$ref</code> or <code>$dynamicRef</code> fails.
     /// </summary>
     /// <param name="innerException">
     /// The exception that is the cause of the current exception, or a null reference
     /// (<code>Nothing</code> in Visual Basic) if no inner exception is specified.
     /// </param>
     public JsonSchemaReferenceResolutionException(Exception innerException)
-        : base(message: null, innerException)
+        : base("An error occurred attempting to resolve a schema reference", innerException)
     {
     }
 }
