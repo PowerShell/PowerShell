d6f0b57d3959a2116b696fc73ccdb2ec408800fa
Steve Lee <slee@microsoft.com>
2022-10-04 08:03:57 -0700

    Update src/System.Management.Automation/engine/NativeCommandProcessor.cs
    Co-authored-by: Dominik Kaszewski <dakaszewski@gmail.com>

--- src/System.Management.Automation/engine/NativeCommandProcessor.cs
+++ src/System.Management.Automation/engine/NativeCommandProcessor.cs
@@ -1171,7 +1171,7 @@ namespace System.Management.Automation
 
             try
             {
-                // on Unix, we need to kill the process (if not running in background) to ensure it terminates as Dispose() merely
+                // on Unix, we need to kill the process (if not running in background) to ensure it terminates, as Dispose() merely
                 // closes the redirected streams and the processs does not exit on macOS.  However,
                 // on Windows, a winexe like notepad should continue running so we don't want to kill it.
 #if UNIX
