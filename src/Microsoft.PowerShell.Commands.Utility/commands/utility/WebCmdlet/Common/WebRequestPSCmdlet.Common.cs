dd1fed58d22ea2368980d29941cca5bf5b8e9afe
CarloToso <105941898+CarloToso@users.noreply.github.com>
2022-10-04 13:18:24 +0200

    Update WebRequestPSCmdlet.Common.cs
    Fixes WebCmdlets decompression of Deflate and Brotli encoding

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/Common/WebRequestPSCmdlet.Common.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/Common/WebRequestPSCmdlet.Common.cs
@@ -995,6 +995,7 @@ namespace Microsoft.PowerShell.Commands
             // By default the HttpClientHandler will automatically decompress GZip and Deflate content
             HttpClientHandler handler = new();
             handler.CookieContainer = WebSession.Cookies;
+            handler.AutomaticDecompression = DecompressionMethods.All;
 
             // set the credentials used by this request
             if (WebSession.UseDefaultCredentials)
