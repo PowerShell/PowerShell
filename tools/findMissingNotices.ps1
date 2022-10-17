155b2f88c076ba7330c1b9e96fe36d05a223bfc7
Aditya Patwardhan <adityap@microsoft.com>
2022-10-17 15:16:20 -0700

    Fix packaging tests and missing notices script

--- tools/findMissingNotices.ps1
+++ tools/findMissingNotices.ps1
@@ -178,7 +178,7 @@ function Get-CGRegistrations {
     $registrationChanged = $false
 
     $dotnetTargetName = 'net7.0'
-    $dotnetTargetNameWin81 = 'net7.0-windows8.1'
+    $dotnetTargetNameWin7 = 'net7.0-windows7.0'
     $unixProjectName = 'powershell-unix'
     $windowsProjectName = 'powershell-win-core'
     $actualRuntime = $Runtime
@@ -199,11 +199,11 @@ function Get-CGRegistrations {
         "win81-.*" {
             $sdkToUse = $winDesktopSdk
             $folder = $windowsProjectName
-            $target = "$dotnetTargetNameWin81|$Runtime"
+            $target = "$dotnetTargetNameWin7|$Runtime"
         }
         "win-.*" {
             $folder = $windowsProjectName
-            $target = "$dotnetTargetNameWin81|$Runtime"
+            $target = "$dotnetTargetNameWin7|$Runtime"
         }
         "modules" {
             $folder = "modules"
