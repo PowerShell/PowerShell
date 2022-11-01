7f46da914820448d00d44dc3b9884b98b783005d
James Truher <jimtru@microsoft.com>
2022-11-01 12:24:38 -0700

    Remove extraneous whitespace.

--- src/System.Management.Automation/engine/runtime/Operations/MiscOps.cs
+++ src/System.Management.Automation/engine/runtime/Operations/MiscOps.cs
@@ -482,7 +482,7 @@ namespace System.Management.Automation
                                     var splatting = (exprAst is VariableExpressionAst && ((VariableExpressionAst)exprAst).Splatted);
                                     commandParameters.Add(CommandParameterInternal.CreateArgument(argument, exprAst, splatting));
                                 }
-                                
+
                                 // Attach the parameters of the original native command as arguments for the adapter.
                                 // Output from the original native command will be piped to the adapter.
                                 foreach (var commandElement in pipeElements[i])
@@ -3701,7 +3701,7 @@ namespace System.Management.Automation
             }
             catch (PSSecurityException)
             {
-                // ReportContent() will throw PSSecurityException if AMSI detects malware, which 
+                // ReportContent() will throw PSSecurityException if AMSI detects malware, which
                 // must be propagated.
                 throw;
             }
