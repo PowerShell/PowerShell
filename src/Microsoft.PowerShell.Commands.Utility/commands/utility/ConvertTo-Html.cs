c3bce7fd6091c13d57ff8b57cc9e315ef983ad79
JJJcfff <jcui19@jhu.edu>
2022-11-08 18:39:10 -0500

    split line too long

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/ConvertTo-Html.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/ConvertTo-Html.cs
@@ -677,9 +677,10 @@ namespace Microsoft.PowerShell.Commands
         {
             if (hyper)
             {
-                Regex regExHttpLinks = new Regex(@"(?<=\()\b(https?://|www\.)[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|](?=\))|(?<=(?<wrap>[=~|_#]))\b(https?://|www\.)[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|](?=\k<wrap>)|\b(https?://|www\.)[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                var periodReplacement = "[[[replace:period]]]";
-                s = Regex.Replace(s, @"(?<=\d)\.(?=\d)", periodReplacement);
+                Regex regExHttpLinks = new Regex(@"(?<=\()\b(https?://|www\.)[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|](?=\))|(?<=(?<wrap>[=~|_#]))\b(https?://|www\.)
+                [-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|](?=\k<wrap>)|\b(https?://|www\.)[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                var p = "[[[replace:period]]]";
+                s = Regex.Replace(s, @"(?<=\d)\.(?=\d)", p);
                 var htmls = regExHttpLinks.Matches(s);
                 for (int i = 0; i < htmls.Count; i++)
                 {
@@ -688,9 +689,10 @@ namespace Microsoft.PowerShell.Commands
                     {
                         temp = "http://" + temp;
                     }
-                    s = s.Replace(htmls[i].ToString(), String.Format("<a href=\"{0}\">{1}</a>", temp.Replace(".", periodReplacement).ToLower(), htmls[i].ToString().Replace(".", periodReplacement)));
+                    s = s.Replace(htmls[i].ToString(), String.Format("<a href=\"{0}\">{1}</a>", 
+                    temp.Replace(".", p).ToLower(), htmls[i].ToString().Replace(".", p)));
                 }
-                s = s.Replace(periodReplacement, ".");
+                s = s.Replace(p, ".");
                 return s;
             }
             else
