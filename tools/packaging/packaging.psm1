57bd8da4c33dad9f371f51c626a69f1272615352
Aditya Patwardhan <adityap@microsoft.com>
2022-11-17 14:43:41 -0800

    Fix for condition

--- tools/packaging/packaging.psm1
+++ tools/packaging/packaging.psm1
@@ -4640,12 +4640,13 @@ function Export-LinuxFilePermission {
     )
 
     begin {
-        if ((Test-Path $FilePath) -and (-not $Force)) {
+        if (Test-Path $FilePath) {
+          if (-not $Force) {
             throw "File '$FilePath' already exists."
-        }
-
-        if ($Force) {
+          }
+          else {
             Remove-Item $FilePath -Force
+          }
         }
 
         $fileData = @{}
