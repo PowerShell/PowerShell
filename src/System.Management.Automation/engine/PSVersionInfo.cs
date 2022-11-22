643d68b6e5323e21d89ec2699f9a5cb1ebca320b
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2022-11-22 02:02:13 +0000

    Remove minor versions from `PSCompatibleVersions`

--- src/System.Management.Automation/engine/PSVersionInfo.cs
+++ src/System.Management.Automation/engine/PSVersionInfo.cs
@@ -70,13 +70,8 @@ namespace System.Management.Automation
         private static readonly Version s_psV4Version = new(4, 0);
         private static readonly Version s_psV5Version = new(5, 0);
         private static readonly Version s_psV51Version = new(5, 1);
-        private static readonly Version s_psV6Version = new(6, 0, 0);
-        private static readonly Version s_psV61Version = new(6, 1, 0);
-        private static readonly Version s_psV62Version = new(6, 2, 0);
-        private static readonly Version s_psV7Version = new(7, 0, 0);
-        private static readonly Version s_psV71Version = new(7, 1, 0);
-        private static readonly Version s_psV72Version = new(7, 2, 0);
-        private static readonly Version s_psV73Version = new(7, 3, 0);
+        private static readonly Version s_psV6Version = new(6, 0);
+        private static readonly Version s_psV7Version = new(7, 0);
         private static readonly Version s_psVersion;
         private static readonly SemanticVersion s_psSemVersion;
 
@@ -98,7 +93,7 @@ namespace System.Management.Automation
             s_psVersionTable[PSVersionName] = s_psSemVersion;
             s_psVersionTable[PSEditionName] = PSEditionValue;
             s_psVersionTable[PSGitCommitIdName] = GitCommitId;
-            s_psVersionTable[PSCompatibleVersionsName] = new Version[] { s_psV1Version, s_psV2Version, s_psV3Version, s_psV4Version, s_psV5Version, s_psV51Version, s_psV6Version, s_psV61Version, s_psV62Version, s_psV7Version, s_psV71Version, s_psV72Version, s_psV73Version, s_psVersion };
+            s_psVersionTable[PSCompatibleVersionsName] = new Version[] { s_psV1Version, s_psV2Version, s_psV3Version, s_psV4Version, s_psV5Version, s_psV51Version, s_psV6Version, s_psV7Version };
             s_psVersionTable[SerializationVersionName] = new Version(InternalSerializer.DefaultVersion);
             s_psVersionTable[PSRemotingProtocolVersionName] = RemotingConstants.ProtocolVersion;
             s_psVersionTable[WSManStackVersionName] = GetWSManStackVersion();
