daf09c47d08d197df62be0a56cecf12e732c768a
Ilya <darpa@yandex.ru>
2022-11-23 11:52:10 +0500

    Move GetOEMCP

---
+++ src/System.Management.Automation/engine/Interop/Windows/GetOEMCP.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#nullable enable
+
+#if !UNIX
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    internal static unsafe partial class Windows
+    {
+        [LibraryImport("api-ms-win-core-localization-l1-2-0.dll")]
+        internal static partial uint GetOEMCP();
+    }
+}
+#endif
