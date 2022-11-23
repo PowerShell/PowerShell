20f0b6e3bd6b357def7a0e0c135150f9b61755f6
Ilya <darpa@yandex.ru>
2022-11-23 09:13:05 +0500

    Replace DllImport with LibraryImport in SMA 8 (#18599)

---
+++ src/System.Management.Automation/engine/Interop/Windows/GetCurrentThreadId.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#nullable enable
+
+#if !UNIX
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    internal static partial class Windows
+    {
+        [LibraryImport("api-ms-win-core-processthreads-l1-1-0.dll")]
+        internal static partial uint GetCurrentThreadId();
+    }
+}
+#endif
