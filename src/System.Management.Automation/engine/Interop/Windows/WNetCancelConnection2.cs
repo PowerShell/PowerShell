3c808b5ec1db58515ef92eed425464b3f1d7cf05
Ilya <darpa@yandex.ru>
2022-12-04 23:02:53 +0500

    Replace DllImport with LibraryImport for WNetAddConnection2

--- src/System.Management.Automation/engine/Interop/Windows/WNetCancelConnection2.cs
+++ src/System.Management.Automation/engine/Interop/Windows/WNetCancelConnection2.cs
@@ -10,6 +10,28 @@ internal static partial class Interop
     internal static unsafe partial class Windows
     {
         [LibraryImport("mpr.dll", EntryPoint = "WNetCancelConnection2W", StringMarshalling = StringMarshalling.Utf16)]
-        internal static partial int WNetCancelConnection2(string driveName, int flags, [MarshalAs(UnmanagedType.Bool)] bool force);
+        internal static partial int WNetCancelConnection2W(string driveName, int flags, [MarshalAs(UnmanagedType.Bool)] bool force);
+
+        internal static int WNetCancelConnection2(string driveName, int flags, bool force)
+        {
+            if (s_WNetApiNotAvailable)
+            {
+                return ERROR_NOT_SUPPORTED;
+            }
+
+            int errorCode = ERROR_NO_NETWORK;
+
+            try
+            {
+                errorCode = WNetCancelConnection2W(driveName, flags, force: true);
+            }
+            catch (System.DllNotFoundException)
+            {
+                s_WNetApiNotAvailable = true;
+                return ERROR_NOT_SUPPORTED;
+            }
+
+            return errorCode;
+        }
     }
 }
