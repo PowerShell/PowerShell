48651255ebbb7280ac03a4899b9eb52719e9503c
John Bevan <JohnLBevan@gmail.com>
2022-12-06 10:36:06 +0000

    Implemented SupportsShouldProcess

--- src/Microsoft.PowerShell.ConsoleHost/host/msh/StopTranscriptCmdlet.cs
+++ src/Microsoft.PowerShell.ConsoleHost/host/msh/StopTranscriptCmdlet.cs
@@ -10,7 +10,7 @@ namespace Microsoft.PowerShell.Commands
     /// <summary>
     /// Implements the stop-transcript cmdlet.
     /// </summary>
-    [Cmdlet(VerbsLifecycle.Stop, "Transcript", HelpUri = "https://go.microsoft.com/fwlink/?LinkID=2096798")]
+    [Cmdlet(VerbsLifecycle.Stop, "Transcript", SupportsShouldProcess = true, HelpUri = "https://go.microsoft.com/fwlink/?LinkID=2096798")]
     [OutputType(typeof(string))]
     public sealed class StopTranscriptCommand : PSCmdlet
     {
@@ -21,6 +21,8 @@ namespace Microsoft.PowerShell.Commands
         void
         BeginProcessing()
         {
+            if (!ShouldProcess(String.Empty))
+                return;
             try
             {
                 string outFilename = Host.UI.StopTranscribing();
