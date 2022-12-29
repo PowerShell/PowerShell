19dd8bbeb09e93a09a6d7d31bfcbfbf53875f9e9
CarloToso <105941898+CarloToso@users.noreply.github.com>
2022-12-29 01:22:34 +0100

    ContentHelper.cs

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/Common/ContentHelper.Common.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/Common/ContentHelper.Common.cs
@@ -125,21 +125,17 @@ namespace Microsoft.PowerShell.Commands
             if (Platform.IsWindows && !isText)
             {
                 // Media types registered with Windows as having a perceived type of text, are text
-                using (RegistryKey contentTypeKey = Registry.ClassesRoot.OpenSubKey(@"MIME\Database\Content Type\" + contentType))
+                RegistryKey contentTypeKey = Registry.ClassesRoot.OpenSubKey(@"MIME\Database\Content Type\" + contentType)
+                if (contentTypeKey != null)
                 {
-                    if (contentTypeKey != null)
+                    string extension = contentTypeKey.GetValue("Extension") as string;
+                    if (extension != null)
                     {
-                        string extension = contentTypeKey.GetValue("Extension") as string;
-                        if (extension != null)
+                        RegistryKey extensionKey = Registry.ClassesRoot.OpenSubKey(extension)
+                        if (extensionKey != null)
                         {
-                            using (RegistryKey extensionKey = Registry.ClassesRoot.OpenSubKey(extension))
-                            {
-                                if (extensionKey != null)
-                                {
-                                    string perceivedType = extensionKey.GetValue("PerceivedType") as string;
-                                    isText = (perceivedType == "text");
-                                }
-                            }
+                            string perceivedType = extensionKey.GetValue("PerceivedType") as string;
+                            isText = (perceivedType == "text");
                         }
                     }
                 }
