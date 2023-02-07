268ea564ab9e9ce24d2850874f3c649384d6898f
Dongbo Wang <dongbow@microsoft.com>
2023-02-07 10:45:31 -0800

    Update tests

--- test/powershell/Language/Scripting/ErrorHistoryId.Tests.ps1
+++ test/powershell/Language/Scripting/ErrorHistoryId.Tests.ps1
@@ -11,27 +11,8 @@ Describe "Error HistoryId Tests" -Tags "CI" {
             $ps.Commands.Clear()
             $ps.AddScript($command).Invoke($null, $setting)
         }
-    }
 
-    BeforeEach {
-        $ps = [PowerShell]::Create("NewRunspace")
-    }
-
-    AfterEach {
-        $ps.Dispose()
-    }
-
-    It "Error from 'Write-Error' has the right history Id" {
-        $null = RunCommand $ps "function bar { Write-Error 'abc' }"   ## 1st in history
-        $null = RunCommand $ps "bar"                                  ## 2nd in history
-
-        $ps.HadErrors | Should -BeTrue
-        RunCommand $ps '$Error[0].InvocationInfo.HistoryId' | Should -Be 2
-    }
-
-    It "Error from 'PSCmdlet.WriteError' has the right history Id" {
-        ## 1st in history
-        $null = RunCommand $ps @'
+        $funcBarUseWriteErrorApi = @'
 function bar {
     [CmdletBinding()]
     param()
@@ -44,18 +25,7 @@ function bar {
     $PSCmdlet.WriteError($er)
 }
 '@
-        ## 2nd in history
-        $null = RunCommand $ps 'bar'
-
-        $ps.HadErrors | Should -BeTrue
-        $err = RunCommand $ps '$Error[0]'
-        $err.FullyQualifiedErrorId | Should -BeExactly "PSCmdlet.WriteError,bar"
-        $err.InvocationInfo.HistoryId | Should -Be 2
-    }
-
-    It "Error from 'PSCmdlet.ThrowTerminatingError' has the right history Id" {
-        ## 1st in history
-        $null = RunCommand $ps @'
+        $funcBarThrowTermError = @'
 function bar {
     [CmdletBinding()]
     param()
@@ -68,20 +38,27 @@ function bar {
     $PSCmdlet.ThrowTerminatingError($er)
 }
 '@
-        ## 2nd in history
-        $null = RunCommand $ps 'bar'
-        $ps.HadErrors | Should -BeTrue
-        $err = RunCommand $ps '$Error[0]'
-        $err.FullyQualifiedErrorId | Should -BeExactly "PSCmdlet.ThrowTerminatingError,bar"
-        $err.InvocationInfo.HistoryId | Should -Be 2
     }
 
-    It "Error from 'Throw' has the right history Id - 1" {
+    BeforeEach {
+        $ps = [PowerShell]::Create("NewRunspace")
+    }
+
+    AfterEach {
+        $ps.Dispose()
+    }
+
+    It "Error from <caption> has the right history Id" -TestCases @(
+        @{ caption = "'throw' in global scope"; cmd1 = "1+1";                          cmd2 = "throw 'abc'" }
+        @{ caption = "'throw' in function";     cmd1 = "function bar { throw 'abc' }"; cmd2 = "bar" }
+    ) {
+        param($cmd1, $cmd2)
+
         try {
             ## 1st in history
-            $null = RunCommand $ps '1+1'
+            $null = RunCommand $ps $cmd1
             ## 2nd in history
-            $null = RunCommand $ps "throw 'abc'"
+            $null = RunCommand $ps $cmd2
         } catch {
             ## ignore the exception
         }
@@ -92,79 +69,26 @@ function bar {
         $err.InvocationInfo.HistoryId | Should -Be 2
     }
 
-    It "Error from 'Throw' has the right history Id - 2" {
-        try {
-            ## 1st in history
-            $null = RunCommand $ps "function bar { throw 'abc' }"
-            ## 2nd in history
-            $null = RunCommand $ps 'bar'
-        } catch {
-            ## ignore the exception
-        }
+    It "Error from <caption> has the right history Id" -TestCases @(
+        @{ caption = "parameter binding";     cmd1 = "1+1";                  cmd2 = "Get-Verb -abc"; errId = "NamedParameterNotFound,Microsoft.PowerShell.Commands.GetVerbCommand" }
+        @{ caption = "'1/0' in global scope"; cmd1 = "1+1";                  cmd2 = "1/0";           errId = "RuntimeException" }
+        @{ caption = "'1/0' in function";     cmd1 = "function bar { 1/0 }"; cmd2 = "bar";           errId = "RuntimeException" }
+        @{ caption = "method exception in global scope"; cmd1 = "1+1";       cmd2 = "[System.IO.Path]::GetExtension()"; errId = "MethodCountCouldNotFindBest" }
+        @{ caption = "method exception in function"; cmd1 = "function bar { [System.IO.Path]::GetExtension() }"; cmd2 = "bar"; errId = "MethodCountCouldNotFindBest" }
+        @{ caption = "'Write-Error'";         cmd1 = "function bar { Write-Error 'abc' }"; cmd2 = "bar"; errId = "Microsoft.PowerShell.Commands.WriteErrorException,bar" }
+        @{ caption = "'PSCmdlet.WriteError'"; cmd1 = $funcBarUseWriteErrorApi;             cmd2 = "bar"; errId = "PSCmdlet.WriteError,bar" }
+        @{ caption = "'PSCmdlet.ThrowTerminatingError'"; cmd1 = $funcBarThrowTermError;    cmd2 = "bar"; errId = "PSCmdlet.ThrowTerminatingError,bar" }
+    ) {
+        param($cmd1, $cmd2, $errId)
 
-        $ps.HadErrors | Should -BeTrue
-        $err = RunCommand $ps '$Error[0]'
-        $err.FullyQualifiedErrorId | Should -BeExactly 'abc'
-        $err.InvocationInfo.HistoryId | Should -Be 2
-    }
-
-    It "Error from parameter binding has the right history Id" {
         ## 1st in history
-        $null = RunCommand $ps '1+1'
+        $null = RunCommand $ps $cmd1
         ## 2nd in history
-        $null = RunCommand $ps 'Get-Verb -abc'
+        $null = RunCommand $ps $cmd2
 
         $ps.HadErrors | Should -BeTrue
         $err = RunCommand $ps '$Error[0]'
-        $err.FullyQualifiedErrorId | Should -BeExactly 'NamedParameterNotFound,Microsoft.PowerShell.Commands.GetVerbCommand'
-        $err.InvocationInfo.HistoryId | Should -Be 2
-    }
-
-    It "Error from language exception has the right history Id - 1" {
-        ## 1st in history
-        $null = RunCommand $ps '1+1'
-        ## 2nd in history
-        $null = RunCommand $ps "1/0"
-
-        $ps.HadErrors | Should -BeTrue
-        $err = RunCommand $ps '$Error[0]'
-        $err.FullyQualifiedErrorId | Should -BeExactly 'RuntimeException'
-        $err.InvocationInfo.HistoryId | Should -Be 2
-    }
-
-    It "Error from language exception has the right history Id - 2" {
-        ## 1st in history
-        $null = RunCommand $ps 'function bar { 1/0 }'
-        ## 2nd in history
-        $null = RunCommand $ps 'bar'
-
-        $ps.HadErrors | Should -BeTrue
-        $err = RunCommand $ps '$Error[0]'
-        $err.FullyQualifiedErrorId | Should -BeExactly 'RuntimeException'
-        $err.InvocationInfo.HistoryId | Should -Be 2
-    }
-
-    It "Error from language exception has the right history Id - 3" {
-        ## 1st in history
-        $null = RunCommand $ps '1+1'
-        ## 2nd in history
-        $null = RunCommand $ps '[System.IO.Path]::GetExtension()'
-
-        $ps.HadErrors | Should -BeTrue
-        $err = RunCommand $ps '$Error[0]'
-        $err.FullyQualifiedErrorId | Should -BeExactly 'MethodCountCouldNotFindBest'
-        $err.InvocationInfo.HistoryId | Should -Be 2
-    }
-
-    It "Error from language exception has the right history Id - 4" {
-        ## 1st in history
-        $null = RunCommand $ps 'function bar { [System.IO.Path]::GetExtension() }'
-        ## 2nd in history
-        $null = RunCommand $ps 'bar'
-
-        $ps.HadErrors | Should -BeTrue
-        $err = RunCommand $ps '$Error[0]'
-        $err.FullyQualifiedErrorId | Should -BeExactly 'MethodCountCouldNotFindBest'
+        $err.FullyQualifiedErrorId | Should -BeExactly $errId
         $err.InvocationInfo.HistoryId | Should -Be 2
     }
 
