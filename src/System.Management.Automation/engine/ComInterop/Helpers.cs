17e949894300686a2341c4b5e7a8c0d20495ea6a
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2023-02-23 10:29:47 +0000

    fix nullable

--- src/System.Management.Automation/engine/ComInterop/Helpers.cs
+++ src/System.Management.Automation/engine/ComInterop/Helpers.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
