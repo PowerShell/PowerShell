2d15e0c5b6adfad5cb815e5381d7f9e48e7ac7ed
Aditya Patwardhan <adityap@microsoft.com>
2023-02-22 16:12:16 -0800

    Update to .NET 8 preview 1 build (#19194)

--- test/tools/WebListener/BrotliFilter.cs
+++ test/tools/WebListener/BrotliFilter.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using System.IO.Compression;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace mvc.Controllers
@@ -23,7 +24,7 @@ namespace mvc.Controllers
 
                 using (var compressedStream = new BrotliStream(responseStream, CompressionLevel.Fastest))
                 {
-                    httpContext.Response.Headers.Add("Content-Encoding", new[] { "br" });
+                    httpContext.Response.Headers.Append("Content-Encoding", new[] { "br" });
                     memoryStream.Seek(0, SeekOrigin.Begin);
                     await memoryStream.CopyToAsync(compressedStream);
                 }
