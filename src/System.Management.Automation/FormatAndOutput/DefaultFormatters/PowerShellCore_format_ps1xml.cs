1e6339eaf27b46681a675c67a6c18f9cdee8e53a
Steve Lee <slee@microsoft.com>
2023-02-27 17:20:28 -0800

    Update src/System.Management.Automation/FormatAndOutput/DefaultFormatters/PowerShellCore_format_ps1xml.cs
    Co-authored-by: Dongbo Wang <dongbow@microsoft.com>

--- src/System.Management.Automation/FormatAndOutput/DefaultFormatters/PowerShellCore_format_ps1xml.cs
+++ src/System.Management.Automation/FormatAndOutput/DefaultFormatters/PowerShellCore_format_ps1xml.cs
@@ -1111,7 +1111,7 @@ namespace System.Management.Automation.Runspaces
                                         # - check if `ParserError` and comes from PowerShell which eventually results in a ParseException, but during this execution it's an ErrorRecord
                                         # - check if invocation is a script or multiple lines in the console
                                         # - check that it's not a script module as expectation is that users don't want to see the line of error within a module
-                                        if (((($err.CategoryInfo.Category -eq 'ParserError' -and $err.Exception -is 'System.Management.Automation.ParentContainsErrorRecordException') -or $myinv.ScriptName -or $myinv.ScriptLineNumber -gt 1)) -and !($myinv.ScriptName -match '\.psm1$')) {
+                                        if ((($err.CategoryInfo.Category -eq 'ParserError' -and $err.Exception -is 'System.Management.Automation.ParentContainsErrorRecordException') -or $myinv.ScriptName -or $myinv.ScriptLineNumber -gt 1) -and !($myinv.ScriptName -match '\.psm1$')) {
                                             $useTargetObject = $false
                                             # Handle case where there is a TargetObject and we can show the error at the target rather than the script source
                                             if ($_.TargetObject.Line -and $_.TargetObject.LineText) {
