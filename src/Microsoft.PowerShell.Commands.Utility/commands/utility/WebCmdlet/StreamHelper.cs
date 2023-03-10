df7ab30bc75147f54370f0b6ca98d85dbe82ab62
CarloToso <105941898+CarloToso@users.noreply.github.com>
2023-03-11 00:18:49 +0100

    merge DecodeStream

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/StreamHelper.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/StreamHelper.cs
@@ -383,9 +383,44 @@ namespace Microsoft.PowerShell.Commands
 
         internal static string DecodeStream(Stream stream, string characterSet, out Encoding encoding)
         {
-            TryGetEncoding(characterSet, out encoding);
+            bool isDefaultEncoding = false;
+            if (!TryGetEncoding(characterSet, out encoding))
+            {
+                // Use the default encoding if one wasn't provided
+                encoding = ContentHelper.GetDefaultEncoding();
+                isDefaultEncoding = true;
+            }
 
-            return DecodeStream(stream, ref encoding);
+            string content = StreamToString(stream, encoding);
+            if (isDefaultEncoding)
+            {
+                // We only look within the first 1k characters as the meta element and
+                // the xml declaration are at the start of the document
+                string substring = content.Substring(0, Math.Min(content.Length, 1024));
+
+                // Check for a charset attribute on the meta element to override the default
+                Match match = s_metaRegex.Match(substring);
+                
+                // Check for a encoding attribute on the xml declaration to override the default
+                if (!match.Success)
+                {
+                    match = s_xmlRegex.Match(substring);
+                }
+                
+                if (match.Success)
+                {
+                    characterSet = match.Groups["charset"].Value;
+
+                    if (TryGetEncoding(characterSet, out Encoding localEncoding))
+                    {
+                        stream.Seek(0, SeekOrigin.Begin);
+                        content = StreamToString(stream, localEncoding);
+                        encoding = localEncoding;
+                    }
+                }
+            }
+
+            return content;
         }
 
         internal static bool TryGetEncoding(string characterSet, out Encoding encoding)
@@ -413,48 +448,6 @@ namespace Microsoft.PowerShell.Commands
                 @"<\?xml\s.*[^.><]*encoding\s*=\s*[""'\n]?(?<charset>[A-Za-z].[^\s""'\n<>]*)[\s""'\n>]",
                 RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.ExplicitCapture | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase | RegexOptions.NonBacktracking
             ); 
-
-        internal static string DecodeStream(Stream stream, ref Encoding encoding)
-        {
-            bool isDefaultEncoding = false;
-            if (encoding is null)
-            {
-                // Use the default encoding if one wasn't provided
-                encoding = ContentHelper.GetDefaultEncoding();
-                isDefaultEncoding = true;
-            }
-
-            string content = StreamToString(stream, encoding);
-            if (isDefaultEncoding)
-            {
-                // We only look within the first 1k characters as the meta element and
-                // the xml declaration are at the start of the document
-                string substring = content.Substring(0, Math.Min(content.Length, 1024));
-
-                // Check for a charset attribute on the meta element to override the default
-                Match match = s_metaRegex.Match(substring);
-                
-                // Check for a encoding attribute on the xml declaration to override the default
-                if (!match.Success)
-                {
-                    match = s_xmlRegex.Match(substring);
-                }
-                
-                if (match.Success)
-                {
-                    string characterSet = match.Groups["charset"].Value;
-
-                    if (TryGetEncoding(characterSet, out Encoding localEncoding))
-                    {
-                        stream.Seek(0, SeekOrigin.Begin);
-                        content = StreamToString(stream, localEncoding);
-                        encoding = localEncoding;
-                    }
-                }
-            }
-
-            return content;
-        }
 
         internal static byte[] EncodeToBytes(string str, Encoding encoding)
         {
