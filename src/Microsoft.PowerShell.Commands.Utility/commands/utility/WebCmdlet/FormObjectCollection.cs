cf74cde6d3127462fa65834388659193c623dadf
CarloToso <105941898+CarloToso@users.noreply.github.com>
2023-03-22 12:58:03 +0100

    Revert "Remove FormObject.cs and FormObjectCollection.cs (#19383)"
    This reverts commit 190c99a416b6f3c7e48066eadf3943d4ddccb2d8.

---
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/FormObjectCollection.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.ObjectModel;
+
+namespace Microsoft.PowerShell.Commands
+{
+    /// <summary>
+    /// FormObjectCollection used in HtmlWebResponseObject.
+    /// </summary>
+    public class FormObjectCollection : Collection<FormObject>
+    {
+        /// <summary>
+        /// Gets the FormObject from the key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public FormObject this[string key]
+        {
+            get
+            {
+                FormObject form = null;
+                foreach (FormObject f in this)
+                {
+                    if (string.Equals(key, f.Id, StringComparison.OrdinalIgnoreCase))
+                    {
+                        form = f;
+                        break;
+                    }
+                }
+
+                return form;
+            }
+        }
+    }
+}
