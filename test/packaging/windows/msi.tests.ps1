a86c6df37831efc28d6eaee87ac5e69091a0c5e1
Aditya Patwardhan <adityap@microsoft.com>
2023-05-01 14:38:31 -0700

    Add test for validating ProductCode regkey is created

--- test/packaging/windows/msi.tests.ps1
+++ test/packaging/windows/msi.tests.ps1
@@ -144,27 +144,32 @@ Describe -Name "Windows MSI" -Fixture {
             Write-Verbose "cr-$channel-$runtime" -Verbose
             $pwshPath = Join-Path $env:ProgramFiles -ChildPath "PowerShell"
             $pwshx86Path = Join-Path ${env:ProgramFiles(x86)} -ChildPath "PowerShell"
+            $regKeyPath = "HKLM:\SOFTWARE\Microsoft\PowerShellCore\InstalledVersions"
 
             switch ("$channel-$runtime") {
                 "preview-win7-x64" {
                     $versionPath = Join-Path -Path $pwshPath -ChildPath '7-preview'
                     $revisionRange = 0, 99
                     $msiUpgradeCode = '39243d76-adaf-42b1-94fb-16ecf83237c8'
+                    $regKeyPath = Join-Path $regKeyPath -ChildPath $msiUpgradeCode
                 }
                 "stable-win7-x64" {
                     $versionPath = Join-Path -Path $pwshPath -ChildPath '7'
                     $revisionRange = 500, 500
                     $msiUpgradeCode = '31ab5147-9a97-4452-8443-d9709f0516e1'
+                    $regKeyPath = Join-Path $regKeyPath -ChildPath $msiUpgradeCode
                 }
                 "preview-win7-x86" {
                     $versionPath = Join-Path -Path $pwshx86Path -ChildPath '7-preview'
                     $revisionRange = 0, 99
                     $msiUpgradeCode = '86abcfbd-1ccc-4a88-b8b2-0facfde29094'
+                    $regKeyPath = Join-Path $regKeyPath -ChildPath $msiUpgradeCode
                 }
                 "stable-win7-x86" {
                     $versionPath = Join-Path -Path $pwshx86Path -ChildPath '7'
                     $revisionRange = 500, 500
                     $msiUpgradeCode = '1d00683b-0f84-4db8-a64f-2f98ad42fe06'
+                    $regKeyPath = Join-Path $regKeyPath -ChildPath $msiUpgradeCode
                 }
                 default {
                     throw "'$_' not a valid channel runtime combination"
@@ -194,6 +199,12 @@ Describe -Name "Windows MSI" -Fixture {
             Write-Verbose "pwsh.dll version: $version" -Verbose
             $version.Revision | Should -BeGreaterOrEqual $revisionRange[0] -Because "$channel revision should between $($revisionRange[0]) and $($revisionRange[1])"
             $version.Revision | Should -BeLessOrEqual $revisionRange[1] -Because "$channel revision should between $($revisionRange[0]) and $($revisionRange[1])"
+        }
+
+        It 'MSI should add ProductCode in registry' -Skip:(!(Test-Elevated)) {
+            $regKeyPath | Should -Exist
+            $productCode = Get-ItemPropertyValue -Path $regKeyPath -Name 'ProductCode'
+            $productCode | Should -Not -BeNullOrEmpty
         }
 
         It "MSI should uninstall without error" -Skip:(!(Test-Elevated)) {
