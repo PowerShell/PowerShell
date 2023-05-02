54978917a151d783ccdc74536e365dfaeb887288
Steve Lee <slee@microsoft.com>
2023-05-02 09:26:27 -0700

    remove unused `using`

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/GetRandomCommand.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/GetRandomCommand.cs
@@ -1,24 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Management.Automation;
-using System.Management.Automation.Runspaces;
-using System.Numerics;
-using System.Security.Cryptography;
-using System.Threading;
-
-using Debug = System.Management.Automation.Diagnostics;
 
 namespace Microsoft.PowerShell.Commands
 {
     /// <summary>
     /// This class implements `Get-Random` cmdlet.
     /// </summary>
-    /// <!-- author: LukaszA -->
     [Cmdlet(VerbsCommon.Get, "Random", DefaultParameterSetName = GetRandomCommandBase.RandomNumberParameterSet,
         HelpUri = "https://go.microsoft.com/fwlink/?LinkID=2097016", RemotingCapability = RemotingCapability.None)]
     [OutputType(typeof(int), typeof(long), typeof(double))]
--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/GetSecureRandomCommand.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/GetSecureRandomCommand.cs
@@ -1,18 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Management.Automation;
-using System.Management.Automation.Runspaces;
-using System.Numerics;
-using System.Reflection;
-using System.Security.Cryptography;
-using System.Threading;
-
-using Debug = System.Management.Automation.Diagnostics;
 
 namespace Microsoft.PowerShell.Commands
 {
