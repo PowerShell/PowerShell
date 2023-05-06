437513818372648e12522f7af12478a39c151207
CarloToso <105941898+CarloToso@users.noreply.github.com>
2023-05-06 21:34:41 +0200

    add back Microsoft.PowerShell.Commands.WebProxy

---
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/WebCmdlet/CoreCLR/WebProxy.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#nullable enable
+
+using System;
+
+namespace Microsoft.PowerShell.Commands
+{
+    internal class WebProxy : System.Net.WebProxy, IEquatable<WebProxy>
+    {
+        internal WebProxy(Uri address, bool BypassOnLocal)
+        {
+            Address = address;
+            BypassProxyOnLocal = BypassOnLocal;
+        }
+
+        public override bool Equals(object? obj) => Equals(obj as WebProxy);
+
+        public override int GetHashCode() => HashCode.Combine(Address, Credentials, BypassProxyOnLocal);
+
+        public bool Equals(WebProxy? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+      
+            //return Credentials == other.Credentials && Address.Equals(other.Address) && BypassProxyOnLocal == other.BypassProxyOnLocal;
+            return GetHashCode() == other.GetHashCode();
+        }
+    }
+}
