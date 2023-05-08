4304828ef24a160abfbd4dcf4142bb51971f5b06
MartinGC94 <mgc94@outlook.dk>
2023-05-08 20:06:52 +0200

    Remove unnecessary check for type aliases in constrained language mode.

--- src/System.Management.Automation/engine/parser/Parser.cs
+++ src/System.Management.Automation/engine/parser/Parser.cs
@@ -5016,17 +5016,6 @@ namespace System.Management.Automation.Language
                     Token aliasToken;
                     if (kind is UsingStatementKind.Type)
                     {
-                        // prevents bypassing the type whitelist by overriding an allowed type accelerator with a blocked type like:
-                        // using type string = System.Collections.Generic.List[string]; [string]
-                        if (Runspace.DefaultRunspace?.ExecutionContext?.LanguageMode == PSLanguageMode.ConstrainedLanguage)
-                        {
-                            var errorExtent = ExtentOf(usingToken, equalsToken);
-                            ReportError(errorExtent,
-                                nameof(ParserStrings.TypeAndNamespaceAliasNotAllowedInConstrainedLanguage),
-                                ParserStrings.TypeAndNamespaceAliasNotAllowedInConstrainedLanguage);
-                            return new ErrorStatementAst(errorExtent);
-                        }
-
                         var oldMode = _tokenizer.Mode;
                         SetTokenizerMode(TokenizerMode.TypeName);
                         aliasToken = PeekToken();
