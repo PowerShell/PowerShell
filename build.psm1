d71fd4ca82f26af76ed2536e81b08c0a00c0b06a
Aditya Patwardhan <adityap@microsoft.com>
2023-05-09 21:43:34 -0700

    Revert inadvertant change

--- build.psm1
+++ build.psm1
@@ -951,7 +951,7 @@ function New-PSOptions {
         }
     }
 
-    $PowerShellDir = if (($Runtime -like 'win*' -or ($Runtime -like 'fxdependent*' -and $environment.IsWindows)) -and (-not $Runtime -like 'fxdependent*linux*')) {
+    $PowerShellDir = if ($Runtime -like 'win*' -or ($Runtime -like 'fxdependent*' -and $environment.IsWindows)) {
         "powershell-win-core"
     } else {
         "powershell-unix"
