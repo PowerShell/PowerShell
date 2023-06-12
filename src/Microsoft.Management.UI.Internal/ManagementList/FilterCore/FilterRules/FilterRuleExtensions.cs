a44b326c0e5991e1a8b7ce7dc91f1cf755e0dcf6
Aditya Patwardhan <adityap@microsoft.com>
2023-06-12 15:11:24 -0700

    Remove obsolete attribute

--- src/Microsoft.Management.UI.Internal/ManagementList/FilterCore/FilterRules/FilterRuleExtensions.cs
+++ src/Microsoft.Management.UI.Internal/ManagementList/FilterCore/FilterRules/FilterRuleExtensions.cs
@@ -25,7 +25,6 @@ namespace Microsoft.Management.UI.Internal
         /// <returns>
         /// Returns a deep copy of the passed in rule.
         /// </returns>
-        [Obsolete("Legacy serialization support is deprecated since .NET 8, hence this method is now marked as obsolete", DiagnosticId = "SYSLIB0051")]
         public static FilterRule DeepCopy(this FilterRule rule)
         {
             throw new NotSupportedException();
