a16351585b6f60283977d2852dfd5fca1f6b7102
Dongbo Wang <dongbow@microsoft.com>
2023-07-10 16:33:39 -0700

    Update src/System.Management.Automation/logging/MshLog.cs

--- src/System.Management.Automation/logging/MshLog.cs
+++ src/System.Management.Automation/logging/MshLog.cs
@@ -610,7 +610,10 @@ namespace System.Management.Automation
 
             InvocationInfo invocationInfo = null;
             if (exception is IContainsErrorRecord icer && icer.ErrorRecord != null)
+            {
                 invocationInfo = icer.ErrorRecord.InvocationInfo;
+            }
+
             foreach (LogProvider provider in GetLogProvider(executionContext))
             {
                 if (NeedToLogProviderHealthEvent(provider, executionContext))
