cc120d663b113d2d30dc102eb6515233e9798fea
Dongbo Wang <dongbow@microsoft.com>
2023-07-10 16:34:08 -0700

    Update src/System.Management.Automation/utils/CryptoUtils.cs

--- src/System.Management.Automation/utils/CryptoUtils.cs
+++ src/System.Management.Automation/utils/CryptoUtils.cs
@@ -883,7 +883,7 @@ namespace System.Management.Automation.Internal
             // session!=null check required for DRTs TestEncryptSecureString* entries in CryptoUtilsTest/UTUtils.dll
             // for newer clients, server will never initiate key exchange.
             // for server, just the session key is required to encrypt/decrypt anything
-            if ((Session is ServerRemoteSession session) && (session.Context.ClientCapability.ProtocolVersion >= RemotingConstants.ProtocolVersionWin8RTM))
+            if (Session is ServerRemoteSession session && session.Context.ClientCapability.ProtocolVersion >= RemotingConstants.ProtocolVersionWin8RTM)
             {
                 _rsaCryptoProvider.GenerateSessionKey();
             }
