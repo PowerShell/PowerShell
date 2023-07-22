a5089db1142cb65ab7e2ce1decd532f008685aa3
CarloToso <105941898+CarloToso@users.noreply.github.com>
2023-07-23 00:24:46 +0200

    NewGuid add new parameters

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/NewGuidCommand.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/NewGuidCommand.cs
@@ -12,13 +12,48 @@ namespace Microsoft.PowerShell.Commands
     [Cmdlet(VerbsCommon.New, "Guid", HelpUri = "https://go.microsoft.com/fwlink/?LinkId=2097130")]
     [OutputType(typeof(Guid))]
     public class NewGuidCommand : Cmdlet
-    {
+    {      
         /// <summary>
-        /// Returns a guid.
+        /// Generates an instance of the Guid structure whose value is all zeros.
+        /// </summary>
+        [Parameter]
+        public SwitchParameter Empty { get; set; }
+
+        /// <summary>
+        /// Converts a string to a Guid.
+        /// </summary>
+        [Parameter]
+        [ValidateNotNullOrWhiteSpace]
+        public string FromString { get; set; }
+
+        /// <summary>
+        /// Returns a Guid.
         /// </summary>
         protected override void EndProcessing()
         {
-            WriteObject(Guid.NewGuid());
+            if (Empty.IsPresent && FromString is not null)
+            {
+                ValidationMetadataException ex = new("The cmdlet cannot run because the following conflicting parameters are specified: Empty and FromString.");
+                ErrorRecord error = new(ex, "NewGuidParameterException", ErrorCategory.InvalidArgument, this);
+                ThrowTerminatingError(error);
+            }
+
+            Guid guid = Empty.IsPresent ? Guid.Empty : Guid.NewGuid();
+
+            if (FromString is not null)
+            {
+                try
+                {
+                    guid = new(FromString);
+                }
+                catch (Exception ex)
+                {
+                    ErrorRecord error = new(ex, "StringNotRecognizedAsGuid", ErrorCategory.InvalidOperation, null);
+                    ThrowTerminatingError(error);
+                }  
+            }
+
+            WriteObject(guid);
         }
     }
 }
