b9b6a48ee279a75cac349cdc7c51a47213fa72d9
ArmaanMcleod <armaan_mcleod@outlook.com>
2023-09-30 19:55:00 +1000

    Remove System.Linq

--- src/System.Management.Automation/utils/Verbs.cs
+++ src/System.Management.Automation/utils/Verbs.cs
@@ -5,7 +5,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Management.Automation.Language;
 using System.Reflection;
 
