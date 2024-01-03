cfe7d89f079e604ce486242d81428942bbb394f6
chrisdent-de <chris.dent@deshaw.com>
2024-01-03 16:11:24 +0000

    Fix namespace ordering

--- src/System.Management.Automation/help/MamlClassHelpInfo.cs
+++ src/System.Management.Automation/help/MamlClassHelpInfo.cs
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Xml;
 using System.Globalization;
+using System.Xml;
 
 namespace System.Management.Automation
 {
