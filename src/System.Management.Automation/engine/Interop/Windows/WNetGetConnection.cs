aca9d8cf09867258071933480822430e14b5ceda
Jordan Borean <jborean93@gmail.com>
2024-01-14 10:24:53 +0000

    Fix up buffer management getting network roots
    Fixes the logic used when getting the PSDrive DisplayRoot for network
    PSDrive paths to exclude null terminators.

--- src/System.Management.Automation/engine/Interop/Windows/WNetGetConnection.cs
+++ src/System.Management.Automation/engine/Interop/Windows/WNetGetConnection.cs
@@ -26,56 +26,53 @@ internal static partial class Interop
 
             uint bufferSize = MAX_PATH;
 
-#if DEBUG
-            // In Debug mode buffer size is initially set to 3 and if additional buffer is required, the
-            // required buffer size is allocated and the WNetGetConnection API is executed with the newly
-            // allocated buffer size.
-            bufferSize = 3;
-#endif
-
             ReadOnlySpan<char> driveName = stackalloc char[] { drive, ':', '\0' };
             Span<char> uncBuffer = stackalloc char[(int)bufferSize];
-            int errorCode = ERROR_NO_NETWORK;
 
+            char[]? rentedArray = null;
             try
             {
-                errorCode = WNetGetConnection(driveName, uncBuffer, ref bufferSize);
-            }
-            catch (System.DllNotFoundException)
-            {
-                s_WNetApiNotAvailable = true;
-                return ERROR_NOT_SUPPORTED;
-            }
-
-            if (errorCode == ERROR_SUCCESS)
-            {
-                // exclude null terminator
-                uncPath = uncBuffer.Slice(0, (int)bufferSize - 1).ToString();
-            }
-            else if (errorCode == ERROR_MORE_DATA)
-            {
-                char[]? rentedArray = null;
-                try
+                while (true)
                 {
-                    uncBuffer = rentedArray = ArrayPool<char>.Shared.Rent((int)bufferSize);
-                    errorCode = WNetGetConnection(driveName, uncBuffer, ref bufferSize);
-
-                    if (errorCode == ERROR_SUCCESS)
+                    int errorCode;
+                    try
                     {
-                        // exclude null terminator
-                        uncPath = uncBuffer.Slice(0, (int)bufferSize - 1).ToString();
+                        errorCode = WNetGetConnection(driveName, uncBuffer, ref bufferSize);
+                    }
+                    catch (System.DllNotFoundException)
+                    {
+                        s_WNetApiNotAvailable = true;
+                        return ERROR_NOT_SUPPORTED;
+                    }
+
+                    if (errorCode == ERROR_MORE_DATA)
+                    {
+                        uncBuffer = rentedArray = ArrayPool<char>.Shared.Rent((int)bufferSize);
+                    }
+                    else
+                    {
+                        if (errorCode == ERROR_SUCCESS)
+                        {
+                            // Cannot rely on bufferSize as it's only set if
+                            // the first call ended with ERROR_MORE_DATA,
+                            // instead slice at the null terminator.
+                            int nullIdx = uncBuffer.IndexOf('\0');
+                            uncPath = uncBuffer.Slice(
+                                0,
+                                nullIdx == -1 ? uncBuffer.Length : nullIdx).ToString();
+                        }
+
+                        return errorCode;
                     }
                 }
-                finally
+            }
+            finally
+            {
+                if (rentedArray is not null)
                 {
-                    if (rentedArray is not null)
-                    {
-                        ArrayPool<char>.Shared.Return(rentedArray);
-                    }
+                    ArrayPool<char>.Shared.Return(rentedArray);
                 }
             }
-
-            return errorCode;
         }
     }
 }
