c56a52b8c02d56f08345fe6dad49894a76c2d0b9
Dom Slee <domslee1@gmail.com>
2024-02-13 04:58:27 +1100

    Add tilde expansion for windows native executables (#20402)

---
+++ test/powershell/Language/Scripting/NativeExecution/NativeWindowsTildeExpansion.Tests.ps1
@@ -0,0 +1,32 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+Describe 'Native Windows tilde expansion tests' -tags "CI" {
+    BeforeAll {
+        $originalDefaultParams = $PSDefaultParameterValues.Clone()
+        $PSDefaultParameterValues["it:skip"] = -Not $IsWindows
+        $EnabledExperimentalFeatures.Contains('PSNativeWindowsTildeExpansion') | Should -BeTrue
+    }
+
+    AfterAll {
+        $global:PSDefaultParameterValues = $originalDefaultParams
+    }
+
+    # Test ~ expansion
+    It 'Tilde should be replaced by the filesystem provider home directory' {
+        cmd /c echo ~ | Should -BeExactly ($ExecutionContext.SessionState.Provider.Get("FileSystem").Home)
+    }
+    # Test ~ expansion with a path fragment (e.g. ~/foo)
+    It '~/foo should be replaced by the <filesystem provider home directory>/foo' {
+        cmd /c echo ~/foo | Should -BeExactly "$($ExecutionContext.SessionState.Provider.Get("FileSystem").Home)/foo"
+        cmd /c echo ~\foo | Should -BeExactly "$($ExecutionContext.SessionState.Provider.Get("FileSystem").Home)\foo"
+    }
+	It '~ should not be replaced when quoted' {
+		cmd /c echo '~' | Should -BeExactly '~'
+		cmd /c echo "~" | Should -BeExactly '~'
+		cmd /c echo '~/foo' | Should -BeExactly '~/foo'
+		cmd /c echo "~/foo" | Should -BeExactly '~/foo'
+        cmd /c echo '~\foo' | Should -BeExactly '~\foo'
+		cmd /c echo "~\foo" | Should -BeExactly '~\foo'
+	}
+}
