f3a3266582a5bc5a9fa678e7ba25d27cc5bb4df3
wmarshall <wmarshall@industrialinfo.com>
2024-03-06 14:00:13 -0600

    Added missing semi-colons.

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/Select-Object.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/Select-Object.cs
@@ -551,7 +551,7 @@ namespace Microsoft.PowerShell.Commands
                     // particular PSObject, so that when the user uses the base object else where,
                     // its members remain the same as before the Select-Object command run.
                     PSObject expandedObject = r.Result is PSObject ? 
-                        r.Result.PSObject.Copy() : PSObject.AsPSObject(r.result, true)
+                        r.Result.PSObject.Copy() : PSObject.AsPSObject(r.result, true);
                     AddNoteProperties(expandedObject, inputObject, matchedProperties);
 
                     FilteredWriteObject(expandedObject, matchedProperties);
@@ -572,7 +572,7 @@ namespace Microsoft.PowerShell.Commands
                     // particular PSObject, so that when the user uses the base object else where,
                     // its members remain the same as before the Select-Object command run.
                     PSObject expandedObject = r.Result is PSObject ? 
-                        r.Result.PSObject.Copy() : PSObject.AsPSObject(r.result, true)
+                        r.Result.PSObject.Copy() : PSObject.AsPSObject(r.result, true);
                     AddNoteProperties(expandedObject, inputObject, matchedProperties);
 
                     FilteredWriteObject(expandedObject, matchedProperties);
