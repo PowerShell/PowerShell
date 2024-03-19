c59de1f95dd7fb37960c1b96d18feea289c1c704
Jordan Borean <jborean93@gmail.com>
2024-03-19 20:36:52 +0000

    Redo tests for Pester v4

--- test/powershell/Modules/Microsoft.PowerShell.Security/CertificateProviderAcl.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Security/CertificateProviderAcl.Tests.ps1
@@ -6,16 +6,99 @@ using namespace System.Security.Cryptography
 using namespace System.Security.Cryptography.X509Certificates
 using namespace System.Security.Principal
 
-Describe "Certificate Provider ACL tests" -Tags "CI" {
-    $IsAdmin = $false
-    if ($IsWindows) {
-        $IsAdmin = ([WindowsPrincipal][WindowsIdentity]::GetCurrent()).IsInRole([WindowsBuiltInRole]::Administrator)
-    }
+$IsAdmin = $false
+if ($IsWindows) {
+    $IsAdmin = ([WindowsPrincipal][WindowsIdentity]::GetCurrent()).IsInRole([WindowsBuiltInRole]::Administrator)
+}
 
+$CertCases = @(
+    @{ Case = 'RSA-CryptoAPI' }
+    @{ Case = 'RSA-CNG' }
+    @{ Case = 'ECDSA-CNG' }
+)
+
+Function New-RSACryptoAPICertificate {
+    [OutputType([X509Certificate2])]
+    [CmdletBinding()]
+    param()
+
+    $rsaParams = @{
+        KeyAlgorithm = 'RSA'
+        KeyExportPolicy = 'Exportable'
+        KeyLength = 2048
+        KeySpec = 'Signature'
+        CertStoreLocation = 'Cert:\CurrentUser\My'
+        Provider = 'Microsoft Base Cryptographic Provider v1.0'
+        Subject = 'ACL Test RSA - CryptoAPI'
+
+    }
+    New-SelfSignedCertificate @rsaParams
+}
+
+Function New-RSACNGertificate {
+    [OutputType([X509Certificate2])]
+    [CmdletBinding()]
+    param()
+
+    $rsaParams = @{
+        KeyAlgorithm = 'RSA'
+        KeyExportPolicy = 'Exportable'
+        KeyLength = 2048
+        CertStoreLocation = 'Cert:\CurrentUser\My'
+        Provider = 'Microsoft Software Key Storage Provider'
+        Subject = 'ACL Test RSA - CNG'
+
+    }
+    New-SelfSignedCertificate @rsaParams
+}
+
+Function New-ECDSACNGCertificate {
+    [OutputType([X509Certificate2])]
+    [CmdletBinding()]
+    param()
+
+    $ecdsaParams = @{
+        CertStoreLocation = 'Cert:\CurrentUser\My'
+        KeyUsage = 'DigitalSignature'
+        KeyAlgorithm = 'ECDSA_nistP256'
+        CurveExport = 'CurveName'
+        Subject = 'ACL Test ECDSA - CNG'
+        Type = 'Custom'
+
+    }
+    New-SelfSignedCertificate @ecdsaParams
+}
+
+Function New-CertificateWithoutKey {
+    [OutputType([X509Certificate2])]
+    [CmdletBinding()]
+    param()
+
+    $request = [CertificateRequest]::new(
+        "CN=ACL Test - NoKey",
+        [System.Security.Cryptography.RSA]::Create(2048),
+        "SHA256",
+        [System.Security.Cryptography.RSASignaturePadding]::Pkcs1)
+    $notBefore = [DateTimeOffset]::UtcNow.AddDays(-1)
+    $notAfter = $notBefore.AddDays(30)
+    $testCert = $request.CreateSelfSigned($notBefore, $notAfter)
+    $userStore = Get-Item Cert:\CurrentUser\My
+    $userStore.Open('ReadWrite')
+    $userStore.Add([X509Certificate2]::new($testCert.Export('Cert')))
+    $userStore.Dispose()
+    $certWithoutKey = Get-Item "Cert:\CurrentUser\My\$($testCert.Thumbprint)"
+    $testCert.Dispose()
+
+    $certWithoutKey
+}
+
+Describe "Certificate Provider tests" -Tags CI {
     BeforeAll {
+        $certs = @{}
+
         $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
         if (-not $IsWindows) {
-            $PSDefaultParameterValues["It:Skip"] = -not $IsWindows
+            $PSDefaultParameterValues["It:Skip"] = $true
             return
         }
 
@@ -25,55 +108,17 @@ Describe "Certificate Provider ACL tests" -Tags "CI" {
         $everyoneSid = [SecurityIdentifier]::new('S-1-1-0')
         $everyoneAccount = $everyoneSid.Translate([NTAccount])
 
-        $rsaParams = @{
-            KeyAlgorithm = 'RSA'
-            KeyExportPolicy = 'Exportable'
-            KeyLength = 2048
-            CertStoreLocation = 'Cert:\CurrentUser\My'
-        }
-        $rsaCrypto = New-SelfSignedCertificate @rsaParams -Subject "ACL Test RSA - CryptoAPI" -Provider "Microsoft Base Cryptographic Provider v1.0" -KeySpec Signature
-        $rsaCNG = New-SelfSignedCertificate @rsaParams -Subject "ACL Test RSA - CNG" -Provider "Microsoft Software Key Storage Provider"
-
-        $ecdsaParams = @{
-            CertStoreLocation = 'Cert:\CurrentUser\My'
-            KeyUsage = 'DigitalSignature'
-            KeyAlgorithm = 'ECDSA_nistP256'
-            CurveExport = 'CurveName'
-            Type = 'Custom'
-        }
-        $ecdsaCNG = New-SelfSignedCertificate @ecdsaParams -Subject "ACL Test ECDSA - CNG"
-
-        $request = [CertificateRequest]::new(
-            "CN=ACL Test - NoKey",
-            [System.Security.Cryptography.RSA]::Create(2048),
-            "SHA256",
-            [System.Security.Cryptography.RSASignaturePadding]::Pkcs1)
-        $notBefore = [DateTimeOffset]::UtcNow.AddDays(-1)
-        $notAfter = $notBefore.AddDays(30)
-        $testCert = $request.CreateSelfSigned($notBefore, $notAfter)
-        $userStore = Get-Item Cert:\CurrentUser\My
-        $userStore.Open('ReadWrite')
-        $userStore.Add([X509Certificate2]::new($testCert.Export('Cert')))
-        $userStore.Dispose()
-        $certWithoutKey = Get-Item "Cert:\CurrentUser\My\$($testCert.Thumbprint)"
-        $testCert.Dispose()
-
-        $testCerts = @{
-            'RSA-CryptoAPI' = $rsaCrypto
-            'RSA-CNG' = $rsaCNG
-            'ECDSA-CNG' = $ecdsaCNG
-            'No-Key' = $certWithoutKey
-        }
+        $certs['RSA-CryptoAPI'] = New-RSACryptoAPICertificate
+        $certs['RSA-CNG'] = New-RSACNGertificate
+        $certs['ECDSA-CNG'] = New-ECDSACNGCertificate
+        $certs['RSA-NoKey'] = New-CertificateWithoutKey
     }
 
     AfterAll {
         $global:PSDefaultParameterValues = $originalDefaultParameterValues
-
-        if ($testCerts.Count) {
-            $testCerts.Values | ForEach-Object {
-                # We cannot pipe as -DeleteKey won't work.
-                Remove-Item -LiteralPath $_.PSPath -DeleteKey
-            }
+        foreach ($cert in $certs.Values) {
+            # We cannot pipe as -DeleteKey won't work.
+            Remove-Item -LiteralPath $cert.PSPath -DeleteKey
         }
     }
 
@@ -90,17 +135,13 @@ Describe "Certificate Provider ACL tests" -Tags "CI" {
 
         It "Fails to get ACL on cert without a key" {
             $expected = "Failed to retrieve certificate key handle as certificate has no associated key: *"
-            { Get-Acl -Path $certWithoutKey.PSPath } | Should -Throw
+            { Get-Acl -Path $certs['RSA-NoKey'].PSPath } | Should -Throw
             [string]$Error[0] | Should -BeLike $expected
         }
 
-        It "Gets ACL for <CertName> key" -TestCases @(
-            @{ CertName = 'RSA-CryptoAPI' }
-            @{ CertName = 'RSA-CNG' }
-            @{ CertName = 'ECDSA-CNG' }
-        ) {
-            param ($CertName)
-            $cert = $testCerts[$CertName]
+        It "Gets ACL for <Case> key" -TestCases $CertCases {
+            param ($Case)
+            $cert = $certs[$Case]
 
             $actual = $cert.PSPath | Get-Acl
 
@@ -117,13 +158,9 @@ Describe "Certificate Provider ACL tests" -Tags "CI" {
             $userAccess.PropagationFlags | Should -Be None
         }
 
-        It "Adds and Removes DACL for <CertName> key" -TestCases @(
-            @{ CertName = 'RSA-CryptoAPI' }
-            @{ CertName = 'RSA-CNG' }
-            @{ CertName = 'ECDSA-CNG' }
-        ) {
-            param ($CertName)
-            $cert = $testCerts[$CertName]
+        It "Adds and Removes DACL for <Case> key" -TestCases $CertCases {
+            param ($Case)
+            $cert = $certs[$Case]
 
             $sd = $cert.PSPath | Get-Acl
 
@@ -145,25 +182,17 @@ Describe "Certificate Provider ACL tests" -Tags "CI" {
             $everyoneRule | Should -BeNullOrEmpty
         }
 
-        It "Failed to get SACL for <CertName> key - Non-Admin" -Skip:($IsAdmin) -TestCases @(
-            @{ CertName = 'RSA-CryptoAPI' }
-            @{ CertName = 'RSA-CNG' }
-            @{ CertName = 'ECDSA-CNG' }
-        ) {
-            param ($CertName)
-            $cert = $testCerts[$CertName]
+        It "Failed to get SACL for <Case> key - Non-Admin" -Skip:(-not $IsWindows -or $IsAdmin) -TestCases $CertCases {
+            param ($Case)
+            $cert = $certs[$Case]
 
             { $cert.PSPath | Get-Acl -Audit } | Should -Throw
             [string]$Error[0] | Should -BeLike 'Failed to retrieve certificate key security descriptor: *'
         }
 
-        It "Failed to add SACL for <CertName> key - Non-Admin" -Skip:($IsAdmin) -TestCases @(
-            @{ CertName = 'RSA-CryptoAPI' }
-            @{ CertName = 'RSA-CNG' }
-            @{ CertName = 'ECDSA-CNG' }
-        ) {
-            param ($CertName)
-            $cert = $testCerts[$CertName]
+        It "Failed to add SACL for <Case> key - Non-Admin" -Skip:(-not $IsWindows -or $IsAdmin) -TestCases $CertCases {
+            param ($Case)
+            $cert = $certs[$Case]
 
             $sd = $cert.PSPath | Get-Acl
 
@@ -178,87 +207,9 @@ Describe "Certificate Provider ACL tests" -Tags "CI" {
         }
     }
 
-    Context "*-Acl cmdlets - Admin" -Skip:(-not $IsAdmin) -Tags "CI", "RequireAdminOnWindows" {
-        It "Changes key owner for <CertName> key" -TestCases @(
-            @{ CertName = 'RSA-CryptoAPI' }
-            @{ CertName = 'RSA-CNG' }
-            @{ CertName = 'ECDSA-CNG' }
-        ) {
-            param ($CertName)
-            $cert = $testCerts[$CertName]
-
-            $sd = $cert.PSPath | Get-Acl
-
-            $oldOwner = $sd.GetOwner([SecurityIdentifier])
-            $sd.SetOwner($everyoneSid)
-            try {
-                $sd | Set-Acl -LiteralPath $cert.PSPath
-
-                $actual = $cert.PSPath | Get-Acl
-                $actual.Owner | Should -Be $everyoneAccount.Value
-            } finally {
-                $sd.SetOwner($oldOwner)
-                $sd | Set-Acl -LiteralPath $cert.PSPath
-            }
-        }
-
-        It "Changes key group for <CertName> key" -TestCases @(
-            @{ CertName = 'RSA-CryptoAPI' }
-            @{ CertName = 'RSA-CNG' }
-            @{ CertName = 'ECDSA-CNG' }
-        ) {
-            param ($CertName)
-            $cert = $testCerts[$CertName]
-
-            $sd = $cert.PSPath | Get-Acl
-
-            $oldOwner = $sd.GetGroup([SecurityIdentifier])
-            $sd.SetGroup($everyoneSid)
-            try {
-                $sd | Set-Acl -LiteralPath $cert.PSPath
-
-                $actual = $cert.PSPath | Get-Acl
-                $actual.Group | Should -Be $everyoneAccount.Value
-            } finally {
-                $sd.SetGroup($oldOwner)
-                $sd | Set-Acl -LiteralPath $cert.PSPath
-            }
-        }
-
-        It "Adds and Removes SACL for <CertName> key" -TestCases @(
-            @{ CertName = 'RSA-CryptoAPI' }
-            @{ CertName = 'RSA-CNG' }
-            @{ CertName = 'ECDSA-CNG' }
-        ) {
-            param ($CertName)
-            $cert = $testCerts[$CertName]
-
-            # We should start off without any audit rules.
-            $sd = $cert.PSPath | Get-Acl -Audit
-            $sd.Audit | Should -BeNullOrEmpty
-
-            $newRule = $sd.AuditRuleType::new(
-                $everyoneSid,
-                'Read',
-                'Success')
-            $sd.AddAuditRule($newRule)
-
-            $sd | Set-Acl -LiteralPath $cert.PSPath
-            $addActual = $cert.PSPath | Get-Acl -Audit
-            $everyoneRule = $addActual.Audit | Where-Object IdentityReference -EQ $everyoneAccount
-            $everyoneRule | Should -Not -BeNullOrEmpty
-
-            $sd.RemoveAuditRuleSpecific($newRule)
-            $sd | Set-Acl -LiteralPath $cert.PSPath
-            $removeActual = $cert.PSPath | Get-Acl -Audit
-            $everyoneRule = $removeActual.Audit | Where-Object IdentityReference -EQ $everyoneAccount
-            $everyoneRule | Should -BeNullOrEmpty
-        }
-    }
-
-    Context "Provider API" -Tags "CI" {
+    Context "Provider API" {
         It "Gets empty SD from path" {
-            $actual = $ExecutionContext.InvokeProvider.SecurityDescriptor.NewFromPath($rsaCrypto.PSPath, "All")
+            $actual = $ExecutionContext.InvokeProvider.SecurityDescriptor.NewFromPath($certs['RSA-CryptoAPI'].PSPath, "All")
             $actual | Should -BeOfType ([Microsoft.PowerShell.Commands.CertificateKeySecurity])
         }
 
@@ -266,15 +217,17 @@ Describe "Certificate Provider ACL tests" -Tags "CI" {
             $expected = "*Cannot process argument because the value of argument ""path"" is null*"
             {
                 $ExecutionContext.InvokeProvider.SecurityDescriptor.NewFromPath([NullString]::Value, "All")
-            } | Should -Throw $expected
+            } | Should -Throw
+            [string]$Error[0] | Should -BeLike $expected
         }
 
         It "Fails to get SD from path - invalid sections" {
             $sections = [Enum]::ToObject([System.Security.AccessControl.AccessControlSections], 20)
             $expected = "*Cannot process argument because the value of argument ""includeSections"" is not valid*"
             {
-                $ExecutionContext.InvokeProvider.SecurityDescriptor.NewFromPath($rsaCrypto.PSPath, $sections)
-            } | Should -Throw $expected
+                $ExecutionContext.InvokeProvider.SecurityDescriptor.NewFromPath($certs['RSA-CryptoAPI'].PSPath, $sections)
+            } | Should -Throw
+            [string]$Error[0] | Should -BeLike $expected
         }
 
         It "Gets empty SD from type" {
@@ -286,7 +239,8 @@ Describe "Certificate Provider ACL tests" -Tags "CI" {
             $expected = "*. You cannot get Certificate SecurityDescriptor for the type 'invalid', only the 'Key' type is supported*"
             {
                 $ExecutionContext.InvokeProvider.SecurityDescriptor.NewOfType("certificate", "invalid", "All")
-            } | Should -Throw $expected
+            } | Should -Throw
+            [string]$Error[0] | Should -BeLike $expected
         }
 
         It "Fails to get SD from type - invalid sections" {
@@ -294,16 +248,13 @@ Describe "Certificate Provider ACL tests" -Tags "CI" {
             $expected = "*Cannot process argument because the value of argument ""includeSections"" is not valid*"
             {
                 $ExecutionContext.InvokeProvider.SecurityDescriptor.NewOfType("certificate", "Key", $sections)
-            } | Should -Throw $expected
+            } | Should -Throw
+            [string]$Error[0] | Should -BeLike $expected
         }
 
-        It "Adds and Removes DACL for <CertName> key" -TestCases @(
-            @{ CertName = 'RSA-CryptoAPI' }
-            @{ CertName = 'RSA-CNG' }
-            @{ CertName = 'ECDSA-CNG' }
-        ) {
-            param ($CertName)
-            $cert = $testCerts[$CertName]
+        It "Adds and Removes DACL for <Case> key" -TestCases $CertCases {
+            param ($Case)
+            $cert = $certs[$Case]
 
             $sd = $ExecutionContext.InvokeProvider.SecurityDescriptor.Get($cert.PSPath, 'Access')[0]
 
@@ -329,5 +280,98 @@ Describe "Certificate Provider ACL tests" -Tags "CI" {
             $everyoneRule = $removeActual.Access | Where-Object IdentityReference -EQ $everyoneAccount
             $everyoneRule | Should -BeNullOrEmpty
         }
+    }
+}
+
+Describe "Certificate Provider tests - Admin" -Tags CI, RequireAdminOnWindows {
+    BeforeAll {
+        $certs = @{}
+        $originalDefaultParameterValues = $PSDefaultParameterValues.Clone()
+        if (-not $IsWindows -or -not $IsAdmin) {
+            $PSDefaultParameterValues["It:Skip"] = $true
+            return
+        }
+
+        $currentSid = [WindowsIdentity]::GetCurrent().User
+        $currentAccount = $currentSid.Translate([NTAccount])
+
+        $everyoneSid = [SecurityIdentifier]::new('S-1-1-0')
+        $everyoneAccount = $everyoneSid.Translate([NTAccount])
+
+        $certs['RSA-CryptoAPI'] = New-RSACryptoAPICertificate
+        $certs['RSA-CNG'] = New-RSACNGertificate
+        $certs['ECDSA-CNG'] = New-ECDSACNGCertificate
+    }
+
+    AfterAll {
+        $global:PSDefaultParameterValues = $originalDefaultParameterValues
+        foreach ($cert in $certs.Values) {
+            # We cannot pipe as -DeleteKey won't work.
+            Remove-Item -LiteralPath $cert.PSPath -DeleteKey
+        }
+    }
+
+    It "Changes key owner for <Case> key" -TestCases $CertCases {
+        param ($Case)
+        $cert = $certs[$Case]
+
+        $sd = $cert.PSPath | Get-Acl
+
+        $oldOwner = $sd.GetOwner([SecurityIdentifier])
+        $sd.SetOwner($everyoneSid)
+        try {
+            $sd | Set-Acl -LiteralPath $cert.PSPath
+
+            $actual = $cert.PSPath | Get-Acl
+            $actual.Owner | Should -Be $everyoneAccount.Value
+        } finally {
+            $sd.SetOwner($oldOwner)
+            $sd | Set-Acl -LiteralPath $cert.PSPath
+        }
+    }
+
+    It "Changes key group for <Case> key" -TestCases $CertCases {
+        param ($Case)
+        $cert = $certs[$Case]
+
+        $sd = $cert.PSPath | Get-Acl
+
+        $oldOwner = $sd.GetGroup([SecurityIdentifier])
+        $sd.SetGroup($everyoneSid)
+        try {
+            $sd | Set-Acl -LiteralPath $cert.PSPath
+
+            $actual = $cert.PSPath | Get-Acl
+            $actual.Group | Should -Be $everyoneAccount.Value
+        } finally {
+            $sd.SetGroup($oldOwner)
+            $sd | Set-Acl -LiteralPath $cert.PSPath
+        }
+    }
+
+    It "Adds and Removes SACL for <Case> key" -TestCases $CertCases {
+        param ($Case)
+        $cert = $certs[$Case]
+
+        # We should start off without any audit rules.
+        $sd = $cert.PSPath | Get-Acl -Audit
+        $sd.Audit | Should -BeNullOrEmpty
+
+        $newRule = $sd.AuditRuleType::new(
+            $everyoneSid,
+            'Read',
+            'Success')
+        $sd.AddAuditRule($newRule)
+
+        $sd | Set-Acl -LiteralPath $cert.PSPath
+        $addActual = $cert.PSPath | Get-Acl -Audit
+        $everyoneRule = $addActual.Audit | Where-Object IdentityReference -EQ $everyoneAccount
+        $everyoneRule | Should -Not -BeNullOrEmpty
+
+        $sd.RemoveAuditRuleSpecific($newRule)
+        $sd | Set-Acl -LiteralPath $cert.PSPath
+        $removeActual = $cert.PSPath | Get-Acl -Audit
+        $everyoneRule = $removeActual.Audit | Where-Object IdentityReference -EQ $everyoneAccount
+        $everyoneRule | Should -BeNullOrEmpty
     }
 }
