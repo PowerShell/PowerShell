69233266873df64aa7512c1dfb9bb73065323b9f
James Truher <jimtru@microsoft.com>
2024-04-10 11:57:30 -0700

    Do not check for signature on non-Windows.

--- tools/packaging/packaging.psm1
+++ tools/packaging/packaging.psm1
@@ -905,20 +905,22 @@ function Update-PSSignedBuildFolder
     $signedFilesFilter = Join-Path -Path $SignedFilesPathNormalized -ChildPath '*'
     Write-Verbose -Verbose "signedFilesFilter = $signedFilesFilter"
 
-    Get-ChildItem -Path $signedFilesFilter -Recurse -File | Select-Object -ExpandProperty FullName | ForEach-Object -Process {
-        Write-Verbose -Verbose "Processing $_"
+    $signedFilesList = Get-ChildItem -Path $signedFilesFilter -Recurse -File
+    foreach ($signedFileObject in $signedFilesList) {
+        $signedFilePath = $signedFileObject.FullName
+        Write-Verbose -Verbose "Processing $signedFilePath"
 
         # Agents seems to be on a case sensitive file system
         if ($IsLinux) {
-            $relativePath = $_.Replace($SignedFilesPathNormalized, '')
+            $relativePath = $signedFilePath.Replace($SignedFilesPathNormalized, '')
         } else {
-            $relativePath = $_.ToLowerInvariant().Replace($SignedFilesPathNormalized.ToLowerInvariant(), '')
+            $relativePath = $signedFilePath.ToLowerInvariant().Replace($SignedFilesPathNormalized.ToLowerInvariant(), '')
         }
 
         Write-Verbose -Verbose "relativePath = $relativePath"
         $destination = (Get-Item (Join-Path -Path $BuildPathNormalized -ChildPath $relativePath)).FullName
         Write-Verbose -Verbose "destination = $destination"
-        Write-Log "replacing $destination with $_"
+        Write-Log "replacing $destination with $signedFilePath"
 
         if (-not (Test-Path $destination)) {
             $parent = Split-Path -Path $destination -Parent
@@ -932,13 +934,20 @@ function Update-PSSignedBuildFolder
             Write-Error "File not found: $destination, parent - $parent exists: $exists"
         }
 
-        $signature = Get-AuthenticodeSignature -FilePath $_
-
-        if ($signature.Status -ne 'Valid') {
-            Write-Error "Invalid signature for $_"
+        # Get-AuthenticodeSignature will only work on Windows
+        if ($IsWindows)
+        {
+            $signature = Get-AuthenticodeSignature -FilePath $signedFilePath
+            if ($signature.Status -ne 'Valid') {
+                Write-Error "Invalid signature for $signedFilePath"
+            }
+        }
+        else
+        {
+            Write-Verbose -Verbose "Skipping certificate check of $signedFilePath on non-Windows"
         }
 
-        Copy-Item -Path $_ -Destination $destination -Force
+        Copy-Item -Path $signedFilePath -Destination $destination -Force
     }
 
     foreach($filter in $RemoveFilter) {
