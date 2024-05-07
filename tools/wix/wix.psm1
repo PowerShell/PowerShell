cb2f5438c8c4504bf62b0894dec9fb5fa2e6a92e
Aditya Patwardhan <adityap@microsoft.com>
2024-05-07 12:56:53 -0700

    Actually copy burn.exe

--- tools/wix/wix.psm1
+++ tools/wix/wix.psm1
@@ -87,7 +87,8 @@ function Install-Wix
     Copy-Item -Path $docExpandPath -Destination $docTargetPath -Force
     Copy-Item -Path $sdkExpandPath -Destination $sdkTargetPath -Force
     Copy-Item -Path "$binPath\wix\3.14.1\tools\*" -Destination $binTargetPath -Force
-    Copy-Item -Path $x86ExpandPath -Destination $x86TargetPath -Force -Recurse
+    Copy-Item -Path $x86ExpandPath -Destination $x86TargetPath -Force -Recurse -Verbose
+    Copy-Item -Path "$x86ExpandPath\burn.exe" -Destination "$x86TargetPath\burn.exe" -Force -Verbose
 
     Append-Path -path $binPath
     Write-Verbose "Done installing WIX!"
