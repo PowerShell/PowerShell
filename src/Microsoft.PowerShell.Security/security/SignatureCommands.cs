776b85720fe6ef2c3716f1a5e8ac161501bd33e8
Jordan Borean <jborean93@gmail.com>
2024-05-20 16:09:22 +1000

    Get-AuthenticodeSignature: Add embedded cert opt
    Added the ability to retrieve an embedded authenticode signture of a
    file that ignores any certificates inside a .cat file. This is done
    through a new switch parameter -EmbeddedOnly on the
    Get-AuthenticodeSignature cmdlet.

--- src/Microsoft.PowerShell.Security/security/SignatureCommands.cs
+++ src/Microsoft.PowerShell.Security/security/SignatureCommands.cs
@@ -264,6 +264,14 @@ namespace Microsoft.PowerShell.Commands
     public sealed class GetAuthenticodeSignatureCommand : SignatureCommandsBase
     {
         /// <summary>
+        /// Property that sets the switch parameter that decides whether to retrieve embedded signatures only or
+        /// also signatures inside a catalogue file.
+        /// </summary>
+        [Parameter(ParameterSetName = "ByPath")]
+        [Parameter(ParameterSetName = "ByLiteralPath")]
+        public SwitchParameter EmbeddedSignature { get; set; }
+
+        /// <summary>
         /// Initializes a new instance of the GetSignatureCommand class.
         /// </summary>
         public GetAuthenticodeSignatureCommand() : base("Get-AuthenticodeSignature") { }
@@ -279,7 +287,7 @@ namespace Microsoft.PowerShell.Commands
         /// </returns>
         protected override Signature PerformAction(string filePath)
         {
-            return SignatureHelper.GetSignature(filePath, null);
+            return SignatureHelper.GetSignature(filePath, null, embeddedSignatureOnly: EmbeddedSignature);
         }
 
         /// <summary>
