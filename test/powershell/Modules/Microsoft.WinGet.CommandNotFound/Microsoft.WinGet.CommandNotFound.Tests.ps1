9b1e9a2621fe43f4a82770e5858270fa065220ce
Carlos Zamora <carlos.zamora@microsoft.com>
2024-07-02 17:53:34 -0700

    fix test

--- test/powershell/Modules/Microsoft.WinGet.CommandNotFound/Microsoft.WinGet.CommandNotFound.Tests.ps1
+++ test/powershell/Modules/Microsoft.WinGet.CommandNotFound/Microsoft.WinGet.CommandNotFound.Tests.ps1
@@ -1,16 +1,21 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 Describe "Microsoft.WinGet.CommandNotFound" -tags "CI" {
+    BeforeAll {
+        if (Get-Module Microsoft.WinGet.CommandNotFound) {
+            Remove-Module Microsoft.WinGet.CommandNotFound
+        }
+    }
 
-    Context "Windows only" -Skip:(-not $IsWindows) {
-        It "Should import the module correctly" {
+    Context "Windows only" {
+        It "Should import the module correctly" -Skip:(-not $IsWindows) {
             Import-Module -Name Microsoft.WinGet.CommandNotFound
             $module = Get-Module Microsoft.WinGet.CommandNotFound
             $module.Name | Should -BeExactly 'Microsoft.WinGet.CommandNotFound'
             $module.Version | Should -Match '^1.0.4$'
         }
 
-        It "Should be installed to `$PSHOME" {
+        It "Should be installed to `$PSHOME" -Skip:(-not $IsWindows) {
             $module = Get-Module (Join-Path -Path $PSHOME -ChildPath "Modules" -AdditionalChildPath "Microsoft.WinGet.CommandNotFound") -ListAvailable
             $module.Name | Should -BeExactly 'Microsoft.WinGet.CommandNotFound'
             $module.Version | Should -Match '^1.0.4$'
@@ -18,9 +23,13 @@ Describe "Microsoft.WinGet.CommandNotFound" -tags "CI" {
         }
     }
 
-    Context "Linux and macOS only" -Skip:($IsWindows) {
-        It "Should not be installed to `$PSHOME" {
+    Context "Linux and macOS only" {
+        It "Should not be installed to `$PSHOME" -Skip:($IsWindows) {
             Test-Path (Join-Path -Path $PSHOME -ChildPath "Modules" -AdditionalChildPath "Microsoft.WinGet.CommandNotFound") | Should -Be $False
         }
+    }
+
+    AfterAll {
+        Remove-Module Microsoft.WinGet.CommandNotFound
     }
 }
