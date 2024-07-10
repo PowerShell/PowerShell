13b2eee387a50c80a12f7e1f1519b25091e27c53
Carlos Zamora <carlos.zamora@microsoft.com>
2024-07-10 13:07:13 -0700

    update BOM, apply feedback on Pester tests

--- test/powershell/Modules/Microsoft.WinGet.CommandNotFound/Microsoft.WinGet.CommandNotFound.Tests.ps1
+++ test/powershell/Modules/Microsoft.WinGet.CommandNotFound/Microsoft.WinGet.CommandNotFound.Tests.ps1
@@ -9,27 +9,27 @@ Describe "Microsoft.WinGet.CommandNotFound" -tags "CI" {
 
     Context "Windows only" {
         It "Should import the module correctly" -Skip:(-not $IsWindows) {
-            Import-Module -Name Microsoft.WinGet.CommandNotFound
-            $module = Get-Module Microsoft.WinGet.CommandNotFound
-            $module.Name | Should -BeExactly 'Microsoft.WinGet.CommandNotFound'
-            $module.Version | Should -Match '^1.0.4$'
+            try {
+                $module = Import-Module -Name Microsoft.WinGet.CommandNotFound -PassThru
+                $module.Name | Should -BeExactly 'Microsoft.WinGet.CommandNotFound'
+                $module.Version | Should -BeGreaterThan "1.0.4"
+            }
+            finally {
+                Get-Module -Name Microsoft.WinGet.CommandNotFound | Remove-Module -Force
+            }
         }
 
         It "Should be installed to `$PSHOME" -Skip:(-not $IsWindows) {
             $module = Get-Module (Join-Path -Path $PSHOME -ChildPath "Modules" -AdditionalChildPath "Microsoft.WinGet.CommandNotFound") -ListAvailable
             $module.Name | Should -BeExactly 'Microsoft.WinGet.CommandNotFound'
-            $module.Version | Should -Match '^1.0.4$'
+            $module.Version | Should -BeGreaterThan "1.0.4"
             $module.Path | Should -Be (Join-Path -Path $PSHOME -ChildPath "Modules/Microsoft.WinGet.CommandNotFound/Microsoft.WinGet.CommandNotFound.psd1")
         }
     }
 
     Context "Linux and macOS only" {
-        It "Should not be installed to `$PSHOME" -Skip:($IsWindows) {
-            Test-Path (Join-Path -Path $PSHOME -ChildPath "Modules" -AdditionalChildPath "Microsoft.WinGet.CommandNotFound") | Should -Be $False
+        It "Should not be installed" -Skip:($IsWindows) {
+            Get-Module -Name Microsoft.WinGet.CommandNotFound -ListAvailable | Should -BeNullOrEmpty
         }
-    }
-
-    AfterAll {
-        Remove-Module Microsoft.WinGet.CommandNotFound
     }
 }
