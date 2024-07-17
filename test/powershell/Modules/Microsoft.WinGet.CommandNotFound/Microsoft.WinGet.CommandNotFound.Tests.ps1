4c1aefaab3296b7a36106000a0edb70a3001747e
Carlos Zamora <carlos.zamora@microsoft.com>
2024-07-17 13:44:06 -0700

    simplify mock call

--- test/powershell/Modules/Microsoft.WinGet.CommandNotFound/Microsoft.WinGet.CommandNotFound.Tests.ps1
+++ test/powershell/Modules/Microsoft.WinGet.CommandNotFound/Microsoft.WinGet.CommandNotFound.Tests.ps1
@@ -9,7 +9,7 @@ Describe "Microsoft.WinGet.CommandNotFound" -tags "CI" {
 
     Context "Windows only" {
         # Microsoft.WinGet.CommandNotFound relies on winget being available
-        Mock -CommandName winget -MockWith {}
+        Mock -CommandName winget
 
         It "Should import the module correctly" -Skip:(-not $IsWindows) {
             try {
