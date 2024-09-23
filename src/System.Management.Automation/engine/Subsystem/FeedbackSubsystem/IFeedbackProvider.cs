91c96f3186525e455faffbc1327a1958545cd424
Aditya Patwardhan <adityap@microsoft.com>
2024-09-23 10:55:16 -0700

    Add telemetry to track the use of features (#24247) (#24331)

--- src/System.Management.Automation/engine/Subsystem/FeedbackSubsystem/IFeedbackProvider.cs
+++ src/System.Management.Automation/engine/Subsystem/FeedbackSubsystem/IFeedbackProvider.cs
@@ -9,6 +9,7 @@ using System.Management.Automation.Internal;
 using System.Management.Automation.Language;
 using System.Management.Automation.Runspaces;
 using System.Threading;
+using Microsoft.PowerShell.Telemetry;
 
 namespace System.Management.Automation.Subsystem.Feedback
 {
@@ -287,13 +288,14 @@ namespace System.Management.Automation.Subsystem.Feedback
                     .AddParameter("ExpandProperty", "Name")
                 .Invoke<string>();
 
-            if (results.Count > 0)
-            {
-                return new FeedbackItem(
-                    SuggestionStrings.Suggestion_CommandNotFound,
-                    new List<string>(results),
-                    FeedbackDisplayLayout.Landscape);
-            }
+			if (results.Count > 0)
+			{
+				ApplicationInsightsTelemetry.SendUseTelemetry("FuzzyMatching", "CommandNotFound");
+				return new FeedbackItem(
+					SuggestionStrings.Suggestion_CommandNotFound,
+					new List<string>(results),
+					FeedbackDisplayLayout.Landscape);
+			}
 
             return null;
         }
