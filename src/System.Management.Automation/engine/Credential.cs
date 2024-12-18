41b6582e38c55431c03d064b2c63ccb6a07ee060
M Hickford <mirth.hickford@gmail.com>
2024-12-18 15:39:05 +0000

    Add PSCredential.ToString

--- src/System.Management.Automation/engine/Credential.cs
+++ src/System.Management.Automation/engine/Credential.cs
@@ -295,6 +295,8 @@ namespace System.Management.Automation
 #pragma warning restore 56506
         }
 
+        public string ToString() => $"PSCredential(UserName={UserName})";
+
         /// <summary>
         /// Gets an empty PSCredential.  This is an PSCredential with both UserName
         /// and Password initialized to null.
