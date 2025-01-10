25b30a53b0d73f6441967807b6a9f86b40df6128
Anam Navied <anam.naviyou@gmail.com>
2025-01-10 15:22:23 -0500

    update service connection name in EV2 test task

--- .pipelines/EV2Specs/ServiceGroupRoot/Shell/Run/Run.ps1
+++ .pipelines/EV2Specs/ServiceGroupRoot/Shell/Run/Run.ps1
@@ -4,6 +4,12 @@ if ($null -eq $env:MAPPING_FILE)
     return 1
 }
 
+if ($null -eq $env:REPO_LIST_FILE)
+{
+    Write-Verbose -Verbose "REPO_LIST_FILE variable didn't get passed correctly"
+    return 1
+}
+
 if ($null -eq $env:PWSH_PACKAGES_TARGZIP)
 {
     Write-Verbose -Verbose "PWSH_PACKAGES_TARGZIP variable didn't get passed correctly"
@@ -17,12 +23,9 @@ if ($null -eq $env:PMC_METADATA)
 }
 
 try {
-    Write-Verbose -Verbose "mapping.json file: $env:MAPPING_FILE"
-    Write-Verbose -Verbose "pwsh packages.tar.gz file: $env:PWSH_PACKAGES_TARGZIP"
-    Write-Verbose -Verbose "pmcMetadata.json file: $env:PMC_METADATA"
-
     Write-Verbose -Verbose "Downloading files"
     Invoke-WebRequest -Uri $env:MAPPING_FILE -OutFile mapping.json
+    Invoke-WebRequest -Uri $env:REPO_LIST_FILE -OutFile repoList.json
     Invoke-WebRequest -Uri $env:PWSH_PACKAGES_TARGZIP -OutFile packages.tar.gz
     Invoke-WebRequest -Uri $env:PMC_METADATA -OutFile pmcMetadata.json
 
@@ -42,6 +45,109 @@ try {
 
     which pmc
     pmc -d -c $settingsFile repo list --name "azurelinux-3.0-prod-ms-oss-x86_64-yum" || exit 1
+
+    $metadataFilePath = Join-Path -Path "/package/unarchive/" -ChildPath "pmcMetadata.json"
+    $metadataContent = Get-Content -Path $metadataFilePath | ConvertFrom-Json
+    $releaseVersion = $metadataContent.ReleaseTag
+
+
+
+    $repoListFilePath = Join-Path -Path "/package/unarchive/" -ChildPath "repoList.json"
+    $repoListPathExists = Test-Path $repoListFilePath
+    if (!$repoListPathExists)
+    {
+        Write-Verbose -Verbose "repoList.json expected at $repoListFilePath does not exist"
+        return 1
+    }
+
+    $repoList = Get-RepoList -RepoConfigPath $configPath -OutFile $repoListPath #TODO: seems to require repoclient cli tool
+
+    # Get-PackageInfo()
+    $mappingFilePath = Join-Path -Path "/package/unarchive/" -ChildPath "mapping.json"
+    $mappingPathExists = Test-Path $mappingFilePath
+    if (!$mappingPathExists)
+    {
+        Write-Verbose -Verbose "mapping.json expected at $mappingFilePath does not exist"
+        return 1
+    }
+
+    $mapping = Get-Content -Raw -LiteralPath $mappingFilePath | ConvertFrom-Json -AsHashtable
+    foreach ($package in $mapping.Packages)
+    {
+        Write-Verbose "package: $package"
+        $packageChannel = $package.channel
+        if (!$packageChannel) {
+            $packageChannel = 'all'
+        }
+
+        Write-Verbose "package channel: $packageChannel"
+        if ($packageChannel -eq 'all' -or $packageChannel -eq $Channel) {
+            $repoIds = [System.Collections.Generic.List[string]]::new()
+            $packageFormat = $package.PackageFormat
+            Write-Verbose "package format: $packageFormat" -Verbose
+            $extension = [System.io.path]::GetExtension($packageFormat)
+            $packageType = $extension -replace '^\.'
+
+            if ($package.distribution.count -gt 1) {
+                throw "Package $($package | out-string) has more than one Distribution."
+            }
+
+            foreach ($distribution in $package.distribution) {
+                $urlGlob = $package.url
+                switch ($packageType) {
+                    'deb' {
+                        $urlGlob = $urlGlob + '-apt'
+                    }
+                    'rpm' {
+                        $urlGlob = $urlGlob + '-yum'
+                    }
+                    default {
+                        throw "Unknown package type: $packageType"
+                    }
+                }
+
+                Write-Verbose "---Finding repo id for: $urlGlob---" -Verbose
+
+                $repos = $repoList | Where-Object { $_.name -eq $urlGlob }
+
+                if ($repos.id) {
+                    Write-Verbose "Found repo id: $($repos.id)" -Verbose
+                    $repoIds.AddRange(([string[]]$repos.id))
+                }
+                else {
+                    Write-Failure "Could not find repo for $urlGlob"
+                }
+            }
+
+            if ($repoIds.Count -le 0) {
+                Write-Verbose -Verbose "no repoIds found that match our packages"
+                # Write-Output ($package + @{ "RepoId" = $repoIds.ToArray() })
+            }
+
+            foreach ($pkg in $Package) {
+                if ($pkg.RepoId.count -gt 1) {
+                    throw "Package $($pkg.name) has more than one repo id."
+                }
+                if ($pkg.Distribution.count -gt 1) {
+                    throw "Package $($pkg.name) has more than one Distribution."
+                }
+                $repo = $pkg.RepoId | Select-Object -First 1
+                $distribution = $pkg.Distribution | Select-Object -First 1
+                foreach ($name in $PackageName) {
+                        $pkgName = $pkg.PackageFormat.Replace('PACKAGE_NAME', $name).Replace('POWERSHELL_RELEASE', $ReleaseVersion)
+                        Write-Verbose "Creating info object for package '$pkgName' for repo '$repo'"
+                    $result = [pscustomobject]@{
+                        PackageName  = $pkgName
+                        RepoId       = $repo
+                        Distribution = $distribution
+                    }
+                    Write-Verbose $result -Verbose
+                    Write-Output $result
+                }
+            }
+        }
+    }
+
 
 }
 catch {
