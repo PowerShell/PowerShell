39a233c11d93f5b1da40f997677e8db1aa8c0fff
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2025-01-17 22:24:45 +0000

    revert addition of virtual

--- src/System.Management.Automation/utils/CryptoUtils.cs
+++ src/System.Management.Automation/utils/CryptoUtils.cs
@@ -583,7 +583,7 @@ namespace System.Management.Automation.Internal
             System.GC.SuppressFinalize(this);
         }
 
-        protected virtual void Dispose(bool disposing)
+        private void Dispose(bool disposing)
         {
             if (disposing)
             {
