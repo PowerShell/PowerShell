28cfd47bae9053c2c9b28de8e3018f9a13792193
Dongbo Wang <dongbow@microsoft.com>
2025-01-23 11:54:39 -0800

    Clean up

--- src/System.Management.Automation/engine/parser/Parser.cs
+++ src/System.Management.Automation/engine/parser/Parser.cs
@@ -14,7 +14,6 @@ using System.Management.Automation.Subsystem;
 using System.Management.Automation.Subsystem.DSC;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Dsc = Microsoft.PowerShell.DesiredStateConfiguration.Internal;
 
