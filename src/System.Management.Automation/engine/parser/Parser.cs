b681f55a40e1d6d5b6bc7756106ffb40af3bbbcd
bdeb1337 <bdeb1337@jng.li>
2025-01-23 19:07:18 +0100

    Check process architecture instead of processor architecture

--- src/System.Management.Automation/engine/parser/Parser.cs
+++ src/System.Management.Automation/engine/parser/Parser.cs
@@ -3004,8 +3004,8 @@ namespace System.Management.Automation.Language
                         dropIntoDebugger: true);
                 }
 
-                // Configuration is not supported on ARM64
-                if (PsUtils.IsRunningOnProcessorArchitectureARM())
+                // Configuration is not supported for ARM or ARM64 binaries
+                if (RuntimeInformation.ProcessArchitecture == Architecture.Arm || RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
                 {
                     ReportError(
                         configurationToken.Extent,
