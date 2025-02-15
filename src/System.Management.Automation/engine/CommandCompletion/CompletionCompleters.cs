d35365dfea067f4db270aa0de623dc70438e3ae4
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2025-02-15 02:08:30 +0000

    Enable `SA1014`: Opening generic brackets should not be preceded by a space
    https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/SA1014.md

--- src/System.Management.Automation/engine/CommandCompletion/CompletionCompleters.cs
+++ src/System.Management.Automation/engine/CommandCompletion/CompletionCompleters.cs
@@ -4737,7 +4737,7 @@ namespace System.Management.Automation
                 {
                     basePath = null;
                 }
-                IEnumerable <FileSystemInfo> fileSystemObjects = containersOnly
+                IEnumerable<FileSystemInfo> fileSystemObjects = containersOnly
                     ? dirInfo.EnumerateDirectories(filterText, enumerationOptions)
                     : dirInfo.EnumerateFileSystemInfos(filterText, enumerationOptions);
 
