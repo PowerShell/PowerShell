d4821152ddaaa83977d53c8928c643def7390748
GameMicrowave <131776171+GameMicrowave@users.noreply.github.com>
2025-04-06 05:09:40 +0800

    Update ResolvePathCommand.cs
    make -relativebase parameter treat path as literal
    fix error occurs if path contains wildcard character

--- src/Microsoft.PowerShell.Commands.Management/commands/management/ResolvePathCommand.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/ResolvePathCommand.cs
@@ -129,7 +129,7 @@ namespace Microsoft.PowerShell.Commands
             {
                 try
                 {
-                    _relativeBasePath = SessionState.Internal.Globber.GetProviderPath(RelativeBasePath, CmdletProviderContext, out _, out _relativeDrive);
+                    _relativeBasePath = SessionState.Path.GetUnresolvedProviderPathFromPSPath(_relativeBasePath, CmdletProviderContext, out _, out _relativeDrive);
                 }
                 catch (ProviderNotFoundException providerNotFound)
                 {
@@ -190,7 +190,7 @@ namespace Microsoft.PowerShell.Commands
                         try
                         {
                             SessionState.Path.PushCurrentLocation(string.Empty);
-                            _ = SessionState.Path.SetLocation(_relativeBasePath);
+                            _ = SessionState.Path.SetLocation(_relativeBasePath, CmdletProviderContext, true);
                             result = SessionState.Path.GetResolvedPSPathFromPSPath(path, CmdletProviderContext);
                         }
                         finally
