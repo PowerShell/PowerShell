8e6dbd7439d65e1f387ed445005669c89fa446b0
Travis Plunk <github@ez13.net>
2025-04-07 10:56:26 -0700

    Enhance error logging in Write-Log function to escape newlines for GitHub Actions compatibility

--- build.psm1
+++ build.psm1
@@ -2711,10 +2711,16 @@ function script:Write-Log
     )
     if ($isError)
     {
-        Write-Host -Foreground Red $message
-        if($env:GITHUB_WORKFLOW)
-        {
-            Write-Host "::error::${message}"
+        if ($env:GITHUB_WORKFLOW) {
+            # https://github.com/actions/toolkit/issues/193#issuecomment-605394935
+            $escapedMessage = $message -replace "`n", "%0A" -replace "`r"
+            Write-Host "::error::${escapedMessage}"
+
+            if ($escapedMessage -ne $message) {
+                Write-Host -Foreground Red $message
+            }
+        } else {
+            Write-Host -Foreground Red $message
         }
     }
     else
