b3e68d3d55c01e7db43c1504a41cda9f54a287a9
Justin Grote <15258962+JustinGrote@users.noreply.github.com.>
2025-05-16 10:15:36 -0700

    Supress progress cluttering the codespaces prebuild logs

--- .devcontainer/scripts/1-onCreate.ps1
+++ .devcontainer/scripts/1-onCreate.ps1
@@ -1,4 +1,7 @@
 #Docs: https://containers.dev/implementors/json_reference/#lifecycle-scripts
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
+
 . $PSScriptRoot/shared.ps1
 Import-Module -Force ./build.psm1
 
--- .devcontainer/scripts/4-postStart.ps1
+++ .devcontainer/scripts/4-postStart.ps1
@@ -1,4 +1,6 @@
 #Docs: https://containers.dev/implementors/json_reference/#lifecycle-scripts
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
 
 . $PSScriptRoot/shared.ps1
 Import-Module -Force ./build.psm1
