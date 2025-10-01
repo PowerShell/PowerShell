31cd731926dcd81cce010f4e3c0f202b9f079eba
Aditya Patwardhan <adityap@microsoft.com>
2025-10-01 14:09:34 -0700

    Remove UseDotnet task and use the dotnet-install script  (#26093)

--- build.psm1
+++ build.psm1
@@ -2092,8 +2092,8 @@ function Install-Dotnet {
     # Note that when it is null, Invoke-Expression (but not &) must be used to interpolate properly
     $sudo = if (!$NoSudo) { "sudo" }
 
-    # $installObtainUrl = "https://dot.net/v1"
-    $installObtainUrl = "https://dotnet.microsoft.com/download/dotnet/scripts/v1"
+    $installObtainUrl = "https://builds.dotnet.microsoft.com/dotnet/scripts/v1"
+    #$installObtainUrl = "https://dotnet.microsoft.com/download/dotnet/scripts/v1"
     $uninstallObtainUrl = "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain"
 
     # Install for Linux and OS X
@@ -2184,7 +2184,6 @@ function Install-Dotnet {
             $installArgs += @{ SkipNonVersionedFiles = $true }
 
             $installArgs | Out-String | Write-Verbose -Verbose
-
             & ./$installScript @installArgs
         }
         else {
