dc27097ad9a0f20e0929459cd1d4d0fd82e23246
MartinGC94 <MGC94@outlook.dk>
2025-10-02 14:19:46 +0200

    Add delimiter completer

--- src/Microsoft.PowerShell.Commands.Management/commands/management/GetClipboardCommand.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/GetClipboardCommand.cs
@@ -2,8 +2,10 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Management.Automation;
+using System.Management.Automation.Language;
 using Microsoft.PowerShell.Commands.Internal;
 
 namespace Microsoft.PowerShell.Commands
@@ -38,6 +40,7 @@ namespace Microsoft.PowerShell.Commands
         /// The delimiters to use when splitting the clipboard content.
         /// </summary>
         [Parameter]
+        [ArgumentCompleter(typeof(DelimiterCompleter))]
         public string[] Delimiter { get; set; } = [Environment.NewLine];
 
         private bool _raw;
@@ -78,6 +81,36 @@ namespace Microsoft.PowerShell.Commands
             }
 
             return result;
+        }
+    }
+
+    /// <summary>
+    /// Provides argument completion for the Delimiter parameter.
+    /// </summary>
+    public sealed class DelimiterCompleter : IArgumentCompleter
+    {
+        /// <summary>
+        /// Provides argument completion for the Delimiter parameter.
+        /// </summary>
+        /// <param name="commandName">The name of the command that is being completed.</param>
+        /// <param name="parameterName">The name of the parameter that is being completed.</param>
+        /// <param name="wordToComplete">The input text to filter the results by.</param>
+        /// <param name="commandAst">The ast of the command that triggered the completion.</param>
+        /// <param name="fakeBoundParameters">The parameters bound to the command.</param>
+        /// <returns>Completion results.</returns>
+        public IEnumerable<CompletionResult> CompleteArgument(string commandName, string parameterName, string wordToComplete, CommandAst commandAst, IDictionary fakeBoundParameters)
+        {
+            wordToComplete ??= string.Empty;
+            var pattern = new WildcardPattern(wordToComplete + '*', WildcardOptions.IgnoreCase);
+            if (pattern.IsMatch("CRLF") || pattern.IsMatch("Windows"))
+            {
+                yield return new CompletionResult("\"`r`n\"", "CRLF", CompletionResultType.ParameterValue, "Windows (CRLF)");
+            }
+
+            if (pattern.IsMatch("LF") || pattern.IsMatch("Unix") || pattern.IsMatch("Linux"))
+            {
+                yield return new CompletionResult("\"`n\"", "LF", CompletionResultType.ParameterValue, "UNIX (LF)");
+            }
         }
     }
 }
