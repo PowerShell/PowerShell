cd908b64d3fae4931ee9d54128fb99b35ed5a04c
kzrnm <gengesa@gmail.com>
2025-10-18 03:24:10 +0900

    Update splat tests

--- test/powershell/Language/Scripting/NativeExecution/NativeWindowsTildeExpansion.Tests.ps1
+++ test/powershell/Language/Scripting/NativeExecution/NativeWindowsTildeExpansion.Tests.ps1
@@ -6,6 +6,8 @@ Describe 'Native Windows tilde expansion tests' -tags "CI" {
         $originalDefaultParams = $PSDefaultParameterValues.Clone()
         $PSDefaultParameterValues["it:skip"] = -Not $IsWindows
         $EnabledExperimentalFeatures.Contains('PSNativeWindowsTildeExpansion') | Should -BeTrue
+        $HomeDir = $ExecutionContext.SessionState.Provider.Get("FileSystem").Home
+        $Tilde = "~"
     }
 
     AfterAll {
@@ -13,20 +15,56 @@ Describe 'Native Windows tilde expansion tests' -tags "CI" {
     }
 
     # Test ~ expansion
-    It 'Tilde should be replaced by the filesystem provider home directory' {
-        cmd /c echo ~ | Should -BeExactly ($ExecutionContext.SessionState.Provider.Get("FileSystem").Home)
+    It '~ should be replaced by the filesystem provider home directory <arg>' -testCases @(
+        @{arg = '~';            Expected = $HomeDir }
+        @{arg = '$Tilde';       Expected = $HomeDir }
+        @{arg = '~/foo';        Expected = "$HomeDir/foo" }
+        @{arg = '~\foo';        Expected = "$HomeDir\foo" }
+        @{arg = '$Tilde/foo';   Expected = "$HomeDir/foo" }
+        @{arg = '$Tilde\foo';   Expected = "$HomeDir\foo" }
+    ) {
+        param($arg, $Expected)
+        Invoke-Expression "cmd /c echo $arg" | Should -BeExactly $Expected
+        Invoke-Expression "testexe -echoargs $arg" | Should -BeExactly "Arg 0 is <$Expected>"
     }
-    # Test ~ expansion with a path fragment (e.g. ~/foo)
-    It '~/foo should be replaced by the <filesystem provider home directory>/foo' {
-        cmd /c echo ~/foo | Should -BeExactly "$($ExecutionContext.SessionState.Provider.Get("FileSystem").Home)/foo"
-        cmd /c echo ~\foo | Should -BeExactly "$($ExecutionContext.SessionState.Provider.Get("FileSystem").Home)\foo"
+    It '~ should not be replaced when quoted <arg>' -testCases @(
+        @{arg = "'~'";          Expected = "~" }
+        @{arg = "'~/foo'";      Expected = "~/foo" }
+        @{arg = "'~\foo'";      Expected = "~\foo" }
+        @{arg = '"~"';          Expected = "~" }
+        @{arg = '"~/foo"';      Expected = "~/foo" }
+        @{arg = '"~\foo"';      Expected = "~\foo" }
+        @{arg = '"$Tilde"';     Expected = "~" }
+        @{arg = '"$Tilde/foo"'; Expected = "~/foo" }
+        @{arg = '"$Tilde\foo"'; Expected = "~\foo" }
+    ) {
+        param($arg, $Expected)
+        Invoke-Expression "cmd /c echo $arg" | Should -BeExactly $Expected
+        Invoke-Expression "testexe -echoargs $arg" | Should -BeExactly "Arg 0 is <$Expected>"
     }
-	It '~ should not be replaced when quoted' {
-		cmd /c echo '~' | Should -BeExactly '~'
-		cmd /c echo "~" | Should -BeExactly '~'
-		cmd /c echo '~/foo' | Should -BeExactly '~/foo'
-		cmd /c echo "~/foo" | Should -BeExactly '~/foo'
-        cmd /c echo '~\foo' | Should -BeExactly '~\foo'
-		cmd /c echo "~\foo" | Should -BeExactly '~\foo'
-	}
+    It '~ should keep its literal meaning when splatted <splattingArgs>'-testCases @(
+        @{
+            splattingArgs = @'
+~ ~/foo ~\foo '~' "~" '~/foo' "~/foo" '~\foo' "~\foo"
+'@;
+            Expected = @("$HomeDir", "$HomeDir/foo", "$HomeDir\foo", "~", "~", "~/foo", "~/foo", "~\foo", "~\foo")
+        }
+        @{
+            splattingArgs = @'
+$Tilde $Tilde/foo $Tilde\foo "$Tilde" "$Tilde/foo" "$Tilde\foo"
+'@;
+            Expected = @("$HomeDir", "$HomeDir/foo", "$HomeDir\foo", "~", "~/foo", "~\foo")
+        }
+    ) {
+        param($splattingArgs, $Expected)
+        function Invoke-Cmd {
+            cmd @args
+        }
+        function Invoke-TestExe {
+            testexe @args
+        }
+
+        Invoke-Expression "Invoke-Cmd /c echo $splattingArgs" | Should -BeExactly ($Expected -join ' ')
+        Invoke-Expression "Invoke-TestExe -echoargs $splattingArgs" | Should -BeExactly @($Expected | ForEach-Object { $i = 0 } { "Arg {0} is <$_>" -f $i++ } )
+    }
 }
