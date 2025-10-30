1e545a939ed5aa2b8c51eead15c269579d271bf6
Dongbo Wang <dongbow@microsoft.com>
2025-10-30 14:26:03 -0700

    Cherry-pick PR #26348 with conflicts for manual resolution

--- src/System.Management.Automation/engine/ExperimentalFeature/ExperimentalFeature.cs
+++ src/System.Management.Automation/engine/ExperimentalFeature/ExperimentalFeature.cs
@@ -22,8 +22,6 @@ namespace System.Management.Automation
 
         internal const string EngineSource = "PSEngine";
         internal const string PSFeedbackProvider = "PSFeedbackProvider";
-        internal const string PSNativeWindowsTildeExpansion = nameof(PSNativeWindowsTildeExpansion);
-        internal const string PSRedirectToVariable = "PSRedirectToVariable";
         internal const string PSSerializeJSONLongEnumAsNumber = nameof(PSSerializeJSONLongEnumAsNumber);
 
         #endregion
@@ -108,21 +106,21 @@ namespace System.Management.Automation
                     description: "Replace the old FileSystemProvider with cleaner design and faster code"),
                 */
                 new ExperimentalFeature(
-                    name: "PSSubsystemPluginModel",
-                    description: "A plugin model for registering and un-registering PowerShell subsystems"),
-                new ExperimentalFeature(
                     name: "PSLoadAssemblyFromNativeCode",
                     description: "Expose an API to allow assembly loading from native code"),
                 new ExperimentalFeature(
                     name: PSFeedbackProvider,
                     description: "Replace the hard-coded suggestion framework with the extensible feedback provider"),
                 new ExperimentalFeature(
+<<<<<<< HEAD
                     name: PSNativeWindowsTildeExpansion,
                     description: "On windows, expand unquoted tilde (`~`) with the user's current home folder."),
                 new ExperimentalFeature(
                     name: PSRedirectToVariable,
                     description: "Add support for redirecting to the variable drive"),
                 new ExperimentalFeature(
+=======
+>>>>>>> 943257b24 (Make some experimental features stable (#26348))
                     name: PSSerializeJSONLongEnumAsNumber,
                     description: "Serialize enums based on long or ulong as an numeric value rather than the string representation when using ConvertTo-Json."
                 )
