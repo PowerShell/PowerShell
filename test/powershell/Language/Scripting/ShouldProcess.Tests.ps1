5d8a7d1967ba62bf852ba561520b79fc1c7dfb28
yotsuda <ytsuda@gmail.com>
2025-11-23 00:03:38 +0900

    Add test for -Verbose:$false and fix VerbosePreference state management
    - Add test case for -Verbose:$false taking precedence over $VerbosePreference
    - Save and restore $VerbosePreference in all test cases using try-finally blocks
    - Clean up whitespace

--- test/powershell/Language/Scripting/ShouldProcess.Tests.ps1
+++ test/powershell/Language/Scripting/ShouldProcess.Tests.ps1
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 Describe "ShouldProcess respects VerbosePreference" -Tags "CI","Feature" {
-    
+
     BeforeAll {
         function Test-ShouldProcess {
             [CmdletBinding(SupportsShouldProcess)]
@@ -13,34 +13,60 @@ Describe "ShouldProcess respects VerbosePreference" -Tags "CI","Feature" {
 
     Context "VerbosePreference variable" {
         It "Emits verbose output when VerbosePreference is Continue" {
-            $VerbosePreference = 'Continue'
-            $output = Test-ShouldProcess 4>&1
-            $VerbosePreference = 'SilentlyContinue'
-            
-            $output | Where-Object { $_ -eq "OK" } | Should -Not -BeNullOrEmpty
-            $verboseOutput = $output | Where-Object { $_ -is [System.Management.Automation.VerboseRecord] }
-            $verboseOutput | Should -Not -BeNullOrEmpty
-            $verboseOutput.Message | Should -Match "TestAction.*TestTarget"
+            $originalVerbosePreference = $VerbosePreference
+            try {
+                $VerbosePreference = 'Continue'
+                $output = Test-ShouldProcess 4>&1
+
+                $output | Where-Object { $_ -eq "OK" } | Should -Not -BeNullOrEmpty
+                $verboseOutput = $output | Where-Object { $_ -is [System.Management.Automation.VerboseRecord] }
+                $verboseOutput | Should -Not -BeNullOrEmpty
+                $verboseOutput.Message | Should -Match "TestAction.*TestTarget"
+            }
+            finally {
+                $VerbosePreference = $originalVerbosePreference
+            }
         }
 
         It "Does not emit verbose output when VerbosePreference is SilentlyContinue" {
-            $VerbosePreference = 'SilentlyContinue'
-            $output = Test-ShouldProcess 4>&1
-            
-            $output | Where-Object { $_ -eq "OK" } | Should -Not -BeNullOrEmpty
-            $verboseOutput = $output | Where-Object { $_ -is [System.Management.Automation.VerboseRecord] }
-            $verboseOutput | Should -BeNullOrEmpty
+            $originalVerbosePreference = $VerbosePreference
+            try {
+                $VerbosePreference = 'SilentlyContinue'
+                $output = Test-ShouldProcess 4>&1
+
+                $output | Where-Object { $_ -eq "OK" } | Should -Not -BeNullOrEmpty
+                $verboseOutput = $output | Where-Object { $_ -is [System.Management.Automation.VerboseRecord] }
+                $verboseOutput | Should -BeNullOrEmpty
+            }
+            finally {
+                $VerbosePreference = $originalVerbosePreference
+            }
         }
     }
 
     Context "Verbose parameter" {
         It "Emits verbose output when -Verbose is specified" {
             $output = Test-ShouldProcess -Verbose 4>&1
-            
+
             $output | Where-Object { $_ -eq "OK" } | Should -Not -BeNullOrEmpty
             $verboseOutput = $output | Where-Object { $_ -is [System.Management.Automation.VerboseRecord] }
             $verboseOutput | Should -Not -BeNullOrEmpty
             $verboseOutput.Message | Should -Match "TestAction.*TestTarget"
+        }
+
+        It "Does not emit verbose output when -Verbose:`$false is specified even if VerbosePreference is Continue" {
+            $originalVerbosePreference = $VerbosePreference
+            try {
+                $VerbosePreference = 'Continue'
+                $output = Test-ShouldProcess -Verbose:$false 4>&1
+
+                $output | Where-Object { $_ -eq "OK" } | Should -Not -BeNullOrEmpty
+                $verboseOutput = $output | Where-Object { $_ -is [System.Management.Automation.VerboseRecord] }
+                $verboseOutput | Should -BeNullOrEmpty
+            }
+            finally {
+                $VerbosePreference = $originalVerbosePreference
+            }
         }
     }
 }
