ad1aa09470337057b6283195862e2e275c302569
Dongbo Wang <dongbow@microsoft.com>
2025-12-03 15:57:32 -0800

    Add more debug messages

--- src/System.Management.Automation/engine/Modules/ModuleIntrinsics.cs
+++ src/System.Management.Automation/engine/Modules/ModuleIntrinsics.cs
@@ -1272,6 +1272,12 @@ namespace System.Management.Automation
 
         private static string UpdatePath(string path, string pathToAdd, ref int insertIndex)
         {
+            Console.WriteLine("---- UpdatePath ----");
+            Console.WriteLine("path: " + path);
+            Console.WriteLine("pathToAdd: " + pathToAdd);
+            Console.WriteLine("insertIndex: " + insertIndex);
+            Console.WriteLine("-------- END --------");
+
             if (!string.IsNullOrEmpty(pathToAdd))
             {
                 path = AddToPath(path, pathToAdd, insertIndex);
