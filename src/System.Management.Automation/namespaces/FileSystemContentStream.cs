f6378d300c615005a985d2320ddd29a9fa6241ad
ArmaanMcleod <armaan_mcleod@outlook.com>
2025-12-06 04:10:34 +1100

    Simplify delimiter condition to if/else

--- src/System.Management.Automation/namespaces/FileSystemContentStream.cs
+++ src/System.Management.Automation/namespaces/FileSystemContentStream.cs
@@ -1187,17 +1187,16 @@ namespace Microsoft.PowerShell.Commands
                 }
                 else if (_usingDelimiter)
                 {
-                    // Prepend delimiter for all lines except first one if we suppress last delimiter
-                    if (_suppressLastDelimiter && !_isFirstLine)
+                    bool prependDelimiter = _suppressLastDelimiter && !_isFirstLine;
+
+                    if (prependDelimiter)
                     {
                         _writer.Write(_delimiter);
+                        _writer.Write(contentToWrite);
                     }
-
-                    _writer.Write(contentToWrite);
-
-                    // Always append delimter if we don't suppress last delimiter
-                    if (!_suppressLastDelimiter)
+                    else
                     {
+                        _writer.Write(contentToWrite);
                         _writer.Write(_delimiter);
                     }
                 }
