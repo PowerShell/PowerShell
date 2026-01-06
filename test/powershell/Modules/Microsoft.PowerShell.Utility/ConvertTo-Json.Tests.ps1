13e911d2531d03afd8a36a0f5b062ecc5734e0ad
yotsuda <ytsuda@gmail.com>
2026-01-06 16:20:57 +0900

    Add test for non-pure PSObject with ETS properties at depth exceeded

--- test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Json.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/ConvertTo-Json.Tests.ps1
@@ -1,4 +1,4 @@
-﻿# Copyright (c) Microsoft Corporation.
+# Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 Describe 'ConvertTo-Json' -tags "CI" {
     BeforeAll {
@@ -401,6 +401,19 @@ Describe 'ConvertTo-Json' -tags "CI" {
 
             $json = $nested | ConvertTo-Json -Depth 2 -Compress -WarningAction SilentlyContinue
             $json | Should -BeExactly '{"inner":{"Name":"test","ETSProp":"ets"}}'
+        }
+
+        It 'Non-pure PSObject (FileInfo) with ETS properties includes ETS when depth exceeded' {
+            $file = Get-Item $PSHOME/pwsh.exe
+            $pso = [PSObject]::new($file)
+            $pso | Add-Member -NotePropertyName CustomExt -NotePropertyValue 'extended_value'
+            $outer = [PSCustomObject]@{ File = $pso }
+
+            $result = $outer | ConvertTo-Json -Depth 0 -WarningAction SilentlyContinue
+            $parsed = $result | ConvertFrom-Json
+
+            $parsed.File.CustomExt | Should -BeExactly 'extended_value'
+            $parsed.File.value | Should -BeLike '*pwsh.exe'
         }
 
     }
