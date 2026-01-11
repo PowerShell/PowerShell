cfd6cf6c85441879d8aa14939caaf3da67f09a7e
Andy Bodnar <bodnar970@outlook.com>
2026-01-11 13:26:37 -0700

    Add New-TemporaryDirectory cmdlet
    Adds a new cmdlet that creates a temporary directory, following the same pattern as New-TemporaryFile. This is useful for users in Constrained Language Mode who cannot use [System.IO] directly.
    
    The cmdlet generates a unique directory name using Path.GetRandomFileName() and creates it in the system temp path.
    
    Fixes #25754

---
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/NewTemporaryDirectoryCommand.cs
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.IO;
+using System.Management.Automation;
+
+namespace Microsoft.PowerShell.Commands
+{
+    /// <summary>
+    /// The implementation of the "New-TemporaryDirectory" cmdlet.
+    /// </summary>
+    [Cmdlet(VerbsCommon.New, "TemporaryDirectory", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Low,
+        HelpUri = "https://go.microsoft.com/fwlink/?LinkId=2097032")]
+    [OutputType(typeof(System.IO.DirectoryInfo))]
+    public class NewTemporaryDirectoryCommand : Cmdlet
+    {
+        /// <summary>
+        /// Returns a TemporaryDirectory.
+        /// </summary>
+        protected override void EndProcessing()
+        {
+            string tempPath = Path.GetTempPath();
+            if (ShouldProcess(tempPath))
+            {
+                string directoryPath = Path.Combine(tempPath, Path.GetRandomFileName());
+
+                try
+                {
+                    DirectoryInfo directory = Directory.CreateDirectory(directoryPath);
+                    WriteObject(directory);
+                }
+                catch (IOException ioException)
+                {
+                    ThrowTerminatingError(
+                        new ErrorRecord(
+                            ioException,
+                            "NewTemporaryDirectoryWriteError",
+                            ErrorCategory.WriteError,
+                            tempPath));
+                }
+            }
+        }
+    }
+}
