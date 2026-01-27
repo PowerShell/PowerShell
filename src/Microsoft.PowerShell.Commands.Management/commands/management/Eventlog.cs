95aae26c5eb301ce25f97c4e9b1203235e02dacf
anamnavi <anam.naviyou@gmail.com>
2026-01-27 18:00:46 -0500

    Get System special folder from SafeDeriveFromSpecialFolder() which ensures the configured location will be returned regardless of if it has been created already

--- src/Microsoft.PowerShell.Commands.Management/commands/management/Eventlog.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/Eventlog.cs
@@ -1128,8 +1128,7 @@ namespace Microsoft.PowerShell.Commands
         {
             try
             {
-                string eventVwrExe = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System),
-                    "eventvwr.exe");
+                string eventVwrExe = Platform.SafeDeriveFromSpecialFolder(Environment.SpecialFolder.System, @"eventvwr.exe");
                 // codeql[cs/microsoft/command-line-injection] - This is expected PowerShell behavior where user inputted ComputerName is supported for the context of this method. The user assumes trust for the computer name they specify for event logging, and the process is on the user's system except for remoting in which case restricted remoting security guidelines should be used.
                 Process.Start(eventVwrExe, ComputerName);
             }
