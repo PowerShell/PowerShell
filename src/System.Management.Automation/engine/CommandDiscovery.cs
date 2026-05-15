c529770aa8df162ebf7803024ea8ce701346eff3
Aditya Patwardhan <adityap@microsoft.com>
2026-05-15 11:50:24 -0700

    [release/v7.4.16] Fix checks for local user config file paths (#27454)

--- src/System.Management.Automation/engine/CommandDiscovery.cs
+++ src/System.Management.Automation/engine/CommandDiscovery.cs
@@ -1221,11 +1221,17 @@ namespace System.Management.Automation
                         string tempDir = directory.TrimStart();
                         if (tempDir.EqualsOrdinalIgnoreCase("~"))
                         {
-                            tempDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                            tempDir = Environment.GetFolderPath(
+                                Environment.SpecialFolder.UserProfile,
+                                Environment.SpecialFolderOption.DoNotVerify);
                         }
                         else if (tempDir.StartsWith("~" + Path.DirectorySeparatorChar))
                         {
-                            tempDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + Path.DirectorySeparatorChar + tempDir.Substring(2);
+                            tempDir = Environment.GetFolderPath(
+                                Environment.SpecialFolder.UserProfile,
+                                Environment.SpecialFolderOption.DoNotVerify)
+                                + Path.DirectorySeparatorChar
+                                + tempDir.Substring(2);
                         }
 
                         _cachedPath.Add(tempDir);
