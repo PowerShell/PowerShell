0e8dd320b5585655048a0e44e646bb3c17716b77
yotsuda <ytsuda@gmail.com>
2026-06-23 11:19:02 +0900

    Seal TestProgressRawUI test helper and move it to its own file
    The xUnit build was failing with CA1852 (type can be sealed). Seal the TestProgressRawUI helper and move it into its own file, which also resolves CodeFactor's 'single type per file' on test_ProgressNode.cs. Remove the now-unused usings from test_ProgressNode.cs.

--- test/xUnit/csharp/test_ProgressNode.cs
+++ test/xUnit/csharp/test_ProgressNode.cs
@@ -1,10 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
 using System.Collections;
 using System.Management.Automation;
-using System.Management.Automation.Host;
 using Microsoft.PowerShell;
 using Xunit;
 
@@ -329,52 +327,5 @@ namespace PSTests.Parallel
                 actualWidth <= maxWidth,
                 $"Surrogate pair test width {maxWidth}: width {actualWidth} exceeds {maxWidth}. Output: [{output}]");
         }
-    }
-
-    /// <summary>
-    /// Test helper class that implements PSHostRawUserInterface for progress bar testing.
-    /// Delegates width calculations to ConsoleControl.LengthInBufferCells.
-    /// </summary>
-    internal class TestProgressRawUI : PSHostRawUserInterface
-    {
-        public override ConsoleColor ForegroundColor { get; set; }
-
-        public override ConsoleColor BackgroundColor { get; set; }
-
-        public override Coordinates CursorPosition { get; set; }
-
-        public override Coordinates WindowPosition { get; set; }
-
-        public override int CursorSize { get; set; }
-
-        public override Size BufferSize { get; set; }
-
-        public override Size WindowSize { get; set; }
-
-        public override Size MaxWindowSize => new Size(120, 50);
-
-        public override Size MaxPhysicalWindowSize => new Size(120, 50);
-
-        public override string WindowTitle { get; set; }
-
-        public override bool KeyAvailable => false;
-
-        public override BufferCell[,] GetBufferContents(Rectangle rectangle) => throw new NotImplementedException();
-
-        public override void SetBufferContents(Rectangle rectangle, BufferCell fill) => throw new NotImplementedException();
-
-        public override void SetBufferContents(Coordinates origin, BufferCell[,] contents) => throw new NotImplementedException();
-
-        public override void ScrollBufferContents(Rectangle source, Coordinates destination, Rectangle clip, BufferCell fill) => throw new NotImplementedException();
-
-        public override KeyInfo ReadKey(ReadKeyOptions options) => throw new NotImplementedException();
-
-        public override void FlushInputBuffer() => throw new NotImplementedException();
-
-        public override int LengthInBufferCells(string str) => ConsoleControl.LengthInBufferCells(str, 0, checkEscapeSequences: true);
-
-        public override int LengthInBufferCells(string str, int offset) => ConsoleControl.LengthInBufferCells(str, offset, checkEscapeSequences: true);
-
-        public override int LengthInBufferCells(char c) => ConsoleControl.LengthInBufferCells(c);
     }
 }
