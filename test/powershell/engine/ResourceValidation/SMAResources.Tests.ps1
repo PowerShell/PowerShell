62ce1852d22a845c581800b2f75539197dd623a0
Dongbo Wang <dongbow@microsoft.com>
2026-07-20 11:15:50 -0700

    Fix test

--- test/powershell/engine/ResourceValidation/SMAResources.Tests.ps1
+++ test/powershell/engine/ResourceValidation/SMAResources.Tests.ps1
@@ -3,5 +3,11 @@
 . "$PSScriptRoot/TestRunner.ps1"
 $AssemblyName = "System.Management.Automation"
 
+# The 'EventResource.resx' is embedded in S.M.A.dll. However, we are not using its strings from a CSharp
+# binding class, but directly using 'ResourceManager.GetString' in 'SysLogProvider.cs' instead.
+# So, the CSharp binding class generated from 'ResGen' doesn't get compiled in S.M.A.dll, and hence we
+# should exclude this resource from this test.
+$excludeList = @("EventResource.resx")
+
 # run the tests
-Test-ResourceStrings -AssemblyName $AssemblyName
+Test-ResourceStrings -AssemblyName $AssemblyName -ExcludeList $excludeList
