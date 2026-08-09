0d61bd2db1a30fd5f7a5f48b9308d2cd77a0e899
Guillermo López <guillermo.lopez@outlook.com>
2026-08-09 16:03:34 +0200

    Add test

--- test/powershell/Modules/Microsoft.PowerShell.Core/Get-Module.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Core/Get-Module.Tests.ps1
@@ -444,6 +444,12 @@ Describe 'Get-Module -ListAvaiable -(FullyQualifiedName|Name) <path> when argume
             $actual[0].Path | Should -BeExactly "$pwd\existing\0.0.1\existing.psd1"
             $actual[1].Path | Should -BeExactly "$pwd\existing2\0.0.1\existing2.psd1"
         }
+
+        # TODO: This looks like a bug.
+        It 'wrongly returns $null for existing manifest modules when using the -FullyQualifiedName parameter' {
+            $actual = Get-Module -ListAvailable -FullyQualifiedName "$pwd\existing*"
+            $actual | Should -Be $null
+        }
     }
 }
 
