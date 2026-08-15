56519a40deca28289b8edc95f5041f2ed1679131
Dongbo Wang <dongbow@microsoft.com>
2026-08-14 17:06:00 -0700

    Fix the regression in tab completing positional parameters

--- src/System.Management.Automation/engine/CommandCompletion/CompletionCompleters.cs
+++ src/System.Management.Automation/engine/CommandCompletion/CompletionCompleters.cs
@@ -1738,15 +1738,22 @@ namespace System.Management.Automation
             int position,
             Dictionary<string, AstParameterArgumentPair> boundArguments = null)
         {
-            bool isProcessedAsPositional = false;
             bool isDefaultParameterSetValid = defaultParameterSetFlag != 0 &&
                                               (defaultParameterSetFlag & validParameterSetFlags) != 0;
-            MergedCompiledCommandParameter positionalParam = null;
 
-            MergedCompiledCommandParameter bestMatchParam = null;
-            ParameterSetSpecificMetadata bestMatchSet = null;
+            // Find all the parameters with the position closest to the specified position. Different parameter
+            // sets can declare a parameter at the same position, so there can be more than one candidate.
+            // Only parameters from the still valid parameter sets are considered: 'GetMatchingParameterSetData'
+            // filters the parameter set data by 'validParameterSetFlags', and the default parameter set is
+            // given priority only when it's still valid.
+            // The candidate from the default parameter set is tried first, and when it doesn't produce any
+            // completion results, the candidates from the other parameter sets are tried in turn.
+            // For example, 'Get-Process | ForEach-Object <Tab>' should complete member names for '-MemberName'
+            // (PropertyAndMethodSet) because '-Process' (the default 'ScriptBlockSet') has nothing to offer.
+            int bestPosition = int.MaxValue;
+            MergedCompiledCommandParameter defaultSetParam = null;
+            List<MergedCompiledCommandParameter> alternativeParams = null;
 
-            // Finds the parameter with the position closest to the specified position
             foreach (MergedCompiledCommandParameter param in parameters)
             {
                 bool isInParameterSet = (param.Parameter.ParameterSetFlags & validParameterSetFlags) != 0 || param.Parameter.IsInAllSets;
@@ -1755,6 +1762,7 @@ namespace System.Management.Automation
                     continue;
                 }
 
+                bool addedToAltParamList = false;
                 var parameterSetDataCollection = param.Parameter.GetMatchingParameterSetData(validParameterSetFlags);
 
                 foreach (ParameterSetSpecificMetadata parameterSetData in parameterSetDataCollection)
@@ -1774,64 +1782,100 @@ namespace System.Management.Automation
                         continue;
                     }
 
-                    if (bestMatchSet is null
-                        || bestMatchSet.Position > positionInParameterSet
-                        || (isDefaultParameterSetValid && positionInParameterSet == bestMatchSet.Position && defaultParameterSetFlag == parameterSetData.ParameterSetFlag))
+                    if (positionInParameterSet > bestPosition)
                     {
-                        bestMatchParam = param;
-                        bestMatchSet = parameterSetData;
-                        if (positionInParameterSet == position)
-                        {
-                            break;
-                        }
-                    }
-                }
-            }
-
-            if (bestMatchParam is not null)
-            {
-                if (isDefaultParameterSetValid)
-                {
-                    if (bestMatchSet.ParameterSetFlag == defaultParameterSetFlag)
-                    {
-                        ProcessParameter(commandName, commandAst, context, result, bestMatchParam, boundArguments);
-                        isProcessedAsPositional = result.Count > 0;
-                    }
-                    else
-                    {
-                        positionalParam ??= bestMatchParam;
-                    }
-                }
-                else
-                {
-                    isProcessedAsPositional = true;
-                    ProcessParameter(commandName, commandAst, context, result, bestMatchParam, boundArguments);
-                }
-            }
-
-            if (!isProcessedAsPositional && positionalParam != null)
-            {
-                isProcessedAsPositional = true;
-                ProcessParameter(commandName, commandAst, context, result, positionalParam, boundArguments);
-            }
-
-            if (!isProcessedAsPositional)
-            {
-                foreach (MergedCompiledCommandParameter param in parameters)
-                {
-                    bool isInParameterSet = (param.Parameter.ParameterSetFlags & validParameterSetFlags) != 0 || param.Parameter.IsInAllSets;
-                    if (!isInParameterSet)
+                        // A parameter closer to the specified position was already found.
                         continue;
+                    }
 
-                    var parameterSetDataCollection = param.Parameter.GetMatchingParameterSetData(validParameterSetFlags);
-                    foreach (ParameterSetSpecificMetadata parameterSetData in parameterSetDataCollection)
+                    if (positionInParameterSet < bestPosition)
                     {
-                        // in the second pass, we check the remaining argument ones
-                        if (parameterSetData.ValueFromRemainingArguments)
+                        // This parameter is closer to the specified position, so the candidates found so far are no longer relevant.
+                        bestPosition = positionInParameterSet;
+                        defaultSetParam = null;
+                        alternativeParams?.Clear();
+                        addedToAltParamList = false;
+                    }
+
+                    // Prioritize the parameter from the default set. A parameter in all sets is considered from the default set if the
+                    // default set is still valid. But, we prioritize it only if we have not found a default param yet.
+                    // If 'defaultSetParam' is not null, then that means there are 2 parameters in the default set with the same position.
+                    // That would be invalid parameter declaration, but we tolerate that in tab completion.
+                    if (isDefaultParameterSetValid
+                        && (parameterSetData.IsInAllSets || parameterSetData.ParameterSetFlag == defaultParameterSetFlag)
+                        && defaultSetParam is null)
+                    {
+                        defaultSetParam = param;
+
+                        if (addedToAltParamList)
                         {
-                            ProcessParameter(commandName, commandAst, context, result, param, boundArguments);
+                            // If we already added the param to the list when processing a previous set, remove it from the list.
+                            alternativeParams.RemoveAt(alternativeParams.Count - 1);
+                        }
+
+                        if (bestPosition == position)
+                        {
+                            // If it's the exact position, no need to go through the rest of the sets for this parameter.
                             break;
                         }
+
+                        // We still need to go through the rest of the sets in case that the position in any of them is closer.
+                        // But for the same position from a different set, no need to add the param to the list anymore.
+                        addedToAltParamList = true;
+                    }
+                    else if (!addedToAltParamList)
+                    {
+                        addedToAltParamList = true;
+                        (alternativeParams ??= new()).Add(param);
+                    }
+                }
+            }
+
+            if (defaultSetParam is not null)
+            {
+                ProcessParameter(commandName, commandAst, context, result, defaultSetParam, boundArguments);
+                if (result.Count > 0)
+                {
+                    return;
+                }
+            }
+
+            if (alternativeParams?.Count > 0)
+            {
+                // There are alternative parameters at the same best position.
+                // Process them in the discovery order.
+                foreach (MergedCompiledCommandParameter param in alternativeParams)
+                {
+                    ProcessParameter(commandName, commandAst, context, result, param, boundArguments);
+                    if (result.Count > 0)
+                    {
+                        // Stop at the first one that we successfully get completion results.
+                        break;
+                    }
+                }
+
+                // We will return here no matter we get completion results or not, because we did find
+                // applicable positional parameters, and we have done processing them.
+                return;
+            }
+
+            // If we found no applicable positional parameter, then try the remaining argument parameters.
+            foreach (MergedCompiledCommandParameter param in parameters)
+            {
+                bool isInParameterSet = (param.Parameter.ParameterSetFlags & validParameterSetFlags) != 0 || param.Parameter.IsInAllSets;
+                if (!isInParameterSet)
+                {
+                    continue;
+                }
+
+                var parameterSetDataCollection = param.Parameter.GetMatchingParameterSetData(validParameterSetFlags);
+                foreach (ParameterSetSpecificMetadata parameterSetData in parameterSetDataCollection)
+                {
+                    // in the second pass, we check the remaining argument ones
+                    if (parameterSetData.ValueFromRemainingArguments)
+                    {
+                        ProcessParameter(commandName, commandAst, context, result, param, boundArguments);
+                        break;
                     }
                 }
             }
