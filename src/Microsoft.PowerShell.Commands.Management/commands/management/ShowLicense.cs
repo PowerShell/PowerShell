e54166d1ad6167359cf9f698249b56229ba2b33d
dev12124 <joaoanandalima@gmail.com>
2026-09-13 07:06:02 -0300

    Update ShowLicense.cs

--- src/Microsoft.PowerShell.Commands.Management/commands/management/ShowLicense.cs
+++ src/Microsoft.PowerShell.Commands.Management/commands/management/ShowLicense.cs
@@ -5,14 +5,14 @@ using System;
 
 using System.Management.Automation;
 
-namespace Microsoft.PowerShell.Commands 
+namespace Microsoft.PowerShell.Commands
 {
     // --- The Class of Module: Show the License ---
     [Cmdlet(VerbsCommon.Show, "PowerShellLicense")]
-    public class ShowLicense : Cmdlet 
+    public class ShowLicense : Cmdlet
     {
         protected override void ProcessRecord()
-         {
+        {
             string SystemLicense = "Copyright (C) Microsoft Corporation and licensed under the MIT License.";
             WriteObject(SystemLicense);
         }
