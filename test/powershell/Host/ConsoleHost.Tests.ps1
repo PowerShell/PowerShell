70e26e50110c59e14a983b324d18c58e2324c0cc
Dongbo Wang <dongbow@microsoft.com>
2026-09-25 11:04:21 -0700

    Fix the `PATH` test for MSIX installed PowerShell (#28068)

--- test/powershell/Host/ConsoleHost.Tests.ps1
+++ test/powershell/Host/ConsoleHost.Tests.ps1
@@ -1156,11 +1156,23 @@ Describe 'Pwsh startup and PATH' -Tag CI {
         $version | Should -BeExactly "PowerShell $($PSVersionTable.GitCommitId)"
     }
 
-    It 'pwsh starts even if PATH is not defined' {
+    It 'pwsh updates PATH correctly even if PATH is not defined' {
         $pwsh = Join-Path -Path $PSHOME -ChildPath "pwsh"
         Remove-Item Env:\PATH
         $path = & $pwsh -noprofile -command '$env:PATH'
-        $path | Should -BeExactly ($PSHOME + [System.IO.Path]::PathSeparator)
+
+        $ExpectedPath = $PSHOME
+        if ($PSHOME.StartsWith("$env:ProgramFiles\WindowsApps\Microsoft.PowerShell")) {
+            # MSIX installed PowerShell uses the stable path instead of PSHOME.
+            $ExpectedPath = $PSHOME.Replace($env:ProgramFiles, "$env:LOCALAPPDATA\Microsoft")
+
+            # Remove the version-specific part from the expected path.
+            $version = $PSVersionTable.PSVersion
+            $verStrRegex = "$($version.Major)\.$($version.Minor)\.$($version.Build).+__"
+            $ExpectedPath = $ExpectedPath -replace $verStrRegex, ''
+        }
+
+        $path | Should -BeExactly ($ExpectedPath + [System.IO.Path]::PathSeparator)
     }
 }
 
